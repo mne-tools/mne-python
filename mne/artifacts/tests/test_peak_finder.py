83f50662d5fcabf910ae3614520533b402f01061
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2011-05-05 18:11:31 -0400

    TEST : better test in artifacts

--- mne/artifacts/tests/test_peak_finder.py
+++ mne/artifacts/tests/test_peak_finder.py
@@ -1,8 +1,10 @@
 import numpy as np
 from ..peak_finder import peak_finder
+from numpy.testing import assert_array_equal
+
 
 def test_peak_finder():
     """Test the peak detection method"""
     x = [0, 2, 5, 0, 6, -1]
     peak_inds, peak_mags = peak_finder(x)
-    print peak_inds
+    assert_array_equal(peak_inds, [2, 4])
