981467339a989b2c6697e3204f58b604b273434f
mluessi@nmr.mgh.harvard.edu <mluessi@nmr.mgh.harvard.edu>
2012-06-27 03:38:26 -0400

    bump version number

--- mne/__init__.py
+++ mne/__init__.py
@@ -1,7 +1,7 @@
 """MNE for MEG and EEG data analysis
 """
 
-__version__ = '0.3'
+__version__ = '0.3.git'
 
 from .cov import read_cov, write_cov, Covariance, \
                  compute_raw_data_covariance, compute_covariance
