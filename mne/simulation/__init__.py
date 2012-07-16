a075c404edfca01f8d346c0d4c56f59effc6d0c9
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2012-07-16 19:44:43 +0200

    rename sim_evoked.py

--- mne/simulation/__init__.py
+++ mne/simulation/__init__.py
@@ -1,5 +1,5 @@
 """Data simulation code
 """
 
-from .sim_evoked import select_source_in_label, generate_sparse_stc, \
+from .source import select_source_in_label, generate_sparse_stc, \
                         generate_evoked
