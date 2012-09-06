a823730d151afebe8f1099d242a383e5493fa292
dengemann <denis.engemann@gmail.com>
2012-09-06 10:42:46 +0200

    Neater implementation of plotting functions  - use of private functions  - included mne.baseline.py  - updated examples

--- examples/time_frequency/plot_tfr_topography.py
+++ examples/time_frequency/plot_tfr_topography.py
@@ -12,12 +12,13 @@ Plot time-frequency representations on topographies for MEG sensors
 
 print __doc__
 
+import numpy as np
 import pylab as pl
-
+import mne
 from mne import fiff
+from mne.time_frequency import induced_power
 from mne.layouts import Layout
-from mne.viz import plot_topo_tfr
-import pylab as pl
+from mne.viz import plot_topo_power, plot_topo_phase_lock
 from mne.datasets import sample
 data_path = sample.data_path('..')
 
@@ -32,9 +33,9 @@ events = mne.read_events(event_fname)
 include = []
 exclude = raw.info['bads'] + ['MEG 2443', 'EEG 053']  # bads + 2 more
 
-# picks MEG gradiometers
+# picks MEG gr  adiometers
 picks = fiff.pick_types(raw.info, meg='grad', eeg=False, eog=True,
-                                stim=False, include=include, exclude=exclude)
+                        stim=False, include=include, exclude=exclude)
 
 epochs = mne.Epochs(raw, events, event_id, tmin, tmax, picks=picks,
                     baseline=(None, 0), reject=dict(grad=4000e-13, eog=150e-6))
@@ -51,22 +52,32 @@ power, phase_lock = induced_power(data, Fs=Fs, frequencies=frequencies,
                                   n_cycles=n_cycles, n_jobs=1, use_fft=False,
                                   decim=decim, zero_mean=True)
 
+
+###############################################################################
+# Prepare topgraphy plots
+
 layout = Layout('Vectorview-all')
 
-###############################################################################
-# Show topography of power (pe patient, may this take some time)
+baseline = (None, 0)  # set the baseline for induced power
 
-plot_topo_tfr(epochs, power, frequencies, lauyout , is_power=True, decim=decim)
-title = '%s - MNE sample data' %"Induced power"
+mode = 'ratio'  # set mode for baseline rescaling 
+
+###############################################################################
+# Show topography of power (be patient, may this take some time)
+
+plot_topo_power(epochs, power, frequencies, layout, baseline=baseline,
+                mode=mode, decim=decim)
+title = 'Induced power - MNE sample data'
 pl.figtext(0.03, 0.93, title, color='w', fontsize=18)
 pl.show()
 
 
 ###############################################################################
-# Show topography of phase_locking value (pe patient, this may take some time)
+# Show topography of phase_locking value (be patient, this may take some time)
 
-plot_topo_tfr(epochs, phase_lock, frequencies, lauyout , is_power=False, decim=decim)
-title = '%s - MNE sample data' %"Phase locking value"
+plot_topo_phase_lock(epochs, phase_lock, frequencies, layout,
+                     baseline=baseline, mode=mode, decim=decim)
+
+title = 'Phase locking value - MNE sample data'
 pl.figtext(0.03, 0.93, title, color='w', fontsize=18)
 pl.show()
-
