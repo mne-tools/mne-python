42e339e3dba9fc2e6ace3c30c7e1a78242a514db
dengemann <denis.engemann@gmail.com>
2012-09-09 23:35:20 +0200

    ...

--- mne/fiff/raw.py
+++ mne/fiff/raw.py
@@ -1,6 +1,7 @@
 # Authors: Alexandre Gramfort <gramfort@nmr.mgh.harvard.edu>
 #          Matti Hamalainen <msh@nmr.mgh.harvard.edu>
 #          Martin Luessi <mluessi@nmr.mgh.harvard.edu>
+#          Denis Engemann <d.engemann@fz-juelich.de>
 #
 # License: BSD (3-clause)
 
