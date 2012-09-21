a5a42da9f5db3be10f0f2eb917a3e46f4dd00077
Eric Larson <larson.eric.d@gmail.com>
2012-09-21 15:09:49 -0700

    Conforming to standards

--- mne/layouts/make_eeg_layout.py
+++ mne/layouts/make_eeg_layout.py
@@ -1,40 +1,40 @@
 from ..preprocessing import maxfilter
 from ..fiff.constants import FIFF
+from ..fiff import pick_types
 import numpy as np
-from math import pi
 
-def make_eeg_layout(info, output_file, prad=20, width=5, height=4):
-    """
+def make_eeg_layout(info, output_file, radius=20, width=5, height=4):
+    """Create .lout file from EEG electrode digitization
+
     Parameters
     ----------
-    info : (e.g., from raw.info)
-        Has info['ch_names'] and info['dig'] including EEG channels
+    info : dict
+        Measurement info (e.g., raw.info)
     output_file : string
         Filename to save the layout file to.
-    prad: float
+    radius: float
         Viewport radius
     width: float
         Viewport width
     height: float
         Viewport height
     """
-    radius, origin_head, origin_device = maxfilter.fit_sphere_to_headshape(info)
-    hsp = [p['r'] for p in info['dig'] if p['kind'] == FIFF.FIFFV_POINT_EEG]
-    names = [p for p in info['ch_names'] if 'EEG' in p]
-    if len(hsp) <= 1:
-      raise ValueError('No non-reference EEG digitization points found')
+    radius_head, origin_head, origin_device = maxfilter.fit_sphere_to_headshape(info)
+    inds = pick_types(info, meg=False, eeg=True)
+    hsp = [info['chs'][ii]['eeg_loc'][:,0] for ii in inds]
+    names = [info['chs'][ii]['ch_name'] for ii in inds]
+    if len(hsp) <= 0:
+        raise ValueError('No EEG digitization points found')
 
-    # Get rid of reference
-    if not len(hsp) == len(names)+1:
-      raise ValueError('Channel names don\'t match digitization values')
+    if not len(hsp) == len(names):
+        raise ValueError('Channel names don\'t match digitization values')
     hsp = np.array(hsp)
-    hsp = hsp[1:,:]
 
     # Move points to origin
-    hsp = hsp - origin_head/1e3
+    hsp -= origin_head/1e3 # convert to millimeters
 
     # Calculate angles
-    r = np.sum(hsp**2,axis=-1)**(1./2)
+    r = np.sqrt(np.sum(hsp ** 2,axis=-1))
     theta = np.arccos(hsp[:,2]/r)
     phi = np.arctan2(hsp[:,1], hsp[:,0])
 
@@ -44,12 +44,12 @@ def make_eeg_layout(info, output_file, prad=20, width=5, height=4):
     phi[iffy] = 0
 
     # Do the azimuthal equidistant projection
-    x = prad*(2.0*theta/pi)*np.cos(phi);
-    y = prad*(2.0*theta/pi)*np.sin(phi);
+    x = radius*(2.0*theta/np.pi)*np.cos(phi)
+    y = radius*(2.0*theta/np.pi)*np.sin(phi)
 
     outStr = '%8.2f %8.2f %8.2f %8.2f\n' % (x.min() - 0.1*width, x.max() + 1.1*width, y.min() - 0.1*width, y.max() + 1.1*height)
     for ii in range(hsp.shape[0]):
-      outStr = outStr + '%03d %8.2f %8.2f %8.2f %8.2f %s\n' % (ii+1, x[ii], y[ii], width, height, names[ii])
+        outStr = outStr + '%03d %8.2f %8.2f %8.2f %8.2f %s\n' % (ii+1, x[ii], y[ii], width, height, names[ii])
 
     f = open(output_file, 'w')
     f.write(outStr)
