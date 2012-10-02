df929aa388fd6a259eb950bd13bd2b452e3dc1e1
Eric Larson <larson.eric.d@gmail.com>
2012-10-02 11:42:47 -0700

    FIX: Speeding up too slow test_ssp.py

--- mne/preprocessing/tests/test_ssp.py
+++ mne/preprocessing/tests/test_ssp.py
@@ -1,6 +1,6 @@
 import os.path as op
 
-from nose.tools import assert_true
+from nose.tools import assert_true, assert_raises
 from numpy.testing import assert_array_almost_equal
 
 from ...fiff import Raw, proj
@@ -50,22 +50,29 @@ def test_compute_proj_eog():
 
 
 def test_compute_proj_eog_multiple():
-    """Test computation of EOG SSP projectors with multiple Raw"""
+    """Test computation of ExG SSP projectors with multiple Raw"""
     average = False
     raw = Raw(raw_fname, preload=True)
-    projs, events = compute_proj_eog(raw, n_mag=2, n_grad=2, n_eeg=2,
-                                 bads=['MEG 2443'], average=average,
-                                 avg_ref=True, no_proj=False)
+    projs, events = compute_proj_ecg(raw, n_mag=2, n_grad=2, n_eeg=2,
+                                         ch_name='MEG 1531', bads=['MEG 2443'],
+                                         average=average, avg_ref=True,
+                                         no_proj=True)
     raw.close()
     p, _, _ = proj.make_projector(projs, raw.info['ch_names'],
                              raw.info['bads'])
 
     raw = [Raw(raw_fname, preload=True), Raw(raw_fname, preload=True)]
-    projs_2, events = compute_proj_eog(raw, n_mag=2, n_grad=2, n_eeg=2,
-                                 bads=['MEG 2443'], average=average,
-                                 avg_ref=True, no_proj=False)
+    projs_2, events = compute_proj_ecg(raw, n_mag=2, n_grad=2, n_eeg=2,
+                                         ch_name='MEG 1531', bads=['MEG 2443'],
+                                         average=average, avg_ref=True,
+                                         no_proj=True)
     raw[0].close()
     raw[1].close()
     p_2, _, _ = proj.make_projector(projs_2, raw[0].info['ch_names'],
                              raw[0].info['bads'])
     assert_array_almost_equal(p, p_2)
+
+    raw = [Raw(raw_fname, preload=True), Raw(raw_fname, preload=False)]
+    assert_raises(ValueError, compute_proj_eog, raw)
+    raw[0].close()
+    raw[1].close()
