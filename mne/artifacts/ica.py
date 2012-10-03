60b4bda24d9436794cbd1218dfa26ab8c5331ff6
dengemann <denis.engemann@gmail.com>
2012-10-03 21:56:14 +0200

    WIP: Improving sources sorting
    - fixing bug that created different sortings for mixing matrix and sources
    - adding sort_sources method for the user. the user can now sort the sources   back and in the course of the inspection process
    - added source_ids attribute that makes sort-book-keeping easier. this can then be used for sorting back the array to the initial state
    - adding support for custom sorting function

--- mne/artifacts/ica.py
+++ mne/artifacts/ica.py
@@ -6,8 +6,9 @@
 
 from sklearn.decomposition import FastICA
 import numpy as np
+from inspect import getargspec
 from ..cov import compute_whitener
-from scipy.stats import kurtosis
+from scipy.stats import kurtosis, skew
 
 
 class ICA(object):
@@ -27,9 +28,6 @@ class ICA(object):
         starting time slice
     stop : integer
         final time slice
-    sort_method : string
-        function used to sort the sources and the mixing matrix.
-        If None, the output will be left unsorted
 
     Attributes
     ----------
@@ -43,11 +41,11 @@ class ICA(object):
         whiter used for preprocessing
     seed : instance of FastICA
         FastICA instance used to perform decomposition
-    sort_func : function
-        function used for sorting the components
+    source_ids : np.ndrarray
+        array of source ids for sorting-book-keeping
     """
     def __init__(self, raw, picks, noise_cov=None, start=None, stop=None,
-                 n_components=None, sort_method='kurtosis', exclude=None):
+                 n_components=None, exclude=None):
 
         self.raw = raw
         self.n_components = n_components
@@ -56,6 +54,7 @@ class ICA(object):
 
         self.comp_picks = np.zeros(_n_comps, dtype=np.bool)
         self.comp_picks.fill(True)
+        self.source_ids = np.arange(n_components)
 
         if noise_cov != None:
             self.whitener, _ = compute_whitener(noise_cov, raw.info,
@@ -74,16 +73,6 @@ class ICA(object):
 
         self.seed = FastICA(n_components)
 
-        self.sort_func = None
-        if sort_method == 'var':
-            self.sort_func = np.var
-        elif sort_method == 'kurtosis':
-            self.sort_func = kurtosis
-        elif isinstance(sort_method, str):
-            print ('\n    This is not a valid sorting function'
-                   '\n    --- No sort will be applied.')
-
-        # if self.sort_func != None:
     def __repr__(self):
         out = 'ICA object.\n    %i components' % self.n_components
         if self.n_selected == self.n_components:
@@ -95,20 +84,71 @@ class ICA(object):
             out += ('\n    %i raw time slices' % self.n_selected)
         return out
 
-    def _sort(self, X):
-        sorter = self.sort_func(X, 1)
-        X_sorted = X[np.argsort(sorter)]
-        return X_sorted
+    def sort_sources(self, sources='raw', smethod='kurtosis'):
+        """
+        Paramerters
+        -----------
+        sort_method : string
+            function used to sort the sources and the mixing matrix.
+            If None, the output will be left unsorted
+        """
+        if smethod == 'skew':
+            sort_func = skew
+        elif smethod == 'kurtosis':
+            sort_func = kurtosis
+        elif callable(smethod):
+            args = getargspec(smethod).args
+            if len(args) > 1:
+                if args[:2] == ['a', 'axis']:
+                    sort_func = smethod
+            else:
+                ValueError('%s is not a valid function.'
+                           'The function needs an array and'
+                           'an axis argument' % smethod.__name__)
+        elif isinstance(smethod, str):
+            ValueError('%s is not a valid sorting option' % smethod.__name__)
 
-    def fit_raw(self):
+        if sources == 'raw':
+            S = self.raw_sources
+            A = self.raw_mixing
+        else:
+            # A = self.epochs_sources
+            return NotImplemented  # not implemented
+
+        sort_args = np.argsort(sort_func(S, 1))
+        S = S[sort_args]
+        A = A[sort_args]
+        self.source_ids = self.source_ids[sort_args]
+
+        if sources == 'raw':
+            self.raw_sources = S
+            self.raw_mixing = A
+        else:
+            # A = self.epochs_sources
+            return NotImplemented  # not implemented
+
+    def fit_raw(self, smethod='kurtosis'):
         """ Run the ica decomposition for raw data
+        Paramerters
+        -----------
+        sort_method : string
+            function used to sort the sources and the mixing matrix.
+            If None, the output will be left unsorted
+
+        Returns
+        -------
+        self : instance of ICA
+            returns the instance for chaining
         """
         print ('\nComputing signal decomposition on raw data.'
                '\n    Please be patient. This may take some time')
         S = self.seed.fit_transform(self.raw_data.T)
         A = self.seed.get_mixing_matrix()
-        self.raw_sources = self._sort(S).T
-        self.raw_mixing = self._sort(A).T
+        self.raw_sources = S.T
+        self.raw_mixing = A.T
+        self.sort_sources(sources='raw', smethod=smethod)
+
+        return self
 
     def fit_epochs(self, epochs, picks):
         pass
