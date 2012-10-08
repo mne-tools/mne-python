2038afdafb859449af97812ebf78b2b70dd8c840
dengemann <denis.engemann@gmail.com>
2012-10-08 02:14:39 +0200

    ENH: improving examples - more clearly demonstrating pandas hierarchical indexing - adding plotting parameters for neater plots - editing comments

--- examples/export/plot_epochs_as_data_frame.py
+++ examples/export/plot_epochs_as_data_frame.py
@@ -19,6 +19,9 @@ from mne.datasets import sample
 
 from pandas.stats.api import rolling_mean
 
+# turn on interactive mode
+pl.ion()
+
 data_path = sample.data_path('..')
 raw_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
 
@@ -40,37 +43,37 @@ epochs_df = epochs.as_data_frame()
 meg_chs = [c for c in epochs.ch_names if c.startswith("MEG")]
 
 #display some channels
-epochs_df.ix[:, :10].head(20)
+epochs_df.ix['epoch 11', :4].head(20)
 
-# split timeslices
+# plot single epoch
+epochs_df.ix['epoch 11'].plot(legend=False, xlim=(0, 105), title='epoch 11')
+
+# split accroding to timeslices
 grouped_tsl = epochs_df[meg_chs].groupby(level='tsl')
 
-# then create a quick average plot
-grouped_tsl.mean().plot(legend=0)
+# then apply mean to each group and create a plot
+grouped_tsl.mean().plot(legend=0, title='MEG average', xlim=(0, 105))
 
-# or a trellis plot on a few channels
-grouped_tsl.mean()[meg_chs[:10]].plot(subplots=1)
+# apply custom numpy function
+grouped_tsl.agg(np.std).plot(legend=0, title='MEG std', xlim=(0, 105))
 
-# use median instead
-grouped_tsl.median().plot(legend=0)
+# average then smooth using a rolling mean and finally plot.
+grouped_tsl.apply(lambda x: rolling_mean(x.mean(), 10)).plot(legend=False,
+                                                             xlim=(0, 105))
 
-# use custom numpy function
-grouped_tsl.agg(np.std).plot(legend=0)
-
-# average then smooth using a rolling mean and finally plot in one single line!
-grouped_tsl.apply(lambda x: rolling_mean(x.mean(), 10)).plot(legend=0)
-
-# apply different functio for channels
+# apply individual functions to channels
 grouped_tsl.agg({"MEG 0113": np.mean, "MEG 0213": np.median})
 
-# investigate epochs and create string table for dumping into file
+# investigate epochs
 grouped_epochs = epochs_df[meg_chs].groupby(level='epochs')
 
-result_table = (grouped_epochs.max().ix[:, 1:3] * 1e15).to_string()
+subgroup = grouped_epochs.max().ix['Epoch 1':'Epoch 12']
+
+subgroup.plot(kind='barh', legend=0)
+
+# create string table for dumping into file.
+result_table = (subgroup * 1e15).to_string()
 
 # investigate a specific channel's std across epochs
-grouped_epochs.std()["MEG 0113"].plot()
-
-grouped_tsl.agg(np.std).plot(legend=0)
-
-pl.show()
+chn = "MEG 0113"
+grouped_epochs.std()[chn].plot(title=chn, xlim=(0, 55))
