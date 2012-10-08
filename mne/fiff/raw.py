c4e32b66877311eea4a0e83521172af215d16a62
dengemann <denis.engemann@gmail.com>
2012-10-08 15:25:31 +0200

    ENH: adding RawFromMerge class

--- mne/fiff/raw.py
+++ mne/fiff/raw.py
@@ -8,6 +8,7 @@
 from math import floor, ceil
 import copy
 import warnings
+from copy import deepcopy
 
 import numpy as np
 from scipy.signal import hilbert
@@ -920,6 +921,58 @@ def _hash_projs(projs, projector):
     if projector is not None:
         out_hash.append(array_hash(projector))
     return out_hash
+
+
+class RawFromMerge(Raw):
+    """ Initializes new raw instance from exisiting instance and custom data
+    Paramerters
+    -----------
+    raw : instance of mne.fiff.Raw
+        existing raw instance
+    data : instance of numpy.core.ndarray
+        processed data matching the data contained by raw
+
+    Attributes
+    ----------
+        See __doc__ of mne.fiff.Raw
+    """
+    def __init__(self, raw, data, info=None):
+
+        print 'Initializing raw object from merge with custom data.'
+
+        ntsl = (raw.last_samp - raw.first_samp) + 1
+        nchan = len(raw.ch_names)
+        assert (nchan, ntsl) == data.shape
+
+        raw = deepcopy(raw)
+        if info == None:
+            info = raw.info
+
+        self.info = info
+        self._data = data
+        self.first_samp, self.last_samp = raw.first_samp, raw.last_samp
+        cals = np.zeros(info['nchan'])
+        for k in range(info['nchan']):
+            cals[k] = info['chs'][k]['range'] * \
+                      info['chs'][k]['cal']
+
+        self.cals = raw.cals
+        self.rawdir = raw.rawdir
+        self.proj = raw.proj
+        self.comp = raw.comp
+
+        self.verbose = True
+        if self.verbose:
+            print '    Range : %d ... %d =  %9.3f ... %9.3f secs' % (
+                       self.first_samp, self.last_samp,
+                       float(self.first_samp) / info['sfreq'],
+                       float(self.last_samp) / info['sfreq'])
+            print 'Ready.'
+        self.fid = None
+        self._preloaded = True
+        self._times = np.arange(self.first_samp,
+            self.last_samp + 1) / info['sfreq']
+        del raw
 
 
 def read_raw_segment(raw, start=0, stop=None, sel=None, data_buffer=None,
