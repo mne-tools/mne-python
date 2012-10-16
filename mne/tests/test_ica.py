629f04a55256b4b7e7f31f2c9edbd78f559e98b2
dengemann <denis.engemann@gmail.com>
2012-10-16 23:57:12 +0200

    WIP: first test for ica.py - invoking the ica methods - for noise_cov and pseudo-whitening - performing simple assert statements for ica results

---
+++ mne/tests/test_ica.py
@@ -0,0 +1,97 @@
+# Author: Denis Engemann <d.engemann@fz-juelich.de>
+#
+# License: BSD (3-clause)
+
+import os.path as op
+from numpy.testing import assert_array_almost_equal
+
+from mne import fiff, Epochs, read_events, cov
+
+from mne.artifacts import ICA
+
+raw_fname = op.join(op.dirname(__file__), '..', 'fiff', 'tests', 'data',
+                     'test_raw.fif')
+event_name = op.join(op.dirname(__file__), '..', 'fiff', 'tests', 'data',
+                     'test-eve.fif')
+evoked_nf_name = op.join(op.dirname(__file__), '..', 'fiff', 'tests', 'data',
+                     'test-nf-ave.fif')
+
+test_cov_name = op.join(op.dirname(__file__), '..', 'fiff', 'tests', 'data',
+                     'test-cov.fif')
+
+event_id, tmin, tmax = 1, -0.2, 0.5
+raw = fiff.Raw(raw_fname)
+events = read_events(event_name)
+picks = fiff.pick_types(raw.info, meg=True, stim=False,
+                            ecg=False, eog=False, exclude=raw.info['bads'])
+reject = dict(grad=1000e-12, mag=4e-12, eeg=80e-6, eog=150e-6)
+flat = dict(grad=1e-15, mag=1e-15)
+
+test_cov = cov.read_cov(test_cov_name)
+
+epochs = Epochs(raw, events[:4], event_id, tmin, tmax, picks=picks,
+                baseline=(None, 0), preload=True)
+
+ica = ICA(noise_cov=None, n_components=25, random_state=0)
+
+ica_cov = ICA(noise_cov=test_cov, n_components=25, random_state=0)
+
+start, stop = 0, 9009
+
+
+def test_decompose_raw():
+    """ Test raw decomposition
+    """
+    ica.decompose_raw(raw, picks=picks)
+    ica_cov.decompose_raw(raw, picks=picks)
+
+
+def test_get_sources_raw():
+    """ Test raw source-extraction
+    """
+    sources = ica.get_sources_raw(raw, picks=picks)
+    assert sources.shape[0] == ica.n_components
+
+    ica_cov.get_sources_raw(raw, picks=picks)
+    assert sources.shape[0] == ica.n_components
+
+
+def test_pick_sources_raw():
+    """ Test sources selection
+    """
+    raw2 = ica.pick_sources_raw(raw, exclude=[], sort_method='kurtosis',
+                         copy=True)
+    assert_array_almost_equal(raw2._data, raw._data)
+
+
+def test_pick_sources_epochs_from_raw():
+    """ Test epochs sources selection using raw fit.
+    """
+    epochs2 = ica.pick_sources_epochs(epochs, exclude=[],
+                                      sort_method='kurtosis', copy=True)
+    assert_array_almost_equal(epochs2._data, epochs._data)
+
+
+def test_decompose_epochs():
+    """ Test epochs decomposition
+    """
+    ica.decompose_epochs(epochs, picks=picks)
+    ica_cov.decompose_epochs(epochs, picks=picks)
+
+
+def test_get_sources_epochs():
+    """ Test epochs sources selection
+    """
+    sources = ica.get_sources_epochs(epochs, picks=picks)
+    assert sources.shape[1] == ica.n_components
+
+    sources = ica_cov.get_sources_epochs(epochs, picks=picks)
+    assert sources.shape[1] == ica.n_components
+
+
+def test_pick_sources_epochs():
+    """ Test sources selection
+    """
+    epochs2 = ica.pick_sources_epochs(epochs, exclude=[],
+                                      sort_method='kurtosis', copy=True)
+    assert_array_almost_equal(epochs2._data, epochs._data)
