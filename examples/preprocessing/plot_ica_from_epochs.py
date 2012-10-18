8d0a3e60dea495aed57e8bc99927665147ff65d7
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2012-10-18 09:47:08 +0200

    ENH : cleanup of ICA code + test + examples

--- examples/preprocessing/plot_ica_from_epochs.py
+++ examples/preprocessing/plot_ica_from_epochs.py
@@ -1,23 +1,24 @@
 """
-==================================
-Compute ICA components on Raw data
-==================================
+================================
+Compute ICA components on Epochs
+================================
+
+25 ICA components are estimated and displayed.
 
 """
+print __doc__
+
 # Authors: Denis Engemann <d.engemann@fz-juelich.de>
 #
 # License: BSD (3-clause)
 
-print __doc__
-
 import matplotlib.pylab as pl
 import mne
 from mne.fiff import Raw
-# from mne.artifacts.ica import decompose_raw
 from mne.artifacts.ica import ICA
 from mne.viz import plot_ica_panel
-
 from mne.datasets import sample
+
 data_path = sample.data_path('..')
 raw_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
 
@@ -25,7 +26,6 @@ raw = Raw(raw_fname, preload=True)
 
 picks = mne.fiff.pick_types(raw.info, meg=True, eeg=False, eog=False,
                             stim=False, exclude=raw.info['bads'])
-
 
 start, stop = raw.time_to_index(100, 160)
 
@@ -37,14 +37,13 @@ print ica
 tmin, tmax, event_id = -0.2, 0.5, 1
 baseline = (None, 0)
 reject = None
-# reject = dict(grad=4000e-13, mag=4e-12, eog=150e-6)
 
 events = mne.find_events(raw, stim_channel='STI 014')
-
 epochs = mne.Epochs(raw, events, event_id, tmin, tmax, proj=True, picks=picks,
                     baseline=baseline, preload=False, reject=reject)
 
 # fit sources from epochs or from raw (both works for epochs)
+picks = mne.fiff.pick_types(epochs.info, meg=True, eeg=False)
 ica.decompose_epochs(epochs, picks=picks)
 
 # get sources from epochs
--- mne/artifacts/tests/test_ica.py
+++ mne/artifacts/tests/test_ica.py
@@ -3,24 +3,24 @@
 # License: BSD (3-clause)
 
 import os.path as op
+from nose.tools import assert_true
 from numpy.testing import assert_array_almost_equal
 
 from mne import fiff, Epochs, read_events, cov
-
 from mne.artifacts import ICA
 
-raw_fname = op.join(op.dirname(__file__), '..', 'fiff', 'tests', 'data',
+raw_fname = op.join(op.dirname(__file__), '..', '..', 'fiff', 'tests', 'data',
                      'test_raw.fif')
-event_name = op.join(op.dirname(__file__), '..', 'fiff', 'tests', 'data',
-                     'test-eve.fif')
-evoked_nf_name = op.join(op.dirname(__file__), '..', 'fiff', 'tests', 'data',
-                     'test-nf-ave.fif')
+event_name = op.join(op.dirname(__file__), '..', '..', 'fiff', 'tests',
+                     'data', 'test-eve.fif')
+evoked_nf_name = op.join(op.dirname(__file__), '..', '..', 'fiff', 'tests',
+                     'data', 'test-nf-ave.fif')
 
-test_cov_name = op.join(op.dirname(__file__), '..', 'fiff', 'tests', 'data',
-                     'test-cov.fif')
+test_cov_name = op.join(op.dirname(__file__), '..', '..', 'fiff', 'tests',
+                     'data', 'test-cov.fif')
 
 event_id, tmin, tmax = 1, -0.2, 0.5
-raw = fiff.Raw(raw_fname)
+raw = fiff.Raw(raw_fname, preload=True)
 events = read_events(event_name)
 picks = fiff.pick_types(raw.info, meg=True, stim=False,
                             ecg=False, eog=False, exclude=raw.info['bads'])
@@ -28,70 +28,51 @@ reject = dict(grad=1000e-12, mag=4e-12, eeg=80e-6, eog=150e-6)
 flat = dict(grad=1e-15, mag=1e-15)
 
 test_cov = cov.read_cov(test_cov_name)
-
 epochs = Epochs(raw, events[:4], event_id, tmin, tmax, picks=picks,
                 baseline=(None, 0), preload=True)
 
 ica = ICA(noise_cov=None, n_components=25, random_state=0)
-
 ica_cov = ICA(noise_cov=test_cov, n_components=25, random_state=0)
 
 start, stop = 0, 9009
 
 
-def test_decompose_raw():
-    """ Test raw decomposition
-    """
+def test_ica_raw():
+    """Test ICA on raw"""
+    print ica  # to test repr
     ica.decompose_raw(raw, picks=picks)
     ica_cov.decompose_raw(raw, picks=picks)
+    print ica  # to test repr
 
-
-def test_get_sources_raw():
-    """ Test raw source-extraction
-    """
     sources = ica.get_sources_raw(raw, picks=picks)
-    assert sources.shape[0] == ica.n_components
+    assert_true(sources.shape[0] == ica.n_components)
 
     ica_cov.get_sources_raw(raw, picks=picks)
-    assert sources.shape[0] == ica.n_components
+    assert_true(sources.shape[0] == ica.n_components)
 
-
-def test_pick_sources_raw():
-    """ Test sources selection
-    """
     raw2 = ica.pick_sources_raw(raw, exclude=[], sort_method='kurtosis',
-                         copy=True)
+                                copy=True)
     assert_array_almost_equal(raw2._data, raw._data)
 
 
 def test_pick_sources_epochs_from_raw():
-    """ Test epochs sources selection using raw fit.
-    """
+    """Test epochs sources selection using raw fit."""
     epochs2 = ica.pick_sources_epochs(epochs, exclude=[],
                                       sort_method='kurtosis', copy=True)
     assert_array_almost_equal(epochs2._data, epochs._data)
 
 
-def test_decompose_epochs():
-    """ Test epochs decomposition
-    """
+def test_ica_epochs():
+    """Test ICA epochs"""
     ica.decompose_epochs(epochs, picks=picks)
     ica_cov.decompose_epochs(epochs, picks=picks)
 
-
-def test_get_sources_epochs():
-    """ Test epochs sources selection
-    """
     sources = ica.get_sources_epochs(epochs, picks=picks)
-    assert sources.shape[1] == ica.n_components
+    assert_true(sources.shape[1] == ica.n_components)
 
     sources = ica_cov.get_sources_epochs(epochs, picks=picks)
-    assert sources.shape[1] == ica.n_components
+    assert_true(sources.shape[1] == ica.n_components)
 
-
-def test_pick_sources_epochs():
-    """ Test sources selection
-    """
     epochs2 = ica.pick_sources_epochs(epochs, exclude=[],
                                       sort_method='kurtosis', copy=True)
     assert_array_almost_equal(epochs2._data, epochs._data)
