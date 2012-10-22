7763ce77a72703df8f48df6f3a340177854954e5
dengemann <denis.engemann@gmail.com>
2012-10-22 13:05:40 +0200

    ENH: removing nonmodule directories

--- mne/__init__.py
+++ mne/__init__.py
@@ -28,10 +28,8 @@ from .proj import read_proj, write_proj, compute_proj_epochs, \
 
 from .selection import read_selection
 from .dipole import read_dip
-
 from . import artifacts
 from . import beamformer
-from . import data
 from . import datasets
 from . import fiff
 from . import layouts
@@ -39,7 +37,7 @@ from . import minimum_norm
 from . import mixed_norm
 from . import preprocessing
 from . import simulation
+from . import stats
 from . import tests
 from . import time_frequency
 from . import viz
-
