b2ae219b31f6732596a61f4c288ed6dfe8680abe
Eric Larson <larson.eric.d@gmail.com>
2012-10-24 14:37:59 -0700

    ENH: De-pythonizing

--- examples/inverse/plot_make_inverse_operator.py
+++ examples/inverse/plot_make_inverse_operator.py
@@ -23,7 +23,8 @@ from mne.minimum_norm import make_inverse_operator, apply_inverse, \
                              write_inverse_operator
 
 data_path = sample.data_path('..')
-fname_fwd = data_path + '/MEG/sample/sample_audvis-meg-eeg-oct-6-fwd.fif'
+fname_fwd_meeg = data_path + '/MEG/sample/sample_audvis-meg-eeg-oct-6-fwd.fif'
+fname_fwd_eeg = data_path + '/MEG/sample/sample_audvis-eeg-oct-6-fwd.fif'
 fname_cov = data_path + '/MEG/sample/sample_audvis-cov.fif'
 fname_evoked = data_path + '/MEG/sample/sample_audvis-ave.fif'
 
@@ -32,40 +33,55 @@ lambda2 = 1.0 / snr ** 2
 
 # Load data
 evoked = Evoked(fname_evoked, setno=0, baseline=(None, 0))
-forward = mne.read_forward_solution(fname_fwd, surf_ori=True)
+forward_meeg = mne.read_forward_solution(fname_fwd_meeg, surf_ori=True)
 noise_cov = mne.read_cov(fname_cov)
 
 # regularize noise covariance
 noise_cov = mne.cov.regularize(noise_cov, evoked.info,
                                mag=0.05, grad=0.05, eeg=0.1, proj=True)
 
+# Restrict forward solution as necessary for MEG
+forward_meg = mne.fiff.pick_types_forward(forward_meeg, meg=True, eeg=False)
+# Alternatively, you can just load a forward solution that is restricted
+forward_eeg = mne.read_forward_solution(fname_fwd_eeg, surf_ori=True)
 
 # make an M/EEG, MEG-only, and EEG-only inverse operators
-names = ['meg-eeg', 'meg', 'eeg']
-meg_bool = [True, True, False]
-eeg_bool = [True, False, True]
-forwards = [mne.fiff.pick_types_forward(forward, meg=m, eeg=e)
-    for m, e in zip(meg_bool, eeg_bool)]
-
 info = evoked.info
-inverse_operators = [make_inverse_operator(info, f, noise_cov, loose=0.2,
-                                           depth=0.8) for f in forwards]
+inverse_operator_meeg = make_inverse_operator(info, forward_meeg, noise_cov,
+                                              loose=0.2, depth=0.8)
+inverse_operator_meg = make_inverse_operator(info, forward_meg, noise_cov,
+                                              loose=0.2, depth=0.8)
+inverse_operator_eeg = make_inverse_operator(info, forward_eeg, noise_cov,
+                                              loose=0.2, depth=0.8)
 
-# Save inverse operators to vizualize with mne_analyze
-stcs = [None] * len(names)
-for ii, (name, inv) in enumerate(zip(names, inverse_operators)):
-    write_inverse_operator('sample_audvis-eeg-oct-6-%s-inv.fif' % name, inv)
+write_inverse_operator('sample_audvis-meeg-oct-6-inv.fif',
+                       inverse_operator_meeg)
+write_inverse_operator('sample_audvis-meg-oct-6-inv.fif',
+                       inverse_operator_meg)
+write_inverse_operator('sample_audvis-eeg-oct-6-inv.fif',
+                       inverse_operator_eeg)
 
-    # Compute inverse solution
-    stcs[ii] = apply_inverse(evoked, inv, lambda2, "dSPM", pick_normal=False)
+# Compute inverse solution
+stcs = [None] * 3
+stcs[0] = apply_inverse(evoked, inverse_operator_meeg, lambda2, "dSPM",
+                        pick_normal=False)
+stcs[1] = apply_inverse(evoked, inverse_operator_meg, lambda2, "dSPM",
+                        pick_normal=False)
+stcs[2] = apply_inverse(evoked, inverse_operator_eeg, lambda2, "dSPM",
+                        pick_normal=False)
 
-    # Save result in stc files
+# Save result in stc files
+names = ['meeg', 'meg', 'eeg']
+for ii in range(len(names)):
+    name = names[ii]
     stcs[ii].save('mne_dSPM_inverse-%s' % name)
 
 ###############################################################################
 # View activation time-series
 pl.close('all')
-for ii, (name, stc) in enumerate(zip(names, stcs)):
+for ii in range(len(stcs)):
+    name = names[ii]
+    stc = stcs[ii]
     pl.subplot(len(stcs), 1, ii)
     pl.plot(1e3 * stc.times, stc.data[::150, :].T)
     pl.ylabel('%s\ndSPM value' % str.upper(name))
