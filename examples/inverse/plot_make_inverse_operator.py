ff590612f6e727525af4d5c3d87cdd7ea8db0525
Eric Larson <larson.eric.d@gmail.com>
2012-10-25 07:56:27 -0700

    FIX: Consistency

--- examples/inverse/plot_make_inverse_operator.py
+++ examples/inverse/plot_make_inverse_operator.py
@@ -78,10 +78,10 @@ for name in names:
 ###############################################################################
 # View activation time-series
 pl.close('all')
-for ii in range(3):
+for ii in range(len(stcs)):
     name = names[ii]
     stc = stcs[name]
-    pl.subplot(3, 1, ii)
+    pl.subplot(len(stcs), 1, ii)
     pl.plot(1e3 * stc.times, stc.data[::150, :].T)
     pl.ylabel('%s\ndSPM value' % str.upper(name))
 pl.xlabel('time (ms)')
