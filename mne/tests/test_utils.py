3fe494afe030bdf0280de485bd15fca9ca2b668e
Eric Larson <larson.eric.d@gmail.com>
2012-11-01 17:24:00 -0700

    ENH: Adding (default) file appending option

--- mne/tests/test_utils.py
+++ mne/tests/test_utils.py
@@ -2,6 +2,8 @@ from scipy import signal
 from numpy.testing import assert_equal
 from nose.tools import assert_true
 import os.path as op
+import os
+import warnings
 
 from mne.utils import _firwin2 as mne_firwin2
 from mne import set_log_level, set_log_file
@@ -11,6 +13,8 @@ fname_evoked = op.join(op.dirname(__file__), '..', 'fiff', 'tests', 'data',
                        'test-ave.fif')
 fname_log = op.join(op.dirname(__file__), '..', 'fiff', 'tests', 'data',
                     'test-ave.log')
+fname_log_2 = op.join(op.dirname(__file__), '..', 'fiff', 'tests', 'data',
+                      'test-ave-2.log')
 test_name = 'test.log'
 
 
@@ -28,7 +32,12 @@ def test_logging():
     old_log_file = open(fname_log, 'r')
     old_lines = old_log_file.readlines()
     old_log_file.close()
+    old_log_file_2 = open(fname_log_2, 'r')
+    old_lines_2 = old_log_file_2.readlines()
+    old_log_file_2.close()
 
+    if op.isfile(test_name):
+        os.remove(test_name)
     # test it one way (printing default off)
     set_log_file(test_name)
     set_log_level('WARNING')
@@ -47,6 +56,7 @@ def test_logging():
     assert_equal(new_log_file.readlines(), old_lines)
 
     # now go the other way (printing default on)
+    os.remove(test_name)
     set_log_file(test_name)
     set_log_level('INFO')
     # should NOT print
@@ -59,5 +69,21 @@ def test_logging():
     evoked = Evoked(fname_evoked, setno=1)
     new_log_file = open(test_name, 'r')
     old_log_file = open(fname_log, 'r')
-    print set_log_level('INFO')
     assert_equal(new_log_file.readlines(), old_lines)
+    # check to make sure appending works (and as default, raises a warning)
+    with warnings.catch_warnings(True) as w:
+        set_log_file(test_name, overwrite=False)
+        assert len(w) == 0
+        set_log_file(test_name)
+        assert len(w) == 1
+    evoked = Evoked(fname_evoked, setno=1)
+    new_log_file = open(test_name, 'r')
+    new_lines = new_log_file.readlines()
+    assert_equal(new_lines, old_lines_2)
+
+    # make sure overwriting works
+    set_log_file(test_name, overwrite=True)
+    evoked = Evoked(fname_evoked, setno=1)
+    new_log_file = open(test_name, 'r')
+    new_lines = new_log_file.readlines()
+    assert_equal(new_lines, old_lines)
