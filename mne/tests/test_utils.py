c529d3d413f8d95b494c2bfc6827da0cdd35eb9a
Eric Larson <larson.eric.d@gmail.com>
2012-11-04 20:20:54 -0800

    FIX: Working around doctest magic, fixing doctests

--- mne/tests/test_utils.py
+++ mne/tests/test_utils.py
@@ -26,14 +26,19 @@ def test_firwin2():
     assert_equal(taps1, taps2)
 
 
+def clean_lines(lines):
+    # Function to scrub filenames for checking logging output (in test_logging)
+    return [l if 'Reading ' not in l else 'Reading test file' for l in lines]
+
+
 def test_logging():
     """Test logging (to file)
     """
     old_log_file = open(fname_log, 'r')
-    old_lines = old_log_file.readlines()
+    old_lines = clean_lines(old_log_file.readlines())
     old_log_file.close()
     old_log_file_2 = open(fname_log_2, 'r')
-    old_lines_2 = old_log_file_2.readlines()
+    old_lines_2 = clean_lines(old_log_file_2.readlines())
     old_log_file_2.close()
 
     if op.isfile(test_name):
@@ -53,7 +58,8 @@ def test_logging():
     # SHOULD print
     evoked = Evoked(fname_evoked, setno=1, verbose=True)
     new_log_file = open(test_name, 'r')
-    assert_equal(new_log_file.readlines(), old_lines)
+    new_lines = clean_lines(new_log_file.readlines())
+    assert_equal(new_lines, old_lines)
 
     # now go the other way (printing default on)
     os.remove(test_name)
@@ -69,7 +75,10 @@ def test_logging():
     evoked = Evoked(fname_evoked, setno=1)
     new_log_file = open(test_name, 'r')
     old_log_file = open(fname_log, 'r')
-    assert_equal(new_log_file.readlines(), old_lines)
+    new_lines = new_log_file.readlines()
+    if len(new_lines) > 0:
+        new_lines = new_lines[1:]
+    assert_equal(new_lines, old_lines[1:])
     # check to make sure appending works (and as default, raises a warning)
     with warnings.catch_warnings(True) as w:
         set_log_file(test_name, overwrite=False)
@@ -78,12 +87,13 @@ def test_logging():
         assert len(w) == 1
     evoked = Evoked(fname_evoked, setno=1)
     new_log_file = open(test_name, 'r')
-    new_lines = new_log_file.readlines()
+    new_lines = clean_lines(new_log_file.readlines())
     assert_equal(new_lines, old_lines_2)
 
     # make sure overwriting works
     set_log_file(test_name, overwrite=True)
+    # this line needs to be called to actually do some logging
     evoked = Evoked(fname_evoked, setno=1)
     new_log_file = open(test_name, 'r')
-    new_lines = new_log_file.readlines()
+    new_lines = clean_lines(new_log_file.readlines())
     assert_equal(new_lines, old_lines)
