818a5c2747a4121f43195f24290a37554ecbc81c
Christian Brodbeck <christianmbrodbeck@gmail.com>
2012-12-08 15:23:24 -0500

    ENH: example plot_label_activation_from_stc

--- examples/inverse/plot_label_activation_from_stc.py
+++ examples/inverse/plot_label_activation_from_stc.py
@@ -41,19 +41,18 @@ mni_rh = mne.vertex_to_mni(vtx, 1, 'sample')[0]
 
 # plot the activation
 plt.figure()
-plt.axes([.1, .3, .8, .6])
+plt.axes([.1, .275, .85, .625])
 hl = plt.plot(stc.times, stc_lh.data.mean(0), 'b')
-plt.axvline(t_lh, color='b')
 hr = plt.plot(stc.times, stc_rh.data.mean(0), 'g')
-plt.axvline(t_rh, color='g')
 hb = plt.plot(stc.times, stc_bh.data.mean(0), 'r')
 plt.xlabel('Time [s]')
-plt.ylabel('source amplitude (dSPM)')
+plt.ylabel('Source amplitude [dSPM]')
+plt.xlim(stc.times[0], stc.times[-1])
 
 # add a legend including center-of-mass mni coordinates to the plot
-labels = ['LH: center of mass = %s' % mni_lh.round(2), 
-          'RH: center of mass = %s' % mni_rh.round(2), 
+labels = ['LH: center of mass = %s' % mni_lh.round(2),
+          'RH: center of mass = %s' % mni_rh.round(2),
           'Combined LH & RH']
-plt.figlegend([hl, hr, hb], labels, 'lower right')
-plt.title('Average activation in auditory cortex labels')
+plt.figlegend([hl, hr, hb], labels, 'lower center')
+plt.suptitle('Average activation in auditory cortex labels', fontsize=20)
 plt.show()
