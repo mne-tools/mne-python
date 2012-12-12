7efdd5aa71d863c5c6bd503689afbeb60b3d6740
Eric Larson <larson.eric.d@gmail.com>
2012-12-12 13:48:38 -0800

    FIX: Moving fixes around

--- mne/tests/test_utils.py
+++ mne/tests/test_utils.py
@@ -1,13 +1,11 @@
-from scipy import signal
 from numpy.testing import assert_equal
 from nose.tools import assert_true
 import os.path as op
 import os
 import warnings
 
-from mne.utils import _firwin2 as mne_firwin2
-from mne import set_log_level, set_log_file
-from mne.fiff import Evoked
+from ..utils import set_log_level, set_log_file
+from ..fiff import Evoked
 
 fname_evoked = op.join(op.dirname(__file__), '..', 'fiff', 'tests', 'data',
                        'test-ave.fif')
@@ -16,14 +14,6 @@ fname_log = op.join(op.dirname(__file__), '..', 'fiff', 'tests', 'data',
 fname_log_2 = op.join(op.dirname(__file__), '..', 'fiff', 'tests', 'data',
                       'test-ave-2.log')
 test_name = 'test.log'
-
-
-def test_firwin2():
-    """Test firwin2 backport
-    """
-    taps1 = mne_firwin2(150, [0.0, 0.5, 1.0], [1.0, 1.0, 0.0])
-    taps2 = signal.firwin2(150, [0.0, 0.5, 1.0], [1.0, 1.0, 0.0])
-    assert_equal(taps1, taps2)
 
 
 def clean_lines(lines):
