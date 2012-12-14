30cfc883a839aad8d2612b24c705330f4e776fef
Eric Larson <larson.eric.d@gmail.com>
2012-12-14 10:11:56 -0800

    ENH: Speeding up / clarifying

--- mne/mixed_norm/optim.py
+++ mne/mixed_norm/optim.py
@@ -65,10 +65,8 @@ def prox_l21(Y, alpha, n_orient):
     n_positions = Y.shape[0] // n_orient
     rows_norm = np.sqrt(np.sum((np.abs(Y) ** 2).reshape(n_positions, -1),
                                 axis=1))
-    # avoid divide-by-zero: no point in dividing by anything smaller than alpha
-    # here because of the maximum on the shrink below
-    rows_norm = np.maximum(rows_norm, alpha)
-    shrink = np.maximum(1.0 - alpha / rows_norm, 0.0)
+    # Ensure shrink is >= 0 while avoiding any division by zero
+    shrink = 1.0 - alpha / np.maximum(rows_norm, alpha)
     active_set = shrink > 0.0
     if n_orient > 1:
         active_set = np.tile(active_set[:, None], [1, n_orient]).ravel()
@@ -101,10 +99,9 @@ def prox_l1(Y, alpha, n_orient):
     """
     n_positions = Y.shape[0] // n_orient
     norms = np.sqrt(np.sum((np.abs(Y) ** 2).T.reshape(-1, n_orient), axis=1))
-    # avoid divide-by-zero: no point in dividing by anything smaller than alpha
-    # here because of the maximum on the shrink below
-    norms = np.maximum(norms, alpha)
-    shrink = np.maximum(1.0 - alpha / norms, 0.0).reshape(-1, n_positions).T
+    # Ensure shrink is >= 0 while avoiding any division by zero
+    shrink = (1.0 - alpha / np.maximum(norms, alpha), 0.0)
+    shrink = shrink.reshape(-1, n_positions).T
     active_set = np.any(shrink > 0.0, axis=1)
     shrink = shrink[active_set]
     if n_orient > 1:
