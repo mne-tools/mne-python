b8fb0ef7ad0c124d501874a1d42fc11e24c83606
Eric Larson <larson.eric.d@gmail.com>
2013-01-06 22:28:21 -0800

    FIX: Addressing comments

--- mne/minimum_norm/__init__.py
+++ mne/minimum_norm/__init__.py
@@ -3,6 +3,6 @@
 from .inverse import read_inverse_operator, apply_inverse, \
                      apply_inverse_raw, make_inverse_operator, \
                      apply_inverse_epochs, write_inverse_operator, \
-                     compute_rank
+                     compute_rank_inverse
 from .time_frequency import source_band_induced_power, source_induced_power, \
                             compute_source_psd, compute_source_psd_epochs
