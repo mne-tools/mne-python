ea4ed9474757ea4dd10a60a46f9205d2fc381f8e
christianmbrodbeck <christianmbrodbeck@gmail.com>
2013-01-12 22:03:17 -0300

    ENH: create translation, rotation and scaling matrices
    ENH: apply transform function
    
    ENH: add transforms/coreg module with fit_matched_pts function

---
+++ mne/transforms/coreg.py
@@ -0,0 +1,57 @@
+"""Coregistration between different coordinate frames"""
+
+# Authors: Christian Brodbeck <christianbrodbeck@nyu.edu>
+#
+# License: BSD (3-clause)
+
+from numpy import dot
+from scipy.optimize import leastsq
+
+from .transforms import apply_trans, rotation, translation
+
+
+def fit_matched_pts(src_pts, tgt_pts, params=False):
+    """Find a transform that minimizes the squared distance between two
+    matching sets of points.
+
+    Uses :func:`scipy.optimize.leastsq` to find a transformation involving
+    rotation and translation.
+
+    Parameters
+    ----------
+    src_pts : array, shape = (n, 3)
+        Points to which the transform should be applied.
+    tgt_pts : array, shape = (n, 3)
+        Points to which src_pts should be fitted. Each point in tgt_pts should
+        correspond to the point in src_pts withthe same index.
+    params : bool
+        Also return the estimated rotation and translation parameters.
+
+    Returns
+    -------
+    trans : array, shape = (4, 4)
+        Transformation that, if applied to src_pts, minimizes the squared
+        distance to tgt_pts.
+    [rotation : array, len = 3, optional]
+        The rotation parameters around the x, y, and z axes (in radians).
+    [translation : array, len = 3, optional]
+        The translation parameters in x, y, and z direction.
+
+    """
+    def error(params):
+        trans = dot(translation(*params[:3]), rotation(*params[3:]))
+        est = apply_trans(trans, src_pts)
+        return (tgt_pts - est).ravel()
+
+    x0 = (0, 0, 0, 0, 0, 0)
+    x, info = leastsq(error, x0)
+    if info not in [1, 2, 3, 4]:
+        raise RuntimeError("Fit failed with code %r" % info)
+
+    transl = x[:3]
+    rot = x[3:]
+    trans = dot(translation(*transl), rotation(*rot))
+    if params:
+        return trans, rot, transl
+    else:
+        return trans
