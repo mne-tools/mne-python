bfa1c61a73e5dffb92662582cc3c06d3a4546fc1
dengemann <denis.engemann@gmail.com>
2013-01-22 15:41:40 +0100

    ENH: adding critical tests

--- mne/fiff/bti/tests/test_bti.py
+++ mne/fiff/bti/tests/test_bti.py
@@ -24,8 +24,12 @@ archs = 'linux', 'solaris'
 pdf_fnames = [op.join(base_dir, 'test_pdf_%s' % a) for a in archs]
 config_fnames = [op.join(base_dir, 'test_config_%s' % a) for a in archs]
 hs_fnames = [op.join(base_dir, 'test_hs_%s' % a) for a in archs]
+exported_fnames = [op.join(base_dir, 'exported4D_%s.fif' % a) for a in archs]
 tmp_raw_fname = op.join(base_dir, 'tmp_raw.fif')
 tempdir = _TempDir()
+
+# the 4D exporter doesn't export all channels, so we confine our comparison
+NCH = 248
 
 
 def test_read_config():
@@ -46,17 +50,31 @@ def test_read_pdf():
 def test_raw():
     """ Test conversion to Raw object """
 
-    for pdf, config, hs in zip(pdf_fnames, config_fnames, hs_fnames):
+    for pdf, config, hs, exported in zip(pdf_fnames, config_fnames, hs_fnames,
+                               exported_fnames):
         print pdf
         assert_raises(ValueError, read_raw_bti, pdf, 'eggs')
         assert_raises(ValueError, read_raw_bti, pdf, config, 'spam')
         if op.exists(tmp_raw_fname):
             os.remove(tmp_raw_fname)
-        with read_raw_bti(pdf, config, hs) as r:
-            r.save(tmp_raw_fname)
-            print r.info['nchan']
-        with Raw(tmp_raw_fname) as r:
-            print r
+        with Raw(exported, preload=True) as e:
+            with read_raw_bti(pdf, config, hs) as r:
+                ntsl = len(r._times)  # reference channels yield longer files
+                assert_array_almost_equal(e.info['dev_head_t'],
+                                          r.info['dev_head_t'])
+                dig1, dig2 = [np.array([d['r'] for d in r_.info['dig']])
+                              for r_ in r, e]
+                assert_array_almost_equal(dig1, dig2)
+                coil1, coil2 = [np.array([d['coil_trans'] for d in r_])
+                                for r_ in r, e]
+                assert_array_almost_equal(coil1, coil2)
+
+                assert_array_almost_equal(r._data[:NCH], e._data[:NCH, ntsl])
+                assert_true(r.ch_names[:NCH] == e.ch_names)
+                assert_true(r.cals[:NCH] == e.cals)
+                r.save(tmp_raw_fname)
+            with Raw(tmp_raw_fname) as r:
+                print r
         os.remove(tmp_raw_fname)
 
 
