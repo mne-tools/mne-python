d06202d1a5fde68af4301a3813d13aa0fbb4c121
Teon Brooks <teon.brooks@gmail.com>
2013-02-01 03:28:01 -0500

    ADD: read_sns, ENH: methods->functions

--- mne/fiff/kit/coreg.py
+++ mne/fiff/kit/coreg.py
@@ -1,6 +1,6 @@
 """Coordinate Point Extractor for KIT system"""
 
-# Author: Teon Brooks <teon.brooks@gmail.com>
+# Author: Teon Brooks <teon@nyu.edu>
 #
 # License: BSD (3-clause)
 
@@ -33,13 +33,13 @@ class coreg:
     """
     def __init__(self, mrk_fname, elp_fname, hsp_fname):
 
-        mrk_points = self._read_mrk(mrk_fname=mrk_fname)
+        mrk_points = read_mrk(mrk_fname=mrk_fname)
         self.mrk_points = transform_pts(mrk_points)
 
-        elp_points = self._read_elp(elp_fname=elp_fname)
+        elp_points = read_elp(elp_fname=elp_fname)
         self.elp_points = transform_pts(elp_points)
 
-        hsp_points = self._read_hsp(hsp_fname=hsp_fname)
+        hsp_points = read_hsp(hsp_fname=hsp_fname)
         self.hsp_points = []
         for idx, point in enumerate(hsp_points):
             point_dict = {}
@@ -52,33 +52,42 @@ class coreg:
             self.hsp_points.append(point_dict)
 
 
-    def _read_mrk(self, mrk_fname):
-        """marker point extraction"""
+def read_mrk(mrk_fname):
+    """marker point extraction"""
 
-        # pattern by Tal Linzen:
-        p = re.compile(r'Marker \d:   MEG:x= *([\.\-0-9]+), ' +
-                       r'y= *([\.\-0-9]+), z= *([\.\-0-9]+)')
-        mrk_points = p.findall(open(mrk_fname).read())
-        mrk_points = np.array(mrk_points, dtype=float)
-        return mrk_points
+    # pattern by Tal Linzen:
+    p = re.compile(r'Marker \d:   MEG:x= *([\.\-0-9]+), ' +
+                   r'y= *([\.\-0-9]+), z= *([\.\-0-9]+)')
+    mrk_points = p.findall(open(mrk_fname).read())
+    mrk_points = np.array(mrk_points, dtype=float)
+    return mrk_points
 
-    def _read_elp(self, elp_fname):
-        """elp point extraction"""
+def read_elp(elp_fname):
+    """elp point extraction"""
 
-        p = re.compile('%N\t\d-[A-Z]+\s+([\.\-0-9]+)\t' +
-                       '([\.\-0-9]+)\t([\.\-0-9]+)')
-        elp_points = p.findall(open(elp_fname).read())
-        elp_points = np.array(elp_points, dtype=float)
-        return elp_points
+    p = re.compile('%N\t\d-[A-Z]+\s+([\.\-0-9]+)\t' +
+                   '([\.\-0-9]+)\t([\.\-0-9]+)')
+    elp_points = p.findall(open(elp_fname).read())
+    elp_points = np.array(elp_points, dtype=float)
+    return elp_points
 
-    def _read_hsp(self, hsp_fname):
-        """hsp point extraction"""
+def read_hsp(hsp_fname):
+    """hsp point extraction"""
 
-        p = re.compile(r'//No.+\n(\d*)\t(\d)\s*')
-        v = re.split(p, open(hsp_fname).read())[1:]
-        hsp_points = np.fromstring(v[-1], sep='\t')
-        hsp_points = hsp_points.reshape(int(v[0]), int(v[1]))
-        return hsp_points
+    p = re.compile(r'//No.+\n(\d*)\t(\d)\s*')
+    v = re.split(p, open(hsp_fname).read())[1:]
+    hsp_points = np.fromstring(v[-1], sep='\t')
+    hsp_points = hsp_points.reshape(int(v[0]), int(v[1]))
+    return hsp_points
+
+def read_sns(sns_fname):
+    """sns coordinate extraction"""
+
+    p = re.compile(r'\d,[A-Za-z]*,([\.\-0-9]+),' +
+                   r'([\.\-0-9]+),([\.\-0-9]+),' +
+                   r'([\.\-0-9]+),([\.\-0-9]+)')
+    locs = np.array(p.findall(open(sns_fname).read()), dtype='float')
+    return locs
 
 def transform_pts(pts):
     """KIT-Neuromag transformer
@@ -89,6 +98,7 @@ def transform_pts(pts):
 
     """
     pts /= 1e3
+    pts = np.array(pts, ndmin=2)
     pts = pts[:, [1, 0, 2]]
     pts[:, 0] *= -1
     return pts
