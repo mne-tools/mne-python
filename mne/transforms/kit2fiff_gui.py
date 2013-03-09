558f607483a34043ce46ba16e3aff6d6a8e5049c
Christian Brodbeck <christianmbrodbeck@gmail.com>
2013-03-09 01:37:42 -0500

    ki2fiff_gui

--- mne/transforms/kit2fiff_gui.py
+++ mne/transforms/kit2fiff_gui.py
@@ -7,9 +7,11 @@
 import cPickle as pickle
 import os
 
+import numpy as np
+from scipy.linalg import inv
+
 from mayavi.core.ui.mayavi_scene import MayaviScene
 from mayavi.tools.mlab_scene_model import MlabSceneModel
-import numpy as np
 from pyface.api import confirm, error, FileDialog, OK, YES, ProgressDialog
 from traits.api import HasTraits, HasPrivateTraits, cached_property, on_trait_change, Instance, Property, \
                        Array, Bool, Button, Color, Enum, File, Float, Int, List, \
@@ -21,15 +23,16 @@ from tvtk.pyface.scene_editor import SceneEditor
 from .marker_gui import MarkerPanel
 from .coreg import fit_matched_pts
 from .transforms import apply_trans, coord_trans
-from .viewer import HeadViewController, PointObject
+from .viewer import HeadViewController, headview_borders, headview_item, PointObject
 from ..fiff.kit.coreg import read_hsp, read_elp, transform_ALS_to_RAS, \
                              get_neuromag_transform
-from ..fiff.kit.kit import RawKIT
+from ..fiff.kit.kit import RawKIT, KIT
 
 
 
 use_editor = CheckListEditor(cols=5, values=[(i, str(i)) for i in xrange(5)])
-hsp_wildcard = ['Pickled Head Shape (*.pickled)|*.pickled',
+hsp_wildcard = ['Supported Files (*.pickled;*.txt)|*.pickled;*.txt',
+                'Pickled Head Shape (*.pickled)|*.pickled',
                 'Plain Text File (*.txt)|*.txt']
 
 
@@ -37,10 +40,13 @@ class Kit2FiffPanel(HasPrivateTraits):
     """Control panel for kit2fiff conversion"""
     # Source Files
     sqd_file = File(exists=True, filter=['*.sqd'])
+    sqd_fname = Property(Str, depends_on='sqd_file')
     sns_file = File(exists=True, filter=['*.txt'])
     hsp_file = File(exists=True, filter=hsp_wildcard, desc="Digitizer head "
                     "shape")
+    hsp_fname = Property(Str, depends_on='hsp_file')
     fid_file = File(exists=True, filter=['*.txt'], desc="Digitizer fiducials")
+    fid_fname = Property(Str, depends_on='fid_file')
 
     # Raw
     raw = Property(depends_on=['sqd_file', 'sns_file'])
@@ -61,7 +67,8 @@ class Kit2FiffPanel(HasPrivateTraits):
     fid_src = Property(depends_on=['neuromag_trans'])
     hsp_src = Property(depends_on=['hsp_raw', 'neuromag_trans'])
 
-    dev_head_trans = Property(depends_on=['elp_src', 'use_mrk', 'mrk'])
+    dev_head_trans = Array(shape=(4, 4))
+    head_dev_trans = Array(shape=(4, 4))
 
     # Events
     events = Array(Int, shape=(None,), value=[])
@@ -78,17 +85,43 @@ class Kit2FiffPanel(HasPrivateTraits):
     can_save = Property(Bool, depends_on=['raw', 'fid_src', 'elp_src',
                                           'hsp_src', 'dev_head_trans'])
     save_as = Button(label='Save FIFF...')
+    save_feedback = Str('')
 
     view = View(VGroup(VGroup(Item('sns_file', label='SNS File'),
                               Item('sqd_file', label="Data"),
+                              Item('sqd_fname', show_label=False, style='readonly'),
                               Item('fid_file', label='Dig Points'),
+                              Item('fid_fname', show_label=False, style='readonly'),
                               Item('hsp_file', label='Head Shape'),
+                              Item('hsp_fname', show_label=False, style='readonly'),
                               Item('use_mrk', editor=use_editor, style='custom'),
                               label="Sources", show_border=True),
 #                       VGroup(Item('endian', style='custom'),
 #                              Item('event_info', style='readonly', show_label=False),
 #                              label='Events', show_border=True),
-                       Item('save_as', enabled_when='can_save', show_label=False)))
+                       Item('save_as', enabled_when='can_save', show_label=False),
+                       Item('save_feedback', show_label=False, style='readonly')))
+
+    @cached_property
+    def _get_sqd_fname(self):
+        if self.sqd_file:
+            return os.path.basename(self.sqd_file)
+        else:
+            return '-'
+
+    @cached_property
+    def _get_hsp_fname(self):
+        if self.hsp_file:
+            return os.path.basename(self.hsp_file)
+        else:
+            return '-'
+
+    @cached_property
+    def _get_fid_fname(self):
+        if self.fid_file:
+            return os.path.basename(self.fid_file)
+        else:
+            return '-'
 
     @cached_property
     def _get_mrk(self):
@@ -96,21 +129,45 @@ class Kit2FiffPanel(HasPrivateTraits):
 
     @cached_property
     def _get_elp_raw(self):
-        if os.path.exists(self.fid_file):
+        if not self.fid_file:
+            return
+
+        try:
             pts = read_elp(self.fid_file)
+        except Exception as err:
+            error(None, str(err), "Error Reading Fiducials")
+            self.reset_traits(['fid_file'])
+            raise
+        else:
             return pts
 
     @cached_property
     def _get_hsp_raw(self):
         fname = self.hsp_file
-        if os.path.exists(fname):
+        if not fname:
+            return
+
+        try:
             _, ext = os.path.splitext(fname)
             if ext == '.pickled':
                 with open(fname) as fid:
                     food = pickle.load(fid)
-                return food['hsp']
+                pts = food['hsp']
             else:
-                return read_hsp(fname)
+                pts = read_hsp(fname)
+
+            if len(pts) > KIT.DIG_POINTS:
+                err = ("Too many points in head shape file (%i). Use the "
+                       "headshape GUI to create a head shape with fewer than "
+                       "%i points." % (len(pts), KIT.DIG_POINTS))
+                raise ValueError(err)
+
+        except Exception as err:
+            error(None, str(err), "Error Reading Head Shape")
+            self.reset_traits(['hsp_file'])
+            raise
+        else:
+            return pts
 
     @cached_property
     def _get_neuromag_trans(self):
@@ -148,13 +205,16 @@ class Kit2FiffPanel(HasPrivateTraits):
             pts = np.dot(pts, self.neuromag_trans.T)
             return pts
 
-    @cached_property
-    def _get_dev_head_trans(self):
+    @on_trait_change('elp_src,use_mrk,mrk')
+    def update_dev_head_trans(self):
         if (self.mrk is None) or not np.any(self.fid_src):
-            return np.empty((0, 0))
+            trans = np.zeros((4, 4))
+            self.dev_head_trans = trans
+            self.head_dev_trans = trans
+            return
 
-        src_pts = self.elp_src
-        dst_pts = self.mrk
+        src_pts = self.mrk
+        dst_pts = self.elp_src
 
         n_use = len(self.use_mrk)
         if n_use < 3:
@@ -167,7 +227,8 @@ class Kit2FiffPanel(HasPrivateTraits):
             dst_pts = dst_pts[self.use_mrk]
 
         trans = fit_matched_pts(src_pts, dst_pts, out='trans')
-        return trans
+        self.dev_head_trans = trans
+        self.head_dev_trans = inv(trans)
 
     @cached_property
     def _get_event_info(self):
@@ -219,6 +280,7 @@ class Kit2FiffPanel(HasPrivateTraits):
                          default_path=default_path)
         dlg.open()
         if dlg.return_code != OK:
+            self.save_feedback = "Saving aborted."
             return
 
         fname = dlg.path
@@ -228,6 +290,7 @@ class Kit2FiffPanel(HasPrivateTraits):
                 answer = confirm(None, "The file %r already exists. Should it be "
                                  "replaced?", "Overwrite File?")
                 if answer != YES:
+                    self.save_feedback = "Saving aborted."
                     return
 
         raw = self.raw
@@ -239,32 +302,35 @@ class Kit2FiffPanel(HasPrivateTraits):
         prog.open()
         prog.update(0)
 
+        self.save_feedback = "Saving ..."
         try:
             raw.save(fname, overwrite=True)
         except Exception as err:
             prog.close()
             msg = str(err)
+            self.save_feedback = "Saving failed."
             error(None, msg, "Error Saving Fiff")
             raise
         else:
             prog.close()
+            self.save_feedback = "Saved: %r" % os.path.basename(fname)
 
     @on_trait_change('scene.activated')
     def _init_plot(self):
         self.fid_obj = PointObject(scene=self.scene, color=(25, 225, 25),
                                    point_scale=5e-3)
         self.sync_trait('fid_src', self.fid_obj, 'points', mutual=False)
-        self.sync_trait('dev_head_trans', self.fid_obj, 'trans', mutual=False)
+        self.sync_trait('head_dev_trans', self.fid_obj, 'trans', mutual=False)
 
         self.elp_obj = PointObject(scene=self.scene, color=(50, 50, 220),
                                    point_scale=1e-2, opacity=.2)
         self.sync_trait('elp_src', self.elp_obj, 'points', mutual=False)
-        self.sync_trait('dev_head_trans', self.elp_obj, 'trans', mutual=False)
+        self.sync_trait('head_dev_trans', self.elp_obj, 'trans', mutual=False)
 
         self.hsp_obj = PointObject(scene=self.scene, color=(200, 200, 200),
                                    point_scale=2e-3)
         self.sync_trait('hsp_src', self.hsp_obj, 'points', mutual=False)
-        self.sync_trait('dev_head_trans', self.hsp_obj, 'trans', mutual=False)
+        self.sync_trait('head_dev_trans', self.hsp_obj, 'trans', mutual=False)
 
 
 
@@ -300,6 +366,18 @@ class ControlPanel(HasTraits):
 
 
 
+view_hrs = View(HGroup(Item('scene',
+                            editor=SceneEditor(scene_class=MayaviScene)),
+                       VGroup(headview_borders,
+                              Item('panel', style='custom'),
+                              show_labels=False),
+                       show_labels=False,
+                      ),
+                resizable=True,
+                buttons=NoButtons)
+
+
+
 class MainWindow(HasTraits):
     """GUI for interpolating between two KIT marker files"""
     scene = Instance(MlabSceneModel, ())
@@ -314,13 +392,14 @@ class MainWindow(HasTraits):
         p = ControlPanel(scene=self.scene)
         return p
 
-    view = View(HGroup(Item('scene',
-                            editor=SceneEditor(scene_class=MayaviScene)),
-                       VGroup(Item('headview', style='custom'),
-                              Item('panel', style='custom'),
+    view = View(HGroup(VGroup(Item('scene', editor=SceneEditor(scene_class=MayaviScene),
+                                   dock='vertical', show_label=False),
+                              VGroup(headview_item,
+                                     show_labels=False,
+                                     ),
+                              ),
+                       VGroup(Item('panel', style='custom'),
                               show_labels=False),
                        show_labels=False,
                       ),
-                resizable=True,
-                height=0.75, width=0.75,
-                buttons=NoButtons)
+                width=1100, resizable=True, buttons=NoButtons)
