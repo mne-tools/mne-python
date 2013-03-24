5dda7baa05c6459fa9d3405f235d77c797199023
dengemann <denis.engemann@gmail.com>
2013-03-24 23:03:10 +0100

    ENH: improve example doc text

--- examples/plot_delayed_ssp_projector.py
+++ examples/plot_delayed_ssp_projector.py
@@ -3,10 +3,13 @@
 Create evoked objects in delayed SSP mode
 =========================================
 
-This script shows how to apply SSP projectors at the evoked
-stage. We first will extract Epochs and create evoked objects
-with the required settings and then explore the different SSP
-projectors at the evoked stage.
+This script shows how to apply SSP projectors delayed, that is,
+at the evoked stage. This is particularly useful to support decisions
+related to the trade-off between denoising and preserving signal.
+We first will extract Epochs and create evoked objects
+with the required settings for delayed SSP application.
+Then we will explore the impact of the particular SSP projectors
+on the evoked data.
 
 """
 # Authors: Alexandre Gramfort <gramfort@nmr.mgh.harvard.edu>
@@ -27,7 +30,7 @@ raw_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
 event_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw-eve.fif'
 event_id, tmin, tmax = 1, -0.2, 0.5
 
-#   Setup for reading the raw data
+# Setup for reading the raw data
 raw = fiff.Raw(raw_fname)
 events = mne.read_events(event_fname)
 
@@ -35,7 +38,12 @@ events = mne.read_events(event_fname)
 picks = fiff.pick_types(raw.info, meg='mag', stim=False, eog=True,
                         include=[], exclude='bads')
 
-# Read epochs with proj == False
+# Note. if the reject parameter is not None but proj == False
+# each epoch will be projected to inform the rejection decision.
+# If, in this mode, the epoch is considered good,
+# instead of the projected epochs an the original data will be included in the
+# epochs object. This allows us to have both, rejection and the option to delay
+# the application of our SSP projectors. This also works for preloaded data.
 epochs = mne.Epochs(raw, events, event_id, tmin, tmax, picks=picks,
                     baseline=(None, 0), reject=dict(mag=4e-12),
                     proj=False)
@@ -56,7 +64,7 @@ pl.ylabel('MEG evoked fields (fT)')
 pl.title('Magnetometers | SSP off')
 pl.show()
 
-# now with projectors activated
+# Now with all projectors activated.
 times = evoked.times * 1e3
 import pylab as pl
 pl.figure()
@@ -64,18 +72,24 @@ evoked.copy().apply_proj().plot()
 pl.xlim([times[0], times[-1]])
 pl.xlabel('time (ms)')
 pl.ylabel('MEG evoked fields (fT)')
-pl.title('Magnetometers | SSP off')
+pl.title('Magnetometers | SSP on')
 pl.show()
 
-# finally we are going to track the incremental effects of the single
+# Finally we are going to investigate the incremental effects of the single
 # projection vectors.
 
 title = 'Incremental SSP application'
 projs, evoked.info['projs'] = evoked.info['projs'], []  # pop projs
 fig, axes = pl.subplots(2, 2)  # create 4 subplots for our four vectors
+
+# As the bulk of projectors was extracted from the same source, an incremental
+# 'protocol' will be informative. We could also easily visualize the impact of
+# single projection vectors by deleting the vector directly after plotting
+# by adding the following line to the loop:
+#   evoked.del_proj(-1)
 for proj, ax in zip(projs, axes.flatten()):
     evoked.add_proj(proj)  # add and apply on a copy,
-    evoked.copy().apply_proj().plot(axes=ax)  # as this can not be undone.
+    evoked.copy().apply_proj().plot(axes=ax)  # as this cannot be undone.
     ax.set_title('+ %s' % proj['desc'])
 pl.suptitle(title)
 pl.show()
