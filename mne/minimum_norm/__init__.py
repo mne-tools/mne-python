76665de14d06170be87681ac19fceff29ded7dcd
Martin Luessi <mluessi@nmr.mgh.harvard.edu>
2013-04-24 16:36:06 -0400

    gamma map cleanup

--- mne/minimum_norm/__init__.py
+++ mne/minimum_norm/__init__.py
@@ -6,4 +6,3 @@ from .inverse import read_inverse_operator, apply_inverse, \
                      compute_rank_inverse
 from .time_frequency import source_band_induced_power, source_induced_power, \
                             compute_source_psd, compute_source_psd_epochs
-from .gamma_map import gamma_map_inverse
