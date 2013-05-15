a99132d674be53955f65920fec6ff9be8d3cb369
rgoj <roman.goj@gmail.com>
2013-05-15 14:32:22 +0100

    Compare orientation picking in LCMV volume example

--- examples/inverse/plot_lcmv_beamformer_volume.py
+++ examples/inverse/plot_lcmv_beamformer_volume.py
@@ -49,7 +49,7 @@ epochs = mne.Epochs(raw, events, event_id, tmin, tmax, proj=True,
                     reject=dict(grad=4000e-13, mag=4e-12, eog=150e-6))
 evoked = epochs.average()
 
-forward = mne.read_forward_solution(fname_fwd)
+forward = mne.read_forward_solution(fname_fwd, surf_ori=True)
 
 noise_cov = mne.read_cov(fname_cov)
 noise_cov = mne.cov.regularize(noise_cov, evoked.info,
@@ -57,31 +57,83 @@ noise_cov = mne.cov.regularize(noise_cov, evoked.info,
 
 data_cov = mne.compute_covariance(epochs, tmin=0.04, tmax=0.15)
 stc = lcmv(evoked, forward, noise_cov, data_cov, reg=0.01)
+stc_normal = lcmv(evoked, forward, noise_cov, data_cov, pick_ori='normal',
+                  reg=0.01)
+stc_optimal = lcmv(evoked, forward, noise_cov, data_cov, pick_ori='optimal',
+                   reg=0.01)
 
 # Save result in stc files
 stc.save('lcmv-vol')
+stc_normal.save('lcmv-normal-vol')
+stc_optimal.save('lcmv-optimal-vol')
 
 stc.crop(0.0, 0.2)
+stc_normal.crop(0.0, 0.2)
+stc_optimal.crop(0.0, 0.2)
 
 # Save result in a 4D nifti file
 img = mne.save_stc_as_volume('lcmv_inverse.nii.gz', stc,
         forward['src'], mri_resolution=False)  # True for full MRI resolution
+img_normal = mne.save_stc_as_volume('lcmv_normal_inverse.nii.gz', stc_normal,
+                                    forward['src'], mri_resolution=False)
+img_optimal = mne.save_stc_as_volume('lcmv_optimal_inverse.nii.gz',
+                                     stc_optimal, forward['src'],
+                                     mri_resolution=False)
 
 # plot result (one slice)
 pl.close('all')
 data = img.get_data()
+data_normal = img_normal.get_data()
+data_optimal = img_optimal.get_data()
 coronal_slice = data[:, 10, :, 60]
-pl.figure()
-pl.imshow(np.ma.masked_less(coronal_slice, 1), cmap=pl.cm.Reds,
+coronal_slice_normal = data_normal[:, 10, :, 60]
+coronal_slice_optimal = data_optimal[:, 10, :, 60]
+c_limits = (-2, 2.3)
+pl.figure(figsize=(20, 5))
+pl.subplot(1, 3, 1)
+#pl.imshow(np.ma.masked_less(coronal_slice, 1), cmap=pl.cm.Reds,
+#          interpolation='nearest')
+pl.imshow(coronal_slice, cmap=pl.cm.Spectral_r,
           interpolation='nearest')
+pl.clim(c_limits)
 pl.colorbar()
 pl.contour(coronal_slice != 0, 1, colors=['black'])
 pl.xticks([])
 pl.yticks([])
 
+pl.subplot(1, 3, 2)
+pl.imshow(coronal_slice_normal, cmap=pl.cm.Spectral_r,
+          interpolation='nearest')
+pl.clim(c_limits)
+pl.colorbar()
+pl.contour(coronal_slice != 0, 1, colors=['black'])
+pl.xticks([])
+pl.yticks([])
+
+pl.subplot(1, 3, 3)
+pl.imshow(coronal_slice_optimal, cmap=pl.cm.Spectral_r,
+          interpolation='nearest')
+pl.clim(c_limits)
+pl.colorbar()
+pl.contour(coronal_slice != 0, 1, colors=['black'])
+pl.xticks([])
+pl.yticks([])
+
+
 # plot source time courses with the maximum peak amplitudes
-pl.figure()
+pl.figure(figsize=(20, 5))
+y_limits = (-2, 2.3)
+pl.subplot(1, 3, 1)
 pl.plot(stc.times, stc.data[np.argsort(np.max(stc.data, axis=1))[-40:]].T)
+pl.ylim(y_limits)
+pl.subplot(1, 3, 2)
+pl.plot(stc_normal.times, stc_normal.data[np.argsort(np.max(stc.data,
+                                          axis=1))[-40:]].T)
+pl.ylim(y_limits)
+pl.subplot(1, 3, 3)
+pl.plot(stc_optimal.times, stc_optimal.data[np.argsort(np.max(stc.data,
+                                            axis=1))[-40:]].T)
+pl.ylim(y_limits)
 pl.xlabel('Time (ms)')
 pl.ylabel('LCMV value')
 pl.show()
