439a5de4701250a2b24966e6510e43e1949100ab
Christian Brodbeck <christianmbrodbeck@gmail.com>
2013-05-19 22:57:51 +0200

    mne.gui

--- mne/gui.py
+++ mne/gui.py
@@ -1,6 +1,8 @@
 """Convenience functions for opening GUIs."""
 
-from .transforms.coreg import trans_fname as _trans
+# Authors: Christian Brodbeck <christianbrodbeck@nyu.edu>
+#
+# License: BSD (3-clause)
 
 
 def combine_markers(mrk1='', mrk2=''):
@@ -12,8 +14,8 @@ def combine_markers(mrk1='', mrk2=''):
         Path to source marker files (*.sqd; can be empty str, in which case the
         files can be loaded in GUI)
     """
-    from .transforms.marker_gui import MainWindow
-    gui = MainWindow(mrk1=mrk1, mrk2=mrk2)
+    from .transforms.marker_gui import CombineMarkersFrame
+    gui = CombineMarkersFrame(mrk1=mrk1, mrk2=mrk2)
     gui.configure_traits()
     return gui
 
@@ -54,34 +56,6 @@ def fiducials(subject=None, subjects_dir=None, fid_file=None):
     """
     from .transforms.fiducials_gui import MainWindow
     gui = MainWindow(subject, subjects_dir, fid_file=fid_file)
-    gui.configure_traits()
-    return gui
-
-
-def fit_mri_to_head(raw, s_from='fsaverage', s_to=None, trans_fname=_trans,
-                    subjects_dir=None):
-    """Open a gui for head-mri coregistration
-
-    Parameters
-    ----------
-    raw : str(path)
-        path to a raw file containing the digitizer data.
-    s_from : str
-        name of the source subject (e.g., 'fsaverage').
-    s_to : str | None
-        Name of the the subject for which the MRI is destined (used to
-        save MRI and in the trans file's file name).
-        Can be None if the raw file-name starts with "{subject}_".
-    trans_fname : str
-        Filename pattern for the trans file. "{raw_dir}" will be formatted to
-        the directory containing the raw file, and "{subject}" will be
-        formatted to s_to.
-    subjects_dir : None | path
-        Override the SUBJECTS_DIR environment variable
-        (sys.environ['SUBJECTS_DIR'])
-    """
-    from .transforms.coreg_gui import MriHeadCoreg
-    gui = MriHeadCoreg(raw, s_from, s_to, trans_fname, subjects_dir)
     gui.configure_traits()
     return gui
 
