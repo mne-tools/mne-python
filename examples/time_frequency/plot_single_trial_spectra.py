f470363212b65506a64b69de6020294c699e3b7b
dengemann <denis.engemann@gmail.com>
2013-05-21 15:18:18 +0200

    ENH: refactor example + address discussion
    COSMITS

--- examples/time_frequency/plot_single_trial_spectra.py
+++ examples/time_frequency/plot_single_trial_spectra.py
@@ -1,14 +1,16 @@
 """
-======================================
-Investigate Single Trial Power Spectra
-======================================
+==========================================
+Investigate Single Trial Magnitude Spectra
+==========================================
 
 In some situations it is desirable conduct a simple FFT spectral
-analyis, either to inform or to validate subsequent / previous
-multitaper or time-frequency analyses.
-In this exammple we will look at single trial spectra and then
-compute average cross-trial spectra to identify channels and
+analysis, either to inform or to validate subsequent / previous
+time-frequency analyses.
+In this example we will look at single trial spectra and then
+compute average spectra to identify channels and
 frequencies of interest for subsequent TFR analyses.
+We will conclude with exposing different windowing techniques that
+help addressing the problem of spectral leakage.
 """
 
 # Authors: Denis Engemann <d.engemann@fz-juelich.de>
@@ -19,6 +21,7 @@ print __doc__
 
 import numpy as np
 import scipy
+from scipy import fftpack
 
 import mne
 from mne import fiff
@@ -32,7 +35,6 @@ event_fname = data_path + '/MEG/sample/sample_audvis_raw-eve.fif'
 
 # Setup for reading the raw data
 raw = fiff.Raw(raw_fname, preload=True)
-# raw.filter(l_freq=5, n_jobs=2)
 events = mne.read_events(event_fname)
 
 event_id = 1
@@ -55,28 +57,36 @@ sfreq, n_times = raw.info['sfreq'], data.shape[2]
 freqs = np.arange(n_times) * sfreq / n_times
 
 # select frequencies of interests
-foi = (freqs > 7) & (freqs < 100)  # cut below 7Hz to improve plot
+freq_mask = (freqs > 7) & (freqs < 100)  # cut below 7Hz to improve plot
 
 # compute magnitudes from Fast Fourier transform for selected frequencies
-magnitudes = np.abs(scipy.fft(data))[:, :, foi]
-
-some_trial = 12
-powers = [magnitudes[some_trial], np.mean(magnitudes, axis=0)]
-titles = ['Single trial FFT magnitudes', 'cross-trial FFT magnitudes']
-yticks = (np.arange(0, 200, 20), freqs[foi][::20].round(1))
+magnitudes = np.abs(fftpack.fft(data))[:, :, freq_mask]
 
 import pylab as pl
-for power, title in zip(powers, titles):
-    pl.figure()
-    pl.title(title)
-    pl.imshow(power.T, aspect='auto', origin='lower')
-    pl.ylabel('Frequency (Hz)')
-    pl.xlabel('MEG channels (Gradiomemters)')
-    pl.yticks(*yticks)
-    pl.show()
+some_trial = 12
+spectra = (magnitudes[some_trial], np.mean(magnitudes, axis=0))
+titles = ('single trial', 'averaged across trials')
+xlabel = 'MEG channels (Gradiomemters)'
+ylabel = 'Frequency (Hz)'
+yticks = freqs[freq_mask].round(1)
 
-max_power_ch = tuple(epochs.ch_names[k] for k in np.argmax(powers[1], 0)[:2])
-print 'Maximum power in channels: %s, %s' % max_power_ch
+fig, axes = pl.subplots(1, 2, sharex=True, sharey=True, figsize=(10, 5))
+for ii, (spec, title, ax) in enumerate(
+        zip(spectra, titles, axes.flatten())):
+    ax.set_title(title, size='small')
+    ax.imshow(spec.T, aspect='auto', origin='lower')
+    ax.set_yticks(np.arange(0, len(yticks), 20))
+    if ii < 1:
+        ax.set_xlabel(xlabel)
+        ax.set_yticklabels(yticks[::20])
+        ax.set_ylabel(ylabel)
+
+fig.suptitle('FFT spectra (rectangular window)')
+mne.viz.tight_layout(2)
+fig.show()
+
+max_ch = tuple(epochs.ch_names[k] for k in np.argmax(spectra[1], 0)[:2])
+print 'Maximum magnitude in channels: %s, %s' % max_ch
 
 # In the second image we can observe certain channel groups exposing
 # stronger power than others. Second, in comparison to the single
@@ -86,3 +96,43 @@ print 'Maximum power in channels: %s, %s' % max_power_ch
 # Against the background of these insights we might want to supply
 # the channels around indices 100, 180 to a time-frequency
 # analysis for frequencies 7-30Hz. (=> continued in plot_time_frequency)
+
+# However, as we did not use an explicit windowing function the waveforms
+# are truncated at the beginning and the end of our time window which
+# is not beneficial for the quality of our spectral estimation.
+# In the following, we will hence apply a Hanning window and multi-taper
+# windows to mitigate the spectral leakage and double-check our results.
+
+# let's assemble a list of of alternative single trial spectral estimates.
+
+# first the Hanning
+hw = scipy.hanning(n_times)
+magnitudes = [np.abs(fftpack.fft(data[some_trial] * hw))[:, freq_mask]]
+
+# now the multi taper ...
+from mne.time_frequency.multitaper import dpss_windows
+half_nbw = 8   # let's take a few tapers to make a difference
+n_tapers_max = half_nbw * 2
+
+# compute windows and weights
+dpss, eigvals = dpss_windows(n_times, half_nbw, n_tapers_max, low_bias=True)
+weights = np.sqrt(eigvals)[np.newaxis, :, np.newaxis]
+# apply windows
+tapered_spectra = np.abs(fftpack.fft(data[some_trial, :, np.newaxis, :] * \
+                         dpss))[:, :, freq_mask]
+# apply weights
+magnitudes += [np.mean(np.abs(weights * tapered_spectra), axis=1)]
+# now put everything together
+for m, title in zip(magnitudes, ['Hanning', 'multitaper']):
+    pl.figure()
+    pl.title('single trial spectra (%s)' % title)
+    pl.imshow(m.T, aspect='auto', origin='lower')
+    pl.ylabel(xlabel)
+    pl.yticks(np.arange(0, len(yticks), 20), yticks[::20])
+    pl.xlabel(ylabel)
+    pl.show()
+
+# The quality of the plots improves with the tapers applied,
+# especially with the multi taper. Still, for our purpose the initial
+# variant produced acceptable results, so we don't need to update
+# our inferences. It is advisable to at least use a Hanning window.
