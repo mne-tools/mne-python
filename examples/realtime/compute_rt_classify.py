f296bdd6462016b6ed98c5673c4b97b5b5a016a4
Mainak Jas <mainakjas@gmail.com>
2013-06-07 17:24:16 +0300

    Modified classifier.py so that it is general enough to take clf from any classifier

--- examples/realtime/compute_rt_classify.py
+++ examples/realtime/compute_rt_classify.py
@@ -3,7 +3,7 @@
 Decoding real-time data
 =======================
 
-Supervised machine learning applied to MEG data in sensor space. 
+Supervised machine learning applied to MEG data in sensor space.
 Here the classifier is updated every 5 trials and the decoding
 accuracy is plotted
 """
@@ -18,6 +18,7 @@ from mne.realtime import RtClient, RtEpochs, RtClassifier
 
 import numpy as np
 import pylab as pl
+from sklearn.svm import SVC
 
 client = RtClient('localhost')
 info = client.get_measurement_info()
@@ -25,19 +26,19 @@ info = client.get_measurement_info()
 tmin, tmax = -0.2, 0.5
 event_id = dict(aud_l=1, vis_l=3)
 
-minchunks = 5 # decode every 5 trials
-tr_percent = 60 # Training %
-min_trials = 10 # minimum trials after which decoding should start
+chunk_size = 5  # decode every 5 trials
+tr_percent = 60  # Training %
+min_trials = 10  # minimum trials after which decoding should start
 
 # select gradiometers
 picks = mne.fiff.pick_types(info, meg='grad', eeg=False, eog=True,
                             stim=True, exclude=info['bads'])
 
 # create the real-time epochs object
-rt_epochs = RtEpochs(client, event_id, tmin, tmax, minchunks,
+rt_epochs = RtEpochs(client, event_id, tmin, tmax, chunk_size,
                      consume_epochs=False, picks=picks, decim=1,
                      reject=dict(grad=4000e-13, eog=150e-6))
-                     
+
 # start the acquisition
 rt_epochs.start()
 
@@ -50,40 +51,48 @@ times, score = [], []
 pl.ion()
 
 epochs = np.array([ep for ep in rt_epochs])
-Y = np.array([y for y in rt_epochs.events])[0:minchunks,2]
+Y = np.array([y for y in rt_epochs.events])[0:chunk_size, 2]
 
-while True:        
-    
-    # Fetch epochs and labels   
-    epochs = np.append(epochs, np.array([ep for ep in rt_epochs]),axis=0)
-    Y = np.append(Y, np.array([y for y in rt_epochs.events])[0:minchunks,2])
-    
-    rt_classifier = RtClassifier()
-    
+while True:
+
+    # Fetch epochs and labels
+    epochs = np.append(epochs, np.array([ep for ep in rt_epochs]), axis=0)
+    Y = np.append(Y, np.array([y for y in rt_epochs.events])[0:chunk_size, 2])
+
+    rt_epochs.remove_old_epochs(chunk_size)
+
+    #rt_classifier = RtClassifier()
+
     if np.shape(epochs)[0] >= min_trials:
 
+        clf = SVC(C=1, kernel='linear')
+
+        rt_classifier = RtClassifier(clf)
+
         # Find number of trials in training and test set
-        [Tr_X, Ts_X, Tr_Y, Ts_Y] = rt_classifier.split_data(epochs, Y, tr_percent)            
-        
+        [Tr_X, Ts_X, Tr_Y, Ts_Y] = rt_classifier.split_data(epochs,
+                                                            Y, tr_percent)
+
         # Online training and testing
-        clf = rt_classifier.fit(Tr_X, Tr_Y, method='SVM')
-        result = clf.predict(clf, Ts_X)
+        rt_classifier = rt_classifier.fit(Tr_X, Tr_Y)
+        result = rt_classifier.predict(Ts_X)
 
-        #acc = sum(result==Ts_Y)/tsnum*100
-        
-        #times.append(1e3 * rt_epochs.times[np.shape(epochs)[0]])
-        #score.append(acc)        
-        
-        # Plot accuracy     
-        #pl.clf()
-        #pl.plot(times, score, '+', label="Classif. score")
-        #pl.hold(True)
-        #pl.plot(times, score)
-        #pl.axhline(50, color='k', linestyle='--', label="Chance level")
-        #pl.xlabel('Times (ms)')
-        #pl.ylabel('Classification score (% correct)')
-        #pl.ylim([30, 105])
-        #pl.title('Real-time decoding')
-        #pl.show()
+        tsnum = np.shape(epochs)[0] - round(np.shape(epochs)[0]*tr_percent/100)
+        acc = sum(result == Ts_Y)/tsnum*100
 
-        #pl.waitforbuttonpress(0.1)
+        times.append(1e3 * rt_epochs.times[np.shape(epochs)[0]])
+        score.append(acc)
+
+        # Plot accuracy
+        pl.clf()
+        pl.plot(times, score, '+', label="Classif. score")
+        pl.hold(True)
+        pl.plot(times, score)
+        pl.axhline(50, color='k', linestyle='--', label="Chance level")
+        pl.xlabel('Times (ms)')
+        pl.ylabel('Classification score (% correct)')
+        pl.ylim([30, 105])
+        pl.title('Real-time decoding')
+        pl.show()
+
+        pl.waitforbuttonpress(0.1)
--- mne/realtime/classifier.py
+++ mne/realtime/classifier.py
@@ -1,40 +1,32 @@
 # Author: Mainak Jas <mainak@neuro.hut.fi>
-#          
+#
 # License: BSD (3-clause)
 
-from sklearn.svm import SVC
 import numpy as np
+
 
 class RtClassifier:
 
     """
-    
+
     Parameters
     ----------
-    
+
     Attributes
     ----------
-    
-    
+
     """
-    
-    def __init__(self):
-        
-        print "Instantiating classifier object ..."
-                
-        #[Tr_X, Ts_X, Tr_Y, Ts_Y] = split_data(self, epochs, Y, tr_percent)
-        
-        #clf = fit(self, Tr_X, Tr_Y, method='SVM')
-        #result = clf.predict(self, Ts_X)        
-        
-        #return result
-                
-    def split_data(self, epochs, Y, tr_percent):      
+
+    def __init__(self, estimator):
+
+        self.estimator = estimator
+
+    def split_data(self, epochs, Y, tr_percent):
         """Split data into training and test set
 
         Parameters
         ----------
-        epochs : 
+        epochs :
         Y :
         tr_percent:
 
@@ -44,34 +36,28 @@ class RtClassifier:
         Ts_X:
         Tr_Y:
         Ts_Y:
-        
+
         """
-        
+
         trnum = round(np.shape(epochs)[0]*tr_percent/100)
-        tsnum = np.shape(epochs)[0] - trnum 
-        
-        Tr_X = np.reshape(epochs[:trnum,:,:], 
+        tsnum = np.shape(epochs)[0] - trnum
+
+        Tr_X = np.reshape(epochs[:trnum, :, :],
                           [trnum, np.shape(epochs)[2]*np.shape(epochs)[1]])
-        Ts_X= np.reshape(epochs[-tsnum:,:,:], 
-                         [tsnum, np.shape(epochs)[2]*np.shape(epochs)[1]])
+        Ts_X = np.reshape(epochs[-tsnum:, :, :],
+                          [tsnum, np.shape(epochs)[2]*np.shape(epochs)[1]])
         Tr_Y = Y[:trnum]
         Ts_Y = Y[-tsnum:]
-        
+
         return Tr_X, Ts_X, Tr_Y, Ts_Y
-    
-    def fit(self, Tr_X, Tr_Y, method):
-        
-        # Online training and testing
-        
-        if method=='SVM':
-            clf = SVC(C=1, kernel='linear')
-              
-        clf.fit(Tr_X,Tr_Y)
-        
-        return clf
-        
-    def predict(self, clf, Ts_X):
-        
-        result = clf.predict(Ts_X)
-        
-        return result, clf
+
+    def fit(self, X, y):
+
+        self.estimator.fit(X, y)
+        return self
+
+    def predict(self, X):
+
+        result = self.estimator.predict(X)
+
+        return result
