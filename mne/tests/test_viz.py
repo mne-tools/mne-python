41f19cd0ec50cdd433614b016551dbbf5cc04901
Christian Brodbeck <christianmbrodbeck@gmail.com>
2013-06-09 14:58:54 -0400

    pep8

--- mne/tests/test_viz.py
+++ mne/tests/test_viz.py
@@ -54,8 +54,9 @@ epochs = Epochs(raw, events[:10], event_id, tmin, tmax, picks=picks,
                 baseline=(None, 0))
 evoked = epochs.average()
 reject = dict(mag=4e-12)
-epochs_delayed_ssp = Epochs(raw, events[:10], event_id, tmin, tmax, picks=picks,
-                            baseline=(None, 0), proj='delayed', reject=reject)
+epochs_delayed_ssp = Epochs(raw, events[:10], event_id, tmin, tmax,
+                            picks=picks, baseline=(None, 0), proj='delayed',
+                            reject=reject)
 evoked_delayed_ssp = epochs_delayed_ssp.average()
 layout = read_layout('Vectorview-all')
 
@@ -186,8 +187,65 @@ def test_plot_image_epochs():
 def test_plot_connectivity_circle():
     """Test plotting connectivity circle
     """
-    node_order = ['frontalpole-lh', 'parsorbitalis-lh', 'lateralorbitofrontal-lh', 'rostralmiddlefrontal-lh', 'medialorbitofrontal-lh', 'parstriangularis-lh', 'rostralanteriorcingulate-lh', 'temporalpole-lh', 'parsopercularis-lh', 'caudalanteriorcingulate-lh', 'entorhinal-lh', 'superiorfrontal-lh', 'insula-lh', 'caudalmiddlefrontal-lh', 'superiortemporal-lh', 'parahippocampal-lh', 'middletemporal-lh', 'inferiortemporal-lh', 'precentral-lh', 'transversetemporal-lh', 'posteriorcingulate-lh', 'fusiform-lh', 'postcentral-lh', 'bankssts-lh', 'supramarginal-lh', 'isthmuscingulate-lh', 'paracentral-lh', 'lingual-lh', 'precuneus-lh', 'inferiorparietal-lh', 'superiorparietal-lh', 'pericalcarine-lh', 'lateraloccipital-lh', 'cuneus-lh', 'cuneus-rh', 'lateraloccipital-rh', 'pericalcarine-rh', 'superiorparietal-rh', 'inferiorparietal-rh', 'precuneus-rh', 'lingual-rh', 'paracentral-rh', 'isthmuscingulate-rh', 'supramarginal-rh', 'bankssts-rh', 'postcentral-rh', 'fusiform-rh', 'posteriorcingulate-rh', 'transversetemporal-rh', 'precentral-rh', 'inferiortemporal-rh', 'middletemporal-rh', 'parahippocampal-rh', 'superiortemporal-rh', 'caudalmiddlefrontal-rh', 'insula-rh', 'superiorfrontal-rh', 'entorhinal-rh', 'caudalanteriorcingulate-rh', 'parsopercularis-rh', 'temporalpole-rh', 'rostralanteriorcingulate-rh', 'parstriangularis-rh', 'medialorbitofrontal-rh', 'rostralmiddlefrontal-rh', 'lateralorbitofrontal-rh', 'parsorbitalis-rh', 'frontalpole-rh']
-    label_names = ['bankssts-lh', 'bankssts-rh', 'caudalanteriorcingulate-lh', 'caudalanteriorcingulate-rh', 'caudalmiddlefrontal-lh', 'caudalmiddlefrontal-rh', 'cuneus-lh', 'cuneus-rh', 'entorhinal-lh', 'entorhinal-rh', 'frontalpole-lh', 'frontalpole-rh', 'fusiform-lh', 'fusiform-rh', 'inferiorparietal-lh', 'inferiorparietal-rh', 'inferiortemporal-lh', 'inferiortemporal-rh', 'insula-lh', 'insula-rh', 'isthmuscingulate-lh', 'isthmuscingulate-rh', 'lateraloccipital-lh', 'lateraloccipital-rh', 'lateralorbitofrontal-lh', 'lateralorbitofrontal-rh', 'lingual-lh', 'lingual-rh', 'medialorbitofrontal-lh', 'medialorbitofrontal-rh', 'middletemporal-lh', 'middletemporal-rh', 'paracentral-lh', 'paracentral-rh', 'parahippocampal-lh', 'parahippocampal-rh', 'parsopercularis-lh', 'parsopercularis-rh', 'parsorbitalis-lh', 'parsorbitalis-rh', 'parstriangularis-lh', 'parstriangularis-rh', 'pericalcarine-lh', 'pericalcarine-rh', 'postcentral-lh', 'postcentral-rh', 'posteriorcingulate-lh', 'posteriorcingulate-rh', 'precentral-lh', 'precentral-rh', 'precuneus-lh', 'precuneus-rh', 'rostralanteriorcingulate-lh', 'rostralanteriorcingulate-rh', 'rostralmiddlefrontal-lh', 'rostralmiddlefrontal-rh', 'superiorfrontal-lh', 'superiorfrontal-rh', 'superiorparietal-lh', 'superiorparietal-rh', 'superiortemporal-lh', 'superiortemporal-rh', 'supramarginal-lh', 'supramarginal-rh', 'temporalpole-lh', 'temporalpole-rh', 'transversetemporal-lh', 'transversetemporal-rh']
+    node_order = ['frontalpole-lh', 'parsorbitalis-lh',
+                  'lateralorbitofrontal-lh', 'rostralmiddlefrontal-lh',
+                  'medialorbitofrontal-lh', 'parstriangularis-lh',
+                  'rostralanteriorcingulate-lh', 'temporalpole-lh',
+                  'parsopercularis-lh', 'caudalanteriorcingulate-lh',
+                  'entorhinal-lh', 'superiorfrontal-lh', 'insula-lh',
+                  'caudalmiddlefrontal-lh', 'superiortemporal-lh',
+                  'parahippocampal-lh', 'middletemporal-lh',
+                  'inferiortemporal-lh', 'precentral-lh',
+                  'transversetemporal-lh', 'posteriorcingulate-lh',
+                  'fusiform-lh', 'postcentral-lh', 'bankssts-lh',
+                  'supramarginal-lh', 'isthmuscingulate-lh', 'paracentral-lh',
+                  'lingual-lh', 'precuneus-lh', 'inferiorparietal-lh',
+                  'superiorparietal-lh', 'pericalcarine-lh',
+                  'lateraloccipital-lh', 'cuneus-lh', 'cuneus-rh',
+                  'lateraloccipital-rh', 'pericalcarine-rh',
+                  'superiorparietal-rh', 'inferiorparietal-rh', 'precuneus-rh',
+                  'lingual-rh', 'paracentral-rh', 'isthmuscingulate-rh',
+                  'supramarginal-rh', 'bankssts-rh', 'postcentral-rh',
+                  'fusiform-rh', 'posteriorcingulate-rh',
+                  'transversetemporal-rh', 'precentral-rh',
+                  'inferiortemporal-rh', 'middletemporal-rh',
+                  'parahippocampal-rh', 'superiortemporal-rh',
+                  'caudalmiddlefrontal-rh', 'insula-rh', 'superiorfrontal-rh',
+                  'entorhinal-rh', 'caudalanteriorcingulate-rh',
+                  'parsopercularis-rh', 'temporalpole-rh',
+                  'rostralanteriorcingulate-rh', 'parstriangularis-rh',
+                  'medialorbitofrontal-rh', 'rostralmiddlefrontal-rh',
+                  'lateralorbitofrontal-rh', 'parsorbitalis-rh',
+                  'frontalpole-rh']
+    label_names = ['bankssts-lh', 'bankssts-rh', 'caudalanteriorcingulate-lh',
+                   'caudalanteriorcingulate-rh', 'caudalmiddlefrontal-lh',
+                   'caudalmiddlefrontal-rh', 'cuneus-lh', 'cuneus-rh',
+                   'entorhinal-lh', 'entorhinal-rh', 'frontalpole-lh',
+                   'frontalpole-rh', 'fusiform-lh', 'fusiform-rh',
+                   'inferiorparietal-lh', 'inferiorparietal-rh',
+                   'inferiortemporal-lh', 'inferiortemporal-rh', 'insula-lh',
+                   'insula-rh', 'isthmuscingulate-lh', 'isthmuscingulate-rh',
+                   'lateraloccipital-lh', 'lateraloccipital-rh',
+                   'lateralorbitofrontal-lh', 'lateralorbitofrontal-rh',
+                   'lingual-lh', 'lingual-rh', 'medialorbitofrontal-lh',
+                   'medialorbitofrontal-rh', 'middletemporal-lh',
+                   'middletemporal-rh', 'paracentral-lh', 'paracentral-rh',
+                   'parahippocampal-lh', 'parahippocampal-rh',
+                   'parsopercularis-lh', 'parsopercularis-rh',
+                   'parsorbitalis-lh', 'parsorbitalis-rh',
+                   'parstriangularis-lh', 'parstriangularis-rh',
+                   'pericalcarine-lh', 'pericalcarine-rh', 'postcentral-lh',
+                   'postcentral-rh', 'posteriorcingulate-lh',
+                   'posteriorcingulate-rh', 'precentral-lh', 'precentral-rh',
+                   'precuneus-lh', 'precuneus-rh',
+                   'rostralanteriorcingulate-lh',
+                   'rostralanteriorcingulate-rh', 'rostralmiddlefrontal-lh',
+                   'rostralmiddlefrontal-rh', 'superiorfrontal-lh',
+                   'superiorfrontal-rh', 'superiorparietal-lh',
+                   'superiorparietal-rh', 'superiortemporal-lh',
+                   'superiortemporal-rh', 'supramarginal-lh',
+                   'supramarginal-rh', 'temporalpole-lh', 'temporalpole-rh',
+                   'transversetemporal-lh', 'transversetemporal-rh']
     node_angles = circular_layout(label_names, node_order, start_pos=90)
     con = np.random.randn(68, 68)
     plot_connectivity_circle(con, label_names, n_lines=300,
