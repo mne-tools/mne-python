FIX Move normal orientation picking after noise norm calculation
Further ensures consistency with minimum_norm solution and fixes test for
normal orientation solution being precisely smaller than the free orientation
solution.

mne/beamformer/_lcmv.py
@@ -160,11 +160,6 @@ def _apply_lcmv(data, info, tmin, forward, noise_cov, data_cov, reg,
     if pick_ori == 'max-power':
         W = W[0::3]
 
-    # Pick source orientation normal to cortical surface
-    if pick_ori == 'normal':
-        W = W[2::3]
-        is_free_ori = False
-
     # noise normalization
     noise_norm = np.sum(W ** 2, axis=1)
     if is_free_ori:
@@ -173,6 +168,11 @@ def _apply_lcmv(data, info, tmin, forward, noise_cov, data_cov, reg,
 
     if not is_free_ori:
         W /= noise_norm[:, None]
+
+    # Pick source orientation normal to cortical surface
+    if pick_ori == 'normal':
+        W = W[2::3]
+        is_free_ori = False
 
     if isinstance(data, np.ndarray) and data.ndim == 2:
         data = [data]