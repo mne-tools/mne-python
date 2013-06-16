87f746619309e41e504af25bbeb194ec99d5f29d
denis <denis.engemann@gmail.com>
2013-06-17 01:08:31 +0200

    FIX: add files

---
+++ mne/info.py
@@ -0,0 +1,30 @@
+# Authors: Denis Engemann <d.engemann@fz-juelich.de>
+#
+# License: Simplified BSD
+
+
+class Info(dict):
+    """ Info class to nicely represent info dicts
+    """
+    def __init__(self, *args, **kwargs):
+        dict.__init__(self, *args, **kwargs)
+
+    def __repr__(self):
+        """Summarize info instead of printing all"""
+        st = '<Info | %s non-empty fields'
+        non_empty = 0
+        strs = []
+        for k, v in self.items():
+            this_len = (len(v) if hasattr(v, '__len__') else
+                       ('%s' % v if v is not None else None))
+            entries = ((' | %d items' % this_len) if isinstance(this_len, int)
+                       else (' | %s' % this_len if this_len else ''))
+            if entries:
+                non_empty += 1
+            strs.append('%s : %s%s' % (k, str(type(v))[7:-2], entries))
+        strs_non_empty = sorted(s for s in strs if '|' in s)
+        strs_empty = sorted(s for s in strs if '|' not in s)
+        st += '\n    '.join(strs_non_empty + strs_empty)
+        st += '\n>'
+        st %= non_empty
+        return st
