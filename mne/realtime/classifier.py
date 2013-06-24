2d8d4da380ad4fd9aad1f44c9dd7adc46c03f0e8
Mainak Jas <mainakjas@gmail.com>
2013-06-24 16:17:04 +0300

    Added a class to concatenate time-series across channels

--- mne/realtime/classifier.py
+++ mne/realtime/classifier.py
@@ -2,9 +2,12 @@
 #
 # License: BSD (3-clause)
 
+import numpy as np
+
 from sklearn.base import TransformerMixin
-from mne.fiff import pick_types
+
 from mne.time_frequency import multitaper_psd
+from mne.fiff import pick_types
 
 
 class RtClassifier:
@@ -37,21 +40,56 @@ class RtClassifier:
 
 
 class Scaler(TransformerMixin):
-
+    """
+        Standardizes data separately for each channel
+    """
     def __init__(self, info):
         self.info = info
 
-    def transform(self, epochs_data):
+    def fit(self, epochs, y):
+        """
+        Dummy fit method
+        """
+
+        return self
+
+    def transform(self, epochs):
+        """
+        Standardizes data for every channel
+        """
+
+        X = epochs.get_data()
 
         picks_list = [pick_types(self.info, meg='mag', exclude='bads'),
                       pick_types(self.info, eeg='True', exclude='bads'),
                       pick_types(self.info, meg='grad', exclude='bads')]
 
         for pick_one in picks_list:
-                ch_mean = epochs_data[:, pick_one, :].mean(axis=1)[:, None, :]
-                epochs_data[:, pick_one, :] -= ch_mean
+            ch_mean = X[:, pick_one, :].mean(axis=1)[:, None, :]
+            X[:, pick_one, :] -= ch_mean
 
-        return epochs_data
+        return X
+
+
+class ConcatenateChannels(TransformerMixin):
+
+    def __init__(self, info=None):
+        self.info = info
+
+    def fit(self, epochs, y):
+        """
+        Dummy fit method
+        """
+        return self
+
+    def transform(self, epochs):
+        """
+        Concatenates data from different channels into a single feature vector
+        """
+        n_epochs, n_channels, n_time = epochs.shape
+        Z = np.reshape(np.transpose(epochs, (0, 2, 1)),
+                       (n_epochs, n_channels*n_time))
+        return Z
 
 
 class PSDEstimator(TransformerMixin):
