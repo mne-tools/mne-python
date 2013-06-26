2d6f29fcf741a3ef9b69c87226e03d270770f0e8
Mainak Jas <mainakjas@gmail.com>
2013-06-26 19:54:35 +0300

    Uses iter_evoked() method in epochs that was newly introduced and pipeline

--- examples/realtime/compute_rt_decoder.py
+++ examples/realtime/compute_rt_decoder.py
@@ -25,7 +25,7 @@ info = client.get_measurement_info()
 tmin, tmax = -0.2, 0.5
 event_id = dict(aud_l=1, vis_l=3)
 
-chunk_size = 5  # decode every 5 trials
+total_trials = 50
 tr_percent = 60  # Training %
 min_trials = 10  # minimum trials after which decoding should start
 
@@ -34,7 +34,7 @@ picks = mne.fiff.pick_types(info, meg='grad', eeg=False, eog=True,
                             stim=True, exclude=info['bads'])
 
 # create the real-time epochs object
-rt_epochs = RtEpochs(client, event_id, tmin, tmax, chunk_size,
+rt_epochs = RtEpochs(client, event_id, tmin, tmax, total_trials,
                      consume_epochs=False, picks=picks, decim=1,
                      reject=dict(grad=4000e-13, eog=150e-6))
 
@@ -43,54 +43,53 @@ rt_epochs.start()
 
 # Decoding in sensor space using a linear SVM
 n_times = len(rt_epochs.times)
-from sklearn.svm import SVC
 
-times, score = [], []
+from sklearn import preprocessing
+from sklearn.svm import SVC
+from sklearn.pipeline import Pipeline
+
+from mne.realtime.classifier import ConcatenateChannels
+
+scores_x, scores = [], []
 
 pl.ion()
 
-epochs = np.array([ep for ep in rt_epochs])
-Y = np.array([y for y in rt_epochs.events])[0:chunk_size, 2]
+scaler = preprocessing.StandardScaler()
+concatenator = ConcatenateChannels()
+clf = SVC(C=1, kernel='linear')
 
-while True:
+scaled_classifier = Pipeline([('concat', concatenator),
+                              ('scaler', scaler), ('svm', clf)])
 
-    # Fetch epochs and labels
-    epochs = np.append(epochs, np.array([ep for ep in rt_epochs]), axis=0)
-    Y = np.append(Y, np.array([y for y in rt_epochs.events])[0:chunk_size, 2])
+for ev_num, ev in enumerate(rt_epochs.iter_evoked()):
 
-    rt_epochs.remove_old_epochs(chunk_size)
+    if ev_num == 0:
+        X = ev.data[None, ...]
+        y = int(ev.comment)
+    else:
+        X = np.concatenate((X, ev.data[None, ...]), axis=0)
+        y = np.append(y, int(ev.comment))
 
-    if np.shape(epochs)[0] >= min_trials:
+    if ev_num >= min_trials:
 
         # Find number of trials in training and test set
-        trnum = round(np.shape(epochs)[0]*tr_percent/100)
-        tsnum = np.shape(epochs)[0] - trnum
+        trnum = round(np.shape(X)[0]*tr_percent/100)
+        tsnum = np.shape(X)[0] - trnum
 
-        # Separate trial and test set
-        Tr_X = np.reshape(epochs[:trnum, :, :],
-                          [trnum, np.shape(epochs)[2]*np.shape(epochs)[1]])
-        Ts_X = np.reshape(epochs[-tsnum:, :, :],
-                          [tsnum, np.shape(epochs)[2]*np.shape(epochs)[1]])
-        Tr_Y = Y[:trnum]
-        Ts_Y = Y[-tsnum:]
+        scaled_classifier = scaled_classifier.fit(X[:trnum], y[:trnum])
+        scores.append(scaled_classifier.score(X[-tsnum:], y[-tsnum:])*100)
 
-        # Online training and testing
-        clf = SVC(C=1, kernel='linear')
-        clf.fit(Tr_X, Tr_Y)
-        result = clf.predict(Ts_X)
-
-        acc = sum(result == Ts_Y)/tsnum*100
-
-        times.append(1e3 * rt_epochs.times[np.shape(epochs)[0]])
-        score.append(acc)
+        scores_x.append(ev_num)
 
         # Plot accuracy
         pl.clf()
-        pl.plot(times[-5:], score[-5:], '+', label="Classif. score")
+
+        pl.plot(scores_x[-5:], scores[-5:], '+',
+                label="Classif. score")
         pl.hold(True)
-        pl.plot(times[-5:], score[-5:])
+        pl.plot(scores_x[-5:], scores[-5:])
         pl.axhline(50, color='k', linestyle='--', label="Chance level")
-        pl.xlabel('Times (ms)')
+        pl.xlabel('Trials')
         pl.ylabel('Classification score (% correct)')
         pl.ylim([30, 105])
         pl.title('Real-time decoding')
