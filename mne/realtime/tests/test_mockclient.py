83f71bb34f535341ed21fab0c2b9bc90b6d3ad95
Mainak Jas <mainakjas@gmail.com>
2013-07-12 15:35:13 +0300

    Modified the test to directly check if the epoch_queue is the same as the raw data

--- mne/realtime/tests/test_mockclient.py
+++ mne/realtime/tests/test_mockclient.py
@@ -1,41 +1,39 @@
 import os.path as op
 
 import mne
-from mne import Epochs, read_events
 from mne.realtime import MockRtClient, RtEpochs
 
 from numpy.testing import assert_array_almost_equal
 
 base_dir = op.join(op.dirname(__file__), '..', '..', 'fiff', 'tests', 'data')
 raw_fname = op.join(base_dir, 'test_raw.fif')
-event_name = op.join(base_dir, 'test-eve.fif')
 
-# Fiff file to simulate the realtime client
 raw = mne.fiff.Raw(raw_fname, preload=True, verbose=False)
 
-events = read_events(event_name)
-
-n_epochs, event_id, tmin, tmax = 1, 1, -0.2, 0.5
-
 picks = mne.fiff.pick_types(raw.info, meg='grad', eeg=False, eog=True,
-                                stim=True, exclude=raw.info['bads'])
+                            stim=True, exclude=raw.info['bads'])
 
-#def test_mockclient():
 
-rt_client = MockRtClient(raw)
-rt_epochs = RtEpochs(rt_client, event_id, tmin, tmax, n_epochs,
-                     consume_epochs=False, picks=picks)
+def test_mockclient():
+    """Test whether the data sent by mockclient is the same as in the raw file
+    """
 
-rt_epochs.start()
-rt_client.send_data(rt_epochs, tmin=0, tmax=2, buffer_size=1000)
+    n_epochs, event_id, tmin, tmax = 1, 1, -0.2, 0.5
 
-for ev in rt_epochs.iter_evoked():
-    x_mock = ev.data[None, ...]
+    rt_client = MockRtClient(raw)
+    rt_epochs = RtEpochs(rt_client, event_id, tmin, tmax, n_epochs,
+                         consume_epochs=False, picks=picks)
 
-print "hi"
+    rt_epochs.start()
+    rt_client.send_data(rt_epochs, tmin=0, tmax=7, buffer_size=1000)
 
-epochs = Epochs(raw, events, event_id=event_id, tmin=tmin, tmax=tmax,
-                picks=picks, baseline=(None, 0))
-x_real = epochs.get_data()[0,:,:]
+    sfreq = raw.info['sfreq']
+    event_samp = rt_epochs.events[0][0]
 
-#assert_array_almost_equal(x_mock, x_real, decimal=6)
+    tmin_samp = int(round(sfreq * tmin)) + event_samp
+    tmax_samp = tmin_samp + len(rt_epochs._raw_times)
+
+    data_raw, _ = raw[picks, tmin_samp:tmax_samp]
+    data_proc = rt_epochs._epoch_queue[0]
+
+    assert_array_almost_equal(data_raw, data_proc, decimal=10)
