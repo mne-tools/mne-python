0053ca1d4966a71f841e58ee659bd1dfbc0c5cbc
trachelr <romain.trachel@inria.fr>
2013-07-22 13:55:57 +0200

    interfaced with sklearn covariance estimators

--- mne/decoding/csp.py
+++ mne/decoding/csp.py
@@ -5,8 +5,7 @@
 
 import numpy as np
 from scipy import linalg
-from . import compute_covariance
-from . import cov
+
 
 class CSP(object):
     """M/EEG signal decomposition using the Common Spatial Patterns (CSP)
@@ -20,6 +19,13 @@ class CSP(object):
     n_components : int, default 4
         The number of components to decompose M/EEG signals.
         This number should be set by cross-validation.
+    reg : float, dict, str, None
+        if not None, allow regularization for covariance estimation
+        if float, shrinkage covariance is used (0 <= shrinkage <= 1).
+        if str, optimal shrinkage using Ledoit-Wolf Shrinkage 'lws' 
+                or Oracle Approximating Shrinkage 'oas'
+        if dict, regularization factor for each sensor type used in 
+                 mne.regularize (e.g. mag, grad, eeg)
 
     Attributes
     ----------
@@ -36,14 +42,15 @@ class CSP(object):
     of the abnormal components in the clinical EEG. Electroencephalography
     and Clinical Neurophysiology, 79(6):440--447, December 1991.
     """
-    def __init__(self, n_components=4):
+    def __init__(self, n_components=4, reg=None):
         self.n_components = n_components
+        self.reg = reg
         self.filters_ = None
         self.patterns_ = None
         self.mean_ = None
         self.std_ = None
 
-    def fit(self, epochs_data, y, reg_cov=False):
+    def fit(self, epochs_data, y):
         """Estimate the CSP decomposition on epochs.
 
         Parameters
@@ -67,25 +74,63 @@ class CSP(object):
         classes = np.unique(y)
         if len(classes) != 2:
             raise ValueError("More than two different classes in the data.")
-        
-        if reg_cov:
-            # concatenate epochs
-            class_1 = np.transpose(epochs_data[y == classes[0]],
-                                   [1, 0, 2]).reshape(epochs_data.shape[1], -1)
-            class_2 = np.transpose(epochs_data[y == classes[1]],
-                                   [1, 0, 2]).reshape(epochs_data.shape[1], -1)
-
-            # fit on empirical covariance
-            self._fit(np.dot(class_1, class_1.T),
-                      np.dot(class_2, class_2.T))
+        # concatenate epochs
+        class_1 = np.transpose(epochs_data[y == classes[0]],
+                               [1, 0, 2]).reshape(epochs_data.shape[1], -1)
+        class_2 = np.transpose(epochs_data[y == classes[1]],
+                               [1, 0, 2]).reshape(epochs_data.shape[1], -1)
+        if self.reg == None:
+            # compute empirical covariance
+            cov_1 = np.dot(class_1, class_1.T)
+            cov_2 = np.dot(class_2, class_2.T)
         else:
-            # compute covariance on epochs
-            cov_1 = compute_covariance(epochs_data[y == classes[0]])
-            cov_2 = compute_covariance(epochs_data[y == classes[1]])
-            # fit on regularized covariance
-            self._fit(cov.regularize(cov_1).data,
-                      cov.regularize(cov_2).data)
+            if isinstance(self.reg, float):
+                # use shrinkage covariance regularization
+                if (self.reg < 0) or (self.reg > 1):
+                    raise ValueError('0 <= shrinkage <= 1 for '
+                                     'covariance regularization.')
+                try:
+                    from sklearn.covariance import ShrunkCovariance
+                except ImportError:
+                    raise Exception('the scikit-learn package is missing and '
+                                    'required for covariance regularization.')
+                
+                # init sklearn.covariance.ShrunkCovariance estimator
+                skl_cov = ShrunkCovariance(shrinkage=self.reg,
+                                           store_precision=False,
+                                           assume_centered=True)
+            elif isinstance(self.reg, str):
+                if self.reg is 'lws':
+                    try:
+                        from sklearn.covariance import LedoitWolf
+                    except ImportError:
+                        raise Exception('the scikit-learn package is missing '
+                                        'and required for regularization.')
+                    # init sklearn.covariance.LedoitWolf estimator
+                    skl_cov = LedoitWolf(store_precision=False,
+                                         assume_centered=True,
+                                         block_size=1000)  # can create errors
+                elif self.reg is 'oas':
+                    try:
+                        from sklearn.covariance import OAS
+                    except ImportError:
+                        raise Exception('the scikit-learn package is missing '
+                                        'and required for regularization.')
+                    # init sklearn.covariance.OAS estimator
+                    skl_cov = OAS(store_precision=False,
+                                  assume_centered=True)
+                else:
+                    raise ValueError('reg parameter must be lws or oas.')
+            else:
+                raise ValueError('reg parameter must be lws or oas.')
 
+            # compute regularized covariance using sklearn
+            cov_1 = skl_cov.fit(class_1.T).covariance_
+            cov_2 = skl_cov.fit(class_2.T).covariance_
+        
+        # then fit on covariance
+        self._fit(cov_1, cov_2)
+        
         pick_filters = self.filters_[:self.n_components]
         X = np.asarray([np.dot(pick_filters, e) for e in epochs_data])
 
