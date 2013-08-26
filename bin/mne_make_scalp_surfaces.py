19db55ef0ab8f8017b1f09831c16e2a6d80fb33f
dengemann <d.engemann@fz-juelich.de>
2013-08-26 17:40:06 +0200

    ENH: almost debugged. mne_surf2bem fails

--- bin/mne_make_scalp_surfaces.py
+++ bin/mne_make_scalp_surfaces.py
@@ -10,7 +10,7 @@
 
 Create high-resulution head surfaces for coordinate alignment.
 
-example usage: mne_make_scalp_surfaces.py -overwrite -subject sample
+example usage: mne_make_scalp_surfaces.py --overwrite --subject sample
 
 """
 import os
@@ -24,28 +24,31 @@ if __name__ == '__main__':
     from optparse import OptionParser
     parser = OptionParser()
     parser.add_option('-o', '--overwrite', dest='overwrite',
+                      action='store_true',
                       help='Overwrite previously computed surface')
     parser.add_option('-s', '--subject', dest='subject',
                       help='The name of the subject', type='str')
     options, args = parser.parse_args()
-    for opt in [options.overwrite, options.subject]:
-        if not opt:
-            parser.print_help()
-            sys.exit(-1)
-
+    if not options.subject:
+        parser.print_help()
+        sys.exit(-1)
     overwrite = options.overwrite
     subject = options.subject
     env = os.environ
+    env['SUBJECT'] = subject
 
     def my_run_cmd(cmd, err_msg):
         ret, out = getstatusoutput(cmd)
         if ret != 0:
             print err_msg
-            sys.exit(-1)
+            sys.exit(1)
 
     if not 'SUBJECTS_DIR' in env:
         print 'The environment variable SUBJECTS_DIR should be set'
         sys.exit(1)
+
+    if not op.isabs(env['SUBJECTS_DIR']):
+        env['SUBJECTS_DIR'] = op.abspath(env['SUBJECTS_DIR'])
     subj_dir = env['SUBJECTS_DIR']
 
     if not 'MNE_ROOT' in env:
@@ -64,40 +67,41 @@ if __name__ == '__main__':
     print '1. Creating a dense scalp tessellation with mkheadsurf...'
 
     my_seghead = None
-    if op.exists(op.join(env['SUBJECTS_DIR'], subject, 'surf',
+    if op.exists(op.join(subj_dir, subject, 'surf',
                  'lh.seghead')):
         my_seghead = 'lh.seghead'
-    elif op.exists(op.join(env['SUBJECTS_DIR'], subject, 'surf',
+    elif op.exists(op.join(subj_dir, subject, 'surf',
                    'lh.smseghead')):
         my_seghead = 'lh.smseghead'
     if my_seghead is None:
-        cmd = 'mkheadsurf -subjid %s -srcvol $mri >/dev/null' % subject
+        cmd = 'mkheadsurf -subjid %s -srcvol %s >/dev/null' % (subject, mri)
+        print cmd
         my_run_cmd(cmd, 'mkheadsurf failed')
     else:
         print '%s/%s/surf/%s already there' % (subj_dir, subject, my_seghead)
 
     surf = None
-    if op.exists(op.join(env['SUBJECTS_DIR'], subject, 'surf',
+    if op.exists(op.join(subj_dir, subject, 'surf',
                  'lh.seghead')):
-        surf = op.join(env['SUBJECTS_DIR'], subject, 'surf', 'lh.seghead')
-    elif op.exists(op.join(env['SUBJECTS_DIR'], subject, 'surf',
+        surf = op.join(subj_dir, subject, 'surf', 'lh.seghead')
+    elif op.exists(op.join(subj_dir, subject, 'surf',
                    'lh.smseghead')):
-        surf = op.join(env['SUBJECTS_DIR'], subject, 'surf', 'lh.seghead')
+        surf = op.join(subj_dir, subject, 'surf', 'lh.seghead')
     if surf is None:
         print 'mkheadsurf did not produce the standard output file.'
         sys.exit(1)
 
-    fif = '{0}/{1}/bem/${1}-head-dense.fif'.format(subj_dir, subject)
+    fif = '{0}/{1}/bem/{1}-head-dense.fif'.format(subj_dir, subject)
     print '2. Creating $fif...'
     cmd = 'mne_surf2bem --surf %s --id 4 --check --fif %s' % (surf, fif)
     my_run_cmd(cmd, 'Failed to create %s, see above' % fif)
 
     levels = 'medium', 'sparse'
     for ii, (ntri, level) in enumerate(zip([30000, 2500], levels), 3):
-        my_surf = mne.read_bem_surfaces(surf)[0]
+        my_surf = mne.read_bem_surfaces(fif)[0]
         print '%i. Creating medium grade tessellation...' % ii
         print '%i.1 Decimating the dense tessellation...' % ii
-        reduction = {30000: 0.6, 2500: 0.005}[ntri]
+        reduction = {30000: 0.87, 2500: 0.992}[ntri]
         points, tris = mne.decimate_surface(points=my_surf['rr'],
                                             triangles=my_surf['tris'],
                                             reduction=reduction)
