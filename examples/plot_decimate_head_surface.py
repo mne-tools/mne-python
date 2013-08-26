24bf4bc632c2d6ba3edbc426739f9875eb1ee21a
dengemann <d.engemann@fz-juelich.de>
2013-08-26 18:17:52 +0200

    ENH: cleanup2

--- examples/plot_decimate_head_surface.py
+++ examples/plot_decimate_head_surface.py
@@ -1,7 +1,3 @@
-# Authors: Denis Engemann <d.engemann@fz-juelich.de>
-#          Alexandre Gramfort <alexandre.gramfort@telecom-paristech.fr>
-#
-# License: BSD (3-clause)
 """
 =======================
 Decimating BEM surfaces
@@ -12,8 +8,11 @@ using a cloud of digitization points for coordinate alignment
 instead of e.g. EEG-cap positions.
 
 """
-
 print __doc__
+# Authors: Denis Engemann <d.engemann@fz-juelich.de>
+#          Alexandre Gramfort <alexandre.gramfort@telecom-paristech.fr>
+#
+# License: BSD (3-clause)
 
 import mne
 from mne.surface import decimate_surface
@@ -21,9 +20,11 @@ from mne.surface import decimate_surface
 path = mne.datasets.sample.data_path()
 surf = mne.read_bem_surfaces(path + '/subjects/sample/bem/sample-head.fif')[0]
 points = surf['rr']
-triangles = surf['tris']
+tris = surf['tris']
 
-points_dec, faces_dec = decimate_surface(points, triangles, reduction=0.944)
+target_ntri = 30000  # reduce to 30000 meshes.
+reduction = (target_ntri / surf['ntri'])
+points_dec, tris_dec = decimate_surface(points, tris, reduction=reduction)
 
 try:
     from enthought.mayavi import mlab
@@ -32,5 +33,5 @@ except:
 
 head_col = (0.95, 0.83, 0.83)  # light pink
 
-p, f = points_dec, faces_dec
+p, f = points_dec, tris_dec
 mlab.triangular_mesh(p[:, 0], p[:, 1], p[:, 2], f, color=head_col)
