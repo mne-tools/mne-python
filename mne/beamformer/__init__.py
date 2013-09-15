3d4ffe469868b08520c70fe7977adba35527d5fb
rgoj <roman.goj@gmail.com>
2013-09-15 20:33:58 +0100

    Alter init after renaming file

--- mne/beamformer/__init__.py
+++ mne/beamformer/__init__.py
@@ -3,4 +3,5 @@
 
 from ._lcmv import lcmv, lcmv_epochs, lcmv_raw
 from ._dics import dics, dics_epochs, dics_source_power
-from ._tf_beamformer import tf_dics
+from ._tf_dics import tf_dics
+from ._tf_lcmv import iter_filter_epochs, tf_lcmv
