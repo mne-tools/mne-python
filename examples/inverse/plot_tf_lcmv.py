03ec52c2006089466244a346b1a5aa4f4da7a1f4
rgoj <roman.goj@gmail.com>
2013-09-16 09:31:11 +0100

    ENH 5D time-frequency beamformer using LCMV

---
+++ examples/inverse/plot_tf_lcmv.py
@@ -0,0 +1,109 @@
+import numpy as np
+import matplotlib.pyplot as pl
+#from scipy.fftpack import fftfreq
+
+import logging
+logger = logging.getLogger('mne')
+
+import mne
+
+from mne.fiff import Raw
+from mne.datasets import sample
+from mne.beamformer import iter_filter_epochs, tf_lcmv
+
+data_path = sample.data_path()
+raw_fname = data_path + '/MEG/sample/sample_audvis_raw.fif'
+event_fname = data_path + '/MEG/sample/sample_audvis_raw-eve.fif'
+fname_fwd = data_path + '/MEG/sample/sample_audvis-meg-eeg-oct-6-fwd.fif'
+subjects_dir = data_path + '/subjects'
+label_name = 'Aud-lh'
+fname_label = data_path + '/MEG/sample/labels/%s.label' % label_name
+
+###############################################################################
+# Read raw data, preload to allow filtering
+raw = Raw(raw_fname, preload=True)
+raw.info['bads'] = ['MEG 2443']  # 1 bad MEG channel
+
+# Read forward operator
+forward = mne.read_forward_solution(fname_fwd, surf_ori=True)
+
+# Read label
+label = mne.read_label(fname_label)
+
+# Set picks
+picks = mne.fiff.pick_types(raw.info, meg=True, eeg=False, eog=False,
+                            stim=False, exclude='bads')
+
+# Read epochs
+event_id, epoch_tmin, epoch_tmax = 1, -0.2, 0.5
+events = mne.read_events(event_fname)[:3]  # TODO: Use all events
+#events = mne.read_events(event_fname)  # TODO: Use all events
+
+###############################################################################
+# Time-frequency beamforming based on LCMV
+
+# Setting frequency bins as in Dalal et al. 2008 (high gamma was subdivided)
+# TODO: Using 300 as last value leads to error, that's probably to be expected
+# due to sampling frequency being roughly 600, but maybe should be investigated
+#freq_bins = [(4, 12), (12, 30), (30, 55), (65, 300)]  # Hz
+freq_bins = [(4, 12), (12, 30), (30, 55), (65, 299)]  # Hz
+#win_lengths = [0.3, 0.2, 0.15, 0.1]  # s
+win_lengths = [0.2, 0.2, 0.2, 0.2]  # s
+
+# Setting time windows
+tmin = -0.2
+tmax = 0.5
+tstep = 0.2
+control = (-0.2, 0.0)
+
+stcs = []
+for i, (epochs_band, freq_bin) in enumerate(iter_filter_epochs(raw, freq_bins,
+                                                               events,
+                                                               event_id,
+                                                               epoch_tmin,
+                                                               epoch_tmax,
+                                                               control,
+                                                               n_jobs=4,
+                                                               picks=picks)):
+    stc = tf_lcmv(epochs_band, forward, label=label, tmin=tmin, tmax=tmax,
+                  tstep=tstep, win_length=win_lengths[i], control=control,
+                  reg=0.05)
+    stcs.append(stc)
+
+
+# Gathering results for each time window
+source_power = []
+for stc in stcs:
+    source_power.append(stc.data)
+
+# Finding the source with maximum source power to plot spectrogram for that
+# source
+source_power = np.array(source_power)
+max_index = np.unravel_index(source_power.argmax(), source_power.shape)
+max_source = max_index[1]
+
+# Preparing the time and frequency grid for plotting
+time_bounds = [tmin]
+for i in range(int(np.floor((tmax - tmin) / tstep))):
+    time_bounds.append(tmin + (i + 1) * tstep)
+freq_bounds = [freq_bins[0][0]]
+for freq_bin in freq_bins:
+    freq_bounds.append(freq_bin[1])
+time_grid, freq_grid = np.meshgrid(time_bounds, freq_bounds)
+
+# Plotting the results
+# TODO: The gap between 55 and 65 Hz should be marked on the final spectrogram
+pl.pcolor(time_grid, freq_grid, source_power[:, max_source, :],
+          cmap=pl.cm.jet)
+ax = pl.gca()
+pl.xlabel('Time window boundaries [s]')
+ax.set_xticks(time_bounds)
+pl.xlim(time_bounds[0], time_bounds[-1])
+pl.ylabel('Frequency bin boundaries [Hz]')
+pl.yscale('log')
+ax.set_yticks(freq_bounds)
+ax.set_yticklabels([np.round(freq, 2) for freq in freq_bounds])
+pl.ylim(freq_bounds[0], freq_bounds[-1])
+pl.grid(True, ls='-')
+pl.colorbar()
+pl.show()
---
+++ mne/beamformer/_tf_lcmv.py
@@ -0,0 +1,146 @@
+import numpy as np
+from scipy import linalg
+
+import logging
+logger = logging.getLogger('mne')
+
+import mne
+
+from ..fiff.pick import pick_channels_cov
+from ..forward import _subject_from_forward
+from ..cov import compute_whitener
+from ..source_estimate import SourceEstimate
+from ._lcmv import _prepare_beamformer_input
+
+
+def iter_filter_epochs(raw, freq_bins, events, event_id, tmin, tmax, baseline,
+                       n_jobs, picks):
+    """Filters raw data, creates and yields epochs
+    """
+    # Getting picks based on rejections prior to filtering
+    tmp_epochs = mne.Epochs(raw, events, event_id, tmin, tmax, picks=picks,
+                            proj=True, baseline=baseline, preload=True)
+    picks = tmp_epochs.picks
+
+    for freq_bin in freq_bins:
+        raw_band = raw.copy()
+        raw_band.filter(l_freq=freq_bin[0], h_freq=freq_bin[1], picks=picks,
+                        n_jobs=n_jobs)
+        # TODO: Which of these parameters should really be exposed? All?
+        # defaults taken from mne.Epochs?
+        epochs_band = mne.Epochs(raw_band, events, event_id, tmin, tmax,
+                                 proj=True, picks=picks, baseline=baseline,
+                                 preload=True,
+                                 reject=dict(grad=4000e-13, mag=4e-12))
+
+        yield epochs_band, freq_bin
+
+
+def tf_lcmv(epochs_band, forward, label, tmin, tmax, tstep, win_length,
+            control, reg):
+    # TODO: Check win_length and freq_bin match in length
+    # TODO: Check that no time window is longer than tstep
+    single_sols = []
+    overlap_sol = []
+    # TODO: Note that 0.3 / 0.05 produces 5.999! So n_overlap will be 5
+    # instead of the 6 that it should be, absurd! How to deal with this
+    # better than by multiplying by 1e3?!?
+    n_steps = int(((tmax - tmin) * 1e3) // (tstep * 1e3))
+    n_overlap = int((win_length * 1e3) // (tstep * 1e3))
+
+    # Calculating noise covariance
+    noise_cov = mne.compute_covariance(epochs_band, tmin=control[0], tmax=control[1])
+    noise_cov = mne.cov.regularize(noise_cov, epochs_band.info, mag=0.05,
+                                   grad=0.05, eeg=0.1, proj=True)
+
+    for i_time in range(n_steps):
+        win_tmin = tmin + i_time * tstep
+        win_tmax = win_tmin + win_length
+
+        if win_tmax < tmax + (epochs_band.times[-1] - epochs_band.times[-2]):
+            # Calculating data covariance in current time window
+            data_cov = mne.compute_covariance(epochs_band, tmin=win_tmin,
+                                              tmax=win_tmax)
+
+            stc = _lcmv_source_power(epochs_band.info, forward, noise_cov,
+                                     data_cov, reg=0.001, label=label)
+            single_sols.append(stc.data[:, 0])
+
+        # Average over all time windows that contain the current time
+        # point, which is the current time window along with n_overlap
+        # others
+        if i_time - n_overlap < 0:
+            curr_sol = np.mean(single_sols[0:i_time + 1], axis=0)
+        else:
+            curr_sol =\
+                np.mean(single_sols[i_time - n_overlap + 1:i_time + 1], axis=0)
+
+        # The final values for the current time point averaged over all
+        # time windows that contain it
+        overlap_sol.append(curr_sol)
+
+    sol = np.array(overlap_sol)
+    return SourceEstimate(sol.T, vertices=stc.vertno, tmin=tmin, tstep=tstep,
+                          subject=stc.subject)
+
+
+
+def _lcmv_source_power(info, forward, noise_cov, data_cov, reg=0.01,
+                       label=None, picks=None, pick_ori=None, verbose=None):
+    # TODO: Results of tf_dics are really weird, this function's results should
+    # be examined.
+    is_free_ori, picks, ch_names, proj, vertno, G =\
+        _prepare_beamformer_input(info, forward, label, picks, pick_ori)
+
+    # Handle whitening
+    whitener, _ = compute_whitener(noise_cov, info, picks)
+
+    # whiten the leadfield
+    G = np.dot(whitener, G)
+
+    # Apply SSPs + whitener to data covariance
+    data_cov = pick_channels_cov(data_cov, include=ch_names)
+    Cm = data_cov['data']
+    if info['projs']:
+        Cm = np.dot(proj, np.dot(Cm, proj.T))
+    Cm = np.dot(whitener, np.dot(Cm, whitener.T))
+
+    # Calculating regularized inverse, equivalent to an inverse operation after
+    # the following regularization:
+    # Cm += reg * np.trace(Cm) / len(Cm) * np.eye(len(Cm))
+    Cm_inv = linalg.pinv(Cm, reg)
+
+    # Compute spatial filters
+    W = np.dot(G.T, Cm_inv)
+    n_orient = 3 if is_free_ori else 1
+    n_sources = G.shape[1] // n_orient
+    source_power = np.zeros((n_sources, 1))
+    for k in range(n_sources):
+        Wk = W[n_orient * k: n_orient * k + n_orient]
+        Gk = G[:, n_orient * k: n_orient * k + n_orient]
+        Ck = np.dot(Wk, Gk)
+
+        if is_free_ori:
+            # Free source orientation
+            Wk[:] = np.dot(linalg.pinv(Ck, 0.1), Wk)
+        else:
+            # Fixed source orientation
+            Wk /= Ck
+
+        # Noise normalization
+        noise_norm = np.dot(Wk, Wk.T)
+        noise_norm = noise_norm.trace()
+
+        # Calculating source power
+        sp_temp = np.dot(np.dot(Wk, Cm), Wk.T)
+        sp_temp /= noise_norm
+        if pick_ori == 'normal':
+            source_power[k, 0] = sp_temp[2, 2]
+        else:
+            source_power[k, 0] = sp_temp.trace()
+
+    logger.info('[done]')
+
+    subject = _subject_from_forward(forward)
+    return SourceEstimate(source_power, vertices=vertno, tmin=1,
+                          tstep=1, subject=subject)
