93312b59beaaee706b3283f24a24ff3abca0dde2
rgoj <roman.goj@gmail.com>
2013-09-18 13:11:29 +0100

    Moving tf beamforming functions in init

--- mne/beamformer/__init__.py
+++ mne/beamformer/__init__.py
@@ -1,7 +1,5 @@
 """Beamformers for source localization
 """
 
-from ._lcmv import lcmv, lcmv_epochs, lcmv_raw
-from ._dics import dics, dics_epochs, dics_source_power
-from ._tf_dics import tf_dics
-from ._tf_lcmv import iter_filter_epochs, tf_lcmv
+from ._lcmv import lcmv, lcmv_epochs, lcmv_raw, iter_filter_epochs, tf_lcmv
+from ._dics import dics, dics_epochs, dics_source_power, tf_dics
