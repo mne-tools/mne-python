6e2cb35361e7d80216cd026962f2419804a6c38b
rgoj <roman.goj@gmail.com>
2013-09-24 13:57:34 +0100

    Refactor data reading into separate function in LCMV tests
    This is in preparation for further tests to be written now.

--- mne/beamformer/tests/test_lcmv.py
+++ mne/beamformer/tests/test_lcmv.py
@@ -9,60 +9,79 @@ from mne.datasets import sample
 from mne.beamformer import lcmv, lcmv_epochs, lcmv_raw
 from mne.source_estimate import SourceEstimate, VolSourceEstimate
 
+
 data_path = sample.data_path()
-fname_data = op.join(data_path, 'MEG', 'sample',
-                     'sample_audvis-ave.fif')
-fname_raw = op.join(data_path, 'MEG', 'sample',
-                    'sample_audvis_raw.fif')
-fname_cov = op.join(data_path, 'MEG', 'sample',
-                    'sample_audvis-cov.fif')
+fname_data = op.join(data_path, 'MEG', 'sample', 'sample_audvis-ave.fif')
+fname_raw = op.join(data_path, 'MEG', 'sample', 'sample_audvis_raw.fif')
+fname_cov = op.join(data_path, 'MEG', 'sample', 'sample_audvis-cov.fif')
 fname_fwd = op.join(data_path, 'MEG', 'sample',
                     'sample_audvis-meg-oct-6-fwd.fif')
 fname_fwd_vol = op.join(data_path, 'MEG', 'sample',
                         'sample_audvis-meg-vol-7-fwd.fif')
-fname_event = op.join(data_path, 'MEG', 'sample',
-                      'sample_audvis_raw-eve.fif')
+fname_event = op.join(data_path, 'MEG', 'sample', 'sample_audvis_raw-eve.fif')
 label = 'Aud-lh'
 fname_label = op.join(data_path, 'MEG', 'sample', 'labels', '%s.label' % label)
+
+
+def _get_data(tmin=-0.1, tmax=0.15, all_forward=True, epochs=True,
+              data_cov=True):
+    """Read in data used in tests
+    """
+    label = mne.read_label(fname_label)
+    events = mne.read_events(fname_event)
+    raw = mne.fiff.Raw(fname_raw, preload=False)
+    forward = mne.read_forward_solution(fname_fwd)
+    if all_forward:
+        forward_surf_ori = mne.read_forward_solution(fname_fwd, surf_ori=True)
+        forward_fixed = mne.read_forward_solution(fname_fwd, force_fixed=True,
+                                                  surf_ori=True)
+        forward_vol = mne.read_forward_solution(fname_fwd_vol, surf_ori=True)
+    else:
+        forward_surf_ori = None
+        forward_fixed = None
+        forward_vol = None
+
+    event_id, tmin, tmax = 1, tmin, tmax
+
+    # Setup for reading the raw data
+    raw.info['bads'] = ['MEG 2443', 'EEG 053']  # 2 bads channels
+
+    if epochs:
+        # Set up pick list: MEG - bad channels
+        left_temporal_channels = mne.read_selection('Left-temporal')
+        picks = mne.fiff.pick_types(raw.info, meg=True, eeg=False,
+                                    stim=True, eog=True, exclude='bads',
+                                    selection=left_temporal_channels)
+
+        # Read epochs
+        epochs = mne.Epochs(raw, events, event_id, tmin, tmax, proj=True,
+                            picks=picks, baseline=(None, 0), preload=True,
+                            reject=dict(grad=4000e-13, mag=4e-12, eog=150e-6))
+        epochs.resample(200, npad=0, n_jobs=2)
+        evoked = epochs.average()
+        info = evoked.info
+    else:
+        epochs = None
+        evoked = None
+        info = raw.info
+
+    noise_cov = mne.read_cov(fname_cov)
+    noise_cov = mne.cov.regularize(noise_cov, info,
+                                   mag=0.05, grad=0.05, eeg=0.1, proj=True)
+    if data_cov:
+        data_cov = mne.compute_covariance(epochs, tmin=0.04, tmax=0.15)
+    else:
+        data_cov = None
+
+    return raw, epochs, evoked, data_cov, noise_cov, label, forward,\
+        forward_surf_ori, forward_fixed, forward_vol
 
 
 def test_lcmv():
     """Test LCMV with evoked data and single trials
     """
-    # load forwards here to save memory
-    forward = mne.read_forward_solution(fname_fwd)
-    forward_surf_ori = mne.read_forward_solution(fname_fwd, surf_ori=True)
-    forward_fixed = mne.read_forward_solution(fname_fwd, force_fixed=True,
-                                              surf_ori=True)
-    forward_vol = mne.read_forward_solution(fname_fwd_vol, surf_ori=True)
-
-    event_id, tmin, tmax = 1, -0.1, 0.15
-    label = mne.read_label(fname_label)
-    noise_cov = mne.read_cov(fname_cov)
-    raw = mne.fiff.Raw(fname_raw, preload=False)
-    events = mne.read_events(fname_event)
-
-    # Setup for reading the raw data
-    raw.info['bads'] = ['MEG 2443', 'EEG 053']  # 2 bads channels
-
-    # Set up pick list: EEG + MEG - bad channels (modify to your needs)
-    left_temporal_channels = mne.read_selection('Left-temporal')
-    picks = mne.fiff.pick_types(raw.info, meg=True, eeg=False,
-                                stim=True, eog=True, exclude='bads',
-                                selection=left_temporal_channels)
-
-    # Read epochs
-    epochs = mne.Epochs(raw, events, event_id, tmin, tmax, proj=True,
-                        picks=picks, baseline=(None, 0), preload=True,
-                        reject=dict(grad=4000e-13, mag=4e-12, eog=150e-6))
-    epochs.resample(200, npad=0, n_jobs=2)
-    evoked = epochs.average()
-
-    noise_cov = mne.read_cov(fname_cov)
-    noise_cov = mne.cov.regularize(noise_cov, evoked.info,
-                                   mag=0.05, grad=0.05, eeg=0.1, proj=True)
-
-    data_cov = mne.compute_covariance(epochs, tmin=0.04, tmax=0.15)
+    raw, epochs, evoked, data_cov, noise_cov, label, forward,\
+        forward_surf_ori, forward_fixed, forward_vol = _get_data()
 
     for fwd in [forward, forward_vol]:
         stc = lcmv(evoked, fwd, noise_cov, data_cov, reg=0.01)
@@ -161,28 +180,14 @@ def test_lcmv():
 def test_lcmv_raw():
     """Test LCMV with raw data
     """
-    forward = mne.read_forward_solution(fname_fwd)
-    label = mne.read_label(fname_label)
-    noise_cov = mne.read_cov(fname_cov)
-    raw = mne.fiff.Raw(fname_raw, preload=False)
+    raw, _, _, _, noise_cov, label, forward, _, _, _ =\
+        _get_data(all_forward=False, epochs=False, data_cov=False)
 
     tmin, tmax = 0, 20
-    # Setup for reading the raw data
-    raw.info['bads'] = ['MEG 2443', 'EEG 053']  # 2 bads channels
-
-    # Set up pick list: EEG + MEG - bad channels (modify to your needs)
-    left_temporal_channels = mne.read_selection('Left-temporal')
-    picks = mne.fiff.pick_types(raw.info, meg=True, eeg=False, stim=True,
-                                eog=True, exclude='bads',
-                                selection=left_temporal_channels)
-
-    noise_cov = mne.read_cov(fname_cov)
-    noise_cov = mne.cov.regularize(noise_cov, raw.info,
-                                   mag=0.05, grad=0.05, eeg=0.1, proj=True)
-
     start, stop = raw.time_as_index([tmin, tmax])
 
     # use only the left-temporal MEG channels for LCMV
+    left_temporal_channels = mne.read_selection('Left-temporal')
     picks = mne.fiff.pick_types(raw.info, meg=True, exclude='bads',
                                 selection=left_temporal_channels)
 
