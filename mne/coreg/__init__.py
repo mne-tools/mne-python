d0a22f034dc6743d1d27c83462a2ca8f4eaf22e3
dengemann <d.engemann@fz-juelich.de>
2013-10-04 07:28:51 +0200

    FIX: coreg imports

--- mne/coreg/__init__.py
+++ mne/coreg/__init__.py
@@ -1,2 +1,3 @@
 from .coreg import create_default_subject, read_mri_cfg, scale_mri, \
-                   scale_labels, fit_matched_points, _decimate_points
+                   scale_labels, fit_matched_points, fit_point_cloud,\
+                   _decimate_points, _point_cloud_error, _is_mri_subject
