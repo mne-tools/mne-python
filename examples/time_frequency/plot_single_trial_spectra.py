db766f9708ccb55735394b1a571eaed9e1af5db3
dengemann <d.engemann@fz-juelich.de>
2013-10-13 16:33:56 +0200

    COSMITS

--- examples/time_frequency/plot_single_trial_spectra.py
+++ examples/time_frequency/plot_single_trial_spectra.py
@@ -22,7 +22,7 @@ import mne
 from mne import fiff
 from mne.datasets import sample
 from mne.time_frequency import compute_epochs_psd
-import pylab as pl
+import matplotlib.pyplot as plt
 ###############################################################################
 # Set parameters
 data_path = sample.data_path()
@@ -45,23 +45,27 @@ epochs = mne.Epochs(raw, events, event_id, tmin, tmax, picks=picks,
                     baseline=(None, 0), reject=dict(grad=4000e-13, eog=150e-6))
 
 
+# setup generator
 NFFT = 256  # the FFT size (NFFT). Ideally a power of 2
 n_jobs = 2
-generate_psd = compute_epochs_psd(epochs, fmin=2, fmax=300, NFFT=NFFT,
-                                  n_jobs=n_jobs, verbose=False)
+generate_psd = compute_epochs_psd(epochs, fmin=2, fmax=200, NFFT=NFFT,
+                                  n_jobs=n_jobs, proj=True, verbose=False)
 
-psds, psd, max_trial = None, None, 48
-for i_epoch, (psd, freqs) in enumerate(generate_psd, 1):
-    psd = 10 * np.log10(psd)  # use power
-    if i_epoch == 1:
-        psds = np.empty_like(psd)
-        first_psd = psd
-    psds += psd
-    if i_epoch == max_trial:
+# accumulate psds and save psds from first trial separately
+average_psds, max_trial = None, 20
+for ii, (psds, freqs) in enumerate(generate_psd):
+    if ii > max_trial:
         break
-psds /= i_epoch
+    # use dB and
+    psds = 10 * np.log10(psds)  # compute it here to avoid artifacts
+    if ii == 0:
+        average_psds = np.empty_like(psds)
+        first_psds =  psds
+    average_psds += psds
+average_psds = average_psds / max_trial
 
-fig, (ax1, ax2) = pl.subplots(1, 2, sharex=True, sharey=True, figsize=(10, 5))
+
+fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(10, 5))
 
 fig.suptitle('Single trial power', fontsize=12)
 
@@ -69,24 +73,24 @@ freq_mask = freqs < 150
 freqs = freqs[freq_mask]
 
 ax1.set_title('single trial', fontsize=10)
+ax1.imshow(first_psds[:, freq_mask].T, aspect='auto', origin='lower')
 ax1.set_yticks(np.arange(0, len(freqs), 10))
 ax1.set_yticklabels(freqs[::10].round(1))
 ax1.set_ylabel('Frequency (Hz)')
-ax1.imshow(first_psd[:, freq_mask].T, aspect='auto', origin='lower')
 
-ax2.set_title('avaraged over trials', fontsize=10)
-ax2.imshow(psds[:, freq_mask].T, aspect='auto', origin='lower')
+ax2.set_title('averaged over trials', fontsize=10)
+ax2.imshow(average_psds[:, freq_mask].T, aspect='auto', origin='lower')
 ax2.set_xticks(np.arange(0, len(picks), 30))
 ax2.set_xticklabels(picks[::30])
 ax2.set_xlabel('MEG channel index (Gradiomemters)')
 
 mne.viz.tight_layout()
-pl.show()
+plt.show()
 
-
-# In the second image we can observe certain channel groups exposing
+# In the second image we clearly observe certain channel groups exposing
 # stronger power than others. Second, in comparison to the single
 # trial image we can see the frequency extent slightly growing for these
-# channels which might indicate oscillatory responses. The time frequency
-# example investigates one of the channels around index 140.
+# channels which might indicate oscillatory responses.
+# The ``plot_time_frequency.py`` example investigates one of the channels
+# around index 140.
 # Finally, also note the power line artifacts across all channels.
