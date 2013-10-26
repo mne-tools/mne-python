23df0cbd106bbf451ed41a249520492f5616fd5f
Teon Brooks <teon.brooks@gmail.com>
2013-10-26 18:06:13 -0400

    UPDATE
    Combine the info dict construction into one method.

--- mne/fiff/edf/edf.py
+++ mne/fiff/edf/edf.py
@@ -11,11 +11,12 @@ import datetime
 import re
 import numpy as np
 
+from ...transforms import als_ras_trans_mm, apply_trans
 from ...fiff import pick_types
 from ...utils import verbose, logger
 from ..raw import Raw
 from ..constants import FIFF
-from ..kit import coreg
+from ..kit.coreg import get_head_coord_trans
 
 
 class RawEDF(Raw):
@@ -34,13 +35,13 @@ class RawEDF(Raw):
         If None, it will use the last channel in data.
 
     hpts : str | None
-        Path to the hpts file.
+        Path to the hpts file containing electrode positions.
         If None, sensor locations are (0,0,0).
 
     annot : str | None
-        Path of the annot file.
+        Path of the annot file containing the triggering information for EDF+.
         Can be None for BDF only.
-        If None for EDF, it will raise an error.
+        If None for EDF+, it will raise an error.
 
     verbose : bool, str, int, or None
         If not None, override default verbose level (see mne.verbose).
@@ -64,7 +65,7 @@ class RawEDF(Raw):
             ValueError('Must be an integer number.')
         logger.info('Extracting edf Parameters from %s...' % input_fname)
         input_fname = os.path.abspath(input_fname)
-        self.info = _get_edf_info(input_fname, hpts)
+        self.info = _get_edf_info(input_fname, n_eeg, stim_channel, hpts)
         logger.info('Creating Raw.info structure...')
 
         # Raw attributes
@@ -76,43 +77,6 @@ class RawEDF(Raw):
         self.last_samp = self.info['nsamples'] - 1
         self.comp = None  # no compensation for KIT
         self.proj = False
-
-        # Creates a list of dicts of eeg channels for raw.info
-        logger.info('Setting channel info structure...')
-        self.info['ch_names'] = ch_names = self.info['ch_names']
-        chan_locs = coreg.transform_pts(self.info['sensor_locs'])
-        self.info['chs'] = []
-        if stim_channel == None:
-            stim_channel = self.info['nchan'] - 1
-        for idx, ch_info in enumerate(zip(ch_names, chan_locs), 1):
-            ch_name, ch_loc = ch_info
-            chan_info = {}
-            chan_info['cal'] = 1.
-            chan_info['logno'] = idx
-            chan_info['scanno'] = idx
-            chan_info['range'] = 1
-            chan_info['unit_mul'] = 0
-            chan_info['ch_name'] = ch_name
-            chan_info['unit'] = FIFF.FIFF_UNIT_V
-            chan_info['coord_frame'] = FIFF.FIFFV_COORD_HEAD
-            chan_info['coil_type'] = FIFF.FIFFV_COIL_EEG
-            chan_info['kind'] = FIFF.FIFFV_EEG_CH
-            chan_info['eeg_loc'] = ch_loc
-            chan_info['loc'] = np.zeros(12)
-            chan_info['loc'][:3] = ch_loc
-            check1 = stim_channel == ch_name
-            check2 = stim_channel == idx
-            stim_check = np.logical_or(check1, check2)
-            # this deals with EOG, AUX channels
-            if idx > n_eeg:
-                chan_info['coil_type'] = FIFF.FIFFV_COIL_NONE
-                chan_info['kind'] = FIFF.FIFFV_MISC_CH
-            if stim_check:
-                chan_info['coil_type'] = FIFF.FIFFV_COIL_NONE
-                chan_info['unit'] = FIFF.FIFF_UNIT_NONE
-                chan_info['kind'] = FIFF.FIFFV_STIM_CH
-                chan_info['ch_name'] = 'STI 014'
-            self.info['chs'].append(chan_info)
 
         if preload:
             self._preloaded = preload
@@ -193,7 +157,7 @@ class RawEDF(Raw):
         data_size = self.info['data_size']
         data_offset = self.info['data_offset']
 
-        with open(self.info['fname'], 'rb') as fid:
+        with open(self.info['file_id'], 'rb') as fid:
             # extract data
             fid.seek(data_offset)
             nchan = self.info['nchan']
@@ -252,7 +216,7 @@ class RawEDF(Raw):
         return stim_channel
 
 
-def _get_edf_info(fname, hpts=None, annot=None):
+def _get_edf_info(fname, n_eeg, stim_channel, hpts=None, annot=None):
     """Extracts all the information from the EDF+,BDF file.
 
     Parameters
@@ -260,42 +224,71 @@ def _get_edf_info(fname, hpts=None, annot=None):
     rawfile : str
         Raw EDF+,BDF file to be read.
 
+    n_eeg : int
+        Number of EEG electrodes.
+
+    stim_channel : str | int | None
+        The channel name or channel index (starting at 0).
+        If None, it will use the last channel in data.
+
+    hpts : str | None
+        Path to the hpts file containing electrode positions.
+        If None, sensor locations are (0,0,0).
+
+    annot : str | None
+        Path of the annot file containing the triggering information for EDF+.
+        Can be None for BDF only.
+        If None for EDF+, it will raise an error.
+
     Returns
     -------
     edf : dict
         A dict containing all the EDF+,BDF parameter settings.
     """
 
-    edf = dict()
-    edf['file_id'] = fname
+    info = dict()
+    info['file_id'] = fname
+    # Add info for fif object
+    info['meas_id'] = None
+    info['projs'] = []
+    info['comps'] = []
+    info['bads'] = []
+    info['acq_pars'], info['acq_stim'] = None, None
+    info['filename'] = None
+    info['ctf_head_t'] = None
+    info['dev_ctf_t'] = []
+    info['filenames'] = []
+    info['dig'] = None
+    info['dev_head_t'] = None
+
     with open(fname, 'rb') as fid:
         assert(fid.tell() == 0)
         fid.seek(8)
 
-        edf['subject_id'] = fid.read(80).strip()
-        edf['recording_id'] = fid.read(80).strip()
+        _ = fid.read(80).strip()  # subject id
+        _ = fid.read(80).strip()  # recording id
         day, month, year = [int(x) for x in re.findall('(\d+)', fid.read(8))]
         hour, minute, sec = [int(x) for x in re.findall('(\d+)', fid.read(8))]
-        edf['meas_date'] = str(datetime.datetime(year + 2000, month, day,
+        info['meas_date'] = str(datetime.datetime(year + 2000, month, day,
                                                  hour, minute, sec))
-        edf['data_offset'] = header_nbytes = int(fid.read(8))
+        info['data_offset'] = header_nbytes = int(fid.read(8))
         subtype = fid.read(44).strip()[:5]
         supported = ['EDF+C', 'EDF+D', '24BIT']
         if subtype not in supported:
             raise ValueError('Filetype must be either %s, %s, or %s'
                              % tuple(supported))
-        edf['subtype'] = subtype
+        info['subtype'] = subtype
 
         n_records = int(fid.read(8))
         record_length = int(fid.read(8))  # in seconds
-        edf['nchan'] = int(fid.read(4))
+        info['nchan'] = int(fid.read(4))
 
-        channels = range(edf['nchan'])
-        edf['ch_names'] = [fid.read(16).strip() for _ in channels]
-        edf['transducer_type'] = [fid.read(80).strip() for _ in channels]
-        edf['units'] = [fid.read(8).strip() for _ in channels]
-        not_stim_ch = np.where(np.array(edf['units']) != 'Boolean')[0]
-        units = list(np.unique(edf['units']))
+        channels = range(info['nchan'])
+        info['ch_names'] = [fid.read(16).strip() for _ in channels]
+        _ = [fid.read(80).strip() for _ in channels]  # transducer type
+        info['units'] = [fid.read(8).strip() for _ in channels]
+        not_stim_ch = np.where(np.array(info['units']) != 'Boolean')[0]
+        units = list(np.unique(info['units']))
         if 'Boolean' in units:
             units.remove('Boolean')
         assert len(units) == 1
@@ -303,85 +296,124 @@ def _get_edf_info(fname, hpts=None, annot=None):
             scale = 1e-6
         elif units[0] == 'V':
             scale = 1
-        edf['physical_min'] = np.array([float(fid.read(8)) for _ in channels])
-        edf['physical_max'] = np.array([float(fid.read(8)) for _ in channels])
-        edf['digital_min'] = np.array([float(fid.read(8)) for _ in channels])
-        edf['digital_max'] = np.array([float(fid.read(8)) for _ in channels])
-        edf['prefiltering'] = [fid.read(80).strip() for _ in channels]
+        physical_min = np.array([float(fid.read(8)) for _ in channels])
+        physical_max = np.array([float(fid.read(8)) for _ in channels])
+        digital_min = np.array([float(fid.read(8)) for _ in channels])
+        digital_max = np.array([float(fid.read(8)) for _ in channels])
+        prefiltering = [fid.read(80).strip() for _ in channels]
+        prefiltering = [re.findall('HP:\s(\w+);\sLP:\s(\d+)', filt)
+                                   for filt in prefiltering[:-1]]
+        if all(prefiltering):
+            filt = prefiltering[0][0]
+            if filt[0] == 'DC':
+                info['highpass'] = 0
+            else:
+                info['highpass'] = int(filt[0])
+            info['lowpass'] = int(filt[1])
+        else:
+            raise NotImplementedError('Channels contain different filtering.')
         n_samples_per_record = [int(fid.read(8)) for _ in channels]
         assert len(np.unique(n_samples_per_record)) == 1
 
         n_samples_per_record = n_samples_per_record[0]
-        fid.read(32 * edf['nchan'])  # reserved
+        fid.read(32 * info['nchan'])  # reserved
         assert fid.tell() == header_nbytes
-    physical_range = edf['physical_max'] - edf['physical_min']
-    digital_range = edf['digital_max'] - edf['digital_min']
-    edf['gains'] = np.array([physical_range / digital_range]).T
-    edf['gains'][not_stim_ch] *= scale
-    edf['sfreq'] = float(n_samples_per_record / record_length)
-    edf['nsamples'] = n_records * n_samples_per_record
-    if edf['subtype'] == '24BIT':
-        edf['data_size'] = 3  # 24-bit (3 byte) integers
+    physical_range = physical_max - physical_min
+    digital_range = digital_max - digital_min
+    info['gains'] = np.array([physical_range / digital_range]).T
+    info['gains'][not_stim_ch] *= scale
+    info['sfreq'] = int(n_samples_per_record / record_length)
+    info['nsamples'] = n_records * n_samples_per_record
+    if info['subtype'] == '24BIT':
+        info['data_size'] = 3  # 24-bit (3 byte) integers
     else:
-        edf['data_size'] = 2  # 16-bit (2 byte) integers
-    if os.path.lexists(hpts):
-        fid = open(hpts, 'rb').read()
-        temp = re.findall('eeg\s(\w+)\s(-?\d+)\s(-?\d+)\s(-?\d+)', fid)
-        temp = [x.split() for x in temp]
+        info['data_size'] = 2  # 16-bit (2 byte) integers
 
+    if hpts and os.path.lexists(hpts):
+        fid = open(hpts, 'rb').read()
         locs = {}
-        temp = re.findall('cardinal\s(\d+)\s(-?\d+)\s(-?\d+)\s(-?\d+)', fid)
+        temp = re.findall('eeg\s(\w+)\s(-?\d+)\s(-?\d+)\s(-?\d+)', fid)
+        temp = temp + re.findall('cardinal\s(\d+)\s(-?\d+)\s(-?\d+)\s(-?\d+)',
+                                 fid)
         for loc in temp:
-            locs[loc[0]] = tuple(map(int, loc[1:]))
-        edf['dig'] = []
+            coord = np.array(map(int, loc[1:]))
+            coord = apply_trans(als_ras_trans_mm, coord)
+            locs[loc[0].lower()] = coord
+        trans = get_head_coord_trans(nasion=locs['2'], lpa=locs['1'],
+                                     rpa=locs['3'])
+        for loc in locs:
+            locs[loc] = apply_trans(trans, locs[loc])
+        info['dig'] = []
 
         point_dict = {}
         point_dict['coord_frame'] = FIFF.FIFFV_COORD_HEAD
         point_dict['ident'] = FIFF.FIFFV_POINT_NASION
         point_dict['kind'] = FIFF.FIFFV_POINT_CARDINAL
-        point_dict['r'] = locs['2']
-        edf['dig'].append(point_dict)
+        point_dict['r'] = apply_trans(trans, locs['2'])
+        info['dig'].append(point_dict)
 
         point_dict = {}
         point_dict['coord_frame'] = FIFF.FIFFV_COORD_HEAD
         point_dict['ident'] = FIFF.FIFFV_POINT_LPA
         point_dict['kind'] = FIFF.FIFFV_POINT_CARDINAL
-        point_dict['r'] = locs['1']
-        edf['dig'].append(point_dict)
+        point_dict['r'] = apply_trans(trans, locs['1'])
+        info['dig'].append(point_dict)
 
         point_dict = {}
         point_dict['coord_frame'] = FIFF.FIFFV_COORD_HEAD
         point_dict['ident'] = FIFF.FIFFV_POINT_RPA
         point_dict['kind'] = FIFF.FIFFV_POINT_CARDINAL
-        point_dict['r'] = locs['3']
-        edf['dig'].append(point_dict)
+        point_dict['r'] = apply_trans(trans, locs['3'])
+        info['dig'].append(point_dict)
 
     else:
         locs = {}
-    locs = [locs[ch_name] if ch_name in locs.keys() else (0, 0, 0)
-                          for ch_name in edf['ch_names']]
-    edf['sensor_locs'] = np.array(locs, int)
+    locs = [locs[ch_name.lower()] if ch_name.lower() in locs.keys()
+            else (0, 0, 0) for ch_name in info['ch_names']]
+    sensor_locs = np.array(locs)
 
-    if edf['subtype'] != '24BIT':
+    if info['subtype'] != '24BIT':
         if not os.path.lexists(annot):
             raise ValueError('Missing required annotation file.')
 
-    # Add info for fif object
-    edf['meas_id'] = None
-    edf['projs'] = []
-    edf['comps'] = []
-    edf['lowpass'] = None
-    edf['highpass'] = None
-    edf['bads'] = []
-    edf['acq_pars'], edf['acq_stim'] = None, None
-    edf['filename'] = None
-    edf['ctf_head_t'] = None
-    edf['dev_ctf_t'] = []
-    edf['filenames'] = []
-    edf['dig'] = None
-    edf['dev_head_t'] = None
-
-    return edf
+    # Creates a list of dicts of eeg channels for raw.info
+    logger.info('Setting channel info structure...')
+    info['ch_names'] = ch_names = info['ch_names']
+    info['chs'] = []
+    if stim_channel == None:
+        stim_channel = info['nchan'] - 1
+    for idx, ch_info in enumerate(zip(ch_names, sensor_locs), 1):
+        ch_name, ch_loc = ch_info
+        chan_info = {}
+        chan_info['cal'] = 1.
+        chan_info['logno'] = idx
+        chan_info['scanno'] = idx
+        chan_info['range'] = 1
+        chan_info['unit_mul'] = 0
+        chan_info['ch_name'] = ch_name
+        chan_info['unit'] = FIFF.FIFF_UNIT_V
+        chan_info['coord_frame'] = FIFF.FIFFV_COORD_HEAD
+        chan_info['coil_type'] = FIFF.FIFFV_COIL_EEG
+        chan_info['kind'] = FIFF.FIFFV_EEG_CH
+        chan_info['eeg_loc'] = ch_loc
+        chan_info['loc'] = np.zeros(12)
+        chan_info['loc'][:3] = ch_loc
+        check1 = stim_channel == ch_name
+        check2 = stim_channel == idx
+        stim_check = np.logical_or(check1, check2)
+        # this deals with EOG, AUX channels
+        if idx > n_eeg:
+            chan_info['coil_type'] = FIFF.FIFFV_COIL_NONE
+            chan_info['kind'] = FIFF.FIFFV_MISC_CH
+        if stim_check:
+            chan_info['coil_type'] = FIFF.FIFFV_COIL_NONE
+            chan_info['unit'] = FIFF.FIFF_UNIT_NONE
+            chan_info['kind'] = FIFF.FIFFV_STIM_CH
+            chan_info['ch_name'] = 'STI 014'
+            info['ch_names'][idx - 1] = chan_info['ch_name']
+        info['chs'].append(chan_info)
+        info['locs'] = sensor_locs
+    return info
 
 
 def read_raw_edf(input_fname, n_eeg, stim_channel=None, hpts=None, annot=None,
@@ -401,13 +433,13 @@ def read_raw_edf(input_fname, n_eeg, stim_channel=None, hpts=None, annot=None,
         If None, it will use the last channel in data.
 
     hpts : str | None
-        Path to the hpts file.
+        Path to the hpts file containing electrode positions.
         If None, sensor locations are (0,0,0).
 
     annot : str | None
-        Path of the annot file.
+        Path of the annot file containing the triggering information for EDF+.
         Can be None for BDF only.
-        If None for EDF, it will raise an error.
+        If None for EDF+, it will raise an error.
 
     verbose : bool, str, int, or None
         If not None, override default verbose level (see mne.verbose).
