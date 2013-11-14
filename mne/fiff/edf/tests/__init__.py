f9a974de0cbda1d7497ba4cf3da834d19640064d
Teon Brooks <teon.brooks@gmail.com>
2013-11-14 03:57:10 -0500

    RM: content is unnecessary, empty file is sufficient

--- mne/fiff/edf/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-"""EDF+,BDF testing for conversion to FIF"""
-
-# Author: Teon Brooks <teon@nyu.edu>
-#
-# License: BSD (3-clause)
-
-from .test_edf import test_bdf_data, test_edf_data, test_read_segment
