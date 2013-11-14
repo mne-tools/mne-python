21fb11e8c18ee43c04e6ceaa7a829633f169a737
Teon Brooks <teon.brooks@gmail.com>
2013-11-14 00:11:09 -0500

    ADD: testing suite

---
+++ mne/fiff/brainvision/tests/__init__.py
@@ -0,0 +1,7 @@
+"""Brainvision testing for conversion to FIF"""
+
+# Author: Teon Brooks <teon@nyu.edu>
+#
+# License: BSD (3-clause)
+
+from .test_brainvision import test_eeg_data, test_read_segment
--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ if __name__ == "__main__":
                     'mne.fiff.bti', 'mne.fiff.bti.tests',
                     'mne.fiff.kit', 'mne.fiff.kit.tests',
                     'mne.fiff.edf', 'mne.fiff.edf.tests',
+                    'mne.fiff.brainvision', 'mne.fiff.brainvision.tests',                    
                     'mne.forward', 'mne.forward.tests',
                     'mne.layouts', 'mne.layouts.tests',
                     'mne.minimum_norm', 'mne.minimum_norm.tests',
