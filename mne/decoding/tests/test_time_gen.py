e2e80e717dc430a269effaa2b75b8433222401f9
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2013-12-10 11:23:44 +0100

    FIX : catch warning in time gen

--- mne/decoding/tests/test_time_gen.py
+++ mne/decoding/tests/test_time_gen.py
@@ -2,6 +2,7 @@
 #
 # License: BSD (3-clause)
 
+import warnings
 import os.path as op
 
 from nose.tools import assert_true
@@ -16,7 +17,7 @@ data_dir = op.join(op.dirname(__file__), '..', '..', 'fiff', 'tests', 'data')
 raw_fname = op.join(data_dir, 'test_raw.fif')
 event_name = op.join(data_dir, 'test-eve.fif')
 
-tmin, tmax = -0.01, 0.01
+tmin, tmax = -0.2, 0.5
 event_id = dict(aud_l=1, vis_l=3)
 
 
@@ -29,13 +30,15 @@ def test_time_generalization():
     picks = fiff.pick_types(raw.info, meg='mag', stim=False, ecg=False,
                             eog=False, exclude='bads')
     picks = picks[1:13:3]
-    decim = 1
-    epochs = Epochs(raw, events, event_id, tmin, tmax, picks=picks,
-                    baseline=(None, 0), preload=True, decim=decim)
+    decim = 30
 
-    epochs_list = [epochs[k] for k in event_id.keys()]
-    scores = time_generalization(epochs_list, cv=2, random_state=42)
-    n_times = len(epochs.times)
-    assert_true(scores.shape == (n_times, n_times))
-    assert_true(scores.max() <= 1.)
-    assert_true(scores.min() >= 0.)
+    with warnings.catch_warnings(record=True) as w:
+        epochs = Epochs(raw, events, event_id, tmin, tmax, picks=picks,
+                        baseline=(None, 0), preload=True, decim=decim)
+
+        epochs_list = [epochs[k] for k in event_id.keys()]
+        scores = time_generalization(epochs_list, cv=2, random_state=42)
+        n_times = len(epochs.times)
+        assert_true(scores.shape == (n_times, n_times))
+        assert_true(scores.max() <= 1.)
+        assert_true(scores.min() >= 0.)
