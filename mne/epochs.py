9e68121a958e8121d5ebbfd1bc1ad7356e339e4d
Tal Linzen <tal.linzen@gmail.com>
2013-12-17 17:45:06 -0500

    Add trial ID and improve drop_epochs: cosmetics

--- mne/epochs.py
+++ mne/epochs.py
@@ -719,7 +719,8 @@ class Epochs(_BaseEpochs):
         return is_delayed
 
     @verbose
-    def drop_epochs(self, indices, reason='', by_id=False, verbose=None):
+    def drop_epochs(self, indices, reason='user', use_trial_id=False,
+            verbose=None):
         """Drop epochs based on indices or boolean mask
 
         Note that the indices refer to the current set of undropped epochs
@@ -735,10 +736,10 @@ class Epochs(_BaseEpochs):
             Set epochs to remove by specifying indices to remove or a boolean
             mask to apply (where True values get removed). Events are
             correspondingly modified.
-        reason : string
-            Reason for dropping the epochs ('ECG', 'Timeout', 'Blink' etc).
-            Default: ''
-        by_id : bool
+        reason : str
+            Reason for dropping the epochs ('ECG', 'timeout', 'blink' etc).
+            Default: 'user'.
+        use_trial_id : bool
             If False (default) indices refer to the current set of undropped
             trials; if True, indices indicate trial IDs.
         verbose : bool, str, int, or None
@@ -754,9 +755,9 @@ class Epochs(_BaseEpochs):
             raise ValueError("indices must be a scalar or a 1-d array")
     
         if by_id:
-            indices = np.array([x for x, y in 
-                    zip(range(len(self.events)), self.trial_id) if
-                    y in set(indices)])
+            mapping = zip(range(len(self.events)), self.trial_id)
+            indices = set(indices)
+            indices = np.array([x for x, y in mapping if y in indices])
 
         out_of_bounds = (indices < 0) | (indices >= len(self.events))
         if out_of_bounds.any():
@@ -765,7 +766,7 @@ class Epochs(_BaseEpochs):
 
         for ind in indices:
             self.drop_log[self.trial_id[ind]].append(reason)
-        self.trial_id = np.delete(self.trial_id, indices, axis=0)
+        self.trial_id = self.trial_id[indices]
         self.events = np.delete(self.events, indices, axis=0)
         if self.preload:
             self._data = np.delete(self._data, indices, axis=0)
