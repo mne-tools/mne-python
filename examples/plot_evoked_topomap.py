2fdce5f573c5fdde6a2821b60f4b5d1ddb07b9c4
Tal Linzen <tal.linzen@gmail.com>
2013-12-26 21:16:01 -0500

    Fixed typo

--- examples/plot_evoked_topomap.py
+++ examples/plot_evoked_topomap.py
@@ -32,7 +32,8 @@ times = np.arange(0.05, 0.15, 0.01)
 evoked.plot_topomap(times, ch_type='mag')
 
 # add channel labels
-evoked.plot_topomap(0.1, ch_type='mag', show_ids=True, colorbar=False, size=8)
+evoked.plot_topomap(0.1, ch_type='mag', show_names=True, colorbar=False,
+                    size=8)
 
 # plot gradiometer data (plots the RMS for each pair of gradiometers)
 evoked.plot_topomap(times, ch_type='grad')
