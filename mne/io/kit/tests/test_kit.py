6af9e234939685f0fcd2f24bb2d4206bd0784d34
Teon Brooks <teon@nyu.edu>
2014-01-01 22:50:17 -0200

    FIX: add transform to legacy points

--- mne/io/kit/tests/test_kit.py
+++ mne/io/kit/tests/test_kit.py
@@ -25,6 +25,8 @@ mrk2_path = op.join(data_dir, 'test_mrk_pre.sqd')
 mrk3_path = op.join(data_dir, 'test_mrk_post.sqd')
 elp_path = op.join(data_dir, 'test_elp.txt')
 hsp_path = op.join(data_dir, 'test_hsp.txt')
+elp_leg_path = op.join(data_dir, 'test.elp')
+hsp_leg_path = op.join(data_dir, 'test.hsp')
 
 tempdir = _TempDir()
 
@@ -114,3 +116,13 @@ def test_stim_ch():
     stim1, _ = raw[stim_pick]
     stim2 = np.array(raw.read_stim_ch(), ndmin=2)
     assert_array_equal(stim1, stim2)
+
+def test_legacy_head_dev_t():
+    """Test raw kit usage of legacy files against txt files
+    """
+    raw = read_raw_kit(sqd_path, mrk_path, elp_path, hsp_path)
+    raw_leg = read_raw_kit(sqd_path, mrk_path, elp_leg_path, hsp_leg_path)
+
+    trans = raw.info['dev_head_t']['trans']
+    trans_leg = raw_leg.info['dev_head_t']['trans']
+    assert_array_almost_equal(trans, trans_leg, decimal=5)
