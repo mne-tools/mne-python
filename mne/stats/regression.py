d8d8714975b0c8b707b4d33a81007f973812bd72
Tal Linzen <tal.linzen@gmail.com>
2014-01-05 18:53:33 -0500

    Eliminate classes

--- mne/stats/regression.py
+++ mne/stats/regression.py
@@ -8,86 +8,56 @@ from scipy import linalg
 
 from mne.source_estimate import SourceEstimate
 
-class OLSFit(object):
-    def __init__(self, beta, stderr, t, p):
-        self.beta = beta
-        self.stderr = stderr
-        self.t = t
-        self.p = p
+def ols(data, design_matrix, names):
+    n_trials = data.shape[0]
+    n_rows, n_predictors = design_matrix.shape
 
-class OLS(object):
-    
-    def __init__(self, data, design_matrix, names):
-        self.data = data
-        self.design_matrix = design_matrix
-        self.names = names
+    if n_trials != n_rows:
+        raise ValueError('Number of rows in design matrix must be equal '
+                         'to number of observations')
+    if n_predictors != len(names):
+        raise ValueError('Number of predictor names must be equal to '
+                         'number of predictors')
 
-    def fit(self):
-        n_trials = self.data.shape[0]
-        n_rows, n_predictors = self.design_matrix.shape
+    y = np.reshape(data, (n_trials, -1))
+    betas, resid_sum_squares, _, _ = linalg.lstsq(design_matrix, y)
 
-        if n_trials != n_rows:
-            raise ValueError('Number of rows in design matrix must be equal '
-                             'to number of observations')
-        if n_predictors != len(self.names):
-            raise ValueError('Number of predictor names must be equal to '
-                             'number of predictors')
+    df = n_rows - n_predictors
+    sqrt_noise_var = np.sqrt(resid_sum_squares / df)
+    sqrt_noise_var = sqrt_noise_var.reshape(data.shape[1:])
+    design_invcov = linalg.inv(np.dot(design_matrix.T, design_matrix))
+    unscaled_stderrs = np.sqrt(np.diag(design_invcov))
 
-        y = np.reshape(self.data, (n_trials, -1))
-        betas, resid_sum_squares, _, _ = linalg.lstsq(self.design_matrix, y)
+    beta = {}
+    stderr = {}
+    t = {}
+    p = {}
+    for x, unscaled_stderr, pred in zip(betas, unscaled_stderrs, names):
+        beta[pred] = x.reshape(data.shape[1:])
+        stderr[pred] = sqrt_noise_var * unscaled_stderr
+        t[pred] = beta[pred] / stderr[pred]
+        cdf = scipy.stats.t.cdf(np.abs(t[pred]), df)
+        p[pred] = (1 - cdf) * 2
 
-        df = n_rows - n_predictors
-        sqrt_noise_var = np.sqrt(resid_sum_squares / df)
-        sqrt_noise_var = sqrt_noise_var.reshape(self.data.shape[1:])
-        design_invcov = linalg.inv(np.dot(self.design_matrix.T,
-                                          self.design_matrix))
-        unscaled_stderrs = np.sqrt(np.diag(design_invcov))
+    return dict(beta=beta, stderr=stderr, t=t, p=p)
 
-        beta = {}
-        stderr = {}
-        t = {}
-        p = {}
-        for x, unscaled_stderr, pred in zip(betas, unscaled_stderrs, 
-                                            self.names):
-            beta_map = x.reshape(self.data.shape[1:])
-            beta[pred] = self.create_object(beta_map)
+def ols_epochs(epochs, design_matrix, names):
+    evoked = epochs.average()
+    data = epochs.get_data()
+    ols_fit = ols(data, design_matrix, names)
+    for v in ols_fit.values():
+        for k in v.keys():
+            ev = evoked.copy()
+            ev.data = v[k]
+            v[k] = ev
+    return ols_fit
 
-            stderr_map = sqrt_noise_var * unscaled_stderr
-            stderr[pred] = self.create_object(stderr_map)
-
-            t_map = beta_map / stderr_map
-            t[pred] = self.create_object(t_map)
-
-            cdf = scipy.stats.t.cdf(np.abs(t_map), df)
-            p[pred] = self.create_object((1 - cdf) * 2)
-
-        return OLSFit(beta, stderr, t, p)
-
-    def create_object(self, data):
-        return data
-
-class OLSEpochs(OLS):
-
-    def __init__(self, epochs, design_matrix, names):
-        self.epochs = epochs
-        self.evoked = epochs.average()
-        data = epochs.get_data()
-        super(OLSEpochs, self).__init__(data, design_matrix, names)
-
-    def create_object(self, data):
-        ev = self.evoked.copy()
-        ev.data = data
-        return ev
-
-class OLSSourceEstimates(OLS):
-
-    def __init__(self, source_estimates, design_matrix, names):
-        self.source_estimates = source_estimates
-        data = np.array([x.data for x in source_estimates])
-        super(OLSSourceEstimates, self).__init__(data, design_matrix, names)
-
-    def create_object(self, data):
-        s = self.source_estimates[0]
-        stc = SourceEstimate(data, vertices=s.vertno, tmin=s.tmin, 
-                             tstep=s.tstep, subject=s.subject)
-        return stc
+def ols_source_estimates(source_estimates, design_matrix, names):
+    data = np.array([stc.data for stc in source_estimates])
+    ols_fit = ols(data, design_matrix, names)
+    s = source_estimates[0]
+    for v in ols_fit.values():
+        for k in v.keys():
+            v[k] = SourceEstimate(v[k], vertices=s.vertno, tmin=s.tmin, 
+                                  tstep=s.tstep, subject=s.subject)
+    return ols_fit
