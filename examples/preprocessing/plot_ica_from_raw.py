ca428e9b12a64e22d44df9e5dd8c86e222b1f79a
dengemann <d.engemann@fz-juelich.de>
2014-01-05 19:01:56 +0100

    ENH: add Paris examples to ICA tutorial

--- examples/preprocessing/plot_ica_from_raw.py
+++ examples/preprocessing/plot_ica_from_raw.py
@@ -35,6 +35,8 @@ raw_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
 
 raw = Raw(raw_fname, preload=True)
 
+raw.filter(1, 45, n_jobs=2)
+
 picks = mne.fiff.pick_types(raw.info, meg=True, eeg=False, eog=False,
                             stim=False, exclude='bads')
 
@@ -42,18 +44,18 @@ picks = mne.fiff.pick_types(raw.info, meg=True, eeg=False, eog=False,
 # Setup ICA seed decompose data, then access and plot sources.
 
 # Instead of the actual number of components here we pass a float value
-# between 0 and 1 to select n_components by a percentage of
-# explained variance. Also we decide to use 64 PCA components before mixing
-# back to sensor space. These include the PCA components supplied to ICA plus
-# additional PCA components up to rank 64 of the MEG data.
-# This allows to control the trade-off between denoising and preserving signal.
+# between 0 and 1 to select n_components based on the percentage of
+# variance explained by the PCA components.
 
-ica = ICA(n_components=0.90, n_pca_components=None, max_pca_components=100,
+ica = ICA(n_components=0.90, n_pca_components=None, max_pca_components=None,
           random_state=0)
 
-# 1 minute exposure should be sufficient for artifact detection.
-# However, rejection performance may significantly improve when using
-# the entire data range
+# Also we decide to use all PCA components before mixing back to sensor space.
+# You can again use percentages (float) or set the total number of components
+# to be kept directly (int) which allows to control the amount of additional
+# denoising.
+
+ica.n_pca_components = 1.0
 
 # decompose sources for raw data using each third sample.
 ica.decompose_raw(raw, picks=picks, decim=3)
@@ -66,13 +68,23 @@ ica.plot_sources_raw(raw, range(30), start=start_plot, stop=stop_plot)
 ###############################################################################
 # Automatically find the ECG component using correlation with ECG signal.
 
-# First, we create a helper function that iteratively applies the pearson
-# correlation function to sources and returns an array of r values
-# This is to illustrate the way ica.find_sources_raw works. Actually, this is
-# the default score_func.
+# Defining a customized distance function.
+
+# You can pass any function object that
+# takes a n_sources X n_samples vector and, optionally, a second
+# n_samples vector, and returns a score vector of length n_sources.
+# Let's illustrate this by creating a function that, when passed as
+# `score_func` argument, does the same as the default value 'pearsonr'.
 
 from scipy.stats import pearsonr
-corr = lambda x, y: np.array([pearsonr(a, y.ravel()) for a in x])[:, 0]
+
+
+def score_func(x, y):
+    """Return pearson correlation between ICA source
+       and target time series
+    """
+    correlation, pval = np.array([pearsonr(a, y) for a in x]).T
+    return correlation
 
 # As we don't have an ECG channel we use one that correlates a lot with heart
 # beats: 'MEG 1531'. To improve detection, we filter the the channel and pass
@@ -83,7 +95,7 @@ ecg_ch_name = 'MEG 1531'
 l_freq, h_freq = 8, 16
 ecg = raw[[raw.ch_names.index(ecg_ch_name)], :][0]
 ecg = band_pass_filter(ecg, raw.info['sfreq'], l_freq, h_freq)
-ecg_scores = ica.find_sources_raw(raw, target=ecg, score_func=corr)
+ecg_scores = ica.find_sources_raw(raw, target=ecg, score_func=score_func)
 
 # get maximum correlation index for ECG
 ecg_source_idx = np.abs(ecg_scores).argmax()
@@ -94,22 +106,23 @@ ica.plot_sources_raw(raw, ecg_source_idx, title=title, stop=3.0)
 # We can do this by reordering the plot by our scores using order
 # and generating sort indices for the sources:
 
-ecg_order = np.abs(ecg_scores).argsort()[::-1][:30]  # ascending order
+ecg_order = np.abs(ecg_scores).argsort()[::-1]  # ascending order
 
-ica.plot_sources_raw(raw, ecg_order, start=start_plot, stop=stop_plot)
+ica.plot_sources_raw(raw, ecg_order[:15], start=start_plot, stop=stop_plot)
 
-# Let's make our ECG component selection more liberal and include sources
-# for which the variance explanation in terms of \{r^2}\ exceeds 5 percent.
-# we will directly extend the ica.exclude list by the result.
+ica.plot_topomap(ecg_order[:15], colorbar=False)
 
-ica.exclude.extend(np.where(np.abs(ecg_scores) ** 2 > .05)[0])
+ecg_inds = np.abs(ecg_scores).argsort()[-3:]  # take the first 3 components
+
+ica.exclude.extend(ecg_inds)
+
 
 ###############################################################################
 # Automatically find the EOG component using correlation with EOG signal.
 
 # As we have an EOG channel, we can use it to detect the source.
 
-eog_scores = ica.find_sources_raw(raw, target='EOG 061', score_func=corr)
+eog_scores = ica.find_sources_raw(raw, target='EOG 061', score_func=score_func)
 
 # get maximum correlation index for EOG
 eog_source_idx = np.abs(eog_scores).argmax()
@@ -130,61 +143,68 @@ plt.suptitle(title, fontsize=12)
 # We now add the eog artifacts to the ica.exclusion list
 ica.exclude += [eog_source_idx]
 
-# Restore sensor space data
-raw_ica = ica.pick_sources_raw(raw, include=None)
+# Restore sensor space data and keep all PCA components
+raw_ica = ica.pick_sources_raw(raw, include=None, n_pca_components=1.0)
 
+# let's now compare the date before and after cleaning.
 start_compare, stop_compare = raw.time_as_index([100, 106])
-
 data, times = raw[picks, start_compare:stop_compare]
 data_clean, _ = raw_ica[picks, start_compare:stop_compare]
 
+# first the raw data
 plt.figure()
-plt.plot(times, data.T)
+plt.plot(times, data.T, color='r')
+plt.plot(times, data_clean.T, color='k')
 plt.xlabel('time (s)')
 plt.xlim(100, 106)
-plt.ylabel('Raw MEG data (T)')
-y0, y1 = plt.ylim()
-
-plt.figure()
-plt.plot(times, data_clean.T)
-plt.xlabel('time (s)')
-plt.xlim(100, 106)
-plt.ylabel('Denoised MEG data (T)')
-plt.ylim(y0, y1)
 plt.show()
 
-###############################################################################
-# Compare the affected channel before and after ICA cleaning.
-
-affected_idx = raw.ch_names.index(ecg_ch_name)
-
-# plot the component that correlates most with the ECG
+# now the affected channel
+affected_idx = raw.ch_names.index('MEG 1531')
 plt.figure()
-plt.plot(times, data[affected_idx], color='k')
-plt.title('Affected channel MEG 1531 before cleaning.')
-y0, y1 = plt.ylim()
-
-# plot the component that correlates most with the ECG
-plt.figure()
+plt.plot(times, data[affected_idx], color='r')
 plt.plot(times, data_clean[affected_idx], color='k')
-plt.title('Affected channel MEG 1531 after cleaning.')
-plt.ylim(y0, y1)
+plt.xlim(100, 106)
 plt.show()
 
-###############################################################################
-# Export ICA as raw for subsequent processing steps in ICA space.
 
-from mne.layouts import make_grid_layout
+###############################################################################
+# Validation: check ECG components extracted
+
+# Export ICA as Raw object for subsequent processing steps in ICA space.
 
 ica_raw = ica.sources_as_raw(raw, start=100., stop=160., picks=None)
 
-print(ica_raw.ch_names[:5])  # just a few
+from mne.preprocessing import find_ecg_events
 
-ica_lout = make_grid_layout(ica_raw.info)
+# find ECG events
+event_id = 999
+events, _, _ = find_ecg_events(raw, ch_name='MEG 1531', event_id=event_id,
+                               l_freq=8, h_freq=16)
 
-# Uncomment the following two lines to save sources and layout.
-# ica_raw.save('ica_raw.fif')
-# ica_lout.save(os.path.join(os.environ['HOME'], '.mne/lout/ica.lout'))
+# pick components, create epochs and evoked in ICA space
+ica_picks = np.arange(ica.n_components_)
+
+ica_raw.info['bads'] = []  # selected components are exported as bad channels
+
+# create epochs around ECG events
+ecg_epochs = mne.Epochs(ica_raw, events=events, event_id=event_id,
+                        tmin=-0.5, tmax=0.5, baseline=None, proj=False,
+                        picks=ica_picks)
+
+ica_ave = ecg_epochs.average(ica_picks)
+
+plt.figure()
+times = ica_ave.times * 1e3
+
+plt.plot(times, ica_ave.data.T, 'k')  # plot unclassified sources
+for ii in ecg_inds:  # use indexing to expose ECG related sources
+    color, label = ('r', 'ICA %02d' % ii)
+    plt.plot(times, ica_ave.data[ii], color=color, label=label)
+
+plt.xlim(times[[0, -1]])
+plt.legend()
+plt.show()
 
 ###############################################################################
 # To save an ICA session you can say:
