fbf0bd14ff2bb142c28ee5aa165c5c65d5d91b5e
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2014-01-06 11:51:40 +0100

    fix test

--- mne/forward/tests/test_field_interpolation.py
+++ mne/forward/tests/test_field_interpolation.py
@@ -5,8 +5,8 @@ from numpy.testing.utils import assert_allclose, assert_array_equal
 from nose.tools import assert_raises
 
 from mne import read_trans
-from mne.forward import (make_surface_mapping, get_meg_helmet_surf,
-                         get_head_surface)
+from mne.forward import make_surface_mapping
+from mne.surface import get_meg_helmet_surf, get_head_surface
 from mne.datasets import sample
 from mne.forward._lead_dots import (_comp_sum_eeg, _comp_sums_meg,
                                     _get_legen_table,
