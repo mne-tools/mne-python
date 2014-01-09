91cfddc113322f03a1ab9e7ec3ad90872fadc5e6
dengemann <d.engemann@fz-juelich.de>
2014-01-09 16:57:39 +0100

    revert undesired changes

--- examples/connectivity/plot_sensor_connectivity.py
+++ examples/connectivity/plot_sensor_connectivity.py
@@ -36,13 +36,13 @@ events = mne.read_events(event_fname)
 raw.info['bads'] += ['MEG 2443']
 
 # Pick MEG gradiometers
-picks = fiff.pick_types(raw.info, meg='mag', eeg=False, stim=False, eog=True,
+picks = fiff.pick_types(raw.info, meg='grad', eeg=False, stim=False, eog=True,
                         exclude='bads')
 
 # Create epochs for the visual condition
 event_id, tmin, tmax = 3, -0.2, 0.5
 epochs = mne.Epochs(raw, events, event_id, tmin, tmax, picks=picks,
-                    baseline=(None, 0), reject=dict(mag=4e-12, eog=150e-6))
+                    baseline=(None, 0), reject=dict(grad=4000e-13, eog=150e-6))
 
 # Compute connectivity for band containing the evoked response.
 # We exclude the baseline period
