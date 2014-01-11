b434393f986dc5b13857e793fdc55c1e236bfff5
dengemann <d.engemann@fz-juelich.de>
2014-01-11 19:18:30 +0100

    fixes 6

--- mne/gui/tests/test_coreg_gui.py
+++ mne/gui/tests/test_coreg_gui.py
@@ -8,7 +8,7 @@ import os
 import numpy as np
 from numpy.testing import assert_allclose
 from nose.tools import (assert_equal, assert_almost_equal, assert_false,
-                        assert_less, assert_raise, assert_true)
+                        assert_less, assert_raises, assert_true)
 
 import mne
 from mne.datasets import sample
