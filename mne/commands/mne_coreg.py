7019a211ae083d1b99d1c3ab580e6b8c0357b4f9
Christian Brodbeck <christianmbrodbeck@gmail.com>
2014-03-26 11:37:58 -0400

    FIX coreg bin: add parser

--- mne/commands/mne_coreg.py
+++ mne/commands/mne_coreg.py
@@ -14,6 +14,11 @@ import mne
 
 
 if __name__ == '__main__':
+    from mne.commands.utils import get_optparser
+
+    parser = get_optparser(__file__)
+    options, args = parser.parse_args()
+
     os.environ['ETS_TOOLKIT'] = 'qt4'
     mne.gui.coregistration()
     sys.exit(0)
