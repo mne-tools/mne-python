3f54a7ae24b7ec6f2453b56fe8be66043bfc2ed5
dengemann <d.engemann@fz-juelich.de>
2014-04-13 14:40:02 +0200

    ENH: attempt to fix py3k

--- mne/fiff/egi/egi.py
+++ mne/fiff/egi/egi.py
@@ -64,7 +64,7 @@ def _read_header(fid):
                          category_lengths=[],
                          pre_baseline=0))
         for event in range(info['n_events']):
-            info['event_codes'].append(''.join(np.fromfile(fid, '>c2', 4)))
+            info['event_codes'].append(np.fromfile(fid, 'S1', 4).tostring())
         info['event_codes'] = np.array(info['event_codes'])
     else:
         raise NotImplementedError('Only continous files are supported')
