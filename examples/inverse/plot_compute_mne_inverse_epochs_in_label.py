2e1e03cf4ff055caa79f096ec5fcd7b09c9e5a9e
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2014-04-24 12:09:15 -0400

    Adding actual single-trial inverse plots to example

--- examples/inverse/plot_compute_mne_inverse_epochs_in_label.py
+++ examples/inverse/plot_compute_mne_inverse_epochs_in_label.py
@@ -20,7 +20,7 @@ import mne
 from mne.datasets import sample
 from mne.fiff import Raw, pick_types
 from mne.minimum_norm import apply_inverse_epochs, read_inverse_operator
-
+from mne.minimum_norm import apply_inverse
 
 data_path = sample.data_path()
 fname_inv = data_path + '/MEG/sample/sample_audvis-meg-oct-6-meg-inv.fif'
@@ -30,8 +30,11 @@ label_name = 'Aud-lh'
 fname_label = data_path + '/MEG/sample/labels/%s.label' % label_name
 
 event_id, tmin, tmax = 1, -0.2, 0.5
-snr = 1.0  # use smaller SNR for raw data
+
+# Using the same inverse operator when inspecting single trials Vs. evoked
+snr = 3.0 # Standard assumption for average data but using it for single trial
 lambda2 = 1.0 / snr ** 2
+
 method = "dSPM"  # use dSPM method (could also be MNE or sLORETA)
 
 # Load data
@@ -54,9 +57,19 @@ epochs = mne.Epochs(raw, events, event_id, tmin, tmax, picks=picks,
                     baseline=(None, 0), reject=dict(mag=4e-12, grad=4000e-13,
                                                     eog=150e-6))
 
+# Get evoked data (averaging across trials in sensor space)
+evoked = epochs.average()
+
 # Compute inverse solution and stcs for each epoch
+# Use the same inverse operator as with evoked data (i.e., set nave)
+# If you use a different nave, dSPM just scales by a factor sqrt(nave)
 stcs = apply_inverse_epochs(epochs, inverse_operator, lambda2, method, label,
-                            pick_ori="normal")
+                            pick_ori="normal", nave = evoked.nave)
+
+stc_evoked = apply_inverse(evoked, inverse_operator, lambda2, method,
+                           pick_ori = "normal")
+
+stc_evoked_label = stc_evoked.in_label(label)
 
 mean_stc = sum(stcs) / len(stcs)
 
@@ -66,14 +79,41 @@ flip = mne.label_sign_flip(label, inverse_operator['src'])
 label_mean = np.mean(mean_stc.data, axis=0)
 label_mean_flip = np.mean(flip[:, np.newaxis] * mean_stc.data, axis=0)
 
+# Get inverse solution by inverting evoked data
+stc_evoked = apply_inverse(evoked, inverse_operator, lambda2, method,
+                           pick_ori = "normal")
+
+stc_evoked_label = stc_evoked.in_label(label)
+label_mean_evoked = np.mean(stc_evoked_label.data, axis = 0)
+
 ###############################################################################
-# View activation time-series
+# View activation time-series to illustrate the benefit of aligning/flipping
 plt.figure()
 h0 = plt.plot(1e3 * stcs[0].times, mean_stc.data.T, 'k')
 h1, = plt.plot(1e3 * stcs[0].times, label_mean, 'r', linewidth=3)
 h2, = plt.plot(1e3 * stcs[0].times, label_mean_flip, 'g', linewidth=3)
 plt.legend((h0[0], h1, h2), ('all dipoles in label', 'mean',
                              'mean with sign flip'))
+plt.xlabel('time (ms)')
+plt.ylabel('dSPM value')
+plt.show()
+
+###############################################################################
+# Viewing single trial dSPM and average dSPM for unflipped pooling over label
+# Compare to (1) Inverse (dSPM) then average, (2) Evoked then dSPM
+
+# Single trial
+plt.figure()
+for k, stc_trial in enumerate(stcs):
+    plt.plot(1e3*stcs[k].times, np.mean(stcs[k].data, axis = 0).T,'k--')
+    plt.hold(True)
+ 
+# Single trial inverse then average.. making linewidth large to not be masked
+plt.plot(1e3 * stcs[0].times, label_mean,'r',linewidth = 5)
+plt.hold(True)
+ 
+# Evoked and then inverse
+plt.plot(1e3 * stcs[0].times, label_mean_evoked,'b',linewidth = 3)
 plt.xlabel('time (ms)')
 plt.ylabel('dSPM value')
 plt.show()
