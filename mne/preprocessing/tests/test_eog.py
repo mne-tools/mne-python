5eb3e126d3e886448eb5b324590d7fc8f4ac22ba
Mainak Jas <mainakjas@gmail.com>
2014-05-29 15:54:32 +0300

    FIX: updating new filenames test_proj -> test-proj

--- mne/preprocessing/tests/test_eog.py
+++ mne/preprocessing/tests/test_eog.py
@@ -7,7 +7,7 @@ from mne.preprocessing.eog import find_eog_events
 data_path = op.join(op.dirname(__file__), '..', '..', 'io', 'tests', 'data')
 raw_fname = op.join(data_path, 'test_raw.fif')
 event_fname = op.join(data_path, 'test-eve.fif')
-proj_fname = op.join(data_path, 'test_proj.fif')
+proj_fname = op.join(data_path, 'test-proj.fif')
 
 
 def test_find_eog():
