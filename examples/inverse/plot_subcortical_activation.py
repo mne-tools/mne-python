614ea73b4d3e181209dc1b1a2c944262375e6a56
leggitta <leggitta3@gmail.com>
2014-05-29 11:29:45 -0700

    merged cortical and subortical source spaces in example. had to comment out ValueErrors in source_estimate.py

--- examples/inverse/plot_subcortical_activation.py
+++ examples/inverse/plot_subcortical_activation.py
@@ -20,54 +20,88 @@ from mne.datasets import spm_face
 from mne.minimum_norm import make_inverse_operator, apply_inverse_epochs
 import matplotlib.pyplot as plt
 from scipy import stats
+import os
+
+# supress text output
+mne.set_log_level(False)
 
 # get the data paths
 data_path = spm_face.data_path()
 subjects_dir = data_path + '/subjects'
-bem = subjects_dir + '/spm/bem/spm-5120-5120-5120-bem-sol.fif'
-mri = data_path + '/MEG/spm/SPM_CTF_MEG_example_faces1_3D_raw-trans.fif'
+subject_dir = subjects_dir + '/spm'
+meg_dir = data_path + '/MEG/spm'
 
-# Load and filter data, set up epochs
-raw_fname = data_path + '/MEG/spm/SPM_CTF_MEG_example_faces%d_3D_raw.fif'
+# get the data files
+bem_fname = subject_dir + '/bem/spm-5120-5120-5120-bem-sol.fif'
+mri_fname = meg_dir + '/SPM_CTF_MEG_example_faces1_3D_raw-trans.fif'
+epo_fname = meg_dir + '/SPM_CTF_MEG_example_faces1_3D_epo.fif'
 
-raw = fiff.Raw(raw_fname % 1, preload=True) # Take first run
+# load the epoch data if it already exists
+if os.path.exists(epo_fname):
 
-picks = mne.fiff.pick_types(raw.info, meg=True, exclude='bads')
-raw.filter(1, 45, method='iir')
+    # read the epoch data
+    epochs = mne.read_epochs(epo_fname)
 
-events = mne.find_events(raw, stim_channel='UPPT001')
-event_ids = {"faces":1, "scrambled":2}
+else:  # must generate epochs from raw
+    
+    # Load and filter data, set up epochs
+    raw_fname = meg_dir + '/SPM_CTF_MEG_example_faces1_3D_raw.fif'
 
-tmin, tmax = -0.2, 0.6
-baseline = None  # no baseline as high-pass is applied
-reject = dict(mag=1.5e-12)
+    raw = fiff.Raw(raw_fname, preload=True) # Take first run
 
-epochs = mne.Epochs(raw, events, event_ids, tmin, tmax,  picks=picks,
-                    baseline=baseline, preload=True, reject=reject)
+    # use the meg channels
+    picks = mne.fiff.pick_types(raw.info, meg=True, exclude='bads')
+    
+    # bandpass filter
+    raw.filter(1, 45, method='iir')
 
-# Fit ICA, find and remove major artifacts
-ica = ICA(None, 50).decompose_epochs(epochs, decim=2)
+    # find the events
+    events = mne.find_events(raw, stim_channel='UPPT001')
+    event_ids = {"faces":1, "scrambled":2}
 
-for ch_name in ['MRT51-2908', 'MLF14-2908']:  # ECG, EOG contaminated chs
-    scores = ica.find_sources_epochs(epochs, ch_name, 'pearsonr')
-    ica.exclude += list(np.argsort(np.abs(scores))[-2:])
+    # setup epoch parameters
+    tmin, tmax = -0.2, 0.6
+    baseline = None  # no baseline as high-pass is applied
+    reject = dict(mag=1.5e-12)
 
-# select ICA sources and reconstruct MEG signals, compute clean ERFs
-epochs = ica.pick_sources_epochs(epochs)
+    # epoch the data
+    epochs = mne.Epochs(raw, events, event_ids, tmin, tmax,  picks=picks,
+                        baseline=baseline, preload=True, reject=reject)
+
+    # Fit ICA, find and remove major artifacts
+    ica = ICA(None, 50).decompose_epochs(epochs, decim=2)
+
+    # exclude sources that resemble ECG or EOG data
+    for ch_name in ['MRT51-2908', 'MLF14-2908']:  # ECG, EOG contaminated chs
+        scores = ica.find_sources_epochs(epochs, ch_name, 'pearsonr')
+        ica.exclude += list(np.argsort(np.abs(scores))[-2:])
+
+    # select ICA sources and reconstruct MEG signals, compute clean ERFs
+    epochs = ica.pick_sources_epochs(epochs)
+    
+    # save the epoched data
+    epochs.save(epo_fname)
+
+# estimate noise covarariance
+noise_cov = mne.compute_covariance(epochs.crop(None, 0, copy=True))
 
 # get positions and orientations of subcortical space
 pos = mne.source_space.get_segment_positions('spm', 'Right-Amygdala',
                                              random_ori=True,
                                              subjects_dir=subjects_dir)
 
-# estimate noise covarariance
-noise_cov = mne.compute_covariance(epochs.crop(None, 0, copy=True))
+# setup the right amygdala volume source space
+vol_src = mne.setup_volume_source_space('spm', pos=pos)
 
-# setup the source space
-src = mne.setup_volume_source_space('spm', pos=pos)
+# setup the cortical surface source space
+src = mne.setup_source_space('spm', overwrite=True)
+
+# combine the source spaces
+src.append(vol_src[0])
 
 # setup the forward model
-forward = mne.make_forward_solution(epochs.info, mri=mri, src=src, bem=bem)
+forward = mne.make_forward_solution(epochs.info, mri=mri_fname, src=src,
+                                    bem=bem_fname)
 forward = mne.convert_forward_solution(forward, surf_ori=True)
 
 # Compute inverse solution
@@ -75,6 +109,10 @@ snr = 5.0
 lambda2 = 1.0 / snr ** 2
 method = 'dSPM'
 
+# estimate noise covarariance
+noise_cov = mne.compute_covariance(epochs.crop(None, 0, copy=True))
+
+# make the inverse operator
 inverse_operator = make_inverse_operator(epochs.info, forward, noise_cov,
                                          loose=0.2, depth=0.8)
 
@@ -83,15 +121,20 @@ stc_faces = apply_inverse_epochs(epochs['faces'], inverse_operator, lambda2,
                                  method)
 stc_scrambled = apply_inverse_epochs(epochs['scrambled'], inverse_operator,
                                      lambda2, method)
-
+                                     
 # compare face vs. scrambled trials
+
+# empty array
 X = np.zeros((len(epochs.events), len(epochs.times)))
 
+# loop through the epochs
 for i, s in enumerate(stc_faces):
-    x = s.data.mean(0)
+    # extract the right amygdala vertices
+    x = s.data[8196:].mean(0)
     X[i] = x
 for i, s in enumerate(stc_scrambled):
-    x = s.data.mean(0)
+    # extract the right amygdala vertices
+    x = s.data[8196:].mean(0)
     X[i+83] = x
 
 t, p = stats.ttest_ind(X[:83], X[83:])
