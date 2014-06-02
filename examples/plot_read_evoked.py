abe2793cc41e23942c19a535c32e761463c8cdf0
kingjr <jeanremi.kibng+github@gmail.com>
2014-06-02 14:52:50 +0200

    EHN: add 2D image for evoked plots

--- examples/plot_read_evoked.py
+++ examples/plot_read_evoked.py
@@ -24,6 +24,10 @@ evoked = read_evokeds(fname, condition=condition, baseline=(None, 0),
                       proj=True)
 
 ###############################################################################
-# Show result:
+# Show result as a butteryfly plot:
 # By using exclude=[] bad channels are not excluded and are shown in red
 plot_evoked(evoked, exclude=[])
+
+# Show result as a 2D image (x: time, y: channels, color: amplitude)
+# In this mode, exclusion does not work
+plot_evoked(evoked, exclude=[], plot_type='image')
