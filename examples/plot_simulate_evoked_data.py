9f97d7469a1cfce6b730300521457c47ebcf2016
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2014-06-04 14:50:16 +0200

    FIX : get rid of plot_evoked examples (use evoked.plot)

--- examples/plot_simulate_evoked_data.py
+++ examples/plot_simulate_evoked_data.py
@@ -20,7 +20,7 @@ from mne import (read_proj, read_forward_solution, read_cov, read_label,
 from mne.io import Raw
 from mne.datasets import sample
 from mne.time_frequency import iir_filter_raw, morlet
-from mne.viz import plot_evoked, plot_sparse_source_estimates
+from mne.viz import plot_sparse_source_estimates
 from mne.simulation import generate_sparse_stc, generate_evoked
 
 ###############################################################################
@@ -86,4 +86,4 @@ plot_sparse_source_estimates(fwd['src'], stc, bgcolor=(1, 1, 1),
 plt.figure()
 plt.psd(evoked.data[0])
 
-plot_evoked(evoked)
+evoked.plot()
