f875671a12a48b3cf28af0f6bccfc7b865efc2b6
maedoc <maedoc@mm.st>
2014-06-12 19:56:07 +0200

    add loc fdr: clean up code & doc

--- mne/stats/multi_comp.py
+++ mne/stats/multi_comp.py
@@ -6,8 +6,9 @@
 # License: BSD (3-clause)
 
 import numpy as np
-
 from scipy import stats, optimize
+from ..fixes import partial
+
 
 def _ecdf(x):
     '''no frills empirical cdf used in fdrcorrection
@@ -103,31 +104,29 @@ def bonferroni_correction(pval, alpha=0.05):
     return reject, pval_corrected
 
 
-def local_fdr(data, nbins=100, h0_maxiter=500, decimate=1):
-    """
-    Implements the local false discovery rate correction, due to Efron 2005, 
-    for univariate data.
+def _local_fdr_h0_err(f, xb, par):
+    "Error function for H0 subdensity used in local FDR optimization"
+    mu, sigma, alo, ahi = par
+    sl = slice(np.argmin(np.abs(xb - alo)), np.argmin(np.abs(xb - ahi)))
+    if sl.start == sl.stop:
+        raise ValueError('insufficient n_bins, restart with higher value')
+    f0 = stats.norm.pdf(xb[sl], loc=mu, scale=sigma)
+    f1 = f[sl]
+    f0 = f0 / f0.max() * f1.max()
+    return np.sum((f0 - f1) ** 2) / np.sum(f1 ** 2) - f1.sum()
 
-    Where FDR performs a correction based on the CDF, lFDR performs a
-    correction based on the PDF, which works by computing density statistics on
-    `x`, where a "null" hypothesis distribution H0, here a Gaussian
-    distribution, is fit to the center of the data, and this provides an
-    estimation of how likely each bin comes from the Gaussian distribution, a
-    measure called the local false discovery rate.
 
-    The use of a Gaussian in this implementation is not required, in fact, the
-    distribution to be fit does not even have to have an analytic PDF, rather
-    it is simply necessary to be able to compute a numerical density for the
-    H0.
+def _local_fdr(data, n_bins=100, h0_maxiter=500, decimate=1):
+    """Local false discovery rate correction for univariate data.
 
-    This function performs the density and FDR estimation, while
-    `local_fdr_correction` provides an interface similar to `fdr_correction`.
-
+    Where FDR corrects multiple comparisons based on the CDF, lFDR corrects
+    based on the PDF, and provides a per-sample estimate of the false discovery
+    rate, assuming a Gaussian null hypothesis.
 
     Parameters
     ----------
-    nbins : int
-        Number of points at which to evaluate the density.
+    n_bins : int
+        Number of points at which to evaluate the density
     h0_maxtier : int
         Max no of iteratios for H0 fit optimization
     decimate : int
@@ -142,7 +141,7 @@ def local_fdr(data, nbins=100, h0_maxiter=500, decimate=1):
     cf : array
         Estimated "center density" of H0
     fdr : array
-        Estimated false discovery rate 
+        Estimated false discovery rate
     mu : float
         Mean of H0 distribution
     sig : float
@@ -151,55 +150,53 @@ def local_fdr(data, nbins=100, h0_maxiter=500, decimate=1):
     Notes
     -----
 
-    Reference:
-    Efron B, Local false discovery rates. 2005
+    The use of a Gaussian in this implementation is not required, in fact, the
+    distribution to be fit does not even have to have an analytic PDF, rather
+    it is simply necessary to be able to compute a numerical density for the
+    H0.
 
+    Reference:
+    Efron B, Tibshirani R.
+    Empirical Bayes methods and false discovery rates for microarrays.
+    Genetic epidemiology 2002; 23(1): 70-86.
     """
 
     x = data.reshape(-1)[::decimate]
 
     # initial estimate of density
     k = stats.gaussian_kde(x)
-    xb = np.r_[x.min() : x.max() : 1j*nbins]
-    f = k(xb)
-    f /= f.sum()
-    
-    # adapt estimation points' density to actual density
-    F = np.cumsum(f)
-    Fb = np.r_[0.0:1.0:1j*nbins]
-    dxb = np.interp(Fb, F, xb + (xb[1] - xb[0])/2.0)
-    xb = np.unique(np.r_[xb, dxb])
-    xb.sort()                
+    xb = np.r_[x.min():x.max():1j * n_bins]
     f = k(xb)
     f /= f.sum()
 
-    # estimate center sub-density
-    def err(par):
-        mu, sigma, alo, ahi = par
-        sl = slice(np.argmin(np.abs(xb - alo)), np.argmin(np.abs(xb - ahi)))
-        if sl.start == sl.stop:
-            raise ValueError('insufficient nbins, restart with higher value')
-        f0 = stats.norm.pdf(xb[sl], loc=mu, scale=sigma)
-        f1 = f[sl]
-        f0 = f0/f0.max()*f1.max()
-        return np.sum((f0 - f1)**2)/np.sum(f1**2) - f1.sum()
-    
-    # initialize opt with guess
+    # adapt estimation points' density to actual density
+    F = np.cumsum(f)
+    Fb = np.r_[0.0:1.0:1j * n_bins]
+    dxb = np.interp(Fb, F, xb + (xb[1] - xb[0]) / 2.0)
+    xb = np.unique(np.r_[xb, dxb])
+    xb.sort()
+    f = k(xb)
+    f /= f.sum()
+
+    # initial search params
     mu0, sig0 = x.mean(), x.std()
-    par0 = mu0, sig0, mu0-sig0, mu0+sig0
-    mu, sig, _, _ = optimize.fmin(err, par0, disp=0, maxiter=h0_maxiter)
+    par0 = mu0, sig0, mu0 - sig0, mu0 + sig0
+
+    # optimize error wrt density
+    err_func = partial(_local_fdr_h0_err, f, xb)
+    mu, sig, _, _ = optimize.fmin(err_func, par0, disp=0, maxiter=h0_maxiter)
 
     # compute center density
     cf = stats.norm.pdf(xb, loc=mu, scale=sig)
-    cf = cf/cf.max()*f.max()
+    cf = cf / cf.max() * f.max()
 
-    # lfdr 
-    fdr = np.clip(cf/f, 0.0, 1.0)
+    # lfdr
+    fdr = np.clip(cf / f, 0.0, 1.0)
 
     return xb, f, cf, fdr, mu, sig
 
 
-def local_fdr_correction(data, q=0.2, **lfdr_kwds):
+def local_fdr_correction(data, q=0.2, n_bins=100, h0_maxiter=500, decimate=1):
     """
     Correction for multiple comparison based on local FDR.
 
@@ -212,27 +209,16 @@ def local_fdr_correction(data, q=0.2, **lfdr_kwds):
 
     data : array_like
         set of observations to test
-    q : float
-        acceptable false discovery rate below which H0 rejected
 
-    **lfdr_kwds : 
-        keywords to pass to the local_fdr routine
-  
     Returns
     -------
 
-    reject : array, bool
-        True if H0 rejected else False
     qvals : array
         Values of FDR for each element of data
 
     """
 
-    xb, _, _, fdr, _, _ = local_fdr(data, **lfdr_kwds)
+    xb, _, _, fdr, _, _ = _local_fdr(data, n_bins=n_bins,
+                                     h0_maxiter=h0_maxiter, decimate=decimate)
     qvals = np.interp(data, xb, fdr, 0.0, 0.0)
     return qvals <= q, qvals
-    
-
-
-
-
