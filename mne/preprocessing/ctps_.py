c857dfe3fdb050a0c0ee2d94bf9ba4466df5f2a1
dengemann <d.engemann@fz-juelich.de>
2014-06-16 15:59:54 +0200

    cleanup maths

--- mne/preprocessing/ctps_.py
+++ mne/preprocessing/ctps_.py
@@ -2,6 +2,7 @@
 #          Denis Engemann <denis.engemann@gmail.com>
 #
 # License: Simplified BSD
+import math
 
 import numpy as np
 from scipy.signal import hilbert
@@ -23,7 +24,7 @@ def _compute_normalized_phase(data):
     return (np.angle(hilbert(data)) + np.pi) / (2 * np.pi)
 
 
-def ctps(data, assume_raw=True):
+def ctps(data, is_raw=True):
     """Compute cross-trial-phase-statistics [1]
 
     Note. It is assumed that the sources are already
@@ -33,7 +34,7 @@ def ctps(data, assume_raw=True):
     ----------
     data: ndarray, shape (n_epochs, n_channels, n_times)
         Any kind of data of dimensions trials, traces, features.
-    assume_raw : bool
+    is_raw : bool
         If True it is assumed that data haven't been transformed to Hilbert
         space and phase angles haven't been normalized. Defaults to True.
 
@@ -45,7 +46,7 @@ def ctps(data, assume_raw=True):
         The normalized kuiper index for ICA sources and
         time slices.
     phase_angles : ndarray, shape (n_epochs, n_sources, n_times) | None
-        The phase values for epochs, sources and time slices. If ``assume_raw``
+        The phase values for epochs, sources and time slices. If ``is_raw``
         is False, None is returned.
 
     References
@@ -59,7 +60,7 @@ def ctps(data, assume_raw=True):
     if not data.ndim == 3:
         ValueError('Data must have 3 dimensions, not %i.' % data.ndim)
 
-    if assume_raw:
+    if is_raw:
         phase_angles = _compute_normalized_phase(data)
     else:
         phase_angles = data  # phase angles can be computed externally
@@ -74,7 +75,7 @@ def ctps(data, assume_raw=True):
         pk_dynamics[ii, :] = pk
         ks_dynamics[ii, :] = ks
 
-    return ks_dynamics, pk_dynamics, phase_angles if assume_raw else None
+    return ks_dynamics, pk_dynamics, phase_angles if is_raw else None
 
 
 def kuiper(data, dtype=np.float64):
@@ -96,14 +97,14 @@ def kuiper(data, dtype=np.float64):
     """
     # if data not numpy array, implicitly convert and make to use copied data
     # ! sort data array along first axis !
-    data = np.sort(data, axis=0).copy().astype(dtype)
+    data = np.sort(data, axis=0).astype(dtype)
     shape = data.shape
     n_dim = len(shape)
     n_trials = shape[0]
 
     # create uniform cdf
     j1 = (np.arange(n_trials, dtype=dtype) + 1.) / float(n_trials)
-    j2 = (np.arange(n_trials, dtype=dtype)) / n_trials
+    j2 = np.arange(n_trials, dtype=dtype) / float(n_trials)
     if n_dim > 1:  # single phase vector (n_trials)
         j1 = j1[:, np.newaxis]
         j2 = j2[:, np.newaxis]
@@ -111,7 +112,7 @@ def kuiper(data, dtype=np.float64):
     d2 = (data - j2).max(axis=0)
     n_eff = n_trials
 
-    d = d1 + d2  # Kuiper's statistic  [n_time_slices]
+    d = d1 + d2  # Kuiper's statistic [n_time_slices]
 
     return d, _prob_kuiper(d, n_eff, dtype=dtype)
 
@@ -140,32 +141,29 @@ def _prob_kuiper(d, n_eff, dtype='f8'):
 
     [2] Kuiper NH 1962. Proceedings of the Koninklijke Nederlands Akademie
     van Wetenschappen, ser Vol 63 pp 38-47
-
-    [3] Press W et al 1995. Nurmerical Receipes in C. The Art of Scientific
-    Computing 2nd Ed. Cambridge University Press,pp 626-628
     """
     n_time_slices = np.size(d)  # single value or vector
     n_points = 100
 
-    en = np.sqrt(np.double(n_eff))
-    k_lambda = (en + 0.155 + 0.24 / en) * d  # see [1], [3]
+    en = math.sqrt(n_eff)
+    k_lambda = (en + 0.155 + 0.24 / en) * d  # see [1]
     l2 = k_lambda ** 2.0
     j2 = (np.arange(n_points) + 1) ** 2
     j2 = j2.repeat(n_time_slices).reshape(n_points, n_time_slices)
-    fact = np.double(4.) * j2 * l2 - np.double(1)
-    expo = np.exp(-2 * j2 * l2)
+    fact = 4. * j2 * l2 - 1.
+    expo = np.exp(-2. * j2 * l2)
     term = 2. * fact * expo
     pk = term.sum(axis=0, dtype=dtype)
 
     # Normalized pK to range [0,1]
     pk_norm = np.zeros(n_time_slices)  # init pk_norm
-    pk_norm[pk > 0] = -np.log(pk[pk > 0]) / (2 * n_eff)
+    pk_norm[pk > 0] = -np.log(pk[pk > 0]) / (2. * n_eff)
     pk_norm[pk <= 0] = 1
 
     # check for no difference to uniform cdf
-    pk_norm = np.where((k_lambda < 0.4), 0.0, pk_norm)
+    pk_norm = np.where(k_lambda < 0.4, 0.0, pk_norm)
 
     # check for round off errors
-    pk_norm = np.where((pk_norm > 1.0), 1.0, pk_norm)
+    pk_norm = np.where(pk_norm > 1.0, 1.0, pk_norm)
 
     return pk_norm
