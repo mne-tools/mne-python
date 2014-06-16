1959624f33389457b6591d8d2dcd2d74d4fe7163
Martin Billinger <martin.billinger@tugraz.at>
2014-06-16 11:27:33 +0200

    moved plot_connectivity_matrix below plot_connectivity_circle

--- mne/viz.py
+++ mne/viz.py
@@ -2472,163 +2472,6 @@ def _plot_connectivity_circle_onpick(event, fig=None, axes=None, indices=None,
         fig.canvas.draw()
 
 
-def plot_connectivity_matrix(con, node_names, indices=None,
-                             node_colors=None, facecolor='black',
-                             textcolor='white',
-                             colormap='hot', vmin=None, vmax=None,
-                             colorbar=True, title=None, colorbar_size=0.2,
-                             colorbar_pos=(-0.3, 0.1), fontsize_title=12,
-                             fontsize_names=8, fontsize_colorbar=8,
-                             fig=None, subplot=111):
-    """Visualize connectivity as a matrix.
-
-    Parameters
-    ----------
-    con : array
-        Connectivity scores. Can be a square matrix, or a 1D array. If a 1D
-        array is provided, "indices" has to be used to define the connection
-        indices.
-    node_names : list of str
-        Node names. The order corresponds to the order in con.
-    indices : tuple of arrays | None
-        Two arrays with indices of connections for which the connections
-        strenghts are defined in con. Only needed if con is a 1D array.
-    node_colors : list of tuples | list of str
-        List with the color to use for each node. If fewer colors than nodes
-        are provided, the colors will be repeated. Any color supported by
-        matplotlib can be used, e.g., RGBA tuples, named colors.
-    facecolor : str
-        Color to use for background. See matplotlib.colors.
-    textcolor : str
-        Color to use for text. See matplotlib.colors.
-    colormap : str
-        Colormap to use for coloring the connections.
-    vmin : float | None
-        Minimum value for colormap. If None, it is determined automatically.
-    vmax : float | None
-        Maximum value for colormap. If None, it is determined automatically.
-    colorbar : bool
-        Display a colorbar or not.
-    title : str
-        The figure title.
-    colorbar_size : float
-        Size of the colorbar.
-    colorbar_pos : 2-tuple
-        Position of the colorbar.
-    fontsize_title : int
-        Font size to use for title.
-    fontsize_names : int
-        Font size to use for node names.
-    fontsize_colorbar : int
-        Font size to use for colorbar.
-    padding : float
-        Space to add around figure to accommodate long labels.
-    fig : None | instance of matplotlib.pyplot.Figure
-        The figure to use. If None, a new figure with the specified background
-        color will be created.
-    subplot : int | 3-tuple
-        Location of the subplot when creating figures with multiple plots. E.g.
-        121 or (1, 2, 1) for 1 row, 2 columns, plot 1. See
-        matplotlib.pyplot.subplot.
-    interactive : bool
-        When enabled, left-click on a node to show only connections to that
-        node. Right-click shows all connections.
-
-    Returns
-    -------
-    fig : instance of matplotlib.pyplot.Figure
-        The figure handle.
-    axes : instance of matplotlib.axes.PolarAxesSubplot
-        The subplot handle.
-    """
-    import matplotlib.pyplot as plt
-
-    n_nodes = len(node_names)
-
-    if node_colors is not None:
-        if len(node_colors) < n_nodes:
-            node_colors = cycle(node_colors)
-    else:
-        # assign colors using colormap
-        node_colors = [plt.cm.spectral(i / float(n_nodes))
-                       for i in range(n_nodes)]
-
-    # handle 1D and 2D connectivity information
-    if con.ndim == 1:
-        if indices is None:
-            raise ValueError('indices has to be provided if con.ndim == 1')
-        tmp = np.zeros((n_nodes, n_nodes))
-        for c, *i in zip(con, *indices):
-            tmp[i] = c
-        con = tmp
-    elif con.ndim == 2:
-        if con.shape[0] != n_nodes or con.shape[1] != n_nodes:
-            raise ValueError('con has to be 1D or a square matrix')
-    else:
-        raise ValueError('con has to be 1D or a square matrix')
-
-    # remove diagonal (do not show node's self-connectivity)
-    np.fill_diagonal(con, 0)
-
-    # get the colormap
-    if isinstance(colormap, string_types):
-        colormap = plt.get_cmap(colormap)
-
-    # Make figure background the same colors as axes
-    if fig is None:
-        fig = plt.figure(figsize=(8, 8), facecolor=facecolor)
-
-    # Use a polar axes
-    if not isinstance(subplot, tuple):
-        subplot = (subplot,)
-    axes = plt.subplot(*subplot, axisbg=facecolor)
-
-    axes.spines['bottom'].set_visible(False)
-    axes.spines['right'].set_visible(False)
-    axes.spines['left'].set_visible(False)
-    axes.spines['top'].set_visible(False)
-
-    tmp = np.empty((n_nodes + 4, n_nodes + 4)) * np.nan
-    tmp[2:-2, 2:-2] = con
-    con = tmp
-
-    h = axes.imshow(con, cmap=colormap, interpolation='nearest', vmin=vmin,
-                    vmax=vmax)
-
-    nodes = np.empty((n_nodes + 4, n_nodes + 4, 4)) * np.nan
-    for i in range(n_nodes):
-        nodes[i + 2, 0, :] = node_colors[i]
-        nodes[i + 2, -1, :] = node_colors[i]
-        nodes[0, i + 2, :] = node_colors[i]
-        nodes[-1, i + 2, :] = node_colors[i]
-    axes.imshow(nodes, interpolation='nearest')
-
-    if colorbar:
-        cb = plt.colorbar(h, ax=axes, use_gridspec=False,
-                          shrink=colorbar_size,
-                          anchor=colorbar_pos)
-        cb_yticks = plt.getp(cb.ax.axes, 'yticklabels')
-        cb.ax.tick_params(labelsize=fontsize_colorbar)
-        plt.setp(cb_yticks, color=textcolor)
-
-    if title is not None:
-        plt.title(title, color=textcolor, fontsize=fontsize_title,
-                  axes=axes)
-
-    # Draw node labels
-    for i, name in enumerate(node_names):
-        axes.text(-1, i + 2, name, size=fontsize_names,
-                  rotation=0, rotation_mode='anchor',
-                  horizontalalignment='right', verticalalignment='center',
-                  color=textcolor)
-        axes.text(i + 2, len(node_names) + 4, name, size=fontsize_names,
-                  rotation=90, rotation_mode='anchor',
-                  horizontalalignment='right', verticalalignment='center',
-                  color=textcolor)
-
-    return fig, axes
-
-
 def plot_connectivity_circle(con, node_names, indices=None, n_lines=None,
                              node_angles=None, node_width=None,
                              node_colors=None, facecolor='black',
@@ -2912,6 +2755,163 @@ def plot_connectivity_circle(con, node_names, indices=None, n_lines=None,
                            node_angles=node_angles)
 
         fig.canvas.mpl_connect('button_press_event', callback)
+
+    return fig, axes
+
+
+def plot_connectivity_matrix(con, node_names, indices=None,
+                             node_colors=None, facecolor='black',
+                             textcolor='white',
+                             colormap='hot', vmin=None, vmax=None,
+                             colorbar=True, title=None, colorbar_size=0.2,
+                             colorbar_pos=(-0.3, 0.1), fontsize_title=12,
+                             fontsize_names=8, fontsize_colorbar=8,
+                             fig=None, subplot=111):
+    """Visualize connectivity as a matrix.
+
+    Parameters
+    ----------
+    con : array
+        Connectivity scores. Can be a square matrix, or a 1D array. If a 1D
+        array is provided, "indices" has to be used to define the connection
+        indices.
+    node_names : list of str
+        Node names. The order corresponds to the order in con.
+    indices : tuple of arrays | None
+        Two arrays with indices of connections for which the connections
+        strenghts are defined in con. Only needed if con is a 1D array.
+    node_colors : list of tuples | list of str
+        List with the color to use for each node. If fewer colors than nodes
+        are provided, the colors will be repeated. Any color supported by
+        matplotlib can be used, e.g., RGBA tuples, named colors.
+    facecolor : str
+        Color to use for background. See matplotlib.colors.
+    textcolor : str
+        Color to use for text. See matplotlib.colors.
+    colormap : str
+        Colormap to use for coloring the connections.
+    vmin : float | None
+        Minimum value for colormap. If None, it is determined automatically.
+    vmax : float | None
+        Maximum value for colormap. If None, it is determined automatically.
+    colorbar : bool
+        Display a colorbar or not.
+    title : str
+        The figure title.
+    colorbar_size : float
+        Size of the colorbar.
+    colorbar_pos : 2-tuple
+        Position of the colorbar.
+    fontsize_title : int
+        Font size to use for title.
+    fontsize_names : int
+        Font size to use for node names.
+    fontsize_colorbar : int
+        Font size to use for colorbar.
+    padding : float
+        Space to add around figure to accommodate long labels.
+    fig : None | instance of matplotlib.pyplot.Figure
+        The figure to use. If None, a new figure with the specified background
+        color will be created.
+    subplot : int | 3-tuple
+        Location of the subplot when creating figures with multiple plots. E.g.
+        121 or (1, 2, 1) for 1 row, 2 columns, plot 1. See
+        matplotlib.pyplot.subplot.
+    interactive : bool
+        When enabled, left-click on a node to show only connections to that
+        node. Right-click shows all connections.
+
+    Returns
+    -------
+    fig : instance of matplotlib.pyplot.Figure
+        The figure handle.
+    axes : instance of matplotlib.axes.PolarAxesSubplot
+        The subplot handle.
+    """
+    import matplotlib.pyplot as plt
+
+    n_nodes = len(node_names)
+
+    if node_colors is not None:
+        if len(node_colors) < n_nodes:
+            node_colors = cycle(node_colors)
+    else:
+        # assign colors using colormap
+        node_colors = [plt.cm.spectral(i / float(n_nodes))
+                       for i in range(n_nodes)]
+
+    # handle 1D and 2D connectivity information
+    if con.ndim == 1:
+        if indices is None:
+            raise ValueError('indices has to be provided if con.ndim == 1')
+        tmp = np.zeros((n_nodes, n_nodes))
+        for c, *i in zip(con, *indices):
+            tmp[i] = c
+        con = tmp
+    elif con.ndim == 2:
+        if con.shape[0] != n_nodes or con.shape[1] != n_nodes:
+            raise ValueError('con has to be 1D or a square matrix')
+    else:
+        raise ValueError('con has to be 1D or a square matrix')
+
+    # remove diagonal (do not show node's self-connectivity)
+    np.fill_diagonal(con, 0)
+
+    # get the colormap
+    if isinstance(colormap, string_types):
+        colormap = plt.get_cmap(colormap)
+
+    # Make figure background the same colors as axes
+    if fig is None:
+        fig = plt.figure(figsize=(8, 8), facecolor=facecolor)
+
+    # Use a polar axes
+    if not isinstance(subplot, tuple):
+        subplot = (subplot,)
+    axes = plt.subplot(*subplot, axisbg=facecolor)
+
+    axes.spines['bottom'].set_visible(False)
+    axes.spines['right'].set_visible(False)
+    axes.spines['left'].set_visible(False)
+    axes.spines['top'].set_visible(False)
+
+    tmp = np.empty((n_nodes + 4, n_nodes + 4)) * np.nan
+    tmp[2:-2, 2:-2] = con
+    con = tmp
+
+    h = axes.imshow(con, cmap=colormap, interpolation='nearest', vmin=vmin,
+                    vmax=vmax)
+
+    nodes = np.empty((n_nodes + 4, n_nodes + 4, 4)) * np.nan
+    for i in range(n_nodes):
+        nodes[i + 2, 0, :] = node_colors[i]
+        nodes[i + 2, -1, :] = node_colors[i]
+        nodes[0, i + 2, :] = node_colors[i]
+        nodes[-1, i + 2, :] = node_colors[i]
+    axes.imshow(nodes, interpolation='nearest')
+
+    if colorbar:
+        cb = plt.colorbar(h, ax=axes, use_gridspec=False,
+                          shrink=colorbar_size,
+                          anchor=colorbar_pos)
+        cb_yticks = plt.getp(cb.ax.axes, 'yticklabels')
+        cb.ax.tick_params(labelsize=fontsize_colorbar)
+        plt.setp(cb_yticks, color=textcolor)
+
+    if title is not None:
+        plt.title(title, color=textcolor, fontsize=fontsize_title,
+                  axes=axes)
+
+    # Draw node labels
+    for i, name in enumerate(node_names):
+        axes.text(-1, i + 2, name, size=fontsize_names,
+                  rotation=0, rotation_mode='anchor',
+                  horizontalalignment='right', verticalalignment='center',
+                  color=textcolor)
+        axes.text(i + 2, len(node_names) + 4, name, size=fontsize_names,
+                  rotation=90, rotation_mode='anchor',
+                  horizontalalignment='right', verticalalignment='center',
+                  color=textcolor)
 
     return fig, axes
 
