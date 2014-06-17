9608dbee9dc16aefbbfe710635fe5b4c9737fe11
Praveen Sripad <pravsripad@gmail.com>
2014-06-17 19:08:45 +0200

    One data path to store them all.

--- mne/datasets/utils.py
+++ mne/datasets/utils.py
@@ -66,14 +66,12 @@ def _data_path(path=None, force_update=False, update_path=True,
                download=True, name=None, check_version=True, verbose=None):
     """Aux function
     """
-    key = {'sample': 'MNE_DATASETS_SAMPLE_PATH',
-           'spm': 'MNE_DATASETS_SPM_FACE_PATH'}[name]
 
     if path is None:
         # use an intelligent guess if it's not defined
         def_path = op.realpath(op.join(op.dirname(__file__),
                                        '..', '..', 'examples'))
-        path = get_config(key, def_path)
+        path = get_config('MNE_DATA', def_path)
         # use the same for all datasets
         if not op.exists(path) or not os.access(path, os.W_OK):
             try:
@@ -152,7 +150,7 @@ def _data_path(path=None, force_update=False, update_path=True,
 
     path = op.abspath(path)
     if update_path is None:
-        if get_config(key, '') != path:
+        if get_config('MNE_DATA', '') != path:
             update_path = True
             msg = ('Do you want to set the path:\n    %s\nas the default '
                    'sample dataset path in the mne-python config [y]/n? '
@@ -164,7 +162,7 @@ def _data_path(path=None, force_update=False, update_path=True,
             update_path = False
 
     if update_path is True:
-        set_config(key, path)
+        set_config('MNE_DATA', path)
 
     path = op.join(path, folder_name)
 
