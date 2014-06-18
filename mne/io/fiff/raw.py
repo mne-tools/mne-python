565ae4648be51fe0bf1d28ed32093ebbd7a39d3f
Martin Luessi <mluessi@nmr.mgh.harvard.edu>
2014-06-18 06:43:55 -0400

    use number for files that don't have next fname

--- mne/io/fiff/raw.py
+++ mne/io/fiff/raw.py
@@ -95,8 +95,8 @@ class RawFIFF(_BaseRaw):
             raws.append(raw)
             if next_fname is not None:
                 if not op.exists(next_fname):
-                    logger.warning('Split raw file detected but next file %s'
-                                   'does not exist.')
+                    logger.warning('Split raw file detected but next file %s '
+                                   'does not exist.' % next_fname)
                     continue
                 if next_fname in fnames:
                     # the user manually specified the split files
@@ -294,6 +294,25 @@ class RawFIFF(_BaseRaw):
                     if ent.kind == FIFF.FIFF_REF_FILE_NAME:
                         tag = read_tag(fid, ent.pos)
                         next_fname = op.join(op.dirname(fname), tag.data)
+                    if ent.kind == FIFF.FIFF_REF_FILE_NUM:
+                        # Some files don't have the name, just the number. So
+                        # we construct the name from the current name.
+                        if next_fname is not None:
+                            continue
+                        next_num = read_tag(fid, ent.pos).data
+                        path, base = op.split(fname)
+                        idx = base.find('.')
+                        idx2 = base.rfind('-')
+                        if idx2 < 0 and next_num == 1:
+                            # this is the first file, which may not be numbered
+                            next_fname = op.join(path, '%s-%d.%s' % (base[:idx],
+                                next_num, base[idx + 1:]))
+                            continue
+                        num_str = base[idx2 + 1:idx]
+                        if not num_str.isdigit():
+                            continue
+                        next_fname = op.join(path, '%s-%d.%s' % (base[:idx2],
+                                             next_num, base[idx + 1:]))
                 if next_fname is not None:
                     break
 
