5fed02457bca6ea997fb3fca55ae1a8c75dd73ec
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2014-06-18 21:59:17 +0200

    FIX : sample data not necesary

--- mne/minimum_norm/tests/test_psf_ctf.py
+++ mne/minimum_norm/tests/test_psf_ctf.py
@@ -12,8 +12,6 @@ s_path = op.join(sample.data_path(download=False), 'MEG', 'sample')
 fname_inv = op.join(s_path, 'sample_audvis-meg-oct-6-meg-inv.fif')
 fname_fwd = op.join(s_path, 'sample_audvis-meg-oct-6-fwd.fif')
 
-data_path = sample.data_path()
-
 fname_label = [data_path + '/MEG/sample/labels/Aud-rh.label',
                data_path + '/MEG/sample/labels/Aud-lh.label']
 
