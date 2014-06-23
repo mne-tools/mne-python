correct h formula

mne/splines.py
@@ -46,8 +46,8 @@ def _calc_h(cosang, stiffnes=4, num_lterms=50):
     num_lterms : int
         number of Legendre terms to evaluate.
     """
-    factors = [(2 * n + 1) ** 2 /
-               (n ** stiffnes * (n + 1) ** stiffnes * -(4 * np.pi))
+    factors = [(2 * n + 1) /
+               (n ** (stiffnes - 1) * (n + 1) ** (stiffnes - 1) * 4 * np.pi)
                for n in range(1, num_lterms + 1)]
     return legval(cosang, factors)
 