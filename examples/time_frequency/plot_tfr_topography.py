77658fc0a51bef629e90aede8a56cd30939dded2
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2014-06-27 18:13:45 +0200

    ENH : first pass on new tfr code

--- examples/time_frequency/plot_tfr_topography.py
+++ examples/time_frequency/plot_tfr_topography.py
@@ -17,7 +17,6 @@ import matplotlib.pyplot as plt
 import mne
 from mne import io
 from mne.time_frequency import induced_power
-from mne.viz import plot_topo_power, plot_topo_phase_lock
 from mne.datasets import sample
 
 data_path = sample.data_path()
@@ -39,42 +38,133 @@ picks = mne.pick_types(raw.info, meg='grad', eeg=False, eog=True,
 
 epochs = mne.Epochs(raw, events, event_id, tmin, tmax, picks=picks,
                     baseline=(None, 0), reject=dict(grad=4000e-13, eog=150e-6))
-data = epochs.get_data()  # as 3D matrix
-
-layout = mne.find_layout(epochs.info, 'meg')
 
 ###############################################################################
 # Calculate power and phase locking value
 
-frequencies = np.arange(7, 30, 3)  # define frequencies of interest
-n_cycles = frequencies / float(7)  # different number of cycle per frequency
-Fs = raw.info['sfreq']  # sampling in Hz
-decim = 3
-power, phase_lock = induced_power(data, Fs=Fs, frequencies=frequencies,
-                                  n_cycles=n_cycles, n_jobs=1, use_fft=False,
-                                  decim=decim, zero_mean=True)
+from mne.channels import ContainsMixin, PickDropChannelsMixin
+from mne.viz import partial, _imshow_tfr, _plot_topo, rescale
+from copy import deepcopy
 
-###############################################################################
-# Prepare topography plots, set baseline correction parameters
+class AverageTFR(ContainsMixin, PickDropChannelsMixin):
+    """docstring for TFR"""
+    def __init__(self, info, data, times, freqs):
+        self.info = info
+        assert data.ndim == 3
+        n_channels, n_freqs, n_times = data.shape
+        assert n_channels == len(info['chs'])
+        assert n_freqs == len(freqs)
+        assert n_times == len(times)
+        self.data = data
+        self.times = times
+        self.freqs = freqs
 
-baseline = (None, 0)  # set the baseline for induced power
-mode = 'ratio'  # set mode for baseline rescaling
+    @property
+    def ch_names(self):
+        return self.info['ch_names']
 
-###############################################################################
-# Show topography of power.
+    def plot_topo(self, picks, tmin, tmax, fmin, fmax, clim, cmap, layout):
+        pass
 
-title = 'Induced power - MNE sample data'
-plot_topo_power(epochs, power, frequencies, layout, baseline=baseline,
-                mode=mode, decim=decim, vmin=0., vmax=14, title=title)
-plt.show()
+    def plot(self, picks=None, baseline=None, mode='mean', tmin=None, tmax=None,
+             fmin=None, fmax=None, vmin=None, vmax=None, layout=None, cmap=None,
+             layout_scale=0.945, title=None, dB=False, colorbar=True, show=True):
 
-###############################################################################
-# Show topography of phase locking value (PLV)
+        times = self.times
+        data = self.data
 
-mode = None  # no baseline rescaling for PLV
+        if mode is not None:
+            data = rescale(data.copy(), times, baseline, mode)
 
-title = 'Phase locking value - MNE sample data'
-plot_topo_phase_lock(epochs, phase_lock, frequencies, layout,
-                     baseline=baseline, mode=mode, decim=decim, title=title)
+        times *= 1e3
+        if dB:
+            data = 20 * np.log10(data)
+        if vmin is None:
+            vmin = data.min()
+        if vmax is None:
+            vmax = data.max()
+        if layout is None:
+            from mne.layouts.layout import find_layout
+            layout = find_layout(self.info)
 
-plt.show()
+        imshow = partial(_imshow_tfr, tfr=data, freq=self.freqs)
+
+        fig = _plot_topo(info=self.info, times=times,
+                         show_func=imshow, layout=layout,
+                         colorbar=colorbar, vmin=vmin, vmax=vmax, cmap=cmap,
+                         layout_scale=layout_scale, title=title, border='w',
+                         x_label='Time (ms)', y_label='Frequency (Hz)')
+
+        if show:
+            plt.show()
+
+        return fig
+
+    def _check_compat(self, tfr):
+        assert np.all(tfr.times == self.times)
+        assert np.all(tfr.freqs == self.freqs)
+
+    def __add__(self, tfr):
+        self._check_compat(tfr)
+        out = self.copy()
+        out.data += tfr.data
+        return out
+
+    def __iadd__(self, tfr):
+        self._check_compat(tfr)
+        self.data += tfr.data
+        return self
+
+    def __sub__(self, tfr):
+        self._check_compat(tfr)
+        out = self.copy()
+        out.data -= tfr.data
+        return out
+
+    def __isub__(self, tfr):
+        self._check_compat(tfr)
+        self.data -= tfr.data
+        return self
+
+    def save(self, fname="foobar-tfr.fif"):
+        pass
+
+    def copy(self):
+        return deepcopy(self)
+
+    def __repr__(self):
+        s = "time : [%f, %f]" % (self.times[0], self.times[-1])
+        s += ", freq : [%f, %f]" % (self.freqs[0], self.freqs[-1])
+        s += ', channels: %d' % self.data.shape[1]
+        return "<AverageTFR  |  %s>" % s
+
+
+def tfr_morlet(epochs, freqs, n_cycles, use_fft=False,
+               return_itc=True, zero_mean=True, decim=1, n_jobs=1):
+    data = epochs.get_data()
+    picks = mne.pick_types(epochs.info, meg=True, eeg=True)
+    info = mne.pick_info(epochs.info, picks)
+    data = data[:, picks, :]
+    power, itc = induced_power(data, Fs=info['sfreq'], frequencies=freqs,
+                               n_cycles=n_cycles, n_jobs=n_jobs,
+                               use_fft=use_fft, decim=decim,
+                               zero_mean=zero_mean)
+    times = epochs.times[::decim].copy()
+    out = AverageTFR(info, power, times, freqs)
+    if return_itc:
+        out = (out, AverageTFR(info, itc, times, freqs))
+    return out
+
+freqs = np.arange(7, 30, 3)  # define frequencies of interest
+n_cycles = freqs / 7.  # different number of cycle per frequency
+decim=3
+power, itc = tfr_morlet(epochs, freqs=freqs, n_cycles=n_cycles, use_fft=False,
+                         return_itc=True, zero_mean=True, decim=decim, n_jobs=1)
+
+power.plot(baseline=(None, 0), mode='ratio',
+           title='Induced power - MNE sample data', vmin=0., vmax=14.)
+itc.plot(title='Inter-Trial coherence - MNE sample data', vmin=0., vmax=1.)
+
+print itc
+itc = itc + power
+itc -= power
