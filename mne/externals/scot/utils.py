412c05b40bfa04d9f2920917a3aa99c79e92b308
Martin <martin.billinger@tugraz.at>
2014-07-01 09:21:40 +0200

    deprecation warning in numpy (scot upstream fix)

--- mne/externals/scot/utils.py
+++ mne/externals/scot/utils.py
@@ -194,10 +194,10 @@ def cartesian(arrays, out=None):
     if out is None:
         out = np.zeros([n, len(arrays)], dtype=dtype)
 
-    m = n / arrays[0].size
-    out[:,0] = np.repeat(arrays[0], m)
+    m = n // arrays[0].size
+    out[:, 0] = np.repeat(arrays[0], m)
     if arrays[1:]:
-        cartesian(arrays[1:], out=out[0:m,1:])
+        cartesian(arrays[1:], out=out[0:m, 1:])
         for j in range(1, arrays[0].size):
-            out[j*m:(j+1)*m,1:] = out[0:m,1:]
+            out[j * m: (j + 1) * m, 1:] = out[0:m, 1:]
     return out
