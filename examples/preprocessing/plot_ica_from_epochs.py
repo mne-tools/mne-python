fbe2c0d8768de4d03189308aae55ea09c3c05251
dengemann <d.engemann@fz-juelich.de>
2014-07-02 17:35:09 +0200

    ENH: improve examples, allow for detecting EEG by CTPS

--- examples/preprocessing/plot_ica_from_epochs.py
+++ examples/preprocessing/plot_ica_from_epochs.py
@@ -5,6 +5,7 @@ Compute ICA components on epochs
 
 ICA is fit to MEG raw data.
 The sources matching the ECG are automatically found and displayed.
+We assume that the non-stationary EOG artifacts have already been removed.
 Subsequently, artefact detection and rejection quality are assessed.
 Finally, the impact on the evoked ERF is visualized.
 """
@@ -28,15 +29,16 @@ raw_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
 
 raw = Raw(raw_fname, preload=True)
 raw.filter(1, 30, method='iir')
-picks = mne.pick_types(raw.info, meg=True, eeg=False, eog=False, ecg=True,
+picks = mne.pick_types(raw.info, meg=True, eeg=False, eog=True, ecg=True,
                        stim=False, exclude='bads')
 
 # longer + more epochs for more artifact exposure
-tmin, tmax = -0.5, 0.5
-event_id = dict(aud_l=1, aud_r=2, vis_l=3, vis_r=4)
 events = mne.find_events(raw, stim_channel='STI 014')
+event_id = dict(aud_l=1, aud_r=2, vis_l=3, vis_r=4)
+reject = dict(eog=250e-6)
+tmin, tmax = -0.5, 0.5
 epochs = mne.Epochs(raw, events, event_id, tmin, tmax, proj=False, picks=picks,
-                    baseline=(None, 0), preload=True, reject=None)
+                    baseline=(None, 0), preload=True, reject=reject)
 
 ###############################################################################
 # 1) Fit ICA model using the FastICA algorithm
@@ -58,7 +60,7 @@ show_picks = np.abs(scores).argsort()[::-1][:5]
 ica.plot_sources(epochs, show_picks, exclude=ecg_inds, title=title % 'ecg')
 ica.plot_components(ecg_inds, title=title % 'ecg')
 
-ica.exclude += ecg_inds[:3]  # whatever happens, we take 3
+ica.exclude += ecg_inds[:3]  # typically 3 reliable ECG components
 
 ###############################################################################
 # 3) Assess component selection and unmixing quality
