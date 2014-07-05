aff60d67313b93f44184cfa2b12ad6f206c22a87
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2014-07-05 21:40:40 +0200

    ENH : improve TFR plotting

--- examples/time_frequency/plot_tfr_topography.py
+++ examples/time_frequency/plot_tfr_topography.py
@@ -16,41 +16,38 @@ import numpy as np
 import mne
 from mne import io
 from mne.time_frequency import tfr_morlet
-from mne.datasets import sample
+from mne.datasets import somato
 
-data_path = sample.data_path()
-
-raw_fname = data_path + '/MEG/sample/sample_audvis_raw.fif'
-event_fname = data_path + '/MEG/sample/sample_audvis_raw-eve.fif'
-event_id, tmin, tmax = 1, -0.2, 0.5
+###############################################################################
+# Set parameters
+data_path = somato.data_path()
+raw_fname = data_path + '/MEG/somato/sef_raw_sss.fif'
+event_id, tmin, tmax = 1, -1., 3.
 
 # Setup for reading the raw data
 raw = io.Raw(raw_fname)
-events = mne.read_events(event_fname)
-
-include = []
-raw.info['bads'] += ['MEG 2443', 'EEG 053']  # bads + 2 more
+baseline = (None, 0)
+events = mne.find_events(raw)
 
 # picks MEG gradiometers
-picks = mne.pick_types(raw.info, meg='grad', eeg=False, eog=True,
-                       stim=False, include=include, exclude='bads')
+picks = mne.pick_types(raw.info, meg='grad', eeg=False, eog=True, stim=False)
 
 epochs = mne.Epochs(raw, events, event_id, tmin, tmax, picks=picks,
-                    baseline=(None, 0), reject=dict(grad=4000e-13, eog=150e-6))
+                    baseline=baseline, reject=dict(grad=4000e-13, eog=350e-6))
 
 ###############################################################################
 # Calculate power and intertrial coherence
 
 freqs = np.arange(7, 30, 3)  # define frequencies of interest
-n_cycles = freqs / 7.  # different number of cycle per frequency
+n_cycles = freqs / 2.  # different number of cycle per frequency
 power, itc = tfr_morlet(epochs, freqs=freqs, n_cycles=n_cycles, use_fft=False,
-                        return_itc=True, decim=3, n_jobs=1)
+                        return_itc=True, decim=3, n_jobs=-1)
 
 # Inspect power
-power.plot_topo(baseline=(None, 0), mode='ratio', title='Average power',
-                vmin=0., vmax=14.)
-power.plot([92], baseline=(None, 0), mode='ratio')
-power.plot_topomap(ch_type='grad', tmin=0.05, tmax=0.150, fmin=0, fmax=10)
+power.plot_topo(baseline=(-0.5, 0), mode='logratio', title='Average power')
+power.plot([82], baseline=(-0.5, 0), mode='logratio')
+power.plot_topomap(ch_type='grad', tmin=0.5, tmax=1.5, fmin=13, fmax=25,
+                   baseline=(-0.5, 0), mode='logratio')
 
 # Inspect ITC
-itc.plot_topo(title='Inter-Trial coherence', vmin=0., vmax=1.)
+itc.plot_topo(title='Inter-Trial coherence', vmin=0., vmax=1., cmap='Reds')
