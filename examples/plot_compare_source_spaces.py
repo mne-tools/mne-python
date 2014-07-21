f830cc6f6e5836cdc267743a890d8c7eda07da8c
Alan Leggitt <leggitta3@gmail.com>
2014-07-21 16:18:55 -0700

    updated example visualization

--- examples/plot_compare_source_spaces.py
+++ examples/plot_compare_source_spaces.py
@@ -2,6 +2,8 @@
 #
 # License: BSD (3-clause)
 
+import numpy as np
+from scipy.spatial import ConvexHull
 from mayavi import mlab
 from mne import (setup_source_space, setup_volume_source_space)
 from mne.datasets import sample
@@ -32,7 +34,20 @@ x1, y1, z1 = surf[0]['rr'].T
 # left cerebellum cortex
 x2, y2, z2 = lh_ctx[0]['rr'][lh_ctx[0]['inuse'].astype(bool)].T
 
-p1 = mlab.points3d(x1, y1, z1, color=(1, 0, 0))
-p2 = mlab.points3d(x2, y2, z2, color=(0, 0, 1))
+# open a 3d figure
+mlab.figure(1, bgcolor=(0, 0, 0))
 
+# plot the left cortical surface
+p1 = mlab.points3d(x1, y1, z1, color=(1, 0, 0))
+
+# plot the convex hull bounding the left cerebellum
+hull = ConvexHull(np.c_[x2, y2, z2])
+mlab.triangular_mesh(x2, y2, z2, hull.simplices, color=(0.5, 0.5, 0.5),
+                     opacity=0.3)
+
+# plot the left cerebellum sources
+mlab.points3d(x2, y2, z2, color=(1, 1, 0), scale_factor=0.001)
+
+mlab.view(173.78, 101.75, 0.30, np.array([-0.03, -0.01,  0.03]))
+mlab.roll(85)
 mlab.show()
