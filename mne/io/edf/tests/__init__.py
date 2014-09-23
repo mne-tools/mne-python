eff6bd1117d44207f711976f839a76e1071bbc77
Teon Brooks <teon@nyu.edu>
2014-09-23 17:07:02 +0200

    RM unnecessary imports

--- mne/io/edf/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-"""EDF,BDF tests"""
-
-# Author: Teon Brooks <teon@nyu.edu>
-#
-# License: BSD (3-clause)
-
-from .test_edf import (test_bdf_data, test_edf_data,
-                       test_read_segment, test_append)
