39e518e245a644405c640ba2975f5928dc159dcf
t3on <teon@nyu.edu>
2014-11-17 02:02:55 +0100

    MV test for get_ras_to_neuromag_trans

--- mne/io/kit/tests/test_coreg.py
+++ mne/io/kit/tests/test_coreg.py
@@ -9,8 +9,6 @@ import numpy as np
 from numpy.testing import assert_array_almost_equal, assert_array_equal
 
 from mne.io.kit import read_hsp, write_hsp, read_mrk, write_mrk
-from mne.transforms import (apply_trans, rotation, translation,
-                            get_ras_to_neuromag_trans)
 from mne.utils import _TempDir
 
 
@@ -49,27 +47,3 @@ def test_io_mrk():
     write_mrk(path, pts)
     pts_2 = read_mrk(path)
     assert_array_equal(pts, pts_2, "read/write mrk to text")
-
-
-def test_hsp_trans():
-    """Test the coordinate transformation for hsp files"""
-    # create model points in neuromag-like space
-    anterior = [0, 1, 0]
-    left = [-1, 0, 0]
-    right = [.8, 0, 0]
-    up = [0, 0, 1]
-    rand_pts = np.random.uniform(-1, 1, (3, 3))
-    pts = np.vstack((anterior, left, right, up, rand_pts))
-
-    # change coord system
-    rx, ry, rz, tx, ty, tz = np.random.uniform(-2 * np.pi, 2 * np.pi, 6)
-    trans = np.dot(translation(tx, ty, tz), rotation(rx, ry, rz))
-    pts_changed = apply_trans(trans, pts)
-
-    # transform back into original space
-    nas, lpa, rpa = pts_changed[:3]
-    hsp_trans = get_ras_to_neuromag_trans(nas, lpa, rpa)
-    pts_restored = apply_trans(hsp_trans, pts_changed)
-
-    assert_array_almost_equal(pts_restored, pts, 6, "Neuromag transformation "
-                              "failed")
