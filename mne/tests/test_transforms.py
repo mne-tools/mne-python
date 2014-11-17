1c82d8a8de90d00ad39f2abf5c730482c0a47b61
t3on <teon@nyu.edu>
2014-11-17 02:02:55 +0100

    MV test for get_ras_to_neuromag_trans

--- mne/tests/test_transforms.py
+++ mne/tests/test_transforms.py
@@ -13,7 +13,9 @@ from mne.utils import _TempDir, run_tests_if_main
 from mne.transforms import (_get_mri_head_t_from_trans_file, invert_transform,
                             rotation, rotation3d, rotation_angles, _find_trans,
                             combine_transforms, transform_coordinates,
-                            collect_transforms)
+                            collect_transforms, apply_trans, translation,
+                            get_ras_to_neuromag_trans)
+
 
 warnings.simplefilter('always')  # enable b/c these tests throw warnings
 
@@ -62,6 +64,30 @@ def test_io_trans():
         fname2 = op.join(tempdir, 'trans-test-bad-name.fif')
         write_trans(fname2, trans0)
     assert_true(len(w) >= 1)
+
+
+def test_get_ras_to_neuromag_trans():
+    """Test the coordinate transformation from ras to neuromag"""
+    # create model points in neuromag-like space
+    anterior = [0, 1, 0]
+    left = [-1, 0, 0]
+    right = [.8, 0, 0]
+    up = [0, 0, 1]
+    rand_pts = np.random.uniform(-1, 1, (3, 3))
+    pts = np.vstack((anterior, left, right, up, rand_pts))
+
+    # change coord system
+    rx, ry, rz, tx, ty, tz = np.random.uniform(-2 * np.pi, 2 * np.pi, 6)
+    trans = np.dot(translation(tx, ty, tz), rotation(rx, ry, rz))
+    pts_changed = apply_trans(trans, pts)
+
+    # transform back into original space
+    nas, lpa, rpa = pts_changed[:3]
+    hsp_trans = get_ras_to_neuromag_trans(nas, lpa, rpa)
+    pts_restored = apply_trans(hsp_trans, pts_changed)
+
+    err = "Neuromag transformation failed"
+    assert_array_almost_equal(pts_restored, pts, 6, err)
 
 
 def test_rotation():
