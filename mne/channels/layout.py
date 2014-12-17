29338551a2821b76f29105143c0561f268c92ceb
Christian Brodbeck <christianmbrodbeck@gmail.com>
2014-12-17 16:15:47 -0500

    PEP8 imports

--- mne/channels/layout.py
+++ mne/channels/layout.py
@@ -11,17 +11,18 @@
 import logging
 from collections import defaultdict
 from itertools import combinations
-import re
 import os
 import os.path as op
+
 import numpy as np
 from scipy.spatial.distance import pdist
+
+from .channels import _contains_ch_type
 from ..io.meas_info import _read_dig_points
 from ..io.pick import pick_types
 from ..io.constants import FIFF
 from ..utils import _clean_names
 from ..externals.six.moves import map
-from .channels import _contains_ch_type
 from ..viz import plot_montage
 from ..transforms import (_sphere_to_cartesian, _polar_to_cartesian,
                           _cartesian_to_sphere, apply_trans, als_ras_trans_mm,
