6537fd515ec6ead32aeab789a0ab0fb84950d47d
Christian Brodbeck <christianmbrodbeck@gmail.com>
2014-12-18 19:42:18 -0500

    API name

--- mne/channels/__init__.py
+++ mne/channels/__init__.py
@@ -5,7 +5,7 @@ setting of sensors locations used for processing and plotting.
 
 from .layout import (Layout, make_eeg_layout, make_grid_layout, read_layout,
                      find_layout, read_montage, apply_montage,
-                     read_polhemus_montage)
+                     read_montage_polhemus)
 
 from .channels import (equalize_channels, rename_channels,
                        read_ch_connectivity)
