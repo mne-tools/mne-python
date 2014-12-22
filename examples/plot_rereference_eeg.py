cad7e35d12eaac00a0b26b948e6cd89099a0ead6
Marijn van Vliet <w.m.vanvliet@gmail.com>
2014-12-22 10:17:38 +0200

    Reduced memory consumption of rerefencing example

--- examples/plot_rereference_eeg.py
+++ examples/plot_rereference_eeg.py
@@ -23,36 +23,38 @@ event_id, tmin, tmax = 1, -0.2, 0.5
 raw = mne.io.Raw(raw_fname, preload=True)
 events = mne.read_events(event_fname)
 
-# Apply different EEG referencing schemes
-#########################################
+# The EEG channels will be plotted to visulualize the difference in referencing
+# schemes.
+picks = mne.pick_types(raw.info, meg=False, eeg=True, exclude='bads')
+plt.figure(figsize=(6, 6))
+
+# Apply different EEG referencing schemes and plot the resulting evokeds
+########################################################################
 
 # No reference. This assumes that the EEG has already been referenced properly.
 # This explicitly prevents MNE from adding a default EEG reference.
 raw_none = mne.io.set_eeg_reference(raw, [])[0]
-
-# Average reference. This is normally added by default, but can also be added
-# explicitly.
-raw_car = mne.io.set_eeg_reference(raw)[0]
-
-# Use the mean of channels EEG 001 and EEG 002 as a reference
-raw_custom = mne.io.set_eeg_reference(raw, ['EEG 001', 'EEG 002'])[0]
-
-# Plot the evokeds to see the effect of the EEG reference
-#########################################################
-picks = mne.pick_types(raw.info, meg=False, eeg=True, exclude='bads')
 ev_none = mne.Epochs(raw_none, events, event_id, tmin, tmax).average(picks)
-ev_car = mne.Epochs(raw_car, events, event_id, tmin, tmax).average(picks)
-ev_custom = mne.Epochs(raw_custom, events, event_id, tmin, tmax).average(picks)
-
-plt.figure(figsize=(6, 6))
+del raw_none  # Free memory
 
 ax = plt.subplot(3, 1, 1)
 ev_none.plot(axes=ax)
 plt.title('Original reference')
 
+# Average reference. This is normally added by default, but can also be added
+# explicitly.
+raw_car = mne.io.set_eeg_reference(raw)[0]
+ev_car = mne.Epochs(raw_car, events, event_id, tmin, tmax).average(picks)
+del raw_car
+
 ax = plt.subplot(3, 1, 2)
 ev_car.plot(axes=ax)
 plt.title('Average reference')
+
+# Use the mean of channels EEG 001 and EEG 002 as a reference
+raw_custom = mne.io.set_eeg_reference(raw, ['EEG 001', 'EEG 002'])[0]
+ev_custom = mne.Epochs(raw_custom, events, event_id, tmin, tmax).average(picks)
+del raw_custom
 
 ax = plt.subplot(3, 1, 3)
 ev_custom.plot(axes=ax)
