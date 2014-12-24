16a6ba6a5a0181bf7a2c6ec04c636488dffae564
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2014-12-24 17:58:05 +0100

    MAINT dev version to follow PEP440

--- mne/__init__.py
+++ mne/__init__.py
@@ -1,7 +1,23 @@
 """MNE for MEG and EEG data analysis
 """
 
-__version__ = '0.9.git'
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.YaN   # Alpha release
+#   X.YbN   # Beta release
+#   X.YrcN  # Release Candidate
+#   X.Y     # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+#
+
+__version__ = '0.9.dev'
 
 # have to import verbose first since it's needed by many things
 from .utils import (set_log_level, set_log_file, verbose, set_config,
