eea90ca9675ddf5767aaf8033b6c6e8a6e0ea9b3
Eric89GXL <larson.eric.d@gmail.com>
2014-12-26 22:21:00 -0800

    FIX: Update conda version

--- make/install_python.ps1
+++ make/install_python.ps1
@@ -9,9 +9,9 @@ $BASE_URL = "https://www.python.org/ftp/python/"
 function DownloadMiniconda ($python_version, $platform_suffix) {
     $webclient = New-Object System.Net.WebClient
     if ($python_version -eq "3.4") {
-        $filename = "Miniconda3-3.5.5-Windows-" + $platform_suffix + ".exe"
+        $filename = "Miniconda3-latest-Windows-" + $platform_suffix + ".exe"
     } else {
-        $filename = "Miniconda-3.5.5-Windows-" + $platform_suffix + ".exe"
+        $filename = "Miniconda-latest-Windows-" + $platform_suffix + ".exe"
     }
     $url = $MINICONDA_URL + $filename
 
