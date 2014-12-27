1db6d5c3409892aa2ad7f9cc327db5e9429f20ac
Eric89GXL <larson.eric.d@gmail.com>
2014-12-26 22:42:14 -0800

    FIX: Conda bug

--- make/install_python.ps1
+++ make/install_python.ps1
@@ -9,9 +9,9 @@ $BASE_URL = "https://www.python.org/ftp/python/"
 function DownloadMiniconda ($python_version, $platform_suffix) {
     $webclient = New-Object System.Net.WebClient
     if ($python_version -eq "3.4") {
-        $filename = "Miniconda3-latest-Windows-" + $platform_suffix + ".exe"
+        $filename = "Miniconda3-3.7.3-Windows-" + $platform_suffix + ".exe"
     } else {
-        $filename = "Miniconda-latest-Windows-" + $platform_suffix + ".exe"
+        $filename = "Miniconda-3.7.3-Windows-" + $platform_suffix + ".exe"
     }
     $url = $MINICONDA_URL + $filename
 
