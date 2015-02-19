73f51dba2581189da0b27d34b3b910a8b1d3ce19
jona-sassenhagen <jona.sassenhagen@gmail.com>
2015-02-19 14:55:28 +0100

    Update bads.py
    Integrated criticism by dengeman

--- mne/preprocessing/bads.py
+++ mne/preprocessing/bads.py
@@ -1,10 +1,10 @@
 # Authors: Denis Engemann <denis.engemann@gmail.com>
 # License: BSD (3-clause)
 
+from collections import defaultdict
 
 import numpy as np
 from scipy import stats
-
 
 def find_outliers(X, threshold=3.0, max_iter=2):
     """Find outliers based on iterated Z-scoring
@@ -40,12 +40,7 @@ def find_outliers(X, threshold=3.0, max_iter=2):
     return bad_idx
 
 
-def corrmap(icas, template, 
-            threshold="auto", 
-            name="marked_ics", 
-            verbose_return=False, 
-            plot=True,
-            inplace=False):
+def corrmap(icas, template, threshold="auto", name="bads", plot=True, inplace=False):
     
     """Corrmap (Viola et al. 2009 Clin Neurophysiol) identifies the best group
     match to a supplied template. Typically, feed it a list of fitted ICAs and
@@ -79,35 +74,31 @@ def corrmap(icas, template,
         If float > 1: use find_outliers to identify ICs within subjects (not in 
         original Corrmap)
         Defaults to "auto".
-    name : string
-        Name of the field under which found ICs should be specified.
-        Defaults to "marked_ics".
-    verbose_return : bool
-        Should the output be restricted to the list of ICs (False), or also the 
-        correlation of the constructed template with the selected maps and the 
-        correlations?
-        Defaults to False.
+    name : str
+        Categorised ICs are stored in a defautl dictionary "categorised_ics". This
+        paramrter gives the key under which found ICs will be stored.
+        Defaults to "bads".
     plot : bool
         Should constructed template and selected maps be plotted?
-    ----------
-    Returns : list 
+    
+    Returns 
+    -------
+    new_icas  :  list 
         Returns a list of fitted ICs, enrichened with the indices of the selected
         maps in the field specified by the name keyword.   
     """
 
-    from mne.preprocessing.bads import find_outliers
-    import numpy as np
 
     def vcorrcoef(X,y): # vectorised correlation, by Jon Herman
-        Xm = np.reshape(np.mean(X,axis=1),(X.shape[0],1))
+        Xm = np.reshape(np.mean(X, axis=1), (X.shape[0], 1))
         ym = np.mean(y)
-        r_num = np.sum((X-Xm)*(y-ym),axis=1)
-        r_den = np.sqrt(np.sum((X-Xm)**2,axis=1)*np.sum((y-ym)**2))
-        r = r_num/r_den
+        r_num = np.sum((X - Xm) * (y - ym), axis=1)
+        r_den = np.sqrt(np.sum((X - Xm) ** 2, axis=1) * np.sum((y - ym) ** 2))
+        r = r_num / r_den
         return r
 
     def get_ica_map(ica, components=None):
-        if not components:
+        if components is None:
             components = range(ica.n_components_)
         maps = np.dot(ica.mixing_matrix_.T, ica.pca_components_)[components]
         return maps
@@ -118,96 +109,88 @@ def corrmap(icas, template,
         corr_polarities = [np.sign(a) for a in all_corrs]
 
         if threshold < 1:
-            max_corrs = [list(np.nonzero(s_corr>threshold))
+            max_corrs = [list(np.nonzero(s_corr > threshold))
                          for s_corr in abs_corrs]
         else:     
             max_corrs = [list(find_outliers(s_corr, threshold = threshold))
                          for s_corr in abs_corrs]
 
-        am = []
-        [am.extend(list(a[m])) for a, m in zip(abs_corrs, max_corrs)]
+        am = [sub[i] for l, i_s in zip(abs_corrs, max_corrs) 
+                                   for sub, i in zip(l, i_s)]
         median_corr_with_target = np.median(am)
 
-        polarities = []
-        [polarities.extend(list(a[m])) for a, m in zip(corr_polarities, max_corrs)]
-        
-        maxmaps = []
-        [maxmaps.extend(list(a[m])) for a, m in zip(all_maps, max_corrs)]
+        polarities = [sub[i] for l, i_s in zip(corr_polarities, max_corrs) 
+                                   for sub, i in zip(l, i_s)]
+
+        maxmaps = [sub[i] for l, i_s in zip(all_maps, max_corrs) 
+                                   for sub, i in zip(l, i_s)]
 
         try:
             newtarget = np.zeros(maxmaps[0].size)
             for maxmap, polarity in zip(maxmaps, polarities):
-                newtarget += maxmap*polarity
+                newtarget += maxmap * polarity
     
             newtarget /= len(maxmaps)
 
-            similarity_i_o = np.abs(np.corrcoef(target, newtarget)[1,0])
+            similarity_i_o = np.abs(np.corrcoef(target, newtarget)[1, 0])
 
             return newtarget, median_corr_with_target, similarity_i_o, max_corrs
         except:
             return [], 0, 0, []    
 
     if threshold == "auto":
-        threshold = [x/100 for x in range(60,95)]
+        threshold = np.arange(60, 95) / 100
 
     all_maps = [get_ica_map(ica) for ica in icas]        
         
     target = all_maps[template[0]][template[1]]
     
     # first run: use user-selected map
-    if isinstance(threshold, int) or isinstance(threshold, float):
+    if isinstance(threshold, (int, float)):
         nt, mt, s, mx = find_max_corrs(all_maps, target, threshold)
-    elif len(threshold)>1:
+    elif len(threshold) > 1:
         paths = [find_max_corrs(all_maps, target, t) for t in threshold]
         # find iteration with highest avg correlation with target
         nt, mt, s, mx = paths[np.argmax([path[2] for path in paths])]
-    else: print("run 1 failed")
 
     # second run: use output from first run
-    if isinstance(threshold, int) or isinstance(threshold, float):
+    if isinstance(threshold, (int, float)):
         nt, mt, s, mx = find_max_corrs(all_maps, nt, threshold)
-    elif len(threshold)>1:
+    elif len(threshold) > 1:
         paths = [find_max_corrs(all_maps, nt, t) for t in threshold]
         # find iteration with highest avg correlation with target
         nt, mt, s, mx = paths[np.argmax([path[1] for path in paths])]
-    else: print("run 2 failed")
-    
-    x = 0
+
     nones = []
     new_icas = []
-    print("Median correlation with constructed map: " + str(mt) 
-    if plot: print ("Displying selected ICs per subject."))
+    logger.info("Median correlation with constructed map: " + str(mt))
+    if plot: logger.info("Displying selected ICs per subject.")
 
-    for ica, max_corr in zip(icas, mx):
+    for i, (ica, max_corr) in enumerate(zip(icas, mx)):
         if inplace == False:
             ica = deepcopy(ica)
+        if not hasattr(ica, 'categorised_ics'):
+            ica.categorised_ics = defaultdict(lambda:[])
         try:
-#                print(ica.info["bads"])
-#                print(max_corr)
             if isinstance(max_corr[0], np.ndarray): max_corr = max_corr[0]
-            try:
-                ica.info["bads"] = list(max_corr) + ica.info["bads"]
-            except:
-                ica.info["bads"] = ica.info["bads"]
-            ica.info["bads"] = list(set(ica.info["bads"]))
-            print("Subject " + str(x))
-            if plot: ica.plot_components(max_corr, ch_type="eeg")
-#                print(ica.info["bads"])
-        except:
-            print("No map selected for subject " + str(x) + 
-                  ", consider a more liberal threshold.")
-            nones.append(x)
-        x += 1
+            ica.categorised_ics[name] = list(set(list(max_corr) + 
+                                                 ica.categorised_ics[name]))
+            if plot: 
+                logger.info("Subject " + str(i))
+                ica.plot_components(max_corr, ch_type="eeg")
+        except IndexError:
+            nones.append(i)
+            if plot: 
+                logger.info("No map selected for subject " + str(i) + 
+                             ", consider a more liberal threshold.")
         if inplace == False:
             new_icas.append(ica)
-    if nones: print("Subjects without any IC selected: ", nones)
-    else: print("At least 1 IC detected for each subject.")
+    if nones: logger.info("Subjects without any IC selected: ", nones)
+    else: logger.info("At least 1 IC detected for each subject.")
 
 
     if inplace == True:
         return
-    elif verbose_return:
-        return new_icas, mt, mx
     else:
         return new_icas
 
