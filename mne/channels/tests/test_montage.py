bc2cd0db44803de32b5d1afc220a0cd0acf28f74
teonlamont <teon@nyu.edu>
2015-03-12 18:28:39 +0100

    PEP8

--- mne/channels/tests/test_montage.py
+++ mne/channels/tests/test_montage.py
@@ -1,7 +1,6 @@
 import os.path as op
 
-from nose.tools import (assert_equal, assert_true, assert_false,
-                        assert_almost_equal)
+from nose.tools import assert_equal
 
 import numpy as np
 from numpy.testing import assert_array_equal, assert_array_almost_equal
