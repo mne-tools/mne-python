34ccf68ffc922b74ff8c26699af3238d91e36a43
teonlamont <teon@nyu.edu>
2015-03-12 13:39:41 +0100

    Remove scale ref

--- mne/channels/tests/test_montage.py
+++ mne/channels/tests/test_montage.py
@@ -4,7 +4,7 @@ from nose.tools import (assert_equal, assert_true, assert_false,
                         assert_almost_equal)
 
 import numpy as np
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_array_equal, assert_array_almost_equal
 
 from mne.channels import read_montage, apply_montage
 from mne.utils import _TempDir
