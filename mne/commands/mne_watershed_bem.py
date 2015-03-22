381963d687f8ef5c3b9752246d27bb3d72d42894
Lorenzo De Santis <desantis.lnz@gmail.com>
2015-03-22 18:26:15 +0100

    Fix docstring, Freesurfer environment variable check, os.path.join

--- mne/commands/mne_watershed_bem.py
+++ mne/commands/mne_watershed_bem.py
@@ -11,7 +11,8 @@ import shutil
 import mne
 
 
-def mne_watershed_bem(subject, subjects_dir, overwrite=False, volume='T1', atlas=False, gcaatlas=False, preflood=None):
+def mne_watershed_bem(subject=None, subjects_dir=None, overwrite=False,
+                      volume='T1', atlas=False, gcaatlas=False, preflood=None):
     """
     Create BEM surfaces using the watershed algorithm included with FreeSurfer
 
@@ -20,28 +21,43 @@ def mne_watershed_bem(subject, subjects_dir, overwrite=False, volume='T1', atlas
     subject : string
         Subject name (SUBJECT environment variable)
     subjects_dir : string
-        Directory containing subjects data (Freesurfer SUBJECTS_DIR environment variable)
-    overwrite
+        Directory containing subjects data. If None use the Freesurfer\
+         SUBJECTS_DIR environment variable.
+    overwrite : bool
         Write over existing files
-    volume
+    volume : string
         Defaults to T1
-    atlas
+    atlas : bool
         Specify the --atlas option for mri_watershed
-    gcaatlas
+    gcaatlas : bool
         Use the subcortical atlas
-    preflood
+    preflood : int
         Change the preflood height
     """
-    os.environ['SUBJECT'] = subject
-    os.environ['SUBJECTS_DIR'] = subjects_dir
+    if not os.environ['FREESURFER_HOME']:
+        raise RuntimeError('FREESURFER_HOME environment variable not set')
+    if subject:
+        os.environ['SUBJECT'] = subject
+    else:
+        try:
+            subject = os.environ['SUBJECT']
+        except KeyError:
+            raise RuntimeError('SUBJECT environment variable not defined')
+    if subjects_dir:
+        os.environ['SUBJECTS_DIR'] = subjects_dir
+    else:
+        try:
+            subjects_dir = os.environ['SUBJECTS_DIR']
+        except KeyError:
+            raise RuntimeError('SUBJECTS_DIR environment variable not defined')
     env = os.environ.copy()
 
-    subject_dir = subjects_dir+'/'+subject
-    mri_dir = subject_dir+'/mri'
-    T1_dir = mri_dir+'/'+volume
-    T1_mgz = mri_dir+'/'+volume+'.mgz'
-    bem_dir = subject_dir+'/bem'
-    ws_dir = subject_dir+'/bem/watershed'
+    subject_dir = os.path.join(subjects_dir, subject)
+    mri_dir = os.path.join(subject_dir, 'mri')
+    T1_dir = os.path.join(mri_dir, volume)
+    T1_mgz = os.path.join(mri_dir, volume+'.mgz')
+    bem_dir = os.path.join(subject_dir, 'bem')
+    ws_dir = os.path.join(subject_dir, 'bem', 'watershed')
 
     if not os.path.exists(subject_dir):
         raise RuntimeError('Could not find the MRI data directory "%s"'
@@ -67,16 +83,18 @@ def mne_watershed_bem(subject, subjects_dir, overwrite=False, volume='T1', atlas
     elif atlas:
         cmd += ['-atlas']
     if os.path.exists(T1_mgz):
-        cmd += ['-useSRAS', '-surf', ws_dir+'/'+subject, T1_mgz, ws_dir+'/ws']
+        cmd += ['-useSRAS', '-surf', os.path.join(ws_dir, subject), T1_mgz,
+                os.path.join(ws_dir, 'ws')]
     else:
-        cmd += ['-useSRAS', '-surf', ws_dir+'/'+subject, T1_dir, ws_dir+'/ws']
+        cmd += ['-useSRAS', '-surf', os.path.join(ws_dir, subject), T1_dir,
+                os.path.join(ws_dir, 'ws')]
     # report and run
     print("\nRunning mri_watershed for BEM segmentation with the following \
         parameters:\n")
     print("SUBJECTS_DIR = %s" % subjects_dir)
     print("SUBJECT = %s" % subject)
     print("Result dir = %s\n" % ws_dir)
-    os.makedirs(ws_dir+'/ws')
+    os.makedirs(os.path.join(ws_dir, 'ws'))
     mne.utils.run_subprocess(cmd, env=env, stdout=sys.stdout)
     #
     os.chdir(ws_dir)
@@ -91,23 +109,25 @@ def mne_watershed_bem(subject, subjects_dir, overwrite=False, volume='T1', atlas
     os.chdir(bem_dir)
     if os.path.exists(subject+'-head.fif'):
         os.remove(subject+'-head.fif')
-    cmd = ['mne_surf2bem', '--surf', ws_dir+'/'+subject+'_outer_skin_surface',
-           '--id', '4', '--fif', subject+'-head.fif']
+    cmd = ['mne_surf2bem', '--surf', os.path.join(ws_dir,
+           subject+'_outer_skin_surface'), '--id', '4', '--fif',
+           subject+'-head.fif']
     mne.utils.run_subprocess(cmd, env=env, stdout=sys.stdout)
     print("Created %s/%s-head.fif" % (bem_dir, subject))
     print("\nComplete.")
 
 
 def run():
-    subject = os.environ['SUBJECT']
-    subjects_dir = os.environ['SUBJECTS_DIR']
+    subject = None
+    subjects_dir = None
     overwrite = True
     volume = 'T1'
     atlas = False
     gcaatlas = False
     preflood = None
-    mne_watershed_bem(subject, subjects_dir, overwrite=overwrite, volume=volume,
-                      atlas=atlas, gcaatlas=gcaatlas, preflood=preflood)
+    mne_watershed_bem(subject=subject, subjects_dir=subjects_dir,
+                      overwrite=overwrite, volume=volume, atlas=atlas,
+                      gcaatlas=gcaatlas, preflood=preflood)
 
 is_main = (__name__ == '__main__')
 if is_main:
