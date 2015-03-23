2c1b77838c32f5feca6843dedfdf2eae59618792
dlsrnsi <dlsrnsladlek@naver.com>
2015-03-23 21:59:01 +0900

    fix_stim_artifact that can take Epochs or Evoked as input. rename eliminate_stim_artifact to fix_stim_artifact_raw with deprecation. #1773

--- mne/preprocessing/tests/test_stim.py
+++ mne/preprocessing/tests/test_stim.py
@@ -9,15 +9,17 @@ from numpy.testing import assert_array_almost_equal
 from nose.tools import assert_true
 
 from mne.io import Raw
+from mne.io.pick import pick_types
 from mne.event import read_events
-from mne.preprocessing.stim import eliminate_stim_artifact
+from mne.epochs import Epochs
+from ..stim import fix_stim_artifact_raw, fix_stim_artifact
 
 data_path = op.join(op.dirname(__file__), '..', '..', 'io', 'tests', 'data')
 raw_fname = op.join(data_path, 'test_raw.fif')
 event_fname = op.join(data_path, 'test-eve.fif')
 
 
-def test_stim_elim():
+def test_stim_fix_raw():
     """Test eliminate stim artifact"""
     raw = Raw(raw_fname, preload=True)
     events = read_events(event_fname)
@@ -30,13 +32,13 @@ def test_stim_elim():
     test_tminidx = int(-0.01 * raw.info['sfreq'])
     test_tmaxidx = int(0.01 * raw.info['sfreq'])
 
-    raw = eliminate_stim_artifact(raw, events, event_id=1, tmin=tmin,
+    raw = fix_stim_artifact_raw(raw, events, event_id=1, tmin=tmin,
                                   tmax=tmax, mode='linear')
     data, times = raw[:, (tidx + test_tminidx):(tidx + test_tmaxidx)]
     diff_data0 = np.diff(data[0])
     diff_data0 -= np.mean(diff_data0)
     assert_array_almost_equal(diff_data0, np.zeros(len(diff_data0)))
-    raw = eliminate_stim_artifact(raw, events, event_id=1, tmin=tmin,
+    raw = fix_stim_artifact_raw(raw, events, event_id=1, tmin=tmin,
                                   tmax=tmax, mode='window')
     data, times = raw[:, (tidx + test_tminidx):(tidx + test_tmaxidx)]
     assert_true(np.all(data) == 0.)
@@ -47,13 +49,13 @@ def test_stim_elim():
     test_tminidx = int(-0.035 * raw.info['sfreq'])
     test_tmaxidx = int(-0.015 * raw.info['sfreq'])
 
-    raw = eliminate_stim_artifact(raw, events, event_id=1, tmin=tmin,
+    raw = fix_stim_artifact_raw(raw, events, event_id=1, tmin=tmin,
                                   tmax=tmax, mode='linear')
     data, times = raw[:, (tidx + test_tminidx):(tidx + test_tmaxidx)]
     diff_data0 = np.diff(data[0])
     diff_data0 -= np.mean(diff_data0)
     assert_array_almost_equal(diff_data0, np.zeros(len(diff_data0)))
-    raw = eliminate_stim_artifact(raw, events, event_id=1, tmin=tmin,
+    raw = fix_stim_artifact_raw(raw, events, event_id=1, tmin=tmin,
                                   tmax=tmax, mode='window')
     data, times = raw[:, (tidx + test_tminidx):(tidx + test_tmaxidx)]
     assert_true(np.all(data) == 0.)
@@ -64,18 +66,19 @@ def test_stim_elim():
     test_tminidx = int(0.015 * raw.info['sfreq'])
     test_tmaxidx = int(0.035 * raw.info['sfreq'])
 
-    raw = eliminate_stim_artifact(raw, events, event_id=1, tmin=tmin,
+    raw = fix_stim_artifact_raw(raw, events, event_id=1, tmin=tmin,
                                   tmax=tmax, mode='linear')
     data, times = raw[:, (tidx + test_tminidx):(tidx + test_tmaxidx)]
     diff_data0 = np.diff(data[0])
     diff_data0 -= np.mean(diff_data0)
     assert_array_almost_equal(diff_data0, np.zeros(len(diff_data0)))
-    raw = eliminate_stim_artifact(raw, events, event_id=1, tmin=tmin,
+    raw = fix_stim_artifact_raw(raw, events, event_id=1, tmin=tmin,
                                   tmax=tmax, mode='window')
     data, times = raw[:, (tidx + test_tminidx):(tidx + test_tmaxidx)]
     assert_true(np.all(data) == 0.)
 
-def test_stim_elim_evoked():
+
+def test_stim_fix_evoked():
     """Test eliminate stim artifact"""
     raw = Raw(raw_fname, preload=True)
     events = read_events(event_fname)
@@ -93,6 +96,6 @@ def test_stim_elim_evoked():
     assert_true(np.all(data) == 0.)
 
     # use window after stimulus
-    evoked = eliminate_stim_artifact_evoked(evoked, mode='window')
+    evoked = fix_stim_artifact(evoked, mode='window')
     data = evoked.data[:, :]
     assert_true(np.all(data) == 0.)
