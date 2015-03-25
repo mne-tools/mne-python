b1ccec20b7dc12df377a8e0148ef11231d6bb49b
Jaakko Leppakangas <jaeilepp@student.jyu.fi>
2015-03-25 11:56:53 -0400

    Fixes to example on creating MNE objects from arrays. Added construction of Raw object from NEO.

---
+++ examples/plot_objects_from_arrays.py
@@ -0,0 +1,111 @@
+"""
+=====================================
+Creating MNE objects from data arrays
+=====================================
+
+In this simple example, the creation of MNE objects from
+numpy arrays is demonstrated. In the last example case, a
+NEO file format is used as a source for the data.
+"""
+# Author: Jaakko Leppakangas <jaeilepp@student.jyu.fi>
+#
+# License: BSD (3-clause)
+
+import mne
+
+import numpy as np
+
+print(__doc__)
+
+sfreq = 1000  # Sampling frequency
+times = np.arange(0, 10, 0.001)  # Use 10000 samples (10s)
+
+sin = np.sin(times)
+cos = np.cos(times)
+sinX2 = sin * 2
+cosX2 = cos * 2
+
+data = np.array([sin, cos, sinX2, cosX2])  # Matrix of size 4 X 10000
+
+ch_names = ['sin', 'cos', 'sinX2', 'cosX2']
+
+# Creation of info dictionary.
+# It is also possible to use info from another raw object.
+info = mne.create_info(ch_names=ch_names, sfreq=sfreq)
+
+raw = mne.io.RawArray(data, info)
+
+# Scaling of the figure.
+# For actual EEG/MEG data a different scaling factor should be used.
+scalings = {'misc': 2}
+
+raw.plot(n_channels=4, scalings=scalings, title='Data created from arrays',
+         show=True, block=True)
+
+
+###############################################################################
+# EpochsArray
+
+event_id = 1
+events = np.array([[200, 0, event_id],
+                   [1200, 0, event_id],
+                   [2000, 0, event_id]])  # List of three arbitrary events
+
+# Here a data set of 700 ms epochs from 2 channels is
+# created from sin and cos data.
+# Any data in shape (n_epochs, n_channels, n_times) can be used.
+epochs_data = [[sin[:700], cos[:700]],
+               [sin[1000:1700], cos[1000:1700]],
+               [sin[1800:2500], cos[1800:2500]]]
+
+info = mne.create_info(ch_names=['sin', 'cos'], sfreq=sfreq)
+
+epochs = mne.EpochsArray(epochs_data, info=info, events=events,
+                         event_id={'arbitrary': 1})
+
+picks = mne.pick_types(info, meg=False, eeg=False, misc=True)
+
+epochs.plot(picks=picks, show=True, block=True)
+
+
+###############################################################################
+# EvokedArray
+
+nave = len(epochs_data)  # Number of averaged epochs
+evoked_data = np.mean(epochs_data, axis=0)
+
+evokeds = mne.EvokedArray(evoked_data, info=info, tmin=-0.2,
+                          comment='Arbitrary', nave=nave)
+evokeds.plot(picks=picks, show=True, units={'misc': '-'},
+             titles={'misc': 'sin and cos'})
+
+
+###############################################################################
+# Extracting data from NEO file
+
+import neo
+
+# The example here uses the ExampleIO object for creating fake data.
+# For actual data and different file formats, consult the NEO documentation.
+reader = neo.io.ExampleIO('fakedata.nof')
+bl = reader.read(cascade=True, lazy=False)[0]
+
+# Get data from first (and only) segment
+seg = bl.segments[0]
+title = seg.file_origin
+
+data = []
+for asig in seg.analogsignals:
+    asig = asig.rescale('V').magnitude
+    data.append(asig)
+
+# data is now a stored in an array of 4 channels.
+ch_names = ['1', '2', '3', '4']
+sfreq = int(seg.analogsignals[0].sampling_rate.magnitude)
+
+# By default, the channel types are assumed to be 'misc'.
+info = mne.create_info(ch_names=ch_names, sfreq=sfreq)
+
+raw = mne.io.RawArray(data, info)
+raw.plot(n_channels=4, scalings={'misc': 1}, title='Data from NEO',
+         show=True, block=True, clipping='clamp')
