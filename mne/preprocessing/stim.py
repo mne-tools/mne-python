bf256f5ad985a7cedca2020d689cd004ef137632
dlsrnsi <dlsrnsladlek@naver.com>
2015-03-31 00:09:08 +0900

    Change stim.py Not change things about "window" #1773

--- mne/preprocessing/stim.py
+++ mne/preprocessing/stim.py
@@ -15,9 +15,21 @@ from ..io.pick import pick_channels
 
 def _get_window(start, end):
     window = 1 - np.r_[signal.hann(4)[:2],
-                           np.ones(np.abs(end - start) - 4),
-                           signal.hann(4)[-2:]].T
+                       np.ones(np.abs(end - start) - 4),
+                       signal.hann(4)[-2:]].T
     return window
+
+
+def _fix_artifact(orig, data, window, picks, first_samp, last_samp, mode):
+    if mode == 'linear':
+        x = np.array([first_samp, last_samp])
+        f = interpolate.interp1d(x, data[:, (0, -1)])
+        xnew = np.arange(first_samp, last_samp)
+        interp_data = f(xnew)
+        orig[picks, first_samp:last_samp] = interp_data
+    if mode == 'window':
+        orig[picks, first_samp:last_samp] = \
+            data[picks, first_samp:last_samp] * window[np.newaxis, :]
 
 
 @deprecated('`eliminate_stim_artifact` will be deprecated '
@@ -81,8 +93,8 @@ def eliminate_stim_artifact(raw, events, event_id, tmin=-0.005,
     return raw
 
 
-def fix_stim_artifact(inst, events=None, event_id=None, tmin=-0.2,
-                      tmax=-0.1, mode='linear'):
+def fix_stim_artifact(inst, events=None, event_id=None, tmin=0.,
+                      tmax=0.01, mode='linear', copy=False):
     """Eliminates stimulations artifacts from instance
 
     The instance will be modified in place (no copy)
@@ -104,45 +116,44 @@ def fix_stim_artifact(inst, events=None, event_id=None, tmin=-0.2,
         way to fill the artifacted time interval.
         'linear' does linear interpolation
         'window' applies a (1 - hanning) window.
+    copy : bool
+        If False inst is cropped in place.
 
     Returns
     -------
     inst : instance of evoked or epochs
         instance with modified data
     """
+    if copy:
+        inst = inst.copy()
     s_start = int(np.ceil(inst.info['sfreq'] * tmin))
     s_end = int(np.ceil(inst.info['sfreq'] * tmax))
     if (s_end - s_start - 4) < 0:
-        raise ValueError('Time between tmin and tmax is to short'
-                         'to fix artifact. Input longer values')
+        raise ValueError('Time between tmin and tmax is too short'
+                         ' to fix artifact. Input longer values')
+    window = None
     if mode == 'window':
         window = _get_window(s_start, s_end)
+    ch_names = inst.info['ch_names']
+    picks = pick_channels(ch_names, ch_names)
 
     if isinstance(inst, Raw):
-        picks = pick_types(inst.info, meg=True, eeg=True, eog=True, ecg=True,
-                           emg=True, ref_meg=True, misc=True, chpi=True,
-                           exclude='bads', stim=False, resp=False)
         if inst.preload is False:
             raise RuntimeError('Modifying data of Raw is only supported '
                                'when preloading is used. Use preload=True '
                                '(or string) in the constructor.')
         events_sel = (events[:, 2] == event_id)
         event_start = events[events_sel, 0]
+        if mode is 'linear':
+            data, _ = inst[picks, :]
+        elif mode is 'window':
+            data, _ = inst[:, :]
         for k in range(len(event_start)):
             first_samp = int(event_start[k]) - inst.first_samp + s_start
             last_samp = int(event_start[k]) - inst.first_samp + s_end
-            data, _ = inst[picks, first_samp:last_samp]
-            if mode == 'linear':
-                x = np.array([first_samp, last_samp])
-                f = interpolate.interp1d(x, data[:, (0, -1)])
-                xnew = np.arange(first_samp, last_samp)
-                interp_data = f(xnew)
-                inst[picks, first_samp:last_samp] = interp_data
-            elif mode == 'window':
-                inst[picks, first_samp:last_samp] = data * window[np.newaxis, :]
+            _fix_artifact(inst, data, window, picks, first_samp, last_samp, mode)
 
     elif isinstance(inst, Epochs):
-        picks = pick_channels(inst.info['ch_names'], inst.info['ch_names'])
         if inst.preload is False:
             raise RuntimeError('Modifying data of Epochs is only supported '
                                'when preloading is used. Use preload=True '
@@ -154,33 +165,16 @@ def fix_stim_artifact(inst, events=None, event_id=None, tmin=-0.2,
         first_samp = s_start - e_start
         last_samp = s_end - e_start
         data = inst.get_data()[:, picks, :]
-        for epoch in data:
-            if mode == 'linear':
-                x = np.array([first_samp, last_samp])
-                f = interpolate.interp1d(x, epoch[:, (0, -1)])
-                xnew = np.arange(first_samp, last_samp)
-                interp_data = f(xnew)
-                epoch[picks, first_samp:last_samp] = interp_data
-            if mode == 'window':
-                epoch[picks, first_samp:last_samp] = \
-                    epoch[picks, first_samp:last_samp] * window[np.newaxis, :]
-        inst._data = data
+        for epoch, k in data, range(len(data)):
+            _fix_artifact(inst._data[k], epoch, window, picks, first_samp, last_samp, mode)
 
     elif isinstance(inst, Evoked):
-        picks = pick_channels(inst.info['ch_names'], inst.info['ch_names'])
         first_samp = s_start - inst.first
         last_samp = s_end - inst.first
         data = inst.data
-        if mode == 'window':
-            data[picks, first_samp:last_samp] = \
-                data[picks, first_samp:last_samp] * window[np.newaxis, :]
-        elif mode == 'linear':
-            x = np.array([first_samp, last_samp])
-            f = interpolate.interp1d(x, inst.data[:, (0, -1)])
-            xnew = np.arange(first_samp, last_samp)
-            interp_data = f(xnew)
-            data[picks, first_samp:last_samp] = interp_data
+        _fix_artifact(inst.data, data, window, picks, first_samp, last_samp, mode)
 
     else:
         raise TypeError('Not a Epochs or Evoked or Raw')
+
     return inst
