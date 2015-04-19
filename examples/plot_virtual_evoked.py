d001c8b32b6a1985f4486cf2ff2b98751324708f
Mainak Jas <mainakjas@gmail.com>
2015-04-19 02:09:43 +0200

    simplify example

--- examples/plot_virtual_evoked.py
+++ examples/plot_virtual_evoked.py
@@ -15,15 +15,7 @@ for magnetometer and gradiometers.
 import mne
 from mne.datasets import sample
 
-import matplotlib.pyplot as plt
-
 print(__doc__)
-
-
-def add_title(title):
-    """Add nice titles."""
-    plt.suptitle(title, verticalalignment='top', size='x-large')
-    plt.gcf().set_size_inches(12, 2, forward=True)
 
 # read the evoked
 data_path = sample.data_path()
@@ -32,16 +24,12 @@ evoked = mne.read_evokeds(fname, condition='Left Auditory', baseline=(None, 0))
 
 # go from grad + mag to mag
 virt_evoked = evoked.as_type('mag')
-evoked.plot_topomap(ch_type='mag')
-add_title('mag (original)')
-virt_evoked.plot_topomap(ch_type='mag')
-add_title('mag (interpolated from mag + grad)')
+evoked.plot_topomap(ch_type='mag', title='mag (original)')
+virt_evoked.plot_topomap(ch_type='mag',
+                         title='mag (interpolated from mag + grad)')
 
 # go from grad + mag to grad
 virt_evoked = evoked.as_type('grad')
-evoked.plot_topomap(ch_type='grad')
-add_title('grad (original)')
-virt_evoked.plot_topomap(ch_type='grad')
-add_title('grad (interpolated from mag + grad)')
-
-plt.show()
+evoked.plot_topomap(ch_type='grad', title='grad (original)')
+virt_evoked.plot_topomap(ch_type='grad',
+                         title='grad (interpolated from mag + grad)')
