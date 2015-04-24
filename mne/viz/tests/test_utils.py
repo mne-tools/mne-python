0ae48858ac488fb950fc19465a3bb3196f619528
Chris Holdgraf <choldgraf@berkeley.edu>
2015-04-24 11:09:20 -0700

    Added tests for ClickableObject

--- mne/viz/tests/test_utils.py
+++ mne/viz/tests/test_utils.py
@@ -4,8 +4,12 @@
 
 import os.path as op
 import warnings
+import numpy as np
 
-from mne.viz.utils import compare_fiff
+from mne.viz.utils import compare_fiff, ClickableImage
+from numpy.testing import assert_allclose
+from nose.tools import assert_true
+from test_raw import _fake_click
 
 # Set our plotters to test mode
 import matplotlib
@@ -24,3 +28,15 @@ def test_compare_fiff():
     import matplotlib.pyplot as plt
     compare_fiff(raw_fname, cov_fname, read_limit=0, show=False)
     plt.close('all')
+
+
+def test_clickable_image():
+    """Test the ClickableImage class."""
+    # Gen data and create clickable image
+    im = np.random.randn(100, 100)
+    clk = ClickableImage(im)
+    clicks = [(12, 8), (46, 48), (10, 24)]
+    for click in clicks:
+        _fake_click(clk.fig, clk.ax, click, xform='data')
+    assert_allclose(np.array(clicks), np.array(clk.coords))
+    assert_true(len(clicks) == len(clk.coords))
