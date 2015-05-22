de4af15fedaeca63bd6a645800e1a256d4d07961
Alexandre Barachant <alexandre.barachant@gmail.com>
2015-05-22 10:27:49 -0400

    add test and data

---
+++ mne/io/gdf/tests/test_gdf.py
@@ -0,0 +1,44 @@
+"""Data Equivalence Tests"""
+from __future__ import print_function
+
+# Authors: Teon Brooks <teon.brooks@gmail.com>
+#          Martin Billinger <martin.billinger@tugraz.at>
+#
+# License: BSD (3-clause)
+
+import os.path as op
+import inspect
+import warnings
+
+from nose.tools import assert_equal, assert_true
+from numpy.testing import assert_array_almost_equal, assert_array_equal
+from numpy.testing import assert_raises, assert_allclose
+import numpy as np
+
+from mne.io.gdf import read_raw_gdf
+from mne import pick_types
+
+warnings.simplefilter('always')
+
+FILE = inspect.getfile(inspect.currentframe())
+data_dir = op.join(op.dirname(op.abspath(FILE)), 'data')
+gdf_path = op.join(data_dir, 'test.gdf')
+gdf_biosig_path = op.join(data_dir, 'test_gdf_biosig.npy')
+
+
+
+def test_gdf_data():
+    """Test reading raw gdf files
+    """
+    raw_py = read_raw_gdf(gdf_path, eog=None,
+                          misc=None, preload=True, stim_channel=None)
+    picks = pick_types(raw_py.info, meg=False, eeg=True, exclude='bads')
+    data_py, _ = raw_py[picks]
+
+    # this .mat was generated using the EEG Lab Biosemi Reader
+    raw_biosig = np.load(gdf_biosig_path)
+    print(raw_biosig.shape)
+    raw_biosig = raw_biosig * 1e-6  # data are stored in microvolts
+    data_biosig = raw_biosig[picks]
+    # bdf saved as a single, resolution to seven decimal points in matlab
+    assert_array_almost_equal(data_py, data_biosig, 8)
