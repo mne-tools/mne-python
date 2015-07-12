15a4f1799ffbb4bad4c24b38768f3fe09d1ca1bd
Eric Larson <larson.eric.d@gmail.com>
2015-07-12 11:02:27 -0400

    FIX: Fix spherical harmonics

--- mne/preprocessing/tests/test_maxwell.py
+++ mne/preprocessing/tests/test_maxwell.py
@@ -12,8 +12,16 @@ from nose.tools import assert_true, assert_raises
 from mne.preprocessing import maxwell
 from mne.datasets import testing
 from mne.io import Raw, proc_history
-from mne.utils import slow_test, _TempDir
+from mne.utils import _TempDir, run_tests_if_main
+
 warnings.simplefilter('always')  # Always throw warnings
+
+data_path = op.join(testing.data_path(download=False))
+raw_fname = op.join(data_path, 'SSS', 'test_move_anon_raw.fif')
+sss_std_fname = op.join(data_path, 'SSS',
+                        'test_move_anon_raw_simp_stdOrigin_sss.fif')
+sss_nonstd_fname = op.join(data_path, 'SSS',
+                           'test_move_anon_raw_simp_nonStdOrigin_sss.fif')
 
 
 @testing.requires_testing_data
@@ -23,16 +31,6 @@ def test_maxwell_filter():
     # TODO: Future tests integrate with mne/io/tests/test_proc_history
 
     # Load testing data (raw, SSS std origin, SSS non-standard origin)
-    data_path = op.join(testing.data_path(download=False))
-
-    file_name = 'test_move_anon'
-
-    raw_fname = op.join(data_path, 'SSS', file_name + '_raw.fif')
-    sss_std_fname = op.join(data_path, 'SSS', file_name +
-                            '_raw_simp_stdOrigin_sss.fif')
-    sss_nonStd_fname = op.join(data_path, 'SSS', file_name +
-                               '_raw_simp_nonStdOrigin_sss.fif')
-
     with warnings.catch_warnings(record=True):  # maxshield
         raw = Raw(raw_fname, preload=False, proj=False,
                   allow_maxshield=True).crop(0., 1., False)
@@ -40,7 +38,7 @@ def test_maxwell_filter():
     with warnings.catch_warnings(record=True):  # maxshield, naming
         sss_std = Raw(sss_std_fname, preload=True, proj=False,
                       allow_maxshield=True)
-        sss_nonStd = Raw(sss_nonStd_fname, preload=True, proj=False,
+        sss_nonStd = Raw(sss_nonstd_fname, preload=True, proj=False,
                          allow_maxshield=True)
         raw_err = Raw(raw_fname, preload=False, proj=True,
                       allow_maxshield=True).crop(0., 0.1, False)
@@ -101,6 +99,29 @@ def test_maxwell_filter():
     error_rms = np.sqrt(np.mean(error ** 2, axis=1))
     assert_true(np.mean(bench_rms / error_rms) > 1000, 'SNR < 1000')
 
+    # Check against SSS functions from proc_history
+    sss_info = raw_sss.info['proc_history'][0]['max_info']
+    assert_equal(maxwell.get_num_moments(int_order, 0),
+                 proc_history._get_sss_rank(sss_info))
+
+
+@testing.requires_testing_data
+def test_maxwell_filter_additional():
+    """Test processing of Maxwell filtered data"""
+
+    # TODO: Future tests integrate with mne/io/tests/test_proc_history
+
+    # Load testing data (raw, SSS std origin, SSS non-standard origin)
+    data_path = op.join(testing.data_path(download=False))
+
+    file_name = 'test_move_anon'
+
+    raw_fname = op.join(data_path, 'SSS', file_name + '_raw.fif')
+    with warnings.catch_warnings(record=True):  # maxshield
+        raw = Raw(raw_fname, preload=False, proj=False,
+                  allow_maxshield=True).crop(0., 1., False)
+    raw_sss = maxwell.maxwell_filter(raw)
+
     # Test io on processed data
     tempdir = _TempDir()
     test_outname = op.join(tempdir, 'test_raw_sss.fif')
@@ -111,7 +132,7 @@ def test_maxwell_filter():
     assert_allclose(raw_sss_loaded._data[:, :], raw_sss._data[:, :],
                     rtol=1e-6, atol=1e-20)
 
-    # Check against SSS functions from proc_history
-    sss_info = raw_sss.info['proc_history'][0]['max_info']
-    assert_equal(maxwell.get_num_moments(int_order, 0),
-                 proc_history._get_sss_rank(sss_info))
+    # Test covariance calculation XXX add this
+
+
+run_tests_if_main()
