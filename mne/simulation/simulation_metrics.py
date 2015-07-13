17fbeda1384cc40dc8751b6d9634f906ca7ed9cd
Yousra Bekhti <yousra.bekhti@gmail.com>
2015-07-13 12:00:35 +0200

    1st draft on simu_eval

---
+++ mne/simulation/simulation_metrics.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+
+
+def _check_stc(stc1, stc2):
+    # XXX What should we check? that the data is having the same size?
+    if stc1.data.shape != stc2.data.shape:
+        raise ValueError('data in stcs must have the same size')
+
+
+def source_estimate_quantification(stc1, stc2, metric='rms'):
+
+    _check_stc(stc1, stc2)
+
+    if metric == 'rms':
+        return np.sum((stc1.data - stc2.data) ** 2)
