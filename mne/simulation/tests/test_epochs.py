cce61b86b643de333f32ccb34a2f63382219cb6f
Yousra Bekhti <yousra.bekhti@gmail.com>
2015-07-14 19:56:50 +0200

    Fix the timing problem

--- mne/simulation/tests/test_epochs.py
+++ mne/simulation/tests/test_epochs.py
@@ -8,17 +8,17 @@ import os.path as op
 
 import numpy as np
 from numpy.testing import assert_array_almost_equal
-from nose.tools import assert_true, assert_raises
+from nose.tools import assert_true  # , assert_raises
 import warnings
 
 from mne.datasets import testing
-from mne import read_label, read_forward_solution, create_info
-from mne.time_frequency import morlet
+from mne import read_forward_solution  # , read_label
+# from mne.time_frequency import morlet
 from mne.simulation import simulate_sparse_stc, generate_epochs
 from mne.forward import restrict_forward_to_stc
 from mne import read_cov
 from mne.io import Raw
-from mne import pick_types_forward, read_evokeds
+from mne import pick_types_forward
 from mne.event import read_events
 
 warnings.simplefilter('always')
@@ -26,14 +26,15 @@ warnings.simplefilter('always')
 data_path = testing.data_path(download=False)
 fwd_fname = op.join(data_path, 'MEG', 'sample',
                     'sample_audvis_trunc-meg-eeg-oct-6-fwd.fif')
-raw_fname = op.join(op.dirname('__file__'), '..', '..', 'io', 'tests', 'data',
+raw_fname = op.join(op.dirname(__file__), '..', '..', 'io', 'tests', 'data',
                     'test_raw.fif')
-ave_fname = op.join(op.dirname('__file__'), '..', '..', 'io', 'tests', 'data',
+ave_fname = op.join(op.dirname(__file__), '..', '..', 'io', 'tests', 'data',
                     'test-ave.fif')
-cov_fname = op.join(op.dirname('__file__'), '..', '..', 'io', 'tests', 'data',
+cov_fname = op.join(op.dirname(__file__), '..', '..', 'io', 'tests', 'data',
                     'test-cov.fif')
-eve_name =  op.join(op.dirname('__file__'), '..', '..', 'io', 'tests', 'data',
-                    'test-eve.fif')
+eve_name = op.join(op.dirname(__file__), '..', '..', 'io', 'tests', 'data',
+                   'test-eve.fif')
+
 
 @testing.requires_testing_data
 def test_simulate_epochs():
@@ -45,18 +46,18 @@ def test_simulate_epochs():
     fwd = pick_types_forward(fwd, meg=True, eeg=True, exclude=raw.info['bads'])
     cov = read_cov(cov_fname)
     eve = read_events(eve_name)
-    label_names = ['Aud-lh', 'Aud-rh']
-    labels = [read_label(op.join(data_path, 'MEG', 'sample', 'labels',
-                            '%s.label' % label)) for label in label_names]
+    # label_names = ['Aud-lh', 'Aud-rh']
+    # labels = [read_label(op.join(data_path, 'MEG', 'sample', 'labels',
+    #                      '%s.label' % label)) for label in label_names]
 
     n_epochs = 3
     snr = 5  # dB
     tmin = -0.1
     sfreq = raw.info['sfreq']  # Hz
-    tstep = 1. / sfreq
     n_samples = 600.
-    n_channels = fwd['nchan']
-    times = np.linspace(tmin, tmin + n_samples * tstep, n_samples)
+
+    times = np.linspace(tmin, tmin + (n_samples - 1) / sfreq, n_samples)
+    times -= times[np.where(times <= 0)[0][-1]]
     eve = eve[:n_epochs, :]
 
     stc = simulate_sparse_stc(fwd['src'], n_dipoles=1, times=times)
@@ -65,12 +66,7 @@ def test_simulate_epochs():
     epochs = generate_epochs(fwd, [stc] * n_epochs, raw.info, cov, eve, snr)
 
     # TODO: Figure out why timing gets slightly off
-    #assert_array_almost_equal(epochs.times, stc.times)
-    assert_true(len(epochs[0].data) == len(fwd['sol']['data']))
-
-    # make a vertex that doesn't exist in fwd, should throw error
-    stc_bad = stc.copy()
-    mv = np.max(fwd['src'][0]['vertno'][fwd['src'][0]['inuse']])
-    stc_bad.vertices[0][0] = mv + 1
-    assert_raises(RuntimeError, generate_evoked, fwd, stc_bad,
-                    info, cov, snr, tmin=0.0, tmax=0.2)
+    assert_array_almost_equal(epochs.times, stc.times)
+    assert_true(len(epochs) == n_epochs)
+    assert_true(epochs[0].get_data().shape[1] == len(fwd['sol']['data']))
+    assert_true(epochs[0].get_data().shape[2] == len(stc.times))
