6a38dd2702a84d0563b2d321f51151c39ef731d1
Jaakko Leppakangas <jaeilepp@student.jyu.fi>
2015-07-14 13:04:46 +0300

    Changed param order.

--- mne/time_frequency/tfr.py
+++ mne/time_frequency/tfr.py
@@ -605,7 +605,7 @@ class AverageTFR(ContainsMixin, PickDropChannelsMixin):
     def plot(self, picks=None, baseline=None, mode='mean', tmin=None,
              tmax=None, fmin=None, fmax=None, vmin=None, vmax=None,
              cmap='RdBu_r', dB=False, colorbar=True, show=True,
-             title=None, verbose=None, axes=None):
+             title=None, axes=None, verbose=None):
         """Plot TFRs in a topography with images
 
         Parameters
@@ -655,21 +655,17 @@ class AverageTFR(ContainsMixin, PickDropChannelsMixin):
             Call pyplot.show() at the end.
         title : str | None
             String for title. Defaults to None (blank/no title).
-        verbose : bool, str, int, or None
-            If not None, override default verbose level (see mne.verbose).
         axes : instance of Axes | list | None
             The axes to plot to. If list, the list must be a list of Axes of
             the same length as the number of channels. If instance of Axes,
             there must be only one channel plotted.
+        verbose : bool, str, int, or None
+            If not None, override default verbose level (see mne.verbose).
 
         Returns
         -------
         fig : matplotlib.figure.Figure
             The figure containing the topography.
-
-        Notes
-        -----
-        .. versionadded:: 0.10.0
         """
         from ..viz.topo import _imshow_tfr
         import matplotlib.pyplot as plt
