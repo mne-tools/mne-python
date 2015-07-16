cd353d6a5656a4d54e7a4dadea86029ed1bdd980
wronk <wronk.mark@gmail.com>
2015-07-16 12:11:58 +0200

    Cleanup epochs

--- mne/simulation/__init__.py
+++ mne/simulation/__init__.py
@@ -3,8 +3,6 @@
 
 from .evoked import generate_evoked, generate_noise_evoked, add_noise_evoked
 
-from .epochs import generate_epochs, generate_noise_epochs, add_noise_epochs
-
 from .raw import simulate_raw
 
 from .source import (select_source_in_label, generate_sparse_stc, generate_stc,
