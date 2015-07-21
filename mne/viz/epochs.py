08a0674ab041f5d85c82bedbdb37f50efdc0a68d
Alan Leggitt <leggitta3@gmail.com>
2015-07-21 16:14:04 -0700

    fixed the x-limit for the evoked trace in mne.viz.plot_image_epochs

--- mne/viz/epochs.py
+++ mne/viz/epochs.py
@@ -130,6 +130,7 @@ def plot_image_epochs(epochs, picks=None, sigma=0., vmin=None,
         ax1.axvline(0, color='m', linewidth=3, linestyle='--')
         ax2.plot(1e3 * evoked.times, scalings[ch_type] * evoked.data[i])
         ax2.set_xlabel('Time (ms)')
+        ax2.set_xlim([1e3 * evoked.times[0], 1e3 * evoked.times[-1]])
         ax2.set_ylabel(units[ch_type])
         ax2.set_ylim([vmin, vmax])
         ax2.axvline(0, color='m', linewidth=3, linestyle='--')
