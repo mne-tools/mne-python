e67ee1c9b30891de1c1f885f923a75754c47e555
Romain Trachel <trachelr@gmail.com>
2015-07-21 19:16:28 +0200

    cleaning rebasing mess up

--- mne/decoding/__init__.py
+++ mne/decoding/__init__.py
@@ -1,9 +1,7 @@
 from .transformer import Scaler, FilterEstimator
 from .transformer import PSDEstimator, ConcatenateChannels
-from .classifier import LinearClassifier
-from .regressor import LinearRegressor
 from .mixin import TransformerMixin
-from .base import BaseEstimator
+from .base import BaseEstimator, LinearModel
 from .csp import CSP
 from .ems import compute_ems
 from .time_gen import GeneralizationAcrossTime, TimeDecoding
