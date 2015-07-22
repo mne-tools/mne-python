c960bf6b3ef0335522845a8cf94ba3900303c1d3
wronk <wronk.mark@gmail.com>
2015-07-22 14:55:18 +0200

    ENH: Initial Commit. Add option to round time_as_index func

--- mne/io/base.py
+++ mne/io/base.py
@@ -1368,7 +1368,7 @@ class _BaseRaw(ProjMixin, ContainsMixin, PickDropChannelsMixin,
                             area_alpha=area_alpha, n_overlap=n_overlap,
                             dB=dB, show=show, n_jobs=n_jobs)
 
-    def time_as_index(self, times, use_first_samp=False):
+    def time_as_index(self, times, use_first_samp=False, use_rounding=False):
         """Convert time to indices
 
         Parameters
@@ -1378,6 +1378,9 @@ class _BaseRaw(ProjMixin, ContainsMixin, PickDropChannelsMixin,
         use_first_samp : boolean
             If True, time is treated as relative to the session onset, else
             as relative to the recording onset.
+        use_rounding : boolean
+            If True, use rounding (instead of truncation) when converting
+            times to indicies. This can help avoid non-unique indices.
 
         Returns
         -------
@@ -1385,7 +1388,7 @@ class _BaseRaw(ProjMixin, ContainsMixin, PickDropChannelsMixin,
             Indices corresponding to the times supplied.
         """
         return _time_as_index(times, self.info['sfreq'], self.first_samp,
-                              use_first_samp)
+                              use_first_samp, use_rounding=use_rounding)
 
     def index_as_time(self, index, use_first_samp=False):
         """Convert indices to time
@@ -1702,16 +1705,22 @@ def _allocate_data(data, data_buffer, data_shape, dtype):
     return data
 
 
-def _time_as_index(times, sfreq, first_samp=0, use_first_samp=False):
+def _time_as_index(times, first_samp=0, use_first_samp=False,
+                   use_rounding=False):
     """Convert time to indices
 
     Parameters
     ----------
     times : list-like | float | int
         List of numbers or a number representing points in time.
+    first_samp : int
+       Index to use as first time point.
     use_first_samp : boolean
         If True, time is treated as relative to the session onset, else
         as relative to the recording onset.
+    use_rounding : boolean
+        If True, use rounding (instead of truncation) when converting times to
+        indicies. This can help avoid non-unique indices.
 
     Returns
     -------
@@ -1720,7 +1729,12 @@ def _time_as_index(times, sfreq, first_samp=0, use_first_samp=False):
     """
     index = np.atleast_1d(times) * sfreq
     index -= (first_samp if use_first_samp else 0)
-    return index.astype(int)
+
+    # TODO: is rounding func okay here? np.around(2.5) returns 2.0
+    if use_rounding:
+        return np.around(index).astype(int)
+    else:
+        return index.astype(int)
 
 
 def _index_as_time(index, sfreq, first_samp=0, use_first_samp=False):
