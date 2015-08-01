c24dc4edd5146574f190eccc5742b70fc53025ac
Mainak Jas <mainakjas@gmail.com>
2015-08-01 20:58:06 +0530

    Update test

--- mne/datasets/__init__.py
+++ mne/datasets/__init__.py
@@ -4,6 +4,7 @@
 from . import sample
 from . import megsim
 from . import spm_face
+from . import brainstorm
 from . import eegbci
 from . import somato
 from . import testing
--- mne/datasets/tests/test_datasets.py
+++ mne/datasets/tests/test_datasets.py
@@ -1,5 +1,5 @@
 from os import path as op
-from nose.tools import assert_true, assert_equal
+from nose.tools import assert_true, assert_equal, assert_raises
 
 from mne import datasets
 from mne.externals.six import string_types
@@ -9,12 +9,14 @@ from mne.utils import _TempDir, run_tests_if_main, requires_good_network
 def test_datasets():
     """Test simple dataset functions
     """
-    for dname in ('sample', 'somato', 'spm_face', 'testing'):
+    for dname in ('sample', 'somato', 'brainstorm', 'spm_face', 'testing'):
         dataset = getattr(datasets, dname)
     if dataset.data_path(download=False) != '':
         assert_true(isinstance(dataset.get_version(), string_types))
     else:
         assert_true(dataset.get_version() is None)
+    # brainstorm tests
+    assert_raises(ValueError, datasets.brainstorm.data_path, archive='foo')
 
 
 @requires_good_network
