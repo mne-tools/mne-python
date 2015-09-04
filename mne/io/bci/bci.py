aafb0a5b1deba62f465f8eb683a95d298b841b0b
Teon Brooks <teon.brooks@gmail.com>
2015-09-04 13:31:16 -0400

    wip

--- mne/io/bci/bci.py
+++ mne/io/bci/bci.py
@@ -57,6 +57,10 @@ class RawBCI(_BaseRaw):
     verbose : bool, str, int, or None
         If not None, override default verbose level (see mne.verbose).
 
+    Notes
+    -----
+    .. versionadded:: 0.10.0
+
     See Also
     --------
     mne.io.Raw : Documentation of attribute and methods.
@@ -72,11 +76,47 @@ class RawBCI(_BaseRaw):
             misc = list()
         data = np.genfromtxt(input_fname, delimiter=',', comments='%',
                              skip_footer=1)
-        missing_data = np.diff(data[:, 0])
+        """
+        Dealing with the missing data
+        -----------------------------
+        When recording with OpenBCI over Bluetooth, it is possible for some of
+        the data packets, samples, to not be recorded. This does not happen
+        often but it poses a problem for maintaining proper sampling periods.
+        OpenBCI data format combats this by providing a counter on the sample to
+        know which ones are missing.
+
+        Solution
+        --------
+        Interpolate the missing samples by resampling the surrounding samples.
+        1. Find where the missing samples are.
+        2. Deal with the counter reset (resets after cycling a byte).
+        3. Resample given the diffs.
+        4. Insert resampled data in the array using the diff indices
+           (index + 1).
+        """
+        diff = np.abs(np.diff(data[:, 0]))
+        # counter goes from 0 to 255, maxdiff is 255, make diff one like others.
+        diff = np.mod(diff, 254)
+        missing_idx = np.where(diff != 1)[0]
+        missing_samps = diff[missing_idx]
+        missing_nsamps = np.sum(missing_samps)
+        missing_cumsum = np.insert(np.cumsum(missing_samps), 0, 0)[:-1]
+        missing_data = np.empty((missing_nsamps, data.shape[-1]), dtype=float)
+        for idx, nn, ii in zip(missing_idx, missing_samps, missing_cumsum):
+            missing_data[ii:ii+nn] = np.mean(data[(idx-1, idx), :])
+        idx = list()
+        for ii in missing_idx:
+            idx.append([ii] * ii)
+        idx = np.hstack(idx)
+        asdf
+        data = np.insert(data, idx, missing_data)
+        first_samps = [0]
+        last_samps = [data.shape[0]]
+        # since there is a counter column, don't need to add 1 to range
         ch_names = ['EEG %03d' % num for num in range(1, data.shape[-1])]
         ch_types = ['eeg'] * len(ch_names)
         if misc:
-            misc_names = ['MISC %03d' % ii for ii in range(len(misc))]
+            misc_names = ['MISC %03d' % ii for ii in range(1, len(misc) + 1)]
             misc_types = ['misc'] * len(misc)
             for ii, mi in enumerate(misc):
                 ch_names[mi] = misc_names[ii]
@@ -90,13 +130,13 @@ class RawBCI(_BaseRaw):
 
         # fix it for eog and misc marking
         info = create_info(ch_names, sfreq, ch_types, montage)
-        super(RawBCI, self).__init__(info, last_samps=last_sampes,
+        super(RawBCI, self).__init__(info, last_samps=last_samps,
                                      filenames=[input_fname], verbose=verbose)
-        self.data = data
+        self._data = data
 
 
-def read_raw_bci(input_fname, montage=None, eog=None, misc=None, reference=None,
-                 scale=1e-6, sfreq=250, preload=True, verbose=None):
+def read_raw_bci(input_fname, montage=None, eog=None, misc=[-3, -2, -1],
+                 reference=None, scale=1e-6, sfreq=250, preload=True, verbose=None):
     """Raw object from BCI file
 
     Parameters
@@ -136,6 +176,10 @@ def read_raw_bci(input_fname, montage=None, eog=None, misc=None, reference=None,
     -------
     raw : Instance of RawBCI
         A Raw object containing OpenBCI data.
+
+    Notes
+    -----
+    .. versionadded:: 0.10.0
 
     See Also
     --------
