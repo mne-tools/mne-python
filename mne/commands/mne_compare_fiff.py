31bb000d77b0b06968524f4cea508696e8793cf3
Eric Larson <larson.eric.d@gmail.com>
2015-10-14 15:46:25 -0400

    FIX: Fix usage

--- mne/commands/mne_compare_fiff.py
+++ mne/commands/mne_compare_fiff.py
@@ -8,14 +8,17 @@ $ mne compare_fiff test_raw.fif test_raw_sss.fif
 
 # Authors : Eric Larson, PhD
 
+import sys
 import mne
 
 
 def run():
-    parser = mne.commands.utils.get_optparser(__file__)
+    parser = mne.commands.utils.get_optparser(
+        __file__, usage='mne compare_fiff <file_a> <file_b>')
     options, args = parser.parse_args()
     if len(args) != 2:
-        raise ValueError('two arguments required')
+        parser.print_help()
+        sys.exit(1)
     mne.viz.compare_fiff(args[0], args[1])
 
 
