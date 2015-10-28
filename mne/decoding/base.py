6d99d1f372de01b50076551901219d5ef348b943
Eric Larson <larson.eric.d@gmail.com>
2015-10-28 10:09:30 -0400

    FIX: Dont import six

--- mne/decoding/base.py
+++ mne/decoding/base.py
@@ -7,8 +7,9 @@
 
 import inspect
 import warnings
-import six
 import numpy as np
+
+from ..externals.six import iteritems
 
 
 class BaseEstimator(object):
@@ -97,7 +98,7 @@ class BaseEstimator(object):
             # Simple optimisation to gain speed (inspect is slow)
             return self
         valid_params = self.get_params(deep=True)
-        for key, value in six.iteritems(params):
+        for key, value in iteritems(params):
             split = key.split('__', 1)
             if len(split) > 1:
                 # nested objects case
@@ -146,7 +147,7 @@ def _pprint(params, offset=0, printer=repr):
     params_list = list()
     this_line_length = offset
     line_sep = ',\n' + (1 + offset // 2) * ' '
-    for i, (k, v) in enumerate(sorted(six.iteritems(params))):
+    for i, (k, v) in enumerate(sorted(iteritems(params))):
         if type(v) is float:
             # use str for representing floating point numbers
             # this way we get consistent representation across
