fbc821a21fd7e0f3c8875fb0bf2c5efa3703a4aa
Christian Brodbeck <christianmbrodbeck@gmail.com>
2015-10-31 09:33:18 -0400

    FIX:  Python 3

--- mne/gui/_help.py
+++ mne/gui/_help.py
@@ -13,4 +13,4 @@ def read_tooltips(gui_name):
     with open(help_path) as fid:
         raw_tooltips = json.load(fid)
     format_ = TextWrapper(width=60, fix_sentence_endings=True).fill
-    return {key: format_(text) for key, text in raw_tooltips.iteritems()}
+    return {key: format_(text) for key, text in raw_tooltips.items()}
