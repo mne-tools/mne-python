af5349e67ac4515fe95ad8ba66ab8f93ae106335
Christian Brodbeck <christianmbrodbeck@gmail.com>
2015-11-03 10:30:52 -0500

    GUI:  warn user of untested/unsupported backends

--- mne/gui/__init__.py
+++ mne/gui/__init__.py
@@ -15,6 +15,8 @@ def combine_kit_markers():
     The functionality in this GUI is also part of :func:`kit2fiff`.
     """
     _check_mayavi_version()
+    from ._backend import _check_backend
+    _check_backend()
     from ._marker_gui import CombineMarkersFrame
     gui = CombineMarkersFrame()
     gui.configure_traits()
@@ -54,6 +56,8 @@ def coregistration(tabbed=False, split=True, scene_width=0o1, inst=None,
     <http://www.slideshare.net/mne-python/mnepython-scale-mri>`_.
     """
     _check_mayavi_version()
+    from ._backend import _check_backend
+    _check_backend()
     from ._coreg_gui import CoregFrame, _make_view
     view = _make_view(tabbed, split, scene_width)
     gui = CoregFrame(inst, subject, subjects_dir)
@@ -80,6 +84,8 @@ def fiducials(subject=None, fid_file=None, subjects_dir=None):
     The functionality in this GUI is also part of :func:`coregistration`.
     """
     _check_mayavi_version()
+    from ._backend import _check_backend
+    _check_backend()
     from ._fiducials_gui import FiducialsFrame
     gui = FiducialsFrame(subject, subjects_dir, fid_file=fid_file)
     gui.configure_traits()
@@ -90,6 +96,8 @@ def kit2fiff():
     """Convert KIT files to the fiff format
     """
     _check_mayavi_version()
+    from ._backend import _check_backend
+    _check_backend()
     from ._kit2fiff_gui import Kit2FiffFrame
     gui = Kit2FiffFrame()
     gui.configure_traits()
