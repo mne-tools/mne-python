cad4caccc136514b97fa7bccb8d281c8f9ea9d3c
Christian Brodbeck <christianmbrodbeck@gmail.com>
2015-11-03 10:31:15 -0500

    DOC:  recommended way to start GUIs (through bash)

--- mne/gui/__init__.py
+++ mne/gui/__init__.py
@@ -1,4 +1,8 @@
-"""Convenience functions for opening GUIs."""
+"""Convenience functions for opening GUIs.
+
+Uses lazy import for all functions that would lead to initializing a GUI
+library.
+"""
 
 # Authors: Christian Brodbeck <christianbrodbeck@nyu.edu>
 #
@@ -27,6 +31,11 @@ def coregistration(tabbed=False, split=True, scene_width=0o1, inst=None,
                    subject=None, subjects_dir=None):
     """Coregister an MRI with a subject's head shape
 
+    The recommended way to use the GUI is through the shell/terminal with::
+
+        $ mne coreg
+
+
     Parameters
     ----------
     tabbed : bool
@@ -48,7 +57,6 @@ def coregistration(tabbed=False, split=True, scene_width=0o1, inst=None,
 
     Notes
     -----
-    All parameters are optional, since they can be set through the GUI.
     Step by step instructions for the coregistrations can be accessed as
     slides, `for subjects with structural MRI
     <http://www.slideshare.net/mne-python/mnepython-coregistration>`_ and `for
@@ -94,6 +102,11 @@ def fiducials(subject=None, fid_file=None, subjects_dir=None):
 
 def kit2fiff():
     """Convert KIT files to the fiff format
+
+    The recommended way to use the GUI is through the shell/terminal with::
+
+        $ mne kit2fiff
+
     """
     _check_mayavi_version()
     from ._backend import _check_backend
