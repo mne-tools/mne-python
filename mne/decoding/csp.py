c6efe0ef2675da05cd6b24d3c6a73dfc278e46e0
Clemens Brunner <clemens.brunner@gmail.com>
2015-11-03 10:14:22 +0100

    Cleaned up code.

--- mne/decoding/csp.py
+++ mne/decoding/csp.py
@@ -133,8 +133,9 @@ class CSP(TransformerMixin):
         ind = np.argsort(vals)[::-1]
         vecs = vecs[:, ind]
         # re-order (first, last, second, second last, third, ...)
-        ind[::2] = np.arange(0, np.ceil(len(ind)/2))
-        ind[1::2] = np.arange(len(ind) - 1, int(len(ind)/2) - 1, -1)
+        n_vals = len(ind)
+        ind[::2] = np.arange(0, int(np.ceil(n_vals/2)))
+        ind[1::2] = np.arange(n_vals - 1, n_vals//2 - 1, -1)
         vecs = vecs[:, ind]
         # and project
         w = np.dot(vecs.T, p)
