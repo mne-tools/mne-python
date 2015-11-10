d3266bc3e70c842c82b258e8ec4d9a2c75a28561
jaeilepp <jaeilepp@student.jyu.fi>
2015-11-10 08:54:07 +0100

    Fix to test.

--- mne/io/nicolet/tests/test_nicolet.py
+++ mne/io/nicolet/tests/test_nicolet.py
@@ -4,19 +4,16 @@
 # License: BSD (3-clause)
 
 import os.path as op
-import inspect
 
 from mne import concatenate_raws
 from mne.io import read_raw_nicolet
 
-FILE = inspect.getfile(inspect.currentframe())
-parent_dir = op.dirname(op.abspath(FILE))
-data_dir = op.join(parent_dir, 'data')
+base_dir = op.join(op.dirname(op.realpath(__file__)), 'data')
+fname = op.join(base_dir, 'test_nicolet_raw.data')
 
 
 def test_data():
     """Test reading raw nicolet files."""
-    fname = op.join(data_dir, 'test_nicolet_raw.data')
     read_raw_nicolet(fname)
 
     # Make sure concatenation works
