85a435f1a49120fab946dc1e92a56ee9c06ba79f
Marian Dovgialo <mdovgialo@fabrizzio.zfb.fuw.edu.pl>
2015-11-13 17:50:52 +0100

    Adding support for Brainvision Analyser header version 2.0 TO DO: Add electrode location reading, check the unit multipliers, merge with original function

--- mne/io/__init__.py
+++ mne/io/__init__.py
@@ -28,6 +28,7 @@ from . import pick
 
 from .array import RawArray
 from .brainvision import read_raw_brainvision
+from .brainvision import read_raw_brainvision2
 from .bti import read_raw_bti
 from .ctf import read_raw_ctf
 from .edf import read_raw_edf
