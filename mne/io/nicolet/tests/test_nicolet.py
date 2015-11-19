50da1770d803cc24d4f28f06b26042796ac3a259
jaeilepp <jaeilepp@student.jyu.fi>
2015-11-19 12:57:16 +0100

    Added tests for filtering.

--- mne/io/nicolet/tests/test_nicolet.py
+++ mne/io/nicolet/tests/test_nicolet.py
@@ -5,11 +5,9 @@
 
 import os.path as op
 import inspect
-from numpy.testing import assert_array_almost_equal, assert_array_equal
 
-from mne import concatenate_raws
-from mne.io import read_raw_nicolet, Raw
-from mne.utils import _TempDir
+from mne.io import read_raw_nicolet
+from mne.io.tests.test_raw import _test_raw_object
 
 FILE = inspect.getfile(inspect.currentframe())
 base_dir = op.join(op.dirname(op.abspath(FILE)), 'data')
@@ -18,26 +16,4 @@ fname = op.join(base_dir, 'test_nicolet_raw.data')
 
 def test_data():
     """Test reading raw nicolet files."""
-    tempdir = _TempDir()
-    raw = read_raw_nicolet(fname, preload=False)
-    raw_preload = read_raw_nicolet(fname, preload=True)
-    picks = [2, 3, 12, 13]
-    assert_array_equal(raw[picks, 20:30][0], raw_preload[picks, 20:30][0])
-
-    # Make sure concatenation works
-    raw2 = concatenate_raws([raw_preload.copy(), raw_preload])
-
-    # Test saving and reading
-    out_fname = op.join(tempdir, 'test_nicolet_raw.fif')
-    raw2.save(out_fname, tmax=raw.times[-1])
-    raw2 = Raw(out_fname)
-
-    full_data = raw_preload._data
-    data1, times1 = raw[:10:3, 10:12]
-    data2, times2 = raw2[:10:3, 10:12]
-    data3, times3 = raw2[[0, 3, 6, 9], 10:12]
-    assert_array_almost_equal(data1, full_data[:10:3, 10:12], 9)
-    assert_array_almost_equal(data1, data2, 9)
-    assert_array_almost_equal(data1, data3, 9)
-    assert_array_almost_equal(times1, times2)
-    assert_array_almost_equal(times1, times3)
+    _test_raw_object(read_raw_nicolet, fname)
--- mne/io/tests/test_raw.py
+++ mne/io/tests/test_raw.py
@@ -1,23 +1,66 @@
 # Generic tests that all raw classes should run
+import warnings
 from os import path as op
-from numpy.testing import assert_allclose
+from numpy.testing import (assert_allclose, assert_array_almost_equal,
+                           assert_array_equal)
+from nose.tools import assert_equal
 
 from mne.datasets import testing
-from mne.io import Raw
+from mne.io import Raw, base
 from mne.utils import _TempDir
-from numpy.ma.testutils import assert_array_equal
+from mne.io.pick import _pick_data_channels
 
 
 def _test_raw_object(reader, *args):
-    """Test reading and writing of raw classes."""
+    """Test reading, writing, concatenating and filtering of raw classes."""
     tempdir = _TempDir()
-    for preload in (True, False):
-        raw = reader(*args, preload=preload)
-        data = raw[:, :][0]
-        out_fname = op.join(tempdir, 'test_raw.fif')
-        raw.save(out_fname, overwrite=True)
-        raw = Raw(out_fname)
-        assert_array_equal(raw[:, :][0], data)
+    raw = reader(*args, preload=False)
+    raw_preload = reader(*args, preload=True)
+    picks = [1, 3, 5]
+    assert_array_equal(raw[picks, 20:30][0], raw_preload[picks, 20:30][0])
+
+    # Make sure concatenation works
+    raw2 = base.concatenate_raws([raw_preload.copy(), raw_preload])
+
+    # Test saving and reading
+    out_fname = op.join(tempdir, 'test_raw.fif')
+    for obj in [raw, raw_preload]:
+        obj.save(out_fname, tmax=raw.times[-1], overwrite=True)
+        Raw(out_fname)
+
+    full_data = raw_preload._data
+    data1, times1 = raw[:10:3, 10:12]
+    data2, times2 = raw2[:10:3, 10:12]
+    data3, times3 = raw2[[0, 3, 6, 9], 10:12]
+    assert_array_almost_equal(data1, full_data[:10:3, 10:12], 9)
+    assert_array_almost_equal(data1, data2, 9)
+    assert_array_almost_equal(data1, data3, 9)
+    assert_array_almost_equal(times1, times2)
+    assert_array_almost_equal(times1, times3)
+
+    # filtering
+    picks = _pick_data_channels(raw_preload.info)[:4]
+    assert_equal(len(picks), 4)
+    raw_lp = raw_preload.copy()
+    with warnings.catch_warnings(record=True):
+        raw_lp.filter(0., 4.0 - 0.25, picks=picks, n_jobs=2)
+    raw_hp = raw_preload.copy()
+    with warnings.catch_warnings(record=True):
+        raw_hp.filter(8.0 + 0.25, None, picks=picks, n_jobs=2)
+    raw_bp = raw_preload.copy()
+    with warnings.catch_warnings(record=True):
+        raw_bp.filter(4.0 + 0.25, 8.0 - 0.25, picks=picks)
+    raw_bs = raw_preload.copy()
+    with warnings.catch_warnings(record=True):
+        raw_bs.filter(8.0 + 0.25, 4.0 - 0.25, picks=picks, n_jobs=2)
+    data, _ = raw_preload[picks, :]
+    lp_data, _ = raw_lp[picks, :]
+    hp_data, _ = raw_hp[picks, :]
+    bp_data, _ = raw_bp[picks, :]
+    bs_data, _ = raw_bs[picks, :]
+
+    assert_array_almost_equal(data, lp_data + bp_data + hp_data, 5)
+    assert_array_almost_equal(data, bp_data + bs_data, 6)
 
 
 def _test_concat(reader, *args):
