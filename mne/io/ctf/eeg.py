5ea91d9c281143324984101bc38639d5bfed5799
Eric Larson <larson.eric.d@gmail.com>
2015-11-24 10:16:04 -0500

    FIX: Test .eeg file support

--- mne/io/ctf/eeg.py
+++ mne/io/ctf/eeg.py
@@ -12,22 +12,20 @@ from ..constants import FIFF
 from .res4 import _make_ctf_name
 
 
-_WHITESPACE_SET = set(' \t\n\r')
-
-
 def _read_eeg(directory):
     """Read the .eeg file"""
     # Missing file is ok
     fname = _make_ctf_name(directory, 'eeg', raise_error=False)
     if fname is None:
         logger.info('    Separate EEG position data file not present.')
-        return None
+        return
     eeg = dict(labels=list(), kinds=list(), ids=list(), rr=list(), np=0,
                assign_to_chs=True, coord_frame=FIFF.FIFFV_MNE_COORD_CTF_HEAD)
     with open(fname, 'rb') as fid:
         for line in fid:
-            if set(line) != _WHITESPACE_SET:
-                parts = line.strip().split()
+            line = line.strip()
+            if len(line) > 0:
+                parts = line.split()
                 if len(parts) != 5:
                     raise RuntimeError('Illegal data in EEG position file: %s'
                                        % line)
