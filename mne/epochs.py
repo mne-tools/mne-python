Fix of tmax calculation in EpochsArray

mne/epochs.py
@@ -1895,7 +1895,7 @@ class EpochsArray(_BaseEpochs):
         if data.shape[0] != len(events):
             raise ValueError('The number of epochs and the number of events'
                              'must match')
-        tmax = (data.shape[2] - 1) / info['sfreq'] + tmin
+        tmax = float(data.shape[2] - 1) / info['sfreq'] + tmin
         if event_id is None:  # convert to int to make typing-checks happy
             event_id = dict((str(e), int(e)) for e in np.unique(events[:, 2]))
         super(EpochsArray, self).__init__(info, data, events, event_id, tmin,

mne/tests/test_epochs.py
@@ -905,7 +905,7 @@ def test_crop():
     assert_array_equal(data3, data_normal[:, :, tmask])
 
     # test time info is correct
-    epochs = EpochsArray(np.zeros((1, 1, 1001)), create_info(1, 1000., 'eeg'),
+    epochs = EpochsArray(np.zeros((1, 1, 1000)), create_info(1, 1000., 'eeg'),
                          np.ones((1, 3), int), tmin=-0.2)
     epochs.crop(-.200, .700)
     last_time = epochs.times[-1]