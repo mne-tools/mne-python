0a7de0decd1784852c718da1c1aceabd5d396031
kingjr <jeanremi.king+github@gmail.com>
2016-02-17 10:53:23 -0500

    fix TimeDecoding docs default cv

--- mne/decoding/time_gen.py
+++ mne/decoding/time_gen.py
@@ -838,7 +838,7 @@ class GeneralizationAcrossTime(_GeneralizationAcrossTime):
     cv : int | object
         If an integer is passed, it is the number of folds.
         Specific cross-validation objects can be passed, see
-        scikit-learn.cross_validation module for the list of possible objects.
+        scikit-learn.model_selection module for the list of possible objects.
         If clf is a classifier, defaults to StratifiedKFold(n_folds=5), else
         defaults to KFold(n_folds=5).
     clf : object | None
@@ -1149,7 +1149,7 @@ class TimeDecoding(_GeneralizationAcrossTime):
     cv : int | object
         If an integer is passed, it is the number of folds.
         Specific cross-validation objects can be passed, see
-        scikit-learn.cross_validation module for the list of possible objects.
+        scikit-learn.model_selection module for the list of possible objects.
         If clf is a classifier, defaults to StratifiedKFold(n_folds=5), else
         defaults to KFold(n_folds=5).
     clf : object | None
