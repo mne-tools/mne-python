653d8b78074151d9246d559237abf9b9d8b59b6d
jaeilepp <jaeilepp@student.jyu.fi>
2016-02-22 15:47:23 +0100

    Address more.

--- mne/io/ctf/ctf.py
+++ mne/io/ctf/ctf.py
@@ -113,8 +113,7 @@ class RawCTF(_BaseRaw):
 
         # Compose a structure which makes fiff writing a piece of cake
         info = _compose_meas_info(res4, coils, coord_trans, eeg)
-        if len(digs) > 0:
-            info['dig'] += digs
+        info['dig'] += digs
 
         # Determine how our data is distributed across files
         fnames = list()
