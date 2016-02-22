a1da3d0547cc764ac7537359c356ae8008fa7bb9
kingjr <jeanremi.king+github@gmail.com>
2016-02-22 11:12:59 -0500

    FIX: warning outside loop

--- mne/io/edf/edf.py
+++ mne/io/edf/edf.py
@@ -114,6 +114,11 @@ class RawEDF(_BaseRaw):
         annotmap = self._raw_extras[fi]['annotmap']
         subtype = self._raw_extras[fi]['subtype']
 
+        # Interpolating stim chan except it later gets overwritten.
+        if ((not (annot and annotmap or tal_channel is not None) and
+                (stim_channel in sel))):
+            warn('Interpolating stim channel. Events may jitter.')
+
         # gain constructor
         physical_range = np.array([ch['range'] for ch in self.info['chs']])
         cal = np.array([ch['cal'] for ch in self.info['chs']])
@@ -165,8 +170,6 @@ class RawEDF(_BaseRaw):
                                 # because it gets overwritten later on.
                                 ch_data = np.zeros(n_buf_samp)
                             else:
-                                warn('Interpolating stim channel. Events may '
-                                     'jitter.')
                                 oldrange = np.linspace(0, 1, n_samp + 1, True)
                                 newrange = np.linspace(0, 1, buf_len, False)
                                 newrange = newrange[r_sidx:r_eidx]
