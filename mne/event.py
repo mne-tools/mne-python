0572d765bb241ddc97f6635a3bd1d3dc66345027
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2016-03-26 15:24:50 +0100

    update docstring

--- mne/event.py
+++ mne/event.py
@@ -647,7 +647,7 @@ def merge_events(events, ids, new_id, replace_events=True):
 
     Parameters
     ----------
-    events : array
+    events : array, shape (n_events_in, 3)
         Events.
     ids : array of int
         The ids of events to merge.
@@ -659,8 +659,29 @@ def merge_events(events, ids, new_id, replace_events=True):
 
     Returns
     -------
-    new_events: array
+    new_events: array, shape (n_events_out, 3)
         The new events
+
+    >>> events = [[134, 0, 1], [341, 0, 2], [502, 0, 3]]
+    >>> merge_events(events, [1, 2], 12, replace_events=True)
+    array([[134,   0,  12],
+           [341,   0,  12],
+           [502,   0,   3]])
+    >>> merge_events(events, [1, 2], 12, replace_events=False)
+    array([[134,   0,   1],
+           [134,   0,  12],
+           [341,   0,   2],
+           [341,   0,  12],
+           [502,   0,   3]])
+
+    Notes
+    -----
+    Rather than merging events you can use hierarchical event_id
+    in Epochs. For example, here
+
+    >>> event_id = {'auditory/left': 1, 'auditory/right': 2}
+
+    And the condition 'auditory' would correspond to either 1 or 2.
     """
     events = np.asarray(events)
     events_out = events.copy()
