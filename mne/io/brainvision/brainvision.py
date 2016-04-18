47bf293068defc850069aabc7816058ceb076d21
Pablo-Arias <pablo12co@hotmail.com>
2016-04-18 18:51:09 +0200

    Added units to brainvision
    Added support for other units than volts when importing brainvision data

--- mne/io/brainvision/brainvision.py
+++ mne/io/brainvision/brainvision.py
@@ -241,7 +241,7 @@ _orientation_dict = dict(MULTIPLEXED='F', VECTORIZED='C')
 _fmt_dict = dict(INT_16='short', INT_32='int', IEEE_FLOAT_32='single')
 _fmt_byte_dict = dict(short=2, int=4, single=4)
 _fmt_dtype_dict = dict(short='<i2', int='<i4', single='<f4')
-_unit_dict = {'V': 1., u'µV': 1e-6, 'uV': 1e-6}
+_unit_dict = {'V': 1., u'µV': 1e-6, 'uV': 1e-6, 'C':1, u'µS': 1e-6, 'ARU' : 1}
 
 
 def _get_vhdr_info(vhdr_fname, eog, misc, scale, montage):
