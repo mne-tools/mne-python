3a784bb22d188fa97243e6b1ce0d1833e1e55a85
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2016-04-19 20:40:02 +0200

    formatting

--- tutorials/plot_artifacts_detection.py
+++ tutorials/plot_artifacts_detection.py
@@ -27,47 +27,53 @@ raw = mne.io.read_raw_fif(raw_fname, preload=True)
            .del_proj(0)
            .plot(duration=60, n_channels=100, remove_dc=False))
 
+###############################################################################
 # we see high amplitude undulations in low frequencies, spanning across tens of
 # seconds
 
 raw.plot_psd(fmax=250)
 
+###############################################################################
 # On MEG sensors we see narrow frequency peaks at 60, 120, 180, 240 Hz,
 # related to line noise.
 # But also some high amplitude signals between 25 and 32 Hz, hinting at other
 # biological artifacts such as ECG. These can be most easily detected in the
 # time domain using MNE helper functions
-
-
-# See the filtering tutorial to learn how to deal with these artifacts
-#     * :ref:`sphx_glr_auto_tutorials_plot_correction_filtering.py`
+#
+# See :ref:`tut_artifacts_filter`.
 
 ###############################################################################
 # ECG
-
+# ---
+#
 # finds ECG events, creates epochs, averages and plots
+
 average_ecg = create_ecg_epochs(raw).average()
 print('We found %i ECG events' % average_ecg.nave)
 average_ecg.plot_joint()
 
+###############################################################################
 # we can see typical time courses and non dipolar topographies
 # not the order of magnitude of the average artifact related signal and
 # compare this to what you observe for brain signals
 
+###############################################################################
 # EOG
+# ---
 
 average_eog = create_eog_epochs(raw).average()
 print('We found %i EOG events' % average_eog.nave)
 average_eog.plot_joint()
 
+###############################################################################
 # Knowing these artifact patterns is of paramount importance when
 # judging about the quality of artifact removal techniques such as SSP or ICA.
 # As a rule of thumb you need artifact amplitudes orders of magnitude higher
 # than your signal of interest and you need a few of such events in order
 # to find decompositions that allow you to estimate and remove patterns related
 # to artifacts.
-
-
-# Consider the following tutorials for correcting this class of artifacts
-#     * :ref:`sphx_glr_auto_tutorials_plot_correction_rejection.py`
-#     * :ref:`sphx_glr_auto_tutorials_plot_correction_ica_ssp.py`
+#
+# Consider the following tutorials for correcting this class of artifacts:
+#     - :ref:`tut_artifacts_filter`
+#     - :ref:`tut_artifacts_correct_ica`
+#     - :ref:`tut_artifacts_correct_ssp`
