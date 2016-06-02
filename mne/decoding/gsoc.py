e133f5f54f51267d88954f1edd09b0eee60c9e40
kingjr <jeanremi.king+github@gmail.com>
2016-06-02 09:34:52 -0400

    ENH: add UnsupervisedSpatialFilter

--- mne/decoding/gsoc.py
+++ mne/decoding/gsoc.py
@@ -36,3 +36,43 @@ class EpochsTransformerMixin(TransformerMixin):
         if self.n_chan is not None or not isinstance(self.n_chan, int):
             raise ValueError('n_chan must be None or an integer, got %s'
                              'instead.' % self.n_chan)
+
+
+class UnsupervisedSpatialFilter(EpochsTransformerMixin):
+    """Fit and transform with an unsupervised spatial filtering across time
+    and samples.
+
+    e.g.
+    filter = UnsupervisedSpatialFilter(PCA())
+    filter.fit_transform(X, y=None)
+    """
+    def __init__(self, estimator, n_chan=None):
+        self.n_chan = n_chan
+        if self.n_chan is not None and not isinstance(self.n_chan, int):
+            raise ValueError('n_chan must be None or an int, got %s '
+                             'instead' % type(n_chan))
+        self.estimator = estimator
+        if not isinstance(estimator, TransformerMixin):
+            # XXX just check if has fit transform etc attributes?
+            raise ValueError('estimator must be a scikit-learn transformer')
+
+    def fit(self, X, y=None):
+        X = self._reshape(X)
+        n_epoch, n_chan, n_time = X.shape
+        # trial as time samples
+        X = np.transpose(X, [1, 0, 2]).reshape([n_chan, n_epoch * n_time]).T
+        self.estimator.fit(X)
+        return self
+
+    def fit_transform(self, X, y=None):
+        self.fit(X)
+        return self.transform(X)
+
+    def transform(self, X):
+        X = self._reshape(X)
+        n_epoch, n_chan, n_time = X.shape
+        # trial as time samples
+        X = np.transpose(X, [1, 0, 2]).reshape([n_chan, n_epoch * n_time]).T
+        X = self.estimator.transform(X)
+        X = np.reshape(X.T, [-1, n_epoch, n_time]).transpose([1, 0, 2])
+        return X
