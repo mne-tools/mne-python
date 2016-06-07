372a556e798c5f2ba808ff9629901c9746f4ecda
jaeilepp <jaeilepp@student.jyu.fi>
2016-06-07 17:16:15 +0200

    Docstrings.

--- mne/viz/utils.py
+++ mne/viz/utils.py
@@ -1055,6 +1055,8 @@ def plot_sensors(info, kind='topomap', ch_type=None, title=None,
     title : str | None
         Title for the figure. If None (default), equals to
         ``'Sensor positions (%s)' % ch_type``.
+    show_names : bool
+        Whether to display all channel names. Defaults to False.
     ch_groups : 'position' | array of shape (ch_groups, picks) | None
         Channel groups for coloring the sensors. If None (default), default
         coloring scheme is used. If 'position', the sensors are divided
@@ -1063,8 +1065,6 @@ def plot_sensors(info, kind='topomap', ch_type=None, title=None,
 
         .. versionadded:: 0.13.0
 
-    show_names : bool
-        Whether to display all channel names. Defaults to False.
     axes : instance of Axes | instance of Axes3D | None
         Axes to draw the sensors to. If ``kind='3d'``, axes must be an instance
         of Axes3D. If None (default), a new axes will be created.
