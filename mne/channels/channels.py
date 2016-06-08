8c63c3c7bf9a65b3c2ed87370c577809d86b1796
jaeilepp <jaeilepp@student.jyu.fi>
2016-06-08 08:35:58 +0200

    Params.

--- mne/channels/channels.py
+++ mne/channels/channels.py
@@ -361,7 +361,7 @@ class SetChannelsMixin(object):
         _set_montage(self.info, montage)
 
     def plot_sensors(self, kind='topomap', ch_type=None, title=None,
-                     ch_groups=None, show_names=False, axes=None, show=True):
+                     show_names=False, ch_groups=None, axes=None, show=True):
         """
         Plot sensors positions.
 
@@ -376,6 +376,8 @@ class SetChannelsMixin(object):
         title : str | None
             Title for the figure. If None (default), equals to
             ``'Sensor positions (%s)' % ch_type``.
+        show_names : bool
+            Whether to display all channel names. Defaults to False.
         ch_groups : 'position' | array of shape (ch_groups, picks) | None
             Channel groups for coloring the sensors. If None (default), default
             coloring scheme is used. If 'position', the sensors are divided
@@ -384,8 +386,6 @@ class SetChannelsMixin(object):
 
             .. versionadded:: 0.13.0
 
-        show_names : bool
-            Whether to display all channel names. Defaults to False.
         axes : instance of Axes | instance of Axes3D | None
             Axes to draw the sensors to. If ``kind='3d'``, axes must be an
             instance of Axes3D. If None (default), a new axes will be created.
@@ -415,8 +415,8 @@ class SetChannelsMixin(object):
         """
         from ..viz.utils import plot_sensors
         return plot_sensors(self.info, kind=kind, ch_type=ch_type, title=title,
-                            ch_groups=ch_groups, show_names=show_names,
-                            show=show)
+                            show_names=show_names, ch_groups=ch_groups,
+                            axes=axes, show=show)
 
     def anonymize(self):
         """Anonymize measurement information in place by removing
