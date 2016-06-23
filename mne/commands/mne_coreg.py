6c3c11b228ccbe27ec781e0933d821a419fd72a8
Christian Brodbeck <christianmbrodbeck@gmail.com>
2016-06-23 09:16:07 -0400

    ENH coreg-GUI:  expose options in $ mne coreg

--- mne/commands/mne_coreg.py
+++ mne/commands/mne_coreg.py
@@ -17,10 +17,24 @@ def run():
     from mne.commands.utils import get_optparser
 
     parser = get_optparser(__file__)
+
+    parser.add_option("-d", "--subjects-dir", dest="subjects_dir",
+                      default=None, help="Subjects directory")
+    parser.add_option("-s", "--subject", dest="subject", default=None,
+                      help="Subject name")
+    parser.add_option("-f", "--fiff", dest="inst", default=None,
+                      help="FIFF file with digitizer data for coregistration")
+    parser.add_option("-t", "--tabbed", dest="tabbed", action="store_true",
+                      default=False, help="Option for small screens: Combine "
+                      "the data source panel and the coregistration panel "
+                      "into a single panel with tabs.")
+
     options, args = parser.parse_args()
 
     os.environ['ETS_TOOLKIT'] = 'qt4'
-    mne.gui.coregistration()
+    mne.gui.coregistration(options.tabbed, inst=options.inst,
+                           subject=options.subject,
+                           subjects_dir=options.subjects_dir)
     if is_main:
         sys.exit(0)
 
