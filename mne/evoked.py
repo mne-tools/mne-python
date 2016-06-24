5df90038fbbd868abd2cd8d7c855b36bd169e9c8
Felix Raimundo <gamaz3ps@gmail.com>
2016-06-24 18:41:15 +0200

    Use apply_baseline in read_evokeds.

--- mne/evoked.py
+++ mne/evoked.py
@@ -1291,8 +1291,8 @@ def read_evokeds(fname, condition=None, baseline=None, kind='average',
         condition = [condition]
         return_list = False
 
-    out = [Evoked(fname, c, baseline=baseline, kind=kind, proj=proj,
-           verbose=verbose) for c in condition]
+    out = [Evoked(fname, c, kind=kind, proj=proj, verbose=verbose)
+            .apply_baseline(baseline) for c in condition]
 
     return out if return_list else out[0]
 
