f62f4343b10c407aed379425bba894be0483e88c
Nick Foti <nfoti01@gmail.com>
2016-06-29 15:10:07 -0700

    Added more descriptions of what the script does.

--- tutorials/plot_point_spread.py
+++ tutorials/plot_point_spread.py
@@ -5,8 +5,8 @@ Corrupt known signal with point spread
 ======================================
 
 The aim of this tutorial is to demonstrate how to put a known signal at a
-desired location(s) in a :class:`SourceEstimate` and then corrupt the signal
-with point-spread by applying a forward and inverse solution.
+desired location(s) in a :class:`mne.SourceEstimate` and then corrupt the
+signal with point-spread by applying a forward and inverse solution.
 """
 
 import os.path as op
@@ -20,7 +20,7 @@ from mne.minimum_norm import read_inverse_operator, apply_inverse
 from mne.simulation import simulate_stc, simulate_evoked
 
 ###############################################################################
-# Parameters
+# First, we set some parameters.
 
 seed = 42
 
@@ -50,7 +50,9 @@ fname_evoked = op.join(data_path, 'MEG', 'sample',
 subject_dir = op.join(data_path, 'subjects/')
 
 ###############################################################################
-# Load and process MEG data
+# Load the MEG data
+# -----------------
+
 fwd = mne.read_forward_solution(fname_fwd, force_fixed=True,
                                 surf_ori=True)
 inv_op = read_inverse_operator(fname_inv)
@@ -69,76 +71,110 @@ label_names = [l.name for l in labels]
 n_labels = len(labels)
 
 ###############################################################################
-# Estimate noise covariance from baseline
+# Estimate the background noise covariance from the baseline period
+# -----------------------------------------------------------------
+
 cov = mne.compute_covariance(epochs, tmin=None, tmax=0.)
 
 ###############################################################################
-# Simulate known signals at two regions
+# Generate sinusoids in two spatially distant labels
+# --------------------------------------------------
 
-# Generate a known signal to corrupt with point spread
-idx = label_names.index('inferiorparietal-lh')
+# The known signal is all zero-s off of the two labels of interest
 signal = np.zeros((n_labels, T))
+idx = label_names.index('inferiorparietal-lh')
 signal[idx, :] = 1e-7 * np.sin(5 * 2 * np.pi * times)
 idx = label_names.index('rostralmiddlefrontal-lh')
 signal[idx, :] = 1e-7 * np.sin(7 * 2 * np.pi * times)
 
 ###############################################################################
-# Create mask for each label so that the signal is only set at the center
-# vertex.
+# Find the center vertices in source space of each label
+# ------------------------------------------------------
+#
+# We want the known signal in each label to only be active at the center. We
+# create a mask for each label that is 1 at the center vertex and 0 at all
+# other vertices in the label. This mask is then used when simulating
+# source-space data.
+
 hemi_to_ind = {'lh': 0, 'rh': 1}
 for i, label in enumerate(labels):
-    # Labels need values to use center_of_mass function
+    # The `center_of_mass` function needs labels to have values.
     labels[i].values.fill(1.)
+
+    # Restrict the eligible vertices to be those on the surface under
+    # consideration and within the label.
     surf_vertices = fwd['src'][hemi_to_ind[label.hemi]]['vertno']
     restrict_verts = np.intersect1d(surf_vertices, label.vertices)
     com = labels[i].center_of_mass(subject='sample',
                                    subjects_dir=subject_dir,
                                    restrict_vertices=restrict_verts,
                                    surf='white')
-    # Get center of vertex index in Label's vertex list
+
+    # Convert the center of vertex index from surface vertex list to Label's
+    # vertex list.
     cent_idx = np.where(label.vertices == com)[0][0]
 
-    # Mask out all vertices except the center.
+    # Create a mask with 1 at center vertex and zeros elsewhere.
     labels[i].values.fill(0.)
     labels[i].values[cent_idx] = 1.
 
 ###############################################################################
-# Create SourceEstimate object with known signal at center vertices of two
-# labels.
+# Create source-space data with known signals
+# -------------------------------------------
+# 
+# Put known signals onto surface vertices using the array of signals and
+# the label masks (stored in labels[i].values).
 stc_gen = simulate_stc(fwd['src'], labels, signal, times[0], dt,
                        value_fun=lambda x: x)
 
-# Use forward solution to generate sensor space data
+###############################################################################
+# Plot original signals
+# ---------------------
+#
+# Note that the original signals are highly concentrated (point) sources.
+#
+lims = (0., np.mean(stc_gen.data[stc_gen.data > 0]), np.max(stc_gen.data))
+brain_gen = stc_gen.copy().crop(0.05, None).plot(subjects_dir=subjects_dir,
+                                                 hemi='split',
+                                                 views=['lat', 'med'],
+                                                 surface='inflated',
+                                                 clim=dict(kind='value',
+                                                           pos_lims=lims))
+
+
+###############################################################################
+# Simulate sensor-space signals
+# -----------------------------
+#
+# Use the foward solution and add Gaussian noise to simulate sensor-space
+# (evoked) data from the known source-space signals. The amount of noise is
+# controlled by `evoked_snr` (higher values imply less noise).
+#
 evoked_gen = simulate_evoked(fwd, stc_gen, evoked.info, cov, evoked_snr,
                              tmin=0., tmax=1., random_state=seed)
 
-# Apply inverse to project sensor space signal back into source space
+# Map the simulated sensor-space data to source-space using the inverse
+# operator.
 stc_inv = apply_inverse(evoked_gen, inv_op, lambda2, method=method)
 
 ###############################################################################
-# Plot original signals
-lims = (0., np.mean(stc_gen.data[stc_gen.data > 0]), np.max(stc_gen.data))
-tlabel = 'Initial point-sources'
-brain_gen = stc_gen.copy().crop(0.05, None).plot(subjects_dir=subjects_dir,
-                                                 hemi='lh',
-                                                 surface='inflated',
-                                                 clim=dict(kind='value',
-                                                           pos_lims=lims),
-                                                 time_label=tlabel,
-                                                 figure=1)
-
-# Plot point-spread of corrupted signal
-tlabel = 'Corrupted with point-spread'
+# Plot the point-spread of corrupted signal
+# -----------------------------------------
+#
+# Notice that after applying the forward- and inverse-operators to the known
+# point sources that the point sources have spread across the source-space.
+# This spread is due to the minimum norm solution so that the signal leaks to
+# nearby vertices with similar orientations so that signal ends up crossing the
+# sulci and gyri.
 brain_inv = stc_inv.copy().crop(0.05, None).plot(subjects_dir=subjects_dir,
-                                                 hemi='lh',
-                                                 surface='inflated',
-                                                 time_label=tlabel,
-                                                 figure=2)
+                                                 hemi='split',
+                                                 views=['lat', 'med'],
+                                                 surface='inflated')
 
 ###############################################################################
 # Exercises
 # ---------
 #    - Change the `method` parameter to either `dSPM` or `MNE` to explore the
 #      effect of the inverse method.
-#    - Try setting `noise_snr` to a small, finite value, e.g. 3., to see the
+#    - Try setting `evoked_snr` to a small, finite value, e.g. 3., to see the
 #      effect of noise.
