b2dbd24afd41da507a90c8bdff2b8e17943fd7c5
kingjr <jeanremi.king+github@gmail.com>
2016-07-19 12:43:05 -0400

    FIX: examples with XDawn instead of XdawnTransformer

--- examples/preprocessing/plot_xdawn_denoising.py
+++ examples/preprocessing/plot_xdawn_denoising.py
@@ -31,10 +31,9 @@ efficient sensor selection in a P300 BCI. In Signal Processing Conference,
 # License: BSD (3-clause)
 
 
-from mne import (io, compute_raw_covariance, read_events, pick_types,
-                 Epochs, EpochsArray)
+from mne import (io, compute_raw_covariance, read_events, pick_types, Epochs)
 from mne.datasets import sample
-from mne.preprocessing import XdawnTransformer
+from mne.preprocessing import Xdawn
 from mne.viz import plot_epochs_image
 
 print(__doc__)
@@ -68,16 +67,13 @@ plot_epochs_image(epochs['vis_r'], picks=[230], vmin=-500, vmax=500)
 signal_cov = compute_raw_covariance(raw, picks=picks)
 
 # Xdawn instance
-xd = XdawnTransformer(n_components=2, signal_cov=signal_cov)
+xd = Xdawn(n_components=2, signal_cov=signal_cov)
 
 # Fit xdawn and pass events to correct for overlapping epochs
 xd.fit(epochs)
 
 # Denoise epochs. Note that it outputs a numpy array
-X_transform = xd.transform(epochs)
-X_denoised = xd.inverse_transform(X_transform)
+epochs_denoised = xd.apply(epochs)
 
-# Plot denoised epochs
-epochs_denoised = EpochsArray(X_denoised, epochs.info, epochs.events,
-                              event_id=event_id, tmin=epochs.tmin)
+# Plot
 plot_epochs_image(epochs_denoised['vis_r'], picks=[230], vmin=-500, vmax=500)
