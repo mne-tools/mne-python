1a31fbbdfd296aed0be0ccea8bfa8d6a1487b40b
kaichogami <asishrocks95@gmail.com>
2016-07-27 13:30:52 +0530

    fixed rebase, addressed comments

--- mne/decoding/__init__.py
+++ mne/decoding/__init__.py
@@ -1,5 +1,6 @@
 from .transformer import Scaler, FilterEstimator
-from .transformer import PSDEstimator, EpochsVectorizer, Vectorizer
+from .transformer import (PSDEstimator, EpochsVectorizer, Vectorizer,
+                          UnsupervisedSpatialFilter)
 from .mixin import TransformerMixin
 from .base import BaseEstimator, LinearModel
 from .csp import CSP
