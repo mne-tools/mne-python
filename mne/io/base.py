a30d94fb73ffe8f2ae057c15186a55738be02934
Marijn van Vliet <w.m.vanvliet@gmail.com>
2016-08-06 21:31:13 +0300

    remove unnecessary import

--- mne/io/base.py
+++ mne/io/base.py
@@ -17,7 +17,7 @@ import numpy as np
 from .constants import FIFF
 from .pick import pick_types, channel_type, pick_channels, pick_info
 from .pick import _pick_data_channels, _pick_data_or_ica
-from .meas_info import write_meas_info, anonymize_info
+from .meas_info import write_meas_info
 from .proj import setup_proj, activate_proj, _proj_equal, ProjMixin
 from ..channels.channels import (ContainsMixin, UpdateChannelsMixin,
                                  SetChannelsMixin, InterpolationMixin)
