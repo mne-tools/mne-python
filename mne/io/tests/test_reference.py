d4a66a80fa8543503206cffe184affb7efff2b08
Phillip Alday <phillip.alday@unisa.edu.au>
2016-08-26 12:08:41 +0200

    test what happens when adding a reference channel without digitization points

--- mne/io/tests/test_reference.py
+++ mne/io/tests/test_reference.py
@@ -261,6 +261,21 @@ def test_add_reference():
     raw = Raw(fif_fname, preload=True)
     picks_eeg = pick_types(raw.info, meg=False, eeg=True)
 
+    # add reference channel to Raw when no digitization points exist
+    raw = Raw(fif_fname, preload=True)
+    picks_eeg = pick_types(raw.info, meg=False, eeg=True)
+    del raw.info['dig']
+    raw_ref = add_reference_channels(raw, 'Ref', copy=True)
+    assert_equal(raw_ref._data.shape[0], raw._data.shape[0] + 1)
+    assert_array_equal(raw._data[picks_eeg, :], raw_ref._data[picks_eeg, :])
+    _check_channel_names(raw_ref, 'Ref')
+
+    orig_nchan = raw.info['nchan']
+    raw = add_reference_channels(raw, 'Ref', copy=False)
+    assert_array_equal(raw._data, raw_ref._data)
+    assert_equal(raw.info['nchan'], orig_nchan + 1)
+    _check_channel_names(raw, 'Ref')
+
     # Test adding an existing channel as reference channel
     assert_raises(ValueError, add_reference_channels, raw,
                   raw.info['ch_names'][0])
