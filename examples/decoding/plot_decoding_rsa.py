181c07b337cf168a292c6cf7fced487ae9dfa491
kingjr <jeanremi.king+github@gmail.com>
2016-09-07 16:31:14 -0400

    RSA on cichy data

--- examples/decoding/plot_decoding_rsa.py
+++ examples/decoding/plot_decoding_rsa.py
@@ -35,15 +35,14 @@ import numpy as np
 from pandas import read_csv
 import matplotlib.pyplot as plt
 
-from mne.decoding import SearchLight
+from mne import find_events, Epochs, pick_types
 from mne.io import Raw
-from mne import find_events, Epochs
 
-from sklearn.model_selection import StratifiedKFold, StratifiedShuffleSplit
+from sklearn.model_selection import StratifiedKFold
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_auc_score
 from sklearn.manifold import MDS
 
 data_path = '/media/DATA/Pro/Projects/NewYork/cichy/'
@@ -59,103 +58,85 @@ for val in triggers:
     inv_conds[val] = sub['condition'].iloc[0]
 
 # Read MEG data
-fname = op.join(data_path, 'sample_subject_3_tsss_mc.fif')
-raw = Raw(fname, preload=True)
-events = find_events(raw)
-raw.pick_types(meg='mag')
-raw.filter(None, 30.)
+fname = op.join(data_path, 'sample_subject_0_tsss_mc.fif')
+raw = Raw(fname)
+picks = pick_types(raw.info, meg='mag')[::2]
+events = find_events(raw, min_duration=.002)
+
+# Select & Format event values to match ad-hoc categories
+events = np.array([ev for ev in events if ev[2] in set(conds['trigger'])])
+# le = LabelEncoder()
+# events[:, 2] = le.fit_transform([inv_conds[val] for val in events[:, 2]])
 
 # Epoch data
 is_stim = [val in triggers for val in events[:, 2]]
 events = events[np.where(is_stim)[0]]
-epochs = Epochs(raw, events=events, decim=4, baseline=None)
-
-# Extract data in numpy format
-times = epochs.times
-X = epochs.get_data()
-le = LabelEncoder()
-y = le.fit_transform([inv_conds[val] for val in epochs.events[:, 2]])
-
-##############################################################################
-
-
-def roc_auc_scores(y_true, y_pred):
-    """While sklearn implements a multiclass ROC (sklearn #3298)"""
-    from sklearn.metrics import roc_auc_score
-    auc = 0.
-    classes = set(y_true)
-    for ii, this_class in enumerate(classes):
-        auc += roc_auc_score(y_true == this_class, y_pred[:, ii])
-    return auc / len(classes)
-
-# Decode over time to identify when information peaks
-# Classifier
-clf = make_pipeline(StandardScaler(), LogisticRegression())
-
-# Search light across time
-sl = SearchLight(clf, n_jobs=-1)
-
-# Cross validation
-n_folds = 5
-cv = StratifiedKFold(n_folds)
-
-n_classes = len(le.classes_)
-n_epochs, n_channels, n_times = X.shape
-scores = np.zeros((n_folds, n_times))
-
-scores = np.zeros((n_folds, n_times))
-for fold, (train, test) in enumerate(cv.split(X, y)):
-    print(fold)
-    sl.fit(X[train], y[train])
-    y_pred = sl.predict_proba(X[test])
-    for time_sample, y_pred_t in enumerate(y_pred.transpose(1, 0, 2)):
-        scores[fold, time_sample] = roc_auc_scores(y[test], y_pred_t)
-
-fig, ax = plt.subplots(1)
-ax.plot(times, scores.mean(0))
-ax.axhline(.5, color='k', linestyle=':', label='chance')
-ax.set_xlabel('Time (ms)')
-ax.set_ylabel('<AUC>')
-plt.show()
+epochs = Epochs(raw, events=events, decim=4, baseline=None, picks=picks,
+                tmin=.050, tmax=.800)
 
 ##############################################################################
 # Representational Similarity Analysis is a neuroimaging-specific
 # appelation to refer to statistics applied to the confusion matrix.
-y_pred = np.zeros((n_epochs, n_classes))
-cv = StratifiedShuffleSplit(200)
-toi = 32
-cm = np.zeros((n_classes, n_classes))
-toi = np.argmax(np.sum(epochs.average().data ** 2, axis=0))
-for train, test in cv.split(X, y):
-    clf.fit(X[train, :, toi], y[train])
-    y_pred = clf.predict(X[test, :, toi])
-    cm += confusion_matrix(y[test], y_pred, labels=range(n_classes))
-cm /= sum(cm)
 
+# Classify on the average ERF
+clf = make_pipeline(StandardScaler(),
+                    LogisticRegression(solver='lbfgs'))
+X = epochs.get_data().mean(2)
+y = epochs.events[:, 2]
+
+n_folds = 5
+classes = set(y)
+cv = StratifiedKFold(n_folds, random_state=0, shuffle=True)
+
+# Compute confusion matrix for each cross-validation fold
+y_pred = np.zeros((len(y), len(classes)))
+for train, test in cv.split(X, y):
+    # Fit
+    clf.fit(X[train], y[train])
+    # Predict
+    y_pred[test] = clf.predict_proba(X[test])
+
+# Compute confusion matrix using AUC
+cm = np.zeros((len(classes), len(classes)))
+for ii, train_class in enumerate(classes):
+    for jj in range(ii, len(classes)):
+        cm[ii, jj] = roc_auc_score(y == train_class, y_pred[:, jj])
+        cm[jj, ii] = cm[ii, jj]
+cm /= n_folds
+
+# Plot with formatted class names for easier reading
+names = [inv_conds[val] for val in classes]
+sparse_names = [name if next_name != name else ''
+                for name, next_name in zip(names[1:], names)]
 fig, ax = plt.subplots(1)
 im = ax.matshow(cm, cmap='plasma')
-ax.set_yticks(range(n_classes))
-ax.set_yticklabels(le.classes_)
-ax.set_xticks(range(n_classes))
-ax.set_xticklabels(le.classes_, rotation=40, ha='left')
-
+ax.set_yticks(range(len(classes)))
+ax.set_yticklabels(sparse_names)
+ax.set_xticks(range(len(classes)))
+ax.set_xticklabels(sparse_names, rotation=40, ha='left')
+for ii, name in enumerate(sparse_names):
+    if name != '':
+        ax.axhline(ii, color='k')
+        ax.axvline(ii, color='k')
 plt.colorbar(im)
 plt.show()
 
-
 ##############################################################################
-# In RSA, it is common to summarize the confusion matrix with a
-# dimensionality reduction. It is common to use multi-dimensional
-# scaling for this, as it was historically introduced by Shepard
-# who proposed to measure representation via the confusion matrix.
-mds = MDS(2)
+# Confusion matrix related to mental representations have been historically
+# summarized with dimensionality reduction using multi-dimensional scaling [1].
+# See how the face samples cluster together
+fig, ax = plt.subplots(1)
+mds = MDS(2, random_state=0)
 summary = mds.fit_transform(cm)
 cmap = plt.get_cmap('rainbow')
-colors = dict(zip(le.classes_, cmap(np.linspace(0., 1., len(le.classes_)))))
-labels = list()
-for condition, (x, y) in zip(le.classes_, summary):
-    labels.append(condition)
-    plt.scatter(x, y, s=100, facecolors=colors[condition],
-                label=condition, edgecolors='k')
-plt.axis('off')
-plt.legend()
+colors = cmap(np.linspace(0., 1., len(set(names))))
+for color, name in zip(colors, set(names)):
+    sel = np.where([this_name == name for this_name in names])[0]
+    if name == 'human face':
+        ax.plot(summary[sel, 0], summary[sel, 1], color='k', zorder=0)
+    ax.scatter(summary[sel, 0], summary[sel, 1],
+               s=100, facecolors=color, label=name, edgecolors='k')
+ax.axis('off')
+ax.legend(loc='best', scatterpoints=1, ncol=2)
+plt.tight_layout()
+plt.show()
