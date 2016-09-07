5c286748b685cd5eb6c6569558c1027466b34e64
kingjr <jeanremi.king+github@gmail.com>
2016-09-06 20:04:03 -0400

    WIP: RSA with Cichy s data

--- examples/decoding/plot_decoding_rsa.py
+++ examples/decoding/plot_decoding_rsa.py
@@ -1,4 +1,7 @@
-"""Representational Similarity Analysis is used to perform summary statistics
+""" Representational Similarity Analysis
+======================================
+
+Representational Similarity Analysis is used to perform summary statistics
 on supervised classifications where the number of classes is relatively high.
 It consists in characterizing the structure of the confusion matrix to infer
 the similarity between brain responses and serve as a proxy for characterizing
@@ -18,9 +21,8 @@ References
 [3] Kriegeskorte, N., Marieke, M., & Bandettini.  P. "Representational
  similarity analysis-connecting the branches of systems neuroscience."
  Frontiers in systems neuroscience 2 (2008): 4.
-[4] Zhang, Ying, et al. "Object decoding with attention in inferior temporal
- cortex." Proceedings of the National Academy of Sciences 108.21 (2011):
- 8850-8855.
+[4] Cichy, R. M., Pantazis, D., & Oliva, A. "Resolving human object recognition
+in space and time. Nature neuroscience" (2014): 17(3), 455-462
 """
 # Authors: Jean-Remi King <jeanremi.king@gmail.com>
 #
@@ -28,130 +30,114 @@ References
 
 print(__doc__)
 
-
-import os
+import os.path as op
 import numpy as np
+from pandas import read_csv
 import matplotlib.pyplot as plt
-from scipy.io import loadmat
-from pandas import DataFrame
-from itertools import product
 
-from sklearn.preprocessing import LabelEncoder
+from mne.decoding import SearchLight
+from mne.io import Raw
+from mne import find_events, Epochs
+
 from sklearn.model_selection import StratifiedKFold, StratifiedShuffleSplit
 from sklearn.pipeline import make_pipeline
-from mne.decoding import SearchLight
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 from sklearn.manifold import MDS
 
-data_path = './data/'
-files = os.listdir(data_path)
-
-classes = ('car', 'couch', 'face', 'flower', 'guitar', 'hand', 'kiwi')
-positions = ('lower', 'middle', 'upper')
-
+data_path = '/media/DATA/Pro/Projects/NewYork/cichy/'
 ###############################################################################
 
-# FIXME Add this spiking data in MNE samples in a fif format?
-# epoched matlab data available from
-# http://www.readout.info/downloads/datasets/zhang-desimone-7-object-dataset/
+# Define stimulus - trigger mapping
+fname = op.join(data_path, 'visual_stimuli.csv')
+conds = read_csv(fname)
+triggers = set(conds['trigger'])
+inv_conds = dict()
+for val in triggers:
+    sub = conds.query('trigger == %i' % val)
+    inv_conds[val] = sub['condition'].iloc[0]
 
-# Read the spike data
-channels = list()
-for ii, this_file in enumerate(files):
-    # load epochs data of one channel
-    mat = loadmat(os.path.join(data_path, this_file),
-                  struct_as_record=True, squeeze_me=False)
+# Read MEG data
+fname = op.join(data_path, 'sample_subject_3_tsss_mc.fif')
+raw = Raw(fname, preload=True)
+events = find_events(raw)
+raw.pick_types(meg='mag')
+raw.filter(None, 30.)
 
-    # Store them per condition. Note that different channels may be recorded
-    # from different epochs.
-    channel = dict(('/'.join(cond), list())
-                   for cond in product(classes, positions))
-    data = mat['raster_data']
-    ch_classes = [ii[0] for ii in mat['raster_labels'][0][0][0][0]]
-    ch_positions = [ii[0] for ii in mat['raster_labels'][0][0][1][0]]
-    for this_x, this_class, this_pos in zip(data, ch_classes, ch_positions):
-        condition = '/'.join((this_class, this_pos))
-        channel[condition].append(this_x)
+# Epoch data
+is_stim = [val in triggers for val in events[:, 2]]
+events = events[np.where(is_stim)[0]]
+epochs = Epochs(raw, events=events, decim=4, baseline=None)
 
-    # Append data
-    channels.append(channel)
-
-
-# Reshape data such that we get a classic (n_epochs, n_channels, n_times)
-# Note that this is a trick which can be misleading. Here, only four channels
-# were recorded simulatenously.
-n_rep = 19  # it should be 20, but there are some missing trials
-X, y = list(), dict(cat=list(), position=list())
-
-for this_class, this_pos in product(classes, positions):
-    condition = '/'.join((this_class, this_pos))
-    X.append(np.transpose([ch[condition][:n_rep]
-                           for ch in channels], [1, 0, 2]))
-    y['cat'].extend([this_class] * n_rep)
-    y['position'].extend([this_pos] * n_rep)
-X = np.vstack(X)
-y = DataFrame(y)
-
-###############################################################################
-# Compute spike rate with a moving average by convolving a window of 20 time
-# samples over the time axis
-Xbin = np.apply_along_axis(lambda m: np.convolve(m, np.ones(20), mode='valid'),
-                           axis=2, arr=X)
-
-# Decimate time for faster computation
-decim = 20
-Xbin = Xbin[..., ::decim]
-
-# Preprocess the categories
+# Extract data in numpy format
+times = epochs.times
+X = epochs.get_data()
 le = LabelEncoder()
-y['condition'] = y[['cat', 'position']].apply(lambda x: '/'.join(x), axis=1)
-y_condition = le.fit_transform(y['condition'])
+y = le.fit_transform([inv_conds[val] for val in epochs.events[:, 2]])
 
 ##############################################################################
+
+
+def roc_auc_scores(y_true, y_pred):
+    """While sklearn implements a multiclass ROC (sklearn #3298)"""
+    from sklearn.metrics import roc_auc_score
+    auc = 0.
+    classes = set(y_true)
+    for ii, this_class in enumerate(classes):
+        auc += roc_auc_score(y_true == this_class, y_pred[:, ii])
+    return auc / len(classes)
+
 # Decode over time to identify when information peaks
+# Classifier
 clf = make_pipeline(StandardScaler(), LogisticRegression())
+
+# Search light across time
 sl = SearchLight(clf, n_jobs=-1)
 
+# Cross validation
 n_folds = 5
-n_classes = len(set(y_condition))
-n_epochs, n_channels, n_times = Xbin.shape
-y_pred = np.zeros((n_epochs, n_times))
+cv = StratifiedKFold(n_folds)
+
+n_classes = len(le.classes_)
+n_epochs, n_channels, n_times = X.shape
 scores = np.zeros((n_folds, n_times))
 
-cv = StratifiedKFold(n_folds)
 scores = np.zeros((n_folds, n_times))
-for fold, (train, test) in enumerate(cv.split(Xbin, y_condition)):
-    sl.fit(Xbin[train], y_condition[train])
-    # compute score for each time point
-    scores[fold] = sl.score(Xbin[test], y_condition[test])
+for fold, (train, test) in enumerate(cv.split(X, y)):
+    print(fold)
+    sl.fit(X[train], y[train])
+    y_pred = sl.predict_proba(X[test])
+    for time_sample, y_pred_t in enumerate(y_pred.transpose(1, 0, 2)):
+        scores[fold, time_sample] = roc_auc_scores(y[test], y_pred_t)
 
 fig, ax = plt.subplots(1)
-ax.plot(scores.mean(0))
-ax.axhline(1. / n_classes, color='k', linestyle=':', label='chance')
-ax.set_xlabel('Time samples')
-ax.set_ylabel('Accuracy')
+ax.plot(times, scores.mean(0))
+ax.axhline(.5, color='k', linestyle=':', label='chance')
+ax.set_xlabel('Time (ms)')
+ax.set_ylabel('<AUC>')
 plt.show()
 
 ##############################################################################
 # Representational Similarity Analysis is a neuroimaging-specific
 # appelation to refer to statistics applied to the confusion matrix.
+y_pred = np.zeros((n_epochs, n_classes))
 cv = StratifiedShuffleSplit(200)
 toi = 32
 cm = np.zeros((n_classes, n_classes))
-for train, test in cv.split(Xbin, y_condition):
-    clf.fit(Xbin[train, :, toi], y_condition[train])
-    y_pred = clf.predict(Xbin[test, :, toi])
-    cm += confusion_matrix(y_condition[test], y_pred)
+toi = np.argmax(np.sum(epochs.average().data ** 2, axis=0))
+for train, test in cv.split(X, y):
+    clf.fit(X[train, :, toi], y[train])
+    y_pred = clf.predict(X[test, :, toi])
+    cm += confusion_matrix(y[test], y_pred, labels=range(n_classes))
 cm /= sum(cm)
 
 fig, ax = plt.subplots(1)
 im = ax.matshow(cm, cmap='plasma')
 ax.set_yticks(range(n_classes))
-ax.set_yticklabels([''] + le.classes_)
+ax.set_yticklabels(le.classes_)
 ax.set_xticks(range(n_classes))
-ax.set_xticklabels([''] + le.classes_, rotation=40, ha='left')
+ax.set_xticklabels(le.classes_, rotation=40, ha='left')
 
 plt.colorbar(im)
 plt.show()
@@ -165,15 +151,11 @@ plt.show()
 mds = MDS(2)
 summary = mds.fit_transform(cm)
 cmap = plt.get_cmap('rainbow')
-colors = dict(zip(classes, cmap(np.linspace(0., 1., len(classes)))))
+colors = dict(zip(le.classes_, cmap(np.linspace(0., 1., len(le.classes_)))))
 labels = list()
 for condition, (x, y) in zip(le.classes_, summary):
-    class_, position = condition.split('/')
-    label = None
-    if class_ not in labels:
-        label = class_
-        labels.append(class_)
-    plt.scatter(x, y, s=100, facecolors=colors[class_],
-                label=label, edgecolors='k')
+    labels.append(condition)
+    plt.scatter(x, y, s=100, facecolors=colors[condition],
+                label=condition, edgecolors='k')
 plt.axis('off')
 plt.legend()
