456bb5fad6ed832df8769f70472664a3dd69bfbb
Jussi Nurminen <jnu@iki.fi>
2016-09-20 11:00:44 +0300

    fine tune example

--- examples/io/plot_elekta_epochs.py
+++ examples/io/plot_elekta_epochs.py
@@ -38,7 +38,7 @@ epochs = mne.Epochs(raw, **cond)
 epochs.average().plot_topo()
 
 ###############################################################################
-# Get epochs from all conditions, average, save to an evoked fiff file
+# Get epochs from all conditions, average
 evokeds = []
 for cat in ap.categories:
     cond = ap.get_condition(raw, cat)
@@ -47,19 +47,21 @@ for cat in ap.categories:
     evoked = epochs.average()
     evoked.comment = cat['comment']
     evokeds.append(evoked)
-# fname_out = 'elekta_evokeds-ave.fif'
+# save to an evoked fiff file
+# fname_out = 'multimodal-ave.fif'
 # mne.write_evokeds(fname_out, evokeds)
 
 ###############################################################################
 # Make a new category using existing one as a template, extract epochs
 newcat = ap.categories[0].copy()
-newcat['comment'] = 'SEF right, longer'
-newcat['event'] = 5  # reference event
-newcat['start'] = -.5  # epoch start rel. to ref. event (in seconds)
-newcat['end'] = 1  # epoch end
+newcat['comment'] = 'Visual lower left, longer epochs'
+newcat['event'] = 3  # reference event
+newcat['start'] = -.2  # epoch start rel. to ref. event (in seconds)
+newcat['end'] = .7  # epoch end
 newcat['reqevent'] = 0  # additional required event; 0 if none
-newcat['reqwithin'] = 0.0  # req. event required within 1.5 sec of ref. event
-newcat['reqwhen'] = 1  # required before (1) or after (2) ref. event
+newcat['reqwithin'] = .5  # ...required within .5 sec (before or after)
+newcat['reqwhen'] = 2  # ...required before (1) or after (2) ref. event
+
 cond = ap.get_condition(raw, newcat)
-epochs = mne.Epochs(raw, **cond)
+epochs = mne.Epochs(raw, reject=ap.reject, flat=ap.flat, **cond)
 epochs.average().plot()
