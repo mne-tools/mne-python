2d5205c081ad240f05f763a16dca0a50a165fda5
kaichogami <asishrocks95@gmail.com>
2016-11-06 23:15:50 +0530

    removed blank space

--- mne/decoding/search_light.py
+++ mne/decoding/search_light.py
@@ -231,7 +231,7 @@ class _SearchLight(BaseEstimator, TransformerMixin):
 
         if len(np.unique(y)) <= 2:
             from sklearn.preprocessing import LabelEncoder
-                                     
+
             le = LabelEncoder()
             y = le.fit_transform(y)
 
