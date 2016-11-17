c323992fc1fce8745771e379df4f29861d80dd67
jaeilepp <jaeilepp@gmail.com>
2016-11-17 10:04:39 +0100

    Small fixes.

--- mne/datasets/visual_96_categories/__init__.py
+++ mne/datasets/visual_96_categories/__init__.py
@@ -1,5 +1,5 @@
-"""MNE visual_96_categories dataset"""
+"""MNE visual_96_categories dataset."""
 
-from visual_96_categories import (data_path, has_visual_96_categories_data,
-                                  get_version,
-                                  requires_visual_96_categories_data)
+from .visual_96_categories import (data_path, has_visual_96_categories_data,
+                                   get_version,
+                                   requires_visual_96_categories_data)
