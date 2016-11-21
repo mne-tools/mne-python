65652b2d3e3162833edcd0a6d14ec8345b4bf13c
Keith Doelling <kd889@nyu.edu>
2016-11-21 14:58:18 -0500

    Use _TempDir() instead of NamedTemporaryFile() for temporary file generation

--- mne/io/kit/tests/test_kit.py
+++ mne/io/kit/tests/test_kit.py
@@ -13,12 +13,11 @@ from nose.tools import (assert_equal, assert_almost_equal, assert_raises,
                         assert_true)
 from scipy import linalg
 import scipy.io
-from tempfile import NamedTemporaryFile
 
 from mne import pick_types, Epochs, find_events, read_events
 from mne.transforms import apply_trans
 from mne.tests.common import assert_dig_allclose
-from mne.utils import run_tests_if_main
+from mne.utils import run_tests_if_main, _TempDir
 from mne.io import read_raw_fif, read_raw_kit, read_epochs_kit
 from mne.io.kit.coreg import read_sns
 from mne.io.kit.constants import KIT, KIT_CONSTANTS, KIT_NY, KIT_UMD_2014
@@ -196,25 +195,26 @@ def test_decimate():
     hsp_mm = _get_ico_surface(5)['rr'] * 100
     hsp_m = hsp_mm / 1000.
 
-    # save headshape to a file in mm
-    sphere_hsp = NamedTemporaryFile()
-    np.savetxt(sphere_hsp.name, hsp_mm)
+    # save headshape to a file in mm in temporary directory
+    tempdir = _TempDir()
+    sphere_hsp_path = op.join(tempdir, 'test_sphere.txt')
+    np.savetxt(sphere_hsp_path, hsp_mm)
+
     # read in raw data using spherical hsp, and extract new hsp
-    raw = read_raw_kit(sqd_path, mrk_path, elp_txt_path, sphere_hsp.name)
-    # close and delete temporary file
-    sphere_hsp.close()
+    raw = read_raw_kit(sqd_path, mrk_path, elp_txt_path, sphere_hsp_path)
     # collect headshape from raw (should now be in m)
     hsp_dec = np.array([dig['r'] for dig in raw.info['dig']])[8:]
+
     # with 10242 points and _decimate_points set to resolution of 5 mm, hsp_dec
     # should be a bit over 5000 points. If not, something is wrong or
     # decimation resolution has been purposefully changed
     assert_true(len(hsp_dec) > 5000)
+
     # should have similar size, distance from center
     dist = np.sqrt(np.sum((hsp_m - np.mean(hsp_m, axis=0))**2, axis=1))
     dist_dec = np.sqrt(np.sum((hsp_dec - np.mean(hsp_dec, axis=0))**2, axis=1))
     hsp_rad = np.mean(dist)
     hsp_dec_rad = np.mean(dist_dec)
-
     assert_almost_equal(hsp_rad, hsp_dec_rad, places=3)
 
 run_tests_if_main()
