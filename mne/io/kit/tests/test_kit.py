FIX: Updating digitizer point decimation to units of meters (#3747)
* FIX: Updating digitizer point decimation to units of meters

* Add test to check that future bugs of this nature are caught

* Updated docstring to read_raw_kit to make clear units should be meters.

* Make sphere hsp a txt file

* Update decimation test to be in meters and to use nose's assert_almost_equal

* Aesthetic changes

* Use assert_true, not assert

* Make spherical headshape file temporary

* Use _TempDir() instead of NamedTemporaryFile() for temporary file generation

mne/io/kit/tests/test_kit.py
@@ -9,18 +9,20 @@ import os.path as op
 import inspect
 import numpy as np
 from numpy.testing import assert_array_almost_equal, assert_array_equal
-from nose.tools import assert_equal, assert_raises, assert_true
+from nose.tools import (assert_equal, assert_almost_equal, assert_raises,
+                        assert_true)
 from scipy import linalg
 import scipy.io
 
 from mne import pick_types, Epochs, find_events, read_events
 from mne.transforms import apply_trans
 from mne.tests.common import assert_dig_allclose
-from mne.utils import run_tests_if_main
+from mne.utils import run_tests_if_main, _TempDir
 from mne.io import read_raw_fif, read_raw_kit, read_epochs_kit
 from mne.io.kit.coreg import read_sns
 from mne.io.kit.constants import KIT, KIT_CONSTANTS, KIT_NY, KIT_UMD_2014
 from mne.io.tests.test_raw import _test_raw_reader
+from mne.surface import _get_ico_surface
 
 FILE = inspect.getfile(inspect.currentframe())
 parent_dir = op.dirname(op.abspath(FILE))
@@ -36,6 +38,8 @@ elp_txt_path = op.join(data_dir, 'test_elp.txt')
 hsp_txt_path = op.join(data_dir, 'test_hsp.txt')
 elp_path = op.join(data_dir, 'test.elp')
 hsp_path = op.join(data_dir, 'test.hsp')
+# this is a fake headshape made up of a unit sphere to test decimation
+sphere_hsp_txt_path = op.join(data_dir, 'test_sphere_hsp.txt')
 
 
 def test_data():
@@ -184,5 +188,33 @@ def test_hsp_elp():
     pts_elp_in_dev = apply_trans(linalg.inv(trans_elp), pts_elp)
     assert_array_almost_equal(pts_elp_in_dev, pts_txt_in_dev, decimal=5)
 
+
+def test_decimate():
+    """Test decimation of digitizer headshapes with too many points. """
+    # load headshape and convert to meters
+    hsp_mm = _get_ico_surface(5)['rr'] * 100
+    hsp_m = hsp_mm / 1000.
+
+    # save headshape to a file in mm in temporary directory
+    tempdir = _TempDir()
+    sphere_hsp_path = op.join(tempdir, 'test_sphere.txt')
+    np.savetxt(sphere_hsp_path, hsp_mm)
+
+    # read in raw data using spherical hsp, and extract new hsp
+    raw = read_raw_kit(sqd_path, mrk_path, elp_txt_path, sphere_hsp_path)
+    # collect headshape from raw (should now be in m)
+    hsp_dec = np.array([dig['r'] for dig in raw.info['dig']])[8:]
+
+    # with 10242 points and _decimate_points set to resolution of 5 mm, hsp_dec
+    # should be a bit over 5000 points. If not, something is wrong or
+    # decimation resolution has been purposefully changed
+    assert_true(len(hsp_dec) > 5000)
+
+    # should have similar size, distance from center
+    dist = np.sqrt(np.sum((hsp_m - np.mean(hsp_m, axis=0))**2, axis=1))
+    dist_dec = np.sqrt(np.sum((hsp_dec - np.mean(hsp_dec, axis=0))**2, axis=1))
+    hsp_rad = np.mean(dist)
+    hsp_dec_rad = np.mean(dist_dec)
+    assert_almost_equal(hsp_rad, hsp_dec_rad, places=3)
 
 run_tests_if_main()