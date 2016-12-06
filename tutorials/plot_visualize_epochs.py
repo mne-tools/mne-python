6742dbf7e8a655a97602f2e048b71ce04f30be9f
jaeilepp <jaeilepp@gmail.com>
2016-12-06 15:57:40 +0100

    Fix to tutorial.

--- tutorials/plot_visualize_epochs.py
+++ tutorials/plot_visualize_epochs.py
@@ -12,9 +12,10 @@ import mne
 data_path = op.join(mne.datasets.sample.data_path(), 'MEG', 'sample')
 raw = mne.io.read_raw_fif(op.join(data_path, 'sample_audvis_raw.fif'))
 raw.set_eeg_reference()  # set EEG average reference
-events = mne.read_events(op.join(data_path, 'sample_audvis_raw-eve.fif'),
-                         include=[5, 32])
-epochs = mne.Epochs(raw, events, 5, tmin=-0.2, tmax=1.)
+event_id = {'auditory/left': 1, 'auditory/right': 2, 'visual/left': 3,
+            'visual/right': 4, 'smiley': 5, 'button': 32}
+events = mne.read_events(op.join(data_path, 'sample_audvis_raw-eve.fif'))
+epochs = mne.Epochs(raw, events, event_id=event_id, tmin=-0.2, tmax=1.)
 
 ###############################################################################
 # This tutorial focuses on visualization of epoched data. All of the functions
@@ -33,10 +34,15 @@ epochs = mne.Epochs(raw, events, 5, tmin=-0.2, tmax=1.)
 epochs.plot(block=True)
 
 ###############################################################################
-# The numbers at the top refer to the event id of the epoch. We only have
-# events with id number of 5 (smiley faces) since we included only those when
-# constructing the epochs. The number at the bottom is the running numbering
-# for the epochs.
+# The numbers at the top refer to the event id of the epoch. The number at the
+# bottom is the running numbering for the epochs.
+#
+# Since we did no artifact correction or rejection, there are epochs
+# contaminated with blinks and saccades. For instance, epoch number 1 seems to
+# be contaminated by a blink (scroll to the bottom to view the EOG channel).
+# This epoch can be marked for rejection by clicking on top of the browser
+# window. The epoch should turn red when you click it. This means that it will
+# be dropped as the browser window is closed.
 #
 # It is possible to plot event markers on epoched data by passing ``events``
 # keyword to the epochs plotter. The events are plotted as vertical lines and
@@ -51,17 +57,11 @@ epochs.plot(block=True)
 # default to avoid overlaps. You can turn it back on via settings dialog by
 # pressing `o` key. You should check out `help` at the lower left corner of the
 # window for more information about the interactive features.
+events = mne.pick_events(events, include=[5, 32])
 mne.viz.plot_events(events)
-epochs.plot(events=events, block=True)
+epochs['smiley'].plot(events=events)
 
 ###############################################################################
-# Since we did no artifact correction or rejection, there are epochs
-# contaminated with blinks and saccades. For instance, epoch number 1 seems to
-# be contaminated by a blink (scroll to the bottom to view the EOG channel).
-# This epoch can be marked for rejection by clicking on top of the browser
-# window. The epoch should turn red when you click it. This means that it will
-# be dropped as the browser window is closed.
-#
 # To plot individual channels as an image, where you see all the epochs at one
 # glance, you can use function :func:`mne.Epochs.plot_image`. It shows the
 # amplitude of the signal over all the epochs plus an average (evoked response)
@@ -70,7 +70,7 @@ epochs.plot(events=events, block=True)
 # interactive mode you can scale and change the colormap with mouse scroll and
 # up/down arrow keys. You can also drag the colorbar with left/right mouse
 # button. Hitting space bar resets the scale.
-epochs.plot_image(189, cmap='interactive')
+epochs.plot_image(278, cmap='interactive')
 
 ###############################################################################
 # You also have functions for plotting channelwise information arranged into a
