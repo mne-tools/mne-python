e08bc2c97f126d57f06c1ef329eea7c788917cee
Eric Larson <larson.eric.d@gmail.com>
2016-12-09 15:26:22 -0500

    WIP

--- mne/tests/test_report.py
+++ mne/tests/test_report.py
@@ -72,9 +72,8 @@ def test_render_report():
     epochs.save(epochs_fname)
     epochs.average().save(evoked_fname)
 
-    report = Report(info_fname=raw_fname_new, subjects_dir=subjects_dir)
-    if sys.version.startswith('3.5'):  # XXX Some strange MPL/3.5 error...
-        raise SkipTest('Python 3.5 and mpl have unresolved issues')
+    report = Report(info_fname=raw_fname_new, subjects_dir=subjects_dir,
+                    verbose=True)
     with warnings.catch_warnings(record=True) as w:
         warnings.simplefilter('always')
         report.parse_folder(data_path=tempdir, on_error='raise')
