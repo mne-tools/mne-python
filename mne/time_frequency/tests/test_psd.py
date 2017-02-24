70576281ba4972abf31de602797da01d3dcca965
mmagnuski <mmagnuski@swps.edu.pl>
2017-02-24 12:07:01 +0100

    STY: make all well with the universe

--- mne/time_frequency/tests/test_psd.py
+++ mne/time_frequency/tests/test_psd.py
@@ -100,8 +100,8 @@ def test_psd():
     assert_array_almost_equal(trim_mean(windows, 0.2, axis=-3), windows_trim)
 
     # test reduction with function
-    func = lambda x: x.max(axis=-1)
-    windows_max, freqs = psd_welch(raw, proj=False, reduction=func, **kws_psd)
+    windows_max, freqs = psd_welch(
+        raw, proj=False, reduction=lambda x: x.max(axis=-1), **kws_psd)
     assert_array_almost_equal(np.max(windows, axis=-3), windows_max)
 
     # -- Epochs/Evoked --
