d80b682536fbba6a204239978585175381564987
kingjr <jeanremi.king+github@gmail.com>
2017-03-27 17:59:36 -0400

    update example

--- examples/decoding/plot_decoding_time_generalization_conditions.py
+++ examples/decoding/plot_decoding_time_generalization_conditions.py
@@ -21,11 +21,15 @@ References
 #
 # License: BSD (3-clause)
 
-import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
 
 import mne
 from mne.datasets import sample
-from mne.decoding import GeneralizationAcrossTime
+from mne.decoding.search_light import _GeneralizationLight
 
 print(__doc__)
 
@@ -38,34 +42,34 @@ raw = mne.io.read_raw_fif(raw_fname, preload=True)
 picks = mne.pick_types(raw.info, meg=True, exclude='bads')  # Pick MEG channels
 raw.filter(1, 30, method='fft')  # Band pass filtering signals
 events = mne.read_events(events_fname)
-event_id = {'AudL': 1, 'AudR': 2, 'VisL': 3, 'VisR': 4}
+event_id = {'AudR': 2, 'VisL': 3}
+tmin = -0.050
+tmax = 0.400
 decim = 2  # decimate to make the example faster to run
-epochs = mne.Epochs(raw, events, event_id, -0.050, 0.400, proj=True,
-                    picks=picks, baseline=None, preload=True,
-                    reject=dict(mag=5e-12), decim=decim, verbose=False)
+params = dict(tmin=tmin, tmax=tmax, proj=True, picks=picks, baseline=None,
+              preload=True, reject=dict(mag=5e-12), decim=decim, verbose=False)
+epochs_left = mne.Epochs(raw, events, event_id={'AudL': 1, 'VisL': 3},
+                         **params)
+epochs_right = mne.Epochs(raw, events, event_id={'AudR': 2, 'VisR': 4},
+                          **params)
 
 # We will train the classifier on all left visual vs auditory trials
 # and test on all right visual vs auditory trials.
+clf = make_pipeline(StandardScaler(), LogisticRegression())
+tg = _GeneralizationLight(clf, scoring='roc_auc', n_jobs=1)
 
-# In this case, because the test data is independent from the train data,
-# we test the classifier of each fold and average the respective predictions.
+# Encode the y with similar values in both cases
+y_left = epochs_left.events[:, 2] > 2
+y_right = epochs_right.events[:, 2] > 2
+tg.fit(X=epochs_left.get_data(), y=y_left)
+scores = tg.score(X=epochs_right.get_data(), y=y_right)
 
-# Define events of interest
-triggers = epochs.events[:, 2]
-viz_vs_auditory = np.in1d(triggers, (1, 2)).astype(int)
-
-gat = GeneralizationAcrossTime(predict_mode='mean-prediction', n_jobs=1)
-
-# For our left events, which ones are visual?
-viz_vs_auditory_l = (triggers[np.in1d(triggers, (1, 3))] == 3).astype(int)
-# To make scikit-learn happy, we converted the bool array to integers
-# in the same line. This results in an array of zeros and ones:
-print("The unique classes' labels are: %s" % np.unique(viz_vs_auditory_l))
-
-gat.fit(epochs[('AudL', 'VisL')], y=viz_vs_auditory_l)
-
-# For our right events, which ones are visual?
-viz_vs_auditory_r = (triggers[np.in1d(triggers, (2, 4))] == 4).astype(int)
-
-gat.score(epochs[('AudR', 'VisR')], y=viz_vs_auditory_r)
-gat.plot(title="Temporal Generalization (visual vs auditory): left to right")
+# plot
+fig, ax = plt.subplots(1)
+im = ax.matshow(scores, vmin=0, vmax=1., cmap='RdBu_r', origin='lower',
+                extent=[epochs_left.times.min(), epochs_left.times.max()] * 2)
+ax.axhline(0., color='k')
+ax.axvline(0., color='k')
+ax.xaxis.set_ticks_position('bottom')
+plt.colorbar(im, ax=ax)
+plt.show()
