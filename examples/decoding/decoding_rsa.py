9f0e6aad99f292be8cb9412a86b6e54073478ed4
Chris Holdgraf <choldgraf@gmail.com>
2017-03-28 16:58:54 -0400

    Update decoding_rsa.py

--- examples/decoding/decoding_rsa.py
+++ examples/decoding/decoding_rsa.py
@@ -14,8 +14,8 @@ a list of 92 images). Subjects were presented with images of human, animal
 and inanimate objects [4]_. Here we use the 24 unique images of faces
 and body parts.
 
-.. note:: this example will download a very large (~6GB) file, so we will not build the
-          images below.
+.. note:: this example will download a very large (~6GB) file, so we will not
+          build the images below.
 
 References
 ----------
