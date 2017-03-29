e2a7fb022ca6272f6f6ab0d926b332aed7959a1a
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2017-03-29 08:32:39 -0400

    fix test

--- mne/stats/tests/test_regression.py
+++ mne/stats/tests/test_regression.py
@@ -140,12 +140,15 @@ def test_continuous_regression_with_overlap():
 
     # test that sklearn solvers can be used
     from sklearn.linear_model.ridge import ridge_regression
-    solver = lambda X, y: ridge_regression(X, y, alpha=0)
+
+    def solver(X, y):
+        return ridge_regression(X, y, alpha=0.).T
     assert_allclose(effect, linear_regression_raw(
-        raw, events, tmin=0, solver=solver)['0'].data.flatten())
+        raw, events, tmin=0, solver=solver)['1'].data.flatten())
 
     # test bad solvers
-    solT = lambda X, y: ridge_regression(X, y).T
+    def solT(X, y):
+        return ridge_regression(X, y, alpha=0.)
     assert_raises(ValueError, linear_regression_raw, raw, events, solver=solT)
     assert_raises(ValueError, linear_regression_raw, raw, events, solver='err')
     assert_raises(TypeError, linear_regression_raw, raw, events, solver=0)
