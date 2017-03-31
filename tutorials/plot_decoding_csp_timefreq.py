37a46d5e58235f61733708b8c5287c30ec04d6fc
nicproulx <nh.proulx@gmail.com>
2017-03-31 11:51:20 -0400

    Combined plot_decoding_csp_freq.py to plot_decoding_csp_timefreq.py and moved to tutorial

--- tutorials/plot_decoding_csp_timefreq.py
+++ tutorials/plot_decoding_csp_timefreq.py
@@ -1,16 +1,16 @@
 """
 ============================================================================
-Decoding in frequency space data using the Common Spatial Pattern (CSP)
+Decoding in time-frequency space data using the Common Spatial Pattern (CSP)
 ============================================================================
 
-The frequency decomposition is estimated by iterating over raw data that
+
+The time-frequency decomposition is estimated by iterating over raw data that
 has been band-passed at different frequencies. This is used to compute a
-covariance matrix over each epoch and extract the CSP filtered
-signals. A linear discriminant classifier is then applied to these signals.
+covariance matrix over each epoch or a rolling time-window and extract the CSP 
+filtered signals. A linear discriminant classifier is then applied to these 
+signals.
 """
-# Authors: Nicole Proulx <nh.proulx@gmail.com>
-#          Hubert Banville <hubert.jbanville@gmail.com>
-#          Laura Gwilliams <laura.gwilliams@nyu.edu>
+# Authors: Laura Gwilliams <laura.gwilliams@nyu.edu>
 #          Jean-Remi King <jeanremi.king@gmail.com>
 #          Alex Barachant <alexandre.barachant@gmail.com>
 #          Alexandre Gramfort <alexandre.gramfort@telecom-paristech.fr>
@@ -20,10 +20,11 @@ signals. A linear discriminant classifier is then applied to these signals.
 import numpy as np
 import matplotlib.pyplot as plt
 
-from mne import Epochs, find_events
+from mne import Epochs, find_events, create_info
 from mne.io import concatenate_raws, read_raw_edf
 from mne.datasets import eegbci
 from mne.decoding import CSP
+from mne.time_frequency import AverageTFR
 
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.model_selection import KFold, cross_val_score
@@ -58,13 +59,18 @@ n_freqs = 8  # how many frequency bins to use
 
 # Assemble list of frequency range tuples
 freqs = np.linspace(min_freq, max_freq, n_freqs)  # assemble frequencies
-freq_ranges = zip(freqs[:-1], freqs[1:])  # make freqs into a list of tuples
+freq_ranges = list(zip(freqs[:-1], freqs[1:]))  # make freqs into a list of tuples
+
+# Infer window spacing from the max freq and number of cycles to avoid gaps
+window_spacing = (n_cycles / np.max(freqs) / 2.)
+centered_w_times = np.arange(tmin, tmax, window_spacing)[1:]
+n_windows = len(centered_w_times)
 
 ###############################################################################
 # Loop through frequencies, apply classifier and save scores
 
 # init scores
-scores = np.zeros((n_freqs - 1,))
+f_scores = np.zeros((n_freqs - 1,))
 
 # Loop through each frequency range of interest
 for freq, (fmin, fmax) in enumerate(freq_ranges):
@@ -84,15 +90,61 @@ for freq, (fmin, fmax) in enumerate(freq_ranges):
     X = epochs.copy().get_data()
 
     # Save mean scores over folds for each frequency and time window
-    scores[freq, ] = np.mean(cross_val_score(estimator=clf, X=X, y=y,
-                                             cv=cv, n_jobs=1), axis=0)
+    f_scores[freq, ] = np.mean(cross_val_score(estimator=clf, X=X, y=y,
+                                               cv=cv, n_jobs=1), axis=0)
 
 ###############################################################################
-# Plot results
+# Plot frequency results
 
-plt.bar(left=freqs[:-1], height=scores, width=np.diff(freqs)[0], align='edge',
-        edgecolor='black')
+plt.bar(left=freqs[:-1], height=f_scores, width=np.diff(freqs)[0], 
+        align='edge', edgecolor='black')
 plt.xticks(freqs)
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Decoding Scores')
 plt.title('Frequency Decoding Scores')
+
+###############################################################################
+# Loop through frequencies and time, apply classifier and save scores
+
+# init scores
+tf_scores = np.zeros((n_freqs - 1, n_windows))
+
+# Loop through each frequency range of interest
+for freq, (fmin, fmax) in enumerate(freq_ranges):
+
+    # Infer window size based on the frequency being used
+    w_size = n_cycles / ((fmax + fmin) / 2.)  # in seconds
+
+    # Apply band-pass filter to isolate the specified frequencies
+    raw_filter = raw.copy().filter(fmin, fmax, n_jobs=1, fir_design='firwin')
+
+    # Extract epochs from filtered data, padded by window size
+    epochs = Epochs(raw_filter, events, event_id, tmin - w_size, tmax + w_size,
+                    proj=False, baseline=None, preload=True)
+    epochs.drop_bad()
+    y = epochs.events[:, 2] - 2
+
+    # Roll covariance, csp and lda over time
+    for t, w_time in enumerate(centered_w_times):
+
+        # Center the min and max of the window
+        w_tmin = w_time - w_size / 2.
+        w_tmax = w_time + w_size / 2.
+
+        # Crop data into time-window of interest
+        X = epochs.copy().crop(w_tmin, w_tmax).get_data()
+
+        # Save mean scores over folds for each frequency and time window
+        tf_scores[freq, t] = np.mean(cross_val_score(estimator=clf, X=X, y=y,
+                                                     cv=cv, n_jobs=1), axis=0)
+
+###############################################################################
+# Plot time-frequency results
+
+# Set up time frequency object
+av_tfr = AverageTFR(create_info(['freq'], sfreq), tf_scores[np.newaxis, :],
+                    centered_w_times, freqs[1:], 1)
+
+chance = np.mean(y)  # set chance level to white in the plot
+av_tfr.plot([0], vmin=chance, title="Time-Frequency Decoding Scores",
+            cmap=plt.cm.Reds)
