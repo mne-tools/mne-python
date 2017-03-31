7eed3be4648712ca59b6b9c36b8ce6a91c8ff152
Teon L Brooks <teon.brooks@gmail.com>
2017-03-31 17:17:29 -0400

    UPDATE lsl client, base_client, FIX epochs

--- mne/realtime/lsl_client.py
+++ mne/realtime/lsl_client.py
@@ -43,8 +43,27 @@ class LSLClient(_BaseClient):
         self.verbose = verbose
 
     def connect(self):
-        self.client = pylsl.resolve_byprop('source_id', self.identifier,
+        stream = pylsl.resolve_byprop('source_id', self.identifier,
                                            timeout=self.wait_max)[0]
+        self.client = pylsl.StreamInlet(stream)
+
+        return self
+
+    def create_info(self):
+        sfreq = self.client.info().nominal_srate()
+
+        lsl_info = self.client.info()
+        ch_info = lsl_info.desc().child("channels").child("channel")
+        ch_names = list()
+        ch_types = list()
+        for k in range(lsl_info.channel_count()):
+            ch_names.append(ch_info.child_value("label"))
+            ch_types.append(ch_info.child_value("type"))
+            ch_info = ch_info.next_sibling()
+
+        info = create_info(ch_names, sfreq, ch_types)
+
+        self.info = info
 
         return self
 
@@ -53,7 +72,9 @@ class LSLClient(_BaseClient):
         """
         inlet = pylsl.StreamInlet(self.client)
 
-        while True:
-            sample, timestamp = inlet.pull_chunk(max_samples=self.buffer_size)
+        ## add tmin and tmax to this logic
 
-            yield sample
+        while True:
+            samples, timestamps = inlet.pull_chunk(max_samples=self.buffer_size)
+
+            yield samples
