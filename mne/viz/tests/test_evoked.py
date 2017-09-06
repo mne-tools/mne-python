6cc3bdf67d5849c4b802f8f8dd57e30859b5232b
jona-sassenhagen <jona.sassenhagen@gmail.com>
2017-09-06 15:47:11 +0200

    fix tests

--- mne/viz/tests/test_evoked.py
+++ mne/viz/tests/test_evoked.py
@@ -18,8 +18,8 @@ from nose.tools import assert_true
 from mne import read_events, Epochs, pick_types, read_cov
 from mne.channels import read_layout
 from mne.io import read_raw_fif
-from mne.utils import slow_test, run_tests_if_main, _parametric_ci
-from mne.viz.evoked import (_line_plot_onselect, plot_compare_evokeds)
+from mne.utils import slow_test, run_tests_if_main
+from mne.viz.evoked import _line_plot_onselect, plot_compare_evokeds
 from mne.viz.utils import _fake_click
 
 # Set our plotters to test mode
