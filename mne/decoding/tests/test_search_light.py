eccaf028b2c594b862975ae9e1c5cc92f625927e
Jean-Rémi KING <jeanremi.king@gmail.com>
2017-09-12 02:49:42 -0400

    MRG: compatibility with bagging classifiers (#4547)
    * fix: compatibility with bagging classifiers
    
    * add comments and joblib tests

--- mne/decoding/tests/test_search_light.py
+++ mne/decoding/tests/test_search_light.py
@@ -28,6 +28,7 @@ def test_search_light():
     from sklearn.linear_model import Ridge, LogisticRegression
     from sklearn.pipeline import make_pipeline
     from sklearn.metrics import roc_auc_score, make_scorer
+    from sklearn.ensemble import BaggingClassifier
 
     X, y = make_data()
     n_epochs, _, n_time = X.shape
@@ -138,6 +139,14 @@ def test_search_light():
         features_shape = pipe.estimators_[0].steps[0][1].features_shape_
         assert_array_equal(features_shape, [3, 4])
     assert_array_equal(y_preds[0], y_preds[1])
+
+    # Bagging classifiers
+    X = np.random.rand(10, 3, 4)
+    for n_jobs in (1, 2):
+        pipe = SlidingEstimator(BaggingClassifier(None, 2), n_jobs=n_jobs)
+        pipe.fit(X, y)
+        pipe.score(X, y)
+        assert_true(isinstance(pipe.estimators_[0], BaggingClassifier))
 
 
 @requires_sklearn_0_15
