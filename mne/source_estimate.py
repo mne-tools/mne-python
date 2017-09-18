FIX: Simplify calculation

mne/source_estimate.py
@@ -2639,22 +2639,19 @@ def morph_data_precomputed(subject_from, subject_to, stc_from, vertices_to,
     return stc_to
 
 
-def _spatio_temporal_src_connectivity_vol(src, n_times):
+def _get_vol_mask(src):
+    """Get the volume source space mask."""
     assert len(src) == 1  # not a mixed source space
+    shape = src[0]['shape'][::-1]
+    mask = np.zeros(shape, bool)
+    mask.flat[src[0]['vertno']] = True
+    return mask
+
+
+def _spatio_temporal_src_connectivity_vol(src, n_times):
     from sklearn.feature_extraction import grid_to_graph
-    vertices = np.where(src[0]['inuse'])[0]
-    n_vertices = len(vertices)
-    data = (1 + np.arange(n_vertices))[:, np.newaxis]
-    # XXX these lines can certainly be replaced by direct logic on the source
-    # space, rather than doing a temp STC, volume export, then data grab. Have
-    # you looked? This would also increase the number of volume source spaces
-    # that can be used to those without MRI, like volume source space within
-    # a generic sphere, because as_volume requires MRI vol source space)
-    stc_tmp = VolSourceEstimate(data, vertices, tmin=0., tstep=1.)
-    img = stc_tmp.as_volume(src, mri_resolution=False)
-    img_data = img.get_data()[:, :, :, 0]
-    img_data = img_data.T
-    edges = grid_to_graph(*img_data.shape, mask=(img_data != 0))
+    mask = _get_vol_mask(src)
+    edges = grid_to_graph(*mask.shape, mask=mask)
     connectivity = _get_connectivity_from_edges(edges, n_times)
     return connectivity
 

mne/tests/test_source_estimate.py
@@ -20,7 +20,7 @@ from mne import (stats, SourceEstimate, VectorSourceEstimate,
                  spatial_inter_hemi_connectivity,
                  spatial_src_connectivity)
 from mne.source_estimate import (compute_morph_matrix, grade_to_vertices,
-                                 grade_to_tris)
+                                 grade_to_tris, _get_vol_mask)
 
 from mne.minimum_norm import read_inverse_operator, apply_inverse
 from mne.label import read_labels_from_annot, label_sign_flip
@@ -887,9 +887,9 @@ def test_vec_stc():
 
 
 @requires_sklearn
-@requires_nibabel()
 @testing.requires_testing_data
 def test_vol_connectivity():
+    """Test volume connectivity."""
     from scipy import sparse
     vol = read_source_spaces(fname_vsrc)
 
@@ -904,6 +904,27 @@ def test_vol_connectivity():
     connectivity2 = spatio_temporal_src_connectivity(vol, n_times=2)
     assert_equal(connectivity2.shape, (2 * n_vertices, 2 * n_vertices))
     assert_true(np.all(connectivity2.data == 1))
+
+
+@requires_sklearn
+@requires_nibabel()
+@testing.requires_testing_data
+def test_vol_mask():
+    """Test extraction of volume mask."""
+    src = read_source_spaces(fname_vsrc)
+    mask = _get_vol_mask(src)
+    # Let's use an alternative way that should be equivalent
+    vertices = src[0]['vertno']
+    n_vertices = len(vertices)
+    data = (1 + np.arange(n_vertices))[:, np.newaxis]
+    stc_tmp = VolSourceEstimate(data, vertices, tmin=0., tstep=1.)
+    img = stc_tmp.as_volume(src, mri_resolution=False)
+    img_data = img.get_data()[:, :, :, 0].T
+    mask_nib = (img_data != 0)
+    assert_array_equal(img_data[mask_nib], data[:, 0])
+    assert_array_equal(np.where(mask_nib.ravel())[0], src[0]['vertno'])
+    assert_array_equal(mask, mask_nib)
+    assert_array_equal(img_data.shape, mask.shape)
 
 
 run_tests_if_main()