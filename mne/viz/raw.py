ae581feedbb23f6cee7277947ae73e23d8f80a30
Clemens Brunner <clemens.brunner@gmail.com>
2017-09-19 09:32:34 +0200

    Only plot event numbers if there are less than 51 per page

--- mne/viz/raw.py
+++ mne/viz/raw.py
@@ -972,11 +972,12 @@ def _plot_raw_traces(params, color, bad_color, event_lines=None,
             else:
                 line.set_xdata([])
                 line.set_ydata([])
-        params['ax'].texts = []
-        for ev_time, ev_num in zip(event_times, event_nums):
-            if -1 in event_color or ev_num in event_color:
-                params['ax'].text(ev_time, -0.05, ev_num, fontsize=8,
-                                  ha='center')
+        if len(event_times) <= 50:
+            params['ax'].texts = []
+            for ev_time, ev_num in zip(event_times, event_nums):
+                if -1 in event_color or ev_num in event_color:
+                    params['ax'].text(ev_time, -0.05, ev_num, fontsize=8,
+                                      ha='center')
 
     if 'segments' in params:
         while len(params['ax'].collections) > 0:
