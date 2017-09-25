ee1ed05d27394f3d6df953019cac0b157455fd3c
Jussi Nurminen <jnu@iki.fi>
2017-09-25 14:42:47 +0300

    compare hf vs unfiltered, single ch

--- examples/datasets/plot_hf_sef_data.py
+++ examples/datasets/plot_hf_sef_data.py
@@ -3,7 +3,8 @@
 HF-SEF dataset
 ==============
 
-This example looks at high frequency SEF responses
+This example looks at high frequency SEF responses.
+
 """
 # Author: Jussi Nurminen (jnu@iki.fi)
 #
@@ -15,12 +16,22 @@ import os
 from mne.datasets import hf_sef
 
 fname_evoked = os.path.join(hf_sef.data_path(),
-                            'MEG/subject_a/sef2_right-ave.fif')
+                            'MEG/subject_b/hf_sef_15min-ave.fif')
 
 print(__doc__)
 
 ###############################################################################
-# Read raw file
+# Read evoked data
 evoked = mne.Evoked(fname_evoked)
 
-evoked.filter(l_freq=200, h_freq=400).plot()
+###############################################################################
+# Create a highpass filtered version
+evoked_hp = evoked.copy()
+evoked_hp.filter(l_freq=300, h_freq=None)
+
+###############################################################################
+# Compare high-pass and unfiltered data on a single channel
+ch = 'MEG0443'
+pick = evoked.ch_names.index(ch)
+edi = {'HF': evoked_hp, 'Regular': evoked}
+mne.viz.plot_compare_evokeds(edi, picks=pick)
