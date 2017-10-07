Fix ori computation: change method to compute depth weighting

mne/inverse_sparse/_gamma_map.py
@@ -277,10 +277,8 @@ def gamma_map(evoked, forward, noise_cov, alpha, loose="auto", depth=0.8,
 
     # Reapply weights to have correct unit
     n_dip_per_pos = 1 if is_fixed_orient(forward) else 3
-    X_ori = X.copy()
     X = _reapply_source_weighting(X, source_weighting,
                                   active_set, n_dip_per_pos)
-    X_ori /= source_weighting[active_set][:, None]
 
     if return_residual:
         residual = _compute_residual(forward, evoked, X, active_set,
@@ -292,25 +290,21 @@ def gamma_map(evoked, forward, noise_cov, alpha, loose="auto", depth=0.8,
         in_pos = 0
         if len(X) < 3 * len(active_src):
             X_xyz = np.zeros((3 * len(active_src), X.shape[1]), dtype=X.dtype)
-            X_ori_ = np.zeros((3 * len(active_src), X.shape[1]), dtype=X.dtype)
             for ii in range(len(active_src)):
                 for jj in range(3):
                     if in_pos >= len(active_set):
                         break
                     if (active_set[in_pos] + jj) % 3 == 0:
                         X_xyz[3 * ii + jj] = X[in_pos]
-                        X_ori_[3 * ii + jj] = X_ori[in_pos]
                         in_pos += 1
             X = X_xyz
-            X_ori = X_ori_
 
     tmin = evoked.times[0]
     tstep = 1.0 / evoked.info['sfreq']
 
     if return_as_dipoles:
         out = _make_dipoles_sparse(X, active_set, forward, tmin, tstep, M,
-                                   M_estimated, active_is_idx=True,
-                                   X_ori=X_ori)
+                                   M_estimated, active_is_idx=True)
     else:
         out = _make_sparse_stc(X, active_set, forward, tmin, tstep,
                                active_is_idx=True, verbose=verbose)

mne/inverse_sparse/mxne_inverse.py
@@ -60,16 +60,26 @@ def _prepare_gain_column(forward, info, noise_cov, pca, depth, loose, weights,
 
     logger.info('Whitening lead field matrix.')
     gain = np.dot(whitener, gain)
+    is_fixed_ori = is_fixed_orient(forward)
 
     if depth is not None:
-        depth_prior = np.sum(gain ** 2, axis=0) ** depth
-        # Spherical leadfield can be zero at the center
-        depth_prior[depth_prior == 0.] = np.min(depth_prior[depth_prior != 0.])
-        source_weighting = np.sqrt(1. / depth_prior)
+        if is_fixed_ori:
+            depth_prior = np.sum(gain ** 2, axis=0) ** depth
+            # Spherical leadfield can be zero at the center
+            depth_prior[depth_prior == 0.] = np.min(
+                depth_prior[depth_prior != 0.])
+            source_weighting = np.sqrt(1. / depth_prior)
+        else:
+            depth_prior = np.sum(gain ** 2, axis=0)
+            # Spherical leadfield can be zero at the center
+            depth_prior[depth_prior == 0.] = np.min(
+                depth_prior[depth_prior != 0.])
+            depth_prior = depth_prior.reshape(-1, 3).sum(axis=1) ** depth
+            source_weighting = np.repeat(1. / np.sqrt(depth_prior), 3)
     else:
         source_weighting = np.ones(gain.shape[1], dtype=gain.dtype)
 
-    assert (is_fixed_orient(forward) or (0 <= loose <= 1))
+    assert (is_fixed_ori or (0 <= loose <= 1))
     if loose is not None and loose < 1.:
         source_weighting *= np.sqrt(compute_orient_prior(forward, loose))
 
@@ -176,11 +186,7 @@ def _make_sparse_stc(X, active_set, forward, tmin, tstep,
 
 @verbose
 def _make_dipoles_sparse(X, active_set, forward, tmin, tstep, M, M_est,
-                         active_is_idx=False, X_ori=None, verbose=None):
-
-    if X_ori is None:
-        X_ori = X
-
+                         active_is_idx=False, verbose=None):
     times = tmin + tstep * np.arange(X.shape[1])
 
     if not active_is_idx:
@@ -203,7 +209,6 @@ def _make_dipoles_sparse(X, active_set, forward, tmin, tstep, M, M_est,
         i_pos = forward['source_rr'][i_dip][np.newaxis, :]
         i_pos = i_pos.repeat(len(times), axis=0)
         X_ = X[k * n_dip_per_pos: (k + 1) * n_dip_per_pos]
-        X_ori_ = X_ori[k * n_dip_per_pos: (k + 1) * n_dip_per_pos]
         if n_dip_per_pos == 1:
             amplitude = X_[0]
             i_ori = forward['source_nn'][i_dip][np.newaxis, :]
@@ -213,12 +218,11 @@ def _make_dipoles_sparse(X, active_set, forward, tmin, tstep, M, M_est,
                 X_ = np.dot(forward['source_nn'][i_dip *
                             n_dip_per_pos:(i_dip + 1) * n_dip_per_pos].T, X_)
 
-            amplitude = np.sqrt(np.sum(X_ori_ ** 2, axis=0))
+            amplitude = np.sqrt(np.sum(X_ ** 2, axis=0))
             i_ori = np.zeros((len(times), 3))
-            i_ori[amplitude > 0.] = (X_ori_[:, amplitude > 0.] /
+            i_ori[amplitude > 0.] = (X_[:, amplitude > 0.] /
                                      amplitude[amplitude > 0.]).T
 
-            amplitude = np.sqrt(np.sum(X_ ** 2, axis=0))
         dipoles.append(Dipole(times, i_pos, amplitude, i_ori, gof))
 
     return dipoles
@@ -441,10 +445,8 @@ def mixed_norm(evoked, forward, noise_cov, alpha, loose='auto', depth=0.8,
         raise Exception("No active dipoles found. alpha is too big.")
 
     # Reapply weights to have correct unit
-    X_ori = X.copy()
     X = _reapply_source_weighting(X, source_weighting,
                                   active_set, n_dip_per_pos)
-    X_ori /= source_weighting[active_set][:, None]
 
     outs = list()
     residual = list()
@@ -453,13 +455,11 @@ def mixed_norm(evoked, forward, noise_cov, alpha, loose='auto', depth=0.8,
         tmin = e.times[0]
         tstep = 1.0 / e.info['sfreq']
         Xe = X[:, cnt:(cnt + len(e.times))]
-        Xe_ori = X_ori[:, cnt:(cnt + len(e.times))]
         if return_as_dipoles:
             out = _make_dipoles_sparse(
                 Xe, active_set, forward, tmin, tstep,
                 M[:, cnt:(cnt + len(e.times))],
-                M_estimated[:, cnt:(cnt + len(e.times))],
-                X_ori=Xe_ori, verbose=None)
+                M_estimated[:, cnt:(cnt + len(e.times))], verbose=None)
         else:
             out = _make_sparse_stc(Xe, active_set, forward, tmin, tstep)
         outs.append(out)
@@ -657,10 +657,8 @@ def tf_mixed_norm(evoked, forward, noise_cov, alpha_space, alpha_time,
         active_set = active_set_tmp
         del active_set_tmp
 
-    X_ori = X.copy()
     X = _reapply_source_weighting(
         X, source_weighting, active_set, n_dip_per_pos)
-    X_ori /= source_weighting[active_set][:, None]
 
     if return_residual:
         residual = _compute_residual(
@@ -669,7 +667,7 @@ def tf_mixed_norm(evoked, forward, noise_cov, alpha_space, alpha_time,
     if return_as_dipoles:
         out = _make_dipoles_sparse(
             X, active_set, forward, evoked.times[0], 1.0 / info['sfreq'],
-            M, M_estimated, X_ori=X_ori, verbose=None)
+            M, M_estimated, verbose=None)
     else:
         out = _make_sparse_stc(
             X, active_set, forward, evoked.times[0], 1.0 / info['sfreq'])

mne/inverse_sparse/tests/test_mxne_inverse.py
@@ -172,18 +172,16 @@ def test_mxne_vol_sphere():
 
     # Compare orientation obtained using fit_dipole and mixed_norm
     # for a simulated evoked
-    pos1 = src[0]['rr'][stc.vertices[0]]
-    pos2 = src[0]['rr'][stc.vertices[1]]
+    pos = src[0]['rr'][stc.vertices[0]]
     evoked_dip = mne.simulation.simulate_evoked(fwd, stc, info, cov, nave=1e9)
 
-    dip_mxne = mixed_norm(evoked_dip, fwd, cov, alpha=30,
+    dip_mxne = mixed_norm(evoked_dip, fwd, cov, alpha=80,
                           n_mxne_iter=1, maxit=30, tol=1e-8,
                           active_set_size=10, return_as_dipoles=True)
-    assert_true(dip_mxne[0].pos[0] in np.array([pos1, pos2]))
-    assert_true(dip_mxne[1].pos[0] in np.array([pos1, pos2]))
 
     amp_max = [np.max(d.amplitude) for d in dip_mxne]
     dip_mxne = dip_mxne[np.argmax(amp_max)]
+    assert_true(dip_mxne[0].pos[0] in pos)
 
     dip_fit = mne.fit_dipole(evoked_dip, cov, sphere)[0]
     assert_true(np.max(np.abs(np.dot(dip_fit.ori,