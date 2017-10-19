c66e5ace32dcb78e0d0fc788ab9199e667d6147c
Clemens Brunner <clemens.brunner@gmail.com>
2017-10-19 16:01:48 +0200

    [MRG] Fix browse raw error when raw has no title (#4683)
    * Fix browse raw error when raw has no title
    
    * Use ternary expression

--- mne/viz/raw.py
+++ mne/viz/raw.py
@@ -834,7 +834,7 @@ def _prepare_mne_browse_raw(params, title, bgcolor, color, bad_color, inds,
         size = tuple([float(s) for s in size])
 
     fig = figure_nobar(facecolor=bgcolor, figsize=size)
-    fig.canvas.set_window_title(title)
+    fig.canvas.set_window_title(title if title else "Raw")
     ax = plt.subplot2grid((10, 10), (0, 1), colspan=8, rowspan=9)
     ax_hscroll = plt.subplot2grid((10, 10), (9, 1), colspan=8)
     ax_hscroll.get_yaxis().set_visible(False)
