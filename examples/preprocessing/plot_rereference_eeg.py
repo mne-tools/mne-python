fb00263b1273faa609cdc9066c41f6e5839647ff
Clemens Brunner <clemens.brunner@gmail.com>
2017-10-24 12:53:13 +0200

    Fix rereferencing (ref_channels=[] didn't remove existing average projections)

--- examples/preprocessing/plot_rereference_eeg.py
+++ examples/preprocessing/plot_rereference_eeg.py
@@ -44,18 +44,22 @@ fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, ncols=1, sharex=True)
 raw.set_eeg_reference([])
 evoked_no_ref = mne.Epochs(raw, **epochs_params).average()
 
-evoked_no_ref.plot(axes=ax1, titles=dict(eeg='EEG Original reference'))
+evoked_no_ref.plot(axes=ax1, titles=dict(eeg='EEG Original reference'),
+                   show=False)
 
 # Average reference. This is normally added by default, but can also be added
 # explicitly.
-raw.set_eeg_reference('average', projection=True)
+raw.set_eeg_reference('average', projection=False)
 evoked_car = mne.Epochs(raw, **epochs_params).average()
 
-evoked_car.plot(axes=ax2, titles=dict(eeg='EEG Average reference'))
+evoked_car.plot(axes=ax2, titles=dict(eeg='EEG Average reference'), show=False)
 
 # Re-reference from an average reference to the mean of channels EEG 001 and
 # EEG 002.
 raw.set_eeg_reference(['EEG 001', 'EEG 002'])
 evoked_custom = mne.Epochs(raw, **epochs_params).average()
 
-evoked_custom.plot(axes=ax3, titles=dict(eeg='EEG Custom reference'))
+evoked_custom.plot(axes=ax3, titles=dict(eeg='EEG Custom reference'),
+                   show=False)
+
+fig.show()
