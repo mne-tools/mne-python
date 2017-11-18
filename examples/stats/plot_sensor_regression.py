3419e27442c8fcfc97ad2f89cdc0821e17e5a55f
jona-sassenhagen <jona.sassenhagen@gmail.com>
2017-11-18 14:27:00 +0100

    init

--- examples/stats/plot_sensor_regression.py
+++ examples/stats/plot_sensor_regression.py
@@ -10,68 +10,38 @@ regression coefficient (beta value) for each combination of sensor
 and timepoint. Example also shows the T statistics and the associated
 p-values.
 
-Note that this example is for educational purposes and that the data used
-here do not contain any significant effect.
+Here, we repeat a few of the analyses from Dufau et al. (2014) by accessing
+the metadata object, which contains word-level information about various
+psycholinguistically relevant features of the words for which we have EEG
+activity.
 
-(See Hauk et al. (2006). The time course of visual word recognition as
-revealed by linear regression analysis of ERP data. Neuroimage.)
+For the general methodology, see e.g. Hauk et al. (2006). The time course of 
+visual word recognition as revealed by linear regression analysis of ERP data. 
+Neuroimage.)
 """
 # Authors: Tal Linzen <linzen@nyu.edu>
 #          Denis A. Engemann <denis.engemann@gmail.com>
+#          Jona Sassenhagen <jona.sassenhagen@gmail.com>
 #
 # License: BSD (3-clause)
 
-import numpy as np
-
+import pandas as pd
 import mne
-from mne.datasets import sample
-from mne.stats.regression import linear_regression
+from mne.stats import linear_regression
+from mne.datasets import kiloword
 
-print(__doc__)
+# Load the data
+path = kiloword.data_path() + '/kword_metadata-epo.fif'
+epochs = mne.read_epochs(path)
+print(epochs.metadata.head())
 
-data_path = sample.data_path()
+# Add intercept column
+df = pd.DataFrame(epochs.metadata)
+epochs.metadata = df.assign(Intercept=np.ones(len(epochs.events)))
 
-###############################################################################
-# Set parameters and read data
-raw_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
-event_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw-eve.fif'
-tmin, tmax = -0.2, 0.5
-event_id = dict(aud_l=1, aud_r=2)
+# Run and visualize the regression
+names = ["Intercept", "Concreteness", "BigramFrequency"]
+res = linear_regression(epochs, epochs.metadata[names], names=names)
 
-# Setup for reading the raw data
-raw = mne.io.read_raw_fif(raw_fname)
-events = mne.read_events(event_fname)
-
-picks = mne.pick_types(raw.info, meg='mag', eeg=False, stim=False,
-                       eog=False, exclude='bads')
-
-# Reject some epochs based on amplitude
-reject = dict(mag=5e-12)
-epochs = mne.Epochs(raw, events, event_id, tmin, tmax, proj=True,
-                    picks=picks, baseline=(None, 0), preload=True,
-                    reject=reject)
-
-###############################################################################
-# Run regression
-
-names = ['intercept', 'trial-count']
-
-intercept = np.ones((len(epochs),), dtype=np.float)
-design_matrix = np.column_stack([intercept,  # intercept
-                                 np.linspace(0, 1, len(intercept))])
-
-# also accepts source estimates
-lm = linear_regression(epochs, design_matrix, names)
-
-
-def plot_topomap(x, units):
-    x.plot_topomap(ch_type='mag', scalings=1., size=1.5, vmax=np.max,
-                   units=units, times=np.linspace(0.1, 0.2, 5))
-
-
-trial_count = lm['trial-count']
-
-plot_topomap(trial_count.beta, units='z (beta)')
-plot_topomap(trial_count.t_val, units='t')
-plot_topomap(trial_count.mlog10_p_val, units='-log10 p')
-plot_topomap(trial_count.stderr, units='z (error)')
+for cond in names:
+    res[cond].beta.plot_image(titles=cond)
