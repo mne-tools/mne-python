2f21c1b3a8146501fefd770e466095053651bb33
Jean-Rémi KING <jeanremi.king@gmail.com>
2017-12-18 09:30:07 -0500

    fix _estimator_type for cross_valmultiscore (#4838)
    * fix _estimator_type for cross_valmultiscore
    
    *  whats new
    
    * add manual tests of cv cross_valmultiscore

--- mne/decoding/tests/test_base.py
+++ mne/decoding/tests/test_base.py
@@ -193,8 +193,8 @@ def test_linearmodel():
 @requires_version('sklearn', '0.18')
 def test_cross_val_multiscore():
     """Test cross_val_multiscore for computing scores on decoding over time."""
-    from sklearn.model_selection import KFold, cross_val_score
-    from sklearn.linear_model import LogisticRegression
+    from sklearn.model_selection import KFold, StratifiedKFold, cross_val_score
+    from sklearn.linear_model import LogisticRegression, LinearRegression
 
     # compare to cross-val-score
     X = np.random.rand(20, 3)
@@ -231,3 +231,20 @@ def test_cross_val_multiscore():
         clf.fit(X[train], y[train])
         scores_auc_manual.append(clf.score(X[test], y[test]))
     assert_array_equal(scores_auc, scores_auc_manual)
+
+    # indirectly test that cross_val_multiscore rightly detects the type of
+    # estimator and generates a StratifiedKFold for classiers and a KFold
+    # otherwise
+    X = np.random.randn(1000, 3)
+    y = np.r_[np.zeros(500), np.ones(500)]
+    clf = LogisticRegression(random_state=0)
+    reg = LinearRegression()
+    for cross_val in (cross_val_score, cross_val_multiscore):
+        manual = cross_val(clf, X, y, cv=StratifiedKFold(2))
+        auto = cross_val(clf, X, y, cv=2)
+        assert_array_equal(manual, auto)
+        assert_raises(ValueError, cross_val, clf, X, y, cv=KFold(2))
+
+        manual = cross_val(reg, X, y, cv=KFold(2))
+        auto = cross_val(reg, X, y, cv=2)
+        assert_array_equal(manual, auto)
