FIX: Fix computation of average quaternion

mne/preprocessing/maxwell.py
@@ -18,7 +18,7 @@ from ..bem import _check_origin
 from ..chpi import quat_to_rot, rot_to_quat
 from ..transforms import (_str_to_frame, _get_trans, Transform, apply_trans,
                           _find_vector_rotation, _cart_to_sph, _get_n_moments,
-                          _sph_to_cart_partials, _deg_ord_idx,
+                          _sph_to_cart_partials, _deg_ord_idx, _average_quats,
                           _sh_complex_to_real, _sh_real_to_complex, _sh_negate)
 from ..forward import _concatenate_coils, _prep_meg_channels, _create_meg_coils
 from ..surface import _normalize_vectors
@@ -658,10 +658,7 @@ def _trans_starts_stops_quats(pos, start, stop, this_pos_data):
     rel_starts = list()
     rel_stops = list()
     quats = list()
-    if this_pos_data is None:
-        avg_trans = None
-    else:
-        avg_trans = np.zeros(6)
+    weights = list()
     for ti in range(-1, len(pos_idx)):
         # first iteration for this block of data
         if ti < 0:
@@ -688,15 +685,19 @@ def _trans_starts_stops_quats(pos, start, stop, this_pos_data):
         used[rel_start:rel_stop] = True
         rel_starts.append(rel_start)
         rel_stops.append(rel_stop)
-        if this_pos_data is not None:
-            avg_trans += quats[-1][:6] * (rel_stop - rel_start)
+        weights.append(rel_stop - rel_start)
     assert used.all()
     # Use weighted average for average trans over the window
-    if avg_trans is not None:
-        avg_trans /= (stop - start)
+    if this_pos_data is None:
+        avg_trans = None
+    else:
+        weights = np.array(weights)
+        quats = np.array(quats)
+        weights /= weights.sum()
+        avg_quat = _average_quats(quats[:, :3], weights)
+        avg_t = np.dot(weights, quats[:, 3:6])
         avg_trans = np.vstack([
-            np.hstack([quat_to_rot(avg_trans[:3]),
-                       avg_trans[3:][:, np.newaxis]]),
+            np.hstack([quat_to_rot(avg_quat), avg_t[:, np.newaxis]]),
             [[0., 0., 0., 1.]]])
     return trans, rel_starts, rel_stops, quats, avg_trans
 
@@ -950,8 +951,7 @@ def _col_norm_pinv(x):
     """
     norm = np.sqrt(np.sum(x * x, axis=0))
     x /= norm
-    u, s, v = linalg.svd(x, full_matrices=False, overwrite_a=True,
-                         **check_disable)
+    u, s, v = _safe_svd(x, full_matrices=False, **check_disable)
     v /= norm
     return np.dot(v.T * 1. / s, u.T), s
 
@@ -1693,8 +1693,7 @@ def _regularize_in(int_order, ext_order, S_decomp, mag_or_fine):
     noise_lev *= noise_lev  # effectively what would happen by earlier multiply
     for ii in range(n_in):
         this_S = S_decomp.take(in_keepers + out_keepers, axis=1)
-        u, s, v = linalg.svd(this_S, full_matrices=False, overwrite_a=True,
-                             **check_disable)
+        u, s, v = _safe_svd(this_S, full_matrices=False, **check_disable)
         del this_S
         eigs[ii] = s[[0, -1]]
         v = v.T[:len(in_keepers)]