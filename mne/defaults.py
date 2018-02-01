e29d1bde09434e675336b9bbc7a9c92af88ca3ec
kostiantyn maksymenko <kostiantyn.maksymenko@inria.fr>
2018-02-01 14:52:53 +0100

    adjust scale seeg

--- mne/defaults.py
+++ mne/defaults.py
@@ -20,7 +20,7 @@ DEFAULTS = dict(
     scalings_plot_raw=dict(mag=1e-12, grad=4e-11, eeg=20e-6, eog=150e-6,
                            ecg=5e-4, emg=1e-3, ref_meg=1e-12, misc=1e-3,
                            stim=1, resp=1, chpi=1e-4, exci=1, ias=1, syst=1,
-                           seeg=1e-5, bio=1e-6, ecog=1e-4, hbo=10e-6,
+                           seeg=1e-4, bio=1e-6, ecog=1e-4, hbo=10e-6,
                            hbr=10e-6),
     scalings_cov_rank=dict(mag=1e12, grad=1e11, eeg=1e5),
     ylim=dict(mag=(-600., 600.), grad=(-200., 200.), eeg=(-200., 200.),
