81bc7cd38e65de6ca9f326164a08e7083bdd5345
kostiantyn maksymenko <kostiantyn.maksymenko@inria.fr>
2018-02-21 16:35:34 +0100

    Add functions to create random parcellation

--- mne/label.py
+++ mne/label.py
@@ -13,6 +13,7 @@ import re
 
 import numpy as np
 from scipy import linalg, sparse
+import random
 
 from .utils import get_subjects_dir, _check_subject, logger, verbose, warn
 from .source_estimate import (morph_data, SourceEstimate, _center_of_mass,
@@ -1655,6 +1656,159 @@ def _grow_nonoverlapping_labels(subject, seeds_, extents_, hemis, vertices_,
             name = str(names[i])
             label_ = Label(vertices, hemi=hemi, name=name, subject=subject)
             labels.append(label_)
+
+    return labels
+
+
+def random_parcellation(subject, n_parcel, hemis, subjects_dir=None,
+                        surface='white'):
+    """Generate random cortex parcellation by growing labels.
+
+        This function generates a number of labels which don't intersect and
+        cover the whole surface. Regions are growing around randomly chosen
+        seeds.
+
+        Parameters
+        ----------
+        subject : string
+            Name of the subject as in SUBJECTS_DIR.
+        n_parcel : int
+            Total number of cortical parcels
+        hemis : array | int
+            Hemispheres to use for the labels (0: left, 1: right).
+        subjects_dir : string
+            Path to SUBJECTS_DIR if not set in the environment.
+        surface : string
+            The surface used to grow the labels, defaults to the white surface.
+
+        Returns
+        -------
+        labels : list of Label
+            Random cortex parcellation
+        """
+    subjects_dir = get_subjects_dir(subjects_dir, raise_error=True)
+    hemis = np.atleast_1d(hemis)
+    hemis = np.array(['lh' if h == 0 else 'rh' for h in hemis])
+
+    # load the surfaces and create the distance graphs
+    tris, vert, dist = {}, {}, {}
+    for hemi in set(hemis):
+        surf_fname = op.join(subjects_dir, subject, 'surf', hemi + '.' +
+                             surface)
+        vert[hemi], tris[hemi] = read_surface(surf_fname)
+        dist[hemi] = mesh_dist(tris[hemi], vert[hemi])
+
+    # create the patches
+    labels = _cortex_parcellation(subject, n_parcel, hemis, vert, dist)
+
+    # add a unique color to each label
+    colors = _n_colors(len(labels))
+    for label, color in zip(labels, colors):
+        label.color = color
+
+    return labels
+
+
+def _cortex_parcellation(subject, n_parcel, hemis, vertices_, graphs):
+    """Random cortex parcellation"""
+    labels = []
+    parcel_size = np.floor((len(vertices_[hemis[0]]) +
+                            len(vertices_[hemis[1]]))/n_parcel)
+    parc_both = []
+    for hemi in set(hemis):
+        graph = graphs[hemi]  # distance graph
+        n_vertices = len(vertices_[hemi])
+
+        # prepare parcellation
+        parc = np.empty(n_vertices, dtype='int32')
+        parc[:] = -1
+
+        # initialize active sources
+        s = 0
+        s = random.choice(range(n_vertices))
+        label_idx = 0
+        edge = [s]  # queue of vertices to process
+        parc[s] = label_idx
+        label_size = 1
+        rest = len(parc)-1
+        # grow from sources
+        while rest:
+            if not edge:
+                rest_idx = np.where(parc < 0)[0]
+                s = random.choice(rest_idx)
+                edge = [s]
+                label_idx += 1
+                label_size = 1
+                parc[s] = label_idx
+                rest -= 1
+
+            vert_from = edge.pop(0)
+
+            # add neighbors within allowable distance
+            row = graph[vert_from, :]
+            for vert_to, dist in zip(row.indices, row.data):
+                vert_to_label = parc[vert_to]
+
+                # abort if the vertex is already occupied
+                if vert_to_label >= 0:
+                    continue
+
+                # abort if outside of extent
+                if label_size > parcel_size:
+                    label_idx += 1
+                    label_size = 1
+                    edge = [vert_to]
+                    parc[vert_to] = label_idx
+                    rest -= 1
+                    break
+
+                # assign label value
+                parc[vert_to] = label_idx
+                label_size += 1
+                edge.append(vert_to)
+                rest -= 1
+
+        # merging small labels
+        # label connectivity matrix
+        n_labels = label_idx + 1
+        label_sizes = np.empty(n_labels, dtype=int)
+        label_conn = np.zeros([n_labels, n_labels], dtype='bool')
+        for i in range(n_labels):
+            vertices = np.nonzero(parc == i)[0]
+            label_sizes[i] = len(vertices)
+            neighbor_vertices = graph[vertices, :].indices
+            neighbor_labels = np.unique(np.array(parc[neighbor_vertices]))
+            label_conn[i, neighbor_labels] = 1
+        np.fill_diagonal(label_conn, 0)
+
+        # merging
+        label_id = range(n_labels)
+        while n_labels > n_parcel / 2:
+            # smallest label and its smallest neighbor
+            i = np.argmin(label_sizes)
+            neighbors = np.nonzero(label_conn[i, :])[0]
+            j = neighbors[np.argmin(label_sizes[neighbors])]
+
+            # merging two labels
+            label_conn[j, :] += label_conn[i, :]
+            label_conn[:, j] += label_conn[:, i]
+            label_conn = np.delete(label_conn, i, 0)
+            label_conn = np.delete(label_conn, i, 1)
+            label_conn[j, j] = 0
+            label_sizes[j] += label_sizes[i]
+            label_sizes = np.delete(label_sizes, i, 0)
+            n_labels -= 1
+            vertices = np.nonzero(parc == label_id[i])[0]
+            parc[vertices] = label_id[j]
+            label_id = np.delete(label_id, i, 0)
+
+        # convert parc to labels
+        for i in xrange(n_labels):
+            vertices = np.nonzero(parc == label_id[i])[0]
+            name = 'label_'+str(i)
+            label_ = Label(vertices, hemi=hemi, name=name, subject=subject)
+            labels.append(label_)
+        parc_both += [parc]
 
     return labels
 
