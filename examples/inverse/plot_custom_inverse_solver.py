d30ac8f7f40c60e3f2458da083c10d4c012de87a
Erik Hornberger <erik.hornberger@shi-g.com>
2018-02-27 16:32:17 +0900

    Removed unused parameter in _reapply_source_weighting

--- examples/inverse/plot_custom_inverse_solver.py
+++ examples/inverse/plot_custom_inverse_solver.py
@@ -111,7 +111,7 @@ def apply_solver(solver, evoked, forward, noise_cov, loose=0.2, depth=0.8):
 
     n_orient = 1 if is_fixed_orient(forward) else 3
     X, active_set = solver(M, gain, n_orient)
-    X = _reapply_source_weighting(X, source_weighting, active_set, n_orient)
+    X = _reapply_source_weighting(X, source_weighting, active_set)
 
     stc = _make_sparse_stc(X, active_set, forward, tmin=evoked.times[0],
                            tstep=1. / evoked.info['sfreq'])
