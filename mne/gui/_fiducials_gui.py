FIX: Fix scaling and units

mne/gui/_fiducials_gui.py
@@ -15,7 +15,7 @@ from pyface.api import confirm, error, FileDialog, OK, YES
 from traits.api import (HasTraits, HasPrivateTraits, on_trait_change,
                         cached_property, DelegatesTo, Event, Instance,
                         Property, Array, Bool, Button, Enum)
-from traitsui.api import HGroup, Item, VGroup, View, ArrayEditor, Handler
+from traitsui.api import HGroup, Item, VGroup, View, Handler, ArrayEditor
 from traitsui.menu import NoButtons
 from tvtk.pyface.scene_editor import SceneEditor
 
@@ -30,9 +30,10 @@ from ._file_traits import (SurfaceSource, fid_wildcard, FiducialsSource,
                            MRISubjectSource, SubjectSelectorPanel,
                            Surf)
 from ._viewer import (HeadViewController, PointObject, SurfaceObject,
-                      headview_borders, _M_WIDTH, _BUTTON_WIDTH,
-                      _MRI_FIDUCIALS_WIDTH, _REDUCED_M_WIDTH,
-                      _RESET_LABEL, _RESET_WIDTH, _m_fmt)
+                      headview_borders, _BUTTON_WIDTH,
+                      _MRI_FIDUCIALS_WIDTH, _MM_WIDTH,
+                      _RESET_LABEL, _RESET_WIDTH, _mm_fmt)
+
 defaults = DEFAULTS['coreg']
 
 
@@ -262,8 +263,7 @@ class FiducialsPanel(HasPrivateTraits):
     locked = DelegatesTo('model', 'lock_fiducials')
 
     set = Enum('LPA', 'Nasion', 'RPA')
-    current_pos = Array(float, (1, 3),
-                        editor=ArrayEditor(width=_REDUCED_M_WIDTH))
+    current_pos_mm = Array(float, (1, 3))
 
     save_as = Button(label='Save as...')
     save = Button(label='Save')
@@ -281,12 +281,13 @@ class FiducialsPanel(HasPrivateTraits):
         HGroup(Item('set', width=_MRI_FIDUCIALS_WIDTH,
                     format_func=lambda x: x, style='custom',
                     tooltip=_SET_TOOLTIP), show_labels=False),
-        HGroup(Item('current_pos', format_func=_m_fmt,
-                    width=_MRI_FIDUCIALS_WIDTH), show_labels=False),
+        HGroup(Item('current_pos_mm',
+                    editor=ArrayEditor(width=_MM_WIDTH, format_func=_mm_fmt),
+                    tooltip='MRI fiducial position (mm)'), show_labels=False),
         HGroup(Item('save', enabled_when='can_save',
                     tooltip="If a filename is currently specified, save to "
                     "that file, otherwise save to the default file name",
-                    width=_M_WIDTH),
+                    width=_BUTTON_WIDTH),
                Item('save_as', enabled_when='can_save_as',
                     width=_BUTTON_WIDTH),
                Item('reset_fid', enabled_when='can_reset', width=_RESET_WIDTH,
@@ -296,7 +297,27 @@ class FiducialsPanel(HasPrivateTraits):
 
     def __init__(self, *args, **kwargs):  # noqa: D102
         super(FiducialsPanel, self).__init__(*args, **kwargs)
-        self.sync_trait('lpa', self, 'current_pos', mutual=True)
+
+    @on_trait_change('current_pos_mm')
+    def _update_pos(self):
+        attr = self.set.lower()
+        if not np.allclose(getattr(self, attr), self.current_pos_mm * 1e-3):
+            setattr(self, attr, self.current_pos_mm * 1e-3)
+
+    @on_trait_change('model:lpa')
+    def _update_lpa(self, name):
+        if self.set == 'LPA':
+            self.current_pos_mm = self.lpa * 1000
+
+    @on_trait_change('model:nasion')
+    def _update_nasion(self, name):
+        if self.set.lower() == 'Nasion':
+            self.current_pos_mm = self.nasion * 1000
+
+    @on_trait_change('model:rpa')
+    def _update_rpa(self, name):
+        if self.set.lower() == 'RPA':
+            self.current_pos_mm = self.rpa * 1000
 
     def _reset_fid_fired(self):
         self.model.reset = True
@@ -382,14 +403,14 @@ class FiducialsPanel(HasPrivateTraits):
 
     @on_trait_change('set')
     def _on_set_change(self, obj, name, old, new):
-        self.sync_trait(old.lower(), self, 'current_pos', mutual=True,
-                        remove=True)
-        self.sync_trait(new.lower(), self, 'current_pos', mutual=True)
         if new == 'Nasion':
+            self.current_pos_mm = self.nasion * 1000
             self.headview.front = True
         elif new == 'LPA':
+            self.current_pos_mm = self.lpa * 1000
             self.headview.left = True
         elif new == 'RPA':
+            self.current_pos_mm = self.rpa * 1000
             self.headview.right = True
 
 

mne/gui/_viewer.py
@@ -15,7 +15,8 @@ from mayavi.tools.mlab_scene_model import MlabSceneModel
 from traits.api import (HasTraits, HasPrivateTraits, on_trait_change,
                         Instance, Array, Bool, Button, Enum, Float, Int, List,
                         Range, Str, RGBColor, Property, cached_property)
-from traitsui.api import View, Item, HGroup, VGrid, VGroup, Spring, TextEditor
+from traitsui.api import (View, Item, HGroup, VGrid, VGroup, Spring,
+                          TextEditor)
 from tvtk.api import tvtk
 
 from ..defaults import DEFAULTS
@@ -29,13 +30,10 @@ headview_borders = VGroup(Item('headview', style='custom', show_label=False),
                           show_border=True, label='View')
 
 
-def _m_fmt(x):
-    """Format data in units of meters (likely in the mm range)."""
-    return '%0.5f' % x
+def _mm_fmt(x):
+    """Format data in units of mm."""
+    return '%0.1f' % x
 
-
-laggy_float_editor_m = TextEditor(auto_set=False, enter_set=True,
-                                  evaluate=float, format_func=_m_fmt)
 
 laggy_float_editor_mm = TextEditor(auto_set=False, enter_set=True,
                                    evaluate=float,
@@ -43,19 +41,34 @@ laggy_float_editor_mm = TextEditor(auto_set=False, enter_set=True,
 
 laggy_float_editor_scale = TextEditor(auto_set=False, enter_set=True,
                                       evaluate=float,
-                                      format_func=lambda x: '%0.3f' % x)
+                                      format_func=lambda x: '%0.1f' % x)
+
+laggy_float_editor_headscale = TextEditor(auto_set=False, enter_set=True,
+                                          evaluate=float,
+                                          format_func=lambda x: '%0.3f' % x)
+
+laggy_float_editor_weight = TextEditor(auto_set=False, enter_set=True,
+                                       evaluate=float,
+                                       format_func=lambda x: '%0.2f' % x)
+
+laggy_float_editor_scale = TextEditor(auto_set=False, enter_set=True,
+                                      evaluate=float,
+                                      format_func=lambda x: '%0.1f' % x)
+laggy_float_editor_deg = TextEditor(auto_set=False, enter_set=True,
+                                    evaluate=float,
+                                    format_func=lambda x: '%0.1f' % x)
 
 _BUTTON_WIDTH = -80
 _OMIT_BUTTON_WIDTH = -100
-_RAD_WIDTH = -65  # radian floats
-_M_WIDTH = _RAD_WIDTH  # m floats
-_MM_WIDTH = -50  # mm floats
+_DEG_WIDTH = -65  # radian floats
+_MM_WIDTH = _DEG_WIDTH  # mm floats
+_SCALE_WIDTH = _DEG_WIDTH  # scale floats
 _INC_BUTTON_WIDTH = -25  # inc/dec buttons
-_SCALE_WIDTH = -50  # scale floats
+_DEG_STEP_WIDTH = -50
+_MM_STEP_WIDTH = _DEG_STEP_WIDTH
+_SCALE_STEP_WIDTH = _DEG_STEP_WIDTH
 _WEIGHT_WIDTH = -40  # weight floats
-_RAD_STEP_WIDTH = -50
 _VIEW_BUTTON_WIDTH = -60
-_M_STEP_WIDTH = _RAD_STEP_WIDTH
 # width is optimized for macOS and Linux avoid a horizontal scroll-bar
 # even when a vertical one is present
 _COREG_WIDTH = -290
@@ -63,7 +76,6 @@ _TEXT_WIDTH = -250
 _REDUCED_TEXT_WIDTH = _TEXT_WIDTH - 40 * np.sign(_TEXT_WIDTH)
 _DIG_SOURCE_WIDTH = _TEXT_WIDTH - 50 * np.sign(_TEXT_WIDTH)
 _MRI_FIDUCIALS_WIDTH = _TEXT_WIDTH - 60 * np.sign(_TEXT_WIDTH)
-_REDUCED_M_WIDTH = _MM_WIDTH
 _SHOW_BORDER = True
 _RESET_LABEL = u"↻"
 _RESET_WIDTH = _INC_BUTTON_WIDTH
@@ -100,8 +112,9 @@ class HeadViewController(HasTraits):
               Item('left', width=_VIEW_BUTTON_WIDTH),
               columns=3, show_labels=False),
         '_',
-        HGroup(Item('scale', label='Scale', editor=laggy_float_editor_scale,
-                    width=_REDUCED_M_WIDTH, show_label=True),
+        HGroup(Item('scale', label='Scale',
+                    editor=laggy_float_editor_headscale,
+                    width=_SCALE_WIDTH, show_label=True),
                Item('interaction', tooltip='Mouse interaction mode',
                     show_label=False), Spring()),
         show_labels=False))
@@ -229,8 +242,8 @@ class PointObject(Object):
 
     def default_traits_view(self):  # noqa: D102
         color = Item('color', show_label=False)
-        scale = Item('point_scale', label='Size', width=_M_WIDTH,
-                     editor=laggy_float_editor_m)
+        scale = Item('point_scale', label='Size', width=_SCALE_WIDTH,
+                     editor=laggy_float_editor_headscale)
         orient = Item('orient_to_surface',
                       enabled_when='orientable and not project_to_surface',
                       tooltip='Orient points toward the surface')