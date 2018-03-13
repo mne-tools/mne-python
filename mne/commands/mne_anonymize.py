8e15ef08d00494596022dd731c7a7ce205a1560b
Dominik Krzeminski (dokato) <raymon92@gmail.com>
2018-03-13 17:08:57 +0000

    changed mne_anonymize command to support filename as an option and not to overwrite files by default (it adds prefix anon- instead)

--- mne/commands/mne_anonymize.py
+++ mne/commands/mne_anonymize.py
@@ -10,18 +10,31 @@ $ mne anonymize sample_audvis_raw.fif
 
 import sys
 import mne
+import os
+
+ANONYMIZE_FILE_PREFIX = 'anon'
 
 
-def mne_anonymize(fif_fname):
+def mne_anonymize(fif_fname, out_fname, dir_name="", overwrite=False):
     """ Call *anonymize_info* on fif file and save.
     Parameters
     ----------
     fif_fname : str
         Raw fif File
+    out_fname : str
+        Output file name
+    dir_name : str
+        Path to directory with files
+    overwrite : bool
+        overwriting flag
     """
-    raw = mne.io.read_raw_fif(fif_fname, preload=True)
+    raw = mne.io.read_raw_fif(os.path.join(dir_name, fif_fname), preload=True)
     mne.io.anonymize_info(raw.info)
-    raw.save(fif_fname, overwrite=True)
+    if out_fname is not None:
+        out = out_fname
+    else:
+        out = "{}-{}".format(ANONYMIZE_FILE_PREFIX, fif_fname)
+    raw.save(os.path.join(dir_name, out), overwrite=overwrite)
 
 
 def run():
@@ -30,17 +43,31 @@ def run():
 
     parser = get_optparser(__file__)
 
+    parser.add_option("-f", "--file", type="string", dest="file",
+                      help="the file to modify.", metavar="FILE",
+                      default=None)
+    parser.add_option("-o", "--output", type="string", dest="output",
+                      help="name of anonymized output file. "
+                      "`anon-` prefix is added to FILE if not given",
+                      metavar="OUTFILE", default=None)
+    parser.add_option("--overwrite", dest="overwrite", action="store_true",
+                      help="overwrite input file", default=False)
+
     options, args = parser.parse_args()
-    if len(args) != 1:
+    if options.file is None:
         parser.print_help()
         sys.exit(1)
 
-    fname = args[0]
+    fname = options.file
+    out_fname = options.output
+    overwrite = options.overwrite
 
+    dir_name = os.path.split(fname)[0]
+    fname = os.path.basename(fname)
     if not fname.endswith('.fif'):
         raise ValueError('%s does not seem to be a .fif file.' % fname)
 
-    mne_anonymize(fname)
+    mne_anonymize(fname, out_fname, dir_name, overwrite)
 
 
 is_main = (__name__ == '__main__')
