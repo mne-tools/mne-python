865a25d2df01cd633b1d7a30dc3636898098ed40
Eric Larson <larson.eric.d@gmail.com>
2018-03-26 10:31:47 -0400

    FIX: Better fix?

--- mne/time_frequency/multitaper.py
+++ mne/time_frequency/multitaper.py
@@ -145,8 +145,10 @@ def dpss_windows(N, half_nbw, Kmax, low_bias=True, interp_from=None,
     """
     from scipy import interpolate
     from ..filter import next_fast_len
-    Kmax = operator.index(Kmax)
-    N = operator.index(N)
+    # This np.int64 business works around a weird Windows bug, see
+    # gh-5039 and https://github.com/scipy/scipy/pull/8608
+    Kmax = np.int64(operator.index(Kmax))
+    N = np.int64(operator.index(N))
     W = float(half_nbw) / N
     nidx = np.arange(N, dtype='d')
 
