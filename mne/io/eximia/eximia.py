50f6fade8fd7ee1a83ecdf683ff50eb55f699b71
Fede Raimondo <federaimondo@gmail.com>
2018-03-26 10:31:52 +0200

    New test

--- mne/io/eximia/eximia.py
+++ mne/io/eximia/eximia.py
@@ -35,7 +35,7 @@ def read_raw_eximia(fname, preload=False, verbose=None):
 
 
 class RawEximia(BaseRaw):
-    """Raw object from a Brain Vision EEG file
+    """Raw object from an Eximia EEG file
 
     Parameters
     ----------
@@ -60,16 +60,17 @@ class RawEximia(BaseRaw):
         # data are multiplexed int16
         ch_names = ['GateIn', 'Trig1', 'Trig2', 'EOG']
         ch_types = ['stim', 'stim', 'stim', 'eog']
-        cals = [1526., 1526., 1526., 0.38147]
-        ch_names += ('FP1 FPZ FP2 AF1 AFZ AF2 '
-                     'F7 F5 F1 FZ F2 F6 F8 '
-                     'FT9 FT7 FC5 FC3 FC1 FCZ FC2 FC4 FC6 FT8 FT10 '
-                     'T3 C5 C3 C1 CZ C2 C4 C6 T4 '
-                     'TP9 TP7 CP5 CP3 CP1 CPZ CP2 CP4 CP6 TP8 TP10 '
-                     'P9 P7 P3 P1 PZ P2 P4 P8 '
-                     'P10 PO3 POZ PO4 O1 OZ O2 IZ'.split())
+        cals = [0.0015259021896696422, 0.0015259021896696422,
+                0.0015259021896696422, 0.3814755474174106]
+        ch_names += ('Fp1 Fpz Fp2 AF1 AFz AF2 '
+                     'F7 F3 F1 Fz F2 F4 F8 '
+                     'FT9 FT7 FC5 FC3 FC1 FCz FC2 FC4 FC6 FT8 FT10 '
+                     'T7 C5 C3 C1 Cz C2 C4 C6 T8 '
+                     'TP9 TP7 CP5 CP3 CP1 CPz CP2 CP4 CP6 TP8 TP10 '
+                     'P9 P7 P3 P1 Pz P2 P4 P8 '
+                     'P10 PO3 POz PO4 O1 Oz O2 Iz'.split())
         n_eeg = len(ch_names) - len(cals)
-        cals += [0.076294] * n_eeg
+        cals += [0.07629510948348212] * n_eeg
         ch_types += ['eeg'] * n_eeg
         assert len(ch_names) == n_chan
         info = create_info(ch_names, sfreq, ch_types)
