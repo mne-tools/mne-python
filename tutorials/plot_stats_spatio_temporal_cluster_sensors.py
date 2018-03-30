2f91787aa7c80f5d89e8d432f06507cd703194b1
jona-sassenhagen <jona.sassenhagen@gmail.com>
2018-03-30 11:24:05 +0200

    nm

--- tutorials/plot_stats_spatio_temporal_cluster_sensors.py
+++ tutorials/plot_stats_spatio_temporal_cluster_sensors.py
@@ -186,7 +186,7 @@ for i_clu, clu_idx in enumerate(good_cluster_inds):
 # - What is the smallest p-value you can obtain, given the finite number of
 #   permutations?
 # - use an F distribution to compute the threshold by traditional significance
-#   levels. Hint: take a look at :obj:`scipy.stats.distributions.f`
+#   levels. Hint: take a look at :obj:`scipy.stats.f`
 #
 # References
 # ==========
