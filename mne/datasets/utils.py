42c2b39c5f1202c582d50b62ee1a9d0ff017c6d9
Moonpie McGee <Moonpie@gmail.com>
2018-03-30 09:34:35 -0400

    Updated release for testing in mne/datasets/utils.py

--- mne/datasets/utils.py
+++ mne/datasets/utils.py
@@ -235,7 +235,7 @@ def _data_path(path=None, force_update=False, update_path=True, download=True,
     path = _get_path(path, key, name)
     # To update the testing or misc dataset, push commits, then make a new
     # release on GitHub. Then update the "releases" variable:
-    releases = dict(testing='0.45', misc='0.3')
+    releases = dict(testing='0.46', misc='0.3')
     # And also update the "hashes['testing']" variable below.
 
     # To update any other dataset, update the data archive itself (upload
