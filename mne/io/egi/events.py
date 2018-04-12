55d0ce706011df25790cfbc60162385c03944778
JenE <jwe080@gmail.com>
2018-04-12 09:51:34 -0400

    fix flake8 errors - miracles can happen when you read the documentation

--- mne/io/egi/events.py
+++ mne/io/egi/events.py
@@ -22,8 +22,8 @@ def _read_events(input_fname, info):
     info : dict
         Header info array.
     """
-    mff_events, event_codes = _read_mff_events(input_fname,
-                              info['sfreq'], info['n_samples'])
+    mff_events, event_codes = _read_mff_events(input_fname, info['sfreq'],
+                                               info['n_samples'])
     info['n_events'] = len(event_codes)
     info['event_codes'] = np.asarray(event_codes).astype('<U4')
     events = np.zeros([info['n_events'],
