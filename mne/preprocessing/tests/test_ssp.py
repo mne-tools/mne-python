c49d2eea888aab7a98ad980e1ea004216428449a
Luke Bloy <luke.bloy@gmail.com>
2018-04-12 13:27:46 -0400

    ENH: added test for ssp computation on CTF data.

--- mne/preprocessing/tests/test_ssp.py
+++ mne/preprocessing/tests/test_ssp.py
@@ -5,10 +5,11 @@ from nose.tools import assert_true, assert_equal
 from numpy.testing import assert_array_almost_equal
 import numpy as np
 
-from mne.io import read_raw_fif
+from mne.io import read_raw_fif, read_raw_ctf
 from mne.io.proj import make_projector, activate_proj
 from mne.preprocessing.ssp import compute_proj_ecg, compute_proj_eog
 from mne.utils import run_tests_if_main
+from mne.datasets import testing
 
 warnings.simplefilter('always')  # enable b/c these tests throw warnings
 
@@ -16,6 +17,8 @@ data_path = op.join(op.dirname(__file__), '..', '..', 'io', 'tests', 'data')
 raw_fname = op.join(data_path, 'test_raw.fif')
 dur_use = 5.0
 eog_times = np.array([0.5, 2.3, 3.6, 14.5])
+
+ctf_fname = op.join(testing.data_path(download=False), 'CTF', 'testdata_ctf.ds')
 
 
 def test_compute_proj_ecg():
@@ -121,5 +124,32 @@ def test_compute_proj_parallel():
     projs_2, _, _ = make_projector(projs_2, raw_2.info['ch_names'],
                                    bads=['MEG 2443'])
     assert_array_almost_equal(projs, projs_2, 10)
+
+
+def test_compute_proj_ctf():
+    """Trivial Test to show that projector code completes on CTF data"""
+    raw = read_raw_ctf(ctf_fname)
+    raw.load_data()
+
+    for average in [False, True]:
+        n_projs_init = len(raw.info['projs'])
+        projs, events = compute_proj_eog(raw, n_mag=2, n_grad=2, n_eeg=2,
+                                         average=average,
+                                         ch_name='EEG059',
+                                         avg_ref=True, no_proj=False,
+                                         l_freq=None, h_freq=None,
+                                         reject=None, tmax=dur_use,
+                                         filter_length=6000)
+        assert_true(len(projs) == (5 + n_projs_init))
+
+        projs, events = compute_proj_ecg(raw, n_mag=1, n_grad=1, n_eeg=2,
+                                         average=average,
+                                         ch_name='EEG059',
+                                         avg_ref=True, no_proj=False,
+                                         l_freq=None, h_freq=None,
+                                         reject=None, tmax=dur_use,
+                                         filter_length=6000)
+        assert_true(len(projs) == (4 + n_projs_init))
+
 
 run_tests_if_main()
