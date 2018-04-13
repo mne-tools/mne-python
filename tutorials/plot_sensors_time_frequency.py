268dc6c76a20ca5eaceeff18f18885ead6e38947
mathurinm <mathurin.massias@gmail.com>
2018-04-13 15:50:40 +0200

    typos

--- tutorials/plot_sensors_time_frequency.py
+++ tutorials/plot_sensors_time_frequency.py
@@ -64,7 +64,7 @@ epochs.plot_psd_topomap(ch_type='grad', normalize=True)
 
 f, ax = plt.subplots()
 psds, freqs = psd_multitaper(epochs, fmin=2, fmax=40, n_jobs=1)
-psds = 10 * np.log10(psds)
+psds = 10. * np.log10(psds)
 psds_mean = psds.mean(0).mean(0)
 psds_std = psds.mean(0).std(0)
 
@@ -76,11 +76,11 @@ ax.set(title='Multitaper PSD (gradiometers)', xlabel='Frequency',
 plt.show()
 
 ###############################################################################
-# Time-frequency analysis: power and intertrial coherence
+# Time-frequency analysis: power and inter-trial coherence
 # -------------------------------------------------------
 #
 # We now compute time-frequency representations (TFRs) from our Epochs.
-# We'll look at power and intertrial coherence (ITC).
+# We'll look at power and inter-trial coherence (ITC).
 #
 # To this we'll use the function :func:`mne.time_frequency.tfr_morlet`
 # but you can also use :func:`mne.time_frequency.tfr_multitaper`
@@ -98,7 +98,7 @@ power, itc = tfr_morlet(epochs, freqs=freqs, n_cycles=n_cycles, use_fft=True,
 #
 # .. note::
 #     The generated figures are interactive. In the topo you can click
-#     on an image to visualize the data for one censor.
+#     on an image to visualize the data for one sensor.
 #     You can also select a portion in the time-frequency plane to
 #     obtain a topomap for a certain time-frequency region.
 power.plot_topo(baseline=(-0.5, 0), mode='logratio', title='Average power')
@@ -139,5 +139,5 @@ itc.plot_topo(title='Inter-Trial coherence', vmin=0., vmax=1., cmap='Reds')
 # Exercise
 # --------
 #
-#    - Visualize the intertrial coherence values as topomaps as done with
+#    - Visualize the inter-trial coherence values as topomaps as done with
 #      power.
