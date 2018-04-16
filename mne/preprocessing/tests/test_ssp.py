5bc564083bdd7f646e74ef482044da39ad740941
Luke Bloy <luke.bloy@gmail.com>
2018-04-16 15:09:25 -0400

    FIX: check ctf projs are the correct channels

--- mne/preprocessing/tests/test_ssp.py
+++ mne/preprocessing/tests/test_ssp.py
@@ -10,6 +10,7 @@ from mne.io.proj import make_projector, activate_proj
 from mne.preprocessing.ssp import compute_proj_ecg, compute_proj_eog
 from mne.utils import run_tests_if_main
 from mne.datasets import testing
+from mne import pick_types
 
 warnings.simplefilter('always')  # enable b/c these tests throw warnings
 
@@ -127,31 +128,54 @@ def test_compute_proj_parallel():
     assert_array_almost_equal(projs, projs_2, 10)
 
 
+def _check_projs_for_expected_channels(projs, n_mags, n_grads, n_eegs):
+    for p in projs:
+        if 'planar' in p['desc']:
+            assert len(p['data']['col_names']) == n_grads
+        elif 'axial' in p['desc']:
+            assert len(p['data']['col_names']) == n_mags
+        elif 'eeg' in p['desc']:
+            assert len(p['data']['col_names']) == n_eegs
+
+
 @testing.requires_testing_data
 def test_compute_proj_ctf():
-    """Test to show that projector code completes on CTF data"""
+    """Test to show that projector code completes on CTF data."""
     raw = read_raw_ctf(ctf_fname)
     raw.load_data()
 
-    for average in [False, True]:
-        n_projs_init = len(raw.info['projs'])
-        projs, events = compute_proj_eog(raw, n_mag=2, n_grad=2, n_eeg=2,
-                                         average=average,
-                                         ch_name='EEG059',
-                                         avg_ref=True, no_proj=False,
-                                         l_freq=None, h_freq=None,
-                                         reject=None, tmax=dur_use,
-                                         filter_length=6000)
-        assert len(projs) == (5 + n_projs_init)
+    # expected channels per projector type
+    n_mags = len(pick_types(raw.info, meg='mag', ref_meg=False,
+                            exclude='bads'))
+    n_grads = len(pick_types(raw.info, meg='grad', ref_meg=False,
+                             exclude='bads'))
+    n_eegs = len(pick_types(raw.info, meg=False, eeg=True, ref_meg=False,
+                            exclude='bads'))
 
-        projs, events = compute_proj_ecg(raw, n_mag=1, n_grad=1, n_eeg=2,
-                                         average=average,
-                                         ch_name='EEG059',
-                                         avg_ref=True, no_proj=False,
-                                         l_freq=None, h_freq=None,
-                                         reject=None, tmax=dur_use,
-                                         filter_length=6000)
-        assert len(projs) == (4 + n_projs_init)
+    # Test with and without gradient compensation
+    for c in [0, 1]:
+        raw.apply_gradient_compensation(c)
+        for average in [False, True]:
+            n_projs_init = len(raw.info['projs'])
+            projs, events = compute_proj_eog(raw, n_mag=2, n_grad=2, n_eeg=2,
+                                             average=average,
+                                             ch_name='EEG059',
+                                             avg_ref=True, no_proj=False,
+                                             l_freq=None, h_freq=None,
+                                             reject=None, tmax=dur_use,
+                                             filter_length=6000)
+            _check_projs_for_expected_channels(projs, n_mags, n_grads, n_eegs)
+            assert len(projs) == (5 + n_projs_init)
+
+            projs, events = compute_proj_ecg(raw, n_mag=1, n_grad=1, n_eeg=2,
+                                             average=average,
+                                             ch_name='EEG059',
+                                             avg_ref=True, no_proj=False,
+                                             l_freq=None, h_freq=None,
+                                             reject=None, tmax=dur_use,
+                                             filter_length=6000)
+            _check_projs_for_expected_channels(projs, n_mags, n_grads, n_eegs)
+            assert len(projs) == (4 + n_projs_init)
 
 
 run_tests_if_main()
