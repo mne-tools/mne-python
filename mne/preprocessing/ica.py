bd5b06245d3879312a3cf5380fd99869977a5e1d
Eric Larson <larson.eric.d@gmail.com>
2018-04-17 13:36:49 -0400

    FIX: Move _plot_corrmap

--- mne/preprocessing/ica.py
+++ mne/preprocessing/ica.py
@@ -38,10 +38,7 @@ from ..epochs import BaseEpochs
 from ..viz import (plot_ica_components, plot_ica_scores,
                    plot_ica_sources, plot_ica_overlay)
 from ..viz.ica import plot_ica_properties
-from ..viz.utils import (_prepare_trellis, tight_layout, plt_show,
-                         _setup_vmin_vmax)
-from ..viz.topomap import (_prepare_topo_plot, _check_outlines,
-                           plot_topomap, _hide_frame)
+from ..viz.topomap import _plot_corrmap
 
 from ..channels.channels import _contains_ch_type, ContainsMixin
 from ..io.write import start_file, end_file, write_id
@@ -2289,61 +2286,6 @@ def _find_max_corrs(all_maps, target, threshold):
     sim_i_o = np.abs(np.corrcoef(target, newtarget)[1, 0])
 
     return newtarget, median_corr_with_target, sim_i_o, max_corrs
-
-
-def _plot_corrmap(data, subjs, indices, ch_type, ica, label, show, outlines,
-                  layout, cmap, contours, template=False):
-    """Customize ica.plot_components for corrmap."""
-    if not template:
-        title = 'Detected components'
-        if label is not None:
-            title += ' of type ' + label
-    else:
-        title = "Supplied template"
-
-    picks = list(range(len(data)))
-
-    p = 20
-    if len(picks) > p:  # plot components by sets of 20
-        n_components = len(picks)
-        figs = [_plot_corrmap(data[k:k + p], subjs[k:k + p],
-                indices[k:k + p], ch_type, ica, label, show,
-                outlines=outlines, layout=layout, cmap=cmap,
-                contours=contours)
-                for k in range(0, n_components, p)]
-        return figs
-    elif np.isscalar(picks):
-        picks = [picks]
-
-    data_picks, pos, merge_grads, names, _ = _prepare_topo_plot(
-        ica, ch_type, layout)
-    pos, outlines = _check_outlines(pos, outlines)
-
-    data = np.atleast_2d(data)
-    data = data[:, data_picks]
-
-    # prepare data for iteration
-    fig, axes = _prepare_trellis(len(picks), max_col=5)
-    fig.suptitle(title)
-
-    if merge_grads:
-        from ..channels.layout import _merge_grad_data
-    for ii, data_, ax, subject, idx in zip(picks, data, axes, subjs, indices):
-        if template:
-            ttl = 'Subj. {0}, {1}'.format(subject, ica._ica_names[idx])
-            ax.set_title(ttl, fontsize=12)
-        data_ = _merge_grad_data(data_) if merge_grads else data_
-        vmin_, vmax_ = _setup_vmin_vmax(data_, None, None)
-        plot_topomap(data_.flatten(), pos, vmin=vmin_, vmax=vmax_,
-                     res=64, axes=ax, cmap=cmap, outlines=outlines,
-                     image_mask=None, contours=contours, show=False,
-                     image_interp='bilinear')[0]
-        _hide_frame(ax)
-    tight_layout(fig=fig)
-    fig.subplots_adjust(top=0.8)
-    fig.canvas.draw()
-    plt_show(show)
-    return fig
 
 
 @verbose
--- mne/viz/topomap.py
+++ mne/viz/topomap.py
@@ -2323,3 +2323,57 @@ def _set_contour_locator(vmin, vmax, contours):
         locator = ticker.MaxNLocator(nbins=contours + 1)
         contours = locator.tick_values(vmin, vmax)
     return locator, contours
+
+
+def _plot_corrmap(data, subjs, indices, ch_type, ica, label, show, outlines,
+                  layout, cmap, contours, template=False):
+    """Customize ica.plot_components for corrmap."""
+    if not template:
+        title = 'Detected components'
+        if label is not None:
+            title += ' of type ' + label
+    else:
+        title = "Supplied template"
+
+    picks = list(range(len(data)))
+
+    p = 20
+    if len(picks) > p:  # plot components by sets of 20
+        n_components = len(picks)
+        figs = [_plot_corrmap(data[k:k + p], subjs[k:k + p],
+                indices[k:k + p], ch_type, ica, label, show,
+                outlines=outlines, layout=layout, cmap=cmap,
+                contours=contours)
+                for k in range(0, n_components, p)]
+        return figs
+    elif np.isscalar(picks):
+        picks = [picks]
+
+    data_picks, pos, merge_grads, names, _ = _prepare_topo_plot(
+        ica, ch_type, layout)
+    pos, outlines = _check_outlines(pos, outlines)
+
+    data = np.atleast_2d(data)
+    data = data[:, data_picks]
+
+    # prepare data for iteration
+    fig, axes = _prepare_trellis(len(picks), max_col=5)
+    fig.suptitle(title)
+
+    if merge_grads:
+        from ..channels.layout import _merge_grad_data
+    for ii, data_, ax, subject, idx in zip(picks, data, axes, subjs, indices):
+        if template:
+            ttl = 'Subj. {0}, {1}'.format(subject, ica._ica_names[idx])
+            ax.set_title(ttl, fontsize=12)
+        data_ = _merge_grad_data(data_) if merge_grads else data_
+        vmin_, vmax_ = _setup_vmin_vmax(data_, None, None)
+        plot_topomap(data_.flatten(), pos, vmin=vmin_, vmax=vmax_,
+                     res=64, axes=ax, cmap=cmap, outlines=outlines,
+                     contours=contours, show=False, image_interp='bilinear')[0]
+        _hide_frame(ax)
+    tight_layout(fig=fig)
+    fig.subplots_adjust(top=0.8)
+    fig.canvas.draw()
+    plt_show(show)
+    return fig
