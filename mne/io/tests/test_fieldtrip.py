a1396e811a3ea1d4d78c1d5c5919861dca7b96db
Thomas Hartmann <thomas.hartmann@th-ht.de>
2018-04-18 15:44:03 +0200

    made flake8 happy for test_fieldtrip.py

--- mne/io/tests/test_fieldtrip.py
+++ mne/io/tests/test_fieldtrip.py
@@ -10,24 +10,31 @@ import numpy as np
 import os.path
 from mne.datasets import testing
 
+
 @testing.requires_testing_data
 def test_whole_process():
-    test_data_folder = os.path.join(mne.datasets.testing.data_path(), 'fieldtrip')
+    test_data_folder = os.path.join(mne.datasets.testing.data_path(),
+                                    'fieldtrip')
     all_versions = ['v7', 'v73']
     for version in all_versions:
         f_name_raw = os.path.join(test_data_folder, 'raw_%s.mat' % (version, ))
-        f_name_epoched = os.path.join(test_data_folder, 'epoched_%s.mat' % (version,))
-        f_name_avg = os.path.join(test_data_folder, 'averaged_%s.mat' % (version,))
+        f_name_epoched = os.path.join(test_data_folder,
+                                      'epoched_%s.mat' % (version,))
+        f_name_avg = os.path.join(test_data_folder,
+                                  'averaged_%s.mat' % (version,))
         f_name_events = os.path.join(test_data_folder, 'events.eve')
 
         # load everything
         data_raw = mne.io.read_raw_ft(f_name_raw, data_name='data')
-        data_epoched = mne.io.read_epochs_ft(f_name_epoched, data_name='data_epoched')
+        data_epoched = mne.io.read_epochs_ft(f_name_epoched,
+                                             data_name='data_epoched')
         data_avg = mne.io.read_evoked_ft(f_name_avg, data_name='data_avg')
         events = mne.read_events(f_name_events)
 
-        mne_epoched = mne.Epochs(data_raw, events, tmin=-0.05, tmax=0.05, preload=True, baseline=None)
-        np.testing.assert_almost_equal(data_epoched.get_data(), mne_epoched.get_data()[:, :, :-1])
+        mne_epoched = mne.Epochs(data_raw, events, tmin=-0.05, tmax=0.05,
+                                 preload=True, baseline=None)
+        np.testing.assert_almost_equal(data_epoched.get_data(),
+                                       mne_epoched.get_data()[:, :, :-1])
 
         mne_avg = mne_epoched.average()
         np.testing.assert_almost_equal(data_avg.data, mne_avg.data[:, :-1])
