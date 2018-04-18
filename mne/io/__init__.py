27a0be52372a79cd4085e285df3accc15f85d3b4
Thomas Hartmann <thomas.hartmann@th-ht.de>
2018-04-18 16:41:38 +0200

    added comma between fieldtrip reader functions imports.

--- mne/io/__init__.py
+++ mne/io/__init__.py
@@ -43,7 +43,7 @@ from .artemis123 import read_raw_artemis123
 from .eeglab import read_raw_eeglab, read_epochs_eeglab, read_events_eeglab
 from .eeglab import read_annotations_eeglab
 from .eximia import read_raw_eximia
-from .fieldtrip import (read_raw_fieldtrip,read_epochs_fieldtrip,
+from .fieldtrip import (read_raw_fieldtrip, read_epochs_fieldtrip,
                         read_evoked_fieldtrip)
 
 # for backward compatibility
