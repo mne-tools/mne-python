21d35c8963e9c616813fae8237ee6d8c6387c2dc
Luke Bloy <luke.bloy@gmail.com>
2018-04-22 07:39:33 -0400

    FIX: CTF Projector comp (#5125)
    * FIX: keep meg_ref channels if comp channels exist
    
    * STYLE: more compact Code
    
    * ENH: added test for ssp computation on CTF data.
    
    * resp to review
    
    * FIX: PEP8
    
    * FIX: check ctf projs are the correct channels

--- mne/preprocessing/tests/test_ssp.py
+++ mne/preprocessing/tests/test_ssp.py
@@ -5,10 +5,12 @@ from nose.tools import assert_true, assert_equal
 from numpy.testing import assert_array_almost_equal
 import numpy as np
 
-from mne.io import read_raw_fif
+from mne.io import read_raw_fif, read_raw_ctf
 from mne.io.proj import make_projector, activate_proj
 from mne.preprocessing.ssp import compute_proj_ecg, compute_proj_eog
 from mne.utils import run_tests_if_main
+from mne.datasets import testing
+from mne import pick_types
 
 warnings.simplefilter('always')  # enable b/c these tests throw warnings
 
@@ -16,6 +18,9 @@ data_path = op.join(op.dirname(__file__), '..', '..', 'io', 'tests', 'data')
 raw_fname = op.join(data_path, 'test_raw.fif')
 dur_use = 5.0
 eog_times = np.array([0.5, 2.3, 3.6, 14.5])
+
+ctf_fname = op.join(testing.data_path(download=False), 'CTF',
+                    'testdata_ctf.ds')
 
 
 def test_compute_proj_ecg():
@@ -121,5 +126,56 @@ def test_compute_proj_parallel():
     projs_2, _, _ = make_projector(projs_2, raw_2.info['ch_names'],
                                    bads=['MEG 2443'])
     assert_array_almost_equal(projs, projs_2, 10)
+
+
+def _check_projs_for_expected_channels(projs, n_mags, n_grads, n_eegs):
+    for p in projs:
+        if 'planar' in p['desc']:
+            assert len(p['data']['col_names']) == n_grads
+        elif 'axial' in p['desc']:
+            assert len(p['data']['col_names']) == n_mags
+        elif 'eeg' in p['desc']:
+            assert len(p['data']['col_names']) == n_eegs
+
+
+@testing.requires_testing_data
+def test_compute_proj_ctf():
+    """Test to show that projector code completes on CTF data."""
+    raw = read_raw_ctf(ctf_fname)
+    raw.load_data()
+
+    # expected channels per projector type
+    n_mags = len(pick_types(raw.info, meg='mag', ref_meg=False,
+                            exclude='bads'))
+    n_grads = len(pick_types(raw.info, meg='grad', ref_meg=False,
+                             exclude='bads'))
+    n_eegs = len(pick_types(raw.info, meg=False, eeg=True, ref_meg=False,
+                            exclude='bads'))
+
+    # Test with and without gradient compensation
+    for c in [0, 1]:
+        raw.apply_gradient_compensation(c)
+        for average in [False, True]:
+            n_projs_init = len(raw.info['projs'])
+            projs, events = compute_proj_eog(raw, n_mag=2, n_grad=2, n_eeg=2,
+                                             average=average,
+                                             ch_name='EEG059',
+                                             avg_ref=True, no_proj=False,
+                                             l_freq=None, h_freq=None,
+                                             reject=None, tmax=dur_use,
+                                             filter_length=6000)
+            _check_projs_for_expected_channels(projs, n_mags, n_grads, n_eegs)
+            assert len(projs) == (5 + n_projs_init)
+
+            projs, events = compute_proj_ecg(raw, n_mag=1, n_grad=1, n_eeg=2,
+                                             average=average,
+                                             ch_name='EEG059',
+                                             avg_ref=True, no_proj=False,
+                                             l_freq=None, h_freq=None,
+                                             reject=None, tmax=dur_use,
+                                             filter_length=6000)
+            _check_projs_for_expected_channels(projs, n_mags, n_grads, n_eegs)
+            assert len(projs) == (4 + n_projs_init)
+
 
 run_tests_if_main()
