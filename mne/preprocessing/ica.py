702355b1817b2530ab00072456ae11fe7a7e148a
Luke Bloy <luke.bloy@gmail.com>
2018-04-24 11:15:02 -0400

    FIX ica and comp_chans

--- mne/preprocessing/ica.py
+++ mne/preprocessing/ica.py
@@ -441,9 +441,10 @@ class ICA(ContainsMixin):
             self.max_pca_components = len(picks)
             logger.info('Inferring max_pca_components from picks')
 
-        self.info = pick_info(raw.info, picks)
-        if self.info['comps']:
-            self.info['comps'] = []
+        info = raw.info.copy()
+        if info['comps']:
+            info['comps'] = []
+        self.info = pick_info(info, picks)
         self.ch_names = self.info['ch_names']
         start, stop = _check_start_stop(raw, start, stop)
 
@@ -463,7 +464,7 @@ class ICA(ContainsMixin):
 
         self.n_samples_ = data.shape[1]
         # this may operate inplace or make a copy
-        data, self.pre_whitener_ = self._pre_whiten(data, raw.info, picks)
+        data, self._pre_whitener = self._pre_whiten(data, info, picks)
 
         self._fit(data, self.max_pca_components, 'raw')
 
@@ -481,9 +482,10 @@ class ICA(ContainsMixin):
                     '(please be patient, this may take a while)' % len(picks))
 
         # filter out all the channels the raw wouldn't have initialized
-        self.info = pick_info(epochs.info, picks)
-        if self.info['comps']:
-            self.info['comps'] = []
+        info = epochs.info.copy()
+        if info['comps']:
+            info['comps'] = []
+        self.info = pick_info(info, picks)
         self.ch_names = self.info['ch_names']
 
         if self.max_pca_components is None:
@@ -500,8 +502,8 @@ class ICA(ContainsMixin):
 
         # This will make at least one copy (one from hstack, maybe one
         # more from _pre_whiten)
-        data, self.pre_whitener_ = \
-            self._pre_whiten(np.hstack(data), epochs.info, picks)
+        data, self._pre_whitener = \
+            self._pre_whiten(np.hstack(data), info, picks)
 
         self._fit(data, self.max_pca_components, 'epochs')
 
