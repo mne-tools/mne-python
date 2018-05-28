160089ee74b056a9394f698050e50a021597faa4
Eric Larson <larson.eric.d@gmail.com>
2018-05-28 17:41:51 -0400

    STY: abap [circle front]

--- doc/conf.py
+++ doc/conf.py
@@ -125,7 +125,7 @@ exclude_patterns = ['source/generated']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'default'
+pygments_style = 'abap'
 
 # A list of ignored prefixes for module index sorting.
 modindex_common_prefix = ['mne.']
