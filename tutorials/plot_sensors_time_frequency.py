ace2db51fa146796f42b14d5648222d0d27989e0
Erkka Heinila <erkkahe@gmail.com>
2018-07-02 22:46:14 +0300

    Touch the example

--- tutorials/plot_sensors_time_frequency.py
+++ tutorials/plot_sensors_time_frequency.py
@@ -9,8 +9,8 @@ The objective is to show you how to explore the spectral content
 of your data (frequency and time-frequency). Here we'll work on Epochs.
 
 We will use the somatosensory dataset that contains so-called
-event related synchronizations (ERS) / desynchronizations (ERD) in
-the beta band.
+event related synchronizations (ERS) / desynchronizations (ERD)
+in the beta band.
 """
 
 import numpy as np
