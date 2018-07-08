28f7a6a651781bf5c74d4a7b397c3a4518bf41e7
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2018-07-08 17:21:26 +0200

    put back ref meg

--- mne/channels/interpolation.py
+++ mne/channels/interpolation.py
@@ -172,9 +172,9 @@ def _interpolate_bads_meg(inst, mode='accurate', verbose=None):
         and :ref:`Logging documentation <tut_logging>` for more).
     """
     picks_meg = pick_types(inst.info, meg=True, eeg=False,
-                           ref_meg=False, exclude=[])
+                           ref_meg=True, exclude=[])
     picks_good = pick_types(inst.info, meg=True, eeg=False,
-                            ref_meg=False, exclude='bads')
+                            ref_meg=True, exclude='bads')
     meg_ch_names = [inst.info['ch_names'][p] for p in picks_meg]
     bads_meg = [ch for ch in inst.info['bads'] if ch in meg_ch_names]
 
