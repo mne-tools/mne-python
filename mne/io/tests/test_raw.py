ea184a3c5a64bf511ada8ced7d141885f2345cd7
Luke Bloy <luke.bloy@gmail.com>
2018-07-09 13:42:06 -0400

    Fix testing imports

--- mne/io/tests/test_raw.py
+++ mne/io/tests/test_raw.py
@@ -4,7 +4,7 @@ import math
 
 import numpy as np
 from numpy.testing import (assert_allclose, assert_array_almost_equal,
-                           assert_equal)
+                           assert_equal, assert_array_equal)
 
 from mne import concatenate_raws
 from mne.datasets import testing
