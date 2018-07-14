5fe8eff4419856b9f3e41dbca118421b8050e87b
TommyClausner <tommy.clausner@gmail.com>
2018-07-14 17:12:31 +0200

    fix utils.py second attempt #5338

--- mne/utils.py
+++ mne/utils.py
@@ -823,7 +823,7 @@ def requires_module(function, name, call=None):
     call = ('import %s' % name) if call is None else call
     reason = 'Test %s skipped, requires %s.' % (function.__name__, name)
     try:
-        exec call in globals(), locals()
+        exec(call) in globals(), locals()
     except Exception as exc:
         if len(str(exc)) > 0 and str(exc) != 'No module named %s' % name:
             reason += ' Got exception (%s)' % (exc,)
