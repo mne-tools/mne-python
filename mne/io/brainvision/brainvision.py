3b0270cad41e53da97fbaa96d2c361afc62e44a5
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-08-07 16:11:12 +0200

    bug fix #5356 scan for BrainVision sections using ^ and $ (#5412)
    * bug fix #5356 scan for BrainVision sections using ^ and $
    
    * add comment why 'New Segment' markers are not parsed
    
    * add test for square brackets in marker comments
    
    * adjust test data to be more versatile

--- mne/io/brainvision/brainvision.py
+++ mne/io/brainvision/brainvision.py
@@ -307,7 +307,7 @@ def _read_vmrk_events(fname, event_id=None, trig_shift_by_type=None):
     if not m:
         return np.zeros((0, 3))
     mk_txt = txt[m.end():]
-    m = re.search(r"\[.*\]", mk_txt)
+    m = re.search(r"^\[.*\]$", mk_txt)
     if m:
         mk_txt = mk_txt[:m.start()]
 
@@ -340,6 +340,9 @@ def _read_vmrk_events(fname, event_id=None, trig_shift_by_type=None):
             if len(mdesc) > 0:
                 dropped.append(mdesc)
 
+    # Drop all markers with no description from the dropped markers
+    # For example, the "New Segment" markers will be dropped from the list
+    # of dropped markers because they usually look like: Mk1=New Segment,,1,1,0
     if len(dropped) > 0:
         dropped = list(set(dropped))
         examples = ", ".join(dropped[:5])
