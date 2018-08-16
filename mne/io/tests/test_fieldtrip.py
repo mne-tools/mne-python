a7a3a9d2c1e527c752bd2f817a1e1519e9f6943c
Thomas Hartmann <thomas.hartmann@th-ht.de>
2018-08-16 12:04:58 +0200

    added _assert_deep_almost_equal

--- mne/io/tests/test_fieldtrip.py
+++ mne/io/tests/test_fieldtrip.py
@@ -7,6 +7,7 @@
 import mne
 import numpy as np
 import os.path
+import types
 from mne.datasets import testing
 from mne.utils import requires_h5py
 
@@ -67,3 +68,60 @@ def test_raw():
         np.testing.assert_almost_equal(raw_fiff_mne.get_data(),
                                        raw_fiff_ft.get_data())
         pass
+
+
+def _assert_deep_almost_equal(expected, actual, *args, **kwargs):
+    """
+    Assert that two complex structures have almost equal contents.
+
+    Compares lists, dicts and tuples recursively. Checks numeric values
+    using test_case's :py:meth:`unittest.TestCase.assertAlmostEqual` and
+    checks all other values with :py:meth:`unittest.TestCase.assertEqual`.
+    Accepts additional positional and keyword arguments and pass those
+    intact to assertAlmostEqual() (that's how you specify comparison
+    precision).
+
+    This code has been adapted from
+    https://github.com/larsbutler/oq-engine/blob/master/tests/utils/helpers.py
+    """
+    is_root = not '__trace' in kwargs
+    trace = kwargs.pop('__trace', 'ROOT')
+
+    if isinstance(expected, np.ndarray) and expected.size == 0:
+        expected = None
+
+    if isinstance(actual, np.ndarray) and actual.size == 0:
+        actual = None
+
+    try:
+        if isinstance(expected, (int, float, complex)):
+            np.testing.assert_almost_equal(expected, actual, *args, **kwargs)
+        elif isinstance(expected, (list, tuple, np.ndarray,
+                                   types.GeneratorType)):
+            if isinstance(expected, types.GeneratorType):
+                expected = list(expected)
+                actual = list(actual)
+
+                np.testing.assert_equal(len(expected), len(actual))
+            for index in range(len(expected)):
+                v1, v2 = expected[index], actual[index]
+                _assert_deep_almost_equal(v1, v2,
+                                          __trace=repr(index), *args, **kwargs)
+        elif isinstance(expected, dict):
+            np.testing.assert_equal(set(expected), set(actual))
+            for key in expected:
+                _assert_deep_almost_equal(expected[key], actual[key],
+                                          __trace=repr(key), *args, **kwargs)
+        else:
+            np.testing.assert_equal(expected, actual)
+    except AssertionError as exc:
+        exc.__dict__.setdefault('traces', []).append(trace)
+        if is_root:
+            trace = ' -> '.join(reversed(exc.traces))
+            message = ''
+            try:
+                message = exc.message
+            except AttributeError:
+                pass
+            exc = AssertionError("%s\nTRACE: %s" % (message, trace))
+        raise exc
