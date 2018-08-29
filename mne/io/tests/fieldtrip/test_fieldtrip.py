0949ba9cdf2eb1465378718dfaac0bc8e0c58604
Thomas Hartmann <thomas.hartmann@th-ht.de>
2018-08-29 15:49:35 +0200

    parametrized tests

--- mne/io/tests/fieldtrip/test_fieldtrip.py
+++ mne/io/tests/fieldtrip/test_fieldtrip.py
@@ -8,103 +8,98 @@ import mne
 import numpy as np
 import os.path
 import pytest
+import itertools
 from mne.datasets import testing
 from .helpers import (check_info_fields, get_data_paths, get_raw_data,
                       get_epoched_data, get_averaged_data, _has_h5py,
                       pandas_not_found_warning_msg)
 from mne.utils import _check_pandas_installed
 
-
 all_systems = ['neuromag306']
+all_versions = ['v7', 'v73']
 
+all_test_params = list(itertools.product(all_systems, all_versions))
 
 @testing.requires_testing_data
 @pytest.mark.filterwarnings('ignore::RuntimeWarning')
-def test_averaged():
+@pytest.mark.parametrize('cur_system, version', all_test_params)
+def test_averaged(cur_system, version):
     """Test comparing reading an Evoked object and the FieldTrip version."""
-    for cur_system in all_systems:
-        test_data_folder_ft = get_data_paths(cur_system)
-        mne_avg = get_averaged_data(cur_system)
+    test_data_folder_ft = get_data_paths(cur_system)
+    mne_avg = get_averaged_data(cur_system)
 
-        all_versions = ['v7', 'v73']
-        for version in all_versions:
-            cur_fname = os.path.join(test_data_folder_ft,
-                                     'averaged_%s.mat' % (version,))
-            if version == 'v73' and not _has_h5py():
-                with pytest.raises(ImportError):
-                    mne.io.read_evoked_fieldtrip(cur_fname)
-                continue
+    cur_fname = os.path.join(test_data_folder_ft,
+                             'averaged_%s.mat' % (version,))
+    if version == 'v73' and not _has_h5py():
+        with pytest.raises(ImportError):
+            mne.io.read_evoked_fieldtrip(cur_fname)
+        return
 
-            avg_ft = mne.io.read_evoked_fieldtrip(cur_fname)
-            avg_ft.pick_types(meg=True, eeg=True, ref_meg=True)
+    avg_ft = mne.io.read_evoked_fieldtrip(cur_fname)
+    avg_ft.pick_types(meg=True, eeg=True, ref_meg=True)
 
-            mne_data = mne_avg.data[:, :-1]
-            ft_data = avg_ft.data
+    mne_data = mne_avg.data[:, :-1]
+    ft_data = avg_ft.data
 
-            np.testing.assert_almost_equal(mne_data, ft_data)
-            check_info_fields(mne_avg, avg_ft)
+    np.testing.assert_almost_equal(mne_data, ft_data)
+    check_info_fields(mne_avg, avg_ft)
 
 
 @testing.requires_testing_data
-def test_epoched():
+@pytest.mark.parametrize('cur_system, version', all_test_params)
+def test_epoched(cur_system, version):
     """Test comparing reading an Epochs object and the FieldTrip version."""
     has_pandas = _check_pandas_installed(strict=False) is not False
-    for cur_system in all_systems:
-        test_data_folder_ft = get_data_paths(cur_system)
-        mne_epoched = get_epoched_data(cur_system)
+    test_data_folder_ft = get_data_paths(cur_system)
+    mne_epoched = get_epoched_data(cur_system)
 
-        all_versions = ['v7', 'v73']
-        for version in all_versions:
-            cur_fname = os.path.join(test_data_folder_ft,
-                                     'epoched_%s.mat' % (version,))
-            if has_pandas:
-                pandas = _check_pandas_installed()
-                if version == 'v73' and not _has_h5py():
-                    with pytest.raises(ImportError):
-                        mne.io.read_epochs_fieldtrip(cur_fname)
-                    continue
-                epoched_ft = mne.io.read_epochs_fieldtrip(cur_fname)
-                assert isinstance(epoched_ft.metadata, pandas.DataFrame)
-            else:
-                with pytest.warns(RuntimeWarning,
-                                  message=pandas_not_found_warning_msg):
-                    if version == 'v73' and not _has_h5py():
-                        with pytest.raises(ImportError):
-                            mne.io.read_epochs_fieldtrip(cur_fname)
-                        continue
-                    epoched_ft = mne.io.read_epochs_fieldtrip(cur_fname)
-                    assert epoched_ft.metadata is None
+    cur_fname = os.path.join(test_data_folder_ft,
+                             'epoched_%s.mat' % (version,))
+    if has_pandas:
+        pandas = _check_pandas_installed()
+        if version == 'v73' and not _has_h5py():
+            with pytest.raises(ImportError):
+                mne.io.read_epochs_fieldtrip(cur_fname)
+            return
+        epoched_ft = mne.io.read_epochs_fieldtrip(cur_fname)
+        assert isinstance(epoched_ft.metadata, pandas.DataFrame)
+    else:
+        with pytest.warns(RuntimeWarning,
+                          message=pandas_not_found_warning_msg):
+            if version == 'v73' and not _has_h5py():
+                with pytest.raises(ImportError):
+                    mne.io.read_epochs_fieldtrip(cur_fname)
+                return
+            epoched_ft = mne.io.read_epochs_fieldtrip(cur_fname)
+            assert epoched_ft.metadata is None
 
-            mne_data = mne_epoched.get_data()[:, :, :-1]
-            ft_data = epoched_ft.get_data()
+    mne_data = mne_epoched.get_data()[:, :, :-1]
+    ft_data = epoched_ft.get_data()
 
-            np.testing.assert_almost_equal(mne_data, ft_data)
-            check_info_fields(mne_epoched, epoched_ft)
+    np.testing.assert_almost_equal(mne_data, ft_data)
+    check_info_fields(mne_epoched, epoched_ft)
 
 
 @testing.requires_testing_data
-def test_raw():
+@pytest.mark.parametrize('cur_system, version', all_test_params)
+def test_raw(cur_system, version):
     """Test comparing reading a raw fiff file and the FieldTrip version."""
     # Load the raw fiff file with mne
-    for cur_system in all_systems:
-        test_data_folder_ft = get_data_paths(cur_system)
-        raw_fiff_mne = get_raw_data(cur_system)
+    test_data_folder_ft = get_data_paths(cur_system)
+    raw_fiff_mne = get_raw_data(cur_system)
 
-        all_versions = ['v7', 'v73']
+    cur_fname = os.path.join(test_data_folder_ft,
+                             'raw_%s.mat' % (version,))
 
-        for version in all_versions:
-            cur_fname = os.path.join(test_data_folder_ft,
-                                     'raw_%s.mat' % (version,))
+    if version == 'v73' and not _has_h5py():
+        with pytest.raises(ImportError):
+            mne.io.read_raw_fieldtrip(cur_fname)
+        return
+    raw_fiff_ft = mne.io.read_raw_fieldtrip(cur_fname)
 
-            if version == 'v73' and not _has_h5py():
-                with pytest.raises(ImportError):
-                    mne.io.read_raw_fieldtrip(cur_fname)
-                continue
-            raw_fiff_ft = mne.io.read_raw_fieldtrip(cur_fname)
+    # Check that the data was loaded correctly
+    np.testing.assert_almost_equal(raw_fiff_mne.get_data(),
+                                   raw_fiff_ft.get_data())
 
-            # Check that the data was loaded correctly
-            np.testing.assert_almost_equal(raw_fiff_mne.get_data(),
-                                           raw_fiff_ft.get_data())
-
-            # Check info field
-            check_info_fields(raw_fiff_mne, raw_fiff_ft)
+    # Check info field
+    check_info_fields(raw_fiff_mne, raw_fiff_ft)
