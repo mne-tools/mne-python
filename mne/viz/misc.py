5fb207f25132122f980be068abc766ad7106f2fd
Eric Larson <larson.eric.d@gmail.com>
2018-09-19 16:11:41 -0400

    FIX: Matplotlib changes

--- mne/viz/misc.py
+++ mne/viz/misc.py
@@ -17,6 +17,7 @@ from glob import glob
 from itertools import cycle
 import os.path as op
 import warnings
+from distutils.version import LooseVersion
 from collections import defaultdict
 
 import numpy as np
@@ -560,12 +561,18 @@ def plot_events(events, sfreq=None, first_samp=0, color=None, event_id=None,
 
 def _get_presser(fig):
     """Get our press callback."""
+    import matplotlib
     callbacks = fig.canvas.callbacks.callbacks['button_press_event']
     func = None
     for key, val in callbacks.items():
-        if val.func.__class__.__name__ == 'partial':
+        if LooseVersion(matplotlib.__version__) >= '3':
+            func = val()
+        else:
             func = val.func
+        if func.__class__.__name__ == 'partial':
             break
+        else:
+            func = None
     assert func is not None
     return func
 
