f82b1c3b67c6d3955b8bea70096478b99686e630
Mainak Jas <mainakjas@gmail.com>
2018-09-25 17:24:34 -0400

    Add dipole fit

--- tutorials/plot_visualize_stc.py
+++ tutorials/plot_visualize_stc.py
@@ -5,89 +5,129 @@ Visualize Source time courses
 =============================
 
 """
-# sphinx_gallery_thumbnail_number = 7
+# sphinx_gallery_thumbnail_number = 2
 
 import os
 
 import mne
 from mne.datasets import sample
 
+###############################################################################
+#  This tutorial focuses on visualization of stcs.
+#
+# .. contents:: Table of Contents
+#    :local:
+#
+#
+# .. _surface_stc:
+# Surface Source Estimates
+# ------------------------
+# First, we get the paths for the evoked data and the
+# source time courses (stcs).
+
 data_path = sample.data_path()
 sample_dir = os.path.join(data_path, 'MEG', 'sample')
 subjects_dir = os.path.join(data_path, 'subjects')
 
 fname_evoked = data_path + '/MEG/sample/sample_audvis-ave.fif'
-
 fname_stc = os.path.join(sample_dir, 'sample_audvis-meg')
 
 ###############################################################################
-# Load example data
-
-# Read stc from file
+# Then, we read the stc from file
 stc = mne.read_source_estimate(fname_stc, subject='sample')
 
 ###############################################################################
-# This tutorial focuses on visualization of source time courses.
-# All of the surface plots introduced here are based on PySurfer. PySurfer
-# in turn uses the 3D visualization capabilities of Mayavi.
-# As in the rest of ...
-stc.plot(subjects_dir=subjects_dir, initial_time=0.1)
+# This is a :class:`SourceEstimate <mne.SourceEstimate>` object
+print(type(stc))
 
 ###############################################################################
-# Note that here we used initial_time=0.1, but we can also browse through
+# The SourceEstimate object is in fact a *surface* source estimate. MNE also
+# supports volume-based source estimates but more on that later.
+#
+# We can plot the source estimate using the ``stc.plot`` just as in other MNE
+# objects. Note that for this visualization to work, you must have ``mayavi``
+# and ``pysurfer`` installed on your machine.
+initial_time = 0.1
+stc.plot(subjects_dir=subjects_dir, initial_time=initial_time)
+
+###############################################################################
+# Note that here we used ``initial_time=0.1``, but we can also browse through
 # time using ``time_viewer=True``.
-
-###############################################################################
-# We can also visualize volume source estimates. Let us first compute
-# the source estimate from the inverse operator on a volume source space.
+#
+# .. _vol_stc:
+# Volume Source Estimates
+# -----------------------
+# We can also visualize volume source estimates (used for deep structures).
 
 from mne.minimum_norm import apply_inverse, read_inverse_operator  # noqa
 from mne import read_evokeds  # noqa
 
-snr = 3.0
-lambda2 = 1.0 / snr ** 2
-method = "dSPM"  # use dSPM method (could also be MNE or sLORETA)
-fname_inv = data_path + '/MEG/sample/sample_audvis-meg-vol-7-meg-inv.fif'
-
+###############################################################################
+# Let us load the sensor-level evoked data. We select the MEG channels
+# to keep things simple.
 evoked = read_evokeds(fname_evoked, condition=0, baseline=(None, 0))
+evoked.pick_types(meg=True, eeg=False)
+
+###############################################################################
+# Then, we can load the precomputed inverse operator from a file.
+fname_inv = data_path + '/MEG/sample/sample_audvis-meg-vol-7-meg-inv.fif'
 inv = read_inverse_operator(fname_inv)
 src = inv['src']
 
-# Compute inverse solution
+###############################################################################
+# The source estimate is computed using the inverse operator and the
+# sensor-space data.
+snr = 3.0
+lambda2 = 1.0 / snr ** 2
+method = "dSPM"  # use dSPM method (could also be MNE or sLORETA)
 stc = apply_inverse(evoked, inv, lambda2, method)
 stc.crop(0.0, 0.2)
 
 ###############################################################################
-# Then, we can plot the stc. For this visualization, nilearn must be installed.
-# This visualization is interactive. Click on any of the slices to explore
-# the time series. Clicking on any time point will bring up the corresponding
-# anatomical map.
+# This time, we have a different container for the source time course.
+print(type(stc))
+
+###############################################################################
+# This too comes with a convenient plot method.
 
 # XXX: subject=None should work with stc object (?)
 stc.plot(src, subject='sample', subjects_dir=subjects_dir)
 
 ###############################################################################
-# We can also plot the vector source estimates, i.e., plot not only the
-# magnitude but also the direction.
+# For this visualization, ``nilearn`` must be installed.
+# This visualization is interactive. Click on any of the anatomical slices
+# to explore the time series. Clicking on any time point will bring up the
+# corresponding anatomical map.
+#
+# .. _vector_stc:
+# Vector Source Estimates
+# -----------------------
 fname_inv = data_path + '/MEG/sample/sample_audvis-meg-oct-6-meg-inv.fif'
 
 inv = read_inverse_operator(fname_inv)
 stc = apply_inverse(evoked, inv, lambda2, 'dSPM', pick_ori='vector')
-stc.plot(subject='sample', subjects_dir=subjects_dir)
+stc.plot(subject='sample', subjects_dir=subjects_dir,
+         initial_time=initial_time)
 
 ###############################################################################
-# We can also plot the vector source estimates, i.e., plot not only the
-# magnitude but also the direction.
+# .. _dipole_fit:
+# Dipole fits
+# -----------
+# For computing a dipole fit, we need to load the noise covariance, the BEM
+# solution, and the coregistration transformation files. Note that for the
+# other methods, these were already used to generate the inverse operator.
 fname_cov = os.path.join(data_path, 'MEG', 'sample', 'sample_audvis-cov.fif')
 fname_bem = os.path.join(subjects_dir, 'sample', 'bem',
                          'sample-5120-bem-sol.fif')
 fname_trans = os.path.join(data_path, 'MEG', 'sample',
                            'sample_audvis_raw-trans.fif')
 
-evoked.pick_types(meg=True, eeg=False)
-evoked_full = evoked.copy()
-evoked.crop(0.07, 0.08)
+##############################################################################
+# Dipoles are fit independantly for each time point, so let us crop our time
+# series to visualize the dipole fit for the time point of interest.
+evoked.crop(0.1, 0.1)
 dip = mne.fit_dipole(evoked, fname_cov, fname_bem, fname_trans)[0]
 
-# Plot the result in 3D brain with the MRI image.
+##############################################################################
+# Finally, we can visualize the dipole.
 dip.plot_locations(fname_trans, 'sample', subjects_dir, mode='orthoview')
