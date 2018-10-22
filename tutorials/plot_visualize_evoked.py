cefeebb5dd9420454351b35aa4822ec5695dd443
sherazkhan <sheraz@nmr.mgh.harvard.edu>
2018-10-22 14:11:13 -0400

    ENH:arrowmap PR15

--- tutorials/plot_visualize_evoked.py
+++ tutorials/plot_visualize_evoked.py
@@ -171,9 +171,9 @@ mne.viz.plot_evoked_topo(evoked, title=title % 'Left/Right Auditory/Visual',
                          background_color='w')
 
 ###############################################################################
-# We can also plot the activations as arrow maps on the top of the topoplot
-# the arrows represent an estimation of the current flow underneath the MEG
-# sensors, here 175 is the time of the maximum sensor space activity.
+# We can also plot the activations as arrow maps on top of the topoplot.
+# The arrows represent an estimation of the current flow underneath the MEG
+# sensors. Here, 175 ms is the time of the maximum sensor space activity.
 evoked_l_aud_mag = evoked_l_aud.copy().pick_types(meg='mag')
 mne.viz.plot_arrowmap(evoked_l_aud_mag.data[:, 175], evoked_l_aud_mag.info)
 
