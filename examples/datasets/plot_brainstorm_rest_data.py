ef6ccbdfa2cad1aaabea7316d63722c4b6730afc
Eric Larson <larson.eric.d@gmail.com>
2018-10-25 17:10:18 -0400

    FIX: Cleanups, use CTF [ci skip]

--- examples/datasets/plot_brainstorm_rest_data.py
+++ examples/datasets/plot_brainstorm_rest_data.py
@@ -35,12 +35,12 @@ data_path = bst_resting.data_path()
 subject = 'bst_resting'
 
 subjects_dir = op.join(data_path, 'subjects')
-
+bem_dir = op.join(subjects_dir, subject, 'bem')
+bem_fname = op.join(bem_dir, '%s-5120-bem-sol.fif' % subject)
+src_fname = op.join(bem_dir, '%s-oct-6-src.fif' % subject)
 raw_fname = (data_path + '/MEG/%s/subj002_spontaneous_20111102_01_AUX.ds'
              % subject)
-
-raw_noise_fname = data_path + '/MEG/%s/subj002_noise_20111104_02.ds' % subject
-
+raw_erm_fname = data_path + '/MEG/%s/subj002_noise_20111104_02.ds' % subject
 trans_fname = data_path + '/MEG/%s/%s-trans.fif' % (subject, subject)
 
 
@@ -48,35 +48,24 @@ trans_fname = data_path + '/MEG/%s/%s-trans.fif' % (subject, subject)
 # Load data, set types and rename ExG channels
 
 raw = mne.io.read_raw_ctf(raw_fname, preload=True)
-raw_er = mne.io.read_raw_ctf(raw_noise_fname, preload=True)
+raw_erm = mne.io.read_raw_ctf(raw_erm_fname, preload=True)
 
 # clean up bad ch names and do common preprocessing
 raw.set_channel_types({'EEG057': 'ecg', 'EEG058': 'eog'})
-for raw_ in (raw, raw_er):
-    raw_.resample(150, n_jobs=2)
-    raw_.rename_channels(
-        dict(zip(raw_.ch_names, mne.utils._clean_names(raw_.ch_names))))
-    picks = mne.pick_types(raw_.info, meg=True, eog=True, ecg=True)
-    raw_.filter(1, None)
+for raw_ in (raw, raw_erm):
+    raw_.resample(100, n_jobs=2)
 
-for comp in raw.info['comps']:
-    for key in ('row_names', 'col_names'):
-        comp['data'][key] = mne.utils._clean_names(comp['data'][key])
+# unify channel names
+raw_erm.rename_channels(lambda x: x.replace('-4408', '-4407'))
 
 ##############################################################################
 # Compute SSP
 
-ssp_ecg, _ = mne.preprocessing.compute_proj_ecg(
-    raw, average=True, n_mag=2)
-ssp_eog, _ = mne.preprocessing.compute_proj_eog(
-    raw, average=True, n_mag=2)
+ssp_ecg, _ = mne.preprocessing.compute_proj_ecg(raw, n_mag=2)
+ssp_eog, _ = mne.preprocessing.compute_proj_eog(raw, n_mag=2)
 
-raw.add_proj(ssp_eog)
-raw.add_proj(ssp_ecg)
-
-raw_er.add_proj(ssp_eog)
-raw_er.add_proj(ssp_ecg)
-
+raw.add_proj(ssp_eog + ssp_ecg)
+raw_erm.add_proj(ssp_eog + ssp_ecg)
 
 ##############################################################################
 # Explore data
@@ -90,27 +79,14 @@ raw.plot_psd(n_fft=2048, fmin=1, fmax=50, xscale='log', proj=True)
 ##############################################################################
 # Make forward stack and get transformation matrix
 
-spacing = 'oct6'
-src = mne.setup_source_space(
-    subject=subject, spacing=spacing, subjects_dir=subjects_dir,
-    add_dist=True)
-
-conductivity = (0.3,)  # for single layer
-model = mne.make_bem_model(subject='bst_resting', ico=4,
-                           conductivity=conductivity,
-                           subjects_dir=subjects_dir)
-bem = mne.make_bem_solution(model)
-raise RuntimeError
-
+src = mne.read_source_spaces(src_fname)
+bem = mne.read_bem_solution(bem_fname)
 picks = mne.pick_types(raw.info, meg=True, eeg=False, ref_meg=True)
-
-
 trans = mne.read_trans(trans_fname)
 mne.viz.plot_alignment(
     raw.info, trans=trans, subject=subject, subjects_dir=subjects_dir)
-
 fwd = mne.make_forward_solution(raw.info, trans, src=src, bem=bem,
-                                eeg=False)
+                                eeg=False, verbose=True)
 
 ##############################################################################
 # Make epochs and look at power
@@ -131,23 +107,19 @@ epochs = mne.Epochs(raw, events, event_id=42, tmin=0, tmax=tmax, picks=picks,
                     baseline=None, reject=reject, preload=False,
                     proj=True)
 
-epochs.plot_psd_topomap(normalize=True)
-
+# epochs.plot_psd_topomap(normalize=True)  # very slow!
 
 ##############################################################################
 # Make inverse
 
-noise_cov = mne.compute_raw_covariance(raw_er, method='shrunk', tmax=30)
+noise_cov = mne.compute_raw_covariance(raw_erm, tmax=30)
 
 inverse_operator = mne.minimum_norm.make_inverse_operator(
     epochs.info, forward=fwd, noise_cov=noise_cov)
 
-
 stc_gen = mne.minimum_norm.apply_inverse_epochs(
-    epochs, inverse_operator,
-    lambda2=1,
-    method='MNE', nave=1, pick_ori="normal",
-    return_generator=True, prepared=False)
+    epochs, inverse_operator, lambda2=1., method='MNE', nave=1,
+    pick_ori="normal", return_generator=True, prepared=False)
 
 # make PSD in source sapce
 psd_src = list()
--- examples/datasets/plot_brainstorm_rest_data_omega.py
+++ examples/datasets/plot_brainstorm_rest_data_omega.py
@@ -11,13 +11,20 @@ The pipeline is meant to mirror the brainstorm resting tutorial pipeline
     http://neuroimage.usc.edu/brainstorm/Tutorials/MedianNerveCtf
 
 Brainstorm pipline is
-1) Notch filter: 60 120 180 240 300 Hz,
-2) Band-pass filter: High-pass filter at 0.3Hz
-3) Artifact (EOG/ECG) detection... XXX skipped
-4) Source localization (dSPM, some depth weighting, constrained)
-5) Frequency > Power spectrum density (Welch): [0,100s], Window=4s, 50% overlap,
-        Group in frequency bands (use the default frequency bands)
-6) Standardize > Spectrum normalization: Relative power (divide by total power)
+
+1. Notch filter
+     60 120 180 240 300 Hz.
+2. Band-pass filter
+     High-pass filter at 0.3Hz.
+3. Artifact (EOG/ECG) detection
+     XXX skipped.
+4. Source localization
+     dSPM, some depth weighting, constrained.
+5. Frequency
+     Power spectrum density (Welch): [0,100s], Window=4s,
+     50% overlap, Group in frequency bands (use the default frequency bands).
+6. Standardize
+     Spectrum normalization: Relative power (divide by total power).
 
 References
 ----------
@@ -35,8 +42,6 @@ References
 import mayavi.mlab as mlab
 import os.path as op
 
-import numpy as np
-
 import mne
 from mne.datasets.brainstorm import bst_resting
 
@@ -47,47 +52,37 @@ data_path = bst_resting.data_path()
 subject = 'bst_resting'
 
 subjects_dir = op.join(data_path, 'subjects')
-
+bem_dir = op.join(subjects_dir, subject, 'bem')
+bem_fname = op.join(bem_dir, '%s-5120-bem-sol.fif' % subject)
+bem_fname = op.join(bem_dir, '%s-5120-bem-sol.fif' % subject)
+src_fname = op.join(bem_dir, '%s-oct-6-src.fif' % subject)
 raw_fname = data_path + '/MEG/' + subject + '/' + \
-                        'subj002_spontaneous_20111102_01_AUX_raw.fif'
-
-raw_noise_fname = op.join(
+                        'subj002_spontaneous_20111102_01_AUX.ds'
+raw_erm_fname = op.join(
     data_path,
-    'MEG/%s/subj002_noise_20111104_02_raw.fif' % subject)
-
-# XXX trans should be replaced by file in dataset.
-# this was not guessed but is from a file that we don't ship.
-trans = mne.Transform(  # Kids please do not do this at home.
-    fro=4,  # head
-    to=5,  # mri
-    trans=np.array(  # run 1
-        [[0.99979711, 0.01138957, 0.01660261, -0.00337221],
-         [-0.00577519, 0.95219451, -0.30543712, -0.00715041],
-         [-0.01928758, 0.30527931, 0.95206732, -0.03214351],
-         [0., 0., 0, 1.]]))
+    'MEG/%s/subj002_noise_20111104_02.ds' % subject)
+trans_fname = data_path + '/MEG/%s/%s-trans.fif' % (subject, subject)
 
 ##############################################################################
 # Load data, set types and rename ExG channels
 
-raw = mne.io.read_raw_fif(raw_fname, preload=True)
-raw_er = mne.io.read_raw_fif(raw_noise_fname, preload=True)
+raw = mne.io.read_raw_ctf(raw_fname, preload=True)
+raw_erm = mne.io.read_raw_ctf(raw_erm_fname, preload=True)
 
 # raw.crop(0, 60)
 
 # clean up bad ch names and do common preprocessing
 raw.set_channel_types({'EEG057': 'ecg', 'EEG058': 'eog'})
-for raw_ in (raw, raw_er):
+for raw_ in (raw, raw_erm):
     raw.apply_gradient_compensation(3)
-    raw_.resample(300, n_jobs=2)
-    raw_.rename_channels(
-        dict(zip(raw_.ch_names, mne.utils._clean_names(raw_.ch_names))))
+    raw_.resample(300)
     picks = mne.pick_types(raw_.info, meg=True, eog=True, ecg=True)
     raw_.notch_filter([60., 120.], filter_length='auto', phase='zero')
     raw_.filter(0.3, None)
 
-for comp in raw.info['comps']:
-    for key in ('row_names', 'col_names'):
-        comp['data'][key] = mne.utils._clean_names(comp['data'][key])
+
+# unify channel names
+raw_erm.rename_channels(lambda x: x.replace('-4408', '-4407'))
 
 ##############################################################################
 # Explore data
@@ -101,18 +96,9 @@ raw.plot_psd(n_fft=2048, fmin=1, fmax=100, proj=True)
 ##############################################################################
 # Make forward stack and get transformation matrix
 
-spacing = 'oct6'
-src = mne.setup_source_space(
-    subject=subject, spacing=spacing, subjects_dir=subjects_dir,
-    add_dist=False)
-
-
-conductivity = (0.3,)  # for single layer
-model = mne.make_bem_model(subject='bst_resting', ico=4,
-                           conductivity=conductivity,
-                           subjects_dir=subjects_dir)
-bem = mne.make_bem_solution(model)
-
+src = mne.read_source_spaces(src_fname)
+bem = mne.read_bem_solution(bem_fname)
+trans = mne.read_trans(trans_fname)
 fwd = mne.make_forward_solution(raw.info, trans, src=src, bem=bem,
                                 eeg=False)
 
@@ -125,7 +111,7 @@ mne.viz.plot_alignment(
 ##############################################################################
 # Make inverse
 
-noise_cov = mne.compute_raw_covariance(raw_er, method='shrunk', tmax=30)
+noise_cov = mne.compute_raw_covariance(raw_erm, tmax=30)
 
 inverse_operator = mne.minimum_norm.make_inverse_operator(
     raw.info, forward=fwd, noise_cov=noise_cov)
@@ -136,20 +122,13 @@ snr = 1.0  # use smaller SNR for raw data
 lambda2 = 1.0 / snr ** 2
 method = "dSPM"  # use dSPM method (could also be MNE or sLORETA)
 
-psd_raw_db = mne.minimum_norm.compute_source_psd(raw, inverse_operator,
-                                                 lambda2=lambda2,
-                                                 method=method,
-                                                 fmin=fmin, fmax=fmax,
-                                                 n_fft=2048, label=None,
-                                                 out_decibels=True)
+psd_raw_db = mne.minimum_norm.compute_source_psd(
+    raw, inverse_operator, lambda2=lambda2, method=method,
+    fmin=fmin, fmax=fmax, n_fft=2048, label=None, out_decibels=True)
 
-
-psd_raw = mne.minimum_norm.compute_source_psd(raw, inverse_operator,
-                                              lambda2=lambda2,
-                                              method=method,
-                                              fmin=fmin, fmax=fmax,
-                                              n_fft=2048, label=None,
-                                              out_decibels=False)
+psd_raw = mne.minimum_norm.compute_source_psd(
+    raw, inverse_operator, lambda2=lambda2, method=method,
+    fmin=fmin, fmax=fmax, n_fft=2048, label=None, out_decibels=False)
 
 # compute total and relative power.
 psd_total_raw_db = psd_raw_db.mean() * len(psd_raw_db.times)
