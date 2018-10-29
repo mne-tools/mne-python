c47f79914bc98fb042f53823b4f56e9cfe2310cf
Eric Larson <larson.eric.d@gmail.com>
2018-10-29 13:08:41 -0400

    MRG: Fix plot_montage (#5676)
    * WIP: Fix plot_montage
    
    * FIX: Cleaner
    
    * DOC: Correct [ci skip]
    
    * DOC: SP [ci skip]

--- doc/whats_new.rst
+++ doc/whats_new.rst
@@ -110,6 +110,8 @@ Bug
 
 - Fix bug with channel names in ``mgh70`` montage in :func:`mne.channels.read_montage` by `Eric Larson`_
 
+- Fix check in :func:`mne.vis.plot_sensors` for invalid channel locations by `Eric Larson`_
+
 - Fix bug in :func:`mne.io.read_raw_edf` where GDF files had ``info['highpass']`` and ``info['lowpass']`` set to NaN and ``info['meas_date']`` set incorrectly, by `Eric Larson`_
 
 - Fix bug in :func:`mne.preprocessing.ICA.apply` to handle arrays as `exclude` property by `Joan Massich`_
--- mne/viz/tests/test_montage.py
+++ mne/viz/tests/test_montage.py
@@ -7,6 +7,7 @@
 # Set our plotters to test mode
 import os.path as op
 
+import pytest
 import matplotlib
 
 from mne.channels import read_montage, read_dig_montage
@@ -19,6 +20,8 @@ elp = op.join(p_dir, 'test_elp.txt')
 hsp = op.join(p_dir, 'test_hsp.txt')
 hpi = op.join(p_dir, 'test_mrk.sqd')
 point_names = ['nasion', 'lpa', 'rpa', '1', '2', '3', '4', '5']
+io_dir = op.join(op.dirname(__file__), '..', '..', 'io', 'tests', 'data')
+fif_fname = op.join(io_dir, 'test_raw.fif')
 
 
 def test_plot_montage():
@@ -36,12 +39,17 @@ def test_plot_montage():
     m.plot(kind='topomap', show_names=True)
     plt.close('all')
     d = read_dig_montage(hsp, hpi, elp, point_names)
-    d.plot()
-    plt.close('all')
-    d.plot(kind='3d')
-    plt.close('all')
-    d.plot(kind='3d', show_names=True)
-    plt.close('all')
+    assert '0 channels' in repr(d)
+    with pytest.raises(RuntimeError, match='No valid channel positions'):
+        d.plot()
+    d = read_dig_montage(fif=fif_fname)
+    assert '61 channels' in repr(d)
+    # XXX this is broken; dm.point_names is used. Sometimes we say this should
+    # Just contain the HPI coils, other times that it's all channels (e.g.,
+    # EEG channels). But there is redundancy with this and dm.dig_ch_pos.
+    # This should be addressed in the pending big refactoring.
+    # d.plot()
+    # plt.close('all')
 
 
 def test_plot_defect_montage():
