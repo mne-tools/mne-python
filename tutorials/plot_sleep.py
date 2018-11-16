36ff2d9c314ad6f057d31423f5a975583e3bbf17
Joan Massich <sik@visor.udg.edu>
2018-11-16 14:29:17 +0100

    Comment the entire tuto to check that it compiles

--- tutorials/plot_sleep.py
+++ tutorials/plot_sleep.py
@@ -13,22 +13,24 @@ my_event_id = {'Sleep stage ?': 1,
                'Sleep stage 3': 5,
                'Sleep stage R': 6}
 
-# names of edf and annot files
-edf_f = "SC4001E0-PSG.edf"
-annot_f = "SC4001EC-Hypnogram.edf"
+print(my_event_id)
 
-# read raw edf
-raw = mne.io.read_raw_edf(edf_f, preload=True)
+# # names of edf and annot files
+# edf_f = "SC4001E0-PSG.edf"
+# annot_f = "SC4001EC-Hypnogram.edf"
 
-# names of EEG and EOG channels to use
-channels = ['EEG Fpz-Cz', 'EEG Pz-Oz', 'EOG horizontal']
+# # read raw edf
+# raw = mne.io.read_raw_edf(edf_f, preload=True)
 
-# set annotations
-annot = mne.read_annotations(annot_f)
-raw.set_annotations(annot)
+# # names of EEG and EOG channels to use
+# channels = ['EEG Fpz-Cz', 'EEG Pz-Oz', 'EOG horizontal']
 
-# get the events and the event ids
-events, event_id = mne.events_from_annotations(raw)
+# # set annotations
+# annot = mne.read_annotations(annot_f)
+# raw.set_annotations(annot)
 
-# perform epoching
-epochs = mne.Epochs(raw, events, event_id, tmin=0., tmax=30., baseline=None)
+# # get the events and the event ids
+# events, event_id = mne.events_from_annotations(raw)
+
+# # perform epoching
+# epochs = mne.Epochs(raw, events, event_id, tmin=0., tmax=30., baseline=None)
