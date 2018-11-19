bc7f5528616f638194c93da47f7dab9466bf3cdb
Joan Massich <sik@visor.udg.edu>
2018-11-19 17:11:07 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ if __name__ == "__main__":
           version=VERSION,
           download_url=DOWNLOAD_URL,
           long_description=open('README.rst').read(),
+          long_description_content_type='text/markdown',
           zip_safe=False,  # the package can run out of an .egg file
           classifiers=['Intended Audience :: Science/Research',
                        'Intended Audience :: Developers',
