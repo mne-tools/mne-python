e506d9e3fab3f1cc37cd0a26812da112ec15d372
dengemann <denis.engemann@gmail.com>
2018-11-26 00:25:09 +0100

    remove subsampling

--- examples/forward/plot_forward_rank.py
+++ examples/forward/plot_forward_rank.py
@@ -54,11 +54,7 @@ picks_mag = mne.pick_types(fwd['info'], meg='mag')
 picks_grad = mne.pick_types(fwd['info'], meg='grad')
 picks_eeg = mne.pick_types(fwd['info'], meg=False, eeg=True)
 
-# We will try to look at the same number of dimensions to
-# avoid looking at trivial differences. We will use spatial
-# subsampling for that purpose.
 n_chans = 50
-n_draws = 30
 threshold = .99
 
 plt.figure(figsize=(8, 6))
@@ -67,22 +63,16 @@ for picks, ch_type, color in zip(
         ('grad', 'mag', 'eeg'),
         ('c', 'b', 'k')):
 
-    # use same number of channels while keeping spatial info.
-    exp_var_ratio = np.array([
-        _get_var_exp(
-            leadfield[np.random.choice(picks, size=n_chans, replace=False)])
-        for _ in range(n_draws)])
+    exp_var_ratio = _get_var_exp(leadfield[picks])
 
-    exp_var_ratio_mean = np.mean(exp_var_ratio, axis=0)
-    approx_rank = np.sum(exp_var_ratio_mean < threshold)
-    plt.plot(exp_var_ratio_mean, color=color, linewidth=1,
-             label=r'%s (rank$_{vexp%d}$ = %d)' % (
-                 ch_type, threshold * 100, approx_rank))
-    plt.plot(exp_var_ratio.T, color=color, linewidth=1,
-             alpha=0.3)
+    approx_rank = np.sum(exp_var_ratio < threshold)
+    plt.plot(exp_var_ratio, color=color, linewidth=1,
+             label=r'%s (rank$_{vexp%d}$ = %d/%d)' % (
+                 ch_type, threshold * 100, approx_rank, len(picks)))
+
 plt.legend(loc='lower right')
 
-# One can see that despite the same number of channels used,
-# the gradiometer dimensionality saturates slower while eeg
-# saturates fastest, leading to, respectively, more or less.
-# independent patterns.
+# One can see that despite similar proportional dimensioality,
+# the gradiometers saturates more slowly while eeg
+# saturates fastest. In otherwords, for EEG, fewer dimensions are
+# more charactersitic of the overall variation.
