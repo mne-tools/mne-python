a6af4bc32b57c941a368bd631cd1691b0e7c87ae
kdoelling1919 <kd889@nyu.edu>
2018-12-03 08:09:18 -1000

    Code cleanup and style adjustments.

--- mne/utils.py
+++ mne/utils.py
@@ -3195,9 +3195,7 @@ def _hid_match(event_id, keys):
     # form the hierarchical event ID mapping
     use_keys = []
     for key in keys:
-        if not isinstance(key, str):
-            raise KeyError('keys must be strings, got %s (%s)'
-                           % (type(key), key))
+        _validate_type(key, types=str, item_name='keys')
         use_keys.extend(k for k in event_id.keys()
                         if set(key.split('/')).issubset(k.split('/')))
     if len(use_keys) == 0:
@@ -3334,7 +3332,8 @@ class GetEpochsMixin(object):
         if drop_event_id:
             # update event id to reflect new content of inst
             inst.event_id = dict((k, v) for k, v in inst.event_id.items()
-                                   if v in inst.events[:, 2])
+                                 if v in inst.events[:, 2])
+
         if return_indices:
             return inst, select
         else:
@@ -3342,7 +3341,7 @@ class GetEpochsMixin(object):
 
     def _keys_to_idx(self, keys):
         """Find entries in event dict."""
-        keys = [keys] if not isinstance(keys, (list, tuple)) else keys
+        keys = keys if isinstance(keys, (list, tuple)) else [keys]
         try:
             # Assume it's a condition name
             return np.where(np.any(
