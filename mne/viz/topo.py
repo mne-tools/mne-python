7361791b75494e08f40ec25aa66ef645e332e5b9
Jussi Nurminen <jnu@iki.fi>
2018-12-12 17:13:36 +0200

    handle different time vecs in plot_evoked_topo()

--- mne/viz/topo.py
+++ mne/viz/topo.py
@@ -184,7 +184,7 @@ def _plot_topo(info, times, show_func, click_func=None, layout=None,
         layout.pos[:, :2] /= layout.pos[:, :2].max(0)
 
     # prepare callbacks
-    tmin, tmax = times[[0, -1]]
+    tmin, tmax = times[0], times[-1]
     click_func = show_func if click_func is None else click_func
     on_pick = partial(click_func, tmin=tmin, tmax=tmax, vmin=vmin,
                       vmax=vmax, ylim=ylim, x_label=x_label,
@@ -342,13 +342,13 @@ def _plot_timeseries(ax, ch_idx, tmin, tmax, vmin, vmax, ylim, data, color,
     import matplotlib.pyplot as plt
     from matplotlib.colors import colorConverter
     picker_flag = False
-    for data_, color_ in zip(data, color):
+    for data_, color_, times_ in zip(data, color, times):
         if not picker_flag:
             # use large tol for picker so we can click anywhere in the axes
-            ax.plot(times, data_[ch_idx], color=color_, picker=1e9)
+            ax.plot(times_, data_[ch_idx], color=color_, picker=1e9)
             picker_flag = True
         else:
-            ax.plot(times, data_[ch_idx], color=color_)
+            ax.plot(times_, data_[ch_idx], color=color_)
 
     if x_label is not None:
         ax.set(xlabel=x_label)
@@ -437,16 +437,16 @@ def _plot_timeseries_unified(bn, ch_idx, tmin, tmax, vmin, vmax, ylim, data,
     """Show multiple time series on topo using a single axes."""
     import matplotlib.pyplot as plt
     if not (ylim and not any(v is None for v in ylim)):
-        ylim = np.array([np.min(data), np.max(data)])
+        ylim = [min([np.min(d) for d in data]), max([np.max(d) for d in data])]
     # Translation and scale parameters to take data->under_ax normalized coords
     _compute_scalings(bn, (tmin, tmax), ylim)
     pos = bn.pos
     data_lines = bn.data_lines
     ax = bn.ax
     # XXX These calls could probably be made faster by using collections
-    for data_, color_ in zip(data, color):
+    for data_, color_, times_ in zip(data, color, times):
         data_lines.append(ax.plot(
-            bn.x_t + bn.x_s * times, bn.y_t + bn.y_s * data_[ch_idx],
+            bn.x_t + bn.x_s * times_, bn.y_t + bn.y_s * data_[ch_idx],
             linewidth=0.5, color=color_, clip_on=True, clip_box=pos)[0])
     if vline:
         vline = np.array(vline) * bn.x_s + bn.x_t
@@ -631,10 +631,6 @@ def _plot_evoked_topo(evoked, layout=None, layout_scale=0.945, color=None,
     else:
         color = cycle([color])
 
-    times = evoked[0].times
-    if not all((e.times == times).all() for e in evoked):
-        raise ValueError('All evoked.times must be the same')
-
     noise_cov = _check_cov(noise_cov, evoked[0].info)
     if noise_cov is not None:
         evoked = [whiten_evoked(e, noise_cov) for e in evoked]
@@ -730,6 +726,8 @@ def _plot_evoked_topo(evoked, layout=None, layout_scale=0.945, color=None,
 
     data = [e.data for e in evoked]
     comments = [e.comment for e in evoked]
+    times = [e.times for e in evoked]
+
     show_func = partial(_plot_timeseries_unified, data=data, color=color,
                         times=times, vline=vline, hline=hline,
                         hvline_color=font_color)
@@ -737,7 +735,9 @@ def _plot_evoked_topo(evoked, layout=None, layout_scale=0.945, color=None,
                          vline=vline, hline=hline, hvline_color=font_color,
                          labels=comments)
 
-    fig = _plot_topo(info=info, times=times, show_func=show_func,
+    time_min = min([t[0] for t in times])
+    time_max = max([t[-1] for t in times])
+    fig = _plot_topo(info=info, times=[time_min, time_max], show_func=show_func,
                      click_func=click_func, layout=layout, colorbar=False,
                      ylim=ylim_, cmap=None, layout_scale=layout_scale,
                      border=border, fig_facecolor=fig_facecolor,
