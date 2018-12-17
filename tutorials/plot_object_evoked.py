c8b61bb03cc3a4088bbe22700273780383051830
Eric Larson <larson.eric.d@gmail.com>
2018-12-17 10:45:22 -0500

    FIX: plt

--- tutorials/plot_object_evoked.py
+++ tutorials/plot_object_evoked.py
@@ -12,6 +12,7 @@ averaging epochs data with :func:`mne.Epochs.average`.
 import os.path as op
 
 import mne
+import matplotlib.pyplot as plt
 
 ###############################################################################
 # Here for convenience we read the evoked dataset from a file.
