eafc25599f3abf2833665b5a8cf8b7e5fea1e92e
Eric Larson <larson.eric.d@gmail.com>
2018-12-21 16:29:25 -0500

    FIX: Fix for sklearn

--- mne/inverse_sparse/mxne_optim.py
+++ mne/inverse_sparse/mxne_optim.py
@@ -302,7 +302,8 @@ def _mixed_norm_solver_cd(M, G, alpha, lipschitz_constant, maxit=10000,
     clf = MultiTaskLasso(alpha=alpha / len(M), tol=tol / sum_squared(M),
                          normalize=False, fit_intercept=False, max_iter=maxit,
                          warm_start=True)
-    clf.coef_ = init.T if init is not None else init
+    if init is not None:
+        clf.coef_ = init.T
     clf.fit(G, M)
 
     X = clf.coef_.T
