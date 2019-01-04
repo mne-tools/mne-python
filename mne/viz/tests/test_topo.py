2f3c7cde58eb1ba12c0f787480b1af1bbe10df05
Jussi Nurminen <jnu@iki.fi>
2019-01-04 14:29:52 +0200

    test plot_evoked_topo for nonuniform times

--- mne/viz/tests/test_topo.py
+++ mne/viz/tests/test_topo.py
@@ -153,7 +153,11 @@ def test_plot_topo():
 def test_plot_topo_single_ch():
     """Test single channel topoplot with time cursor."""
     evoked = _get_epochs().average()
-    fig = plot_evoked_topo(evoked, background_color='w')
+    evoked2 = evoked.copy()
+    # test plotting several evokeds on different time grids
+    evoked.crop(-.19, 0)
+    evoked2.crop(.05, .19)
+    fig = plot_evoked_topo([evoked, evoked2], background_color='w')
     # test status bar message
     ax = plt.gca()
     assert ('MEG 0113' in ax.format_coord(.065, .63))
