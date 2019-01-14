871d2def7642371c61228d6a17ce1066b99a279b
Joan Massich <sik@visor.udg.edu>
2019-01-14 14:51:43 +0100

    mock _fetch_file to avoid downloading and speed up the testing

--- mne/datasets/sleep_physionet/tests/test_physionet.py
+++ mne/datasets/sleep_physionet/tests/test_physionet.py
@@ -24,6 +24,10 @@ def physionet_tmpdir(tmpdir_factory):
     return str(tmpdir_factory.mktemp('physionet_files'))
 
 
+def _fake_fetch_file(url, path, print_destination, hash_, hash_type):
+    pass
+
+
 def _keep_basename_only(path_structure):
     return np.vectorize(op.basename)(np.array(path_structure))
 
@@ -42,8 +46,11 @@ def test_run_update_age_records(tmpdir):
 
 
 @requires_good_network
-def test_sleep_physionet_age(physionet_tmpdir):
+def test_sleep_physionet_age(physionet_tmpdir, mocker):
     """Test Sleep Physionet URL handling."""
+    mocker.patch('mne.datasets.sleep_physionet._utils._fetch_file',
+                 side_effect=_fake_fetch_file)
+
     params = {'path': physionet_tmpdir, 'update_path': False}
 
     with pytest.raises(ValueError, match='Only subjects 0 to 19 are'):
@@ -78,13 +85,24 @@ def test_run_update_temazepam_records(tmpdir):
 
 
 @requires_good_network
-def test_sleep_physionet_temazepam(physionet_tmpdir):
+def test_sleep_physionet_temazepam(physionet_tmpdir, mocker):
     """Test Sleep Physionet URL handling."""
+    mm = mocker.patch('mne.datasets.sleep_physionet._utils._fetch_file',
+                      side_effect=_fake_fetch_file)
+
     params = {'path': physionet_tmpdir, 'update_path': False}
 
     paths = temazepam.fetch_data(subjects=[0], **params)
     assert_array_equal(_keep_basename_only(paths),
                        [['ST7011J0-PSG.edf', 'ST7011JP-Hypnogram.edf']])
+
+    EXPECTED_URL = 'https://physionet.org/pn4/sleep-edfx//ST7011JP-Hypnogram.edf'
+    EXPECTED_PATH = physionet_tmpdir + '/physionet-sleep-data/ST7011JP-Hypnogram.edf'
+    mm.assert_called_with(EXPECTED_URL,
+                          EXPECTED_PATH,
+                          hash_='ff28e5e01296cefed49ae0c27cfb3ebc42e710bf',
+                          hash_type='sha1',
+                          print_destination=False)
 
     with pytest.raises(ValueError, match='Only subjects 0 to 21 are'):
         paths = temazepam.fetch_data(subjects=[22], **params)
