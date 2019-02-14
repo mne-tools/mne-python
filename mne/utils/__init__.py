e4ba0d30273fd32025792c449ede88488d937b0c
britta-wstnr <britta.wstnr@gmail.com>
2019-02-14 09:15:19 +0100

    fix comment

--- mne/utils/__init__.py
+++ mne/utils/__init__.py
@@ -1,6 +1,6 @@
 # # # WARNING # # #
-# This list must also be updated in doc/_templates/class.rst if it is
-# changed here!
+# This list must also be updated in doc/_templates/autosummary/class.rst if it
+# is changed here!
 _doc_special_members = ('__contains__', '__getitem__', '__iter__', '__len__',
                         '__add__', '__sub__', '__mul__', '__div__',
                         '__neg__', '__hash__')
