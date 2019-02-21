5edd62ceea754dd7c8a1fa64bd170ee312c72b11
skjerns <14980558+skjerns@users.noreply.github.com>
2019-02-21 14:39:39 +0100

    changed checking for int to include int64

--- mne/io/pick.py
+++ mne/io/pick.py
@@ -829,7 +829,7 @@ def _picks_to_idx(info, picks, none='data', exclude='bads', allow_empty=False,
         raise ValueError('picks must be 1D, got %sD' % (picks.ndim,))
     if picks.dtype.char in ('S', 'U'):
         picks = _picks_str_to_idx(info, picks, exclude, with_ref_meg)
-    if picks.dtype != np.int:
+    if picks.dtype.kind != 'i':
         raise TypeError('picks must be a list of int or list of str, got '
                         'a data type of %s' % (picks.dtype,))
 
