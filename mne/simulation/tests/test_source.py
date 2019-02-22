c49f2f203f8ae7536e841b65f540fafa7ffacb61
Quentin Bertrand <quentinbertrand54@gmail.com>
2019-02-22 12:03:29 +0100

    [ENH] simulate_sparse_stc throws error when n_dipoles > len(labels) (#5976)
    This PR ensures that `simulate_sparse_stc` throws an error when `n_dipoles > len(labels)`
    
    Fixes #5953r

--- mne/simulation/tests/test_source.py
+++ mne/simulation/tests/test_source.py
@@ -146,6 +146,12 @@ def test_simulate_sparse_stc():
                   subjects_dir=subjects_dir)  # no subject
     pytest.raises(ValueError, simulate_sparse_stc, fwd['src'], len(labels),
                   times, labels=labels, location='foo')  # bad location
+    err_str = 'Number of labels'
+    with pytest.raises(ValueError, match=err_str):
+        simulate_sparse_stc(
+            fwd['src'], len(labels) + 1, times, labels=labels,
+            random_state=random_state, location=location,
+            subjects_dir=subjects_dir)
 
 
 @testing.requires_testing_data
