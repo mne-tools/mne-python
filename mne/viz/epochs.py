d34fb741a526255815f8a99d127fe5846e696bc1
Mainak Jas <mainakjas@gmail.com>
2019-03-01 13:25:50 -0500

    FIX: don't use .keys when looking up dict

--- mne/viz/epochs.py
+++ mne/viz/epochs.py
@@ -1951,7 +1951,7 @@ def _plot_histogram(params):
         plt.ylabel('Count')
         color = colors[types[idx]]
         rej = None
-        if epochs.reject is not None and types[idx] in epochs.reject.keys():
+        if epochs.reject is not None and types[idx] in epochs.reject:
             rej = epochs.reject[types[idx]] * scalings[types[idx]]
             rng = [0., rej * 1.1]
         else:
