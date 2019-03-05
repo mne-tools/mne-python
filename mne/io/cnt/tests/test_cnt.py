1296537756fd2e8b0cacfb6d88f9a21ecf0ddc64
Joan Massich <sik@visor.udg.edu>
2019-03-05 18:05:33 +0100

    iter

--- mne/io/cnt/tests/test_cnt.py
+++ mne/io/cnt/tests/test_cnt.py
@@ -5,8 +5,10 @@
 
 import os.path as op
 import pytest
+import socket
 
 from mne import pick_types
+from mne import __file__ as _mne_file
 from mne.utils import run_tests_if_main
 from mne.datasets import testing
 from mne.io.tests.test_raw import _test_raw_reader
@@ -15,6 +17,11 @@ from mne.io.cnt import read_raw_cnt
 data_path = testing.data_path(download=False)
 fname = op.join(data_path, 'CNT', 'scan41_short.cnt')
 
+if socket.gethostname() == 'toothless':
+    test_files = [fname, op.join(op.dirname(_mne_file), '..',
+                                 'sandbox', 'data', '914flankers.cnt')]
+else:
+    test_files = [fname]
 
 @testing.requires_testing_data
 def test_data():
@@ -28,6 +35,24 @@ def test_data():
 
     # XXX: the data has "05/10/200 17:35:31" so it is set to None
     assert raw.info['meas_date'] is None
+
+
+@testing.requires_testing_data
+@pytest.mark.parametrize('fname', test_files, ids=op.basename)
+def test_explore_stuff_for_is5493(fname):
+    SETUP_MEAN_AGE_OFFSET = 247
+
+    # float  mean_age;       /* Mean age (Group files only)         */
+    # float  stdev;          /* Std dev of age (Group files only)   */
+    # short int n;           /* Number in group file                */
+    # char   compfile[38];   /* Path and name of comparison file    */
+    # float  SpectWinComp;   /* Spectral window compensation factor */
+    # float  MeanAccuracy;   /* Average respose accuracy            */
+    # float  MeanLatency;    /* Average response latency            */
+    # char   sortfile[46];   /* Path and name of sort file          */
+    # long   NumEvents;      /* Number of events in eventable       */
+
+    assert True
 
 
 run_tests_if_main()
