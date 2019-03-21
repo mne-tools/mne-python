68cdc1fb2c7a2b6182e3ca92e1a684947d3c0f6c
Guillaume Favelier <guillaume.favelier@gmail.com>
2019-03-21 12:07:00 +0100

    Catch DeprecationWarning coming from open with U flag

--- mne/viz/backends/_vispy.py
+++ mne/viz/backends/_vispy.py
@@ -5,10 +5,13 @@ Actual implementation of _Renderer and _Projection classes.
 """
 
 import numpy as np
-from vispy import scene
 from vispy.color import Colormap
 from vispy.visuals.filters import Alpha
 from vispy.visuals.transforms import STTransform
+import warnings
+with warnings.catch_warnings():  # catch the VisPy warnings
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    from vispy import scene
 
 default_sphere_radius = 0.5
 default_mesh_shininess = 0.0
