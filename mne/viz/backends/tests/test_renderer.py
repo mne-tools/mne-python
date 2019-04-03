5d12035bd02384d776fb791e171f6d1d1cdef94d
Guillaume Favelier <guillaume.favelier@gmail.com>
2019-04-03 10:59:47 +0200

    Move skipif marks and has_not functions to mne.utils._testing

--- mne/viz/backends/tests/test_renderer.py
+++ mne/viz/backends/tests/test_renderer.py
@@ -10,9 +10,8 @@ import pytest
 import importlib
 import numpy as np
 from mne.viz.backends.renderer import get_3d_backend
-from mne.viz.backends.tests._utils import (skips_if_not_vispy,
-                                           skips_if_not_mayavi)
-from mne.utils import requires_mayavi
+from mne.utils import (requires_mayavi, skips_if_not_mayavi,
+                       skips_if_not_vispy)
 
 # from .._utils import DEFAULT_3D_BACKEND
 DEFAULT_3D_BACKEND = 'mayavi'  # This should be done with the import
