98d0b8f7bac6f47f2f530d89e24f8aab24cc414e
Marijn van Vliet <w.m.vanvliet@gmail.com>
2019-04-26 14:58:07 +0200

    Fix weight_norm='nai' on DICS
    The case when inversion='single', pick_ori='max-power', weight_norm='ai'
    was not caught by the if-statements in _compute_beamformer. This fixes
    it.

--- mne/beamformer/_compute_beamformer.py
+++ mne/beamformer/_compute_beamformer.py
@@ -262,7 +262,7 @@ def _compute_beamformer(G, Cm, reg, n_orient, weight_norm, pick_ori,
 
             if pick_ori == 'max-power':
                 # Compute the power
-                if inversion == 'single' and weight_norm == 'unit-noise-gain':
+                if inversion == 'single' and weight_norm is not None:
                     # First make the filters unit gain, then apply them to the
                     # cov matrix to compute power.
                     Wk_norm = Wk / np.sqrt(np.sum(Wk ** 2, axis=1,
