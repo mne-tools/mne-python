3d33e8eeb3e77b50b6b405b8cdf0ebdd5d5d8658
jona-sassenhagen <jona.sassenhagen@gmail.com>
2019-04-27 16:44:55 +0200

    MRG +1 plot_compare_evokeds topo functionality (#6211)
    * init
    
    * basic topo functionality
    
    * progress
    
    * better
    
    * ...
    
    * del legend title
    
    * interactive
    
    * gfpmagic
    
    * fixes
    
    * fix split_legend
    
    * :D
    
    * fix missing ticks
    
    * fix docstrings
    
    * fix pep
    
    * pep again
    
    * str picks
    
    * pep
    
    * fix gfp stuff
    
    * gfp thing
    
    * refactor ticking, etc [ci skip]
    
    * do not plot CIs for legend
    
    * move legend to iter_topo
    
    * spelling
    
    * comment
    
    * test for iter_topo change
    
    * dans comments
    
    * fix picks
    
    * ...
    
    * another dumb commit to deal with np.bool_
    
    * ...
    
    * add iter_topo test
    
    * comments

--- mne/viz/topo.py
+++ mne/viz/topo.py
@@ -25,7 +25,7 @@ from .utils import (_check_delayed_ssp, _get_color_list, _draw_proj_checkbox,
 
 def iter_topography(info, layout=None, on_pick=None, fig=None,
                     fig_facecolor='k', axis_facecolor='k',
-                    axis_spinecolor='k', layout_scale=None):
+                    axis_spinecolor='k', layout_scale=None, legend=False):
     """Create iterator over channel positions.
 
     This function returns a generator that unpacks into
@@ -58,6 +58,10 @@ def iter_topography(info, layout=None, on_pick=None, fig=None,
     layout_scale: float | None
         Scaling factor for adjusting the relative size of the layout
         on the canvas. If None, nothing will be scaled.
+    legend: bool
+        If True, an additional axis is created in the bottom right corner
+        that can be used to, e.g., construct a legend. The index of this
+        axis will be -1.
 
     Returns
     -------
@@ -70,12 +74,22 @@ def iter_topography(info, layout=None, on_pick=None, fig=None,
 
     """
     return _iter_topography(info, layout, on_pick, fig, fig_facecolor,
-                            axis_facecolor, axis_spinecolor, layout_scale)
+                            axis_facecolor, axis_spinecolor, layout_scale,
+                            legend=legend)
+
+
+def _legend_axis(pos):
+    """Add a legend axis to the bottom right."""
+    import matplotlib.pyplot as plt
+    left, bottom = pos[:, 0].max(), pos[:, 1].min()
+    wid, hei = pos[-1, 2:]
+    return plt.axes([left, bottom + .05, wid, hei])
 
 
 def _iter_topography(info, layout, on_pick, fig, fig_facecolor='k',
                      axis_facecolor='k', axis_spinecolor='k',
-                     layout_scale=None, unified=False, img=False, axes=None):
+                     layout_scale=None, unified=False, img=False, axes=None,
+                     legend=False):
     """Iterate over topography.
 
     Has the same parameters as iter_topography, plus:
@@ -139,10 +153,12 @@ def _iter_topography(info, layout, on_pick, fig, fig_facecolor='k',
         if not unified:  # old, slow way
             ax = plt.axes(pos[idx])
             ax.patch.set_facecolor(axis_facecolor)
-            plt.setp(list(ax.spines.values()), color=axis_spinecolor)
-            ax.set(xticklabels=[], yticklabels=[])
-            plt.setp(ax.get_xticklines(), visible=False)
-            plt.setp(ax.get_yticklines(), visible=False)
+            for spine in ax.spines.values():
+                spine.set_color(axis_spinecolor)
+            if not legend:
+                ax.set(xticklabels=[], yticklabels=[])
+                for tick in ax.get_xticklines() + ax.get_yticklines():
+                    tick.set_visible(False)
             ax._mne_ch_name = name
             ax._mne_ch_idx = ch_idx
             ax._mne_ax_face_color = axis_facecolor
@@ -153,6 +169,10 @@ def _iter_topography(info, layout, on_pick, fig, fig_facecolor='k',
                        _mne_ch_name=name, _mne_ch_idx=ch_idx,
                        _mne_ax_face_color=axis_facecolor)
             axs.append(ax)
+    if not unified and legend:
+        ax = _legend_axis(pos)
+        yield ax, -1
+
     if unified:
         under_ax._mne_axs = axs
         # Create a PolyCollection for the axis backgrounds
