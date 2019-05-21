b6be6fff5963c471d7cc203cada2ffea33167226
Kambiz Tavabi <ktavabi@uw.edu>
2019-05-21 15:30:03 -0700

    Update mne/source_estimate.py
    Co-Authored-By: Daniel McCloy <dan.mccloy@gmail.com>

--- mne/source_estimate.py
+++ mne/source_estimate.py
@@ -2688,7 +2688,7 @@ def extract_label_time_course(stcs, labels, src, mode='mean_flip',
         Max value within each label.
     
     If encountering a ``ValueError`` due to mismatch between number of
-    source points in the subject source space and computed ``'stc'`` object set
+    source points in the subject source space and computed ``stc`` object set
     ``'src'`` argument to ``'fwd['src']'`` to ensure the source space is
     compatible between forward and inverse routines.
 
