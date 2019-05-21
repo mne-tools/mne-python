fix: wrong sign; forgot orig_time

tutorials/intro/plot_object_annotations.py
@@ -222,13 +222,18 @@ print(events_from_annot[:5])
 # Event ID to string descriptions, and use the :class:`~mne.Annotations`
 # constructor to create the :class:`~mne.Annotations` object, and use the
 # :meth:`~mne.io.Raw.set_annotations` method to add the annotations to the
-# :class:`~mne.io.Raw` object:
+# :class:`~mne.io.Raw` object. Because the :ref:`sample data <sample-dataset>`
+# was recorded on a Neuromag system (where sample numbering starts when the
+# acquisition system is initiated, not when the *recording* is initiated), we
+# also need to pass in the ``orig_time`` parameter so that the onsets are
+# properly aligned relative to the start of recording:
 
 mapping = {1: 'auditory/left', 2: 'auditory/right', 3: 'visual/left',
            4: 'visual/right', 5: 'smiley', 32: 'buttonpress'}
-onsets = events[:, 0] * raw.info['sfreq']
+onsets = events[:, 0] / raw.info['sfreq']
 durations = np.zeros_like(onsets)  # assumes instantaneous events
 descriptions = [mapping[event_id] for event_id in events[:, 2]]
 annot_from_events = mne.Annotations(onset=onsets, duration=durations,
-                                    description=descriptions)
+                                    description=descriptions,
+                                    orig_time=raw.info['meas_date'])
 raw.set_annotations(annot_from_events)