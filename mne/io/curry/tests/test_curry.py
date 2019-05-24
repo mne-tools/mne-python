a8136b938f6e0a4424b80f8a49be53310faa7448
Dirk Gütlin <dirk.guetlin@stud.sbg.ac.at>
2019-05-24 19:19:04 +0200

    Moved Test data.
    Moved all the test files to mne-testing-data.
    
    Set test data version accordingly in `mne.datasets.utils`
    
    Changed `test_curry` to read test data from the new location
    
    Signed-off-by: Dirk Gütlin <dirk.guetlin@stud.sbg.ac.at>

--- mne/io/curry/tests/test_curry.py
+++ mne/io/curry/tests/test_curry.py
@@ -5,8 +5,6 @@
 #
 # License: BSD (3-clause)
 
-
-import inspect
 import os
 import os.path as op
 from numpy.testing import assert_allclose, assert_equal, assert_array_almost_equal
@@ -16,31 +14,27 @@ from mne.io.curry import read_raw_curry
 from mne.io.ctf import read_raw_ctf
 from mne.io.bti import read_raw_bti
 
-FILE = inspect.getfile(inspect.currentframe())
+data_dir = testing.data_path(download=False)
+curry_dir = op.join(data_dir, "curry")
 
-curry_data_dir = op.join(op.dirname(op.abspath(FILE)), 'data')
+bdf_file = op.join(data_dir, 'BDF/test_bdf_stim_channel.bdf')
 
+ctf_alp_file = op.join(data_dir, 'CTF/catch-alp-good-f.ds')
 
-ext_data_dir = testing.data_path(download=False)
+bti_rfDC_file = op.join(data_dir, 'BTi/erm_HFH/c,rfDC')
 
-bdf_file = op.join(ext_data_dir, 'BDF/test_bdf_stim_channel.bdf')
+curry7_rfDC_file = op.join(curry_dir, "c,rfDC Curry 7.dat")
+curry8_rfDC_file = op.join(curry_dir, "c,rfDC Curry 8.cdt")
 
-ctf_alp_file = op.join(ext_data_dir, 'CTF/catch-alp-good-f.ds')
+curry7_alp_file = op.join(curry_dir, "catch-alp-good-f Curry 7.dat")
+curry8_alp_file = op.join(curry_dir, "catch-alp-good-f Curry 8.cdt")
 
-bti_rfDC_file = op.join(ext_data_dir, 'BTi/erm_HFH/c,rfDC')
-
-curry7_rfDC_file = op.join(curry_data_dir, "c,rfDC Curry 7.dat")
-curry8_rfDC_file = op.join(curry_data_dir, "c,rfDC Curry 8.cdt")
-
-curry7_alp_file = op.join(curry_data_dir, "catch-alp-good-f Curry 7.dat")
-curry8_alp_file = op.join(curry_data_dir, "catch-alp-good-f Curry 8.cdt")
-
-curry7_bdf_file = op.join(curry_data_dir, "test_bdf_stim_channel Curry 7.dat")
-curry7_bdf_ascii_file = op.join(curry_data_dir,
+curry7_bdf_file = op.join(curry_dir, "test_bdf_stim_channel Curry 7.dat")
+curry7_bdf_ascii_file = op.join(curry_dir,
                                 "test_bdf_stim_channel Curry 7 ASCII.dat")
 
-curry8_bdf_file = op.join(curry_data_dir, "test_bdf_stim_channel Curry 8.cdt")
-curry8_bdf_ascii_file = op.join(curry_data_dir,
+curry8_bdf_file = op.join(curry_dir, "test_bdf_stim_channel Curry 8.cdt")
+curry8_bdf_ascii_file = op.join(curry_dir,
                                 "test_bdf_stim_channel Curry 8 ASCII.cdt")
 
 
@@ -48,8 +42,8 @@ def test_io_curry():
     """Test reading CURRY files."""
 
     # make sure all test files can be passed to read_raw_curry
-    for filename in os.listdir(curry_data_dir):
-        read_raw_curry(op.join(curry_data_dir, filename))
+    for filename in os.listdir(curry_dir):
+        read_raw_curry(op.join(curry_dir, filename))
 
     ctf_alp = read_raw_ctf(ctf_alp_file)
     curry7_alp = read_raw_curry(curry7_alp_file)
@@ -60,15 +54,15 @@ def test_io_curry():
     assert_equal(curry7_alp.info["sfreq"], ctf_alp.info["sfreq"])
     assert_equal(curry8_alp.info["sfreq"], ctf_alp.info["sfreq"])
 
-    assert_array_almost_equal(curry7_alp._data, ctf_alp.get_data())
-    assert_array_almost_equal(curry8_alp._data, ctf_alp.get_data())
+    assert_array_almost_equal(curry7_alp.get_data(), ctf_alp.get_data())
+    assert_array_almost_equal(curry8_alp.get_data(), ctf_alp.get_data())
 
     bti_rfDC = read_raw_bti(bti_rfDC_file)  # where is the headfile for this?
     curry7_rfDC = read_raw_curry(curry7_rfDC_file)
     curry8_rfDC = read_raw_curry(curry8_rfDC_file)
 
-    assert_array_almost_equal(curry7_rfDC._data, bti_rfDC.get_data())
-    assert_array_almost_equal(curry8_rfDC._data, bti_rfDC.get_data())
+    assert_array_almost_equal(curry7_rfDC.get_data(), bti_rfDC.get_data())
+    assert_array_almost_equal(curry8_rfDC.get_data(), bti_rfDC.get_data())
 
     bdf = mne.io.read_raw_bdf(bdf_file)
     curry7_bdf = read_raw_curry(curry7_bdf_file)
@@ -83,7 +77,8 @@ def test_io_curry():
                      curry8_bdf.info["sfreq"], curry8_bdf_ascii.info["sfreq"]],
                     bdf.info["sfreq"])
 
-    assert_array_almost_equal(curry7_bdf._data, bdf.get_data()[0:3])
-    assert_array_almost_equal(curry7_bdf_ascii._data, bdf.get_data())
-    assert_array_almost_equal(curry8_bdf._data, bdf.get_data())
-    assert_array_almost_equal(curry8_bdf_ascii._data, bdf.get_data())
+    # we only use [0:3] here since the stim channel was already extracted for the curry files
+    assert_array_almost_equal(curry7_bdf.get_data(), bdf.get_data()[0:3])
+    assert_array_almost_equal(curry7_bdf_ascii.get_data(), bdf.get_data()[0:3])
+    assert_array_almost_equal(curry8_bdf.get_data(), bdf.get_data()[0:3])
+    assert_array_almost_equal(curry8_bdf_ascii.get_data(), bdf.get_data()[0:3])
