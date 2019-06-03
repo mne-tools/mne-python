f1fecb31756ec84db0cf27a79d35b85fdc7c89ee
Daniel McCloy <dan.mccloy@gmail.com>
2019-06-03 11:20:51 -0800

    address comments; reorg/smooth a little

--- tutorials/misc/plot_sensor_locations.py
+++ tutorials/misc/plot_sensor_locations.py
@@ -31,9 +31,15 @@ raw = mne.io.read_raw_fif(sample_data_raw_file, preload=True, verbose=False)
 #
 # MNE-Python comes pre-loaded with information about the sensor positions of
 # many MEG and EEG systems. This information is stored in *layout files* and
-# *montages*, which are stored in your ``mne-python`` installation directory,
-# in the ``mne/channels/data/layouts`` and ``mne/channels/data/montages``
-# folders respectively:
+# *montages*. :class:`Layouts <mne.channels.Layout>` give sensor positions in 2
+# dimensions (defined by ``x``, ``y``, ``width``, and ``height`` values for
+# each sensor), and are primarily used for illustrative purposes (i.e., making
+# diagrams of approximate sensor positions in top-down diagrams of the head).
+# In contrast, :class:`montages <mne.channels.Montage>` give sensor positions
+# in 3D (``x``, ``y``, ``z``, in meters). Many layout and montage files are
+# included during MNE-Python installation, and are stored in your
+# ``mne-python`` directory, in the :file:`mne/channels/data/layouts` and
+# :file:`mne/channels/data/montages` folders, respectively:
 
 data_dir = os.path.join(os.path.dirname(mne.__file__), 'channels', 'data')
 for subfolder in ['layouts', 'montages']:
@@ -45,18 +51,29 @@ for subfolder in ['layouts', 'montages']:
 # .. sidebar:: Computing sensor locations
 #
 #     If you are interested in how standard ("idealized") EEG sensor positions
-#     are computed on a spherical head model,
-#     the `eeg_positions`_ repository provides code and documentation to this
-#     end.
+#     are computed on a spherical head model, the `eeg_positions`_ repository
+#     provides code and documentation to this end.
 #
-# *Layouts* give sensor positions in 2 dimensions (defined by ``x``, ``y``,
-# ``width``, and ``height`` values for each sensor), and are primarily used for
-# illustrative purposes (i.e., making diagrams of approximate sensor
-# positions). In contrast, *montages* give sensor positions in 3D (``x``,
-# ``y``, ``z``, in meters). In addition, montages come in two "flavors" —
-# :class:`~mne.channels.Montage` objects (for idealized sensor montages) and
-# :class:`~mne.channels.DigMontage` (for subject-specific digitizations of
-# sensor positions, based on measurements during the recording session).
+# As you may be able to tell from the filenames shown above, the included
+# montage files are all for EEG systems. These are *idealized* sensor positions
+# based on a spherical head model. Montage files for MEG systems are not
+# provided because the 3D coordinates of MEG sensors are included in the raw
+# recordings from MEG systems, and are automatically stored in the ``info``
+# attribute of the :class:`~mne.io.Raw` file upon loading. In contrast, layout
+# files *are* included for MEG systems (to facilitate easy plotting of MEG
+# sensor location diagrams).
+#
+# You may also have noticed that the file formats and filename extensions of
+# layout and montage files vary considerably. This reflects different
+# manufacturers' conventions; to simplify this, the montage and layout loading
+# functions in MNE-Python take the filename *without its extension* so you
+# don't have to keep track of which file format is used by which manufacturer.
+# Examples of this can be seen in the following sections.
+#
+# If you have digitized the locations of EEG sensors on the scalp during your
+# recording session (e.g., with a Polhemous Fastrak digitizer), these can be
+# loaded in MNE-Python as :class:`~mne.channels.DigMontage` objects; see
+# :ref:`reading-dig-montages` (below).
 #
 #
 # Working with layout files
@@ -76,7 +93,9 @@ biosemi_layout.plot()  # same result as: mne.viz.plot_layout(biosemi_layout)
 # :class:`~mne.channels.Layout` objects also has a ``picks`` argument. However,
 # because layouts only contain information about sensor name and location (not
 # sensor type), the :meth:`~mne.channels.Layout.plot` method only allows
-# picking channels by index (not by name or by type).
+# picking channels by index (not by name or by type). Here we find the indices
+# we want using :func:`numpy.where`; selection by name or type is possible via
+# :func:`mne.pick_channels` or :func:`mne.pick_types`.
 
 midline = np.where([name.endswith('z') for name in biosemi_layout.names])[0]
 biosemi_layout.plot(picks=midline)
@@ -84,8 +103,8 @@ biosemi_layout.plot(picks=midline)
 ###############################################################################
 # If you're working with a :class:`~mne.io.Raw` object that already has sensor
 # positions incorporated, you can create a :class:`~mne.channels.Layout` object
-# with either the :func:`mne.channels.make_eeg_layout` function or (equivalently) the
-# :func:`mne.channels.find_layout` function.
+# with either the :func:`mne.channels.make_eeg_layout` function or
+# (equivalently) the :func:`mne.channels.find_layout` function.
 
 layout_from_raw = mne.channels.make_eeg_layout(raw.info)
 # same result as: mne.channels.find_layout(raw.info, ch_type='eeg')
@@ -96,14 +115,18 @@ layout_from_raw.plot()
 #
 #     There is no corresponding ``make_meg_layout`` function because sensor
 #     locations are fixed in a MEG system (unlike in EEG, where the sensor caps
-#     deform to fit each subject's head). Thus for MEG layouts you can simply
-#     load them with :func:`mne.channels.read_layout` or
-#     :func:`mne.channels.find_layout` as shown above.
+#     deform to fit each subject's head). Thus MEG layouts are consistent for a
+#     given system and you can simply load them with
+#     :func:`mne.channels.read_layout`, or use :func:`mne.channels.find_layout`
+#     with the ``ch_type`` parameter, as shown above for EEG.
 #
 # All :class:`~mne.channels.Layout` objects have a
 # :meth:`~mne.channels.Layout.save` method that allows writing layouts to disk,
-# in either ``.lout`` or ``.lay`` format (which format is inferred from the
-# file extension you pass to the method's ``fname`` parameter).
+# in either :file:`.lout` or :file:`.lay` format (which format gets written is
+# inferred from the file extension you pass to the method's ``fname``
+# parameter). The choice between :file:`.lout` and :file:`.lay` format only
+# matters if you need to load the layout file in some other software
+# (MNE-Python can read either format equally well).
 #
 #
 # Working with montage files
@@ -139,19 +162,26 @@ raw.plot_sensors(ch_type='eeg', axes=ax3d, kind='3d')
 ax3d.view_init(azim=70, elev=15)
 
 ###############################################################################
+# .. _reading-dig-montages:
+#
 # Reading sensor digitization files
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
 # It's probably evident from the 2D topomap above that there is some
-# irregularity in the sensor positions — this is because the sensor positions
-# stored in the :ref:`sample dataset <sample-dataset>` files are digitizations
-# of the sensor positions on an actual subject's head. Sensor digitizations are
-# read with :func:`mne.channels.read_dig_montage` and added to
-# :class:`~mne.io.Raw` objects with the :meth:`~mne.io.Raw.set_montage` method;
-# in the sample data this was done prior to saving the :class:`~mne.io.Raw`
-# object to disk. See the documentation of
-# :func:`~mne.channels.read_dig_montage` and :meth:`~mne.io.Raw.set_montage`
-# for further details.
+# irregularity in the EEG sensor positions in the :ref:`sample dataset
+# <sample-dataset>` — this is because the sensor positions in that dataset are
+# digitizations of the sensor positions on an actual subject's head. Sensor
+# digitizations are read with :func:`mne.channels.read_dig_montage` and added
+# to :class:`~mne.io.Raw` objects with the :meth:`~mne.io.Raw.set_montage`
+# method; in the sample data this was done prior to saving the
+# :class:`~mne.io.Raw` object to disk, so the sensor positions are already
+# incorporated into the ``info`` attribute of the :class`~mne.io.Raw` object.
+# See the documentation of :func:`~mne.channels.read_dig_montage` and
+# :meth:`~mne.io.Raw.set_montage` for further details. Once loaded,
+# :class:`~mne.channels.DigMontage` objects work similarly to
+# :class:`~mne.channels.Montage` objects (e.g, they have similar
+# :meth:`~mne.channels.DigMontage.plot` and
+# :meth:`~mne.channels.DigMontage.save` methods).
 #
 # .. TODO sample data doesn't have separate .hsp or .elp files, so can't demo
 #    this functionality
@@ -160,7 +190,7 @@ ax3d.view_init(azim=70, elev=15)
 # Rendering sensor position with mayavi
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
-# It is also possible to render an image of the sensor helmet in 3D, using
+# It is also possible to render an image of a MEG sensor helmet in 3D, using
 # mayavi instead of matplotlib, by calling the :func:`mne.viz.plot_alignment`
 # function:
 
