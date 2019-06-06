f1ed53a85a04ef449c9283ec0f85ec9c1141ed4c
Joan Massich <sik@visor.udg.edu>
2019-06-06 22:12:38 +0200

    cosmit

--- mne/io/brainvision/brainvision.py
+++ mne/io/brainvision/brainvision.py
@@ -848,6 +848,6 @@ class _BVEventParser(_DefaultEventParser):
 
 def _check_bv_annot(descriptions):
     markers_basename = set([dd.rstrip('0123456789 ') for dd in descriptions])
-    bv_markers = set(_BV_EVENT_IO_OFFSETS.keys()).union(
-        set(_OTHER_ACCEPTED_MARKERS.keys()))
+    bv_markers = (set(_BV_EVENT_IO_OFFSETS.keys())
+                  .union(set(_OTHER_ACCEPTED_MARKERS.keys())))
     return len(markers_basename - bv_markers) == 0
