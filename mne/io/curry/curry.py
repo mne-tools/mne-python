0a6565783182e9df934355be7b15745cae6b36b9
Dirk Gütlin <dirk.guetlin@stud.sbg.ac.at>
2019-06-23 13:19:28 +0200

    reverted commit 638c319aad263753f1696c0f05ff67f9ca8499c4
    since np.isin is not contained in mne's minimally required numpy version
    
    Signed-off-by: Dirk Gütlin <dirk.guetlin@stud.sbg.ac.at>

--- mne/io/curry/curry.py
+++ mne/io/curry/curry.py
@@ -182,7 +182,7 @@ def _read_events_curry(fname, event_ids=None):
     curry_events = np.array(events_dict["NUMBER_LIST"], dtype=int)[:, 0:3]
 
     if event_ids is not None:
-        idx = np.isin(curry_events[:, -1], event_ids)
+        idx = np.array([i in event_ids for i in curry_events[:, -1]])
         curry_events = curry_events[idx]
 
     return curry_events
