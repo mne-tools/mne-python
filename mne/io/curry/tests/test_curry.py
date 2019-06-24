5fe9c646cdf78101141944f110aed653ed05a6ea
Dirk Gütlin <dirk.guetlin@stud.sbg.ac.at>
2019-06-24 16:46:10 +0200

    changed warning handling in test_curry
    warnings are now catched for python version < 1.16.0
    
    Signed-off-by: Dirk Gütlin <dirk.guetlin@stud.sbg.ac.at>

--- mne/io/curry/tests/test_curry.py
+++ mne/io/curry/tests/test_curry.py
@@ -17,6 +17,7 @@ from mne.io.edf import read_raw_bdf
 from mne.io.bti import read_raw_bti
 from mne.io.curry import read_raw_curry
 from mne.io.curry.curry import _check_missing_files, _read_events_curry
+from mne.utils import check_version
 
 
 data_dir = testing.data_path(download=False)
@@ -39,7 +40,13 @@ curry8_bdf_ascii_file = op.join(curry_dir,
 
 event_file = op.join(curry_dir, "test_bdf_stim_channel Curry 7.cef")
 
+if not check_version("numpy", "1.16.0"):
+    do_warn = 'ignore:.*take longer for ASCII.*:'
+else:
+    do_warn = ''
 
+
+@pytest.mark.filterwarnings(do_warn)
 @testing.requires_testing_data
 def test_read_raw_curry():
     """Test reading CURRY files."""
@@ -72,9 +79,8 @@ def test_read_raw_curry():
     curry8_bdf = read_raw_curry(curry8_bdf_file, preload=False)
     curry7_bdf_ascii = read_raw_curry(curry7_bdf_ascii_file,
                                       preload=False)
-    with pytest.warns(RuntimeWarning, match="take longer for ASCII"):
-        curry8_bdf_ascii = read_raw_curry(curry7_bdf_ascii_file,
-                                          preload=True)
+    curry8_bdf_ascii = read_raw_curry(curry7_bdf_ascii_file,
+                                      preload=True)
 
     assert_allclose([curry7_bdf.n_times, curry7_bdf_ascii.n_times,
                      curry8_bdf.n_times, curry8_bdf_ascii.n_times],
@@ -86,22 +92,17 @@ def test_read_raw_curry():
     # test all types of data
     picks = ["C3", "C4"]
     start, stop = 200, 800
-    with pytest.warns(RuntimeWarning, match="take longer for ASCII"):
-        for curry_file in [curry7_bdf, curry7_bdf_ascii,
-                           curry8_bdf, curry8_bdf_ascii]:
-            # use chans [:3] here since curry stim chan was already extracted
-            assert_allclose([ch["kind"] for ch in curry_file.info["chs"]],
-                            [ch["kind"] for ch in bdf.info["chs"][:3]])
-            assert_array_equal([ch["ch_name"] for ch in
-                                curry_file.info["chs"]],
-                               [ch["ch_name"] for ch in
-                                bdf.info["chs"][:3]])
-            assert_allclose(curry_file.get_data(), bdf.get_data()[:3],
-                            atol=1e-6)
-            # can't use bdf.get_data(picks) here, since it seems bugged
-            assert_allclose(curry_file.get_data(picks, start, stop),
-                            bdf.get_data(start=start, stop=stop)[:2],
-                            atol=1e-6)
+    for curry_file in [curry7_bdf, curry7_bdf_ascii,
+                       curry8_bdf, curry8_bdf_ascii]:
+        # use chans [:3] here since curry stim chan was already extracted
+        assert_allclose([ch["kind"] for ch in curry_file.info["chs"]],
+                        [ch["kind"] for ch in bdf.info["chs"][:3]])
+        assert_array_equal([ch["ch_name"] for ch in curry_file.info["chs"]],
+                           [ch["ch_name"] for ch in bdf.info["chs"][:3]])
+        assert_allclose(curry_file.get_data(), bdf.get_data()[:3], atol=1e-6)
+        # can't use bdf.get_data(picks) here, since it seems bugged
+        assert_allclose(curry_file.get_data(picks, start, stop),
+                        bdf.get_data(start=start, stop=stop)[:2], atol=1e-6)
 
 
 @testing.requires_testing_data
