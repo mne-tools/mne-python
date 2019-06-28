62ec7b5577dafa6a8fd96ad920c0befb740e6400
Guillaume Favelier <guillaume.favelier@gmail.com>
2019-06-28 11:39:32 +0200

    Add support for fixed orientation fwd

--- mne/viz/_3d.py
+++ mne/viz/_3d.py
@@ -712,7 +712,10 @@ def plot_alignment(info=None, trans=None, subject=None, subjects_dir=None,
     if fwd is not None:
         _validate_type(fwd, [Forward])
         fwd_rr = fwd['source_rr']
-        fwd_nn = fwd['source_nn'].reshape(-1, 3, 3)
+        if fwd['source_ori'] == FIFF.FIFFV_MNE_FIXED_ORI:
+            fwd_nn = fwd['source_nn']
+        else:
+            fwd_nn = fwd['source_nn'].reshape(-1, 3, 3)
     else:
         fwd_rr = fwd_nn = np.empty((0, 3))
 
@@ -1134,14 +1137,23 @@ def plot_alignment(info=None, trans=None, subject=None, subjects_dir=None,
         red = (1.0, 0.0, 0.0)
         green = (0.0, 1.0, 0.0)
         blue = (0.0, 0.0, 1.0)
-        for ori, color in zip((0, 1, 2), (red, green, blue)):
+        if fwd['source_ori'] == FIFF.FIFFV_MNE_FIXED_ORI:
             renderer.quiver3d(fwd_rr[:, 0],
                               fwd_rr[:, 1],
                               fwd_rr[:, 2],
-                              fwd_nn[:, ori, 0],
-                              fwd_nn[:, ori, 1],
-                              fwd_nn[:, ori, 2],
-                              color=color, mode='arrow', scale=1E-3)
+                              fwd_nn[:, 0],
+                              fwd_nn[:, 1],
+                              fwd_nn[:, 2],
+                              color=red, mode='arrow', scale=1E-3)
+        else:
+            for ori, color in zip((0, 1, 2), (red, green, blue)):
+                renderer.quiver3d(fwd_rr[:, 0],
+                                  fwd_rr[:, 1],
+                                  fwd_rr[:, 2],
+                                  fwd_nn[:, ori, 0],
+                                  fwd_nn[:, ori, 1],
+                                  fwd_nn[:, ori, 2],
+                                  color=color, mode='arrow', scale=1E-3)
     renderer.set_camera(azimuth=90, elevation=90,
                         distance=0.6, focalpoint=(0., 0., 0.))
     renderer.show()
