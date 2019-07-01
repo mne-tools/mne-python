9b9d65680e53572b6fe857ec11fd3ac2bb7943b6
Joan Massich <sik@visor.udg.edu>
2019-07-01 16:37:12 +0200

    TST: Add test_sfreq

--- mne/io/curry/tests/test_curry.py
+++ mne/io/curry/tests/test_curry.py
@@ -19,7 +19,7 @@ from mne.io.edf import read_raw_bdf
 from mne.io.bti import read_raw_bti
 from mne.io.curry import read_raw_curry
 from mne.io.curry.curry import _check_missing_files, _read_events_curry
-from mne.utils import check_version
+from mne.utils import check_version, run_tests_if_main
 
 
 data_dir = testing.data_path(download=False)
@@ -62,7 +62,6 @@ def bdf_curry_ref():
     pytest.param(curry8_bdf_file, 1e-7, id='curry 8'),
     pytest.param(curry7_bdf_ascii_file, 1e-4, id='curry 7 ascii'),
     pytest.param(curry8_bdf_ascii_file, 1e-4, id='curry 8 ascii'),
-    pytest.param(test_sfreq_0, 1e-7, id='test sfreq = 0'),
 ])
 @pytest.mark.parametrize('preload', [True, False])
 def test_read_raw_curry(fname, tol, preload, bdf_curry_ref):
@@ -136,3 +135,46 @@ def test_check_missing_files():
     with pytest.raises(FileNotFoundError, match="files cannot be found"):
         _check_missing_files(invalid_fname, 7)
         _check_missing_files(invalid_fname, 8)
+
+
+@pytest.fixture(params=[
+    pytest.param(dict(sfreq=500, time_step=0), id='correct sfreq'),
+    pytest.param(dict(sfreq=0, time_step=2000), id='correct time_step'),
+    pytest.param(dict(sfreq=500, time_step=2000), id='both correct'),
+    pytest.param(dict(sfreq=0, time_step=0), id='both 0',
+                 marks=pytest.mark.raises),
+    pytest.param(dict(sfreq=500, time_step=42), id='mismatch',
+                 marks=pytest.mark.raises),
+])
+def sfreq_testing_data(tmpdir, request):
+    """Generate different sfreq, time_step scenarios to be tested."""
+    sfreq, time_step = request.param['sfreq'], request.param['time_step']
+
+    in_base_name = curry7_bdf_file.strip('dat')
+    out_base_name = str(tmpdir.join('curry.'))
+
+    # create dummy empty files for 'dat' and 'rs3'
+    for fname in [out_base_name + ext for ext in ['dat', 'rs3']]:
+        open(fname, 'a').close()
+
+    # mock the info file based on sfreq and time_step
+    with open(in_base_name + 'dap', 'r') as in_file:
+        with open(out_base_name + 'dap', 'w') as out_file:
+            for line in in_file:
+                if 'SampleFreqHz' in line:
+                    out_file.write(line.replace('500', str(sfreq)))
+                elif 'SampleTimeUsec' in line:
+                    out_file.write(line.replace('2000', str(time_step)))
+                else:
+                    out_file.write(line)
+
+    return out_base_name + 'dat'
+
+
+def test_sfreq(sfreq_testing_data):
+    """Test sfreq and time_step."""
+    raw = read_raw_curry(sfreq_testing_data, preload=False)
+    assert raw.info['sfreq'] == 500
+
+
+run_tests_if_main()
