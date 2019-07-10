5c34fabc009b1275a15d4f804af2de7a77e7b47a
Eric Larson <larson.eric.d@gmail.com>
2019-07-10 16:30:29 -0400

    FIX: Ignore

--- mne/conftest.py
+++ mne/conftest.py
@@ -67,6 +67,7 @@ def pytest_configure(config):
     # This is only necessary until sklearn updates their wheels for NumPy 1.16
     ignore:numpy.ufunc size changed:RuntimeWarning
     ignore:.*mne-realtime.*:DeprecationWarning
+    ignore:.*imp.*:DeprecationWarning
     """  # noqa: E501
     for warning_line in warning_lines.split('\n'):
         warning_line = warning_line.strip()
