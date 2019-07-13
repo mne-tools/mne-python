f38cc0fdb3ac33e91dbaeaf8edbd7cc1d994f81a
Daniel McCloy <dan.mccloy@gmail.com>
2019-07-12 19:00:39 -0800

    add bad channels / interpolation tutorial

---
+++ tutorials/preprocessing/plot_15_handling_bad_channels.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+"""
+.. _tut-bad-channels:
+
+Interpolating bad channels
+==========================
+
+This tutorial covers manual marking of bad channels and reconstructing bad
+channels based on good signals at other sensors.
+
+.. contents:: Page contents
+   :local:
+   :depth: 2
+
+As usual we'll start by importing the modules we need, and loading some example
+data:
+"""
+
+import os
+import mne
+
+sample_data_folder = mne.datasets.sample.data_path()
+sample_data_raw_file = os.path.join(sample_data_folder, 'MEG', 'sample',
+                                    'sample_audvis_raw.fif')
+raw = mne.io.read_raw_fif(sample_data_raw_file, verbose=False)
+
+###############################################################################
+# Marking bad channels
+# ^^^^^^^^^^^^^^^^^^^^
+#
+# Sometimes individual channels malfunction and provide data that is too noisy
+# to be usable. MNE-Python makes it easy to remove bad channels from the
+# analysis stream without actually deleting the data in those channels, by
+# keeping track of the bad channel indices in a list and looking at that list
+# when doing analysis or plotting tasks. The list of bad channels is stored in
+# the `'bads'` field of the :class:`~mne.Info` object that is attached to
+# :class:`~mne.io.Raw`, :class:`~mne.Epochs`, and :class:`~mne.Evoked` objects.
+
+print(raw.info['bads'])
+
+###############################################################################
+# Here you can see that the :file:`.fiff` file we loaded from disk must have
+# been keeping track of channels marked as "bad" — which is good news, because
+# it means any changes we make to the list of bad channels will be preserved
+# if we save our data at intermediate stages and re-load it later. In the case
+# of the example data, you can see why those channels were marked bad by
+# using the standard :meth:`~mne.io.Raw.plot` method; to make it easier to see
+# we'll use the :func:`~mne.pick_channels_regexp` function to narrow down which
+# channels we see:
+
+for pattern in (r'MEG 24..', r'EEG 05.'):
+    picks = mne.pick_channels_regexp(raw.ch_names, regexp=pattern)
+    raw.plot(order=picks, n_channels=len(picks))
+
+###############################################################################
+# Notice first of all that the channels marked as "bad" are plotted in a light
+# gray color in a layer behind the other channels, to make it easy to
+# distinguish them from "good" channels. The plots make it clear that `EEG 053`
+# is not picking up scalp potentials at all, and `MEG 2443` looks like it's
+# picking up a lot *more* than its neighbors — its signal is at least an order
+# of magnitude greater than the other MEG channels. Regardless of what is
+# causing the problems, neither are realistic reflections of the
+# electrophysiological signals we expect, so it makes sense to exclude them.
+#
+# If you want to change which channels are marked as bad, you can edit
+# ``raw.info['bads']`` directly; it's an ordinary Python :class:`list` so the
+# usual list methods will work:
+
+original_bads = raw.info['bads']
+raw.info['bads'].append('EEG 050')               # add a single channel
+raw.info['bads'].extend(['EEG 051', 'EEG 052'])  # add a list of channels
+bad_chan = raw.info['bads'].pop(-1)  # remove the last entry in the list
+raw.info['bads'] = original_bads     # change the whole list at once
+
+###############################################################################
+# .. sidebar:: Blocking execution
+#     If you want to build an interactive bad-channel-marking step into an
+#     analysis script, be sure to include the parameter ``block=True`` in your
+#     call to ``raw.plot()`` or ``epochs.plot()``. This will pause the script
+#     while the plot is open, giving you time to mark bad channels before
+#     subsequent analysis or plotting steps are executed.
+#
+# You can also interactively toggle whether a channel is marked "bad" in the
+# plot windows of ``raw.plot()`` or ``epochs.plot()`` by clicking on the
+# channel name along the vertical axis (in ``raw.plot()`` windows you can also
+# do this by clicking the channel's trace in the plot area). The ``bads`` field
+# gets updated immediately each time you toggle a channel, and will retain its
+# modified state after the plot window is closed.
+#
+# The list of bad channels in the :class:`mne.Info` object's ``bads`` field is
+# automatically taken into account in dozens of functions and methods across
+# the MNE-Python codebase. This is done consistently with a parameter
+# ``exclude='bads'`` in the function or method signature. Typically this
+# ``exclude`` parameter also accepts a list of channel names or indices, so if
+# you want to *include* the bad channels you can do so by passing
+# ``exclude=[]`` (or some other list of channels to exclude).
+#
+#
+# Interpolating bad channels
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# In some cases simply excluding bad channels is sufficient (for example, if
+# you plan only to analyze a specific sensor ROI, and the bad channel is
+# outside that ROI). But it is also possible to reconstruct a bad channel by
+# interpolating its signal based on the signals of the good sensors around it.
+#
+#
+# How interpolation works
+# ~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Interpolation of EEG channels in MNE-Python is done using the spherical
+# spline method [1]_, which projects the sensor locations onto a unit sphere
+# and interpolates the signal at the bad sensor locations based on the signals
+# at the good locations. Mathematical details are presented in
+# :ref:`channel_interpolation`. Interpolation of MEG channels uses the field
+# mapping algorithms used in computing the :ref:`forward solution
+# <tut_forward>`.
+#
+#
+# Interpolation in MNE-Python
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Interpolating bad channels in :class:`~mne.io.Raw` objects is done with the
+# :meth:`~mne.io.Raw.interpolate_bads` method, which automatically applies the
+# correct method (spherical splines or field interpolation) to EEG and MEG
+# channels, respectively (there is a corresponding method
+# :meth:`mne.Epochs.interpolate_bads` that works for :class:`~mne.Epochs`
+# objects). To illustrate how it works, we'll start by cropping the raw object
+# to just three seconds for easier plotting, then create ``interp_raw`` with
+# interpolated channels:
+
+raw.crop(tmin=0, tmax=3).load_data()
+raw_interp = raw.copy().interpolate_bads()
+
+###############################################################################
+# By default, :meth:`~mne.io.Raw.interpolate_bads` will clear out
+# ``raw.info['bads']`` after interpolation, so that the interpolated channels
+# are no longer excluded from subsequent computations. Here, for illustration
+# purposes, we'll prevent that by specifying ``reset_bads=False`` so that when
+# we plot the data before and after interpolation, the affected channels will
+# still plot in red:
+
+eeg_data = raw.copy().pick_types(meg=False, eeg=True, exclude=[])
+eeg_data_interp = eeg_data.copy().interpolate_bads(reset_bads=False)
+
+for data in (eeg_data, eeg_data_interp):
+    data.plot(butterfly=True, color='#00000022', bad_color='r')
+
+###############################################################################
+# Note that we used the ``exclude=[]`` trick in the call to
+# :meth:`~mne.io.Raw.pick_types` to make sure the bad channels were not
+# automatically dropped from the selection. Here is the corresponding example
+# with the interpolated gradiometer channel; since there are more channels
+# we'll use a more transparent gray color this time:
+
+grad_data = raw.copy().pick_types(meg='grad', exclude=[])
+grad_data_interp = grad_data.copy().interpolate_bads(reset_bads=False)
+
+for data in (grad_data, grad_data_interp):
+    data.plot(butterfly=True, color='#00000011', bad_color='r')
+
+###############################################################################
+# References
+# ^^^^^^^^^^
+#
+# .. [1] Perrin, F., Pernier, J., Bertrand, O. and Echallier, JF. (1989).
+#        Spherical splines for scalp potential and current density mapping.
+#        *Electroencephalography Clinical Neurophysiology* 72(2):184-187.
