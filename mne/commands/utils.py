b8a89d4dcfcb00a63fdcd434cb1b9ba69dff3155
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2019-07-15 12:17:35 +0200

    make mne's optParser more adaptable

--- mne/commands/utils.py
+++ mne/commands/utils.py
@@ -1,6 +1,7 @@
-"""Some utility functions for commands (e.g. for cmdline handling)."""
+"""Some utility functions for commands (e.g., for cmdline handling)."""
 
 # Authors: Yaroslav Halchenko <debian@onerussian.com>
+#          Stefan Appelhoff <stefan.appelhoff@mailbox.org>
 #
 # License: BSD (3-clause)
 
@@ -25,6 +26,7 @@ def load_module(name, path):
     -------
     mod : module
         Imported module.
+
     """
     from importlib.util import spec_from_file_location, module_from_spec
     spec = spec_from_file_location(name, path)
@@ -33,8 +35,8 @@ def load_module(name, path):
     return mod
 
 
-def get_optparser(cmdpath, usage=None):
-    """Create OptionParser with cmd specific settings (e.g. prog value)."""
+def get_optparser(cmdpath, usage=None, prog=None, version=None):
+    """Create OptionParser with cmd specific settings (e.g., prog value)."""
     command = os.path.basename(cmdpath)
     if re.match('mne_(.*).py', command):
         command = command[4:-3]
@@ -51,10 +53,18 @@ def get_optparser(cmdpath, usage=None):
         if len(doc_lines) > 1:
             epilog = '\n'.join(doc_lines[1:])
 
+    # Set prog
+    if prog is None:
+        prog = 'mne {}'.format(command)
+
+    # Set version
+    if version is None:
+        version = mne.__version__
+
     # monkey patch OptionParser to not wrap epilog
     OptionParser.format_epilog = lambda self, formatter: self.epilog
-    parser = OptionParser(prog="mne %s" % command,
-                          version=mne.__version__,
+    parser = OptionParser(prog=prog,
+                          version=version,
                           description=description,
                           epilog=epilog, usage=usage)
 
