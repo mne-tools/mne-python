e821eb7a627c1562303417a4ed9fa8fd5e0eb8ca
Daniel McCloy <dan.mccloy@gmail.com>
2019-07-15 18:25:35 -0800

    fix flake

--- tutorials/preprocessing/plot_10_preprocessing_overview.py
+++ tutorials/preprocessing/plot_10_preprocessing_overview.py
@@ -235,6 +235,7 @@ eog_epochs.average().plot_topomap(times=np.linspace(-0.1, 0.1, 9))
 #
 # .. LINKS
 #
-# .. _`AC power line frequency`: https://en.wikipedia.org/wiki/Mains_electricity
+# .. _`AC power line frequency`:
+#    https://en.wikipedia.org/wiki/Mains_electricity
 # .. _`QRS`: https://en.wikipedia.org/wiki/QRS_complex
 # .. _`memory-mapped`: https://en.wikipedia.org/wiki/Memory-mapped_file
