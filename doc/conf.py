7a0580a56471d56d8593ea45b18d3fc9c4366016
Guillaume Favelier <guillaume.favelier@gmail.com>
2019-07-18 15:52:10 +0200

    Revert doc testing

--- doc/conf.py
+++ doc/conf.py
@@ -322,8 +322,6 @@ try:
     # Do not pop up any mayavi windows while running the
     # examples. These are very annoying since they steal the focus.
     mlab.options.offscreen = True
-    import pyvista
-    pyvista.OFF_SCREEN = True
 except Exception:
     scrapers = ('matplotlib',)
     report_scraper = None
@@ -332,7 +330,7 @@ else:
     from traits.api import push_exception_handler
     push_exception_handler(reraise_exceptions=True)
     report_scraper = mne.report._ReportScraper()
-    scrapers = ('matplotlib', 'mayavi', 'pyvista', report_scraper)
+    scrapers = ('matplotlib', 'mayavi', report_scraper)
 
 
 def setup(app):
