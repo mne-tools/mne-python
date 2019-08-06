mne-tools/mne-python

commit d89e3ae132e6e1d49d3bf60f9e831763ec8cb155

FIX: set_annotations when meas_date is prior to 1970 (#6628)
* TST: Add regression test

* fix

* update whatsnew

mne/tests/test_annotations.py
@@ -959,4 +959,16 @@ def test_date_none(tmpdir):
     assert raw_read.info['meas_date'] is None
 
 
+def test_negative_meas_dates():
+    """Test meas_date previous to 1970."""
+    # Regression test for gh-6621
+    raw = RawArray(data=np.empty((1, 1), dtype=np.float64),
+                   info=create_info(ch_names=1, sfreq=1.))
+    raw.info['meas_date'] = (-908196946, 988669)
+    raw.set_annotations(Annotations(description='foo', onset=[0],
+                                    duration=[0], orig_time=None))
+    events, _ = events_from_annotations(raw)
+    assert events[:, 0] == 0
+
+
 run_tests_if_main()

mne/annotations.py
@@ -350,8 +350,8 @@ class Annotations(object):
         if len(self) == 0:
             return  # no annotations, nothing to do
 
-        offset = 0 if self.orig_time is None else self.orig_time
-        absolute_onset = self.onset + offset
+        orig_time_offset = 0 if self.orig_time is None else self.orig_time
+        absolute_onset = self.onset + orig_time_offset
         absolute_offset = absolute_onset + self.duration
 
         tmin = tmin if tmin is not None else absolute_onset.min()
@@ -360,14 +360,14 @@ class Annotations(object):
         if tmin > tmax:
             raise ValueError('tmax should be greater than tmin.')
 
-        if tmin < 0:
+        if tmin < orig_time_offset:
             raise ValueError('tmin should be positive.')
 
         out_of_bounds = (absolute_onset > tmax) | (absolute_offset < tmin)
 
         # clip the left side
         clip_left_elem = (absolute_onset < tmin) & ~out_of_bounds
-        self.onset[clip_left_elem] = tmin - offset
+        self.onset[clip_left_elem] = tmin - orig_time_offset
         diff = tmin - absolute_onset[clip_left_elem]
         self.duration[clip_left_elem] = self.duration[clip_left_elem] - diff
 

commit 937627a1de63a7603ef8724a9883239aefd70c14

TST: replicate Circle error introduced in #6628

mne/tests/test_annotations.py
@@ -971,4 +971,26 @@ def test_negative_meas_dates():
     assert events[:, 0] == 0
 
 
+def test_xxxx():
+    """Test meas_date previous to 1970."""
+    # Regression test for gh-6621
+    raw = RawArray(data=np.empty((376, 36038), dtype=np.float64),
+                   info=create_info(ch_names=376, sfreq=600.614990234375))
+
+    raw.info['meas_date'] = (1038942070, 720100)
+    meas_date = 1038942070.7201
+
+    time_format = '%Y-%m-%d %H:%M:%S.%f'
+    new_orig_time = (datetime
+                     .utcfromtimestamp(meas_date + 50)
+                     .strftime(time_format))
+
+    later_annot = mne.Annotations(onset=[3, 5, 7],
+                                  duration=[1, 0.5, 0.25],
+                                  description=['DDD', 'EEE', 'FFF'],
+                                  orig_time=new_orig_time)
+
+    raw.copy().set_annotations(later_annot)
+
+
 run_tests_if_main()