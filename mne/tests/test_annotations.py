mne-tools/mne-python

commit d89e3ae132e6e1d49d3bf60f9e831763ec8cb155

FIX: set_annotations when meas_date is prior to 1970 (#6628)
* TST: Add regression test

* fix

* update whatsnew

mne/annotations.py
@@ -350,8 +350,8 @@ class Annotations(object):
         if len(self) == 0:
             return  # no annotations, nothing to do
 
-        offset = 0 if self.orig_time is None else self.orig_time
-        absolute_onset = self.onset + offset
+        orig_time_offset = 0 if self.orig_time is None else self.orig_time
+        absolute_onset = self.onset + orig_time_offset
         absolute_offset = absolute_onset + self.duration
 
         tmin = tmin if tmin is not None else absolute_onset.min()
@@ -360,14 +360,14 @@ class Annotations(object):
         if tmin > tmax:
             raise ValueError('tmax should be greater than tmin.')
 
-        if tmin < 0:
+        if tmin < orig_time_offset:
             raise ValueError('tmin should be positive.')
 
         out_of_bounds = (absolute_onset > tmax) | (absolute_offset < tmin)
 
         # clip the left side
         clip_left_elem = (absolute_onset < tmin) & ~out_of_bounds
-        self.onset[clip_left_elem] = tmin - offset
+        self.onset[clip_left_elem] = tmin - orig_time_offset
         diff = tmin - absolute_onset[clip_left_elem]
         self.duration[clip_left_elem] = self.duration[clip_left_elem] - diff
 

mne/tests/test_annotations.py
@@ -959,4 +959,16 @@ def test_date_none(tmpdir):
     assert raw_read.info['meas_date'] is None
 
 
+def test_negative_meas_dates():
+    """Test meas_date previous to 1970."""
+    # Regression test for gh-6621
+    raw = RawArray(data=np.empty((1, 1), dtype=np.float64),
+                   info=create_info(ch_names=1, sfreq=1.))
+    raw.info['meas_date'] = (-908196946, 988669)
+    raw.set_annotations(Annotations(description='foo', onset=[0],
+                                    duration=[0], orig_time=None))
+    events, _ = events_from_annotations(raw)
+    assert events[:, 0] == 0
+
+
 run_tests_if_main()

commit 486db19e7a06e4107d37c82290f22c4a3948d44c

FIX: set_annotation regression introduced in #6628 (#6631)
support negative `tmin` and `tmax` in Annotations.crop

mne/annotations.py
@@ -350,8 +350,8 @@ class Annotations(object):
         if len(self) == 0:
             return  # no annotations, nothing to do
 
-        orig_time_offset = 0 if self.orig_time is None else self.orig_time
-        absolute_onset = self.onset + orig_time_offset
+        offset = 0 if self.orig_time is None else self.orig_time
+        absolute_onset = self.onset + offset
         absolute_offset = absolute_onset + self.duration
 
         tmin = tmin if tmin is not None else absolute_onset.min()
@@ -360,14 +360,11 @@ class Annotations(object):
         if tmin > tmax:
             raise ValueError('tmax should be greater than tmin.')
 
-        if tmin < orig_time_offset:
-            raise ValueError('tmin should be positive.')
-
         out_of_bounds = (absolute_onset > tmax) | (absolute_offset < tmin)
 
         # clip the left side
         clip_left_elem = (absolute_onset < tmin) & ~out_of_bounds
-        self.onset[clip_left_elem] = tmin - orig_time_offset
+        self.onset[clip_left_elem] = tmin - offset
         diff = tmin - absolute_onset[clip_left_elem]
         self.duration[clip_left_elem] = self.duration[clip_left_elem] - diff
 

tutorials/raw/plot_30_annotate_raw.py
@@ -5,8 +5,8 @@
 Annotating continuous data
 ==========================
 
-This tutorial describes adding annotations to a Raw object, and how annotations
-are used in later stages of data processing.
+This tutorial describes adding annotations to a :class:`~mne.io.Raw` object,
+and how annotations are used in later stages of data processing.
 
 .. contents:: Page contents
    :local:

mne/tests/test_annotations.py
@@ -475,7 +475,7 @@ def test_annotations_crop():
                     orig_time=0)
 
     # cropping window larger than annotations --> do not modify
-    a_ = a.copy().crop(tmin=0, tmax=42)
+    a_ = a.copy().crop(tmin=-10, tmax=42)
     assert_array_equal(a_.onset, a.onset)
     assert_array_equal(a_.duration, a.duration)
 
@@ -508,8 +508,6 @@ def test_annotations_crop():
     # test error raising
     with pytest.raises(ValueError, match='tmax should be greater than tmin'):
         a.copy().crop(tmin=42, tmax=0)
-    with pytest.raises(ValueError, match='tmin should be positive'):
-        a.copy().crop(tmin=-10, tmax=0)
 
     # test warnings
     with pytest.warns(RuntimeWarning, match='Omitted .* were outside'):
@@ -969,6 +967,25 @@ def test_negative_meas_dates():
                                     duration=[0], orig_time=None))
     events, _ = events_from_annotations(raw)
     assert events[:, 0] == 0
+
+
+def test_crop_when_negative_orig_time():
+    """Test croping with orig_time, tmin and tmax previous to 1970."""
+    # Regression test for gh-6621
+    orig_time_stamp = -908196945.011331  # 1941-03-22 11:04:14.988669
+    annot = Annotations(description='foo', onset=np.arange(0, 1, 0.1),
+                        duration=[0], orig_time=orig_time_stamp)
+    assert annot.orig_time == orig_time_stamp
+
+    # do not raise
+    annot.crop()
+
+    # Crop with negative tmin, tmax
+    tmin, tmax = [orig_time_stamp + t for t in (0.25, .75)]
+    assert tmin < 0 and tmax < 0
+    crop_annot = annot.crop(tmin=tmin, tmax=tmax)
+    assert_allclose(crop_annot.onset, [0.3, 0.4, 0.5, 0.6, 0.7])
+    assert crop_annot.orig_time == orig_time_stamp  # orig_time does not change
 
 
 run_tests_if_main()