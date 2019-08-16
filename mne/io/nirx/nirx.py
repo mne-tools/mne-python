e8b949226398bda0904bd9a88435b033de955ec4
Robert Luke <hello@robertluke.net>
2019-08-16 16:12:52 +1000

    Add test call to nirx reader. Ensure required files are present

--- mne/io/nirx/nirx.py
+++ mne/io/nirx/nirx.py
@@ -3,8 +3,77 @@
 # License: BSD (3-clause)
 
 import os.path as op
+import glob as glob
+import pandas as pd
 
 from ..base import BaseRaw
 from ..utils import _read_segments_file, _file_size
 from ..meas_info import create_info
 from ...utils import logger, verbose, warn, fill_doc
+
+@fill_doc
+def read_raw_nirx(fname, preload=False, verbose=None):
+    """Reader for a NIRX fNIRS file.
+    Parameters
+    ----------
+    fname : str
+        Path to the NIRX data folder.
+    %(preload)s
+    %(verbose)s
+    Returns
+    -------
+    raw : instance of RawNIRX
+        A Raw object containing NIRX data.
+    See Also
+    --------
+    mne.io.Raw : Documentation of attribute and methods.
+    """
+    return RawNIRX(fname, preload, verbose)
+
+@fill_doc
+class RawNIRX(BaseRaw):
+    """Raw object from a NIRX fNIRS file.
+    Parameters
+    ----------
+    fname : str
+        Path to the NIRX data folder.
+    %(preload)s
+    %(verbose)s
+    See Also
+    --------
+    mne.io.Raw : Documentation of attribute and methods.
+    """
+
+    @verbose
+    def __init__(self, fname, preload=False, verbose=None):
+        logger.info('Loading %s' % fname)
+
+        # Check if required files exist
+
+        file_dat = glob.glob(fname + '/*.dat')
+        file_evt = glob.glob(fname + '/*.evt')
+        file_hdr = glob.glob(fname + '/*.hdr')
+        file_inf = glob.glob(fname + '/*.inf')
+        file_set = glob.glob(fname + '/*.set')
+        file_tpl = glob.glob(fname + '/*.tpl')
+        file_wl1 = glob.glob(fname + '/*.wl1')
+        file_wl2 = glob.glob(fname + '/*.wl2')
+        file_cfg = glob.glob(fname + '/*config.txt')
+        file_mat = glob.glob(fname + '/*probeInfo.mat')
+
+        assert (len(file_dat) == 1), "Should be one dat file"
+        assert (len(file_evt) == 1), "Should be one evt file"
+        assert (len(file_hdr) == 1), "Should be one hdr file"
+        assert (len(file_inf) == 1), "Should be one inf file"
+        assert (len(file_set) == 1), "Should be one set file"
+        assert (len(file_tpl) == 1), "Should be one tpl file"
+        assert (len(file_wl1) == 1), "Should be one wl1 file"
+        assert (len(file_wl2) == 1), "Should be one wl2 file"
+        assert (len(file_cfg) == 1), "Should be one config file"
+        assert (len(file_mat) == 1), "Should be one mat file"
+
+        # Read wavelength data
+
+        wl1 = pd.read_csv(file_wl1[0], sep = ' ' )
+        wl2 = pd.read_csv(file_wl2[0], sep = ' ' )
+        assert (wl1.shape == wl2.shape), "Wavelength files should contain same amount of data"
--- mne/io/nirx/tests/test_nirx.py
+++ mne/io/nirx/tests/test_nirx.py
@@ -1,20 +1,27 @@
 # -*- coding: utf-8 -*-
 # Authors: Robert Luke  <mail@robertluke.net>
 #          simplified BSD-3 license
+
 import os.path as op
 
 from numpy.testing import assert_array_equal
 from scipy import io as sio
 
-from mne.io import read_raw_eximia
+from mne.io import read_raw_nirx
 from mne.io.tests.test_raw import _test_raw_reader
 from mne.utils import run_tests_if_main
 from mne.datasets.testing import data_path, requires_testing_data
+from ....utils import logger, verbose, warn, fill_doc
 
+print("Running NRIX test")
 
 @requires_testing_data
 def test_nirx():
     """Test reading NIRX files."""
+    fname = op.join(data_path(), 'nirx', 'test_nirx.nxe')
+    fname = '/home/rluke/Documents/Repositories/mne-python/mne/io/nirx/tests/nirx_15_2_recording'
+    logger.info('Calling loader on %s' % fname )
+    raw = read_raw_nirx(fname, preload=True)
 
 
 run_tests_if_main()
