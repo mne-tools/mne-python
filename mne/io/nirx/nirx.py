f53db7dc8a0e519562d7e1b09395f75474dd0f5c
Robert Luke <hello@robertluke.net>
2019-08-19 07:49:45 +1000

    Add reader for header file

--- mne/io/nirx/nirx.py
+++ mne/io/nirx/nirx.py
@@ -5,6 +5,8 @@
 import os.path as op
 import glob as glob
 import pandas as pd
+import configparser as cp
+import re as re
 
 from ..base import BaseRaw
 from ..utils import _read_segments_file, _file_size
@@ -77,3 +79,41 @@ class RawNIRX(BaseRaw):
         wl1 = pd.read_csv(file_wl1[0], sep = ' ' )
         wl2 = pd.read_csv(file_wl2[0], sep = ' ' )
         assert (wl1.shape == wl2.shape), "Wavelength files should contain same amount of data"
+
+        # Read demographic information file
+
+        inf = cp.ConfigParser()
+        inf.read(file_inf)
+        print(inf['Subject Demographics']['Age'])
+
+        # Read event/triggers file
+
+
+        # Read header file
+
+        hdr = cp.ConfigParser(allow_no_value=True)
+        hdr.read(file_hdr)
+        #print(hdr['GeneralInfo']['Mod'])
+        #print(hdr['ImagingParameters']['SamplingRate'])
+        #print(hdr['DataStructure']['S-D-Key'])
+        #print(hdr['ChannelsDistance']['ChanDis'])
+
+        # This solution gets 90% of the way there, but fails on the key values
+        # where data has been stored in comments. All the failing fields are listed
+        # below. They all evaluate to #
+        # TODO: Write custom parser for these fields
+        print(hdr['GainSettings']['Gains'])
+        print(hdr['Markers']['Events'])
+        print(hdr['DataStructure']['S-D-Mask'])
+        print(hdr['DarkNoise']['Wavelength1'])
+        print(hdr['DarkNoise']['Wavelength2'])
+
+        # Parse required header fields
+
+        ## Extract requested Source Detector Keys
+
+        sources = [int(s) for s in re.findall('(\d)-\d:\d+', hdr['DataStructure']['S-D-Key'])]
+        detectors = [int(s) for s in  re.findall('\d-(\d):\d+', hdr['DataStructure']['S-D-Key'])]
+        sdindex = [int(s) for s in re.findall('\d-\d:(\d+)', hdr['DataStructure']['S-D-Key'])]
+        assert (len(sources) == len(detectors)), "Same amount of sources and detectors required"
+        assert (len(sources) == len(sdindex)), "Same amount of sources and keys required"
