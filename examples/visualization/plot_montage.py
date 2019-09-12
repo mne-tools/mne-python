7e65fbe9dcdb699a6c6a78229b89462622ea9a65
Joan Massich <sik@visor.udg.edu>
2019-09-12 16:37:55 +0200

    try

--- examples/visualization/plot_montage.py
+++ examples/visualization/plot_montage.py
@@ -16,10 +16,13 @@ shipped in MNE-python, and display it on fsaverage template.
 import os.path as op
 
 import mne
+from mne.channels.montage import get_builtin_montages
 from mne.datasets import fetch_fsaverage
 from mne.viz import plot_alignment, set_3d_view, set_3d_title
 
 subjects_dir = op.dirname(fetch_fsaverage())
+
+sphere = mne.make_sphere_model(r0=(0., 0., 0.), head_radius=0.085)
 
 ###############################################################################
 # check all montages
@@ -41,3 +44,97 @@ for current_montage in get_builtin_montages():
                          )
     set_3d_view(figure=fig, azimuth=135, elevation=80)
     set_3d_title(figure=fig, title=current_montage)
+
+
+
+###############################################################################
+# Show them on the sphere with 'unk' (or whatever they have been read)
+#
+
+for current_montage in get_builtin_montages():
+    montage = mne.channels.make_standard_montage(current_montage)
+    fig=mne.viz.plot_alignment(
+        # Plot options
+        show_axes=True, dig=True, surfaces='inner_skull', bem=sphere,
+
+        # Create dummy info 
+        info=mne.create_info(
+            ch_names=montage.ch_names,
+            sfreq=1,
+            ch_types='eeg',
+            montage=montage,
+        ),
+    )
+    set_3d_view(figure=fig, azimuth=135, elevation=80)
+    set_3d_title(figure=fig, title='{} [unk]'.format(current_montage))
+
+###############################################################################
+# Show them on the sphere with 'head' (only those that need transform)
+#
+
+for current_montage in get_builtin_montages():
+    montage = mne.channels.make_standard_montage(current_montage)
+    if montage._coord_frame == 'head':
+        continue
+    else:
+        fig=mne.viz.plot_alignment(
+            # Plot options
+            show_axes=True, dig=True, surfaces='inner_skull', bem=sphere,
+
+            # Create dummy info 
+            info=mne.create_info(
+                ch_names=montage.ch_names,
+                sfreq=1,
+                ch_types='eeg',
+                montage=montage,
+            ),
+        )
+        set_3d_view(figure=fig, azimuth=135, elevation=80)
+        set_3d_title(figure=fig, title='{} [head]'.format(current_montage))
+
+###############################################################################
+# Show them on the fsaverage with 'unk' (or whatever they have been read)
+#
+
+for current_montage in get_builtin_montages():
+    montage = mne.channels.make_standard_montage(current_montage)
+    fig=mne.viz.plot_alignment(
+        # Plot options
+        show_axes=True, dig=True, surfaces='inner_skull', trans=None,
+        subject='fsaverage', subjects_dir=subjects_dir,
+
+        # Create dummy info 
+        info=mne.create_info(
+            ch_names=montage.ch_names,
+            sfreq=1,
+            ch_types='eeg',
+            montage=montage,
+        ),
+    )
+    set_3d_view(figure=fig, azimuth=135, elevation=80)
+    set_3d_title(figure=fig, title='{} [unk]'.format(current_montage))
+
+###############################################################################
+# Show them on the sphere with 'head' (only those that need transform)
+#
+
+for current_montage in get_builtin_montages():
+    montage = mne.channels.make_standard_montage(current_montage)
+    if montage._coord_frame == 'head':
+        continue
+    else:
+        fig=mne.viz.plot_alignment(
+            # Plot options
+            show_axes=True, dig=True, surfaces='inner_skull', trans=None,
+            subject='fsaverage', subjects_dir=subjects_dir,
+
+            # Create dummy info 
+            info=mne.create_info(
+                ch_names=montage.ch_names,
+                sfreq=1,
+                ch_types='eeg',
+                montage=montage,
+            ),
+        )
+        set_3d_view(figure=fig, azimuth=135, elevation=80)
+        set_3d_title(figure=fig, title='{} [head]'.format(current_montage))
