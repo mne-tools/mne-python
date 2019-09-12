7a6d73d9bb747cdee46df6e7fd8521b06b486c9b
Joan Massich <sik@visor.udg.edu>
2019-09-12 18:10:23 +0200

    UPS: actually display the differences between 'head' and 'unk'

--- examples/visualization/plot_montage.py
+++ examples/visualization/plot_montage.py
@@ -17,6 +17,7 @@ import os.path as op
 
 import mne
 from mne.channels.montage import get_builtin_montages
+from mne.channels.montage import transform_to_head
 from mne.datasets import fetch_fsaverage
 from mne.viz import plot_alignment, set_3d_view, set_3d_title
 
@@ -91,7 +92,7 @@ for current_montage in get_builtin_montages():
                 ch_names=montage.ch_names,
                 sfreq=1,
                 ch_types='eeg',
-                montage=montage,
+                montage=transform_to_head(montage),
             ),
         )
         set_3d_view(figure=fig, azimuth=135, elevation=80)
@@ -120,7 +121,7 @@ for current_montage in get_builtin_montages():
     set_3d_title(figure=fig, title='{} [unk]'.format(current_montage))
 
 ###############################################################################
-# Show them on the sphere with 'head' (only those that need transform)
+# Show them on the fsaverage with 'head' (only those that need transform)
 #
 
 for current_montage in get_builtin_montages():
@@ -138,8 +139,71 @@ for current_montage in get_builtin_montages():
                 ch_names=montage.ch_names,
                 sfreq=1,
                 ch_types='eeg',
-                montage=montage,
+                montage=transform_to_head(montage),
             ),
         )
         set_3d_view(figure=fig, azimuth=135, elevation=80)
         set_3d_title(figure=fig, title='{} [head]'.format(current_montage))
+
+
+
+###############################################################################
+# Plot them side by side  in sphere
+#
+
+for current_montage in get_builtin_montages():
+    montage = mne.channels.make_standard_montage(current_montage)
+    if montage._coord_frame == 'head':
+        continue
+    else:
+        for kk, vv in {
+                'unk': montage,
+                'head': transform_to_head(montage)
+        }.items():
+            fig=mne.viz.plot_alignment(
+                # Plot options
+                show_axes=True, dig=True, surfaces='inner_skull', bem=sphere,
+
+                # Create dummy info
+                info=mne.create_info(
+                    ch_names=montage.ch_names,
+                    sfreq=1,
+                    ch_types='eeg',
+                    montage=vv,
+                ),
+            )
+            set_3d_view(figure=fig, azimuth=135, elevation=80)
+            set_3d_title(
+                figure=fig, title='{} [{}]'.format(current_montage, kk)
+            )
+
+###############################################################################
+# Plot them side by side  in fsaverage
+#
+
+for current_montage in get_builtin_montages():
+    montage = mne.channels.make_standard_montage(current_montage)
+    if montage._coord_frame == 'head':
+        continue
+    else:
+        for kk, vv in {
+                'unk': montage,
+                'head': transform_to_head(montage)
+        }.items():
+            fig=mne.viz.plot_alignment(
+                # Plot options
+                show_axes=True, dig=True, surfaces='inner_skull', trans=None,
+                subject='fsaverage', subjects_dir=subjects_dir,
+
+                # Create dummy info
+                info=mne.create_info(
+                    ch_names=montage.ch_names,
+                    sfreq=1,
+                    ch_types='eeg',
+                    montage=vv,
+                ),
+            )
+            set_3d_view(figure=fig, azimuth=135, elevation=80)
+            set_3d_title(
+                figure=fig, title='{} [{}]'.format(current_montage, kk)
+            )
