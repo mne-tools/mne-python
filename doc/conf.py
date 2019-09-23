61215b17bf1fb56d837273346149bd8518ba0a93
Daniel McCloy <dan.mccloy@gmail.com>
2019-09-23 14:43:39 -0700

    ignore _includes folder when building

--- doc/conf.py
+++ doc/conf.py
@@ -84,6 +84,11 @@ autodoc_default_options = {'inherited-members': None}
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_includes']
+
 # The suffix of source filenames.
 source_suffix = '.rst'
 
