23caa56d887385e11b9de3f8a55a059d6eab77e0
johnsam7 <johnsam@mit.edu>
2019-10-02 11:01:57 -0400

    Adds the CSS method to preprocessing folder with an example in the examples folder.

--- mne/preprocessing/__init__.py
+++ mne/preprocessing/__init__.py
@@ -28,3 +28,5 @@ from .artifact_detection import (annotate_movement, compute_average_dev_head_t,
 from ._regress import regress_artifact
 from ._fine_cal import (compute_fine_calibration,  read_fine_calibration,
                         write_fine_calibration)
+from ._css import filter_cortical
+
