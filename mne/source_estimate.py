Corrected SNR calculation to take unwhitened sensor signal b instead of whitened, updated tutorial file

mne/source_estimate.py
@@ -1098,71 +1098,6 @@ class _BaseSourceEstimate(ToDataFrameMixin, TimeMixin):
 
         return stcs
 
-    @verbose
-    def estimate_snr(self, info, fwd, cov, verbose=None):
-        r"""Compute time-varying SNR in the source space.
-
-        This function should only be used with source estimates with units
-        nanoAmperes (i.e., MNE-like solutions, *not* dSPM or sLORETA).
-
-        Parameters
-        ----------
-        info : instance of io.meas_info.Info
-            The measurement info.
-        fwd : instance of Forward
-            The forward solution used to create the source estimate.
-        cov : instance of Covariance
-            The noise covariance used to estimate the resting cortical
-            activations. Should be an evoked covariance, not empty room.
-
-        Returns
-        -------
-        snr_stc : instance of SourceEstimate
-            The source estimate with the SNR computed.
-
-        Notes
-        -----
-        We define the SNR in decibels for each source location at each
-        time point as:
-
-        .. math::
-
-            {\rm SNR} = 10\log_10[\frac{a^2}{N}\sum_k\frac{b_k^2}{s_k^2}]
-
-        where :math:`\\b_k` is the signal on sensor :math:`k` provided by the
-        forward model for a source with unit amplitude, :math:`a` is the
-        source amplitude, :math:`N` is the number of sensors, and
-        :math:`s_k^2` is the noise variance on sensor :math:`k`.
-               
-        References
-        ----------
-        .. [1] Goldenholz, D. M., Ahlfors, S. P., Hämäläinen, M. S., Sharon, 
-               D., Ishitobi, M., Vaina, L. M., & Stufflebeam, S. M. (2009).
-               Mapping the Signal-To-Noise-Ratios of Cortical Sources in
-               Magnetoencephalography and Electroencephalography.
-               Human Brain Mapping, 30(4), 1077–1086. doi:10.1002/hbm.20571
-        """
-        from .forward import convert_forward_solution
-        from .minimum_norm.inverse import _prepare_forward
-        
-        fwd = convert_forward_solution(fwd, surf_ori=True, force_fixed=True)
-        
-        # G is gain matrix [ch x src], cov is noise covariance [ch x ch]
-        _, _, G, _, _, _, _, cov, _ = _prepare_forward(fwd, info, cov,
-                                fixed=True, loose=0, rank=None, pca=False,
-                                use_cps=True, exp=None, limit_depth_chs=False,
-                                combine_xyz='fro', allow_fixed_depth=True,
-                                limit=None)
-
-        n_channels = cov['dim'] # number of sensors/channels    
-        b_k2 = (G * G).T
-        s_k2 = np.diag(cov['data'])                
-        scaling = (1 / n_channels) * np.sum(b_k2 / s_k2, axis=1)[:, np.newaxis]  
-        snr_stc = SourceEstimate(10 * np.log10((self.data * self.data) * scaling), 
-                                 self.vertices, self.tmin, self.tstep, self.subject)
-                        
-        return snr_stc
-
 
 def _center_of_mass(vertices, values, hemi, surf, subject, subjects_dir,
                     restrict_vertices):
@@ -1570,6 +1505,81 @@ class SourceEstimate(_BaseSurfaceSourceEstimate):
 
         return label_tc
 
+
+    @verbose
+    def estimate_snr(self, info, fwd, cov, verbose=None):
+        r"""Compute time-varying SNR in the source space.
+
+        This function should only be used with source estimates with units
+        nanoAmperes (i.e., MNE-like solutions, *not* dSPM or sLORETA).
+
+        .. warning:: This function currently only works properly for fixed
+                     orientation...
+                     
+        Parameters
+        ----------
+        info : instance of io.meas_info.Info
+            The measurement info.
+        fwd : instance of Forward
+            The forward solution used to create the source estimate.
+        cov : instance of Covariance
+            The noise covariance used to estimate the resting cortical
+            activations. Should be an evoked covariance, not empty room.
+
+        Returns
+        -------
+        snr_stc : instance of SourceEstimate
+            The source estimate with the SNR computed.
+
+        Notes
+        -----
+        We define the SNR in decibels for each source location at each
+        time point as:
+
+        .. math::
+
+            {\rm SNR} = 10\log_10[\frac{a^2}{N}\sum_k\frac{b_k^2}{s_k^2}]
+
+        where :math:`\\b_k` is the signal on sensor :math:`k` provided by the
+        forward model for a source with unit amplitude, :math:`a` is the
+        source amplitude, :math:`N` is the number of sensors, and
+        :math:`s_k^2` is the noise variance on sensor :math:`k`.
+               
+        References
+        ----------
+        .. [1] Goldenholz, D. M., Ahlfors, S. P., Hämäläinen, M. S., Sharon, 
+               D., Ishitobi, M., Vaina, L. M., & Stufflebeam, S. M. (2009).
+               Mapping the Signal-To-Noise-Ratios of Cortical Sources in
+               Magnetoencephalography and Electroencephalography.
+               Human Brain Mapping, 30(4), 1077–1086. doi:10.1002/hbm.20571
+        """
+        from .forward import convert_forward_solution
+        from .minimum_norm.inverse import _prepare_forward
+        
+        if (self.data >= 0).all():
+            warn_('This STC appears to be from free orientation, currently SNR '
+                 'function is valid only for fixed orientation')
+            
+        fwd = convert_forward_solution(fwd, surf_ori=True, force_fixed=True)
+        
+        # G is gain matrix [ch x src], cov is noise covariance [ch x ch]
+        G, _, _, _, _, _, _, cov, _ = _prepare_forward(fwd, info, cov,
+                                fixed=True, loose=0, rank=None, pca=False,
+                                use_cps=True, exp=None, limit_depth_chs=False,
+                                combine_xyz='fro', allow_fixed_depth=True,
+                                limit=None)
+        G = G['sol']['data']
+        n_channels = cov['dim'] # number of sensors/channels  
+        print('Number of channels in calculating SNR: %d' % n_channels)
+        b_k2 = (G * G).T
+        s_k2 = np.diag(cov['data'])                
+        scaling = (1 / n_channels) * np.sum(b_k2 / s_k2, axis=1, keepdims=True)
+        snr_stc = SourceEstimate(10 * np.log10((self.data * self.data) * scaling), 
+                                 self.vertices, self.tmin, self.tstep, self.subject)
+                        
+        return snr_stc
+
+
     def get_peak(self, hemi=None, tmin=None, tmax=None, mode='abs',
                  vert_as_index=False, time_as_index=False):
         """Get location and latency of peak amplitude.
@@ -1701,6 +1711,7 @@ class SourceEstimate(_BaseSurfaceSourceEstimate):
         t_ind = np.sum(masses * np.arange(self.shape[1])) / np.sum(masses)
         t = self.tmin + self.tstep * t_ind
         return vertex, hemi, t
+
 
 
 class _BaseVectorSourceEstimate(_BaseSourceEstimate):

tutorials/source-modeling/plot_source_space_SNR.py
@@ -7,6 +7,13 @@ Computing source space SNR
 ===============================
 
 This example shows how to compute and plot source space SNR.
+
+Reference:
+Goldenholz, D. M., Ahlfors, S. P., Hämäläinen, M. S., Sharon, D., Ishitobi, M.,
+Vaina, L. M., & Stufflebeam, S. M. (2009). Mapping the Signal-To-Noise-Ratios
+of Cortical Sources in Magnetoencephalography and Electroencephalography.
+Human Brain Mapping, 30(4), 1077–1086. doi:10.1002/hbm.20571
+
 """
 # Author: Padma Sundaram <tottochan@gmail.com>
 #         Kaisu Lankinen <klankinen@mgh.harvard.edu>
@@ -15,7 +22,9 @@ This example shows how to compute and plot source space SNR.
 
 import mne
 from mne.datasets import sample
-from mne.minimum_norm import make_inverse_operator, apply_inverse
+from mne.minimum_norm import make_inverse_operator, apply_inverse, read_inverse_operator
+import numpy as np
+import matplotlib.pyplot as plt
 
 print(__doc__)
 
@@ -32,24 +41,90 @@ fwd = mne.read_forward_solution(fname_fwd)
 cov = mne.read_cov(fname_cov)
 
 ###############################################################################
-# Fixed orientation
-# -----------------
-# First let's create a fixed-orientation inverse, with the default weighting.
+# MEG-EEG:
 
-inv = make_inverse_operator(evoked.info, fwd, cov, loose=0., depth=0.8,
-                            verbose=True)
+# Read inverse operator:
+inv_op = make_inverse_operator(evoked.info, fwd, cov, fixed=True, verbose=True)
 
-###############################################################################
-# Let's look at the current estimates using MNE. We'll take the absolute
-# value of the source estimates to simplify the visualization.
-
+# Calculate MNE:
 snr = 3.0
 lambda2 = 1.0 / snr ** 2
+stc = apply_inverse(evoked, inv_op, lambda2, 'MNE', verbose=True)
 
-stc = apply_inverse(evoked, inv, lambda2, 'MNE', verbose=True)
+# Calculate SNR in source space:
 snr_stc = stc.estimate_snr(evoked.info, fwd, cov)
 
-kwargs = dict(initial_time=0.29, hemi='both', subjects_dir=subjects_dir,
-              size=(600, 600), colormap='viridis',
-              clim=dict(kind='value', lims=(-20, 0, 20)))
+# Plot an average SNR across source points over time:
+ave = np.mean(snr_stc.data, axis=0)
+
+fig, ax = plt.subplots()
+ax.plot(evoked.times, ave)
+ax.set(xlabel='Time (sec)', ylabel='SNR MEG-EEG')
+fig.tight_layout()
+
+# Find time point of maximum SNR:
+maxidx = np.argmax(ave)
+
+# Plot SNR on source space at the time point of maximum SNR:
+kwargs = dict(initial_time=evoked.times[maxidx], hemi='both',
+              subjects_dir=subjects_dir, size=(600, 600), colormap='viridis',
+              clim=dict(kind='value', lims=(-100, -75, -50)))
 snr_stc.plot(figure=1, **kwargs)
+
+###############################################################################
+# EEG:
+
+evoked_eeg = evoked.copy().pick_types(eeg=True, meg=False)
+
+# Make inverse oparator:
+inv_op_eeg = make_inverse_operator(evoked_eeg.info, fwd, cov, fixed=True,
+                                   verbose=True)
+
+# Calculate MNE:
+stc_eeg = apply_inverse(evoked_eeg, inv_op_eeg, lambda2, 'MNE', verbose=True)
+
+# Calculate SNR in source space:
+snr_stc_eeg = stc_eeg.estimate_snr(evoked_eeg.info, fwd, cov)
+
+# Plot an average SNR across source points over time:
+ave_eeg = np.mean(snr_stc_eeg.data, axis=0)
+fig, ax = plt.subplots()
+ax.plot(evoked_eeg.times, ave_eeg)
+ax.set(xlabel='Time (sec)', ylabel='SNR EEG')
+fig.tight_layout()
+
+# Plot SNR on source space at the time point of maximum SNR (same time point as
+# for MEG-EEG)
+kwargs = dict(initial_time=evoked.times[maxidx], hemi='both',
+              subjects_dir=subjects_dir, size=(600, 600), colormap='viridis',
+              clim=dict(kind='value', lims=(-100, -75, -50)))
+snr_stc_eeg.plot(figure=2, **kwargs)
+
+###############################################################################
+# MEG:
+
+evoked_meg = evoked.copy().pick_types(eeg=False, meg=True)
+
+# Make inverse oparator:
+inv_op_meg = make_inverse_operator(evoked_meg.info, fwd, cov, fixed=True,
+                                   verbose=True)
+
+# Calculate MNE:
+stc_meg = apply_inverse(evoked_meg, inv_op_meg, lambda2, 'MNE', verbose=True)
+
+# Calculate SNR in source space:
+snr_stc_meg = stc_meg.estimate_snr(evoked_meg.info, fwd, cov)
+
+# Plot an average SNR across source points over time:
+ave_meg = np.mean(snr_stc_meg.data, axis=0)
+fig, ax = plt.subplots()
+ax.plot(evoked_meg.times, ave_meg)
+ax.set(xlabel='Time (sec)', ylabel='SNR MEG')
+fig.tight_layout()
+
+# Plot SNR on source space at the time point of maximum SNR (same time point as
+# for MEG-EEG)
+kwargs = dict(initial_time=evoked.times[maxidx], hemi='both',
+              subjects_dir=subjects_dir, size=(600, 600), colormap='viridis',
+              clim=dict(kind='value', lims=(-100, -75, -50)))
+snr_stc_meg.plot(figure=3, **kwargs)