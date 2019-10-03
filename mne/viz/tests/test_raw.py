df7e6ea451f4c7ee404fd105ec71612a64c15f94
Daniel McCloy <dan.mccloy@gmail.com>
2019-10-02 18:03:23 -0700

    fix long line

--- mne/viz/tests/test_raw.py
+++ mne/viz/tests/test_raw.py
@@ -412,7 +412,7 @@ def test_plot_raw_psd():
     raw.plot_psd(reject_by_annotation=False)
 
     # test fmax value checking
-    with pytest.raises(ValueError, match='must not exceed one half the sampling'):
+    with pytest.raises(ValueError, match='not exceed one half the sampling'):
         raw.plot_psd(fmax=50000)
 
     # gh-5046
