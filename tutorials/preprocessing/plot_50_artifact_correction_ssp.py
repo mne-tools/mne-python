c25e021f10c1f49e0f9d8fb7eafac4055d26f52e
Daniel McCloy <dan.mccloy@gmail.com>
2019-10-03 09:15:38 -0700

    touch a couple tutorials to test plotting

--- tutorials/preprocessing/plot_50_artifact_correction_ssp.py
+++ tutorials/preprocessing/plot_50_artifact_correction_ssp.py
@@ -5,8 +5,8 @@
 Repairing artifacts with SSP
 ============================
 
-This tutorial covers the basics of signal-space projection (SSP) and
-shows how SSP can be used for artifact repair; extended examples illustrate use
+This tutorial covers the basics of signal-space projection (SSP) and shows
+how SSP can be used for artifact repair; extended examples illustrate use
 of SSP for environmental noise reduction, and for repair of ocular and
 heartbeat artifacts.
 
