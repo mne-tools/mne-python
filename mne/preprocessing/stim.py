f9e8ee6fa77fa0a0d2e0e27b30dc578f72bae1a1
fmamashli <33672431+fmamashli@users.noreply.github.com>
2019-10-07 15:45:22 -0400

    Update mne/preprocessing/stim.py
    Co-Authored-By: Mainak Jas <jasmainak@users.noreply.github.com>

--- mne/preprocessing/stim.py
+++ mne/preprocessing/stim.py
@@ -1,6 +1,7 @@
 # Authors: Daniel Strohmeier <daniel.strohmeier@tu-ilmenau.de>
 #          Fahimeh Mamashli <fmamashli@mgh.harvard.edu>
 #          Padma Sundaram <tottochan@gmail.com>
+#
 # License: BSD (3-clause)
 
 import numpy as np
