44641a9e790426b93cb30473f9136c0b1092f230
Mainak Jas <mainakjas@gmail.com>
2019-10-21 16:07:44 -0400

    DOC: clarify docstring for stc object

--- mne/source_estimate.py
+++ mne/source_estimate.py
@@ -1350,12 +1350,18 @@ class SourceEstimate(_BaseSurfaceSourceEstimate):
     Parameters
     ----------
     data : array of shape (n_dipoles, n_times) | tuple, shape (2,)
-        The data in source space. The data can either be a single array or
-        a tuple with two arrays: "kernel" shape (n_vertices, n_sensors) and
-        "sens_data" shape (n_sensors, n_times). In this case, the source
-        space data corresponds to "numpy.dot(kernel, sens_data)".
+        The data in source space. When it is a single array, the
+        left hemisphere is stored in data[:vertices[0]] and the right
+        hemisphere is stored in data[:-vertices[1]].
+        When data is a tuple, it contains two arrays:
+            "kernel" shape (n_vertices, n_sensors) and
+            "sens_data" shape (n_sensors, n_times).
+        In this case, the source space data corresponds to
+        "numpy.dot(kernel, sens_data)".
     vertices : list of shape (2,)
-        Vertex numbers corresponding to the data.
+        Vertex numbers corresponding to the data. The first element of the list
+        contains vertices of left hemisphere and the second element contains
+        vertices of right hemisphere.
     tmin : scalar
         Time point of the first sample in data.
     tstep : scalar
