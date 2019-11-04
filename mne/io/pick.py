daa162cf8b1ba0c7439eab2f4aefc5b298ee3415
Eric Larson <larson.eric.d@gmail.com>
2019-11-04 10:02:20 -0500

    DOC: Tweak

--- mne/io/pick.py
+++ mne/io/pick.py
@@ -138,7 +138,7 @@ def channel_type(info, idx):
 def pick_channels(ch_names, include, exclude=[], ordered=False):
     """Pick channels by names.
 
-    Returns the indices of the channels in ch_names and not in "info['bads']".
+    Returns the indices of ``ch_names`` in ``include`` but not in ``exclude``.
 
     Parameters
     ----------
