011966025a57b5f24f929d4b869674e775ec90a2
Eric Larson <larson.eric.d@gmail.com>
2019-11-09 13:57:06 -0500

    FIX: Older sklearn

--- mne/fixes.py
+++ mne/fixes.py
@@ -652,6 +652,16 @@ class BaseEstimator(object):
     # ``if type(self).__module__.startswith('sklearn.')``.
 
 
+# newer sklearn deprecates importing from sklearn.metrics.scoring,
+# but older sklearn does not expose check_scoring in sklearn.metrics.
+def _get_check_scoring():
+    try:
+        from sklearn.metrics import check_scoring  # noqa
+    except ImportError:
+        from sklearn.metrics.scoring import check_scoring  # noqa
+    return check_scoring
+
+
 ###############################################################################
 # Copied from sklearn to simplify code paths
 
