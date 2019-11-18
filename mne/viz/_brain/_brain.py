Fix scaling by adding rng parameter

mne/viz/_brain/_brain.py
@@ -466,7 +466,7 @@ class _Brain(object):
 
                 if scale_factor is None:
                     width = np.ptp(self.geo[hemi].coords[:, 1])
-                    final_scale_factor = width * 0.1
+                    final_scale_factor = width * 0.05
                 else:
                     if self._units == 'm':
                         scale_factor = scale_factor / 1000.
@@ -480,6 +480,7 @@ class _Brain(object):
                     colormap=ctable,
                     vmin=fmin,
                     vmax=fmax,
+                    scale_mode='scalar',
                     scalars=vector_values,
                     scale=final_scale_factor,
                     opacity=vector_alpha