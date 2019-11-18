fa989b4c6b059aae5539319829d6d4530045b9ce
Eric Larson <larson.eric.d@gmail.com>
2019-11-18 13:52:39 -0500

    FIX: Units [circle full]

--- mne/simulation/tests/test_raw.py
+++ mne/simulation/tests/test_raw.py
@@ -64,9 +64,10 @@ def test_iterable():
     """Test iterable support for simulate_raw."""
     raw = read_raw_fif(raw_fname_short).load_data()
     raw.pick_channels(raw.ch_names[:10] + ['STI 014'])
-    src = setup_volume_source_space(
-        pos=dict(rr=[[-0.05, 0, 0], [0.1, 0, 0]],
-                 nn=[[0, 1., 0], [0, 1., 0]]))
+    with pytest.deprecated_call(match='units'):
+        src = setup_volume_source_space(
+            pos=dict(rr=[[-0.05, 0, 0], [0.1, 0, 0]],
+                    nn=[[0, 1., 0], [0, 1., 0]]))
     assert src.kind == 'discrete'
     trans = None
     sphere = make_sphere_model(head_radius=None, info=raw.info)
@@ -437,8 +438,9 @@ def test_simulate_raw_chpi():
     raw.info.normalize_proj()
     sphere = make_sphere_model('auto', 'auto', raw.info)
     # make sparse spherical source space
-    sphere_vol = tuple(sphere['r0'] * 1000.) + (sphere.radius * 1000.,)
-    src = setup_volume_source_space(sphere=sphere_vol, pos=70.)
+    sphere_vol = tuple(sphere['r0']) + (sphere.radius,)
+    src = setup_volume_source_space(sphere=sphere_vol, pos=70.,
+                                    sphere_units='m')
     stcs = [_make_stc(raw, src)] * 15
     # simulate data with cHPI on
     raw_sim = simulate_raw(raw.info, stcs, None, src, sphere,
