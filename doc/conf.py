c95b3827d11fb660a8a3da493419ae0efa9fc078
olafhauk <olaf.hauk@mrc-cbu.cam.ac.uk>
2019-11-28 14:08:02 +0000

    FIX: fixed error in conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -600,7 +600,7 @@ numpydoc_xref_ignore = {
     'nd_features', 'n_classes', 'n_targets', 'n_slices', 'n_hpi', 'n_fids',
     'n_elp', 'n_pts', 'n_tris', 'n_nodes', 'n_nonzero', 'n_events_out',
     'n_segments', 'n_orient_inv', 'n_orient_fwd', 'n_orient', 'n_dipoles_lcmv',
-    'n_dipoles_fwd'
+    'n_dipoles_fwd',
 
     # Undocumented (on purpose)
     'RawKIT', 'RawEximia', 'RawEGI', 'RawEEGLAB', 'RawEDF', 'RawCTF', 'RawBTi',
