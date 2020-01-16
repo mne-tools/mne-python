0329678887dce1bf29e9ad26382f0204a3116e6e
Clemens Brunner <clemens.brunner@gmail.com>
2020-01-16 19:42:49 +0100

    Add test for BDF file with multiple annotation channels

--- mne/datasets/utils.py
+++ mne/datasets/utils.py
@@ -239,7 +239,7 @@ def _data_path(path=None, force_update=False, update_path=True, download=True,
     path = _get_path(path, key, name)
     # To update the testing or misc dataset, push commits, then make a new
     # release on GitHub. Then update the "releases" variable:
-    releases = dict(testing='0.77', misc='0.5')
+    releases = dict(testing='0.78', misc='0.5')
     # And also update the "md5_hashes['testing']" variable below.
 
     # To update any other dataset, update the data archive itself (upload
@@ -323,7 +323,7 @@ def _data_path(path=None, force_update=False, update_path=True, download=True,
         sample='12b75d1cb7df9dfb4ad73ed82f61094f',
         somato='f08f17924e23c57a751b3bed4a05fe02',
         spm='9f43f67150e3b694b523a21eb929ea75',
-        testing='e825eec08cfd75e85f33ed2b06fc82f1',
+        testing='b9a96e774cc6593d0125017ea35b4898',
         multimodal='26ec847ae9ab80f58f204d09e2c08367',
         fnirs_motor='c4935d19ddab35422a69f3326a01fef8',
         opm='370ad1dcfd5c47e029e692c85358a374',
