0349aeefbd1852b957ee03a1308a828f29029d6d
Adam Li <adam2392@gmail.com>
2020-02-05 22:54:49 -0500

    Fixing annotations bug.

--- mne/tests/test_annotations.py
+++ mne/tests/test_annotations.py
@@ -101,6 +101,15 @@ def test_basics():
     assert_array_equal(raw.annotations.description, np.repeat('test', 10))
 
 
+def test_annot_sanitizing(tmpdir):
+    """Test description sanitizing."""
+    annot = Annotations([0], [1], ['a;:b'])
+    fname = str(tmpdir.join('custom-annot.fif'))
+    annot.save(fname)
+    annot_read = read_annotations(fname)
+    _assert_annotations_equal(annot, annot_read)
+
+
 def test_raw_array_orig_times():
     """Test combining with RawArray and orig_times."""
     data = np.random.randn(2, 1000) * 10e-12
