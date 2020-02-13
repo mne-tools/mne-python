MRG, ENH/FIX: Make cHPI calculation code public (#7290)
* ENH: Expose calculate_head_pos_chpi

* FIX: Many fixes to move toward MF equiv

* FIX: Dist limit

* ENH: Speed up by skipping more

* MAINT: Unify computations

* FIX: Revert analytical solution, use COBYLA

* MAINT: Simplifications

* ENH: Add interference suppression

* FIX: Two-pass simplification

* ENH: Better ProgressBar

* FIX: Refactor after rebase

* API: Refactor into two stages

* API: Cleaner return and API

* ENH: Add guesses

* ENH: Rename

* ENH: Tutorials

* DOC: Tutorial

* FIX: Test

* DOC: Link

* FIX: Unicode

* FIX: Try another

* FIX: Dep

* API: Refactor more clean naming

* DOC: Returns

* DOC: Clearer [ci skip]

* MAINT: Revert progressbar changes

* FIX: But fix ProgressBar docstrings

* WIP: Refactor but its much slower

* FIX: Separate out amplitude step

* DOC: Correct links

* DOC: One more

* FIX: Restore amplitude

* ENH: Add coil moments

mne/dipole.py
@@ -728,7 +728,8 @@ def _dipole_forwards(fwd_data, whitener, rr, n_jobs=1):
     return B, B_orig, scales
 
 
-def _make_guesses(surf, grid, exclude, mindist, n_jobs):
+@verbose
+def _make_guesses(surf, grid, exclude, mindist, n_jobs=1, verbose=None):
     """Make a guess space inside a sphere or BEM surface."""
     if 'rr' in surf:
         logger.info('Guess surface (%s) is in %s coordinates'
@@ -792,16 +793,17 @@ def _fit_Q(fwd_data, whitener, B, B2, B_orig, rd, ori=None):
         uu, sing, vv = fwd_svd
         gof, one = _dipole_gof(uu, sing, vv, B, B2)
         ncomp = len(one)
-        # Counteract the effect of column normalization
-        Q = scales[0] * np.sum(uu.T[:ncomp] *
-                               (one / sing[:ncomp])[:, np.newaxis], axis=0)
+        one /= sing[:ncomp]
+        Q = np.dot(one, uu.T[:ncomp])
     else:
         fwd = np.dot(ori[np.newaxis], fwd)
         sing = np.linalg.norm(fwd)
         one = np.dot(fwd / sing, B)
         gof = (one * one)[0] / B2
-        Q = ori * (scales[0] * np.sum(one / sing))
+        Q = ori * np.sum(one / sing)
         ncomp = 3
+    # Counteract the effect of column normalization
+    Q *= scales[0]
     B_residual_noproj = B_orig - np.dot(fwd_orig.T, Q)
     return Q, gof, B_residual_noproj, ncomp
 

mne/preprocessing/maxwell.py
@@ -16,11 +16,11 @@ from scipy import linalg
 from .. import __version__
 from ..annotations import _annotations_starts_stops
 from ..bem import _check_origin
-from ..chpi import quat_to_rot, rot_to_quat
 from ..transforms import (_str_to_frame, _get_trans, Transform, apply_trans,
                           _find_vector_rotation, _cart_to_sph, _get_n_moments,
                           _sph_to_cart_partials, _deg_ord_idx, _average_quats,
-                          _sh_complex_to_real, _sh_real_to_complex, _sh_negate)
+                          _sh_complex_to_real, _sh_real_to_complex, _sh_negate,
+                          quat_to_rot, rot_to_quat)
 from ..forward import _concatenate_coils, _prep_meg_channels, _create_meg_coils
 from ..surface import _normalize_vectors
 from ..io.constants import FIFF, FWD
@@ -31,7 +31,7 @@ from ..io import _loc_to_coil_trans, _coil_trans_to_loc, BaseRaw
 from ..io.pick import pick_types, pick_info
 from ..utils import (verbose, logger, _clean_names, warn, _time_mask, _pl,
                      _check_option)
-from ..fixes import _get_args, _safe_svd, einsum
+from ..fixes import _get_args, _safe_svd, einsum, bincount
 from ..channels.channels import _get_T1T2_mag_inds
 
 
@@ -224,7 +224,7 @@ def maxwell_filter(raw, origin='auto', int_order=8, ext_order=3,
        +-----------------------------------------------------------------------------+-----+-----------+
        | Automatic bad channel detection                                             |     | ✓         |
        +-----------------------------------------------------------------------------+-----+-----------+
-       | Head position estimation                                                    |     | ✓         |
+       | Head position estimation (:func:`~mne.chpi.compute_head_pos`)               | ✓   | ✓         |
        +-----------------------------------------------------------------------------+-----+-----------+
 
     Epoch-based movement compensation is described in [1]_.
@@ -650,7 +650,8 @@ def _check_destination(destination, info, head_frame):
     return recon_trans
 
 
-def _prep_mf_coils(info, ignore_ref=True):
+@verbose
+def _prep_mf_coils(info, ignore_ref=True, verbose=None):
     """Get all coil integration information loaded and sorted."""
     coils, comp_coils = _prep_meg_channels(
         info, accurate=True, head_frame=False,
@@ -913,7 +914,8 @@ def _regularize(regularize, exp, S_decomp, mag_or_fine, t, verbose=None):
     return S_decomp, pS_decomp, sing, reg_moments, n_use_in
 
 
-def _get_mf_picks(info, int_order, ext_order, ignore_ref=False):
+@verbose
+def _get_mf_picks(info, int_order, ext_order, ignore_ref=False, verbose=None):
     """Pick types for Maxwell filtering."""
     # Check for T1/T2 mag types
     mag_inds_T1T2 = _get_T1T2_mag_inds(info)
@@ -1172,11 +1174,7 @@ def _sss_basis(exp, all_coils):
     rmags, cosmags, bins, n_coils = all_coils[:4]
     int_order, ext_order = exp['int_order'], exp['ext_order']
     n_in, n_out = _get_n_moments([int_order, ext_order])
-    S_tot = np.empty((n_coils, n_in + n_out), np.float64)
-
     rmags = rmags - exp['origin']
-    S_in = S_tot[:, :n_in]
-    S_out = S_tot[:, n_in:]
 
     # do the heavy lifting
     max_order = max(int_order, ext_order)
@@ -1187,16 +1185,20 @@ def _sss_basis(exp, all_coils):
     cos_pol = rmags[:, 2] / r_n  # cos(theta); theta 0...pi
     sin_pol = np.sqrt(1. - cos_pol * cos_pol)  # sin(theta)
     z_only = (r_xy <= 1e-16)
+    sin_pol_nz = sin_pol.copy()
+    sin_pol_nz[z_only] = 1.  # will be overwritten later
     r_xy[z_only] = 1.
     cos_az = rmags[:, 0] / r_xy  # cos(phi)
     cos_az[z_only] = 1.
     sin_az = rmags[:, 1] / r_xy  # sin(phi)
     sin_az[z_only] = 0.
-    del rmags
     # Appropriate vector spherical harmonics terms
     #  JNE 2012-02-08: modified alm -> 2*alm, blm -> -2*blm
     r_nn2 = r_n.copy()
     r_nn1 = 1.0 / (r_n * r_n)
+    S_tot = np.empty((n_coils, n_in + n_out), np.float64)
+    S_in = S_tot[:, :n_in]
+    S_out = S_tot[:, n_in:]
     for degree in range(max_order + 1):
         if degree <= ext_order:
             r_nn1 *= r_n  # r^(l-1)
@@ -1239,7 +1241,7 @@ def _sss_basis(exp, all_coils):
             # alpha
             if degree <= int_order:
                 b_r = (degree + 1) * r_fact / r_nn2
-                b_az = az_fact / (sin_pol * r_nn2)
+                b_az = az_fact / (sin_pol_nz * r_nn2)
                 b_az[z_only] = 0.
                 b_pol = pol_fact / (2 * r_nn2)
                 S_in[:, idx] = _integrate_points(
@@ -1248,7 +1250,7 @@ def _sss_basis(exp, all_coils):
             # beta
             if degree <= ext_order:
                 b_r = -degree * r_fact * r_nn1
-                b_az = az_fact * r_nn1 / sin_pol
+                b_az = az_fact * r_nn1 / sin_pol_nz
                 b_az[z_only] = 0.
                 b_pol = pol_fact * r_nn1 / 2.
                 S_out[:, idx] = _integrate_points(
@@ -1265,7 +1267,7 @@ def _sss_basis(exp, all_coils):
             # alpha
             if degree <= int_order:
                 b_r = -(degree + 1) * r_fact / r_nn2
-                b_az = az_fact / (sin_pol * r_nn2)
+                b_az = az_fact / (sin_pol_nz * r_nn2)
                 b_az[z_only] = 0.
                 b_pol = pol_fact / (2 * r_nn2)
                 S_in[:, idx] = _integrate_points(
@@ -1274,7 +1276,7 @@ def _sss_basis(exp, all_coils):
             # beta
             if degree <= ext_order:
                 b_r = degree * r_fact * r_nn1
-                b_az = az_fact * r_nn1 / sin_pol
+                b_az = az_fact * r_nn1 / sin_pol_nz
                 b_az[z_only] = 0.
                 b_pol = pol_fact * r_nn1 / 2.
                 S_out[:, idx] = _integrate_points(
@@ -1287,8 +1289,8 @@ def _integrate_points(cos_az, sin_az, cos_pol, sin_pol, b_r, b_az, b_pol,
                       cosmags, bins, n_coils):
     """Integrate points in spherical coords."""
     grads = _sp_to_cart(cos_az, sin_az, cos_pol, sin_pol, b_r, b_az, b_pol).T
-    grads = einsum('ij,ij->i', grads, cosmags)
-    return np.bincount(bins, grads, n_coils)
+    grads = (grads * cosmags).sum(axis=1)
+    return bincount(bins, grads, n_coils)
 
 
 def _tabular_legendre(r, nind):
@@ -1299,7 +1301,7 @@ def _tabular_legendre(r, nind):
     for degree in range(nind + 1):
         L.append(np.zeros((degree + 2, len(r))))
     L[0][0] = 1.
-    pnn = 1.
+    pnn = np.ones(x.shape)
     fact = 1.
     sx2 = np.sqrt((1. - x) * (1. + x))
     for degree in range(nind + 1):
@@ -1319,11 +1321,11 @@ def _tabular_legendre(r, nind):
 
 def _sp_to_cart(cos_az, sin_az, cos_pol, sin_pol, b_r, b_az, b_pol):
     """Convert spherical coords to cartesian."""
-    return np.array([(sin_pol * cos_az * b_r +
-                      cos_pol * cos_az * b_pol - sin_az * b_az),
-                     (sin_pol * sin_az * b_r +
-                      cos_pol * sin_az * b_pol + cos_az * b_az),
-                     cos_pol * b_r - sin_pol * b_pol])
+    out = np.empty((3,) + sin_pol.shape)
+    out[0] = sin_pol * cos_az * b_r + cos_pol * cos_az * b_pol - sin_az * b_az
+    out[1] = sin_pol * sin_az * b_r + cos_pol * sin_az * b_pol + cos_az * b_az
+    out[2] = cos_pol * b_r - sin_pol * b_pol
+    return out
 
 
 def _get_degrees_orders(order):

mne/tests/test_transforms.py
@@ -18,7 +18,7 @@ from mne.transforms import (invert_transform, _get_trans,
                             _topo_to_sph, _average_quats,
                             _SphericalSurfaceWarp as SphericalSurfaceWarp,
                             rotation3d_align_z_axis, _read_fs_xfm,
-                            _write_fs_xfm, _quat_real)
+                            _write_fs_xfm, _quat_real, _fit_matched_points)
 
 data_path = testing.data_path(download=False)
 fname = op.join(data_path, 'MEG', 'sample', 'sample_audvis_trunc-trans.fif')
@@ -304,6 +304,13 @@ def test_quaternions():
             expected = np.pi if ii != jj else 0.
             assert_allclose(_angle_between_quats(a, b), expected, atol=1e-5)
 
+    y_180 = np.array([[-1, 0, 0], [0, 1, 0], [0, 0, -1.]])
+    assert_allclose(_angle_between_quats(rot_to_quat(y_180),
+                                         np.zeros(3)), np.pi)
+    h_180_attitude_90 = np.array([[0, 1, 0], [1, 0, 0], [0, 0, -1.]])
+    assert_allclose(_angle_between_quats(rot_to_quat(h_180_attitude_90),
+                                         np.zeros(3)), np.pi)
+
 
 def test_vector_rotation():
     """Test basic rotation matrix math."""
@@ -384,6 +391,12 @@ def test_fs_xfm():
         _write_fs_xfm(fname_out, xfm[:2], 'foo')
         with pytest.raises(ValueError, match='Could not find'):
             _read_fs_xfm(fname_out)
+
+
+def test_fit_matched_points():
+    """Test analytical least-squares matched point fitting."""
+    quat = _fit_matched_points(np.eye(3), np.eye(3))
+    assert_allclose(quat, 0., atol=1e-14)
 
 
 run_tests_if_main()

mne/transforms.py
@@ -14,7 +14,7 @@ import numpy as np
 from copy import deepcopy
 from scipy import linalg
 
-from .fixes import einsum
+from .fixes import einsum, mean
 from .io.constants import FIFF
 from .io.open import fiff_open
 from .io.tag import read_tag
@@ -1191,31 +1191,34 @@ def _one_rot_to_quat(rot):
     """Convert a rotation matrix to quaternions."""
     # see e.g. http://www.euclideanspace.com/maths/geometry/rotations/
     #                 conversions/matrixToQuaternion/
+    det = np.linalg.det(np.reshape(rot, (3, 3)))
+    if np.abs(det - 1.) > 1e-3:
+        raise ValueError('Matrix is not a pure rotation, got determinant != 1')
     t = 1. + rot[0] + rot[4] + rot[8]
     if t > np.finfo(rot.dtype).eps:
         s = np.sqrt(t) * 2.
+        # qw = 0.25 * s
         qx = (rot[7] - rot[5]) / s
         qy = (rot[2] - rot[6]) / s
         qz = (rot[3] - rot[1]) / s
-        # qw = 0.25 * s
     elif rot[0] > rot[4] and rot[0] > rot[8]:
         s = np.sqrt(1. + rot[0] - rot[4] - rot[8]) * 2.
+        # qw = (rot[7] - rot[5]) / s
         qx = 0.25 * s
         qy = (rot[1] + rot[3]) / s
         qz = (rot[2] + rot[6]) / s
-        # qw = (rot[7] - rot[5]) / s
     elif rot[4] > rot[8]:
         s = np.sqrt(1. - rot[0] + rot[4] - rot[8]) * 2
+        # qw = (rot[2] - rot[6]) / s
         qx = (rot[1] + rot[3]) / s
         qy = 0.25 * s
         qz = (rot[5] + rot[7]) / s
-        # qw = (rot[2] - rot[6]) / s
     else:
         s = np.sqrt(1. - rot[0] - rot[4] + rot[8]) * 2.
+        # qw = (rot[3] - rot[1]) / s
         qx = (rot[2] + rot[6]) / s
         qy = (rot[5] + rot[7]) / s
         qz = 0.25 * s
-        # qw = (rot[3] - rot[1]) / s
     return np.array((qx, qy, qz))
 
 
@@ -1239,6 +1242,14 @@ def rot_to_quat(rot):
     """
     rot = rot.reshape(rot.shape[:-2] + (9,))
     return np.apply_along_axis(_one_rot_to_quat, -1, rot)
+
+
+def _quat_to_affine(quat):
+    assert quat.shape == (6,)
+    affine = np.eye(4)
+    affine[:3, :3] = quat_to_rot(quat[:3])
+    affine[:3, 3] = quat[3:]
+    return affine
 
 
 def _angle_between_quats(x, y):
@@ -1299,6 +1310,41 @@ def _find_vector_rotation(a, b):
     vx = _skew_symmetric_cross(v)
     R += vx + np.dot(vx, vx) * (1 - c) / s
     return R
+
+
+@jit()
+def _fit_matched_points(p, x):
+    """Fit matched points using an analytical formula."""
+    # Follow notation of P.J. Besl and N.D. McKay, A Method for
+    # Registration of 3-D Shapes, IEEE Trans. Patt. Anal. Machine Intell., 14,
+    # 239 - 255, 1992.
+    #
+    # Caution: This can be dangerous if there are 3 points, or 4 points in
+    #          a symmetric layout, as the geometry can be explained
+    #          equivalently under 180 degree rotations.
+    #
+    # XXX eventually we should use this in coreg to speed things up.
+    assert p.shape == x.shape
+    assert p.ndim == 2
+    assert p.shape[1] == 3
+    mu_p = mean(p, axis=0)  # eq 23
+    mu_x = mean(x, axis=0)
+    Sigma_px = np.dot(p.T, x) / p.shape[0] - np.outer(mu_p, mu_x)  # eq 24
+    A_ij = Sigma_px - Sigma_px.T
+    Delta = np.array([A_ij[1, 2], A_ij[2, 0], A_ij[0, 1]])
+    tr_Sigma_px = np.trace(Sigma_px)
+    Q = np.empty((4, 4))
+    Q[0, 0] = tr_Sigma_px
+    Q[0, 1:] = Delta
+    Q[1:, 0] = Delta
+    Q[1:, 1:] = Sigma_px + Sigma_px.T - tr_Sigma_px * np.eye(3)
+    _, v = np.linalg.eigh(Q)  # sorted ascending
+    quat = np.empty(6)
+    quat[:3] = v[1:, -1]
+    if v[0, -1] != 0:
+        quat[:3] *= np.sign(v[0, -1])
+    quat[3:] = mu_x - np.dot(quat_to_rot(quat[:3]), mu_p)
+    return quat
 
 
 def _average_quats(quats, weights=None):