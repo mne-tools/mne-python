MRG, FIX: Fix bug with cHPI err calculation (#7339)
* FIX: Fix bug with err calculation

* FIX: Fix bug with no dev_head_t

* FIX: Better error in plot_events

* FIX: Fix velocity and add GOF test

* FIX: Fix sim test

mne/tests/test_chpi.py
@@ -120,12 +120,16 @@ def test_hpi_info(tmpdir):
         assert len(info['hpi_subsystem']) == len(raw.info['hpi_subsystem'])
 
 
-def _assert_quats(actual, desired, dist_tol=0.003, angle_tol=5.):
+def _assert_quats(actual, desired, dist_tol=0.003, angle_tol=5., err_rtol=0.5,
+                  gof_rtol=0.001, vel_atol=2e-3):  # 2 mm/s
     """Compare estimated cHPI positions."""
     __tracebackhide__ = True
     trans_est, rot_est, t_est = head_pos_to_trans_rot_t(actual)
     trans, rot, t = head_pos_to_trans_rot_t(desired)
     quats_est = rot_to_quat(rot_est)
+    gofs, errs, vels = desired[:, 7:].T
+    gofs_est, errs_est, vels_est = actual[:, 7:].T
+    del actual, desired
 
     # maxfilter produces some times that are implausibly large (weird)
     if not np.isclose(t[0], t_est[0], atol=1e-1):  # within 100 ms
@@ -136,6 +140,7 @@ def _assert_quats(actual, desired, dist_tol=0.003, angle_tol=5.):
     trans = trans[use_mask]
     quats = rot_to_quat(rot)
     quats = quats[use_mask]
+    gofs, errs, vels = gofs[use_mask], errs[use_mask], vels[use_mask]
 
     # double-check our angle function
     for q in (quats, quats_est):
@@ -159,6 +164,21 @@ def _assert_quats(actual, desired, dist_tol=0.003, angle_tol=5.):
     assert angles[arg_worst] <= angle_tol, (
         '@ %0.3f seconds: %0.3f > %0.3f deg'
         % (t[arg_worst], angles[arg_worst], angle_tol))
+
+    # error calculation difference
+    errs_est_interp = interp1d(t_est, errs_est)(t)
+    assert_allclose(errs_est_interp, errs, rtol=err_rtol, atol=1e-3,
+                    err_msg='err')  # 1 mm
+
+    # gof calculation difference
+    gof_est_interp = interp1d(t_est, gofs_est)(t)
+    assert_allclose(gof_est_interp, gofs, rtol=gof_rtol, atol=1e-7,
+                    err_msg='gof')
+
+    # velocity calculation difference
+    vel_est_interp = interp1d(t_est, vels_est)(t)
+    assert_allclose(vel_est_interp, vels, atol=vel_atol,
+                    err_msg='velocity')
 
 
 def _decimate_chpi(raw, decim=4):
@@ -253,8 +273,11 @@ def test_calculate_chpi_positions_artemis():
     """Test on 5k artemis data."""
     raw = read_raw_artemis123(art_fname, preload=True)
     mf_quats = read_head_pos(art_mc_fname)
+    mf_quats[:, 8:] /= 100  # old code errantly had this factor
     py_quats = _calculate_chpi_positions(raw, t_step_min=2., verbose='debug')
-    _assert_quats(py_quats, mf_quats, dist_tol=0.001, angle_tol=1.)
+    _assert_quats(
+        py_quats, mf_quats,
+        dist_tol=0.001, angle_tol=1., err_rtol=0.7, vel_atol=1e-2)
 
 
 def test_initial_fit_redo():
@@ -312,7 +335,8 @@ def test_calculate_head_pos_chpi_on_chpi5_in_one_second_steps():
     # needs no interpolation, because maxfilter pos files comes with 1 s steps
     py_quats = _calculate_chpi_positions(
         raw, t_step_min=1.0, t_step_max=1.0, t_window=1.0, verbose='debug')
-    _assert_quats(py_quats, mf_quats, dist_tol=0.002, angle_tol=1.2)
+    _assert_quats(py_quats, mf_quats, dist_tol=0.002, angle_tol=1.2,
+                  vel_atol=3e-3)  # 3 mm/s
 
 
 @pytest.mark.slowtest
@@ -327,7 +351,8 @@ def test_calculate_head_pos_chpi_on_chpi5_in_shorter_steps():
         py_quats = _calculate_chpi_positions(
             raw, t_step_min=0.1, t_step_max=0.1, t_window=0.1, verbose='debug')
     # needs interpolation, tolerance must be increased
-    _assert_quats(py_quats, mf_quats, dist_tol=0.002, angle_tol=1.2)
+    _assert_quats(py_quats, mf_quats, dist_tol=0.002, angle_tol=1.2,
+                  vel_atol=0.02)  # 2 cm/s is not great but probably fine
 
 
 def test_simulate_calculate_head_pos_chpi():
@@ -386,8 +411,8 @@ def test_simulate_calculate_head_pos_chpi():
         np.outer(np.arange(S) * dz, ez)
     dev_head_pos[:, 7] = 1.0
 
-    # cm/s
-    dev_head_pos[:, 9] = 100 * dz / (info['sfreq'] * head_pos_sfreq_quotient)
+    # m/s
+    dev_head_pos[:, 9] = dz / (info['sfreq'] * head_pos_sfreq_quotient)
 
     # Round number of samples to the next integer
     raw_data = np.zeros((len(picks), int(duration * info['sfreq'] + 0.5)))
@@ -396,7 +421,8 @@ def test_simulate_calculate_head_pos_chpi():
     quats = _calculate_chpi_positions(
         raw, t_step_min=raw.info['sfreq'] * head_pos_sfreq_quotient,
         t_step_max=raw.info['sfreq'] * head_pos_sfreq_quotient, t_window=1.0)
-    _assert_quats(quats, dev_head_pos, dist_tol=0.001, angle_tol=1.)
+    _assert_quats(quats, dev_head_pos, dist_tol=0.001, angle_tol=1.,
+                  vel_atol=4e-3)  # 4 mm/s
 
 
 def _calculate_chpi_coil_locs(raw, verbose):
@@ -521,7 +547,9 @@ def test_calculate_head_pos_ctf():
     raw = read_raw_ctf(ctf_chpi_fname)
     quats = calculate_head_pos_ctf(raw)
     mc_quats = read_head_pos(ctf_chpi_pos_fname)
-    _assert_quats(quats, mc_quats, dist_tol=0.004, angle_tol=2.5)
+    mc_quats[:, 9] /= 10000  # had old factor in there twice somehow...
+    _assert_quats(quats, mc_quats, dist_tol=0.004, angle_tol=2.5, err_rtol=1.,
+                  vel_atol=7e-3)  # 7 mm/s
 
     raw = read_raw_fif(ctf_fname)
     with pytest.raises(RuntimeError, match='Could not find'):