1765aabdf3c829bfc15df47dd7af4c806a323346
Robert Luke <748691+rob-luke@users.noreply.github.com>
2020-03-11 14:19:31 +1100

    Add topomap test for nirs recording with overlapping channels

--- mne/datasets/utils.py
+++ mne/datasets/utils.py
@@ -239,7 +239,7 @@ def _data_path(path=None, force_update=False, update_path=True, download=True,
     path = _get_path(path, key, name)
     # To update the testing or misc dataset, push commits, then make a new
     # release on GitHub. Then update the "releases" variable:
-    releases = dict(testing='0.80', misc='0.5')
+    releases = dict(testing='0.81', misc='0.5')
     # And also update the "md5_hashes['testing']" variable below.
 
     # To update any other dataset, update the data archive itself (upload
@@ -323,7 +323,7 @@ def _data_path(path=None, force_update=False, update_path=True, download=True,
         sample='12b75d1cb7df9dfb4ad73ed82f61094f',
         somato='ea825966c0a1e9b2f84e3826c5500161',
         spm='9f43f67150e3b694b523a21eb929ea75',
-        testing='de51a47739798064fd20bd3345d5eba0',
+        testing='f1cb4447a0c27a120ad0f7fb98ae8368',
         multimodal='26ec847ae9ab80f58f204d09e2c08367',
         fnirs_motor='c4935d19ddab35422a69f3326a01fef8',
         opm='370ad1dcfd5c47e029e692c85358a374',
