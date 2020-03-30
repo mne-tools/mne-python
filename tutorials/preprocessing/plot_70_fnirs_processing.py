7af57aad595f2912ccc3dd389a44113f07a28a4a
mshader <55732126+mshader@users.noreply.github.com>
2020-03-30 14:50:44 +1100

    Changed thumbnail picture

--- tutorials/preprocessing/plot_70_fnirs_processing.py
+++ tutorials/preprocessing/plot_70_fnirs_processing.py
@@ -14,7 +14,7 @@ deoxyhaemoglobin (HbR) concentration.
 
 Here we will work with the :ref:`fNIRS motor data <fnirs-motor-dataset>`.
 """
-# sphinx_gallery_thumbnail_number = 3
+# sphinx_gallery_thumbnail_number = 12
 
 import os
 import numpy as np
