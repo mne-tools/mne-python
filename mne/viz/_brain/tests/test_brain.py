ac5f08ca2e36886b314689081ff0ce82cccf5b4d
Guillaume Favelier <guillaume.favelier@gmail.com>
2020-04-10 17:06:17 +0200

    Update requirements

--- mne/viz/_brain/tests/test_brain.py
+++ mne/viz/_brain/tests/test_brain.py
@@ -170,7 +170,7 @@ def test_brain_save_movie(tmpdir, renderer):
     if renderer.get_3d_backend() == "mayavi":
         pytest.skip()
     brain_data = _create_testing_brain(hemi='lh')
-    filename = str(path.join(tmpdir, "brain_test.gif"))
+    filename = str(path.join(tmpdir, "brain_test.mov"))
     brain_data.save_movie(filename, time_dilation=1,
                           interpolation='nearest')
     assert path.isfile(filename)
