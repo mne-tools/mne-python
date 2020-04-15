FIX: Explicit gap check and overflow

mne/io/egi/egimff.py
@@ -33,18 +33,22 @@ def _read_mff_header(filepath):
     # sanity check and update relevant values
     for key in ('last_samps', 'first_samps'):
         # convert from times in µS to samples
-        epochs[key] = (epochs[key] * signal_blocks['sfreq']) // 1000000
+        for ei, e in enumerate(epochs[key]):
+            epochs[key][ei] = (
+                epochs[key][ei] * signal_blocks['sfreq']) // 1000000
     n_samps_block = signal_blocks['samples_block'].sum()
-    n_samps_epochs = (epochs['last_samps'] - epochs['first_samps']).sum()
+    n_samps_epochs = sum(l - f for l, f in zip(epochs['last_samps'],
+                                               epochs['first_samps']))
     # XXX perhaps there is a way to detect micro versus nano (it is different
     # in the recording time) but this should be safe enough...
     if n_samps_epochs == n_samps_block * 1000:
         for key in ('last_samps', 'first_samps'):
-            epochs[key] //= 1000
+            for ei, e in enumerate(epochs[key]):
+                epochs[key][ei] = e // 1000
     for key in ('last_samps', 'first_samps'):
         # Should be safe to cast to int now, which makes things later not
         # upbroadcast to float
-        epochs[key] = epochs[key].astype(np.int64)
+        epochs[key] = np.array(epochs[key], np.int64)
     n_samps_epochs = (epochs['last_samps'] - epochs['first_samps']).sum()
     bad = (n_samps_epochs != n_samps_block or
            not (epochs['first_samps'] < epochs['last_samps']).all() or

mne/io/egi/general.py
@@ -63,8 +63,8 @@ def _get_ep_info(filepath):
         epoch_info['last_samps'].append(ep_end)
         epoch_info['first_blocks'].append(first_block)
         epoch_info['last_blocks'].append(last_block)
-    for key in keys:
-        epoch_info[key] = np.array(epoch_info[key], np.uint64)
+    # Don't turn into ndarray here, keep native int because it can deal with
+    # huge numbers (could use np.uint64 but it's more work)
     return epoch_info
 
 

mne/io/egi/tests/test_egi.py
@@ -42,13 +42,24 @@ def test_egi_mff_pause(fname, n_pause, n_events):
     with pytest.warns(RuntimeWarning, match='Acquisition skips detected'):
         raw = _test_raw_reader(read_raw_egi, input_fname=fname)
     assert len(raw.annotations) == n_pause
-    assert (raw.annotations.description == 'BAD_ACQ_SKIP').all()
     if n_events == 0:
         with pytest.raises(ValueError, match='Consider using .*events_from'):
             find_events(raw)
     else:
         events = find_events(raw)
         assert len(events) == n_events
+    # read some data from the middle of the skip, assert it's all zeros
+    stim_picks = pick_types(raw.info, meg=False, stim=True, exclude=())
+    other_picks = np.setdiff1d(np.arange(len(raw.ch_names)), stim_picks)
+    for annot in raw.annotations:
+        assert annot['description'] == 'BAD_ACQ_SKIP'
+        start, stop = raw.time_as_index(
+            [annot['onset'], annot['onset'] + annot['duration']])
+        data, _ = raw[:, start:stop]
+        assert_array_equal(data[other_picks], 0.)
+        if n_events > 0:
+            assert raw.ch_names[-1] == 'STI 014'
+            assert not np.array_equal(data[stim_picks], 0.)
 
 
 @requires_testing_data