cc91e1cddda5ae285eb855ab6cfa772bddfcbed3
britta-wstnr <britta.wstnr@gmail.com>
2020-04-20 20:54:21 +0200

    make test_linalg more precise

--- mne/utils/tests/test_linalg.py
+++ mne/utils/tests/test_linalg.py
@@ -13,13 +13,13 @@ def test_pos_semidef_inv():
     random = np.random.RandomState(73)
 
     # matrix with real values
-    real_part = random.rand(3, 3) * 10
+    real_part = random.rand(3, 3)
     real_part = real_part.dot(real_part.T)
     mat_real = np.array([real_part, real_part])  # _pos_def_inv expects >2 dim.
 
     # matrix with complex values
     random.seed(21)
-    imag_part = random.rand(3, 3) * 10
+    imag_part = random.rand(3, 3)
     mat_complex = real_part.copy()
     mat_complex = np.array(imag_part, dtype=complex)
     mat_complex.imag = imag_part
@@ -30,12 +30,16 @@ def test_pos_semidef_inv():
     # Test for real-valued matrices:
     mat_pinv = np.linalg.pinv(mat_real)
     mat_symv = _pos_semidef_inv(mat_real, reduce_rank=False)
-    assert_allclose(mat_pinv, mat_symv)
+    assert_allclose(mat_pinv, mat_symv, rtol=1e-7,
+                    atol=1e-7 * np.median(mat_pinv[0].ravel()))
 
     # Test for complex values:
     mat_pinv = np.linalg.pinv(mat_complex)
     mat_symv = _pos_semidef_inv(mat_complex, reduce_rank=False)
-    assert_allclose(mat_pinv, mat_symv)
+    assert_allclose(np.real(mat_pinv), np.real(mat_symv), rtol=1e-7,
+                    atol=1e-7 * np.median(np.real(mat_pinv).ravel()))
+    assert_allclose(np.imag(mat_pinv), np.imag(mat_symv), rtol=1e-7,
+                    atol=1e-7 * np.median(np.imag(mat_pinv).ravel()))
 
 
 run_tests_if_main()
