39e79b53d5d98ac06b263efc38626f00a50a4f28
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2020-04-21 16:07:10 +0200

    address review comments

--- mne/channels/__init__.py
+++ mne/channels/__init__.py
@@ -9,6 +9,7 @@ from .layout import (Layout, make_eeg_layout, make_grid_layout, read_layout,
 from .montage import (DigMontage,
                       get_builtin_montages, make_dig_montage, read_dig_dat,
                       read_dig_egi, read_dig_captrak, read_dig_fif,
+                      read_dig_captrack,  # XXX: Remove in 0.22
                       read_dig_polhemus_isotrak, read_polhemus_fastscan,
                       compute_dev_head_t, make_standard_montage,
                       read_custom_montage, read_dig_hpts,
