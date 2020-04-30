f4f80baac8a9639c32bc6832f3febf27cae26ffe
Guillaume Favelier <guillaume.favelier@gmail.com>
2020-04-30 16:20:11 +0200

    Fix test_renderer

--- mne/viz/tests/test_3d.py
+++ mne/viz/tests/test_3d.py
@@ -791,10 +791,10 @@ def test_link_brains(renderer_interactive):
 
 def test_renderer(renderer):
     """Test that renderers are available on demand."""
-    backend = renderer._get_3d_backend()
+    backend = renderer.get_3d_backend()
     cmd = [sys.executable, '-uc',
            'import mne; mne.viz.create_3d_figure((800, 600)); '
-           'backend = mne.viz._get_3d_backend(); '
+           'backend = mne.viz.get_3d_backend(); '
            'assert backend == %r, backend' % (backend,)]
     with modified_env(MNE_3D_BACKEND=backend):
         run_subprocess(cmd)
