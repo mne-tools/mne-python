fe46221ebc92f85bca24890ae1ebe3106d6a49fe
Kyle Mathewson <kylemath@gmail.com>
2020-05-10 17:10:25 -0700

    default doc

--- mne/defaults.py
+++ mne/defaults.py
@@ -85,6 +85,7 @@ DEFAULTS = dict(
 
 def _handle_default(k, v=None):
     """Avoid dicts as default keyword arguments.
+
     Use this function instead to resolve default dict values. Example usage::
         scalings = _handle_default('scalings', scalings)
     """
