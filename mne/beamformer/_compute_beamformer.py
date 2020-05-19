FIX: Rotation invariant direct method

mne/beamformer/_compute_beamformer.py
@@ -301,7 +301,7 @@ def _compute_beamformer(G, Cm, reg, n_orient, weight_norm, pick_ori,
     assert bf_denom_inv.shape == (n_sources, n_orient, n_orient)
     W = np.matmul(bf_denom_inv, bf_numer)
     assert W.shape == (n_sources, n_orient, n_channels)
-    del bf_numer, bf_denom, bf_denom_inv, sk
+    del bf_denom_inv, sk
 
     #
     # 5. Re-scale filter weights according to the selected weight_norm
@@ -313,10 +313,22 @@ def _compute_beamformer(G, Cm, reg, n_orient, weight_norm, pick_ori,
     # and W_ug referring to the above-calculated unit-gain filter stored in W
 
     if weight_norm is not None:
-        # Compute sqrt(W_ug @ W_ug.T):
-        # Frobenius norm (default) is the sqrt of sum of squares, also
-        # the sqrt(trace(A.T.conj() @ A))
-        noise_norm = np.linalg.norm(W, axis=(1, 2), keepdims=True)
+        # Use sqrt(diag(W_ug @ W_ug.T)), which will not be rotation invariant:
+        # noise_norm = np.matmul(W, W.transpose(0, 2, 1).conj())
+        # noise_norm.shape = (n_sources, n_orient * n_orient)
+        # noise_norm = noise_norm[:, ::n_orient + 1][:, :, np.newaxis]
+        # np.sqrt(noise_norm, out=noise_norm)
+        # assert noise_norm.shape == (n_sources, n_orient, 1)
+
+        # Old way (rotation invariant) uses the Frobenius matrix norm,
+        # yielding a single scale factor for each source point:
+        # noise_norm = np.linalg.norm(W, axis=(1, 2), keepdims=True)
+
+        # Direct way (rotation invariant, lowest bias) also uses Fro mat norm:
+        # W = G.T @ Cm_inv / sqrt(G.T @ Cm_inv @ Cm_inv @ G)
+        #   = bf_numer / noise_norm
+        W = bf_numer
+        noise_norm = np.linalg.norm(bf_numer, axis=(1, 2), keepdims=True)
 
         if weight_norm == 'nai':
             # Estimate noise level based on covariance matrix, taking the