700bad8d061da44343f85bae31a68f953ad9d7cf
Eric Larson <larson.eric.d@gmail.com>
2020-05-21 10:13:18 -0400

    FIX: Simplify with minigallery [circle front]

--- doc/conf.py
+++ doc/conf.py
@@ -391,12 +391,8 @@ def append_attr_meth_examples(app, what, name, obj, options, lines):
 
 .. rubric:: Examples using ``{0}``:
 
-.. include:: {1}.examples
-   :start-line: 5
+.. minigallery:: {1}
 
-.. raw:: html
-
-    <div style="clear:both"></div>
 """.format(name.split('.')[-1], name).split('\n')
 
 
