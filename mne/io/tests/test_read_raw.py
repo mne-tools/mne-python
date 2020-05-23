77a82dbe0ecb9eb0f0310f30b805bdbb4f8982d5
Clemens Brunner <clemens.brunner@gmail.com>
2020-05-23 12:44:24 +0200

    Add tests

---
+++ mne/io/tests/test_read_raw.py
@@ -0,0 +1,36 @@
+"""Test generic read_raw function."""
+
+# Authors: Clemens Brunner <clemens.brunner@gmail.com>
+#
+# License: BSD (3-clause)
+
+
+import pytest
+
+from mne.io import read_raw
+
+
+@pytest.mark.parametrize('fname', ['x.xxx', 'x'])
+def test_read_raw_unsupported(fname):
+    """Test handling of unsupported file types."""
+    with pytest.raises(ValueError, match='Unsupported file type'):
+        read_raw(fname)
+
+
+@pytest.mark.parametrize('fname', ['x.vmrk', 'x.eeg'])
+def test_read_raw_suggested(fname):
+    """Test handling of unsupported file types with suggested alternatives."""
+    with pytest.raises(ValueError, match='Try reading'):
+        read_raw(fname)
+
+
+@pytest.mark.parametrize('fname', ['../edf/tests/data/test.edf',
+                                   '../edf/tests/data/test.bdf',
+                                   '../brainvision/tests/data/test.vhdr',
+                                   '../kit/tests/data/test.sqd'])
+def test_read_raw_supported(fname):
+    """Test supported file types."""
+    read_raw(fname)
+    read_raw(fname, verbose=False)
+    raw = read_raw(fname, preload=True)
+    assert "data loaded" in str(raw)
