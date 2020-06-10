709c45876307b8ff03f3b4794d560189edb4ad94
Richard Höchenberger <richard.hoechenberger@gmail.com>
2020-06-10 18:10:51 +0200

    Only plot gradiometers

--- tutorials/preprocessing/plot_60_maxwell_filtering_sss.py
+++ tutorials/preprocessing/plot_60_maxwell_filtering_sss.py
@@ -131,51 +131,52 @@ raw.info['bads'] = bads
 # dictionary of all data related to the scoring used to classify channels as
 # noisy or flat. This information can be used to produce diagnostic figures.
 #
-# In the following, we will generate a total of four of such visualizations for
-# the automated detection of *noisy* channels.
+# In the following, we will generate such visualizations for
+# the automated detection of *noisy* gradiometer channels.
 
-for ch_type in ('mag', 'grad'):
-    # Only select the data for mag or grad channels.
-    ch_subset = auto_scores['ch_types'] == ch_type
-    ch_names = auto_scores['ch_names'][ch_subset]
-    scores = auto_scores['scores_noisy'][ch_subset]
-    limits = auto_scores['limits_noisy'][ch_subset]
-    bins = auto_scores['bins']  # The the windows that were evaluated.
-    # We will label each segment by its start and stop time, with up to 3
-    # 3 digits before and 3 digits after the decimal place (1 ms precision).
-    bin_labels = [f'{start:3.3f} – {stop:3.3f}'
-                  for start, stop in bins]
+# Only select the data for mag or grad channels.
+ch_type = 'grad'
+ch_subset = auto_scores['ch_types'] == ch_type
+ch_names = auto_scores['ch_names'][ch_subset]
+scores = auto_scores['scores_noisy'][ch_subset]
+limits = auto_scores['limits_noisy'][ch_subset]
+bins = auto_scores['bins']  # The the windows that were evaluated.
+# We will label each segment by its start and stop time, with up to 3
+# 3 digits before and 3 digits after the decimal place (1 ms precision).
+bin_labels = [f'{start:3.3f} – {stop:3.3f}'
+              for start, stop in bins]
 
-    # We store the data in a Pandas DataFrame. The seaborn heatmap function
-    # we will call below will then be able to automatically assign the correct
-    # labels to all axes.
-    data_to_plot = pd.DataFrame(data=scores,
-                                columns=pd.Index(bin_labels, name='Time (s)'),
-                                index=pd.Index(ch_names, name='Channel'))
-    # Plot the "raw" scores.
-    fig, ax = plt.subplots(1, 2, figsize=(12, 8))
-    fig.suptitle(f'Automated noisy channel detection: {ch_type}',
-                 fontsize=16, fontweight='bold')
-    sns.heatmap(data=data_to_plot, cmap='Reds', cbar_kws=dict(label='Score'),
-                ax=ax[0])
-    [ax[0].axvline(x, ls='dashed', lw=0.25, dashes=(25, 15), color='gray')
-     for x in range(1, len(bins))]
-    ax[0].set_title('All Scores', fontweight='bold')
+# We store the data in a Pandas DataFrame. The seaborn heatmap function
+# we will call below will then be able to automatically assign the correct
+# labels to all axes.
+data_to_plot = pd.DataFrame(data=scores,
+                            columns=pd.Index(bin_labels, name='Time (s)'),
+                            index=pd.Index(ch_names, name='Channel'))
 
-    # Only plot scores that exceeded the limits.
-    # We also pass the optional parameters ``vmin`` and ``vmax`` to the
-    # plotting function to avoid a (harmless) warning about missing values.
-    # However, this is purely optional.
-    mask = scores <= limits
-    sns.heatmap(data=data_to_plot, mask=mask,
-                vmin=limits.min(), vmax=scores.max(),  # May be omitted.
-                cmap='Reds', cbar_kws=dict(label='Score'), ax=ax[1])
-    [ax[1].axvline(x, ls='dashed', lw=0.25, dashes=(25, 15), color='gray')
-     for x in range(1, len(bins))]
-    ax[1].set_title('Scores > Limit', fontweight='bold')
+# First, plot the "raw" scores.
+fig, ax = plt.subplots(1, 2, figsize=(12, 8))
+fig.suptitle(f'Automated noisy channel detection: {ch_type}',
+             fontsize=16, fontweight='bold')
+sns.heatmap(data=data_to_plot, cmap='Reds', cbar_kws=dict(label='Score'),
+            ax=ax[0])
+[ax[0].axvline(x, ls='dashed', lw=0.25, dashes=(25, 15), color='gray')
+    for x in range(1, len(bins))]
+ax[0].set_title('All Scores', fontweight='bold')
 
-    # Figure title should not overlap with subplots.
-    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+# Now, only plot scores that exceeded the limits.
+# We also pass the optional parameters ``vmin`` and ``vmax`` to the
+# plotting function to avoid a (harmless) warning about missing values.
+# However, this is purely optional.
+mask = scores <= limits
+sns.heatmap(data=data_to_plot, mask=mask,
+            vmin=limits.min(), vmax=scores.max(),  # May be omitted.
+            cmap='Reds', cbar_kws=dict(label='Score'), ax=ax[1])
+[ax[1].axvline(x, ls='dashed', lw=0.25, dashes=(25, 15), color='gray')
+    for x in range(1, len(bins))]
+ax[1].set_title('Scores > Limit', fontweight='bold')
+
+# The figure title should not overlap with the subplots.
+fig.tight_layout(rect=[0, 0.03, 1, 0.95])
 
 ###############################################################################
 #
