63a0ad41a828a6f15f8acddc9cc2a1735eb395b6
Daniel McCloy <dan@mccloy.info>
2020-07-20 10:53:43 -0700

    delay plt_show until after quiver

--- mne/viz/topomap.py
+++ mne/viz/topomap.py
@@ -2619,7 +2619,7 @@ def plot_arrowmap(data, info_from, info_to=None, scale=3e-10, vmin=None,
     plot_topomap(data, pos, axes=axes, vmin=vmin, vmax=vmax, cmap=cmap,
                  sensors=sensors, res=res, names=names, show_names=show_names,
                  mask=mask, mask_params=mask_params, outlines=outlines,
-                 contours=contours, image_interp=image_interp, show=show,
+                 contours=contours, image_interp=image_interp, show=False,
                  onselect=onselect, extrapolate=extrapolate, sphere=sphere,
                  ch_type=ch_type)
     x, y = tuple(pos.T)
@@ -2629,5 +2629,6 @@ def plot_arrowmap(data, info_from, info_to=None, scale=3e-10, vmin=None,
     axes.quiver(x, y, dxx, dyy, scale=scale, color='k', lw=1, clip_on=False)
     axes.figure.canvas.draw_idle()
     tight_layout(fig=fig)
+    plt_show(show)
 
     return fig
