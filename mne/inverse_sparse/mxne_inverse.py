FIX: Weighting

mne/inverse_sparse/mxne_inverse.py
@@ -152,13 +152,11 @@ def _split_gof(M, X, gain):
     # assuming x is estimated using elements of gain, with residual res
     # along the first axis
     assert M.ndim == X.ndim == gain.ndim == 2, (M.ndim, X.ndim, gain.ndim)
-    assert gain.shape == (M.shape[0], X.shape[0])
+    assert gain.shape == (M.shape[0], X.shape[0]), (gain.shape, M.shape)
     assert M.shape[1] == X.shape[1]
+    U, s, Vh = np.linalg.svd(gain, full_matrices=False)
     M_est = gain @ X
-    assert M.shape == M_est.shape
     res = M - M_est
-    assert gain.shape[0] == M.shape[0], (gain.shape, M.shape)
-    U, _, Vh = np.linalg.svd(gain, full_matrices=False)
     # the part that gets explained
     fit_orth = U.T @ M
     # the part that got over-explained (landed in residual)
@@ -173,6 +171,10 @@ def _split_gof(M, X, gain):
         (fit_back * fit_back.conj()).real -
         (res_back * res_back.conj()).real) / norm
     assert gof_back.shape == X.shape, (gof_back.shape, X.shape)
+    assert (gof_back >= -1e-12).all()
+    gof_sum = gof_back.sum(0)
+    gof = 100. - 100. * (res * res.conj()).real.sum(0) / norm[0]
+    np.testing.assert_allclose(gof_sum, gof)
     return gof_back
 
 
@@ -440,7 +442,9 @@ def mixed_norm(evoked, forward, noise_cov, alpha, loose='auto', depth=0.8,
 
     # Reapply weights to have correct unit
     X = _reapply_source_weighting(X, source_weighting, active_set)
+    source_weighting[source_weighting == 0] = 1  # zeros
     gain_active /= source_weighting[active_set]
+    del source_weighting
     M_estimate = np.dot(gain_active, X)
 
     outs = list()

mne/inverse_sparse/tests/test_mxne_inverse.py
@@ -256,7 +256,7 @@ def test_mxne_vol_sphere():
     assert_array_almost_equal(stc.times, evoked.times, 5)
 
 
-@pytest.mark.parametrize('mod', (None, 'mult', 'augment', 'sign'))
+@pytest.mark.parametrize('mod', (None, 'mult', 'augment', 'sign', 'same'))
 def test_split_gof_basic(mod):
     """Test splitting the goodness of fit."""
     # first a trivial case
@@ -275,6 +275,10 @@ def test_split_gof_basic(mod):
         gain[1] *= -1
         M[1] *= -1
         M_est[1] *= -1
+    elif mod == 'same':
+        gain[:, 1] = gain[:, 0]
+        X[:, 0] = [2. / 3., 1. / 3.]  # half as big
+        M_est = gain @ X
     else:
         assert mod is None
     res = M - M_est
@@ -286,6 +290,8 @@ def test_split_gof_basic(mod):
         want = np.concatenate((want, [[0]]))
     if mod == 'mult':
         assert_array_less(gof_split[1], gof_split[0])
+    elif mod == 'same':
+        assert_allclose(gof_split[0], gof_split[1])
     else:
         assert_allclose(gof_split, want, atol=1e-12)
 
@@ -299,7 +305,7 @@ def test_split_gof_basic(mod):
     ([0, 15157, 19448], [1, 1, 1]),
     ([0, 15157, 19448], [1e-2, 1, 5]),
 ])
-def test_gof_split_meg(forward, idx, weights):
+def test_split_gof_meg(forward, idx, weights):
     """Test GOF splitting on MEG data."""
     gain = forward['sol']['data'][:, idx]
     # close to orthogonal