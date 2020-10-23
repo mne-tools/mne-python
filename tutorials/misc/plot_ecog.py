874abac88d275486316c90a227f4e4dcebde1251
Adam Li <adam2392@gmail.com>
2020-10-22 23:10:20 -0400

    Update tutorials/misc/plot_ecog.py
    Co-authored-by: Eric Larson <larson.eric.d@gmail.com>

--- tutorials/misc/plot_ecog.py
+++ tutorials/misc/plot_ecog.py
@@ -15,7 +15,8 @@ This example shows how to use:
 - channel locations in subject's MRI space
 - projection onto a surface
 
-For an example that involves sEEG data, channel locations in a template-specific MRI, or projection into a volume, see :ref:`tut-working-with-seeg`.
+For an example that involves sEEG data, channel locations in
+MNI space, or projection into a volume, see :ref:`tut-working-with-seeg`.
 """
 # Authors: Eric Larson <larson.eric.d@gmail.com>
 #          Chris Holdgraf <choldgraf@gmail.com>
