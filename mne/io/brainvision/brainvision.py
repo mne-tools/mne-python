e8d8dc19a34e6d62f7471bac542216a1c81b6fb2
Richard Höchenberger <richard.hoechenberger@gmail.com>
2020-10-27 11:31:13 +0100

    Fix BrainVision reading for certain vhdr files
    Fixes #8432

--- mne/io/brainvision/brainvision.py
+++ mne/io/brainvision/brainvision.py
@@ -498,11 +498,16 @@ def _get_vhdr_info(vhdr_fname, eog, misc, scale):
     for chan, props in cfg.items('Channel Infos'):
         n = int(re.findall(r'ch(\d+)', chan)[0]) - 1
         props = props.split(',')
+
         # default to microvolts because that's what the older brainvision
         # standard explicitly assumed; the unit is only allowed to be
         # something else if explicitly stated (cf. EEGLAB export below)
         if len(props) < 4:
-            props += (u'µV',)
+            props += ('µV',)
+        elif len(props) == 4 and props[-1] == '':
+            # Deal with files with 4 props where unit is simply empty.
+            props[-1] = 'µV'
+
         name, _, resolution, unit = props[:4]
         ch_dict[chan] = name
         ch_names[n] = name
