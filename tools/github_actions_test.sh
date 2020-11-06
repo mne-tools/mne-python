02e5f720f8d78bfdcd8aceda3c6b5e36ba11e688
Guillaume Favelier <guillaume.favelier@gmail.com>
2020-11-06 17:17:48 +0100

    Add comments

--- tools/github_actions_test.sh
+++ tools/github_actions_test.sh
@@ -1,13 +1,20 @@
 #!/bin/bash -ef
 
+# Install MNE
 python setup.py build
 python setup.py install
 mne sys_info
 python -c "import numpy; numpy.show_config()"
+
+# Download testing data
 python -c 'import mne; mne.datasets.testing.data_path(verbose=True)';
+
+# Print locale
 echo "Print locale "
 locale
 echo "Other stuff"
+
+# Run tests
 USE_DIRS="mne/"
 if [ "${TRAVIS_OS_NAME}" != "osx" ]; then
   CONDITION="not ultraslowtest"
