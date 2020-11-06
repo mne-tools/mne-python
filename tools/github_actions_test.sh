5b062d733c947600442d912fca2f4c644a671e1f
Guillaume Favelier <guillaume.favelier@gmail.com>
2020-11-06 17:46:41 +0100

    Split main script and migrate MacOS too

--- tools/github_actions_test.sh
+++ tools/github_actions_test.sh
@@ -1,22 +1,5 @@
 #!/bin/bash -ef
 
-# Install MNE
-python setup.py build
-python setup.py install
-
-# Show infos
-mne sys_info
-python -c "import numpy; numpy.show_config()"
-
-# Download testing data
-python -c 'import mne; mne.datasets.testing.data_path(verbose=True)';
-
-# Print locale
-echo "Print locale "
-locale
-echo "Other stuff"
-
-# Run tests
 USE_DIRS="mne/"
 if [ "${TRAVIS_OS_NAME}" != "osx" ]; then
   CONDITION="not ultraslowtest"
