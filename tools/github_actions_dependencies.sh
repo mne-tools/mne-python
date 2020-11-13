06faa33e972d13cb56936d04ece66bc24606cc58
Guillaume Favelier <guillaume.favelier@gmail.com>
2020-11-13 18:58:25 +0100

    Debug env [skip circle][skip travis]

--- tools/github_actions_dependencies.sh
+++ tools/github_actions_dependencies.sh
@@ -1,15 +1,12 @@
 #!/bin/bash -ef
 
 if [ ! -z "$CONDA_ENV" ]; then
-	echo "conda env detected"
 	conda env update --file $CONDA_ENV
 	conda activate mne
 	pip uninstall -yq mne
 elif [ ! -z "$CONDA_DEPENDENCIES" ]; then
-	echo "conda deps detected"
-	conda install $CONDA_DEPENDENCIES
+	conda install -y $CONDA_DEPENDENCIES
 else # pip
-	echo "pip env detected"
 	python -m pip install --upgrade pip setuptools wheel
 	pip uninstall -yq numpy
 	pip install -i "https://pypi.anaconda.org/scipy-wheels-nightly/simple" --pre "numpy!=1.20.0.dev0+20201111233731.0ffaaf8,!=1.20.0.dev0+20201111232921.0ffaaf8"
