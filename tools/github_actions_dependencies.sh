be4a5a1ee0eaa56454aa2123960a2f7dc3b2519e
Guillaume Favelier <guillaume.favelier@gmail.com>
2020-11-13 15:58:10 +0100

    Test matrix with pip job [skip circle][skip azp][skip travis]

--- tools/github_actions_dependencies.sh
+++ tools/github_actions_dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ef
 
 if [ -z "$CONDA_ENV" ] && [ -z "$CONDA_DEPENDENCIES" ]; then
+	python -m pip install --upgrade pip setuptools wheel
 	pip uninstall -yq numpy
 	pip install -i "https://pypi.anaconda.org/scipy-wheels-nightly/simple" --pre "numpy!=1.20.0.dev0+20201111233731.0ffaaf8,!=1.20.0.dev0+20201111232921.0ffaaf8"
 	pip install -f "https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com" scipy pandas scikit-learn matplotlib h5py Pillow
