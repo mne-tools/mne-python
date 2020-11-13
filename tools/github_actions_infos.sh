d2e89b70235a1651d23c1c1a0c2f5c8340f9bded
Guillaume Favelier <guillaume.favelier@gmail.com>
2020-11-13 20:16:08 +0100

    Re-activate conda a lot [skip circle][skip travis]

--- tools/github_actions_infos.sh
+++ tools/github_actions_infos.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -ef
 
+if [ ! -z "$CONDA_ENV" ]; then
+	CONDA_BASE=$(conda info --base)
+	source $CONDA_BASE/etc/profile.d/conda.sh
+	conda activate mne
+fi
+
 mne sys_info
 python -c "import numpy; numpy.show_config()"
