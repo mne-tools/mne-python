f00edfe851cc860e1c1fc6166ad14184dd315505
Eric Larson <larson.eric.d@gmail.com>
2020-11-17 11:47:30 -0500

    FIX: GLIBC_2.29

--- tools/github_actions_dependencies.sh
+++ tools/github_actions_dependencies.sh
@@ -4,13 +4,13 @@ if [ ! -z "$CONDA_ENV" ]; then
 	pip uninstall -yq mne
 elif [ ! -z "$CONDA_DEPENDENCIES" ]; then
 	conda install -y $CONDA_DEPENDENCIES
-else # pip 3.9
+else # pip 3.9 (missing statsmodels and dipy)
 	python -m pip install --progress-bar off --upgrade pip setuptools wheel
 	pip uninstall -yq numpy
 	pip install --progress-bar off --upgrade --pre --only-binary ":all:" python-dateutil pytz joblib threadpoolctl
 	pip install --progress-bar off --upgrade --pre --only-binary ":all:" -i "https://pypi.anaconda.org/scipy-wheels-nightly/simple" numpy scipy pandas scikit-learn
 	pip install --progress-bar off --upgrade --pre --only-binary ":all:" -f "https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com" matplotlib
-	wget https://osf.io/kej3v/download -O vtk-9.0.20201117-cp39-cp39-linux_x86_64.whl
+	wget -q https://osf.io/kej3v/download -O vtk-9.0.20201117-cp39-cp39-linux_x86_64.whl
 	pip install vtk-9.0.20201117-cp39-cp39-linux_x86_64.whl
 	pip install --progress-bar off https://github.com/pyvista/pyvista/zipball/master
 	pip install --progress-bar off https://github.com/pyvista/pyvistaqt/zipball/master
