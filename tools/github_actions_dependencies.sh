0bd99d6133f91ea4a23f9750a1b8c0628f03f4a4
Eric Larson <larson.eric.d@gmail.com>
2020-11-17 11:22:35 -0500

    FIX: No dipy or statsmodels yet

--- tools/github_actions_dependencies.sh
+++ tools/github_actions_dependencies.sh
@@ -7,7 +7,7 @@ elif [ ! -z "$CONDA_DEPENDENCIES" ]; then
 else # pip 3.9
 	python -m pip install --upgrade pip setuptools wheel
 	pip uninstall -yq numpy
-	pip install --upgrade --pre --only-binary ":all:" -i "https://pypi.anaconda.org/scipy-wheels-nightly/simple" numpy scipy pandas scikit-learn statsmodels dipy
+	pip install --upgrade --pre --only-binary ":all:" -i "https://pypi.anaconda.org/scipy-wheels-nightly/simple" numpy scipy pandas scikit-learn
 	pip install --upgrade --pre --only-binary ":all:" -f "https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com" matplotlib
 	wget https://osf.io/kej3v/download -O vtk-9.0.20201117-cp39-cp39-linux_x86_64.whl
 	pip install vtk-9.0.20201117-cp39-cp39-linux_x86_64.whl
