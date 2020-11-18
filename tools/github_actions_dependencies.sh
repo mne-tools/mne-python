68a4f3e92f94d8472bfaae41a444ace8410e0f49
Eric Larson <larson.eric.d@gmail.com>
2020-11-17 20:05:44 -0500

    FIX: Comments

--- tools/github_actions_dependencies.sh
+++ tools/github_actions_dependencies.sh
@@ -10,6 +10,7 @@ else # pip 3.9 (missing statsmodels and dipy)
 	pip install --progress-bar off --upgrade --pre --only-binary ":all:" python-dateutil pytz joblib threadpoolctl
 	pip install --progress-bar off --upgrade --pre --only-binary ":all:" -i "https://pypi.anaconda.org/scipy-wheels-nightly/simple" numpy scipy pandas scikit-learn
 	pip install --progress-bar off --upgrade --pre --only-binary ":all:" -f "https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com" matplotlib
+	# built using vtk master branch on an Ubuntu 18.04.5 VM and uploaded to OSF:
 	wget -q https://osf.io/kej3v/download -O vtk-9.0.20201117-cp39-cp39-linux_x86_64.whl
 	pip install vtk-9.0.20201117-cp39-cp39-linux_x86_64.whl
 	pip install --progress-bar off https://github.com/pyvista/pyvista/zipball/master
