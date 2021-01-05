c08b5583ee6096fbf433e0894625737638f57337
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-01-05 12:02:38 +0100

    Try another syntax

--- tools/get_testing_version.sh
+++ tools/get_testing_version.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ef
 
-TESTING_VERSION=$(grep -oP "(?<=testing=')[0-9.]*(?=')" mne/datasets/utils.py)
+# TESTING_VERSION=$(grep -oP "(?<=testing=')[0-9.]*(?=')" mne/datasets/utils.py)
+TESTING_VERSION=$(grep -o "testing='[0-9.]\+'" mne/datasets/utils.py | cut -d \' -f 2)
 if [ ! -z $GITHUB_ENV ]; then
 	echo "TESTING_VERSION="$TESTING_VERSION >> $GITHUB_ENV
 else
