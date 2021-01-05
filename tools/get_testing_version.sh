f64e3bd39ed6000b9c412135af214d6f6a0cca43
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-01-05 10:50:47 +0100

    Try again [skip azp][skip circle]

--- tools/get_testing_version.sh
+++ tools/get_testing_version.sh
@@ -2,7 +2,7 @@
 
 TESTING_VERSION=$(grep -oP "(?<=testing=')[0-9.]*(?=')" mne/datasets/utils.py)
 if [ ! -z $GITHUB_ENV ]; then
-	echo $TESTING_VERSION >> $GITHUB_ENV
+	echo "TESTING_VERSION="$TESTING_VERSION >> $GITHUB_ENV
 else
 	echo $TESTING_VERSION
 fi
