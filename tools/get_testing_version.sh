6523a1b5bcf42e30abff47510e9e209d93a8152f
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-01-05 15:27:26 +0100

    Try another syntax [skip github][skip circle]

--- tools/get_testing_version.sh
+++ tools/get_testing_version.sh
@@ -4,6 +4,8 @@
 TESTING_VERSION=$(grep -o "testing='[0-9.]\+'" mne/datasets/utils.py | cut -d \' -f 2)
 if [ ! -z $GITHUB_ENV ]; then
 	echo "TESTING_VERSION="$TESTING_VERSION >> $GITHUB_ENV
+elif [ ! -z $Build.Repository.Name ]; then
+	echo "##vso[task.setvariable variable=testing_version;isOutput=true]$TESTING_VERSION"
 else
 	echo $TESTING_VERSION
 fi
