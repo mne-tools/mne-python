fa8f6cef023ecd01975e818ffa972caf583311da
Daniel McCloy <dan@mccloy.info>
2021-01-07 15:54:02 -0600

    use pooch for EEGBCI

--- mne/datasets/eegbci/eegbci.py
+++ mne/datasets/eegbci/eegbci.py
@@ -1,16 +1,22 @@
-# Author: Martin Billinger <martin.billinger@tugraz.at>
+# Authors: Martin Billinger <martin.billinger@tugraz.at>
+#          Daniel McCloy <dan@mccloy.info>
 # License: BSD Style.
 
 import os
 from os import path as op
+import re
+import pkg_resources
+import pooch
 
 from ..utils import _get_path, _do_path_update
-from ...utils import _fetch_file, _url_to_local_path, verbose
+from ...utils import _fetch_file, _url_to_local_path, verbose, deprecated
 
 
 EEGMI_URL = 'https://physionet.org/files/eegmmidb/1.0.0/'
 
 
+@deprecated('mne.datasets.eegbci.data_path() is deprecated and will be removed'
+            ' in version 0.24. Use mne.datasets.eegbci.load_data() instead.')
 @verbose
 def data_path(url, path=None, force_update=False, update_path=None,
               verbose=None):
@@ -90,8 +96,8 @@ def load_data(subject, runs, path=None, force_update=False, update_path=None,
               base_url=EEGMI_URL, verbose=None):  # noqa: D301
     """Get paths to local copies of EEGBCI dataset files.
 
-    This will fetch data for the EEGBCI dataset [1]_, which is also
-    available at PhysioNet [2]_.
+    This will fetch data for the EEGBCI dataset :footcite:`SchalkEtAl2004`,
+    which is also available at PhysioNet :footcite:`GoldbergerEtAl2000`.
 
     Parameters
     ----------
@@ -147,24 +153,45 @@ def load_data(subject, runs, path=None, force_update=False, update_path=None,
 
     References
     ----------
-    .. [1] Schalk, G., McFarland, D.J., Hinterberger, T., Birbaumer, N.,
-           Wolpaw, J.R. (2004) BCI2000: A General-Purpose Brain-Computer
-           Interface (BCI) System. IEEE TBME 51(6):1034-1043
-    .. [2] Goldberger AL, Amaral LAN, Glass L, Hausdorff JM, Ivanov PCh,
-           Mark RG, Mietus JE, Moody GB, Peng C-K, Stanley HE. (2000)
-           PhysioBank, PhysioToolkit, and PhysioNet: Components of a New
-           Research Resource for Complex Physiologic Signals.
-           Circulation 101(23):e215-e220
-    """  # noqa: E501
+    .. footbibliography::
+    """
     if not hasattr(runs, '__iter__'):
         runs = [runs]
 
+    # get local storage path
+    config_key = 'MNE_DATASETS_EEGBCI_PATH'
+    folder = 'MNE-eegbci-data'
+    name = 'EEGBCI'
+    path = _get_path(path, config_key, name)
+    # extract path parts
+    pattern = r'(?:https?://.*)(files)/(eegmmidb)/(\d+\.\d+\.\d+)/?'
+    match = re.compile(pattern).match(base_url)
+    if match is None:
+        raise ValueError('base_url does not match the expected EEGMI folder '
+                         'structure. Please notify MNE-Python developers.')
+    base_path = op.join(path, folder, *match.groups())
+    # create the download manager
+    fetcher = pooch.create(
+        path=base_path,
+        base_url=base_url,
+        version=None,   # Data versioning is decoupled from MNE-Python version.
+        registry=None,  # Registry is loaded from file, below.
+        retry_if_failed=2  # 2 retries = 3 total attempts
+    )
+    # load the checksum registry
+    registry = pkg_resources.resource_stream(
+        'mne', op.join('data', 'eegbci_checksums.txt'))
+    fetcher.load_registry(registry)
+    # fetch the file(s)
     data_paths = []
-    for r in runs:
-        url = '{u}S{s:03d}/S{s:03d}R{r:02d}.edf'.format(u=base_url,
-                                                        s=subject, r=r)
-        data_paths.extend(data_path(url, path, force_update, update_path))
-
+    for run in runs:
+        file_part = f'S{subject:03d}/S{subject:03d}R{run:02d}.edf'
+        destination = op.join(base_path, file_part)
+        if force_update and op.isfile(destination):
+            os.remove(destination)
+        data_paths.append(fetcher.fetch(file_part))
+        # update path in config if desired
+        _do_path_update(path, update_path, config_key, name)
     return data_paths
 
 
