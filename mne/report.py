44a3c44f2a4723c0aa7c5a62ee9964436496dbe8
Richard Höchenberger <richard.hoechenberger@gmail.com>
2021-01-18 13:02:42 +0100

    Add versionadded [skip azp][skip github]

--- mne/report.py
+++ mne/report.py
@@ -1032,6 +1032,10 @@ class Report(object):
         css : str
             Style definitions to add to the report. The content of this string
             will be embedded between HTML ``<style>`` and ``</style>`` tags.
+
+        Notes
+        -----
+        .. versionadded:: 0.23
         """
         style = f'\n<style type="text/css">\n{css}\n</style>'
         self.include += style
