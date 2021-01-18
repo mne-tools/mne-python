69069d73d9c2e005e1995773640ad7592c70e72f
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-01-18 16:19:05 +0100

    The pragmatic approach

--- mne/viz/_brain/_brain.py
+++ mne/viz/_brain/_brain.py
@@ -2608,25 +2608,43 @@ class Brain(object):
                 not self.separate_canvas:
             canvas = self.mpl_canvas.fig.canvas
             canvas.draw_idle()
-            # In theory, one of these should work:
-            #
-            # trace_img = np.frombuffer(
-            #     canvas.tostring_rgb(), dtype=np.uint8)
-            # trace_img.shape = canvas.get_width_height()[::-1] + (3,)
-            #
-            # or
-            #
-            # trace_img = np.frombuffer(
-            #     canvas.tostring_rgb(), dtype=np.uint8)
-            # size = time_viewer.mpl_canvas.getSize()
-            # trace_img.shape = (size.height(), size.width(), 3)
-            #
-            # But in practice, sometimes the sizes does not match the
-            # renderer tostring_rgb() size. So let's directly use what
-            # matplotlib does in lib/matplotlib/backends/backend_agg.py
-            # before calling tobytes():
-            trace_img = np.asarray(
-                canvas.renderer._renderer).take([0, 1, 2], axis=2)
+            if self.notebook:
+                from io import BytesIO
+                # Here we need to save to a buffer without taking into
+                # account the dpi to keep the size used on creation
+                # of the canvas which is used for the 3D renderer.
+                # For some reason doing
+                # canvas.get_width_height() overestimates the size
+                # of the image while doing fig.savefig('fname.png')
+                # returns the right number of pixels.
+                fig = self.mpl_canvas.fig
+                output = BytesIO()
+                fig.savefig(output, format='raw')
+                output.seek(0)
+                trace_img = np.reshape(
+                    np.frombuffer(output.getvalue(), dtype=np.uint8),
+                    newshape=(-1, img.shape[1], 4))[:, :, :3]
+                output.close()
+            else:
+                # In theory, one of these should work:
+                #
+                # trace_img = np.frombuffer(
+                #     canvas.tostring_rgb(), dtype=np.uint8)
+                # trace_img.shape = canvas.get_width_height()[::-1] + (3,)
+                #
+                # or
+                #
+                # trace_img = np.frombuffer(
+                #     canvas.tostring_rgb(), dtype=np.uint8)
+                # size = time_viewer.mpl_canvas.getSize()
+                # trace_img.shape = (size.height(), size.width(), 3)
+                #
+                # But in practice, sometimes the sizes does not match the
+                # renderer tostring_rgb() size. So let's directly use what
+                # matplotlib does in lib/matplotlib/backends/backend_agg.py
+                # before calling tobytes():
+                trace_img = np.asarray(
+                    canvas.renderer._renderer).take([0, 1, 2], axis=2)
             # need to slice into trace_img because generally it's a bit
             # smaller
             delta = trace_img.shape[1] - img.shape[1]
