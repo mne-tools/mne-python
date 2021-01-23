ae8a466051a43ac929eaf23e02b40c27a6d62e1a
Richard Höchenberger <richard.hoechenberger@gmail.com>
2021-01-23 12:43:13 +0100

    WIP, BUG: Make plot_evoked() not plot GFP for single channel (#8774)
    * Make plot_evoked() not plot GFP for single channel
    
    Fixes #8772
    
    * Better
    
    * Comments
    
    * Docstring
    
    * Docstring
    
    * Fix docs [skip azp][skip github]

--- mne/viz/evoked.py
+++ mne/viz/evoked.py
@@ -473,7 +473,14 @@ def _plot_lines(data, info, picks, fig, axes, spatial_colors, unit, units,
                                 linewidth=0.5)[0])
                     line_list[-1].set_pickradius(3.)
 
-            if gfp:  # 'only' or boolean True
+            if gfp == 'only' and D.shape[0] == 1:
+                # Only a single channel + only GFP -> abort mission!
+                raise ValueError('Cannot calculate GFP with only one data '
+                                 'channel')
+            elif gfp and D.shape[0] == 1:
+                warn('Cannot calculate GFP with only one data channel, '
+                     'not plotting GFP')
+            elif gfp:  # gfp 'only' or boolean True
                 gfp_color = 3 * (0.,) if spatial_colors is True else (0., 1.,
                                                                       0.)
                 this_gfp = np.sqrt((D * D).mean(axis=0))
@@ -674,8 +681,9 @@ def plot_evoked(evoked, picks=None, exclude='bads', unit=True, show=True,
         the same length as the number of channel types. If instance of
         Axes, there must be only one channel type plotted.
     gfp : bool | 'only'
-        Plot GFP in green if True or "only". If "only", then the individual
-        channel traces will not be shown.
+        Plot the GFP and the traces for all channels if ``True``; or only the
+        GFP if ``'only'``, hiding the channel traces. The color of the GFP
+        trace will be green if ``spatial_colors=False``, and black otherwise.
     window_title : str | None
         The title to put at the top of the figure.
     spatial_colors : bool
