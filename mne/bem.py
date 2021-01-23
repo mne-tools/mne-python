4f28a11d8fc835e7c57c25bf5b8f5be71d6629bd
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2021-01-23 18:59:29 +0100

    expanduser

--- mne/bem.py
+++ mne/bem.py
@@ -1633,6 +1633,7 @@ def _prepare_env(subject, subjects_dir):
 
     subjects_dir = get_subjects_dir(subjects_dir, raise_error=True)
     subjects_dir = op.abspath(subjects_dir)  # force use of an absolute path
+    subjects_dir = op.expanduser(subjects_dir)
     if not op.isdir(subjects_dir):
         raise RuntimeError('Could not find the MRI data directory "%s"'
                            % subjects_dir)
