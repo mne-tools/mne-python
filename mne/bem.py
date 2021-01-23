885abdf70a0b46ad76a251433ac80884a43b9b06
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2021-01-23 12:45:29 +0100

    enforce abs path

--- mne/bem.py
+++ mne/bem.py
@@ -1632,6 +1632,7 @@ def _prepare_env(subject, subjects_dir):
     _validate_type(subject, "str")
 
     subjects_dir = get_subjects_dir(subjects_dir, raise_error=True)
+    subjects_dir = op.abspath(subjects_dir)  # force use of an absolute path
     if not op.isdir(subjects_dir):
         raise RuntimeError('Could not find the MRI data directory "%s"'
                            % subjects_dir)
