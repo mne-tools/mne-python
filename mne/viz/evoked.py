8ac355103558797226981f2748520eaaa0d70dbb
Richard Höchenberger <richard.hoechenberger@gmail.com>
2021-01-26 09:35:35 +0100

    use np.linalg.norm
    Co-authored-by: Alexandre Gramfort <alexandre.gramfort@m4x.org>

--- mne/viz/evoked.py
+++ mne/viz/evoked.py
@@ -479,7 +479,7 @@ def _plot_lines(data, info, picks, fig, axes, spatial_colors, unit, units,
                     label = 'GFP'
                 elif gfp in ['power', 'power-only']:
                     # RMS
-                    this_gfp = np.sqrt((D ** 2).mean(axis=0))
+                    this_gfp = np.linalg.norm(D, axis=0)
                     label = 'RMS'
 
                 gfp_color = 3 * (0.,) if spatial_colors is True else (0., 1.,
