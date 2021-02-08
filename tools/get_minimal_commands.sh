d8ce89703471bec29a43d3413ec69587625720b5
Eric Larson <larson.eric.d@gmail.com>
2021-02-08 16:31:27 -0500

    MAINT: Update CIs (#8847)
    * MAINT: Update CIs
    
    * FIX: Paths
    
    * FIX: For real
    
    * FIX: Missed
    
    * FIX: Unify
    
    * FIX: Try again

--- tools/get_minimal_commands.sh
+++ tools/get_minimal_commands.sh
@@ -4,6 +4,14 @@ if [ "${DEPS}" != "minimal" ]; then
 	pushd ~ > /dev/null
 	export MNE_ROOT="${PWD}/minimal_cmds"
 	export PATH=${MNE_ROOT}/bin:$PATH
+	if [ "${GITHUB_ACTIONS}" == "true" ]; then
+		echo "MNE_ROOT=${MNE_ROOT}" >> $GITHUB_ENV;
+		echo "${MNE_ROOT}/bin" >> $GITHUB_PATH;
+	fi;
+	if [ "${AZURE_CI}" == "true" ]; then
+		echo "##vso[task.setvariable variable=MNE_ROOT]${MNE_ROOT}"
+		echo "##vso[task.setvariable variable=PATH]${PATH}";
+	fi;
 	if [ "${CI_OS_NAME}" != "osx" ]; then
 		if [ ! -d "${PWD}/minimal_cmds" ]; then
 			curl -L https://osf.io/g7dzs/download | tar xz
@@ -11,12 +19,32 @@ if [ "${DEPS}" != "minimal" ]; then
 		export LD_LIBRARY_PATH=${MNE_ROOT}/lib:$LD_LIBRARY_PATH
 		export NEUROMAG2FT_ROOT="${PWD}/minimal_cmds/bin"
 		export FREESURFER_HOME="${MNE_ROOT}"
+		if [ "${GITHUB_ACTIONS}" == "true" ]; then
+			echo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" >> $GITHUB_ENV;
+			echo "NEUROMAG2FT_ROOT=${NEUROMAG2FT_ROOT}" >> $GITHUB_ENV;
+			echo "FREESURFER_HOME=${FREESURFER_HOME}" >> $GITHUB_ENV;
+		fi;
+		if [ "${AZURE_CI}" == "true" ]; then
+			echo "##vso[task.setvariable variable=LD_LIBRARY_PATH]${LD_LIBRARY_PATH}"
+			echo "##vso[task.setvariable variable=NEUROMAG2FT_ROOT]${NEUROMAG2FT_ROOT}"
+			echo "##vso[task.setvariable variable=FREESURFER_HOME]${FREESURFER_HOME}"
+		fi;
 	else
 		if [ ! -d "${PWD}/minimal_cmds" ]; then
 			curl -L https://osf.io/rjcz4/download | tar xz
 		fi;
 		export DYLD_LIBRARY_PATH=${MNE_ROOT}/lib:$DYLD_LIBRARY_PATH
+		if [ "${GITHUB_ACTIONS}" == "true" ]; then
+			echo "DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}" >> $GITHUB_ENV;
+			wget https://dl.bintray.com/xquartz/downloads/XQuartz-2.7.11.dmg
+			sudo hdiutil attach XQuartz-2.7.11.dmg
+			sudo installer -package /Volumes/XQuartz-2.7.11/XQuartz.pkg -target /
+			sudo ln -s /opt/X11 /usr/X11
+		fi;
+		if [ "${AZURE_CI}" == "true" ]; then
+			echo "##vso[task.setvariable variable=DYLD_LIBRARY_PATH]${DYLD_LIBRARY_PATH}"
+		fi;
 	fi
 	popd > /dev/null
-	mne_surf2bem --version
+	mne_process_raw --version
 fi
