aa07ce40827e4556666b66b30e3004c64f47d042
Daniel McCloy <dan@mccloy.info>
2021-02-12 17:33:31 -0600

    workaround both theme bugs (anchor scrolling & navbar overflow)

--- doc/_static/scrollfix.js
+++ doc/_static/scrollfix.js
@@ -1,18 +1,31 @@
-/* this prevents in-text anchors from being hidden under the navbar when the */
-/* page scrolls to them (citation/equation backlinks are worst affected).    */
-/* adapted from https://github.com/copyleft-org/copyleft-guide/commit/476a42bf0d737e13a561dbaf6f4e1e91a333e80d */
-$(document).ready(function() {
+function anchorScrollFix() {
+    // this prevents anchors from being hidden under the navbar when the page
+    // scrolls to them (citation/equation backlinks are worst affected).
+    // adapted from https://github.com/copyleft-org/copyleft-guide/commit/476a42bf0d737e13a561dbaf6f4e1e91a333e80d
     if ($(".navbar.fixed-top").length > 0) {
         var navHeight = $(".navbar").height();
         var shiftWindow = function() {
-            var ourURL = document.URL;
-            if ( (ourURL.search(/#[a-z]+\d\d\d\d[a-z]?$/) > 0) ||  // footbib item (bibtex key)
-                 (ourURL.search("#id") > 0) ||                     // footbib backreference
-                 (ourURL.search("#equation") > 0) ) {              // numbered equation
+            if (document.URL.search(/#[a-z]+/) > 0) {
                 scrollBy(0, -navHeight - 12);
             };
         };
         if (location.hash) shiftWindow();
         window.addEventListener("hashchange", shiftWindow);
     };
+};
+
+function navbarShadow() {
+    var addRemoveShadow = function() {
+        if ($('.navbar-toggler').css('display') == 'none') {
+            $('#navbar-menu').removeClass('shadow-lg');
+        } else {
+            $('#navbar-menu').addClass('shadow-lg');
+        };
+    };
+    window.addEventListener('resize', addRemoveShadow);
+};
+
+$(document).ready( () => {
+    anchorScrollFix();
+    navbarShadow();
 });
