13e90cd2d9d9a1a20472621384699c3d8f06879b
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-02-24 12:18:52 +0100

    make pydocstyle

--- mne/viz/_brain/float_slider.py
+++ mne/viz/_brain/float_slider.py
@@ -3,9 +3,12 @@ from PyQt5.QtWidgets import QSlider
 
 
 class QFloatSlider(QSlider):
+    """Slider that handles float values."""
+
     valueChanged = pyqtSignal(float)
 
     def __init__(self, ori, parent=None):
+        """Initialize the slider."""
         super().__init__(ori, parent)
         self._precision = 10000
         super().valueChanged.connect(self._convert)
@@ -14,19 +17,25 @@ class QFloatSlider(QSlider):
         self.valueChanged.emit(value / self._precision)
 
     def minimum(self):
+        """Get the minimum."""
         return super().minimum() / self._precision
 
     def setMinimum(self, value):
+        """Set the minimum."""
         super().setMinimum(int(value * self._precision))
 
     def maximum(self):
+        """Get the maximum."""
         return super().maximum() / self._precision
 
     def setMaximum(self, value):
+        """Set the maximum."""
         super().setMaximum(int(value * self._precision))
 
     def value(self):
+        """Get the current value."""
         return super().value() / self._precision
 
     def setValue(self, value):
+        """Set the current value."""
         super().setValue(int(value * self._precision))
