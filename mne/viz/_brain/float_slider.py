2cd038c9f24b8efee0d5ba09bf656c823f6c6eb2
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-02-24 12:14:04 +0100

    Refactor

--- mne/viz/_brain/float_slider.py
+++ mne/viz/_brain/float_slider.py
@@ -7,46 +7,26 @@ class QFloatSlider(QSlider):
 
     def __init__(self, ori, parent=None):
         super().__init__(ori, parent)
-        self._float_min = 0.0
-        self._float_max = 1.0
-        self._float_value = 0.5
         self._precision = 10000
-        self._int_min = int(self._float_min * self._precision)
-        self._int_max = int(self._float_min * self._precision)
-        self._int_value = int(self._float_value * self._precision)
         super().valueChanged.connect(self._convert)
 
     def _convert(self, value):
-        int_value = value
-        float_value = int_value / self._precision
-        self.valueChanged.emit(float_value)
+        self.valueChanged.emit(value / self._precision)
 
     def minimum(self):
-        self._int_min = super.minimum()
-        self._float_min = self._int_min / self._precision
-        return self._float_min
+        return super().minimum() / self._precision
 
     def setMinimum(self, value):
-        self._float_min = value
-        self._int_min = int(self._float_min * self._precision)
-        super().setMinimum(self._int_min)
+        super().setMinimum(int(value * self._precision))
 
     def maximum(self):
-        self._int_max = super.maximum()
-        self._float_max = self._int_max / self._precision
-        return self._float_max
+        return super().maximum() / self._precision
 
     def setMaximum(self, value):
-        self._float_max = value
-        self._int_max = int(self._float_max * self._precision)
-        super().setMaximum(self._int_max)
+        super().setMaximum(int(value * self._precision))
 
     def value(self):
-        self._int_value = super.value()
-        self._float_value = self._int_value / self._precision
-        return self._float_value
+        return super().value() / self._precision
 
     def setValue(self, value):
-        self._float_value = value
-        self._int_value = int(self._float_value * self._precision)
-        super().setValue(self._int_value)
+        super().setValue(int(value * self._precision))
