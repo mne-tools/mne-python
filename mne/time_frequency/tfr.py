a3306e6e09a35c73cbb1b13dc002f1613ca47db7
Adam Li <adam2392@gmail.com>
2021-02-25 10:28:55 -0500

    Update mne/time_frequency/tfr.py
    Co-authored-by: Alexandre Gramfort <alexandre.gramfort@m4x.org>

--- mne/time_frequency/tfr.py
+++ mne/time_frequency/tfr.py
@@ -883,7 +883,7 @@ def tfr_multitaper(inst, freqs, n_cycles, time_bandwidth=4.0,
     -----
     .. versionadded:: 0.9.0
 
-    If the function is run on a ``Raw`` object, then it one should be
+    If the function is run on a ``Raw`` object, then it should be
     wary that the resulting ``AverageTFR`` object might be very large.
     One can reduce RAM usage via the ``decim`` argument. Alternatively,
     if one is trying to compute spectral analysis of only short time
