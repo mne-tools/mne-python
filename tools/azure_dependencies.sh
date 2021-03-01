d42421d06b35ee8deed949aa9912bffd5f09c970
Eric Larson <larson.eric.d@gmail.com>
2021-03-01 13:11:54 -0500

    FIX: Missed one

--- tools/azure_dependencies.sh
+++ tools/azure_dependencies.sh
@@ -14,7 +14,7 @@ elif [ "${TEST_MODE}" == "pip-pre" ]; then
 	python -m pip install --upgrade pip setuptools
 	python -m pip install --use-deprecated=legacy-resolver --upgrade --pre --only-binary ":all:" -i "https://pypi.anaconda.org/scipy-wheels-nightly/simple" numpy
 	python -m pip install --use-deprecated=legacy-resolver --upgrade --pre --only-binary ":all:" -f "https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com" scipy pandas scikit-learn h5py Pillow
-	python -m pip install --use-deprecated=legacy-resolver --upgrade --pre --only-binary matplotlib
+	python -m pip install --use-deprecated=legacy-resolver --upgrade --pre --only-binary ":all:" matplotlib
 	python -m pip install --upgrade --only-binary vtk vtk;
 	python -m pip install https://github.com/pyvista/pyvista/zipball/master
 	python -m pip install https://github.com/pyvista/pyvistaqt/zipball/master
