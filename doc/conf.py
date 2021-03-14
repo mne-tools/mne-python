f8dc2d2c506face3168e23c63cb9fedbc7a30ca3
Eric Larson <larson.eric.d@gmail.com>
2021-03-14 16:57:26 -0400

    FIX: Category

--- doc/conf.py
+++ doc/conf.py
@@ -709,7 +709,6 @@ def reset_warnings(gallery_conf, fname):
                 r'Converting `np\.character` to a dtype is deprecated',  # vtk
                 r'sphinx\.util\.smartypants is deprecated',
                 'is a deprecated alias for the builtin',  # NumPy
-                r'Fetchers from the nilearn\.datasets',  # nilearn
                 ):
         warnings.filterwarnings(  # deal with other modules having bad imports
             'ignore', message=".*%s.*" % key, category=DeprecationWarning)
@@ -721,6 +720,8 @@ def reset_warnings(gallery_conf, fname):
         category=FutureWarning)
     warnings.filterwarnings(  # nilearn
         'ignore', message=r'The sklearn.* module is.*', category=FutureWarning)
+    warnings.filterwarnings(  # nilearn
+        'ignore', message=r'Fetchers from the nilea.*', category=FutureWarning)
     warnings.filterwarnings(  # deal with other modules having bad imports
         'ignore', message=".*ufunc size changed.*", category=RuntimeWarning)
     warnings.filterwarnings(  # realtime
