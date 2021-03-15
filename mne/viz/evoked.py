a069b8fcf77436162ff2370ff91644e39abc4b75
Maggie Clarke <mdclarke@uw.edu>
2021-03-15 12:06:36 -0700

    Update mne/viz/evoked.py
    Co-authored-by: Stefan Appelhoff <stefan.appelhoff@mailbox.org>

--- mne/viz/evoked.py
+++ mne/viz/evoked.py
@@ -1036,7 +1036,7 @@ def plot_evoked_white(evoked, noise_cov, show=True, rank=None, time_unit='s',
     """Plot whitened evoked response.
 
     Plots the whitened evoked response and the whitened GFP as described in
-    :footcite:`EngemannGramfort2015'. This function is especially useful for
+    :footcite:`EngemannGramfort2015`. This function is especially useful for
     investigating noise covariance properties to determine if data are
     properly whitened (e.g., achieving expected values in line with model
     assumptions, see Notes below).
