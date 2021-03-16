f247531920dfbb0ac86990b8a01563b62ab1eb65
Silvia Cotroneo <silvia.cotroneo@aalto.fi>
2021-03-16 13:27:23 +0200

    Minor fixes

--- mne/epochs.py
+++ mne/epochs.py
@@ -3668,8 +3668,8 @@ def average_movements(epochs, head_pos=None, orig_sfreq=None, picks=None,
 
 @verbose
 def make_fixed_length_epochs(raw, duration=1., preload=False,
-                             reject_by_annotation=True, proj=True,
-                             verbose=None, overlap=0.):
+                             reject_by_annotation=True, proj=True, overlap=0.,
+                             verbose=None):
     """Divide continuous raw data into equal-sized consecutive epochs.
 
     Parameters
@@ -3681,6 +3681,7 @@ def make_fixed_length_epochs(raw, duration=1., preload=False,
     overlap : float
         The overlap between epochs. Must be ``0 <= overlap < duration``.
         Default is 0.
+
         .. versionadded:: 0.23
     %(preload)s
     %(reject_by_annotation_epochs)s
