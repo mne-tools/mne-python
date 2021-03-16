5560036ba21b68aca13921d0d92be2c284d5e52d
Catalina M. Galvan <cmgalvan@sinc.unl.edu.ar>
2021-03-16 11:56:43 -0300

    convert references in mne/datasets/sleep_physionet/age.py to use footcite / footbibliography

--- mne/datasets/sleep_physionet/age.py
+++ mne/datasets/sleep_physionet/age.py
@@ -22,8 +22,7 @@ def fetch_data(subjects, recording=(1, 2), path=None, force_update=False,
     """Get paths to local copies of PhysioNet Polysomnography dataset files.
 
     This will fetch data from the publicly available subjects from PhysioNet's
-    study of age effects on sleep in healthy subjects 
-    :footcite:`MourtazaevEtAl1995,GoldbergerEtAl2000`. This corresponds to a 
+    study of age effects on sleep in healthy subjects :footcite:`MourtazaevEtAl1995,GoldbergerEtAl2000`. This corresponds to a 
     subset of  153 recordings from 37 males and 41 females that were 25-101 
     years old at the time of the recordings. There are two night recordings 
     per subject except for subjects 13, 36 and 52 which have one record 
