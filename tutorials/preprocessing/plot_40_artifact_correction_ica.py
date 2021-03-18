c2d86d1d752123affa32a2eb10485cd24bc688b4
Giorgio_M <giorgio.marinato@unitn.it>
2021-03-18 11:29:06 +0100

    fix issue #9003 combine ICA example into the ICA tutorial
    Relevant information to combine the code:
    - reuse the filt_raw object instead of importing again the raw fif
    - change the number of components in accordance of the ICA on raw
    - kept the band-pass filter like in the example

--- tutorials/preprocessing/plot_40_artifact_correction_ica.py
+++ tutorials/preprocessing/plot_40_artifact_correction_ica.py
@@ -437,7 +437,7 @@ new_ica.plot_sources(ecg_evoked)
 # before accepting them.
 
 # clean up memory before moving on
-del raw, filt_raw, ica, new_ica
+del raw, ica, new_ica
 
 ###############################################################################
 # Selecting ICA components using template matching
@@ -586,29 +586,18 @@ print(template_eog_component)
 # .. _`this EEGLAB tutorial`: https://labeling.ucsd.edu/tutorial/labels
 
 
-"""
-================================
-Compute ICA components on epochs
-================================
-
-ICA is fit to MEG raw data.
-We assume that the non-stationary EOG artifacts have already been removed.
-The sources matching the ECG are automatically found and displayed.
-
-.. note:: This example does quite a bit of processing, so even on a
-          fast machine it can take about a minute to complete.
-"""
-# Authors: Denis Engemann <denis.engemann@gmail.com>
-#
-# License: BSD (3-clause)
-
-import mne
-from mne.preprocessing import ICA, create_ecg_epochs
-from mne.datasets import sample
-
-print(__doc__)
-
 ###############################################################################
+# Example: Compute ICA components on epochs
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# ICA is now fit to MEG epochs data instead of the raw data.
+# We assume that the non-stationary EOG artifacts have already been removed.
+# The sources matching the ECG are automatically found and displayed.
+#
+# .. note::
+#     This example does quite a bit of processing, so even on a
+#     fast machine it can take about a minute to complete.
+#
 # Read and preprocess the data. Preprocessing consists of:
 #
 # - MEG channel selection
@@ -616,27 +605,23 @@ print(__doc__)
 # - epoching -0.2 to 0.5 seconds with respect to events
 # - rejection based on peak-to-peak amplitude
 
-data_path = sample.data_path()
-raw_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
-
-raw = mne.io.read_raw_fif(raw_fname)
-raw.pick_types(meg=True, eeg=False, exclude='bads', stim=True).load_data()
-raw.filter(1, 30, fir_design='firwin')
+filt_raw.pick_types(meg=True, eeg=False, exclude='bads', stim=True).load_data()
+filt_raw.filter(1, 30, fir_design='firwin')
 
 # peak-to-peak amplitude rejection parameters
 reject = dict(grad=4000e-13, mag=4e-12)
 # longer + more epochs for more artifact exposure
-events = mne.find_events(raw, stim_channel='STI 014')
-epochs = mne.Epochs(raw, events, event_id=None, tmin=-0.2, tmax=0.5,
+events = mne.find_events(filt_raw, stim_channel='STI 014')
+epochs = mne.Epochs(filt_raw, events, event_id=None, tmin=-0.2, tmax=0.5,
                     reject=reject)
 
 ###############################################################################
 # Fit ICA model using the FastICA algorithm, detect and plot components
 # explaining ECG artifacts.
 
-ica = ICA(n_components=0.95, method='fastica').fit(epochs)
+ica = ICA(n_components=15, method='fastica').fit(epochs)
 
-ecg_epochs = create_ecg_epochs(raw, tmin=-.5, tmax=.5)
+ecg_epochs = create_ecg_epochs(filt_raw, tmin=-.5, tmax=.5)
 ecg_inds, scores = ica.find_bads_ecg(ecg_epochs, threshold='auto')
 
 ica.plot_components(ecg_inds)
@@ -647,11 +632,12 @@ ica.plot_properties(epochs, picks=ecg_inds)
 
 ###############################################################################
 # Plot the estimated source of detected ECG related components:
-ica.plot_sources(raw, picks=ecg_inds)
+ica.plot_sources(filt_raw, picks=ecg_inds)
 
 
 
 ###############################################################################
 # References
-# ----------
+# ^^^^^^^^^^
+#
 # .. footbibliography::
