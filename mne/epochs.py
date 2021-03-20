523413f911e23bfef98ca92e3cfa00c80501f94d
anaradanovic <79697247+anaradanovic@users.noreply.github.com>
2021-03-20 01:13:03 +0100

    DOC: Add HEDs to glossary and reference in tutorial and epochs.py  #8974. (#9097)
    * DOC: Add HED to glossary, and reference #8974.
    
    * DOC: Add HEDs to glossary and references issue #8974.
    
    * Add HEDs to glossary and reference in tutorial and epochs.py #8974
    
    * Apply suggestions from code review
    
    Co-authored-by: Britta Westner <britta.wstnr@gmail.com>
    
    * whitespace error
    
    Co-authored-by: Daniel McCloy <dan@mccloy.info>
    Co-authored-by: Britta Westner <britta.wstnr@gmail.com>

--- mne/epochs.py
+++ mne/epochs.py
@@ -2161,9 +2161,10 @@ def make_metadata(events, event_id, tmin, tmax, sfreq,
         (default), rows are created for **all** event types present in
         ``event_id``.
     keep_first : str | list of str | None
-        Specify subsets of hierarchical event descriptors (HEDs) matching
-        events of which  the **first occurrence** within each
-        time window shall be stored in addition to the original events.
+        Specify subsets of :term:`hierarchical event descriptors` (HEDs,
+        inspired by :footcite:`BigdelyShamloEtAl2013`) matching events of which
+        the **first occurrence** within each time window shall be stored in
+        addition to the original events.
 
         .. note::
            There is currently no way to retain **all** occurrences of a
@@ -2178,7 +2179,7 @@ def make_metadata(events, event_id, tmin, tmax, sfreq,
         case, you can pass ``keep_first='response'``. This will add two new
         columns to the metadata: ``response``, indicating at what **time** the
         event  occurred, relative to the time-locked event; and
-        ``first_response``, stating which **type** (``'left'`` or``'right'``)
+        ``first_response``, stating which **type** (``'left'`` or ``'right'``)
         of event occurred.
         To match specific subsets of HEDs describing different sets of events,
         pass a list of these subsets, e.g.
@@ -2237,6 +2238,10 @@ def make_metadata(events, event_id, tmin, tmax, sfreq,
     before or after an epoch, e.g. during the inter-trial interval.
 
     .. versionadded:: 0.23
+
+    References
+    ----------
+    .. footbibliography::
     """
     from .utils.mixin import _hid_match
     pd = _check_pandas_installed()
