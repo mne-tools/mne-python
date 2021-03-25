19db73552fda221499f652b7c7b30335fccae045
Sumalyo Datta <39642083+Sumalyo@users.noreply.github.com>
2021-03-25 20:58:32 +0530

    MRG: Support µV in EDF files (in addition to uV) (#9187)
    * edf unit mismatch warning given as #9186
    
    * warning user if unit is not recognized
    
    * created elif branches and added warning messages
    
    * adding support for symbols denoting microvolt
    
    * Updated Changelog
    
    * Corrected Flake Warnings
    
    * added name
    
    * W391 Flake Issue
    
    * Update doc/changes/latest.inc
    
    Co-authored-by: Clemens Brunner <clemens.brunner@gmail.com>
    
    Co-authored-by: Clemens Brunner <clemens.brunner@gmail.com>

--- mne/io/edf/edf.py
+++ mne/io/edf/edf.py
@@ -613,7 +613,7 @@ def _read_edf_header(fname, exclude):
         for i, unit in enumerate(units):
             if i in exclude:
                 continue
-            if unit == 'uV':
+            if unit in ('uV', 'µV', 'µV'):
                 edf_info['units'].append(1e-6)
             elif unit == 'mV':
                 edf_info['units'].append(1e-3)
