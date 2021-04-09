898061a81148480962923177d6e6e05b40b3b4fc
eioe <klotzsche@cbs.mpg.de>
2021-04-09 15:24:11 +0200

    extend ERDS example with df method

--- examples/time_frequency/plot_time_frequency_erds.py
+++ examples/time_frequency/plot_time_frequency_erds.py
@@ -30,11 +30,13 @@ References
 .. footbibliography::
 """
 # Authors: Clemens Brunner <clemens.brunner@gmail.com>
+# Felix Klotzsche <klotzsche@cbs.mpg.de>
 #
 # License: BSD (3-clause)
 
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import mne
 from mne.datasets import eegbci
 from mne.io import concatenate_raws, read_raw_edf
@@ -43,7 +45,7 @@ from mne.stats import permutation_cluster_1samp_test as pcluster_test
 from mne.viz.utils import center_cmap
 
 
-# load and preprocess data ####################################################
+# load and preprocess data ###################################################
 subject = 1  # use data from subject 1
 runs = [6, 10, 14]  # use only hand and feet motor imagery runs
 
@@ -57,14 +59,14 @@ events, _ = mne.events_from_annotations(raw, event_id=dict(T1=2, T2=3))
 
 picks = mne.pick_channels(raw.info["ch_names"], ["C3", "Cz", "C4"])
 
-# epoch data ##################################################################
+# epoch data #################################################################
 tmin, tmax = -1, 4  # define epochs around events (in s)
 event_ids = dict(hands=2, feet=3)  # map event IDs to tasks
 
 epochs = mne.Epochs(raw, events, event_ids, tmin - 0.5, tmax + 0.5,
                     picks=picks, baseline=None, preload=True)
 
-# compute ERDS maps ###########################################################
+# compute ERDS maps ##########################################################
 freqs = np.arange(2, 36, 1)  # frequencies from 2-35Hz
 n_cycles = freqs  # use constant t/f resolution
 vmin, vmax = -1, 1.5  # set min and max ERDS values in plot
@@ -82,7 +84,7 @@ tfr.apply_baseline(baseline, mode="percent")
 for event in event_ids:
     # select desired epochs for visualization
     tfr_ev = tfr[event]
-    fig, axes = plt.subplots(1, 4, figsize=(12, 4),
+    fig, axes = plt.subplots(1, 4,  # figsize=(12, 4),
                              gridspec_kw={"width_ratios": [10, 10, 10, 1]})
     for ch, ax in enumerate(axes[:-1]):  # for each channel
         # positive clusters
@@ -111,3 +113,85 @@ for event in event_ids:
     fig.colorbar(axes[0].images[-1], cax=axes[-1])
     fig.suptitle("ERDS ({})".format(event))
     fig.show()
+
+##############################################################################
+# Similar as for :class:`~mne.Epochs` objects, we can also export data from
+# :class:`~mne.time_frequency.EpochsTFR` and
+# `~mne.time_frequency.AverageTFR` objects to a
+# :class:`Pandas DataFrame <pandas.DataFrame>`:
+
+df = tfr.to_data_frame()
+df.time = df.time / 1000  # time to s
+df.head()
+
+##############################################################################
+# This allows us to use additional plotting functions like
+# :func:`seaborn.lineplot` to easily plot confidence bands:
+
+freq_bands = {'delta': (0.5, 4), 
+              'theta': (5, 7),
+              'alpha': (8, 14),
+              'beta': (15, 35)}
+fig, axes = plt.subplots(4, 3, sharey=True, sharex=True)
+for f_idx, f_band in enumerate(['beta', 'alpha', 'theta', 'delta']):
+    data = df.loc[(df.freq >= freq_bands[f_band][0]) &
+                  (df.freq <= freq_bands[f_band][1])]
+    for ch_idx, ch in enumerate(["C3", "Cz", "C4"]):
+        g = sns.lineplot(data=data.reset_index(),
+                         x='time', y=ch, hue='condition',
+                         n_boot=10,  # only few repetitions for speed
+                         ax=axes[f_idx, ch_idx])
+        if f_idx == 0:
+            g.set_title(ch)
+        g.axhline(0, color='black', linestyle='dashed', linewidth=0.5,
+                  alpha=0.5)
+        g.axvline(0, color='black', linestyle='dashed', linewidth=0.5,
+                  alpha=0.5)
+        g.set(ylim=(-0.5, 1.5))
+        g.set_ylabel(f_band + '\n(%)', rotation=0, labelpad=20)
+        g.set_xlabel('Time (s)')
+        g.get_legend().remove()
+fig.subplots_adjust(top=0.9, left=0.1, right=0.9, bottom=0.2)
+fig.align_ylabels()
+handles, labels = g.get_legend_handles_labels()
+plt.figlegend(handles=reversed(handles), labels=reversed(labels),
+              loc='lower center', bbox_to_anchor=(0.1, 0.01, 0.9, 0.01),
+              ncol=2, title=None)
+plt.show()
+
+
+##############################################################################
+# Having the data in form of a DataFrame also facilitates subsetting,
+# grouping, and other transforms.
+# Here, we use seaborn to plot average ERDS in the motor-imaginery interval
+# as a function of frequency band and imagery condition:
+
+fbands_p = [((df['freq'] >= freq_bands[f][0]) &
+             (df['freq'] <= freq_bands[f][1])) for f in freq_bands]
+df['freq_band'] = np.select(fbands_p, freq_bands.keys())
+dat = (df.query('time > 1')
+       .filter(regex=r'condition|epoch|C3|Cz|C4|freq_band')
+       .groupby(['condition', 'epoch', 'freq_band'])
+       .mean()
+       .reset_index()
+       .melt(id_vars=['condition', 'epoch', 'freq_band'],
+             var_name='channel',
+             value_name='mean power'))
+fig, axes = plt.subplots(1, 2, sharey=True)
+for cond, ax in zip(['hands', 'feet'], axes.flatten()):
+    axo = sns.violinplot(x='channel', y='mean power', hue='freq_band',
+                         data=dat[dat['condition'] == cond], palette='deep',
+                         saturation=1, ylab='ERDS',
+                         hue_order=['delta', 'theta', 'alpha', 'beta'],
+                         ax=ax)
+    axo.set_ylabel('ERDS (%)')
+    axo.set_xlabel('')
+    axo.set_title(cond)
+    axo.get_legend().remove()
+    axo.axhline(0, color='black', linestyle='dashed', linewidth=0.5,
+                alpha=0.5)
+fig.subplots_adjust(top=0.9, left=0.1, right=0.9, bottom=0.15)
+handles, labels = axo.get_legend_handles_labels()
+plt.figlegend(handles=handles, labels=labels, loc='lower center',
+              bbox_to_anchor=(0.1, 0.01, 0.9, 0.01), ncol=4, title=None)
+plt.show()
