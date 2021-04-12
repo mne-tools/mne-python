6bb965f2ee5add276073d1b7efa0c3788ab81244
Ram Pari <ramsbam@gmail.com>
2021-04-12 20:44:57 +0530

    Update relevant docstring in evoked

--- mne/viz/evoked.py
+++ mne/viz/evoked.py
@@ -802,7 +802,7 @@ def plot_evoked_topo(evoked, layout=None, layout_scale=0.945, color=None,
         determines the upper and lower subplot limits. e.g.
         ylim = dict(eeg=[-20, 20]). Valid keys are eeg, mag, grad, misc.
         If None, the ylim parameter for each channel is determined by
-        the maximum absolute peak.
+        the minimum and maximum peak.
     scalings : dict | None
         The scalings of the channel types to be applied for plotting. If None,`
         defaults to ``dict(eeg=1e6, grad=1e13, mag=1e15)``.
