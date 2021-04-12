eeffad798dfd74b0c6ff4f675d2c8919da9a69ca
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2021-04-12 21:54:52 +0200

    ENH+DOC: read_raw_nicolet raise informative message when passing header fname

--- mne/io/nicolet/tests/test_nicolet.py
+++ mne/io/nicolet/tests/test_nicolet.py
@@ -10,15 +10,24 @@ from mne.utils import run_tests_if_main
 from mne.io import read_raw_nicolet
 from mne.io.tests.test_raw import _test_raw_reader
 
+import pytest
+
 FILE = inspect.getfile(inspect.currentframe())
 base_dir = op.join(op.dirname(op.abspath(FILE)), 'data')
-fname = op.join(base_dir, 'test_nicolet_raw.data')
+fname_data = op.join(base_dir, 'test_nicolet_raw.data')
+fname_head = op.join(base_dir, 'test_nicolet_raw.head')
 
 
 def test_data():
     """Test reading raw nicolet files."""
-    _test_raw_reader(read_raw_nicolet, input_fname=fname, ch_type='eeg',
+    _test_raw_reader(read_raw_nicolet, input_fname=fname_data, ch_type='eeg',
                      ecg='auto', eog='auto', emg='auto', misc=['PHO'])
+
+    with pytest.raises(ValueError,
+                       match='File name should end with .data not ".head".'):
+        _test_raw_reader(read_raw_nicolet, input_fname=fname_head,
+                         ch_type='eeg', ecg='auto', eog='auto', emg='auto',
+                         misc=['PHO'])
 
 
 run_tests_if_main()
