5ad66fb1665b203b2994df52294a285b011fd15f
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-04-16 10:53:26 +0200

    Add comment hints

--- mne/viz/_brain/_brain.py
+++ mne/viz/_brain/_brain.py
@@ -1692,8 +1692,10 @@ class Brain(object):
         # Remove the default key binding
         if getattr(self, "iren", None) is not None:
             try:
+                # pyvista<=0.30.0
                 self.plotter._key_press_event_callbacks.clear()
             except AttributeError:
+                # pyvista>=0.30.0
                 self.plotter.iren.clear_key_event_callbacks()
 
     def _clear_widgets(self):
