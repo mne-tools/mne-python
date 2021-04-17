e30db1657416d8456c500c60066072ed336bbc9c
Daniel McCloy <dan@mccloy.info>
2021-04-17 11:48:26 -0500

    overhaul in-place processing tutorial

--- tutorials/intro/15_inplace.py
+++ tutorials/intro/15_inplace.py
@@ -1,78 +1,101 @@
 """
-.. _tut_modifying_data_inplace:
+.. _tut-inplace:
 
 Modifying data in-place
 =======================
 
-It is often necessary to modify data once you have loaded it into memory.
-Common examples of this are signal processing, feature extraction, and data
-cleaning. Some functionality is pre-built into MNE-python, though it is also
-possible to apply an arbitrary function to the data.
+Many of MNE-Python's data objects (`~mne.io.Raw`, `~mne.Epochs`, `~mne.Evoked`,
+etc) have methods that modify the data in-place (either optionally or
+obligatorily). This can be advantageous when working with large datasets
+because it reduces the amount of computer memory needed to perform the
+computations. However, it can lead to unexpected results if you're not aware
+that it's happening. This tutorial provides a few examples of in-place
+processing, and how and when to avoid it.
+
+As usual we'll start by importing the modules we need and
+loading some :ref:`example data <sample-dataset>`:
 """
 
+import os
 import mne
-import os.path as op
-import numpy as np
-from matplotlib import pyplot as plt
 
 ###############################################################################
-# Load an example dataset, the preload flag loads the data into memory now
-data_path = op.join(mne.datasets.sample.data_path(), 'MEG',
-                    'sample', 'sample_audvis_raw.fif')
-raw = mne.io.read_raw_fif(data_path, preload=True)
-raw = raw.crop(0, 10)
-print(raw)
+sample_data_folder = mne.datasets.sample.data_path()
+sample_data_raw_file = os.path.join(sample_data_folder, 'MEG', 'sample',
+                                    'sample_audvis_raw.fif')
+# the preload flag loads the data into memory now
+raw = mne.io.read_raw_fif(sample_data_raw_file, preload=True)
+raw.crop(tmax=10.)  # raw.crop() always happens in-place
+
 
 ###############################################################################
 # Signal processing
 # -----------------
 #
-# Most MNE objects have in-built methods for filtering:
+# Most MNE-Python data objects have built-in methods for filtering, including
+# high-, low-, and band-pass filters (`~mne.io.Raw.filter`),
+# `~mne.io.Raw.notch_filter`, Hilbert transforms (`~mne.io.Raw.apply_hilbert`),
+# and even arbitrary or user-defined functions (`~mne.io.Raw.apply_function`).
+# These typically **always** modify data in-place, so if we want to preserve
+# the unprocessed data for comparison, we must first make a copy of it. For
+# example:
 
-filt_bands = [(1, 3), (3, 10), (10, 20), (20, 60)]
-_, (ax, ax2) = plt.subplots(2, 1, figsize=(15, 10))
-data, times = raw[0]
-_ = ax.plot(data[0])
-for fmin, fmax in filt_bands:
-    raw_filt = raw.copy()
-    raw_filt.filter(fmin, fmax, fir_design='firwin')
-    _ = ax2.plot(raw_filt[0][0][0])
-ax2.legend(filt_bands)
-ax.set_title('Raw data')
-ax2.set_title('Band-pass filtered data')
+original_raw = raw.copy()
+raw.apply_hilbert()
+print(f'original data type was {original_raw.get_data().dtype}, after '
+      f'apply_hilbert the data type changed to {raw.get_data().dtype}.')
 
-###############################################################################
-# In addition, there are functions for applying the Hilbert transform, which is
-# useful to calculate phase / amplitude of your signal.
-
-# Filter signal with a fairly steep filter, then take hilbert transform
-
-raw_band = raw.copy()
-raw_band.filter(12, 18, l_trans_bandwidth=2., h_trans_bandwidth=2.,
-                fir_design='firwin')
-raw_hilb = raw_band.copy()
-hilb_picks = mne.pick_types(raw_band.info, meg=False, eeg=True)
-raw_hilb.apply_hilbert(hilb_picks)
-print(raw_hilb[0][0].dtype)
 
 ###############################################################################
-# Finally, it is possible to apply arbitrary functions to your data to do
-# what you want. Here we will use this to take the amplitude and phase of
-# the hilbert transformed data.
+# Channel picking
+# ---------------
 #
-# .. note:: You can also use ``envelope=True`` in the call to
-#           :meth:`mne.io.Raw.apply_hilbert` to do this automatically.
+# Another group of methods where data is modified in-place are the
+# channel-picking methods. For example:
+
+print(f'original data had {original_raw.info["nchan"]} channels.')
+original_raw.pick('eeg')  # selects only the EEG channels
+print(f'after picking, it has {original_raw.info["nchan"]} channels.')
+
+
+###############################################################################
+# Note also that when picking only EEG channels, projectors that affected only
+# the magnetometers were dropped, since there are no longer any magnetometer
+# channels.
 #
+#
+# The ``copy`` parameter
+# ----------------------
+#
+# Above we saw an example of using the ``.copy()`` method so that we can
+# compare data before and after processing. Unlike the signal processing and
+# channel picking *methods* above, MNE-Python *functions* often have a ``copy``
+# *parameter* that determines whether the operation happens in-place or on a
+# copy of the data. For example, here we're plotting ``raw`` *after* the
+# rereferencing has been done, but ``raw`` is unaffected because we specified
+# ``copy=True``:
 
-# Take the amplitude and phase
-raw_amp = raw_hilb.copy()
-raw_amp.apply_function(np.abs, hilb_picks)
-raw_phase = raw_hilb.copy()
-raw_phase.apply_function(np.angle, hilb_picks)
+# sphinx_gallery_thumbnail_number=2
+rereferenced_raw, ref_data = mne.set_eeg_reference(original_raw, ['EEG 003'],
+                                                   copy=True)
+original_raw.plot()
+rereferenced_raw.plot()
 
-_, (a1, a2) = plt.subplots(2, 1, figsize=(15, 10))
-a1.plot(raw_band[hilb_picks[0]][0][0].real)
-a1.plot(raw_amp[hilb_picks[0]][0][0].real)
-a2.plot(raw_phase[hilb_picks[0]][0][0].real)
-a1.set_title('Amplitude of frequency band')
-a2.set_title('Phase of frequency band')
+###############################################################################
+# Another example is the picking *function* `mne.pick_info`, which operates on
+# `mne.Info` dictionaries rather than on data objects. See
+# :ref:`tut-info-class` for details.
+#
+#
+# Summary
+# -------
+#
+# Generally speaking, you should expect that *methods of data objects* will
+# operate in-place, and *functions that take a data object as an argument* will
+# operate on a copy of the data (unless the function has a ``copy=False``
+# option). During the exploratory phase of your analysis, where you might want
+# to try out the effects of different data cleaning approaches, you should get
+# used to patterns like ``raw.copy().filter(...).plot()`` or
+# ``raw.copy().apply_proj().plot_psd()`` if you want to avoid having to re-load
+# data and repeat earlier steps each time you change a computation (see the
+# :ref:`sect-meth-chain` section for more info on method chaining).
