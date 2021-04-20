a92114076743d9fc0b3064d70fcc4ecfb8ad44d0
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-04-20 15:04:03 +0200

    Refactor

--- mne/viz/backends/_qt.py
+++ mne/viz/backends/_qt.py
@@ -506,9 +506,13 @@ class _Renderer(_PyVistaRenderer, _QtDock, _QtToolBar, _QtMenuBar,
         with _qt_disable_paint(self.plotter):
             with self._window_ensure_minimum_sizes():
                 self.plotter.app_window.show()
-        self.plotter._render()
         self._update()
         self._process_events()
+
+    def _update(self):
+        super()._update()
+        for plotter in self._all_plotters:
+            plotter._render()
 
 
 def _create_dock_widget(window, name, area):
