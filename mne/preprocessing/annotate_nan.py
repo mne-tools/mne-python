8c6a8e13bdcf1fffe6573aa96880243ca2b3f38f
Robert Luke <hello@robertluke.net>
2021-04-28 15:01:03 +1000

    Further suggestions from review

--- mne/preprocessing/annotate_nan.py
+++ mne/preprocessing/annotate_nan.py
@@ -7,7 +7,7 @@ import numpy as np
 import warnings
 
 from ..annotations import Annotations
-from ..utils import _mask_to_onsets_offsets, verbose
+from ..utils import _mask_to_onsets_offsets, verbose, warn
 
 
 @verbose
@@ -32,18 +32,15 @@ def annotate_nan(raw, verbose=None):
     nans = np.any(np.isnan(data), axis=0)
     starts, stops = _mask_to_onsets_offsets(nans)
 
-    if len(starts) > 0:
-        starts, stops = np.array(starts), np.array(stops)
-        onsets = (starts + raw.first_samp) * sampling_duration
-        durations = (stops - starts) * sampling_duration
-    else:
-        warnings.warn("The dataset you provided does not contain 'NaN' "
-                      "values. No annotations were made.")
-        return
+    if len(starts) == 0:
+        warn("The dataset you provided does not contain 'NaN' values. "
+             "No annotations were made.")
+        return annot
 
-    if annot is None:
-        annot = Annotations(onsets, durations, 'bad_NAN')
-    else:
-        annot.append(onsets, durations, 'bad_NAN')
+    starts, stops = np.array(starts), np.array(stops)
+    onsets = (starts + raw.first_samp) * sampling_duration
+    durations = (stops - starts) * sampling_duration
+
+    annot.append(onsets, durations, 'bad_NAN')
 
     return annot
