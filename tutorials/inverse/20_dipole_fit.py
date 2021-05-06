e291752a82c641c57f923b95428e2ded0dd5f59f
mdovgialo <marian.dowgialo@gmail.com>
2021-05-06 14:57:03 +0200

    ENH: add anatomical label finding from atlas to dipole fit tutorial

--- tutorials/inverse/20_dipole_fit.py
+++ tutorials/inverse/20_dipole_fit.py
@@ -15,10 +15,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import mne
+from mne import read_freesurfer_lut
 from mne.forward import make_forward_dipole
 from mne.evoked import combine_evoked
 from mne.simulation import simulate_evoked
 
+import nibabel
 from nilearn.plotting import plot_anat
 from nilearn.datasets import load_mni152_template
 
@@ -57,8 +59,29 @@ mni_pos = mne.head_to_mni(dip.pos, mri_head_t=trans,
 mri_pos = mne.head_to_mri(dip.pos, mri_head_t=trans,
                           subject=subject, subjects_dir=subjects_dir)
 
+# In the meantime let's find an anatomical label for the best fitted dipole
+best_dip_id = dip.gof.argmax()
+best_dip_mri_pos = mri_pos[best_dip_id]
+
+# Load parcelization:
+atlas_fname = op.join(subjects_dir, subject, 'mri', 'aparc.a2009s+aseg.mgz')
+parcelation_img = nibabel.load(atlas_fname)
+
+# Load freesurface atlas LUT
+lut_inv_dict = read_freesurfer_lut()[0]
+label_lut = {v: k for k, v in lut_inv_dict.items()}
+
+# Find voxel for dipole position
+mri_vox_t = np.linalg.inv(parcelation_img.header.get_vox2ras_tkr())
+vox_dip_pos = mne.transforms.apply_trans(mri_vox_t, best_dip_mri_pos).astype(int)
+
+# Get voxel value and label from LUT
+vol_values = parcelation_img.get_fdata()[tuple(vox_dip_pos.T)]
+label = label_lut.get(vol_values, 'Unknown')
+
+# Draw the dipole position on the MRI scan and write the anatomical label from parcelization
 t1_fname = op.join(subjects_dir, subject, 'mri', 'T1.mgz')
-fig_T1 = plot_anat(t1_fname, cut_coords=mri_pos[0], title='Dipole loc.')
+fig_T1 = plot_anat(t1_fname, cut_coords=mri_pos[0], title='Dipole loc. anatomical label: {}'.format(label))
 
 template = load_mni152_template()
 fig_template = plot_anat(template, cut_coords=mni_pos[0],
