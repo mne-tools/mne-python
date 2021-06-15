54836227e0379d271d1736d4c2792a128137fe6c
Eric Larson <larson.eric.d@gmail.com>
2021-06-14 21:04:53 -0400

    MAINT: Make Azure happy [skip actions] [skip circle]

--- tools/azure_dependencies.sh
+++ tools/azure_dependencies.sh
@@ -8,7 +8,8 @@ elif [ "${TEST_MODE}" == "pip-pre" ]; then
 	python -m pip install --progress-bar off --upgrade pip setuptools
 	python -m pip install --progress-bar off --upgrade --pre --only-binary ":all:" -i "https://pypi.anaconda.org/scipy-wheels-nightly/simple" --extra-index-url https://www.riverbankcomputing.com/pypi/simple numpy scipy pandas scikit-learn PyQt5 dipy statsmodels
 	python -m pip install --progress-bar off --upgrade --pre --only-binary ":all:" -f "https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com" h5py Pillow matplotlib
-	python -m pip install --progress-bar off --upgrade --pre --only-binary ":all" vtk
+	# we want --pre for VTK, but it breaks Azure as of 2021/06/14 (mayavi has problems with the 20210612 binary)
+	python -m pip install --progress-bar off --upgrade --only-binary ":all" vtk
 	python -m pip install --progress-bar off https://github.com/pyvista/pyvista/zipball/master
 	python -m pip install --progress-bar off https://github.com/pyvista/pyvistaqt/zipball/master
 	python -m pip install --progress-bar off --upgrade --only-binary="numba,llvmlite" -r requirements.txt
