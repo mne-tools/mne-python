d563e5546a140821c64993213acf85ede6fcd125
Eric Larson <larson.eric.d@gmail.com>
2021-06-23 09:12:30 -0400

    MAINT: Fix CIs [skip circle]

--- tools/azure_dependencies.sh
+++ tools/azure_dependencies.sh
@@ -6,8 +6,9 @@ if [ "${TEST_MODE}" == "pip" ]; then
 	python -m pip install --upgrade --only-binary="numba,llvmlite" -r requirements.txt
 elif [ "${TEST_MODE}" == "pip-pre" ]; then
 	python -m pip install --progress-bar off --upgrade pip setuptools
-	python -m pip install --progress-bar off --upgrade --pre --only-binary ":all:" -i "https://pypi.anaconda.org/scipy-wheels-nightly/simple" --extra-index-url https://www.riverbankcomputing.com/pypi/simple numpy scipy pandas scikit-learn PyQt5 dipy statsmodels
-	python -m pip install --progress-bar off --upgrade --pre --only-binary ":all:" -f "https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com" h5py Pillow matplotlib
+	python -m pip install --progress-bar off --upgrade --pre --only-binary ":all:" python-dateutil pytz joblib threadpoolctl six
+	python -m pip install --progress-bar off --upgrade --pre --only-binary ":all:" --no-deps -i "https://pypi.anaconda.org/scipy-wheels-nightly/simple" --extra-index-url https://www.riverbankcomputing.com/pypi/simple numpy scipy pandas scikit-learn PyQt5 dipy statsmodels
+	python -m pip install --progress-bar off --upgrade --pre --only-binary ":all:" --no-deps -f "https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com" h5py Pillow matplotlib
 	# we want --pre for VTK, but it breaks Azure as of 2021/06/14 (mayavi has problems with the 20210612 binary)
 	python -m pip install --progress-bar off --upgrade --only-binary ":all" vtk
 	python -m pip install --progress-bar off https://github.com/pyvista/pyvista/zipball/main
