4282ae47c518c85c0901f66109aaf701a45dad60
Alex <aprockhill@mailbox.org>
2021-06-23 07:34:57 -0700

    add fiducials and trans, no warning

--- tutorials/clinical/10_ieeg_localize.py
+++ tutorials/clinical/10_ieeg_localize.py
@@ -261,26 +261,15 @@ plot_overlay(T1, CT_aligned, 'Aligned CT Overlaid on T1', thresh=0.95)
 #
 # Electrode contact locations are plotted below.
 
-# load the subject's brain
-subject_brain = nib.freesurfer.load(
-    op.join(misc_path, 'seeg', 'sample_seeg_brain.mgz'))
-
 # Load electrode positions from file
 elec_df = pd.read_csv(op.join(misc_path, 'seeg', 'sample_seeg_electrodes.tsv'),
                       sep='\t', header=0, index_col=None)
 ch_names = elec_df['name'].tolist()
 ch_coords = elec_df[['R', 'A', 'S']].to_numpy(dtype=float)
 
-# convert electrode positions from surface RAS to voxels
-ch_coords = mne.transforms.apply_trans(
-    np.linalg.inv(subject_brain.header.get_vox2ras_tkr()), ch_coords)
-
-# separate the fiducial coordinates
-lpa, nasion, rpa = [ch_coords[ch_names.index(fid)] for fid in
-                    ('lpa', 'nasion', 'rpa')]
-for fid in ('lpa', 'nasion', 'rpa'):
-    ch_coords = np.delete(ch_coords, [ch_names.index(fid)])
-    ch_names.remove(fid)
+# load the subject's brain
+subject_brain = nib.freesurfer.load(
+    op.join(misc_path, 'seeg', 'sample_seeg_brain.mgz'))
 
 # Make brain surface from T1
 verts, triangles = mne.viz.marching_cubes(subject_brain.get_fdata(), level=100)
@@ -421,6 +410,10 @@ def peak_to_volume(loc, img, thresh, voxels_max=100):
     return voxels_in_volume
 
 
+# convert electrode positions from surface RAS to voxels
+ch_coords = mne.transforms.apply_trans(
+    np.linalg.inv(subject_brain.header.get_vox2ras_tkr()), ch_coords)
+
 # Take channel coordinates and use the CT to transform them
 # into a 3D image where all the voxels over a threshold nearby
 # are labeled with an index
@@ -469,13 +462,21 @@ ch_coords = mne.transforms.apply_trans(
 # load electrophysiology data
 raw = mne.io.read_raw(op.join(misc_path, 'seeg', 'sample_seeg_ieeg.fif'))
 
+lpa, nasion, rpa = mne.coreg.get_mni_fiducials(
+    subject, subjects_dir=subjects_dir)
+lpa, nasion, rpa = lpa['r'], nasion['r'], rpa['r']
+
 # Create a montage with our new points
 ch_pos = dict(zip(ch_names, ch_coords / 1000))  # mm -> m
-montage = mne.channels.make_dig_montage(ch_pos, coord_frame='mri_voxel')
+montage = mne.channels.make_dig_montage(
+    ch_pos, coord_frame='mri', nasion=nasion, lpa=lpa, rpa=rpa)
 raw.set_montage(montage)
 
+# get trans
+trans = mne.channels.compute_native_head_t(montage)
+
 # plot the resulting alignment
-fig = mne.viz.plot_alignment(raw.info, None, subject,
+fig = mne.viz.plot_alignment(raw.info, trans, subject,
                              subjects_dir=subjects_dir, show_axes=True,
                              surfaces=['pial', 'head'])
 
