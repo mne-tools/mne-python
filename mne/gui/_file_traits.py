6f9c7a274fa7061155a1295857def37e2a439323
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-07-05 14:05:14 +0200

    Revert "Start moving DigSource"
    This reverts commit dced5c9ddb37d420ffa422051e4ec6135f2976f8.

--- mne/gui/_file_traits.py
+++ mne/gui/_file_traits.py
@@ -10,9 +10,9 @@ import os.path as op
 
 import numpy as np
 
-from traits.api import (HasTraits, HasPrivateTraits, cached_property,
+from traits.api import (Any, HasTraits, HasPrivateTraits, cached_property,
                         on_trait_change, Array, Bool, Button, DelegatesTo,
-                        Directory, Enum, Event, File, Instance, List,
+                        Directory, Enum, Event, File, Instance, Int, List,
                         Property, Str, ArrayOrNone, BaseFile)
 from traitsui.api import View, Item, VGroup
 from pyface.api import DirectoryDialog, OK, ProgressDialog, error, information
@@ -20,12 +20,17 @@ from pyface.api import DirectoryDialog, OK, ProgressDialog, error, information
 from ._viewer import _DIG_SOURCE_WIDTH
 
 from ..bem import read_bem_surfaces
-from ..io import read_fiducials
+from ..io.constants import FIFF
+from ..io import read_info, read_fiducials, read_raw
+from ..io._read_raw import supported
+from ..io.meas_info import _empty_info
+from ..io.open import fiff_open, dir_tree_find
 from ..surface import read_surface, complete_surface_info
 from ..coreg import (_is_mri_subject, _mri_subject_has_bem,
                      create_default_subject)
 from ..utils import get_config, set_config
 from ..viz._3d import _fiducial_coords
+from ..channels import read_dig_fif
 
 
 fid_wildcard = "*.fif"
@@ -239,6 +244,196 @@ class FiducialsSource(HasTraits):
                   "Error Reading Fiducials")
             self.reset_traits(['file'])
             raise
+
+
+class DigSource(HasPrivateTraits):
+    """Expose digitization information from a file.
+
+    Parameters
+    ----------
+    file : File
+        Path to the BEM file (*.fif).
+
+    Attributes
+    ----------
+    fid : Array, shape = (3, 3)
+        Each row contains the coordinates for one fiducial point, in the order
+        Nasion, RAP, LAP. If no file is set all values are 0.
+    """
+
+    file = FileOrDir(exists=True,
+                     filter=[' '.join([f'*{ext}' for ext in supported])])
+
+    inst_fname = Property(Str, depends_on='file')
+    inst_dir = Property(depends_on='file')
+    _info = Property(depends_on='file')
+
+    points_filter = Any(desc="Index to select a subset of the head shape "
+                             "points")
+    n_omitted = Property(Int, depends_on=['points_filter'])
+
+    # head shape
+    _hsp_points = Property(depends_on='_info',
+                           desc="Head shape points in the file (n x 3 array)")
+    points = Property(depends_on=['_hsp_points', 'points_filter'],
+                      desc="Head shape points selected by the filter (n x 3 "
+                           "array)")
+
+    # fiducials
+    lpa = Property(depends_on='_info',
+                   desc="LPA coordinates (1 x 3 array)")
+    nasion = Property(depends_on='_info',
+                      desc="Nasion coordinates (1 x 3 array)")
+    rpa = Property(depends_on='_info',
+                   desc="RPA coordinates (1 x 3 array)")
+
+    # EEG
+    eeg_points = Property(depends_on='_info',
+                          desc="EEG sensor coordinates (N x 3 array)")
+    hpi_points = Property(depends_on='_info',
+                          desc='HPI coil coordinates (N x 3 array)')
+
+    view = View(Item('file', width=_DIG_SOURCE_WIDTH, tooltip='FIF file '
+                     '(Raw, Epochs, Evoked, or DigMontage)', show_label=False))
+
+    @cached_property
+    def _get_n_omitted(self):
+        if self.points_filter is None:
+            return 0
+        else:
+            return np.sum(self.points_filter == False)  # noqa: E712
+
+    @cached_property
+    def _get__info(self):
+        if not self.file:
+            return
+        elif self.file.endswith(('.fif', '.fif.gz')):
+            info = None
+            fid, tree, _ = fiff_open(self.file)
+            fid.close()
+            if len(dir_tree_find(tree, FIFF.FIFFB_MEAS_INFO)) > 0:
+                info = read_info(self.file, verbose=False)
+            elif len(dir_tree_find(tree, FIFF.FIFFB_ISOTRAK)) > 0:
+                info = _empty_info(1)
+                info['dig'] = read_dig_fif(fname=self.file).dig
+        else:
+            info = read_raw(self.file).info
+
+        # check that digitizer info is present
+        if info is None or info['dig'] is None:
+            error(None, "The selected file does not contain digitization "
+                  "information. Please select a different file.",
+                  "Error Reading Digitization File")
+            self.reset_traits(['file'])
+            return
+
+        # check that all fiducial points are present
+        point_kinds = {d['kind'] for d in info['dig']}
+        missing = [key for key in ('LPA', 'Nasion', 'RPA') if
+                   getattr(FIFF, f'FIFFV_POINT_{key.upper()}') not in
+                   point_kinds]
+        if missing:
+            points = _fiducial_coords(info['dig'])
+            if len(points == 3):
+                _append_fiducials(info['dig'], *points.T)
+            else:
+                error(None, "The selected digitization file does not contain "
+                      f"all cardinal points (missing: {', '.join(missing)}). "
+                      "Please select a different file.",
+                      "Error Reading Digitization File")
+                self.reset_traits(['file'])
+                return
+        return info
+
+    @cached_property
+    def _get_inst_dir(self):
+        return op.dirname(self.file)
+
+    @cached_property
+    def _get_inst_fname(self):
+        if self.file:
+            return op.basename(self.file)
+        else:
+            return '-'
+
+    @cached_property
+    def _get__hsp_points(self):
+        if not self._info or not self._info['dig']:
+            return np.empty((0, 3))
+
+        points = np.array([d['r'] for d in self._info['dig']
+                           if d['kind'] == FIFF.FIFFV_POINT_EXTRA])
+        points = np.empty((0, 3)) if len(points) == 0 else points
+        return points
+
+    @cached_property
+    def _get_points(self):
+        if self.points_filter is None:
+            return self._hsp_points
+        else:
+            return self._hsp_points[self.points_filter]
+
+    def _cardinal_point(self, ident):
+        """Coordinates for a cardinal point."""
+        if not self._info or not self._info['dig']:
+            return np.zeros((1, 3))
+
+        for d in self._info['dig']:
+            if d['kind'] == FIFF.FIFFV_POINT_CARDINAL and d['ident'] == ident:
+                return d['r'][None, :]
+        return np.zeros((1, 3))
+
+    @cached_property
+    def _get_nasion(self):
+        return self._cardinal_point(FIFF.FIFFV_POINT_NASION)
+
+    @cached_property
+    def _get_lpa(self):
+        return self._cardinal_point(FIFF.FIFFV_POINT_LPA)
+
+    @cached_property
+    def _get_rpa(self):
+        return self._cardinal_point(FIFF.FIFFV_POINT_RPA)
+
+    @cached_property
+    def _get_eeg_points(self):
+        if not self._info or not self._info['dig']:
+            return np.empty((0, 3))
+
+        out = [d['r'] for d in self._info['dig'] if
+               d['kind'] == FIFF.FIFFV_POINT_EEG and
+               d['coord_frame'] == FIFF.FIFFV_COORD_HEAD]
+        out = np.empty((0, 3)) if len(out) == 0 else np.array(out)
+        return out
+
+    @cached_property
+    def _get_hpi_points(self):
+        if not self._info or not self._info['dig']:
+            return np.zeros((0, 3))
+
+        out = [d['r'] for d in self._info['dig'] if
+               d['kind'] == FIFF.FIFFV_POINT_HPI and
+               d['coord_frame'] == FIFF.FIFFV_COORD_HEAD]
+        out = np.empty((0, 3)) if len(out) == 0 else np.array(out)
+        return out
+
+    def _file_changed(self):
+        self.reset_traits(('points_filter',))
+
+
+def _append_fiducials(dig, lpa, nasion, rpa):
+    dig.append({'coord_frame': FIFF.FIFFV_COORD_HEAD,
+                'ident': FIFF.FIFFV_POINT_LPA,
+                'kind': FIFF.FIFFV_POINT_CARDINAL,
+                'r': lpa})
+    dig.append({'coord_frame': FIFF.FIFFV_COORD_HEAD,
+                'ident': FIFF.FIFFV_POINT_NASION,
+                'kind': FIFF.FIFFV_POINT_CARDINAL,
+                'r': nasion})
+    dig.append({'coord_frame': FIFF.FIFFV_COORD_HEAD,
+                'ident': FIFF.FIFFV_POINT_RPA,
+                'kind': FIFF.FIFFV_POINT_CARDINAL,
+                'r': rpa})
 
 
 class MRISubjectSource(HasPrivateTraits):
