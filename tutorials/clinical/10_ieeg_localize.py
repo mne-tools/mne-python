79a312f18384c359009c0970bddc2b6ab6f46d12
Alex <aprockhill@mailbox.org>
2021-07-16 11:28:06 -0700

    remove marching cubes

--- tutorials/clinical/10_ieeg_localize.py
+++ tutorials/clinical/10_ieeg_localize.py
@@ -207,6 +207,9 @@ del CT_data, T1
 #
 # .. note:: MNE represents data in the "head" space internally
 
+# load electrophysiology data
+raw = mne.io.read_raw(op.join(misc_path, 'seeg', 'sample_seeg_ieeg.fif'))
+
 # load electrode positions from file
 elec_df = pd.read_csv(op.join(misc_path, 'seeg', 'sample_seeg_electrodes.tsv'),
                       sep='\t', header=0, index_col=None)
@@ -231,23 +234,21 @@ montage = mne.channels.make_dig_montage(
 subject_brain = nib.load(
     op.join(misc_path, 'seeg', 'sample_seeg', 'mri', 'brain.mgz'))
 
-# Make brain surface from T1
-verts, triangles = mne.surface.marching_cubes(
-    np.asarray(subject_brain.dataobj), level=100)
-# transform from voxels to surface RAS
-verts = mne.transforms.apply_trans(
-    subject_brain.header.get_vox2ras_tkr(), verts) / 1000.  # to meters
+# get native to head trans
+trans = mne.channels.compute_native_head_t(montage)
 
+# set new montage
+raw.set_montage(montage)
+
+# plot the alignment
 fig_kwargs = dict(size=(800, 600), bgcolor='w', scene=False)
 renderer = mne.viz.backends.renderer.create_3d_figure(**fig_kwargs)
-renderer.mesh(*verts.T, triangles=triangles, color='gray',
-              opacity=0.05, representation='surface')
-for ch_coord in ch_coords:
-    renderer.sphere(center=tuple(ch_coord / 1000.), color='y', scale=0.005)
+fig = mne.viz.plot_alignment(raw.info, trans, 'sample_seeg',
+                             fig=renderer.figure,
+                             subjects_dir=op.join(misc_path, 'seeg'),
+                             show_axes=True, surfaces=dict(pial=0.2))
 view_kwargs = dict(azimuth=60, elevation=100)
-mne.viz.set_3d_view(renderer.figure, focalpoint=(0, 0, 0), distance=0.3,
-                    **view_kwargs)
-renderer.show()
+mne.viz.set_3d_view(fig, focalpoint=(0, 0, 0.05), distance=0.4, **view_kwargs)
 
 # %%
 # Warping to a Common Atlas
@@ -332,9 +333,6 @@ del CT_aligned
 
 # sphinx_gallery_thumbnail_number = 8
 
-# load electrophysiology data
-raw = mne.io.read_raw(op.join(misc_path, 'seeg', 'sample_seeg_ieeg.fif'))
-
 # get native to head trans
 trans = mne.channels.compute_native_head_t(montage_warped)
 
@@ -344,9 +342,8 @@ raw.set_montage(montage_warped)
 # plot the resulting alignment
 renderer = mne.viz.backends.renderer.create_3d_figure(**fig_kwargs)
 fig = mne.viz.plot_alignment(raw.info, trans, 'fsaverage',
-                             fig=renderer.figure,
-                             subjects_dir=subjects_dir, show_axes=True,
-                             surfaces=dict(pial=0.2, head=0.2))
+                             fig=renderer.figure, subjects_dir=subjects_dir,
+                             show_axes=True, surfaces=dict(pial=0.2))
 mne.viz.set_3d_view(fig, focalpoint=(0, 0, 0.05), distance=0.4, **view_kwargs)
 
 # %%
