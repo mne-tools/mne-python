9c938059703a6022365e40bf8c6d3d17398393cc
Eric Larson <larson.eric.d@gmail.com>
2021-07-26 11:01:33 -0400

    WIP: Check to make sure it works [skip azp] [skip circle]

--- tools/github_actions_test.sh
+++ tools/github_actions_test.sh
@@ -8,12 +8,3 @@ else
 fi
 echo 'pytest -m "${CONDITION}" --tb=short --cov=mne --cov-report xml -vv ${USE_DIRS}'
 pytest -m "${CONDITION}" --tb=short --cov=mne --cov-report xml -vv ${USE_DIRS}
-
-# run the minimal one with the testing data as well
-if [ "${DEPS}" == "minimal" ]; then
-	export MNE_SKIP_TESTING_DATASET_TESTS=false;
-	python -c 'import mne; mne.datasets.testing.data_path(verbose=True)';
-fi;
-if [ "${DEPS}" == "minimal" ]; then
-	pytest -m "${CONDITION}" --tb=short --cov=mne -vv ${USE_DIRS};
-fi;
