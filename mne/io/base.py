4567d42938cc9044e7ce0f49b5b39118b1d6b849
Richard Höchenberger <richard.hoechenberger@gmail.com>
2021-07-28 22:41:58 +0200

    Fix FIFF

--- mne/io/base.py
+++ mne/io/base.py
@@ -1447,7 +1447,7 @@ class BaseRaw(ProjMixin, ContainsMixin, UpdateChannelsMixin, SetChannelsMixin,
                              '"double", not "short"')
 
         # check for file existence and expand `~` if present
-        fname = _check_fname(fname, overwrite)
+        fname = _check_fname(fname=fname, overwrite=overwrite)
 
         if proj:
             info = deepcopy(self.info)
@@ -2178,7 +2178,9 @@ def _write_raw(fname, raw, info, picks, fmt, data_type, reset_range, start,
             raw, info, picks, fid, cals, part_idx, start, stop,
             buffer_size, prev_fname, split_size, use_fname,
             projector, drop_small_buffer, fmt, fname, reserved_fname,
-            data_type, reset_range, split_naming, overwrite)
+            data_type, reset_range, split_naming,
+            overwrite=True  # we've started writing already above
+        )
     if final_fname != use_fname:
         assert split_naming == 'bids'
         logger.info(f'Renaming BIDS split file {op.basename(final_fname)}')
