a4efd1f2012df717e4fea4fe4a864eaf3e750315
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-07-28 10:39:28 +0200

    clean up

--- tutorials/forward/25_automated_coreg.py
+++ tutorials/forward/25_automated_coreg.py
@@ -27,33 +27,27 @@ subject = 'sample'
 fname_raw = op.join(data_path, 'MEG', subject, subject + '_audvis_raw.fif')
 info = read_info(fname_raw)
 
+plot_kwargs = dict(subject=subject, subjects_dir=subjects_dir,
+                   surfaces=dict(head=0.4), dig=True, eeg=[],
+                   meg=False, coord_frame='meg')
+view_kwargs = dict(azimuth=45, elevation=90, distance=0.6,
+                   focalpoint=(0., 0., 0.))
+
 # Set up coreg model
 coreg = Coregistration(info, subject, subjects_dir)
+
 # Do initial coreg fit for outlier detection
 coreg.fit_fiducials(verbose=True)
-kwargs = dict(azimuth=45, elevation=90, distance=0.6, focalpoint=(0., 0., 0.))
-fig = mne.viz.plot_alignment(info, trans=coreg.trans, subject=subject,
-                             subjects_dir=subjects_dir,
-                             surfaces=dict(head=0.4),
-                             dig=True, eeg=[], meg=False,
-                             coord_frame='meg')
-mne.viz.set_3d_view(fig, **kwargs)
+fig = mne.viz.plot_alignment(info, trans=coreg.trans, **plot_kwargs)
+mne.viz.set_3d_view(fig, **view_kwargs)
 
 # Overweighting at this step also seems to throw off the fit for some datasets
 coreg.fit_icp(n_iterations=6, nasion_weight=2., verbose=True)
-fig = mne.viz.plot_alignment(info, trans=coreg.trans, subject=subject,
-                             subjects_dir=subjects_dir,
-                             surfaces=dict(head=0.4),
-                             dig=True, eeg=[], meg=False,
-                             coord_frame='meg')
-mne.viz.set_3d_view(fig, **kwargs)
+fig = mne.viz.plot_alignment(info, trans=coreg.trans, **plot_kwargs)
+mne.viz.set_3d_view(fig, **view_kwargs)
 
 coreg.omit_hsp_points(distance=5. / 1000)  # distance is in meters
 # Do final coreg fit
 coreg.fit_icp(n_iterations=20, nasion_weight=10., verbose=True)
-fig = mne.viz.plot_alignment(info, trans=coreg.trans, subject=subject,
-                             subjects_dir=subjects_dir,
-                             surfaces=dict(head=0.4),
-                             dig=True, eeg=[], meg=False,
-                             coord_frame='meg')
-mne.viz.set_3d_view(fig, **kwargs)
+fig = mne.viz.plot_alignment(info, trans=coreg.trans, **plot_kwargs)
+mne.viz.set_3d_view(fig, **view_kwargs)
