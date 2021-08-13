82aa6769973c6fdab8dfccff154937f1b8d42c35
Adam Li <adam2392@gmail.com>
2021-08-13 11:51:01 -0400

    Revert change.

--- mne/viz/circle.py
+++ mne/viz/circle.py
@@ -13,12 +13,10 @@ from functools import partial
 import numpy as np
 
 from .utils import plt_show
-from ..utils import (_validate_type, deprecated, CONNECTIVITY_DEPRECATION_MSG,
-                     verbose)
+from ..utils import _validate_type, deprecated, CONNECTIVITY_DEPRECATION_MSG
 
 
 @deprecated(CONNECTIVITY_DEPRECATION_MSG)
-@verbose
 def circular_layout(node_names, node_order, start_pos=90, start_between=True,
                     group_boundaries=None, group_sep=10):
     """Create layout arranging nodes on a circle.
@@ -123,7 +121,6 @@ def _plot_connectivity_circle_onpick(event, fig=None, axes=None, indices=None,
 
 
 @deprecated(CONNECTIVITY_DEPRECATION_MSG)
-@verbose
 def plot_connectivity_circle(con, node_names, indices=None, n_lines=None,
                              node_angles=None, node_width=None,
                              node_colors=None, facecolor='black',
