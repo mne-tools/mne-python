b527dc6683165d1e5982036d6144010fd710860f
Eric Larson <larson.eric.d@gmail.com>
2021-09-13 12:23:12 -0400

    FIX: Restore dipy (#9731)
    * FIX: Restore dipy
    
    * FIX: Dont use numba
    
    * BUG: Fix for Windows

--- tools/github_actions_dependencies.sh
+++ tools/github_actions_dependencies.sh
@@ -15,12 +15,11 @@ else
 	echo "PyQt5"
 	pip install --progress-bar off --upgrade --pre --only-binary ":all:" --no-deps --extra-index-url https://www.riverbankcomputing.com/pypi/simple PyQt5 PyQt5-sip PyQt5-Qt5
 	echo "NumPy/SciPy/pandas etc."
-	pip install --progress-bar off --upgrade --pre --only-binary ":all:" --no-deps -i "https://pypi.anaconda.org/scipy-wheels-nightly/simple" numpy scipy pandas "scikit-learn>=0.24.2" statsmodels
-	pip install --progress-bar off --upgrade --only-binary ":all:" --no-deps dipy
+	pip install --progress-bar off --upgrade --pre --only-binary ":all:" --no-deps -i "https://pypi.anaconda.org/scipy-wheels-nightly/simple" numpy scipy pandas "scikit-learn>=0.24.2" statsmodels dipy
 	echo "H5py, pillow, matplotlib"
 	pip install --progress-bar off --upgrade --pre --only-binary ":all:" --no-deps -f "https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com" h5py pillow matplotlib
-	echo "Numba, nilearn"
-	pip install --progress-bar off --upgrade --pre --only-binary ":all:" numba llvmlite nilearn
+	echo "nilearn"
+	pip install --progress-bar off --upgrade --pre --only-binary ":all:" nilearn
 	echo "VTK"
 	# built using vtk master branch on an Ubuntu 18.04.5 VM and uploaded to OSF,
 	# can't use VTK's pre wheel because it breaks Mayavi (probably event processing)
