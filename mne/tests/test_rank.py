1e2fc0183ca531cc905367c698e46c82ab7b8725
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-09-13 11:41:35 +0200

    Fix style 2.

--- mne/tests/test_rank.py
+++ mne/tests/test_rank.py
@@ -264,8 +264,7 @@ def test_maxfilter_get_rank(n_proj, fname, rank_orig, meg, tol_kind, tol):
 
 
 def test_explicit_bads_pick():
-    """Test when bads channels are explicitly passed to picks and def ault
-    behavior with picks=None."""
+    """Test when bads channels are explicitly passed + default picks=None."""
     raw = read_raw_fif(raw_fname, preload=True)
     raw.pick_types(eeg=True, meg=True, ref_meg=True)
 
