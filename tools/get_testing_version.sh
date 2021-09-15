0d8b31d1e31e9e9342586d6a067d1bc1fa07b2c4
Adam Li <adam2392@gmail.com>
2021-09-15 14:22:44 -0400

    Try again to fix CI

--- tools/get_testing_version.sh
+++ tools/get_testing_version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ef
 
-TESTING_VERSION=`grep -o "testing='[0-9.]\+'" mne/datasets/utils.py | cut -d \' -f 2 | sed "s/\./-/g"`
+TESTING_VERSION=`grep -o "testing='[0-9.]\+'" mne/datasets/config.py | cut -d \' -f 2 | sed "s/\./-/g"`
 # This can be incremented to start fresh when the cache misbehaves, e.g.:
 # TESTING_VERSION=${TESTING_VERSION}-1
 if [ ! -z $GITHUB_ENV ]; then
