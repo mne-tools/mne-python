f6f8153f37f115f7741e3b6c00d3d05b6a800872
Eric Larson <larson.eric.d@gmail.com>
2021-10-04 15:29:13 -0400

    CI: Try VTK pre

--- mne/conftest.py
+++ mne/conftest.py
@@ -126,6 +126,7 @@ def pytest_configure(config):
     ignore:.*Creating an ndarray from ragged.*:numpy.VisibleDeprecationWarning
     ignore:^Please use.*scipy\..*:DeprecationWarning
     ignore:.*Found the following unknown channel type.*:RuntimeWarning
+    ignore:.*Passing a schema to Validator.*:DeprecationWarning
     always::ResourceWarning
     """  # noqa: E501
     for warning_line in warning_lines.split('\n'):
