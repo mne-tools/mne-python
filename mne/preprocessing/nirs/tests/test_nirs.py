220752219711919de0f38de342a4bc6a0aead89b
Robert Luke <hello@robertluke.net>
2021-10-06 15:52:11 +1100

    Add support for data from NIRx Aurora 2021.9 software

--- mne/preprocessing/nirs/tests/test_nirs.py
+++ mne/preprocessing/nirs/tests/test_nirs.py
@@ -31,15 +31,6 @@ fname_nirx_15_2_short = op.join(data_path(download=False),
                                 'nirx_15_2_recording_w_short')
 
 
-
-
-@testing.requires_testing_data
-def test_fnirs_pickst():
-    """Test picking of fnirs types after different conversions."""
-    raw = read_raw_nirx(fname_nirx_15_0)
-    raw.plot()
-
-
 @testing.requires_testing_data
 def test_fnirs_picks():
     """Test picking of fnirs types after different conversions."""
