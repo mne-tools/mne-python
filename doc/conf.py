7154c6d16389c38447f16534847f7ae7503d42fc
Daniel McCloy <dan@mccloy.info>
2021-10-09 12:03:03 -0500

    unpin jsonschema and filter its warning instead (#9822)
    * unpin jsonschema and filter its warning instead
    
    * filter traits deprecation warning

--- doc/conf.py
+++ doc/conf.py
@@ -850,6 +850,8 @@ def reset_warnings(gallery_conf, fname):
                 r'sphinx\.util\.smartypants is deprecated',
                 'is a deprecated alias for the builtin',  # NumPy
                 'the old name will be removed',  # Jinja, via sphinx
+                r'Passing a schema to Validator\.iter_errors',  # jsonschema
+                "default value of type 'dict' in an Any trait will",  # traits
                 'rcParams is deprecated',  # PyVista rcParams -> global_theme
                 'to mean no clipping',
                 ):
