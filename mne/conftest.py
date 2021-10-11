4f74ee2a96c618b506afeda3c09e0bf578cc607a
Eric Larson <larson.eric.d@gmail.com>
2021-10-11 10:31:58 -0400

    MAINT: Update broken link, fix rendering (#9829)
    * MAINT: Update broken link, fix rendering
    
    * FIX: Dep
    
    * DOC: Simpler / corrected docstrings
    
    * MAINT: Grrr mayavi again

--- mne/conftest.py
+++ mne/conftest.py
@@ -127,6 +127,7 @@ def pytest_configure(config):
     ignore:^Please use.*scipy\..*:DeprecationWarning
     ignore:.*Passing a schema to Validator.*:DeprecationWarning
     ignore:.*Found the following unknown channel type.*:RuntimeWarning
+    ignore:.*in an Any trait will be shared.*:DeprecationWarning
     always::ResourceWarning
     """  # noqa: E501
     for warning_line in warning_lines.split('\n'):
