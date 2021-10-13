892b57e0e1e29429ffc0e9d09e0edf6b03a6c407
Daniel McCloy <dan@mccloy.info>
2021-10-13 11:44:53 -0500

    use new pip capabilities in CI setups

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -23,7 +23,7 @@ if [[ "$CIRCLE_JOB" == "interactive_test" ]]; then
 	python -m pip install --progress-bar off vtk-9.0.20201117-cp39-cp39-linux_x86_64.whl
 	python -m pip install --progress-bar off https://github.com/pyvista/pyvista/zipball/main
 	python -m pip install --progress-bar off https://github.com/pyvista/pyvistaqt/zipball/main
-	python -m pip install --progress-bar off --upgrade -r requirements_testing.txt -r requirements_testing_extra.txt
+	python -m pip install --progress-bar off --upgrade -e .[test]
 elif [[ "$CIRCLE_JOB" == "linkcheck"* ]]; then
 	echo "Installing minimal linkcheck dependencies"
 	python -m pip install --progress-bar off numpy scipy matplotlib pillow pytest
