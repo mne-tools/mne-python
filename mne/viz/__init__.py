2dd65c0cf74667b0dc5978fd1cdfa0afc465d927
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-10-20 12:56:40 +0200

    ENH: Introduce close_3d_figure and close_all_3d_figures (#9874)
    * add close_3d_figure and close_all_3d_figures
    
    * update doc
    
    * fix
    
    * fix

--- mne/viz/__init__.py
+++ mne/viz/__init__.py
@@ -30,6 +30,6 @@ from .ica import (plot_ica_scores, plot_ica_sources, plot_ica_overlay,
 from .montage import plot_montage
 from .backends.renderer import (set_3d_backend, get_3d_backend, use_3d_backend,
                                 set_3d_view, set_3d_title, create_3d_figure,
-                                get_brain_class)
+                                close_3d_figure, close_all_3d_figures, get_brain_class)
 from . import backends
 from ._brain import Brain
