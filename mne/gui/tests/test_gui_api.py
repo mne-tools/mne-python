69917a36c393e5aceed9b404c33fead5da2071f1
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-10-21 14:32:37 +0200

    update testing

--- mne/gui/tests/test_gui_api.py
+++ mne/gui/tests/test_gui_api.py
@@ -2,12 +2,13 @@
 #
 # License: Simplified BSD
 
-def test_gui_api(renderer_interactive_pyvistaqt):
+def test_gui_api(renderer_gui, nbexec):
     """Test GUI API."""
     def noop(x):
         return
 
-    renderer = renderer_interactive_pyvistaqt._get_renderer(size=(300, 300))
+    import mne
+    renderer = mne.viz.backends.renderer._get_renderer(size=(300, 300))
     renderer._dock_initialize(name='', area='left')
     renderer._dock_add_text('', '', '')
     renderer._dock_add_file_button('', '', noop, directory=True)
