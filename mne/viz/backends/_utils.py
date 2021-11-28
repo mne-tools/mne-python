2b3d74cf8d9f29ffa0052ebbe66872a44d04f994
marsipu <dev@earthman-music.de>
2021-11-28 15:50:58 +0100

    Fix missing app name for macOS

--- mne/viz/backends/_utils.py
+++ mne/viz/backends/_utils.py
@@ -113,22 +113,32 @@ def _init_mne_qtapp(enable_icon=True, pg_app=False):
     app: ``PyQt5.QtWidgets.QApplication``
         Instance of QApplication.
     """
-    from PyQt5.QtCore import Qt
     from PyQt5.QtWidgets import QApplication
     from PyQt5.QtGui import QIcon
 
+    app_name = 'MNE-Python'
+    organization_name = 'MNE'
+
     if pg_app:
         from pyqtgraph import mkQApp
-        app = mkQApp('MNE-Python')
+        app = mkQApp(app_name)
     else:
-        app = QApplication.instance() or QApplication(sys.argv or ['MNE'])
-        app.setApplicationName('MNE-Python')
-    app.setOrganizationName('MNE')
+        app = QApplication.instance() or QApplication(sys.argv or [app_name])
+        app.setApplicationName(app_name)
+    app.setOrganizationName(organization_name)
 
     if enable_icon:
         # Set icon
         _init_qt_resources()
         kind = 'bigsur-' if platform.mac_ver()[0] >= '10.16' else ''
         app.setWindowIcon(QIcon(f":/mne-{kind}icon.png"))
+
+    # Fix from cbrnr/mnelab for app name in menu bar
+    if sys.platform.startswith("darwin"):
+        # set bundle name on macOS (app name shown in the menu bar)
+        from Foundation import NSBundle
+        bundle = NSBundle.mainBundle()
+        info = bundle.localizedInfoDictionary() or bundle.infoDictionary()
+        info["CFBundleName"] = app_name
 
     return app
