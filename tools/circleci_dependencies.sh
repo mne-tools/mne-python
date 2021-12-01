4e4d7638cf2fee7749d0542a4e37b6e74e3c31e2
Eric Larson <larson.eric.d@gmail.com>
2021-12-01 12:45:26 -0800

    MAINT: Only do nightly full builds

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -15,18 +15,7 @@ fi
 echo "Installing setuptools and sphinx"
 python -m pip install --progress-bar off --upgrade "pip!=20.3.0" setuptools wheel
 python -m pip install --upgrade --progress-bar off --pre sphinx
-if [[ "$CIRCLE_JOB" == "interactive_test" ]]; then
-	echo "Installing latest dependencies for interactive_test"
-	python -m pip install --progress-bar off --upgrade --pre --only-binary ":all:" python-dateutil pytz joblib threadpoolctl
-	python -m pip install --progress-bar off --upgrade --pre --only-binary ":all:" -i "https://pypi.anaconda.org/scipy-wheels-nightly/simple" --extra-index-url https://www.riverbankcomputing.com/pypi/simple numpy scipy pandas scikit-learn PyQt5
-	python -m pip install --progress-bar off --upgrade --pre --only-binary ":all:" -f "https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com" h5py pillow matplotlib
-	python -m pip install --progress-bar off --upgrade --pre --only-binary ":all:" numba llvmlite
-	wget -q https://osf.io/kej3v/download -O vtk-9.0.20201117-cp39-cp39-linux_x86_64.whl
-	python -m pip install --progress-bar off vtk-9.0.20201117-cp39-cp39-linux_x86_64.whl
-	python -m pip install --progress-bar off https://github.com/pyvista/pyvista/zipball/main
-	python -m pip install --progress-bar off https://github.com/pyvista/pyvistaqt/zipball/main
-	python -m pip install --progress-bar off --upgrade -e .[test]
-elif [[ "$CIRCLE_JOB" == "linkcheck"* ]]; then
+if [[ "$CIRCLE_JOB" == "linkcheck"* ]]; then
 	echo "Installing minimal linkcheck dependencies"
 	python -m pip install --progress-bar off numpy scipy matplotlib pillow pytest
 	python -m pip install --progress-bar off -r requirements_doc.txt
