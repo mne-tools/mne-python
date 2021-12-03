f2ae58f6bfdbabf0aaa06a6f650124f9b2788581
Eric Larson <larson.eric.d@gmail.com>
2021-12-02 21:37:14 -0800

    CI: Try no depth peeling (#10078)

--- tutorials/clinical/20_seeg.py
+++ tutorials/clinical/20_seeg.py
@@ -5,8 +5,8 @@
 Working with sEEG data
 ======================
 
-MNE supports working with more than just MEG and EEG data. Here we show some
-of the functions that can be used to facilitate working with
+MNE-Python supports working with more than just MEG and EEG data. Here we show
+some of the functions that can be used to facilitate working with
 stereoelectroencephalography (sEEG) data.
 
 This example shows how to use:
