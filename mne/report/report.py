e3a050ce39852f89378b689b2f3cf596e826283d
Richard Höchenberger <richard.hoechenberger@gmail.com>
2021-12-09 09:14:20 +0100

    Add try/except block

--- mne/report/report.py
+++ mne/report/report.py
@@ -2548,21 +2548,28 @@ class Report(object):
     def _render_raw_butterfly_segments(
         self, *, raw: BaseRaw, image_format, tags
     ):
-        orig_annotations = raw.annotations.copy()
-        raw.set_annotations(None)
-
         # Pick 10 1-second time slices
         times = np.linspace(raw.times[0], raw.times[-1], 12)[1:-1]
+        orig_annotations = raw.annotations.copy()
         figs = []
-        for t in times:
-            tmin = max(t - 0.5, 0)
-            tmax = min(t + 0.5, raw.times[-1])
-            duration = tmax - tmin
-            fig = raw.plot(butterfly=True, show_scrollbars=False, start=tmin,
-                           duration=duration, show=False)
-            figs.append(fig)
+        # Remove annotations before plotting for better performance.
+        # Ensure we later restore raw.annotations even in case of an exception
+        try:
+            raw.set_annotations(None)
+            for t in times:
+                tmin = max(t - 0.5, 0)
+                tmax = min(t + 0.5, raw.times[-1])
+                duration = tmax - tmin
+                fig = raw.plot(
+                    butterfly=True, show_scrollbars=False, start=tmin,
+                    duration=duration, show=False
+                )
+                figs.append(fig)
+        except Exception:
+            raise
+        finally:
+            raw.set_annotations(orig_annotations)
 
-        raw.set_annotations(orig_annotations)
         del orig_annotations
 
         captions = [f'Segment {i+1} of {len(figs)}'
