67a50454ae5a12012391df5011eaa13544e32023
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2021-12-11 21:43:57 +0100

    fix flake

--- mne/preprocessing/tests/test_annotate_nan.py
+++ mne/preprocessing/tests/test_annotate_nan.py
@@ -15,6 +15,7 @@ from mne.preprocessing import annotate_nan
 base_dir = op.join(op.dirname(__file__), '..', '..', 'io', 'tests', 'data')
 raw_fname = op.join(base_dir, 'test_raw.fif')
 
+
 @pytest.mark.parametrize('meas_date', (None, 'orig'))
 def test_annotate_nan(meas_date):
     """Tests automatic NaN annotation generation."""
@@ -35,15 +36,15 @@ def test_annotate_nan(meas_date):
 
     # insert block of NaN from 1s to 3s for one channel
     nan_ch_idx = 0
-    raw._data[nan_ch_idx, 1*sfreq:3*sfreq] = np.nan
+    raw._data[nan_ch_idx, 1 * sfreq:3 * sfreq] = np.nan
 
     # annotate_nan accurately finds this
     annot_nan = annotate_nan(raw)
     assert_array_equal(annot_nan.onset, np.array([1]))
     assert_array_equal(annot_nan.duration, np.array([2]))
     assert_array_equal(annot_nan.description, np.array(['BAD_NAN']))
+    assert len(annot_nan.ch_names) == 1
     assert annot_nan.ch_names[0] == (raw.ch_names[nan_ch_idx],)
 
     # Set the NaN annotations to the raw object
     raw.set_annotations(annot_nan)
-
