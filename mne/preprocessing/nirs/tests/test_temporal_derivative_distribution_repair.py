71e5ba49c882eac3277772e574c3570df72c4665
Johann Benerradi <johann.benerradi@gmail.com>
2021-12-13 09:40:31 +0000

    Add tests for TDDR on Hb data

--- mne/preprocessing/nirs/tests/test_temporal_derivative_distribution_repair.py
+++ mne/preprocessing/nirs/tests/test_temporal_derivative_distribution_repair.py
@@ -12,6 +12,7 @@ from mne.datasets.testing import data_path
 from mne.io import read_raw_nirx
 from mne.preprocessing.nirs import optical_density, tddr
 from mne.datasets import testing
+from mne.nirs import beer_lambert_law
 
 
 fname_nirx_15_2 = op.join(data_path(download=False),
@@ -23,18 +24,35 @@ fname_nirx_15_2 = op.join(data_path(download=False),
 def test_temporal_derivative_distribution_repair(fname, tmp_path):
     """Test running artifact rejection."""
     raw = read_raw_nirx(fname)
-    raw = optical_density(raw)
+    raw_od = optical_density(raw)
+    raw_hb = beer_lambert_law(raw_od)
 
+    # With optical densities
     # Add a baseline shift artifact about half way through data
-    max_shift = np.max(np.diff(raw._data[0]))
+    max_shift = np.max(np.diff(raw_od._data[0]))
     shift_amp = 5 * max_shift
     raw._data[0, 0:30] = raw._data[0, 0:30] - (1.1 * shift_amp)
     # make one channel zero std
-    raw._data[1] = 0.
-    raw._data[2] = 1.
-    assert np.max(np.diff(raw._data[0])) > shift_amp
+    raw_od._data[1] = 0.
+    raw_od._data[2] = 1.
+    assert np.max(np.diff(raw_od._data[0])) > shift_amp
     # Ensure that applying the algorithm reduces the step change
-    raw = tddr(raw)
-    assert np.max(np.diff(raw._data[0])) < shift_amp
-    assert_allclose(raw._data[1], 0.)  # unchanged
-    assert_allclose(raw._data[2], 1.)  # unchanged
+    raw_od = tddr(raw_od)
+    assert np.max(np.diff(raw_od._data[0])) < shift_amp
+    assert_allclose(raw_od._data[1], 0.)  # unchanged
+    assert_allclose(raw_od._data[2], 1.)  # unchanged
+
+    # With Hb
+    # Add a baseline shift artifact about half way through data
+    max_shift = np.max(np.diff(raw_hb._data[0]))
+    shift_amp = 5 * max_shift
+    raw_hb._data[0, 0:30] = raw_hb._data[0, 0:30] - (shift_amp)
+    # make one channel zero std
+    raw_hb._data[1] = 0.
+    raw_hb._data[2] = 1.
+    assert np.max(np.diff(raw_hb._data[0])) > shift_amp
+    # Ensure that applying the algorithm reduces the step change
+    raw_hb = tddr(raw_hb)
+    assert np.max(np.diff(raw_hb._data[0])) < shift_amp
+    assert_allclose(raw_hb._data[1], 0.)  # unchanged
+    assert_allclose(raw_hb._data[2], 1.)  # unchanged
