MRG: Fix scaling issue in Report.add_trans() (#10142)
* Fix scaling issue in Report.add_trans(), remove head transparency

We observed that the figure created by `Report.add_trans()`
was cropped. I narrowed this down to the `_ndarray_to_fig()`
function, which uses `Figure.figimage()`, which seems to later
interfere with our attempts to scale / adjust resolution of the
figure.

My fix is to not use `Figure.figimage()` anymore, and instead
add an axes that spans across the entire figure, and use `imshow()`.

With these changes, things seem to be looking good again.

It could be that this issue was introduced through #10129, but I haven't
invested much time to actually verify this hypothesis.

I have also removed transparency of the head surface in the
alighment plots – this makes it easier to visually inspect the
sensor alignment.

* Revert head transparency changes

* Update doc/changes/latest.inc [skip actions][skip azp]

mne/viz/utils.py
@@ -2298,12 +2298,12 @@ def _figure_agg(**kwargs):
     return fig
 
 
-def _ndarray_to_fig(img):
+def _ndarray_to_fig(img, dpi=100):
     """Convert to MPL figure, adapted from matplotlib.image.imsave."""
-    dpi = 100
     figsize = np.array(img.shape[:2][::-1]) / dpi
     fig = _figure_agg(dpi=dpi, figsize=figsize, frameon=False)
-    fig.figimage(img, resize=True)
+    ax = fig.add_axes([0, 0, 1, 1])
+    ax.imshow(img)
     return fig
 
 