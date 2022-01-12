05bca97b5e8eeecceae4d917d49ff548264c9dba
Eric Larson <larson.eric.d@gmail.com>
2022-01-12 15:57:50 -0500

    FIX: Verbose

--- tools/setup_xvfb.sh
+++ tools/setup_xvfb.sh
@@ -4,8 +4,8 @@
 # https://github.com/actions/virtual-environments/issues/323
 # via
 # https://github.community/t/ubuntu-latest-apt-repository-list-issues/17182/10#M4501
-ls -al /etc/apt/sources.list.d/
 for apt_file in `grep -lr microsoft /etc/apt/sources.list.d/`; do
+    echo "Removing $apt_file"
     sudo rm $apt_file
 done
 
