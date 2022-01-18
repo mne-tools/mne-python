8c9f9e1f878f46c20e578d4db9792e4e4d6fefcb
Eric Larson <larson.eric.d@gmail.com>
2022-01-18 11:01:09 -0500

    MAINT: Bump sample

--- mne/datasets/config.py
+++ mne/datasets/config.py
@@ -168,8 +168,8 @@ MNE_DATASETS['phantom_4dbti'] = dict(
 
 MNE_DATASETS['sample'] = dict(
     archive_name='MNE-sample-data-processed.tar.gz',
-    hash='md5:12b75d1cb7df9dfb4ad73ed82f61094f',
-    url='https://osf.io/86qa2/download?version=5',
+    hash='md5:e8f30c4516abdc12a0c08e6bae57409c',
+    url='https://osf.io/86qa2/download?version=6',
     folder_name='MNE-sample-data',
     config_key='MNE_DATASETS_SAMPLE_PATH',
 )
