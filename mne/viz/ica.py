44db61ce94f36f3fd797d404163bf2207a3a4022
eort <eduardxort@gmail.com>
2022-01-31 14:28:33 +0100

    Update mne/viz/ica.py
    Co-authored-by: Alexandre Gramfort <alexandre.gramfort@m4x.org>

--- mne/viz/ica.py
+++ mne/viz/ica.py
@@ -273,7 +273,7 @@ def plot_ica_properties(ica, inst, picks=None, axes=None, dB=True,
     inst : instance of Epochs or Raw
         The data to use in plotting properties.
     picks : str | list | slice | None
-        Sources to include. Slices and lists of integers will be interpreted
+        Components to include. Slices and lists of integers will be interpreted
         as component indices. None (default) will use the first five components.
         If more than one components were chosen in the picks,
         each one will be plotted in a separate figure.
