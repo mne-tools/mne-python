71b3434e51fcb022e28865b23e9db16c0f65ba1c
Eric Larson <larson.eric.d@gmail.com>
2022-02-08 16:50:29 -0500

    FIX: nilearn

--- doc/conf.py
+++ doc/conf.py
@@ -832,6 +832,7 @@ def reset_warnings(gallery_conf, fname):
                 "default value of type 'dict' in an Any trait will",  # traits
                 'rcParams is deprecated',  # PyVista rcParams -> global_theme
                 'to mean no clipping',
+                'Please use.*from the scipy',
                 '`np.MachAr` is deprecated',  # Numba
                 ):
         warnings.filterwarnings(  # deal with other modules having bad imports
