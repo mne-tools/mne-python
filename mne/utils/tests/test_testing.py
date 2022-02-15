919489a2da08ddb3d70e4056de1ab7c7305c5e6f
Guillaume Favelier <guillaume.favelier@gmail.com>
2022-02-15 15:07:24 +0100

    WIP,MNT: disable 3d options during testing (#10334)
    * update
    
    * fix
    
    * monkeypatch - module
    
    * monkeypatch - function
    
    * Revert "monkeypatch - function"
    
    This reverts commit 876a7c91ae51d63edb54c9dadf1f5d4bb5a513c6.
    
    * Revert "monkeypatch - module"
    
    This reverts commit 59a068796630a119f17269bc0350081068df6d1a.
    
    * FIX: Deprecate modified_env
    
    * FIX: Sty
    
    Co-authored-by: Eric Larson <larson.eric.d@gmail.com>

--- mne/utils/tests/test_testing.py
+++ mne/utils/tests/test_testing.py
@@ -1,10 +1,17 @@
+# -*- coding: utf-8 -*-
+# Authors: Eric Larson <larson.eric.d@gmail.com>
+#
+# License: BSD-3-Clause
+
+import os
 import os.path as op
 
 import numpy as np
 import pytest
 
 from mne.datasets import testing
-from mne.utils import (_TempDir, _url_to_local_path, buggy_mkl_svd)
+from mne.utils import (_TempDir, _url_to_local_path, buggy_mkl_svd,
+                       modified_env)
 
 
 def test_buggy_mkl():
@@ -49,3 +56,20 @@ def test_url_to_local_path():
     """Test URL to local path."""
     assert _url_to_local_path('http://google.com/home/why.html', '.') == \
         op.join('.', 'home', 'why.html')
+
+
+@pytest.mark.filterwarnings('ignore:.*monkeypatch.*:DeprecationWarning')
+def test_modified_env(monkeypatch):
+    """Test modified_env."""
+    monkeypatch.setenv('_MNE_1', 'true')
+    monkeypatch.setenv('_MNE_2', 'truer')
+    assert os.getenv('_MNE_1') == 'true'
+    assert os.getenv('_MNE_2') == 'truer'
+    assert os.getenv('_MNE_3') is None
+    with modified_env(_MNE_1='false', _MNE_2=None, _MNE_3='falsest'):
+        assert os.getenv('_MNE_1') == 'false'
+        assert os.getenv('_MNE_2') is None
+        assert os.getenv('_MNE_3') == 'falsest'
+    assert os.getenv('_MNE_1') == 'true'
+    assert os.getenv('_MNE_2') == 'truer'
+    assert os.getenv('_MNE_3') is None
