0cf14a912995563461a5f5f23cd13961ae971805
Guillaume Favelier <guillaume.favelier@gmail.com>
2022-02-25 12:01:51 +0100

    fix

--- mne/viz/backends/_abstract.py
+++ mne/viz/backends/_abstract.py
@@ -626,7 +626,7 @@ class _AbstractPlayback(ABC):
 class _AbstractDialog(ABC):
     @abstractmethod
     def _dialog_warning(self, title, text, info_text, callback, *,
-                        modal=True, window=None):
+                        buttons=[], modal=True, window=None):
         pass
 
 
