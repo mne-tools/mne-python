7b50d3baa7077a42007d13facc3db639b6dfa19c
Eric Larson <larson.eric.d@gmail.com>
2022-04-11 18:05:30 -0400

    FIX: Try PyQt6 on CircleCI

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -11,7 +11,8 @@ else  # standard doc build
 	echo "Installing doc build dependencies"
 	python -m pip uninstall -y pydata-sphinx-theme
 	python -m pip install --upgrade --progress-bar off --only-binary "numpy,scipy,matplotlib,pandas,statsmodels" -r requirements.txt -r requirements_testing.txt -r requirements_doc.txt
-	python -m pip uninstall -yq sphinx-gallery mne-qt-browser
+	python -m pip uninstall -yq sphinx-gallery mne-qt-browser PySide6
+	pythom -m pip install PyQt6
 	# TODO: Revert to upstream/main once https://github.com/mne-tools/mne-qt-browser/pull/105 is merged
 	python -m pip install --upgrade --progress-bar off https://github.com/mne-tools/mne-qt-browser/zipball/main https://github.com/sphinx-gallery/sphinx-gallery/zipball/master https://github.com/pyvista/pyvista/zipball/main https://github.com/pyvista/pyvistaqt/zipball/main
 	# deal with comparisons and escapes (https://app.circleci.com/pipelines/github/mne-tools/mne-python/9686/workflows/3fd32b47-3254-4812-8b9a-8bab0d646d18/jobs/32934)
