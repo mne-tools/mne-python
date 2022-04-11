6ecff1475adeb498f7d8551b38966acd6031a275
Eric Larson <larson.eric.d@gmail.com>
2022-04-11 11:28:59 -0400

    FIX: Another [circle front]

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -1,17 +1,5 @@
 #!/bin/bash -ef
 
-echo "Working around Qt bugs and installing doc build apt dependencies"
-# https://github.com/ContinuumIO/anaconda-issues/issues/9190#issuecomment-386508136
-# https://github.com/golemfactory/golem/issues/1019
-sudo apt update
-sudo apt install libosmesa6 libglx-mesa0 libopengl0 libglx0 libdbus-1-3 \
-	libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 \
-	libxcb-render-util0 libxcb-shape0 libxcb-xfixes0 libxcb-xinerama0 \
-	graphviz optipng
-if [ ! -f /usr/lib/x86_64-linux-gnu/libxcb-util.so.1 ]; then
-	sudo ln -s /usr/lib/x86_64-linux-gnu/libxcb-util.so.0 /usr/lib/x86_64-linux-gnu/libxcb-util.so.1
-fi
-
 echo "Installing setuptools and sphinx"
 python -m pip install --upgrade "pip!=20.3.0"
 python -m pip install --upgrade --progress-bar off setuptools wheel
