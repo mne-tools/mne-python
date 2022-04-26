1b5e7e2921ec73d8fab73332e906119acb7e24c7
Alessandro Tonin <alessandro.tonin@wysscenter.ch>
2022-04-26 12:09:42 +0200

    Update test using dataset from mne-testing-data

--- mne/datasets/config.py
+++ mne/datasets/config.py
@@ -87,7 +87,7 @@ I agree to the following:
 # respective repos, and make a new release of the dataset on GitHub. Then
 # update the checksum in the MNE_DATASETS dict below, and change version
 # here:                  ↓↓↓↓↓         ↓↓↓
-RELEASES = dict(testing='0.133', misc='0.23')
+RELEASES = dict(testing='0.134', misc='0.23')
 TESTING_VERSIONED = f'mne-testing-data-{RELEASES["testing"]}'
 MISC_VERSIONED = f'mne-misc-data-{RELEASES["misc"]}'
 
@@ -111,7 +111,7 @@ MNE_DATASETS = dict()
 # Testing and misc are at the top as they're updated most often
 MNE_DATASETS['testing'] = dict(
     archive_name=f'{TESTING_VERSIONED}.tar.gz',  # 'mne-testing-data',
-    hash='md5:32173080843e20558f664df1531ae4c6',
+    hash='md5:9d234ff0156e6feccbbbc01469a861eb',
     url=('https://codeload.github.com/mne-tools/mne-testing-data/'
          f'tar.gz/{RELEASES["testing"]}'),
     folder_name='MNE-testing-data',
