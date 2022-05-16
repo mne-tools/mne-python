a344d52f7315eee94f0d4cbd13d91da9ca834e7e
Guillaume Favelier <guillaume.favelier@gmail.com>
2022-05-16 17:21:06 +0200

    update

--- tools/github_actions_dependencies.sh
+++ tools/github_actions_dependencies.sh
@@ -14,8 +14,8 @@ else
 	echo "Date utils"
 	# https://pip.pypa.io/en/latest/user_guide/#possible-ways-to-reduce-backtracking-occurring
 	pip install $STD_ARGS --pre --only-binary ":all:" python-dateutil pytz joblib threadpoolctl six
+	echo "PyQt6"
 	# XXX: the wheels for PyQt6-sip 13.4 are not available yet from https://www.riverbankcomputing.com/pypi/simple
-	# echo "PyQt6"
 	# pip install $STD_ARGS --pre --only-binary ":all:" --no-deps --extra-index-url https://www.riverbankcomputing.com/pypi/simple PyQt6 PyQt6-sip PyQt6-Qt6
 	pip install $STD_ARGS --only-binary ":all:" PyQt6 PyQt6-sip PyQt6-Qt6
 	echo "NumPy/SciPy/pandas etc."
