c7ee1c3dbfa6d92d67c229116151f8837c8f383f
Guillaume Favelier <guillaume.favelier@gmail.com>
2022-05-16 17:18:14 +0200

    fix

--- tools/github_actions_dependencies.sh
+++ tools/github_actions_dependencies.sh
@@ -17,6 +17,7 @@ else
 	# XXX: the wheels for PyQt6-sip 13.4 are not available yet from https://www.riverbankcomputing.com/pypi/simple
 	# echo "PyQt6"
 	# pip install $STD_ARGS --pre --only-binary ":all:" --no-deps --extra-index-url https://www.riverbankcomputing.com/pypi/simple PyQt6 PyQt6-sip PyQt6-Qt6
+	pip install $STD_ARGS --only-binary ":all:" PyQt6 PyQt6-sip PyQt6-Qt6
 	echo "NumPy/SciPy/pandas etc."
 	pip install $STD_ARGS --pre --only-binary ":all:" --no-deps  --default-timeout=60 -i "https://pypi.anaconda.org/scipy-wheels-nightly/simple" numpy scipy pandas scikit-learn statsmodels dipy
 	echo "H5py, pillow, matplotlib"
