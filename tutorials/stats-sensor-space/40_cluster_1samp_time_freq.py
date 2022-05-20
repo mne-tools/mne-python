a6e0f16cc1142b8b6275e50f50c4d47813c0c55f
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2022-05-20 16:48:32 +0200

    DOC: make tutorial on clusters more explicit

--- tutorials/stats-sensor-space/40_cluster_1samp_time_freq.py
+++ tutorials/stats-sensor-space/40_cluster_1samp_time_freq.py
@@ -18,8 +18,15 @@ The procedure consists of:
   - baseline line correct the power estimates (power ratios)
   - compute stats to see if ratio deviates from 1.
 
+Here, the unit of observation is epochs from a specific study subject.
+However, the same logic applies when the unit observation is
+a number of study subject each of whom contribute their own averaged
+data (i.e., an average of their epochs). This would then be considered
+an analysis as the "2nd level".
+
 """
 # Authors: Alexandre Gramfort <alexandre.gramfort@inria.fr>
+#          Stefan Appelhoff <stefan.appelhoff@mailbox.org>
 #
 # License: BSD-3-Clause
 
@@ -27,13 +34,12 @@ The procedure consists of:
 
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.stats
 
 import mne
 from mne.time_frequency import tfr_morlet
 from mne.stats import permutation_cluster_1samp_test
 from mne.datasets import sample
-
-print(__doc__)
 
 # %%
 # Set parameters
@@ -59,6 +65,7 @@ event_id = 1
 epochs = mne.Epochs(raw, events, event_id, tmin, tmax, picks=picks,
                     baseline=(None, 0), preload=True,
                     reject=dict(grad=4000e-13, eog=150e-6))
+
 # just use right temporal sensors for speed
 epochs.pick_channels(mne.read_vectorview_selection('Right-temporal'))
 evoked = epochs.average()
@@ -69,8 +76,11 @@ evoked = epochs.average()
 # operations such as nonparametric statistics) if you don't need high
 # spectrotemporal resolution.
 decim = 5
-freqs = np.arange(8, 40, 2)  # define frequencies of interest
-sfreq = raw.info['sfreq']  # sampling in Hz
+
+# define frequencies of interest
+freqs = np.arange(8, 40, 2)
+
+# run the TFR decomposition
 tfr_epochs = tfr_morlet(epochs, freqs, n_cycles=4., decim=decim,
                         average=False, return_itc=False, n_jobs=None)
 
@@ -87,59 +97,102 @@ epochs_power = tfr_epochs.data
 # Define adjacency for statistics
 # -------------------------------
 # To compute a cluster-corrected value, we need a suitable definition
-# for the adjacency/adjacency of our values. So we first compute the
-# sensor adjacency, then combine that with a grid/lattice adjacency
-# assumption for the time-frequency plane:
+# for the adjacency of sensors, time points, and frequency bins.
+# These adjacencies will be used to form clusters.
+#
+# We first compute the sensor adjacency, and then combine that with a
+# "lattice" adjacency for the time-frequency plane, which assumes
+# that elements at index "N" are adjacent to elements at indices
+# "N+1" and "N-1" (forming a "grid" on the time-frequency plane).
 
+# find_ch_adjacency first attempts to find an existing "neighbor"
+# (adjacency) file for the present data to read.
+# If such a file doesn't exist, adjacencies are computed on the fly,
+# using Delaunay triangulations.
 sensor_adjacency, ch_names = mne.channels.find_ch_adjacency(
     tfr_epochs.info, 'grad')
-# Subselect the channels we are actually using
+
+# In this case, find_ch_adjacency finds an appropriate file and
+# reads it (see log output: "neuromag306planar").
+# However, we need to subselect the channels we are actually using
 use_idx = [ch_names.index(ch_name.replace(' ', ''))
            for ch_name in tfr_epochs.ch_names]
 sensor_adjacency = sensor_adjacency[use_idx][:, use_idx]
+
+# Our sensor adjacencies are of shape n_chs X n_chs
 assert sensor_adjacency.shape == \
     (len(tfr_epochs.ch_names), len(tfr_epochs.ch_names))
+
+# Now we need to prepare adjacency information for the time-frequency
+# plane. For that, we use "combine_adjacency", and pass dimensions
+# as in the data we want to test. Here:
+# observations X channels X frequencies X times
+# (for observations, no adjacency is specified)
 assert epochs_power.data.shape == (
     len(epochs), len(tfr_epochs.ch_names),
     len(tfr_epochs.freqs), len(tfr_epochs.times))
 adjacency = mne.stats.combine_adjacency(
     sensor_adjacency, len(tfr_epochs.freqs), len(tfr_epochs.times))
 
-# our adjacency is square with each dim matching the data size
+# The overall adjacency we end up with is a square matrix with each
+# dimension matching the data size (excluding observations)
 assert adjacency.shape[0] == adjacency.shape[1] == \
     len(tfr_epochs.ch_names) * len(tfr_epochs.freqs) * len(tfr_epochs.times)
 
 # %%
 # Compute statistic
 # -----------------
-threshold = 3.
-n_permutations = 50  # Warning: 50 is way too small for real-world analysis.
+# For forming clusters, we need to specify a critical test statistic threshold
+# beyond which data bins are considered statistically significant. Here, we
+# use a t-test and can make use of Scipy's perfent point function of the t
+# distribution to get a t-value that corresponds to a specific alpha level
+# for significance. This threshold is often called the
+# "cluster forming threshold".
+
+# We want a two-tailed test
+tail = 0
+
+# We arbitrarily pick an alpha level of 0.001 and divide it by 2, because we
+# have a two-tailed test and need to look at both tails of the distribution.
+# As the degrees of freedom, we specify the number of observations
+# (here: epochs) minus 1.
+# Finally, we subtract 0.001 / 2 from 1, to get a positive critical
+# t-value (this is needed for MNE-Python internals)
+degrees_of_freedom = len(epochs) - 1
+t_thresh = scipy.stats.t.ppf(1 - 0.001 / 2, df=degrees_of_freedom)
+
+# Set the number of permutations to run.
+# Warning: 50 is way too small for real-world analysis, but here we use this
+# for increased computation speed.
+n_permutations = 50
+
+# Run the analysis
 T_obs, clusters, cluster_p_values, H0 = \
     permutation_cluster_1samp_test(epochs_power, n_permutations=n_permutations,
-                                   threshold=threshold, tail=0,
+                                   threshold=t_thresh, tail=tail,
                                    adjacency=adjacency,
                                    out_type='mask', verbose=True)
 
 # %%
 # View time-frequency plots
 # -------------------------
-
 evoked_data = evoked.data
 times = 1e3 * evoked.times
 
 plt.figure()
 plt.subplots_adjust(0.12, 0.08, 0.96, 0.94, 0.2, 0.43)
 
-# Create new stats image with only significant clusters
+# Create new stats image with only significant clusters at alpha level 0.05
 T_obs_plot = np.nan * np.ones_like(T_obs)
 for c, p_val in zip(clusters, cluster_p_values):
     if p_val <= 0.05:
         T_obs_plot[c] = T_obs[c]
 
 # Just plot one channel's data
+# use the following to show a specific one:
+# ch_idx = tfr_epochs.ch_names.index('MEG 1332')
 ch_idx, f_idx, t_idx = np.unravel_index(
     np.nanargmax(np.abs(T_obs_plot)), epochs_power.shape[1:])
-# ch_idx = tfr_epochs.ch_names.index('MEG 1332')  # to show a specific one
 
 vmax = np.max(np.abs(T_obs))
 vmin = -vmax
