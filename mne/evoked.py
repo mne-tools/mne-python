Fix sign check in Evoked.get_peak()
We didn't apply the mask to the data and hence the sign check
didn't always fire when it was supposed to.

This issue was reported on the forum:
https://mne.discourse.group/t/get-peak-mode-pos-also-gives-negative-values/4931

mne/evoked.py
@@ -1552,11 +1552,11 @@ def _get_peak(data, times, tmin=None, tmax=None, mode='abs'):
 
     maxfun = np.argmax
     if mode == 'pos':
-        if not np.any(data > 0):
+        if not np.any(data[~mask] > 0):
             raise ValueError('No positive values encountered. Cannot '
                              'operate in pos mode.')
     elif mode == 'neg':
-        if not np.any(data < 0):
+        if not np.any(data[~mask] < 0):
             raise ValueError('No negative values encountered. Cannot '
                              'operate in neg mode.')
         maxfun = np.argmin