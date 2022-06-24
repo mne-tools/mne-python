75433c5a445cb0103163ee61e981f57ffef1acb7
Alex <aprockhill@mailbox.org>
2022-06-24 11:19:49 -0700

    remove, do in separate PR

--- mne/conftest.py
+++ mne/conftest.py
@@ -164,7 +164,6 @@ def pytest_configure(config):
     ignore:.*Setting theme=.*:RuntimeWarning
     # scikit-learn using this arg
     ignore:.*The 'sym_pos' keyword is deprecated.*:DeprecationWarning
-    ignore:is obsoleted by Node:PendingDeprecationWarning
     """  # noqa: E501
     for warning_line in warning_lines.split('\n'):
         warning_line = warning_line.strip()
