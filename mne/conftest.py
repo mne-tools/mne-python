391ca91b3e2acc4e17b1dd436453a668ea9e1242
Eric Larson <larson.eric.d@gmail.com>
2022-06-27 16:35:09 -0400

    FIX: Last hopefully

--- mne/conftest.py
+++ mne/conftest.py
@@ -115,6 +115,8 @@ def pytest_configure(config):
     ignore:.*Setting theme=.*:RuntimeWarning
     # scikit-learn using this arg
     ignore:.*The 'sym_pos' keyword is deprecated.*:DeprecationWarning
+    # Should be removable by 2022/07/08, SciPy savemat issue
+    ignore:.*elementwise comparison failed; returning scalar in.*:FutureWarning
     """.format(first_kind)  # noqa: E501
     for warning_line in warning_lines.split('\n'):
         warning_line = warning_line.strip()
