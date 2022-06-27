13c90939345bb6a3bd0a8d6703d9b8384969429e
Carina Forster <carinaforster0611@gmail.com>
2022-06-27 15:00:55 +0200

    added dashes

--- examples/io/read_noise_covariance_matrix.py
+++ examples/io/read_noise_covariance_matrix.py
@@ -18,8 +18,8 @@ import mne
 from mne.datasets import sample
 
 data_path = sample.data_path()
-fname_cov = data_path / 'MEG', 'sample', 'sample_audvis-cov.fif'
-fname_evo = data_path / 'MEG', 'sample', 'sample_audvis-ave.fif'
+fname_cov = data_path / 'MEG' / 'sample' / 'sample_audvis-cov.fif'
+fname_evo = data_path / 'MEG' / 'sample' / 'sample_audvis-ave.fif'
 
 cov = mne.read_cov(fname_cov)
 print(cov)
