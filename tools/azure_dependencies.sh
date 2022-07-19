feba77471d20ea30adfbcd286035ba6b88a306ac
Alex <aprockhill@mailbox.org>
2022-07-19 13:53:06 -0700

    revert pyvista latest version just to be extra sure

--- tools/azure_dependencies.sh
+++ tools/azure_dependencies.sh
@@ -7,8 +7,6 @@ if [ "${TEST_MODE}" == "pip" ]; then
 	python -m pip install --upgrade --only-binary="numba,llvmlite" -r requirements.txt
 	# This can be removed once PyVistaQt 0.6 is out (including https://github.com/pyvista/pyvistaqt/pull/127)
 	python -m pip install --upgrade https://github.com/pyvista/pyvistaqt/zipball/main
-	# Temporary workaround until the fix is released
-	python -m pip install --progress-bar off https://github.com/pyvista/pyvista/zipball/main
 elif [ "${TEST_MODE}" == "pip-pre" ]; then
 	python -m pip install --progress-bar off --upgrade pip setuptools wheel
 	python -m pip install --progress-bar off --upgrade --pre --only-binary ":all:" python-dateutil pytz joblib threadpoolctl six cycler kiwisolver pyparsing patsy
