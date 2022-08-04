ed7b48f56208550d4b7781701218f334eb1b9e01
Clemens Brunner <clemens.brunner@gmail.com>
2022-08-04 16:36:39 +0200

    Nest scipy.io imports

--- mne/io/eeglab/eeglab.py
+++ mne/io/eeglab/eeglab.py
@@ -7,12 +7,6 @@
 import os.path as op
 
 import numpy as np
-from scipy.io import loadmat
-try:
-    from scipy.io.matlab import MatlabOpaque, MatlabFunction
-except ImportError:  # scipy < 1.8
-    from scipy.io.matlab.mio5_params import MatlabOpaque
-    from scipy.io.matlab.mio5 import MatlabFunction
 
 from ..pick import _PICK_TYPES_KEYS
 from ..utils import _read_segments_file, _find_channels
@@ -53,6 +47,11 @@ def _todict_from_np_struct(data):  # taken from pymatreader.utils
 
 
 def _handle_scipy_ndarray(data):  # taken from pymatreader.utils
+    try:
+        from scipy.io.matlab import MatlabFunction
+    except ImportError:  # scipy < 1.8
+        from scipy.io.matlab.mio5 import MatlabFunction
+
     if data.dtype == np.dtype('object') and not \
             isinstance(data, MatlabFunction):
         as_list = []
@@ -71,6 +70,11 @@ def _handle_scipy_ndarray(data):  # taken from pymatreader.utils
 
 def _check_for_scipy_mat_struct(data):  # taken from pymatreader.utils
     """Convert all scipy.io.matlab.mio5_params.mat_struct elements."""
+    try:
+        from scipy.io.matlab import MatlabOpaque
+    except ImportError:  # scipy < 1.8
+        from scipy.io.matlab.mio5_params import MatlabOpaque
+
     if isinstance(data, dict):
         for key in data:
             data[key] = _check_for_scipy_mat_struct(data[key])
@@ -124,6 +128,7 @@ def _check_load_mat(fname, uint16_codec):
     try:
         read_mat = _import_pymatreader_funcs('EEGLAB I/O')
     except RuntimeError:  # pymatreader not installed
+        from scipy.io import loadmat
         eeg = loadmat(fname, squeeze_me=True, mat_dtype=False)
         eeg = _check_for_scipy_mat_struct(eeg)
     else:
