45508793e9909ea457702be35c4937c1d8a4a1a5
Daniel McCloy <dan@mccloy.info>
2022-08-17 14:10:40 -0500

    test upstream fix [skip azp][skip actions]

--- doc/conf.py
+++ doc/conf.py
@@ -621,6 +621,7 @@ html_theme_options = {
     'show_toc_level': 1,
     'navbar_end': ['theme-switcher', 'version-switcher', 'navbar-icon-links'],
     'footer_items': ['copyright'],
+    'page_sidebar_items': ['page-toc'],
     'analytics': dict(google_analytics_id='UA-37225609-1'),
     'switcher': {
         'json_url': 'https://mne.tools/dev/_static/versions.json',
