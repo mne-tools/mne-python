6a8168811ecf6cd93c07880329270c88083dfe3e
Eric Larson <larson.eric.d@gmail.com>
2022-08-22 00:12:47 +0100

    FIX: Circle, too

--- doc/conf.py
+++ doc/conf.py
@@ -922,6 +922,12 @@ def reset_warnings(gallery_conf, fname):
     ):
         warnings.filterwarnings(  # deal with other modules having bad imports
             'ignore', message=".*%s.*" % key, category=DeprecationWarning)
+    # matplotlib 3.6
+    # gh-11063, gh-11062
+    warnings.filterwarnings(
+        'ignore', message='.*event function was deprecated in Matplotlib.*')
+    warnings.filterwarnings(
+        'ignore', message='.*cmap function will be deprecated.*')
     # xarray/netcdf4
     warnings.filterwarnings(
         'ignore', message=r'numpy\.ndarray size changed, may indicate.*',
