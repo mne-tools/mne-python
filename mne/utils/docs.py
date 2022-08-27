6cebb1332a5e33c9a4e4d6999625a4cc585803e1
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2022-08-27 09:40:46 +0100

    ENH : add units parameter to read_raw_edf in case units is missing from the file (#11099)
    * ENH : add units parameter to read_raw_edf in case units is missing from the file
    
    * cleanup
    
    * update what's new + flake8
    
    * adding units to bdf too
    
    * fix docstring

--- mne/utils/docs.py
+++ mne/utils/docs.py
@@ -3508,6 +3508,14 @@ units : str | dict | None
     channel-type-specific default unit.
 """
 
+docdict['units_edf_bdf_io'] = """
+units : dict | str
+    The units of the channels as stored in the file. This argument
+    is useful only if the units are missing from the original file.
+    If a dict, it must map a channel name to its unit, and if str
+    it is assumed that all channels have the same units.
+"""
+
 docdict['units_topomap'] = """
 units : dict | str | None
     The unit of the channel type used for colorbar label. If
