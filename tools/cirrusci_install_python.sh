65a812e024931a34781f391089a7be98574c37c7
Eric Larson <larson.eric.d@gmail.com>
2022-09-27 12:10:26 -0400

    TST: Try again [skip azp] [skip actions] [skip circle]

--- tools/cirrusci_install_python.sh
+++ tools/cirrusci_install_python.sh
@@ -2,8 +2,8 @@
 
 if ! -f /opt/homebrew/opt/python@3.10/bin/python; then
     brew install python@3.10
-    ln -s python3 /opt/homebrew/opt/python@3.10/bin/python
-    ln -s pip3 /opt/homebrew/opt/python@3.10/bin/pip
+    sudo ln -s python3 /opt/homebrew/opt/python@3.10/bin/python
+    sudo ln -s pip3 /opt/homebrew/opt/python@3.10/bin/pip
 fi
 which python
 python -c "import sys; assert sys.version.startswith('3.10')"
