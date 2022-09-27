05256ff92b30d5e59badb8d29a97c44909d98806
Eric Larson <larson.eric.d@gmail.com>
2022-09-27 12:59:29 -0400

    TST: Fix conditional [skip azp] [skip actions] [skip circle]

--- tools/cirrus_install_python.sh
+++ tools/cirrus_install_python.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-if ! -f /opt/homebrew/opt/python@3.10/bin/python; then
+if [[ ! -f /opt/homebrew/opt/python@3.10/bin/python ]]; then
     brew install python@3.10
     sudo ln -s python3 /opt/homebrew/opt/python@3.10/bin/python
     sudo ln -s pip3 /opt/homebrew/opt/python@3.10/bin/pip
