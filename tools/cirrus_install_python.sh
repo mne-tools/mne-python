d72d47be1fb67facab591568abeb3ee8b9cb86b1
Eric Larson <larson.eric.d@gmail.com>
2022-09-27 13:00:35 -0400

    TST: Full

--- tools/cirrus_install_python.sh
+++ tools/cirrus_install_python.sh
@@ -2,8 +2,6 @@
 
 if [[ ! -f /opt/homebrew/opt/python@3.10/bin/python ]]; then
     brew install python@3.10
-    sudo ln -s python3 /opt/homebrew/opt/python@3.10/bin/python
-    sudo ln -s pip3 /opt/homebrew/opt/python@3.10/bin/pip
 fi
 which python
 python -c "import sys; assert sys.version.startswith('3.10')"
