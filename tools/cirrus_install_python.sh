435234afed345624100260098942e92c9fefb25e
Eric Larson <larson.eric.d@gmail.com>
2022-09-27 14:41:27 -0400

    Update cirrus_install_python.sh

--- tools/cirrus_install_python.sh
+++ tools/cirrus_install_python.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -e
 
-if [[ ! -f /opt/homebrew/opt/python@3.10/bin/python ]]; then
-    brew install python@3.10
-fi
+brew install python@3.10 ffmpeg
 which python
 python -c "import sys; assert sys.version.startswith('3.10')"
 which pip
