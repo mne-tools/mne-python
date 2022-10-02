86a488834ac781373f43638bb5fbd758307e62f4
Daniel Carlström Schad <daniel.c.schad@protonmail.com>
2022-10-02 10:43:41 +0200

    Update time_frequency for sklearn conformity

--- mne/decoding/tests/test_time_frequency.py
+++ mne/decoding/tests/test_time_frequency.py
@@ -7,7 +7,7 @@ import numpy as np
 from numpy.testing import assert_array_equal
 import pytest
 
-from mne.utils import requires_sklearn
+from mne.utils import requires_sklearn, _check_sklearn_estimator
 from mne.decoding.time_frequency import TimeFrequency
 
 
@@ -20,13 +20,13 @@ def test_timefrequency():
     freqs = [20, 21, 22]
     tf = TimeFrequency(freqs, sfreq=100)
     for output in ['avg_power', 'foo', None]:
-        pytest.raises(ValueError, TimeFrequency, freqs, output=output)
-    tf = clone(tf)
+        pytest.raises(ValueError, TimeFrequency(freqs, output=output).fit, np.random.rand(10, 2, 100))
+    _check_sklearn_estimator(tf)
 
     # Clone estimator
     freqs_array = np.array(np.asarray(freqs))
     tf = TimeFrequency(freqs_array, 100, "morlet", freqs_array / 5.)
-    clone(tf)
+    _check_sklearn_estimator(tf)
 
     # Fit
     n_epochs, n_chans, n_times = 10, 2, 100
