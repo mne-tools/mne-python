f7650eb7f2e2c1fc7d9ea149f69ce916f7aa37e7
yuval.realpe <yuval.realpe@gmail.com>
2022-12-01 20:11:15 +0200

    the entire code for Alternate Projections:   _ap(main function) , ap(usage example) , test_ap(test file) , __init__ (adding _ap call) , references (adding 2 paper for reference)

--- mne/beamformer/__init__.py
+++ mne/beamformer/__init__.py
@@ -5,5 +5,6 @@ from ._lcmv import (make_lcmv, apply_lcmv, apply_lcmv_epochs, apply_lcmv_raw,
 from ._dics import (make_dics, apply_dics, apply_dics_epochs,
                     apply_dics_tfr_epochs, apply_dics_csd)
 from ._rap_music import rap_music
+from ._ap import ap
 from ._compute_beamformer import Beamformer, read_beamformer
 from .resolution_matrix import make_lcmv_resolution_matrix
