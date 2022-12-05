BUG: Fix normalization bug

mne/time_frequency/tests/test_csd.py
@@ -357,9 +357,9 @@ def _test_csd_matrix(csd):
     assert np.all(np.tril(csd_22).T.conj() == np.triu(csd_22))
 
     # Off-diagonals show phase difference
-    assert np.abs(csd_10[0, 1].imag) > 0.4
-    assert np.abs(csd_10[0, 2].imag) > 0.4
-    assert np.abs(csd_10[1, 2].imag) > 0.4
+    assert np.abs(csd_10[0, 1].imag) > 0.2
+    assert np.abs(csd_10[0, 2].imag) > 0.2
+    assert np.abs(csd_10[1, 2].imag) > 0.2
 
     # No phase differences at 22 Hz
     assert np.all(np.abs(csd_22[0, 2].imag) < 1E-3)

mne/time_frequency/tests/test_tfr.py
@@ -53,13 +53,13 @@ def test_morlet(sfreq, freq, n_cycles):
     else:
         assert np.abs(np.mean(np.real(W))) < 1e-5
 
-    assert_allclose(np.linalg.norm(W), np.sqrt(2))  # different norm!
+    assert_allclose(np.linalg.norm(W), 1., atol=1e-6)
 
     # Convert to SciPy nomenclature and compare
     M = len(W)
     w = n_cycles
     s = w * sfreq / (2 * freq * np.pi)  # from SciPy docs
-    Ws = morlet2(M, s, w) * np.sqrt(2)
+    Ws = morlet2(M, s, w)
     assert_allclose(W, Ws)
 
     # Check FWHM
@@ -124,7 +124,8 @@ def test_time_frequency():
                    freqs=freqs, n_cycles=n_cycles, use_fft=True,
                    return_itc=False, picks=picks, average=False)
     assert_allclose(
-        epochs_power_picks.data[0, 0, 0, 0], 9.130315e-23, rtol=1e-4)
+        epochs_power_picks.data[0, 0, 0, 0], 9.130315e-23 / 2.,
+        rtol=1e-4)
     power_picks_avg = epochs_power_picks.average()
     # the actual data arrays here are equivalent, too...
     assert_allclose(power.data, power_picks.data)

mne/time_frequency/tfr.py
@@ -97,7 +97,6 @@ def morlet(sfreq, freqs, n_cycles=7.0, sigma=None, zero_mean=False):
         >>> M, w = len(wavelet), n_cycles # convert to SciPy convention
         >>> s = w * sfreq / (2 * freq * np.pi)  # from SciPy docs
         >>> wavelet_sp = sp_morlet(M, s, w)
-        >>> wavelet_sp *= np.sqrt(2)  # match MNE's normalization
         >>> _, ax = plt.subplots(constrained_layout=True)
         >>> colors = {
         ...     ('MNE', 'real'): '#66CCEE',
@@ -156,7 +155,7 @@ def morlet(sfreq, freqs, n_cycles=7.0, sigma=None, zero_mean=False):
             real_offset = np.exp(- 2 * (np.pi * f * sigma_t) ** 2)
             oscillation -= real_offset
         W = oscillation * gaussian_enveloppe
-        W /= np.sqrt(0.5) * np.linalg.norm(W.ravel())
+        W /= np.linalg.norm(W.ravel())
         Ws.append(W)
     if singleton:
         Ws = Ws[0]