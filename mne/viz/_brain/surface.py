3d51185b16b34d5f8fb837e737d0cc65654ab88a
Alex Rockhill <aprockhill@mailbox.org>
2022-12-08 15:28:50 -0800

    [BUG][MRG] Fix surface finding for mne.viz.Brain

--- mne/viz/_brain/surface.py
+++ mne/viz/_brain/surface.py
@@ -121,9 +121,15 @@ class _Surface(object):
             coords = coords[:, [1, 0, 2]]
             coords[:, 1] *= -1
         else:
-            coords, faces = read_surface(
-                path.join(self.data_path, 'surf',
-                          '%s.%s' % (self.hemi, self.surf)))
+            # allow ?h.pial.T1 if ?h.pial doesn't exist for instance
+            # end with '' for better file not found error
+            for img in ('', 'T1', 'T2', ''):
+                surf_fname = path.join(self.data_path, 'surf',
+                                       '.'.join((self.hemi, self.surf, img)
+                                                ).strip('.'))
+                if path.isfile(surf_fname):
+                    break
+            coords, faces = read_surface(surf_fname)
             orig_faces = faces
         if self.units == 'm':
             coords /= 1000.
