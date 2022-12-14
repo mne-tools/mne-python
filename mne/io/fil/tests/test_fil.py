30196ed554d8df52fb0ba57fc976cb943937dff6
George O'Neill <g.o'neill@ucl.ac.uk>
2022-12-14 00:35:25 +0000

    FIX: Test script revert to old scipy.io.readmat

--- mne/io/fil/tests/test_fil.py
+++ mne/io/fil/tests/test_fil.py
@@ -12,10 +12,29 @@ from mne.io import read_raw_fil
 from mne.io.fil.sensors import _get_pos_units
 from mne.io.pick import pick_types
 
-from pymatreader import read_mat
+import scipy.io
 
 
 fil_path = testing.data_path(download=False) / 'FIL'
+
+
+def unpack_mat(matin):
+    """Extract relevant entries from unstructred readmat."""
+    data = matin['data']
+    grad = data[0][0]['grad']
+    label = list()
+    coil_label = list()
+    for ii in range(len(data[0][0]['label'])):
+        label.append(str(data[0][0]['label'][ii][0][0]))
+    for ii in range(len(grad[0][0]['label'])):
+        coil_label.append(str(grad[0][0]['label'][ii][0][0]))
+
+    matout = {'label': label,
+              'trial': data['trial'][0][0][0][0],
+              'coil_label': coil_label,
+              'coil_pos': grad[0][0]['coilpos'],
+              'coil_ori': grad[0][0]['coilori']}
+    return matout
 
 
 def _match_str(A_list, B_list):
@@ -43,7 +62,7 @@ def _fil_megmag(raw_test, raw_mat):
     test_inds = pick_types(raw_test.info, meg="mag",
                            ref_meg=False, exclude="bads")
     test_list = list(raw_test.info["ch_names"][i] for i in test_inds)
-    mat_list = list(raw_mat["data"]["label"])
+    mat_list = raw_mat["label"]
     mat_inds = _match_str(test_list, mat_list)
 
     assert len(mat_inds) == len(
@@ -51,7 +70,7 @@ def _fil_megmag(raw_test, raw_mat):
     ), "Number of magnetometer channels in RAW does not match .mat file!"
 
     a = raw_test._data[test_inds, :]
-    b = raw_mat["data"]["trial"][mat_inds, :] * 1e-15  # fT to T
+    b = raw_mat["trial"][mat_inds, :] * 1e-15  # fT to T
 
     assert_array_equal(a, b)
 
@@ -62,7 +81,7 @@ def _fil_stim(raw_test, raw_mat):
         raw_test.info, meg=False, ref_meg=False, stim=True, exclude="bads"
     )
     test_list = list(raw_test.info["ch_names"][i] for i in test_inds)
-    mat_list = list(raw_mat["data"]["label"])
+    mat_list = raw_mat["label"]
     mat_inds = _match_str(test_list, mat_list)
 
     assert len(mat_inds) == len(
@@ -70,7 +89,7 @@ def _fil_stim(raw_test, raw_mat):
     ), "Number of stim channels in RAW does not match .mat file!"
 
     a = raw_test._data[test_inds, :]
-    b = raw_mat["data"]["trial"][mat_inds, :]  # fT to T
+    b = raw_mat["trial"][mat_inds, :]  # fT to T
 
     assert_array_equal(a, b)
 
@@ -78,15 +97,15 @@ def _fil_stim(raw_test, raw_mat):
 def _fil_sensorpos(raw_test, raw_mat):
     """Test the sensor positions/orientations."""
     test_list, test_inds = _get_channels_with_positions(raw_test.info)
-    grad_list = list(raw_mat["data"]["grad"]["label"])
+    grad_list = raw_mat["coil_label"]
     grad_inds = _match_str(test_list, grad_list)
 
     assert len(grad_inds) == len(
         test_inds
     ), "Number of channels with position data in RAW does not match .mat file!"
 
-    mat_pos = raw_mat["data"]["grad"]["coilpos"][grad_inds, :]
-    mat_ori = raw_mat["data"]["grad"]["coilori"][grad_inds, :]
+    mat_pos = raw_mat["coil_pos"][grad_inds, :]
+    mat_ori = raw_mat["coil_ori"][grad_inds, :]
     _, sf1 = _get_pos_units(mat_pos)
 
     test_pos = empty((len(test_inds), 3))
@@ -111,7 +130,8 @@ def test_fil_all():
 
     raw = read_raw_fil(binname)
     raw.load_data(verbose=False)
-    mat = read_mat(matname)
+    tmp = scipy.io.loadmat(matname)
+    mat = unpack_mat(tmp)
 
     _fil_megmag(raw, mat)
     _fil_stim(raw, mat)
