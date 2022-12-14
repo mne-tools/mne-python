1c7904243a27a0e97ccec3cb1aa6d30995be593a
Eric Larson <larson.eric.d@gmail.com>
2022-12-14 13:43:12 -0500

    FIX: Coverage

--- mne/io/tests/test_read_raw.py
+++ mne/io/tests/test_read_raw.py
@@ -17,9 +17,18 @@ test_base = Path(testing.data_path(download=False))
 
 
 @pytest.mark.parametrize('fname', ['x.xxx', 'x'])
-def test_read_raw_unsupported(fname):
+def test_read_raw_unsupported_single(fname):
     """Test handling of unsupported file types."""
     with pytest.raises(ValueError, match='Unsupported file type'):
+        read_raw(fname)
+
+
+@pytest.mark.parametrize('fname', ['x.bin'])
+def test_read_raw_unsupported_multi(fname, tmp_path):
+    """Test handling of supported file types but with bad data."""
+    fname = tmp_path / fname
+    fname.write_text('')
+    with pytest.raises(RuntimeError, match='Could not read.*using any'):
         read_raw(fname)
 
 
