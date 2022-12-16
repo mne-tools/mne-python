63b4f1a62b9f6c2381fff11de7c388a36876956e
Mark <mah@optoceutics.com>
2022-12-16 13:52:46 +0100

    Moved check and conversion of sex to private method

--- mne/export/_edf.py
+++ mne/export/_edf.py
@@ -18,24 +18,10 @@ def _try_to_set_value(header, key, value, channel_index=None):
     # for example "setPatientName()"
     func_name = f'set{key}'
     func = getattr(header, func_name)
-
-    # The EDFwriter.setPatientGender method only accepts sex specification by
-    # integer options of 0 (f), 1 (m), 2 (NA / other / unk.).
-    # If sex is specified as strings, convert them from BIDS compatible
-    # options to their respective integer encoding.
-    if func_name == "setPatientGender":
-        if isinstance(value, str):
-            sex_translation_table = dict({
-                "f": 0, "F": 0, "female": 0, "Female": 0, "FEMALE": 0,
-                "m": 1, "M": 1, "male": 1, "Male": 1, "MALE": 1,
-                "o": 2, "O": 2, "other": 2, "Other": 2, "OTHER": 2,
-            })
-            # If sex definition is not from the set of BIDS-compatible
-            # options, raise a KeyError with this detail.
-            try:
-                value = sex_translation_table[value]
-            except KeyError:
-                raise KeyError(f"Provided perticipant sex information, '{value}', not a BIDS-compitible option.\n")
+    
+    # Some setter functions require different encoding of
+    # `value`. Check and convert if needed.
+    value = _check_value_compatibility(func_name, value)
 
     # some setter functions are indexed by channels
     if channel_index is None:
@@ -298,3 +284,29 @@ def _export_raw(fname, raw, physical_range, add_ch_type):
                     raise RuntimeError(f'writeAnnotation() returned an error '
                                        f'trying to write {desc} at {onset} '
                                        f'for {duration} seconds.')
+
+def _check_value_compatibility(func_name: str, value: str) -> str:
+    """Check compatibility of value in EDF header"""
+    # The EDFwriter.setPatientGender method only accepts sex specification by
+    # integer options of 0 (f), 1 (m), 2 (NA / other / unk.).
+    # If BIDS-compatible string values for indicating phenotypical sex
+    # are provided, convert these to their EDFlib-compatible integer encoding.
+
+    if not func_name == "setPatientGender":
+        return value
+
+    if isinstance(value, str):
+        sex_translation_table = dict({
+            "f": 0, "F": 0, "female": 0, "Female": 0, "FEMALE": 0,
+            "m": 1, "M": 1, "male": 1, "Male": 1, "MALE": 1,
+            "o": 2, "O": 2, "other": 2, "Other": 2, "OTHER": 2,
+        })
+        # If sex definition is not from the set of BIDS-compatible
+        # options, raise a KeyError with this detail.
+        try:
+            value = sex_translation_table[value]
+        except KeyError:
+            raise KeyError(f"Provided perticipant sex information, '{value}', not a BIDS-compitible option.\n")
+
+    return value
+    
