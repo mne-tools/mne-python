b5365dc6916803f3df694e50243c520c7f94a48f
George O'Neill <g.o'neill@ucl.ac.uk>
2023-01-05 23:31:58 +0000

    Complete tutorial basics.

--- tutorials/preprocessing/80_opm_processing.py
+++ tutorials/preprocessing/80_opm_processing.py
@@ -42,20 +42,20 @@ raw.crop(120, 240).load_data()  # crop for speed
 # ------------------
 #
 # First, let's look at the raw data, noting that there are large fluctuations
-# in the sub 1 Hz band. In some cases the range of fields a single sensor
-# sensor is as much as 600 pT.
+# in the sub 1 Hz band. In some cases the range of fields a single channel
+# reports is as much as 600 pT across this experiment.
 
 picks = mne.pick_types(raw.info, meg=True)
 
 amp_scale = 1e12  # T->pT
 stop = len(raw.times) - 300
 step = 300
-data_ds, time_ds = raw[picks, :stop]
+data_ds, time_ds = raw[picks[::5], :stop]
 data_ds, time_ds = data_ds[:, ::step] * amp_scale, time_ds[::step]
 
 fig, ax = plt.subplots(constrained_layout=True)
 plot_kwargs = dict(lw=1, alpha=0.5)
-ax.plot(time_ds, data_ds.T, **plot_kwargs)
+ax.plot(time_ds, data_ds.T - np.mean(data_ds, axis=1), **plot_kwargs)
 ax.grid(True)
 set_kwargs = dict(ylim=(-500, 500), xlim=time_ds[[0, -1]],
                   xlabel='Time (s)', ylabel='Amplitude (pT)')
@@ -65,17 +65,18 @@ ax.set(title='No preprocessing', **set_kwargs)
 # Denoising: Regressing via reference sensors
 # -------------------------------------------
 #
-# The simplest method for reducing the low frequency drift in the data is to
+# The simplest method for reducing low frequency drift in the data is to
 # use a set of reference sensors away from the scalp, which only sample the
 # ambient fields in the room. An advantage of this method is that no prior
-# knowldge of the locations of the sensors is required, however it assumes that
-# the reference sensors experience the same interference as scalp recordings.
+# knowldge of the locations of the sensors is required. However, it assumes
+# that the reference sensors experience the same interference as scalp
+# recordings.
 #
-# To do this in our current dataset we require a bit of housekeeping.
+# To do this in our current dataset, we require a bit of housekeeping.
 # There are a set of channels beginning with the name "Flux" which do not
 # contain any evironmental data, these need to be set to as bad channels.
 #
-# For now we are only interested in removing artefacts seen below 2 Hz, so we
+# For now we are only interested in removing artefacts seen below 5 Hz, so we
 # initially low-pass filter the good reference channels in this dataset prior
 # to regression
 #
@@ -100,11 +101,11 @@ regress.fit(raw)
 regress.apply(raw, copy=False)
 
 # plot
-data_ds, _ = raw[picks, :stop]
+data_ds, _ = raw[picks[::5], :stop]
 data_ds = data_ds[:, ::step] * amp_scale
 
 fig, ax = plt.subplots(constrained_layout=True)
-ax.plot(time_ds, data_ds.T, **plot_kwargs)
+ax.plot(time_ds, data_ds.T - np.mean(data_ds, axis=1), **plot_kwargs)
 ax.grid(True, ls=':')
 ax.set(title='After reference regression', **set_kwargs)
 
@@ -129,6 +130,48 @@ ax.grid(True, ls=':')
 ax.set(xticks=psd_post.freqs)
 ax.set(xlim=(0, 20), title='Reference regression shielding',
        xlabel='Frequency (Hz)', ylabel='Shielding (dB)')
+
+# %%
+# Filtering nuisance signals
+# ---------------------------------
+#
+# Having regressed much of the high-amplitude, low-frequency interference, we
+# can now look to filtering the remnant nuisance signals. The motivation for
+# filtering after regression (rather than before) is to minimise any filter
+# artefacts generated when removing such high-amplitude interfece (compared
+# to the neural signals we are interested in).
+#
+# We are going to remove the 50 Hz mains signal with a notch filter,
+# followed by a bandpass filter between 1 and 48 Hz. From here it becomes clear
+# that the variance in our signal has been reduced from 100s of pT to 10s of
+# pT instead.
+
+# notch
+raw.notch_filter(np.arange(50, 251, 50))
+# bandpass
+raw.filter(1, 48, picks='meg')
+# plot
+data_ds, _ = raw[picks[::5], :stop]
+data_ds = data_ds[:, ::step] * amp_scale
+fig, ax = plt.subplots(constrained_layout=True)
+plot_kwargs = dict(lw=1, alpha=0.5)
+ax.plot(time_ds, data_ds.T - np.mean(data_ds, axis=1), **plot_kwargs)
+ax.grid(True)
+set_kwargs = dict(ylim=(-500, 500), xlim=time_ds[[0, -1]],
+                  xlabel='Time (s)', ylabel='Amplitude (pT)')
+ax.set(title='After regression and filtering', **set_kwargs)
+
+# %%
+# Generating an evoked response
+# ----------------------------
+#
+# With the data preprocessed, it is now possible to see an auditory evoked
+# response at the sensor level.
+
+events = mne.find_events(raw, min_duration=0.1)
+epochs = mne.Epochs(raw, events, tmin=-0.1, tmax=0.4, baseline=(-0.1, 0.))
+evoked = epochs.average()
+evoked.plot()
 
 # %%
 # References
