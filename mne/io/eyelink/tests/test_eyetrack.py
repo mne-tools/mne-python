e653a26a643fe79d7e82b1c46492df5b133c2d71
Scott Huberty <52462026+scott-huberty@users.noreply.github.com>
2023-01-30 18:29:22 -0500

    added to and fixed flake in mne/io/eyelink/tests

--- mne/io/eyelink/tests/test_eyetrack.py
+++ mne/io/eyelink/tests/test_eyetrack.py
@@ -1,22 +1,26 @@
 import pytest
 from mne.io import read_raw_eyelink
+from mne.io.constants import FIFF
 from mne.datasets.testing import data_path, requires_testing_data
 
+
 testing_path = data_path(download=False)
-eyelink_fname = fname = testing_path / 'eyetrack' / 'test_eyelink.asc'
+fname = testing_path / 'eyetrack' / 'test_eyelink.asc'
 
 
 @requires_testing_data
-@pytest.mark.parametrize('fname, read_eye_events, interpolate_missing, '
-                         'annotate_missing',
-                         [(eyelink_fname, False, False, False),
-                          (eyelink_fname, True, False, False)])
-def test_eyelink(fname, read_eye_events, interpolate_missing,
-                 annotate_missing):
+@pytest.mark.parametrize('fname, create_annotations, find_overlaps',
+                         [(fname, False, False),
+                          (fname, True, False),
+                          (fname, True, True)])
+def test_eyelink(fname, create_annotations, find_overlaps):
     """Test reading eyelink asc files."""
+    raw = read_raw_eyelink(fname, create_annotations, find_overlaps)
 
-    raw = read_raw_eyelink(fname, read_eye_events, interpolate_missing,
-                           annotate_missing)
+    # First, tests that shouldn't change based on function arguments
+    assert raw.info['sfreq'] == 500  # True for this file
+    assert len(raw.info['ch_names']) == 6
+    assert raw.info['chs'][0]['kind'] == FIFF.FIFFV_EYETRACK_CH
     assert 'RawEyelink' in repr(raw)
 
     # TODO: test some annotation values for accuracy.
