b3fd2a28ce31b18e13edc6a3d898716a1e17cf9c
mmagnuski <mmagnuski@swps.edu.pl>
2023-02-11 02:04:30 +0100

    ENH: add picks etc.

--- mne/utils/xarray.py
+++ mne/utils/xarray.py
@@ -3,14 +3,18 @@ import numpy as np
 from .. import Epochs, Evoked
 from ..time_frequency import AverageTFR
 
-
-def to_xarray(mne_inst):
+def to_xarray(inst, picks=None, copy=True):
     """Convert MNE object instance to xarray DataArray.
 
     Parameters
     ----------
-    mne_inst : Epochs | Evoked
+    inst : Epochs | Evoked
         The MNE object to convert.
+    picks : list of str | array-like of int | None
+        Channels to include. If None only good data channels are kept.
+        (I couldn't use @fill_doc here, so I put this temporarily here)
+    copy : bool
+        If ``True``, return a copy of the data. Defaults to ``True``.
 
     Returns
     -------
@@ -18,23 +22,28 @@ def to_xarray(mne_inst):
         The xarray object.
     """
     from xarray import DataArray
+    from .. import Epochs, Evoked
     from mne.utils import _validate_type
 
-    _validate_type(mne_inst, (Epochs, Evoked, AverageTFR))
+    _validate_type(inst, (Epochs, Evoked))
 
-    if isinstance(mne_inst, Epochs):
-        data = mne_inst.get_data()
-        dims = ('chan', 'trial', 'time')
-    elif isinstance(mne_inst, Evoked):
-        data = mne_inst.data
+    if isinstance(inst, Epochs):
+        dims = ('chan', 'epoch', 'time')
+    elif isinstance(inst, Evoked):
         dims = ('chan', 'time')
     else:
         raise ValueError('MNE instance must be Epochs or Evoked.')
 
-    coords = dict(chan=mne_inst.ch_names)
+    coords = dict(chan=inst.ch_names)
     if 'time' in dims:
-        coords['time'] = mne_inst.times
-    if 'trial' in dims:
-        coords['trial'] = np.arange(mne_inst.n_epochs)
+        coords['time'] = inst.times
+    if 'epoch' in dims:
+        coords['epoch'] = np.arange(inst.n_epochs)
 
-    return DataArray(data, dims=dims, coords=coords)
+    data = inst.get_data(picks=picks)
+    data = data.copy() if copy else data
+    xarr = DataArray(data, dims=dims, coords=coords)
+
+    # add channel types as additional dimension coordinate
+    xarr = xarr.assign_coords(ch_type=('chan', inst.get_channel_types()))
+    return xarr
