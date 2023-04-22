b1452ed5392b8ced3f5787761e764f022957d0a0
Eric Larson <larson.eric.d@gmail.com>
2023-04-22 19:04:34 -0400

    FIX: More

--- tools/github_actions_env_vars.sh
+++ tools/github_actions_env_vars.sh
@@ -4,20 +4,22 @@ set -eo pipefail -x
 # old and minimal use conda
 if [[ "$MNE_CI_KIND" == "old" ]]; then
     echo "Setting conda env vars for old"
+    echo "CONDA_ACTIVATE_ENV=true" >> $GITHUB_ENV
     echo "CONDA_DEPENDENCIES=numpy=1.20.2 scipy=1.6.3 matplotlib=3.4 pandas=1.2.4 scikit-learn=0.24.2" >> $GITHUB_ENV
     echo "MNE_IGNORE_WARNINGS_IN_TESTS=true" >> $GITHUB_ENV
 elif [[ "$MNE_CI_KIND" == "minimal" ]]; then
     echo "Setting conda env vars for minimal"
+    echo "CONDA_ACTIVATE_ENV=true" >> $GITHUB_ENV
     echo "CONDA_DEPENDENCIES=numpy scipy matplotlib" >> $GITHUB_ENV
-elif [[ "$MNE_CI_KIND" != "pip"* ]]; then
+elif [[ "$MNE_CI_KIND" != "pip"* ]]; then  # notebook, conda, mamba
     echo "Setting conda env vars for $MNE_CI_KIND"
     echo "CONDA_ENV=environment.yml" >> $GITHUB_ENV
     echo "CONDA_ACTIVATE_ENV=mne" >> $GITHUB_ENV
     echo "MNE_QT_BACKEND=PyQt5" >> $GITHUB_ENV
-elif [[ "$MNE_CI_KIND" != "notebook" ]]; then
+else  # pip-like
     echo "Setting pip env vars for $MNE_CI_KIND"
     echo "MNE_QT_BACKEND=PyQt6" >> $GITHUB_ENV
 else
-    echo "Not setting env vars for notebook"
+    echo "Setting pip env vars for $MNE_CI_KIND"
 fi
 set +x
