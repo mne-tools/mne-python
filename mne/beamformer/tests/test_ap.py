30d518f1a7cdab7be6af806dc164bac162a853d5
YuvalRealpe <yuval.realpe@gmail.com>
2023-04-27 06:56:49 +0300

    Update test_ap.py
    removed _get_data() and imported the _det_data() function from test_rap.py

--- mne/beamformer/tests/test_ap.py
+++ mne/beamformer/tests/test_ap.py
@@ -4,42 +4,17 @@
 
 import os.path as op
 
-from numpy.random import choice
-
 import mne
 from mne.beamformer import alternating_projections
-from mne.cov import regularize
 from mne.datasets import testing
-from mne.beamformer.tests.test_rap_music import simu_data, _check_dipoles
+from mne.beamformer.tests.test_rap_music import (simu_data, _check_dipoles,
+                                                 _get_data)
 
 data_path = testing.data_path(download=False)
 fname_ave = op.join(data_path, 'MEG', 'sample', 'sample_audvis-ave.fif')
 fname_cov = op.join(data_path, 'MEG', 'sample', 'sample_audvis-cov.fif')
 fname_fwd = op.join(data_path, 'MEG', 'sample',
                     'sample_audvis_trunc-meg-eeg-oct-4-fwd.fif')
-
-
-def _get_data(ch_decim=1):
-    """Read in data used in tests."""
-    # Read evoked
-    condition = choice(['Left Auditory',
-                        'Right Auditory',
-                        'Left visual',
-                        'Right visual'])
-    evoked = mne.read_evokeds(fname_ave, condition=condition,
-                              baseline=(None, 0))
-    evoked.crop(tmin=0.05, tmax=0.15)
-    picks = mne.pick_types(evoked.info, meg=True, eeg=False)
-    picks = picks[::ch_decim]
-    evoked.pick_channels([evoked.ch_names[pick] for pick in picks])
-    evoked.info.normalize_proj()
-
-    # Read noise_cov
-    noise_cov = mne.read_cov(fname_cov)
-    noise_cov['projs'] = []
-    noise_cov = regularize(noise_cov, evoked.info, rank='full', proj=False)
-
-    return evoked, noise_cov
 
 
 @testing.requires_testing_data
