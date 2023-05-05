947f21f213adaa9c741ed7f358add215a4eb67fe
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2023-05-05 15:54:32 +0200

    Add test for TRAP-MUSIC.

--- mne/beamformer/tests/test_rap_music.py
+++ mne/beamformer/tests/test_rap_music.py
@@ -212,3 +212,15 @@ def test_rap_music_picks():
     noise_cov = mne.read_cov(fname_cov)
     dipoles = rap_music(evoked, forward, noise_cov, n_dipoles=2)
     assert len(dipoles) == 2
+
+
+@testing.requires_testing_data
+def test_rap_music_trap():
+    """Test TRAP-MUSIC."""
+    evoked = mne.read_evokeds(fname_ave, condition="Right Auditory", baseline=(None, 0))
+    evoked.crop(tmin=0.05, tmax=0.15)  # select N100
+    evoked.pick_types(meg=True, eeg=False)
+    forward = mne.read_forward_solution(fname_fwd)
+    noise_cov = mne.read_cov(fname_cov)
+    dipoles = rap_music(evoked, forward, noise_cov, n_dipoles=2, use_trap=True)
+    assert len(dipoles) == 2
