1328f9621f94752a8493b46a33395a43921a10af
Eric Larson <larson.eric.d@gmail.com>
2023-05-12 12:48:54 -0400

    FIX: Remove cruft

--- mne/proj.py
+++ mne/proj.py
@@ -483,7 +483,6 @@ def sensitivity_map(
     n_locations = n_dipoles // 3
     del n_dipoles
     sensitivity_map = np.empty(n_locations)
-    assert n_locations == len(fwd["src"][0]["vertno"]) + len(fwd["src"][1]["vertno"])
 
     for k in range(n_locations):
         gg = gain[:, 3 * k : 3 * (k + 1)]
