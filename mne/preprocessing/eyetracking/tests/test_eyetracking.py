1f0e3dc9b8b8a4a9a3e3126fdd9edcad9f221509
Scott Huberty <scott.huberty@mail.mcgill.ca>
2023-06-26 11:14:34 -0400

    TESTS: remove mark xfail and add test to catch error - added a test to catch the code block that should raise an error when a blink annotaiton doesnt have ch_name info

--- mne/preprocessing/eyetracking/tests/test_eyetracking.py
+++ mne/preprocessing/eyetracking/tests/test_eyetracking.py
@@ -15,19 +15,29 @@ fname = data_path(download=False) / "eyetrack" / "test_eyelink.asc"
 
 @requires_testing_data
 @requires_pandas
-@pytest.mark.xfail(reason="This will fail until PR 11746 is merged")
 @pytest.mark.parametrize(
-    "buffer, match",
-    [(0.025, "BAD_blink"), ((0.025, 0.025), ["BAD_blink", "blink_manual"])],
+    "buffer, match, cause_error",
+    [
+        (0.025, "BAD_blink", False),
+        ((0.025, 0.025), ["BAD_blink", "blink_manual"], False),
+        (0.025, "BAD_blink", True),
+    ],
 )
-def test_interpolate_blinks(buffer, match):
+def test_interpolate_blinks(buffer, match, cause_error):
     """Test interpolating pupil data during blinks."""
     raw = read_raw_eyelink(
         fname, preload=True, create_annotations=["blinks"], find_overlaps=True
     )
     first_blink_start = raw.annotations[0]["onset"]
     first_blink_end = raw.annotations[0]["onset"] + raw.annotations[0]["duration"]
-    interpolate_blinks(raw, buffer=buffer, match=match)
+    if cause_error:
+        # Make an annotation without ch_names info
+        raw.annotations.append(onset=1, duration=1, description="BAD_blink")
+        with pytest.raises(ValueError):
+            interpolate_blinks(raw, buffer=buffer, match=match)
+        return
+    else:
+        interpolate_blinks(raw, buffer=buffer, match=match)
 
     # Now get the data and check that the blinks are interpolated
     data, times = raw.get_data(return_times=True)
