BUG: Fix bug with SNR calculation

mne/chpi.py
@@ -661,13 +661,15 @@ def _setup_hpi_amplitude_fitting(
     inv_model_reord = _reorder_inv_model(inv_model, len(hpi_freqs))
     proj, proj_op, meg_picks = _setup_ext_proj(info, ext_order)
     # include mag and grad picks separately, for SNR computations
-    mag_picks = _picks_to_idx(info, "mag", allow_empty=True)
-    grad_picks = _picks_to_idx(info, "grad", allow_empty=True)
+    mag_subpicks = _picks_to_idx(info, "mag", allow_empty=True)
+    mag_subpicks = np.searchsorted(meg_picks, mag_subpicks)
+    grad_subpicks = _picks_to_idx(info, "grad", allow_empty=True)
+    grad_subpicks = np.searchsorted(meg_picks, grad_subpicks)
     # Set up magnetic dipole fits
     hpi = dict(
         meg_picks=meg_picks,
-        mag_picks=mag_picks,
-        grad_picks=grad_picks,
+        mag_subpicks=mag_subpicks,
+        grad_subpicks=grad_subpicks,
         hpi_pick=hpi_pick,
         model=model,
         inv_model=inv_model,
@@ -779,8 +781,8 @@ def _fit_chpi_amplitudes(raw, time_sl, hpi, snr=False):
             len(hpi["freqs"]),
             hpi["model"],
             hpi["inv_model"],
-            hpi["mag_picks"],
-            hpi["grad_picks"],
+            hpi["mag_subpicks"],
+            hpi["grad_subpicks"],
         )
     return _fast_fit(
         this_data,