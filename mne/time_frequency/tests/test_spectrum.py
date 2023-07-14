145b6bb23ea8045b71f2ac9ebb35dba84dd3940b
Alex Rockhill <aprockhill@mailbox.org>
2023-07-14 15:34:30 -0700

    style

--- mne/time_frequency/tests/test_spectrum.py
+++ mne/time_frequency/tests/test_spectrum.py
@@ -419,19 +419,23 @@ def test_plot_spectrumaray(raw, epochs):
     """Test SpectrumArray and EpochsSpectrumArray plotting."""
     spects = list()
     spect_orig = raw.compute_psd()
-    spects.append(SpectrumArray(
-        data=spect_orig.get_data(),
-        freqs=spect_orig.freqs,
-        info=spect_orig.info,
-    ))
+    spects.append(
+        SpectrumArray(
+            data=spect_orig.get_data(),
+            freqs=spect_orig.freqs,
+            info=spect_orig.info,
+        )
+    )
     spect_orig = epochs.compute_psd()
-    spects.append(EpochsSpectrumArray(
-        data=spect_orig.get_data(),
-        freqs=spect_orig.freqs,
-        info=spect_orig.info,
-        events=epochs.events,
-        event_id=epochs.event_id,
-    ))
+    spects.append(
+        EpochsSpectrumArray(
+            data=spect_orig.get_data(),
+            freqs=spect_orig.freqs,
+            info=spect_orig.info,
+            events=epochs.events,
+            event_id=epochs.event_id,
+        )
+    )
     for spect in spects:
         spect.plot(average=True, amplitude=True, spatial_colors=True)
         spect.plot(average=False, amplitude=False, spatial_colors=False)
