61d65d3d54692ccf2e3065b99c5d67e638ba4014
Eric Larson <larson.eric.d@gmail.com>
2023-07-20 14:26:28 -0400

    FIX: Shell [skip azp] [skip cirrus] [skip circle]

--- tools/github_actions_env_vars.sh
+++ tools/github_actions_env_vars.sh
@@ -4,13 +4,11 @@ set -eo pipefail -x
 # old and minimal use conda
 if [[ "$MNE_CI_KIND" == "old" ]]; then
     echo "Setting conda env vars for old"
-    echo "CONDA_ACTIVATE_ENV=true" >> $GITHUB_ENV
     echo "CONDA_DEPENDENCIES=numpy=1.20.2 scipy=1.6.3 matplotlib=3.4 pandas=1.2.4 scikit-learn=0.24.2" >> $GITHUB_ENV
     echo "MNE_IGNORE_WARNINGS_IN_TESTS=true" >> $GITHUB_ENV
     echo "MNE_SKIP_NETWORK_TESTS=1" >> $GITHUB_ENV
 elif [[ "$MNE_CI_KIND" == "minimal" ]]; then
     echo "Setting conda env vars for minimal"
-    echo "CONDA_ACTIVATE_ENV=true" >> $GITHUB_ENV
     echo "CONDA_DEPENDENCIES=numpy scipy matplotlib" >> $GITHUB_ENV
 elif [[ "$MNE_CI_KIND" == "notebook" ]]; then
     echo "CONDA_ENV=environment.yml" >> $GITHUB_ENV
@@ -20,7 +18,6 @@ elif [[ "$MNE_CI_KIND" == "notebook" ]]; then
 elif [[ "$MNE_CI_KIND" != "pip"* ]]; then  # conda, mamba (use warning level for completeness)
     echo "Setting conda env vars for $MNE_CI_KIND"
     echo "CONDA_ENV=environment.yml" >> $GITHUB_ENV
-    echo "CONDA_ACTIVATE_ENV=mne" >> $GITHUB_ENV
     echo "MNE_QT_BACKEND=PyQt5" >> $GITHUB_ENV
     echo "MNE_LOGGING_LEVEL=warning" >> $GITHUB_ENV
 else  # pip-like
