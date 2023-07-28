bd388b019ae7ceebbfef8c639e0ec4199c19a92f
Daniel McCloy <dan@mccloy.info>
2023-07-28 16:45:35 -0500

    lazy-load mne/utils

--- mne/channels/montage.py
+++ mne/channels/montage.py
@@ -60,8 +60,8 @@ from ..utils import (
     _check_fname,
     _on_missing,
     fill_doc,
-    _docdict,
 )
+from ..utils.docs import docdict
 
 from ._dig_montage_utils import _read_dig_montage_egi
 from ._dig_montage_utils import _parse_brainvision_dig_montage
@@ -1275,7 +1275,7 @@ def _set_montage(info, montage, match_case=True, match_alias=False, on_missing="
             f"Not setting position{_pl(extra)} of {len(extra)} {types} "
             f"channel{_pl(extra)} found in montage:\n{names}\n"
             "Consider setting the channel types to be of "
-            f'{_docdict["montage_types"]} '
+            f'{docdict["montage_types"]} '
             "using inst.set_channel_types before calling inst.set_montage, "
             "or omit these channels when creating your montage."
         )
