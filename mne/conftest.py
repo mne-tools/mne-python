412f7cd6ef8556c69ff838648aa909c0b9a5a554
Eric Larson <larson.eric.d@gmail.com>
2023-07-31 13:04:35 -0400

    MAINT: Use pytest-qt

--- mne/conftest.py
+++ mne/conftest.py
@@ -209,6 +209,7 @@ def verbose_debug():
 def qt_config():
     """Configure the Qt backend for viz tests."""
     os.environ["_MNE_BROWSER_NO_BLOCK"] = "true"
+    os.environ["_MNE_BROWSER_BACK"] = "true"
 
 
 @pytest.fixture(scope="session")
