0982579242e1f57a7ea86dc3d05f465433225cf1
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-08-03 10:33:55 +0200

    trigger cis

--- tutorials/preprocessing/40_artifact_correction_ica.py
+++ tutorials/preprocessing/40_artifact_correction_ica.py
@@ -6,8 +6,8 @@ Repairing artifacts with ICA
 ============================
 
 This tutorial covers the basics of independent components analysis (ICA) and shows how
-ICA can be used for artifact repair; an extended example illustrates
-repair of ocular and heartbeat artifacts. For conceptual background on ICA, see
+ICA can be used for artifact repair; an extended example illustrates repair of ocular
+and heartbeat artifacts. For conceptual background on ICA, see
 :ref:`this scikit-learn tutorial
 <sphx_glr_auto_examples_decomposition_plot_ica_blind_source_separation.py>`.
 
