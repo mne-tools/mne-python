8175b3d3ee36b5098404b3f6a0324f5cb248dbbf
Dmitrii Altukhov <dm.altukhov@ya.ru>
2023-08-15 11:55:13 +0200

    clarify what we test via var names
    [skip ci]

--- mne/tests/test_epochs.py
+++ mne/tests/test_epochs.py
@@ -1561,25 +1561,26 @@ def test_split_saving_and_loading_back(tmp_path, epochs_to_split, preload):
 
 
 @pytest.mark.parametrize(
-    "split_naming, split_fname, split_fname_fn",
+    "split_naming, dst_fname, split_fname_fn",
     [
         ("neuromag", "test_epo.fif", lambda n: f"test_epo-{n}.fif"),
         ("bids", "test_epo.fif", lambda n: f"test_split-{n:02d}_epo.fif"),
     ],
 )
 def test_split_naming(
-    tmp_path, epochs_to_split, split_naming, split_fname, split_fname_fn
+    tmp_path, epochs_to_split, split_naming, dst_fname, split_fname_fn
 ):
     """Test naming of the split files."""
     epochs, split_size, n_files = epochs_to_split
-    split_fpath = tmp_path / split_fname
+    dst_fpath = tmp_path / dst_fname
+    split_0_fpath = dst_fpath
     save_kwargs = {"split_size": split_size, "split_naming": split_naming}
     # we don't test for reserved files as it's not implemented here
 
-    epochs.save(split_fpath, verbose=True, **save_kwargs)
+    epochs.save(dst_fpath, verbose=True, **save_kwargs)
 
     # check that the filenames match the intended pattern
-    assert split_fpath.is_file()
+    assert split_0_fpath.is_file()
     assert (tmp_path / split_fname_fn(n_files - 1)).is_file()
     assert not (tmp_path / split_fname_fn(n_files)).is_file()
 
