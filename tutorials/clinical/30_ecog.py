47d1d8c2adf94dc27b0c1f901fad251cd1cd6659
Eric Larson <larson.eric.d@gmail.com>
2023-08-16 12:05:38 -0400

    FIX: Links [skip azp] [skip actions]

--- tutorials/clinical/30_ecog.py
+++ tutorials/clinical/30_ecog.py
@@ -20,7 +20,7 @@ For a complementary example that involves sEEG data, channel locations in MNI
 space, or projection into a volume, see :ref:`tut-working-with-seeg`.
 
 Please note that this tutorial requires 3D plotting dependencies (see
-:ref:`manual-install`) as well as ``mne_bids`` which can be installed using
+:ref:`manual-install`) as well as ``mne-bids`` which can be installed using
 ``pip``.
 """
 # Authors: Eric Larson <larson.eric.d@gmail.com>
