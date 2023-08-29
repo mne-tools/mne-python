74d04541b492909667389e53b27ad363d53b2d27
Scott Huberty <scott.huberty@mail.mcgill.ca>
2023-08-29 09:35:11 -0400

    FIX: change  all RawEyelink helper methods to functions
    - Moved all the methods to the _utils folder
    - some refactoring should be done to make the code more idiomatic now that these are functions that operate on raw_extras

--- mne/io/eyelink/tests/test_eyelink.py
+++ mne/io/eyelink/tests/test_eyelink.py
@@ -8,7 +8,7 @@ from mne.datasets.testing import data_path, requires_testing_data
 from mne.io import read_raw_eyelink
 from mne.io.tests.test_raw import _test_raw_reader
 from mne._fiff.constants import FIFF
-from mne.io.eyelink.eyelink import _adjust_times, _find_overlaps
+from mne.io.eyelink._utils import _adjust_times, _find_overlaps
 from mne._fiff.pick import _DATA_CH_TYPES_SPLIT
 
 pd = pytest.importorskip("pandas")
