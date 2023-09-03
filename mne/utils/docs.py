f44636f00666b8eb869417960926d01690ff4f42
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-09-03 16:15:18 +0200

    Add x-ref to Projection object and to scipy, improve formatting with double backticks (#11928)
    Co-authored-by: Daniel McCloy <dan@mccloy.info>

--- mne/utils/docs.py
+++ mne/utils/docs.py
@@ -1984,7 +1984,7 @@ docdict[
 ] = """
 iir_params : dict | None
     Dictionary of parameters to use for IIR filtering.
-    If iir_params is None and method="iir", 4th order Butterworth will be used.
+    If ``iir_params=None`` and ``method="iir"``, 4th order Butterworth will be used.
     For more information, see :func:`mne.filter.construct_iir_filter`.
 """
 
@@ -2104,7 +2104,7 @@ docdict[
     "interp"
 ] = """
 interp : str
-    Either 'hann', 'cos2' (default), 'linear', or 'zero', the type of
+    Either ``'hann'``, ``'cos2'`` (default), ``'linear'``, or ``'zero'``, the type of
     forward-solution interpolation to use between forward solutions
     at different head positions.
 """
@@ -2114,8 +2114,8 @@ docdict[
 ] = """
 interpolation : str | None
     Interpolation method (:class:`scipy.interpolate.interp1d` parameter).
-    Must be one of 'linear', 'nearest', 'zero', 'slinear', 'quadratic',
-    or 'cubic'.
+    Must be one of ``'linear'``, ``'nearest'``, ``'zero'``, ``'slinear'``,
+    ``'quadratic'`` or ``'cubic'``.
 """
 
 docdict[
@@ -2512,8 +2512,8 @@ docdict[
     "method_fir"
 ] = """
 method : str
-    'fir' will use overlap-add FIR filtering, 'iir' will use IIR
-    forward-backward filtering (via filtfilt).
+    ``'fir'`` will use overlap-add FIR filtering, ``'iir'`` will use IIR
+    forward-backward filtering (via :func:`~scipy.signal.filtfilt`).
 """
 
 docdict[
@@ -2659,7 +2659,7 @@ docdict[
     "n_jobs_cuda"
 ] = """
 n_jobs : int | str
-    Number of jobs to run in parallel. Can be 'cuda' if ``cupy``
+    Number of jobs to run in parallel. Can be ``'cuda'`` if ``cupy``
     is installed properly.
 """
 
@@ -2667,8 +2667,8 @@ docdict[
     "n_jobs_fir"
 ] = """
 n_jobs : int | str
-    Number of jobs to run in parallel. Can be 'cuda' if ``cupy``
-    is installed properly and method='fir'.
+    Number of jobs to run in parallel. Can be ``'cuda'`` if ``cupy``
+    is installed properly and ``method='fir'``.
 """
 
 docdict[
@@ -2841,7 +2841,7 @@ docdict[
 ] = """
 npad : int | str
     Amount to pad the start and end of the data.
-    Can also be "auto" to use a padding that will result in
+    Can also be ``"auto"`` to use a padding that will result in
     a power-of-two size (can be much faster).
 """
 
@@ -3116,7 +3116,7 @@ docdict[
 pca_vars : array, shape (n_comp,) | list of array
     The explained variances of the first n_comp SVD components across the
     PSFs/CTFs for the specified vertices. Arrays for multiple labels are
-    returned as list. Only returned if mode='svd' and return_pca_vars=True.
+    returned as list. Only returned if ``mode='svd'`` and ``return_pca_vars=True``.
 """
 
 docdict[
@@ -3141,9 +3141,10 @@ phase : str
     suppression.
     When ``method='iir'``, ``phase='zero'`` (default) or
     ``phase='zero-double'`` constructs and applies IIR filter twice, once
-    forward, and once backward (making it non-causal) using filtfilt.
+    forward, and once backward (making it non-causal) using
+    :func:`~scipy.signal.filtfilt`.
     If ``phase='forward'``, it constructs and applies forward IIR filter using
-    lfilter.
+    :func:`~scipy.signal.lfilter`.
 
     .. versionadded:: 0.13
 """
