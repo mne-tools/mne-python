cfe27466cf2a4332dde3c4653073bb0f2b527d57
Eric Larson <larson.eric.d@gmail.com>
2023-09-11 11:59:29 -0400

    FIX: Fix check

--- mne/viz/backends/_pyvista.py
+++ mne/viz/backends/_pyvista.py
@@ -15,7 +15,6 @@ from contextlib import contextmanager
 from inspect import signature
 import platform
 import re
-import sys
 import warnings
 
 import numpy as np
@@ -226,7 +225,7 @@ class _PyVistaRenderer(_AbstractRenderer):
         _require_version("pyvista", "use 3D rendering", "0.32")
         multi_samples = _get_3d_option("multi_samples")
         # multi_samples > 1 is broken on macOS + Intel Iris + volume rendering
-        if sys.platform == "darwin":
+        if platform.system() == "macOS":
             multi_samples = 1
         figure = PyVistaFigure()
         figure._init(
@@ -1385,7 +1384,7 @@ def _is_mesa(plotter):
     # MESA (could use GPUInfo / _get_gpu_info here, but it takes
     # > 700 ms to make a new window + report capabilities!)
     # CircleCI's is: "Mesa 20.0.8 via llvmpipe (LLVM 10.0.0, 256 bits)"
-    if platform.system() != "Linux":
+    if platform.system() == "macOS":  # segfaults on macOS sometimes
         return False
     gpu_info_full = plotter.ren_win.ReportCapabilities()
     gpu_info = re.findall(
