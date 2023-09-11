c9d6275b056e75c94a8d79e49ac03454e7e9a971
Daniel McCloy <dan@mccloy.info>
2023-09-11 09:15:56 -0500

    Update mne/time_frequency/spectrum.py
    Co-authored-by: Eric Larson <larson.eric.d@gmail.com>

--- mne/time_frequency/spectrum.py
+++ mne/time_frequency/spectrum.py
@@ -324,7 +324,8 @@ class BaseSpectrum(ContainsMixin, UpdateChannelsMixin):
                 f"Complex output support in {type(self).__name__} objects is "
                 "deprecated and will be removed in version 1.7. If you need complex "
                 f"output please use mne.time_frequency.{psd_funcs[method].__name__}() "
-                "instead."
+                "instead.",
+                FutureWarning,
             )
         # triage method and kwargs. partial() doesn't check validity of kwargs,
         # so we do it manually to save compute time if any are invalid.
