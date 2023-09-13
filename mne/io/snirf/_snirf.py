e7d8c2c7b956cc2ae724734c2def60427955c36b
Daniel McCloy <dan@mccloy.info>
2023-09-13 10:16:47 -0500

    better var names, different jitter computation

--- mne/io/snirf/_snirf.py
+++ mne/io/snirf/_snirf.py
@@ -529,7 +529,7 @@ def _extract_sampling_rate(dat):
     # This is a workaround to provide support for Artinis data.
     # It allows for a 1% variation in the sampling times relative
     # to the average sampling rate of the file.
-    MAXIMUM_ALLOWED_SAMPLING_JITTER_PERCENTAGE = 1
+    MAXIMUM_ALLOWED_SAMPLING_JITTER_PERCENTAGE = 1.0
 
     time_data = np.array(dat.get("nirs/data1/time"))
     sampling_rate = 0
@@ -538,26 +538,30 @@ def _extract_sampling_rate(dat):
         sampling_rate = 1.0 / (time_data[1] - time_data[0])
     else:
         # specified as time points
-        fs_diff = np.around(np.diff(time_data), decimals=4)
-
-        if len(np.unique(fs_diff)) == 1:
+        periods = np.diff(time_data)
+        uniq_periods = np.unique(periods.round(decimals=4))
+        if uniq_periods.size == 1:
             # Uniformly sampled data
-            sampling_rate = 1.0 / np.unique(fs_diff).item()
+            sampling_rate = 1.0 / uniq_periods.item()
         else:
-            mean_sample_difference = np.mean(np.diff(time_data))
-            max_sample_jitter = np.max(np.diff(np.diff(time_data)))
-            sample_jitter_percentage = 100 * (
-                max_sample_jitter / mean_sample_difference
-            )
             # Hopefully uniformly sampled data with some precision issues.
             # This is a workaround to provide support for Artinis data.
-            sampling_rate = 1.0 / mean_sample_difference
-            if sample_jitter_percentage > MAXIMUM_ALLOWED_SAMPLING_JITTER_PERCENTAGE:
+            mean_period = np.mean(periods)
+            sampling_rate = 1.0 / mean_period
+            ideal_times = np.linspace(time_data[0], time_data[-1], time_data.size)
+            max_jitter = np.max(np.abs(time_data - ideal_times))
+            percent_jitter = 100.0 * max_jitter / mean_period
+            msg = (
+                f"Found jitter of {percent_jitter:3f}% in sample times. Sampling "
+                f"rate has been set to {sampling_rate:1f}."
+            )
+            if percent_jitter > MAXIMUM_ALLOWED_SAMPLING_JITTER_PERCENTAGE:
                 warn(
-                    "MNE does not currently support reading "
-                    "SNIRF files with non-uniform sampled data."
+                    f"{msg} Note that MNE-Python does not currently support SNIRF "
+                    "files with non-uniformly-sampled data."
                 )
-
+            else:
+                logger.info(msg)
     time_unit = _get_metadata_str(dat, "TimeUnit")
     time_unit_scaling = _get_timeunit_scaling(time_unit)
     sampling_rate *= time_unit_scaling
