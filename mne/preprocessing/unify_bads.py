a85093cdd523726d62629724344fb941c3e03908
Ana Radanovic <radanovica@protonmail.com>
2023-09-25 14:44:04 -0400

    adding unify channels to preproc

---
+++ mne/preprocessing/unify_bads.py
@@ -0,0 +1,32 @@
+
+#%%
+def unifying_bads(
+    list_instances,
+    
+):
+    
+    common_bad_channels = []
+    #first check that each object is mne object
+    
+    #then interate through the objects to get ch names as set
+    ch_set_1 = list_instances[0].info['bads']
+    common_bad_channels.extend(ch_set_1)
+    
+    for inst in list_instances[1:]:
+        ch_set_2 = set(inst.info['bads'])
+        set_of_bads = set(common_bad_channels)
+        new_bads = ch_set_2.difference(set_of_bads)
+        
+        if len(new_bads) >1 :
+            common_bad_channels.extend(list(new_bads))
+    
+    new_instances = []
+    
+    for inst in list_instances:
+        inst.info["bads"] = common_bad_channels
+        new_instances.append(inst)
+
+
+    return new_instances
+
+
