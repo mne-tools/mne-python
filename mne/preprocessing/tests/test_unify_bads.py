7c28118dbf77ccc1f13e4443ad13aa35a40cb70b
nordme <nordme@uw.edu>
2023-09-26 12:57:20 -0700

    make functions work

--- mne/preprocessing/tests/test_unify_bads.py
+++ mne/preprocessing/tests/test_unify_bads.py
@@ -1,49 +1,45 @@
-from pathlib import Path
-
 import numpy as np
 import pytest
 
-import mne
-from mne.preprocessing import unify_bad_channels
-from ..io import BaseRaw
-from ..epochs import Epochs
-from ..evoked import Evoked
-from ..time_frequency.spectrum import BaseSpectrum
+from mne.preprocessing.unify_bads import unify_bad_channels
+from mne.time_frequency.tests.test_spectrum import _get_inst()
 
-
-raw_fname = (
-    Path(__file__).parent.parent.parent / "io" / "tests" / "data" / "test_raw.fif"
-)
-@pytest.mark.parametrize('instance', ('raw', 'epochs', 'evoked', 'spectrum'))
-def test_instance_support(instance):
+@pytest.mark.parametrize('instance', ('raw', 'epochs', 'evoked', 'raw_spectrum', 'epochs_spectrum'))
+def test_instance_support(instance, request, evoked):
+    '''Tests support of different classes.'''
     # test unify_bads function on instance (single input, no bads scenario)
-    unify_bad_channels(instance)
+    inst = _get_inst(instance, request, evoked)
+    inst_out = unify_bad_channels([inst])
+    assert inst_out == [inst]
 
 
-def test_bads_order(raw):
+@pytest.mark.parametrize('instance', ([], ['bad_instance'], 'mixed'))
+def test_error_raising(instance, request, evoked):
+    '''Tests input checking.'''
+    if instance == 'mixed':
+        instance = [_get_inst('raw', request, evoked),
+                    _get_inst('epochs', request, evoked)]
+    with pytest.raises(TypeError):
+        unify_bad_channels(instance)
 
-def test_unify_bads(raw, epochs):
-    ## test error raising
-    # err 1: no instance passed to function
-    with pytest.raises(UserWarning): # FIX RAISE TYPE
-        unify_bad_channels([])
-    # err 2: bad instance passed to function
-    bad_inst = 'bad_instance'
-    with pytest.raises(UserWarning):  # FIX RAISE TYPE
-        unify_bad_channels(bad_inst)
-    # err 3: mixed instance types passed to function
-    with pytest.raises(ValueError):
-        unify_bad_channels([raw, epochs])
+
+def test_bads_compilation(raw):
+    '''Tests that bads are compiled properly in two cases: a) single instance
+    passed to function with an existing bad, and b) multiple instances passed
+    to function with varying compilation scenarios including empty bads,
+    unique bads, partially duplicated bads listed out-of-order, and
+    nonsense channel names.'''
     ## check unification scenarios
-    # scnario 1: single instance with actual bads (already tested no bads)
-    raw.info['bads'] += raw.info['ch_names'][0]
-    s_unified = unify_bad_channels(raw)
+    chns = raw.info['ch_names'][0:3]
+    # scnario 1: single instance passed with actual bads (already tested no bads)
+    assert raw.info['bads'] == []
+    raw.info['bads'] += [chns[0]]
+    s_unified = unify_bad_channels([raw])
     assert len(s_unified) == 1
-    assert s_unified[0].info['bads'] == raw.info['ch_names'][0]
-    # scenario 2: multiple instances
+    assert s_unified[0].info['bads'] == [chns[0]], (s_unified[0].info['bads'], chns[0])
+    # scenario 2: multiple instances passed, bads types as follows:
     # a) empty bads list, b) unique bads, c) overlapping out-of-order bads,
     # d) channel name not included in raw channels
-    chns = raw.info['ch_names'][0:3]
     raws = [raw, raw.copy(), raw.copy(), raw.copy(), raw.copy()]
     assert raws[0].info['bads'] == [chns[0]]
     raws[1].info['bads'] = []
@@ -54,5 +50,6 @@ def test_unify_bads(raw, epochs):
     m_unified = unify_bad_channels(raws)
     assert len(m_unified) == len(raws)
     # check results
+    correct_bads = [chns[0], chns[2], chns[1], 'nonsense_ch_name']
     for i in np.arange(len(m_unified)):
-        assert m_unified[i].info['bads'] ==
+        assert m_unified[i].info['bads'] == correct_bads
