ccba9359494d762f96445ff977d7a1fc2880c61e
Ana Radanovic <79697247+anaradanovic@users.noreply.github.com>
2023-09-27 15:36:00 -0400

    Update mne/preprocessing/bads.py
    Co-authored-by: Daniel McCloy <dan@mccloy.info>

--- mne/preprocessing/bads.py
+++ mne/preprocessing/bads.py
@@ -67,7 +67,7 @@ def unify_bad_channels(insts):
     insts : list
         List of instances (:class:`~mne.io.Raw`, :class:`~mne.Epochs`,
         :class:`~mne.Evoked`, :class:`~mne.time_frequency.Spectrum`,
-        :class:`~mne.time_frequency.EpochSpectrum`) to unify bad channels.
+        :class:`~mne.time_frequency.EpochSpectrum`) across which to unify bad channels.
 
     Returns
     -------
