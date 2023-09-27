9b745cb6ece743d857fc5692f6223a645b23dd39
nordme <nordme@uw.edu>
2023-09-27 09:54:21 -0700

    style fix

--- mne/preprocessing/tests/test_unify_bads.py
+++ mne/preprocessing/tests/test_unify_bads.py
@@ -16,16 +16,18 @@ def test_instance_support(instance, request, evoked):
     assert inst_out == [inst]
 
 
-@pytest.mark.parametrize("instance", ([], ["bad_instance"], "mixed"))
-def test_error_raising(instance, request, evoked):
+def test_error_raising(raw, epochs):
     """Tests input checking."""
-    if instance == "mixed":
-        instance = [
-            _get_inst("raw", request, evoked),
-            _get_inst("epochs", request, evoked),
-        ]
-    with pytest.raises(TypeError):
-        unify_bad_channels(instance)
+    with pytest.raises(IndexError, match=r"empty list"):
+        unify_bad_channels([])
+    with pytest.raises(TypeError, match=r"must be an instance of"):
+        unify_bad_channels(["bad_instance"])
+    with pytest.raises(ValueError, match=r"same type"):
+        unify_bad_channels([raw, epochs])
+    with pytest.raises(AssertionError):
+        raw_alt = raw.copy()
+        raw_alt.info["ch_names"] = raw.info["ch_names"][0]
+        unify_bad_channels([raw, raw_alt])
 
 
 def test_bads_compilation(raw):
@@ -33,30 +35,23 @@ def test_bads_compilation(raw):
 
     Tests two cases: a) single instance passed to function with an existing
     bad, and b) multiple instances passed to function with varying compilation
-    scenarios including empty bads, unique bads, partially duplicated bads
-    listed out-of-order, and nonsense channel names.
+    scenarios including empty bads, unique bads, and partially duplicated bads
+    listed out-of-order.
     """
-    ## check unification scenarios
-    chns = raw.info["ch_names"][0:3]
-    # scenario 1: single instance passed with actual bads (already tested no bads)
     assert raw.info["bads"] == []
-    raw.info["bads"] += [chns[0]]
-    s_unified = unify_bad_channels([raw])
-    assert len(s_unified) == 1
-    assert s_unified[0].info["bads"] == [chns[0]], (s_unified[0].info["bads"], chns[0])
-    # scenario 2: multiple instances passed, bads types as follows:
-    # a) empty bads list, b) unique bads, c) overlapping out-of-order bads,
-    # d) channel name not included in raw channels
-    raws = [raw, raw.copy(), raw.copy(), raw.copy(), raw.copy()]
-    assert raws[0].info["bads"] == [chns[0]]
-    raws[1].info["bads"] = []
-    raws[2].info["bads"] = [chns[2]]
-    raws[3].info["bads"] = [chns[2], chns[1]]
-    raws[4].info["bads"] = ["nonsense_ch_name"]
-    # use unify_bads function
-    m_unified = unify_bad_channels(raws)
-    assert len(m_unified) == len(raws)
-    # check results
-    correct_bads = [chns[0], chns[2], chns[1], "nonsense_ch_name"]
-    for i in np.arange(len(m_unified)):
-        assert m_unified[i].info["bads"] == correct_bads
+    chns = raw.info["ch_names"][0:3]
+    no_bad = raw.copy()
+    one_bad = raw.copy()
+    one_bad.info["bads"] = chns[1]
+    three_bad = raw.copy()
+    three_bad.info["bads"] = chns
+    # scenario 1: single instance passed with actual bads
+    s_out = unify_bad_channels([one_bad])
+    assert len(s_out) == 1
+    assert s_out[0].info["bads"] == [chns[1]], (s_out[0].info["bads"], chns[1])
+    # scenario 2: multiple instances passed
+    m_out = unify_bad_channels([one_bad, no_bad, three_bad])
+    assert len(m_out) == 3
+    correct_bads = [chns[1], chns[0], chns[2]]
+    for i in np.arange(len(m_out)):
+        assert m_out[i].info["bads"] == correct_bads
