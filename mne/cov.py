2d01909157e5fca7a7784769abf101914f17ce86
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-09-27 16:16:50 +0200

    add r to docstring

--- mne/cov.py
+++ mne/cov.py
@@ -883,7 +883,7 @@ def compute_covariance(
     rank=None,
     verbose=None,
 ):
-    """Estimate noise covariance matrix from epochs.
+    r"""Estimate noise covariance matrix from epochs.
 
     The noise covariance is typically estimated on pre-stimulus periods
     when the stimulus onset is defined from events.
