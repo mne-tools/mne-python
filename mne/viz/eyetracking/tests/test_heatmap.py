20e3056fd33c4a523d0def9b0a3641d4a51a1a2c
Scott Huberty <scott.huberty@mail.mcgill.ca>
2023-09-29 13:31:29 -0400

    TST: add a test

--- mne/viz/eyetracking/tests/test_heatmap.py
+++ mne/viz/eyetracking/tests/test_heatmap.py
@@ -1,27 +1,38 @@
-import matplotlib.pyplot as plt
 import pytest
 
-from mne import make_fixed_length_epochs
-from mne.datasets.testing import data_path, requires_testing_data
-from mne.io import read_raw_eyelink
-from mne.preprocessing.eyetracking import interpolate_blinks
-from mne.viz.eyetracking import plot_gaze
+import matplotlib.pyplot as plt
+import numpy as np
 
-fname = data_path(download=False) / "eyetrack" / "test_eyelink.asc"
-pd = pytest.importorskip("pandas")
+import mne
 
 
-@requires_testing_data
 @pytest.mark.parametrize(
-    "fname, axes",
-    [(fname, None), (fname, True)],
+    "axes",
+    [(None), (True)],
 )
-def test_plot_heatmap(fname, axes):
+def test_plot_heatmap(axes):
     """Test plot_gaze."""
-    raw = read_raw_eyelink(fname, find_overlaps=True)
-    interpolate_blinks(raw, interpolate_gaze=True, buffer=(0.05, 0.2))
-    epochs = make_fixed_length_epochs(raw, duration=5)
+    # Create a toy epochs instance
+    info = info = mne.create_info(
+        ch_names=["xpos", "ypos"], sfreq=100, ch_types="eyegaze"
+    )
+    # here we pretend that the subject was looking at the center of the screen
+    # we limit the gaze data between 860-1060px horizontally and 440-640px vertically
+    data = np.vstack([np.full((1, 100), 1920 / 2), np.full((1, 100), 1080 / 2)])
+    epochs = mne.EpochsArray(data[None, ...], info)
+    epochs.info["chs"][0]["loc"][4] = -1
+    epochs.info["chs"][1]["loc"][4] = 1
 
     if axes:
         axes = plt.subplot()
-    plot_gaze(epochs, width=1920, height=1080, axes=axes)
+    fig = mne.viz.eyetracking.plot_gaze(
+        epochs, width=1920, height=1080, axes=axes, cmap="Greys"
+    )
+    img = fig.axes[0].images[0].get_array()
+    # the pixels in the center of canvas
+    assert 960 in np.where(img)[1]
+    assert np.isclose(np.min(np.where(img)[1]), 860)
+    assert np.isclose(np.max(np.where(img)[1]), 1060)
+    assert 540 in np.where(img)[0]
+    assert np.isclose(np.min(np.where(img)[0]), 440)
+    assert np.isclose(np.max(np.where(img)[0]), 640)
