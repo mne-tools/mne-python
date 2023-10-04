4f1f622a97ad32e04d71256983414b6413890b42
Daniel McCloy <dan@mccloy.info>
2023-10-04 14:41:38 +0300

    remove lazy-backcompat code in inits

--- mne/source_space/__init__.py
+++ mne/source_space/__init__.py
@@ -2,7 +2,7 @@
 
 import lazy_loader as lazy
 
-__getattr_lz__, __dir__, __all__ = lazy.attach(
+__getattr__, __dir__, __all__ = lazy.attach(
     __name__,
     submodules=["_source_space"],
     submod_attrs={
@@ -20,35 +20,3 @@ __getattr_lz__, __dir__, __all__ = lazy.attach(
         ],
     },
 )
-
-
-from . import _source_space
-from ..utils import warn as _warn
-
-
-def __getattr__(name):
-    msg = out = None
-    try:
-        return __getattr_lz__(name)
-    except AttributeError:
-        try:
-            out = getattr(_source_space, name)
-        except AttributeError:
-            pass  # will raise original error below
-        else:
-            # These should be removed (they're in the MNE namespace)
-            msg = f"mne.source_space.{name} is deprecated and will be removed in 1.6, "
-            if name in (
-                "read_freesurfer_lut",
-                "get_mni_fiducials",
-                "get_volume_labels_from_aseg",
-                "get_volume_labels_from_src",
-            ):
-                msg += f"use mne.{name} instead"
-            else:
-                msg += "use public API instead"
-        if out is None:
-            raise
-    if msg is not None:
-        _warn(msg, FutureWarning)
-    return out
