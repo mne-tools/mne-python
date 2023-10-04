f2d7b206fcd0fa67e3bed747803dc0434c0f8e5f
Daniel McCloy <dan@mccloy.info>
2023-10-04 23:05:12 +0300

    simplify __init__.py files

--- mne/gui/__init__.py
+++ mne/gui/__init__.py
@@ -1,9 +1,4 @@
 """Convenience functions for opening GUIs."""
-
 import lazy_loader as lazy
 
-__getattr__, __dir__, __all__ = lazy.attach(
-    __name__,
-    submodules=[],
-    submod_attrs={"_gui": ["coregistration", "_GUIScraper"]},
-)
+(__getattr__, __dir__, __all__) = lazy.attach_stub(__name__, __file__)
