9f174184647872e5a37aae79fa50b6896f3af69a
Judy D Zhu <38392787+JD-Zhu@users.noreply.github.com>
2023-10-16 17:33:15 +1100

    Update config.py

--- mne/datasets/config.py
+++ mne/datasets/config.py
@@ -174,6 +174,14 @@ MNE_DATASETS["opm"] = dict(
     config_key="MNE_DATASETS_OPM_PATH",
 )
 
+MNE_DATASETS["phantom_kit"] = dict(
+    archive_name="MNE-phantom-KIT-24bit.zip",
+    hash="md5:2AEFCF6DE9DFB8B1F6E8D1B7F768EE4E",
+    url="https://osf.io/download/ed3j8/",
+    folder_name="MNE-phantom-KIT-data",
+    config_key="MNE_DATASETS_PHANTOM_KIT_PATH",
+)
+
 MNE_DATASETS["phantom_4dbti"] = dict(
     archive_name="MNE-phantom-4DBTi.zip",
     hash="md5:938a601440f3ffa780d20a17bae039ff",
