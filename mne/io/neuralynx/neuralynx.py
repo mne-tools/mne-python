6bcb83348a02ee631920428416c9410befabf32a
Kristijan Armeni <kristijan.armeni@gmail.com>
2023-10-20 14:18:24 -0400

    - ENH add sample2segment to _raw_extras - use in _read_segment_file instead of _find_first_last_segment

--- mne/io/neuralynx/neuralynx.py
+++ mne/io/neuralynx/neuralynx.py
@@ -60,11 +60,22 @@ class RawNeuralynx(BaseRaw):
             nlx_reader.get_signal_size(block_id, segment)
             for segment in range(n_segments)
         )
+
+        # construct an array of shape (n_total_samples,) indicating
+        # segment membership for each sample
+        sample2segment = np.concatenate(
+            [
+                np.full(shape=(nlx_reader.get_signal_size(block_id, i),), fill_value=i)
+                for i in range(n_segments)
+            ]
+        )
+
         super(RawNeuralynx, self).__init__(
             info=info,
             last_samps=[n_total_samples - 1],
             filenames=[fname],
             preload=preload,
+            raw_extras=[dict(smp2seg=sample2segment)],
         )
 
     def _read_segment_file(self, data, idx, fi, start, stop, cals, mult):
@@ -72,61 +83,47 @@ class RawNeuralynx(BaseRaw):
         from neo.io import NeuralynxIO
 
         nlx_reader = NeuralynxIO(dirname=self._filenames[fi])
-        bl = nlx_reader.read(lazy=True)
-        # TODO: This is massively inefficient -- we should not need to read *all*
-        # samples just to get the ones from `idx` channels and `start:stop` time span.
-        # KA: neuralynx stores continuous recording in smaller segments (read in as neo.Segment objects)
-        # below code tries to be more efficient it delays reading into memory (nlx_reader.read(lazy=True)) 
-        # and then reads in the idx chanels and specified times by passing these as arguments in neo.Segment.AnalogSignal.load()
-
+        neo_block = nlx_reader.read(lazy=True)
         sr = nlx_reader.header["signal_channels"]["sampling_rate"][0]
 
-        # check that every segment has 1 associated neo.AnalogSignal() object (not sure what multiple analogsignals per neo.Segment would mean)
-        assert sum([len(segment.analogsignals) for segment in bl[0].segments]) == len(bl[0].segments)
+        # check that every segment has 1 associated neo.AnalogSignal() object
+        # (not sure what multiple analogsignals per neo.Segment would mean)
+        assert sum([len(segment.analogsignals) for segment in neo_block[0].segments]) == len(neo_block[0].segments)
 
-        # just a wrapper for readability
-        def _find_first_last_segment(segments, start_spl:int, stop_spl:int, sr:float):
+        # gather the start and stop times (in sec) for all signals in segments (assumes 1 signal per segment)
+        # shape = (n_segments, 2) where 2nd dim is (start_time, stop_time)
+        start_stop_times = np.array(
+            [(signal.t_start.item(), signal.t_stop.item()) 
+            for segment in neo_block[0].segments 
+            for signal in segment.analogsignals
+            ]
+        )
 
-            # gather the start and stop times (in sec) for all signals in segments (assumes 1 signal per segment)
-            # shape = (n_segments, 2) where 2nd dim is (start_time, stop_time)
-            onst_offt = np.array([(signal.t_start.item(), signal.t_stop.item()) for segment in segments for signal in segment.analogsignals])  
+        # get times (in sec) for the first and last segment
+        start_time = start/sr
+        stop_time = stop/sr
 
-            # if start/stop are not speficied, read all segments
-            start_segment = 0
-            stop_segment = len(segments) - 1  # -1 because python indexing
-
-            # determine which segment contains the start sample and which the stop sample if start/stop is specified
-            if start_spl > 0: 
-                start_time = start_spl/sr
-                start_segment = np.where((onst_offt[:, 0] <= start_time) & (onst_offt[:, 1] >= start_time))[0].item()
-            if stop_spl is not None:
-                stop_time = stop_spl/sr
-                stop_segment = np.where((onst_offt[:, 0] <= stop_time) & (onst_offt[:, 1] >= stop_time))[0].item()
-
-            return start_segment, stop_segment, onst_offt
-        
-
-        stop_spl = None if stop == data.shape[-1] else stop  # assume we read until the end if not speficied otherwise
-
-        first_seg, last_seg, start_stop_times = _find_first_last_segment(bl[0].segments, 
-                                                                        start_spl=start, 
-                                                                        stop_spl=stop_spl, 
-                                                                        sr=sr
-                                                                        )
-        
+        first_seg = self._raw_extras[0]["smp2seg"][start]  # segment index for the first sample
+        last_seg = self._raw_extras[0]["smp2seg"][stop-1]  # segment index for the last sample
 
         # now select only segments between the one that containst the start sample
         # and the one that contains the stop sample
         sel_times = start_stop_times[first_seg:last_seg+1, :]
         
-        if (start is not None) or (start > 0):
-            sel_times[0, 0] = start/sr   # for the segment containting the start sample, reset the segment start time to the requested `start` value
-        if stop_spl is not None:
-            sel_times[-1, 1] = stop/sr   # same but for stop sample
+        # if we're reading in later than first sample in first segment
+        # or earlier than the last sample in last segment
+        # we need to adjust the start and stop times accordingly
+        if start_time > sel_times[0, 0]:
+            sel_times[0, 0] = start_time
+        if stop_time < sel_times[-1, -1]:
+            sel_times[-1, 1] = stop_time
 
-        # now load the data arrays via neo.Segment.Analogsignal.load() only from the selected segments and channels
+        # now load the data arrays via neo.Segment.Analogsignal.load()
+        # only from the selected segments and channels
         all_data = np.concatenate(
-            [signal.load(channel_indexes=idx, time_slice=(time[0], time[-1])).magnitude for seg, time in zip(bl[0].segments[first_seg:last_seg+1], sel_times) for signal in seg.analogsignals]
+            [signal.load(channel_indexes=idx, time_slice=(time[0], time[-1])).magnitude 
+             for seg, time in zip(neo_block[0].segments[first_seg:last_seg+1], sel_times)
+             for signal in seg.analogsignals]
         ).T
 
         block = all_data  # shape = (len(idx), n_samples))
