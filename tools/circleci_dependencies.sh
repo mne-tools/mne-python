b757510395aa74b972f7858de91695611d32a9b6
Richard Höchenberger <richard.hoechenberger@gmail.com>
2023-11-05 19:40:15 +0100

    Fix parameter order

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ef
 
 python -m pip install --upgrade "pip!=20.3.0" setuptools wheel
-python -m pip install --upgrade --progress-bar off --only-binary "numpy,scipy,matplotlib,pandas,statsmodels" -ev .[full] -r requirements_testing.txt -r requirements_doc.txt PyQt6 git+https://github.com/mne-tools/mne-qt-browser
+python -m pip install --upgrade --progress-bar off --only-binary "numpy,scipy,matplotlib,pandas,statsmodels" -ve .[full] -r requirements_testing.txt -r requirements_doc.txt PyQt6 git+https://github.com/mne-tools/mne-qt-browser
