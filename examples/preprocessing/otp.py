b2e7c466d33e9bdfecb35e471eb159fc83c14c52
Eric Larson <larson.eric.d@gmail.com>
2023-11-09 14:31:18 -0500

    FIX: Simpler

--- examples/preprocessing/otp.py
+++ examples/preprocessing/otp.py
@@ -68,7 +68,8 @@ def compute_bias(raw):
     )
     sphere = mne.make_sphere_model(r0=(0.0, 0.0, 0.0), head_radius=None, verbose=False)
     cov = mne.compute_covariance(epochs, tmax=0, method="oas", rank=None, verbose=False)
-    data = epochs.get_data(tmin=0.036, tmax=0.036, copy=False)[:, :, 0].T
+    idx = epochs.time_as_index(0.036)[0]
+    data = epochs.get_data(copy=False)[:, :, idx].T
     evoked = mne.EvokedArray(data, epochs.info, tmin=0.0)
     dip = fit_dipole(evoked, cov, sphere, n_jobs=None, verbose=False)[0]
     actual_pos = mne.dipole.get_phantom_dipoles()[0][dipole_number - 1]
