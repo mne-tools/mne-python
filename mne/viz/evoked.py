3f2783b889d0749367801485decfecaeed06b7a1
withmywoessner <woessner.jacob@gmail.com>
2023-11-11 00:23:00 -0600

    Add default var

--- mne/viz/evoked.py
+++ mne/viz/evoked.py
@@ -2929,7 +2929,7 @@ def plot_compare_evokeds(
     title = _title_helper_pce(
         title, picked_types, picks=orig_picks, ch_names=ch_names, combine=combine
     )
-
+    topo_disp_title = False
     # setup axes
     if do_topo:
         show_sensors = False
@@ -2940,7 +2940,7 @@ def plot_compare_evokeds(
                 "mne.viz.plot_topo, which is optimized for speed."
             )
         topo_title = title
-        topo_true = axes
+        topo_disp_title = axes
         axes = ["topo"] * len(ch_types)
     else:
         if axes is None:
@@ -3219,7 +3219,7 @@ def plot_compare_evokeds(
     if cmap is not None:
         _draw_colorbar_pce(ax, _colors, _cmap, colorbar_title, colorbar_ticks)
     # finish
-    if topo_true == 'topo':
+    if topo_disp_title:
         plt.suptitle(topo_title)
     plt_show(show)
     return [ax.figure]
