cdd2843a0b3838dbf8d76bbb752d03abcd5d68bd
withmywoessner <woessner.jacob@gmail.com>
2023-11-20 10:58:51 -0600

    Add docstrings and make flake8 compliant

--- mne/tests/test_epochs.py
+++ mne/tests/test_epochs.py
@@ -2129,8 +2129,8 @@ def test_reject_epochs(tmp_path):
 
 
 @testing.requires_testing_data
-# @pytest.mark.parametrize("fname", (fname_raw_testing))
 def test_callable_reject():
+    """Test using a callable for rejection."""
     raw = read_raw_fif(fname_raw_testing, preload=True)
     raw.crop(0, 5)
     raw.del_proj()
@@ -2138,14 +2138,17 @@ def test_callable_reject():
     raw.pick(chans)
     data = raw.get_data()
 
-    # Multipy 20 points of the first channel by 10
+    # Add some artifacts
     new_data = data
     new_data[0, 180:200] *= 1e7
     new_data[0, 610:880] += 1e-3
     edit_raw = mne.io.RawArray(new_data, raw.info)
 
-    events = mne.make_fixed_length_events(edit_raw, id=1, duration=1.0, start=0)
-    epochs = mne.Epochs(edit_raw, events, tmin=0, tmax=1, baseline=None, preload=True)
+    events = mne.make_fixed_length_events(
+        edit_raw, id=1, duration=1.0, start=0)
+    epochs = mne.Epochs(edit_raw, events, tmin=0, tmax=1,
+                        baseline=None, preload=True)
+
     assert len(epochs) == 5
     epochs = mne.Epochs(
         edit_raw,
@@ -2153,9 +2156,8 @@ def test_callable_reject():
         tmin=0,
         tmax=1,
         baseline=None,
-        reject=dict(
-            eeg=lambda x: True if (np.median(x, axis=1) > 1e-3).any() else False
-        ),
+        reject=dict(eeg=lambda x: True if (
+            np.median(x, axis=1) > 1e-3).any() else False),
         preload=True,
     )
     assert epochs.drop_log[2] != ()
@@ -2166,7 +2168,8 @@ def test_callable_reject():
         tmin=0,
         tmax=1,
         baseline=None,
-        reject=dict(eeg=lambda x: True if (np.max(x, axis=1) > 1).any() else False),
+        reject=dict(eeg=lambda x: True if (
+            np.max(x, axis=1) > 1).any() else False),
         preload=True,
     )
     assert epochs.drop_log[0] != ()
