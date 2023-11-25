b58849216d8b4967f5411bdb47d7c822e0e33509
Richard Höchenberger <richard.hoechenberger@gmail.com>
2023-11-25 14:34:05 +0100

    Rearrange signature

--- mne/io/neuralynx/neuralynx.py
+++ mne/io/neuralynx/neuralynx.py
@@ -38,7 +38,12 @@ def read_raw_neuralynx(
     --------
     mne.io.Raw : Documentation of attributes and methods of RawNeuralynx.
     """
-    return RawNeuralynx(fname, preload, verbose, exclude_fname_patterns)
+    return RawNeuralynx(
+        fname,
+        preload=preload,
+        exclude_fname_patterns=exclude_fname_patterns,
+        verbose=verbose,
+    )
 
 
 @fill_doc
@@ -46,7 +51,14 @@ class RawNeuralynx(BaseRaw):
     """RawNeuralynx class."""
 
     @verbose
-    def __init__(self, fname, preload=False, verbose=None, exclude_fname_patterns=None):
+    def __init__(
+        self,
+        fname,
+        *,
+        preload=False,
+        exclude_fname_patterns=None,
+        verbose=None,
+    ):
         _soft_import("neo", "Reading NeuralynxIO files", strict=True)
         from neo.io import NeuralynxIO
 
