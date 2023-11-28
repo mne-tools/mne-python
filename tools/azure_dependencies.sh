4cf857cfeaaca9b43a52317afb80d97b2cebf9bd
Eric Larson <larson.eric.d@gmail.com>
2023-11-28 09:14:53 -0500

    MAINT: Restore NumPy 2.0 testing

--- tools/azure_dependencies.sh
+++ tools/azure_dependencies.sh
@@ -10,16 +10,17 @@ elif [ "${TEST_MODE}" == "pip-pre" ]; then
 	echo "Numpy etc."
 	# See github_actions_dependencies.sh for comments
 	python -m pip install $STD_ARGS --only-binary "numpy" numpy
-	python -m pip install $STD_ARGS --only-binary ":all:" --extra-index-url "https://pypi.anaconda.org/scientific-python-nightly-wheels/simple" "scipy>=1.12.0.dev0" scikit-learn matplotlib pandas statsmodels
-	echo "dipy"
-	python -m pip install $STD_ARGS --only-binary ":all:" --extra-index-url "https://pypi.anaconda.org/scipy-wheels-nightly/simple" dipy
-	echo "h5py"
-	python -m pip install $STD_ARGS --only-binary ":all:" -f "https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com" h5py
+	python -m pip install $STD_ARGS --only-binary ":all:" --extra-index-url "https://pypi.anaconda.org/scientific-python-nightly-wheels/simple" "numpy>=2.0.0.dev0" "scipy>=1.12.0.dev0" scikit-learn matplotlib pandas statsmodels
+	# echo "dipy"
+	# python -m pip install $STD_ARGS --only-binary ":all:" --extra-index-url "https://pypi.anaconda.org/scipy-wheels-nightly/simple" dipy
+	# echo "h5py"
+	# python -m pip install $STD_ARGS --only-binary ":all:" -f "https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com" h5py
+	# echo "OpenMEEG"
+	# pip install $STD_ARGS --only-binary ":all:" --extra-index-url "https://test.pypi.org/simple" openmeeg
 	echo "vtk"
 	python -m pip install $STD_ARGS --only-binary ":all:" --extra-index-url "https://wheels.vtk.org" vtk
-	echo "nilearn and openmeeg"
+	echo "nilearn"
 	python -m pip install $STD_ARGS git+https://github.com/nilearn/nilearn
-	python -m pip install $STD_ARGS --only-binary ":all:" --extra-index-url "https://test.pypi.org/simple" openmeeg
 	echo "pyvista/pyvistaqt"
 	python -m pip install --progress-bar off git+https://github.com/pyvista/pyvista
 	python -m pip install --progress-bar off git+https://github.com/pyvista/pyvistaqt
@@ -30,9 +31,9 @@ elif [ "${TEST_MODE}" == "pip-pre" ]; then
 	echo "joblib"
 	python -m pip install --progress-bar off git+https://github.com/joblib/joblib@master
 	echo "EDFlib-Python"
-	python -m pip install $STD_ARGS git+https://gitlab.com/Teuniz/EDFlib-Python@master
+	python -m pip install $STD_ARGS git+https://github.com/the-siesta-group/edfio
 	./tools/check_qt_import.sh PyQt6
-	python -m pip install $STD_ARGS -e .[hdf5,test]
+	python -m pip install $STD_ARGS -e .[test]
 else
 	echo "Unknown run type ${TEST_MODE}"
 	exit 1
--- tools/github_actions_dependencies.sh
+++ tools/github_actions_dependencies.sh
@@ -24,20 +24,18 @@ else
 	echo "PyQt6"
 	pip install $STD_ARGS --only-binary ":all:" --default-timeout=60 --extra-index-url https://www.riverbankcomputing.com/pypi/simple PyQt6
 	echo "NumPy/SciPy/pandas etc."
-	# As of 2023/11/20 no NumPy 2.0 because it requires everything using its ABI to
-	# compile against 2.0, and h5py isn't (and probably not VTK either)
-	pip install $STD_ARGS --only-binary "numpy" --default-timeout=60 numpy
-	pip install $STD_ARGS --only-binary ":all:" --default-timeout=60 --extra-index-url "https://pypi.anaconda.org/scientific-python-nightly-wheels/simple" scipy scikit-learn matplotlib pillow pandas statsmodels
-	echo "dipy"
-	pip install $STD_ARGS --only-binary ":all:" --default-timeout=60 --extra-index-url "https://pypi.anaconda.org/scipy-wheels-nightly/simple" dipy
-	echo "H5py"
-	pip install $STD_ARGS --only-binary ":all:" -f "https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com" h5py
-	echo "OpenMEEG"
-	pip install $STD_ARGS --only-binary ":all:" --extra-index-url "https://test.pypi.org/simple" openmeeg
+	pip install $STD_ARGS --only-binary ":all:" --default-timeout=60 --extra-index-url "https://pypi.anaconda.org/scientific-python-nightly-wheels/simple" "numpy>=2.0.0.dev0" "scipy>=1.12.0.dev0" scikit-learn matplotlib pillow pandas statsmodels
+	# No dipy, h5py, openmeeg until they update to NumPy 2.0 compat
+	INSTALL_KIND="test_extra"
+	# echo "dipy"
+	# pip install $STD_ARGS --only-binary ":all:" --default-timeout=60 --extra-index-url "https://pypi.anaconda.org/scipy-wheels-nightly/simple" dipy
+	# echo "H5py"
+	# pip install $STD_ARGS --only-binary ":all:" -f "https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com" h5py
+	# echo "OpenMEEG"
+	# pip install $STD_ARGS --only-binary ":all:" --extra-index-url "https://test.pypi.org/simple" openmeeg
 	# No Numba because it forces an old NumPy version
-	echo "nilearn and openmeeg"
+	echo "nilearn"
 	pip install $STD_ARGS git+https://github.com/nilearn/nilearn
-	pip install $STD_ARGS openmeeg
 	echo "VTK"
 	pip install $STD_ARGS --only-binary ":all:" --extra-index-url "https://wheels.vtk.org" vtk
 	python -c "import vtk"
@@ -53,10 +51,10 @@ else
 	pip install $STD_ARGS git+https://github.com/nipy/nibabel.git
 	echo "joblib"
 	pip install $STD_ARGS git+https://github.com/joblib/joblib@master
-	echo "EDFlib-Python"
-	pip install $STD_ARGS git+https://gitlab.com/Teuniz/EDFlib-Python@master
-	# Until Pandas is fixed, make sure we didn't install it
-	! python -c "import pandas"
+	echo "edfio"
+	pip install $STD_ARGS git+https://github.com/the-siesta-group/edfio
+	# Make sure we're on a NumPy 2.0 variant
+	python -c "import numpy as np; assert np.__version__[0] == '2', np.__version__"
 fi
 echo ""
 
