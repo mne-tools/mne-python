88fb4a612ed7f109c3bac1910c38e32ccea49ebb
Richard Höchenberger <richard.hoechenberger@gmail.com>
2023-11-29 16:17:37 +0100

    MRG: Remove `_version.py` file; and small doc cleanups (#12245)

--- mne/__init__.py
+++ mne/__init__.py
@@ -23,11 +23,10 @@ try:
 
     __version__ = version("mne")
 except Exception:
-    try:
-        from ._version import __version__
-    except ImportError:
-        __version__ = "0.0.0"
+    __version__ = "0.0.0"
+
 (__getattr__, __dir__, __all__) = lazy.attach_stub(__name__, __file__)
+
 # initialize logging
 from .utils import set_log_level, set_log_file
 
