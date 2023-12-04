44ad96fcb1421b0d030161925a7d89dcda30b5b8
Eric Larson <larson.eric.d@gmail.com>
2023-12-04 13:18:56 -0500

    BUG: Move defusedxml to optional dependencies

--- mne/io/egi/events.py
+++ mne/io/egi/events.py
@@ -7,9 +7,8 @@ from glob import glob
 from os.path import basename, join, splitext
 
 import numpy as np
-from defusedxml.ElementTree import parse
 
-from ...utils import logger
+from ...utils import _soft_import, logger
 
 
 def _read_events(input_fname, info):
@@ -82,6 +81,9 @@ def _read_mff_events(filename, sfreq):
 
 def _parse_xml(xml_file):
     """Parse XML file."""
+    _soft_import("defusedxml", "reading EGI MFF data")
+    from defusedxml.minidom import parse
+
     xml = parse(xml_file)
     root = xml.getroot()
     return _xml2list(root)
