accbbd1599a13b854fee87ca1678fb8f943154d2
Scott Huberty <scott.huberty@mail.mcgill.ca>
2023-12-16 12:49:38 -0800

    STY: rm no-op quotations after black formatting

--- mne/io/egi/egimff.py
+++ mne/io/egi/egimff.py
@@ -498,7 +498,7 @@ class RawMff(BaseRaw):
                             more_excludes.append(ii)
                 if len(exclude_inds) + len(more_excludes) == len(event_codes):
                     warn(
-                        "Did not find any event code with more than one " "event.",
+                        "Did not find any event code with more than one event.",
                         RuntimeWarning,
                     )
                 else:
@@ -626,7 +626,7 @@ class RawMff(BaseRaw):
             np.concatenate([idx[key] for key in keys]), np.arange(len(chs))
         ):
             raise ValueError(
-                "Currently interlacing EEG and PNS channels" "is not supported"
+                "Currently interlacing EEG and PNS channels is not supported"
             )
         egi_info["kind_bounds"] = [0]
         for key in keys:
