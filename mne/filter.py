712de2b2874a41d15ae19ca74194f09cb10575eb
Hamza Abdelhedi <hamza.abdelhedii@gmail.com>
2023-12-24 21:42:33 -0500

    return to line/description

--- mne/filter.py
+++ mne/filter.py
@@ -2630,7 +2630,8 @@ class FilterMixin:
                   ``len(picks) * n_times`` additional time points need to
                   be temporarily stored in memory.
 
-        .. note:: When working on SourceEstimates the sample rate of the original data is inferred from tstep.
+        .. note:: When working on SourceEstimates the sample rate of the original 
+                    data is inferred from tstep.
 
         For more information, see the tutorials
         :ref:`disc-filtering` and :ref:`tut-filter-resample` and
