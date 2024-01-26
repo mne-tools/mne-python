57a454ef7f9984565c4f937cf708f7803d818ce8
Eric Larson <larson.eric.d@gmail.com>
2024-01-26 15:15:59 -0500

    FIX: Better

--- mne/preprocessing/tests/test_lof.py
+++ mne/preprocessing/tests/test_lof.py
@@ -13,20 +13,25 @@ raw_fname = base_dir / "test_raw.fif"
 
 
 @pytest.mark.parametrize(
-    "n_neighbors, ch_type, n_bad",
+    "n_neighbors, ch_type, n_ch, n_bad",
     [
-        (8, "eeg", 8),
-        (10, "grad", 2),
-        (20, "mag", 0),
-        (30, "grad", 2),
+        (8, "eeg", 60, 8),
+        (10, "grad", 204, 2),
+        (20, "mag", 102, 0),
+        (30, "grad", 204, 2),
     ],
 )
-def test_lof(n_neighbors, ch_type, n_bad):
+def test_lof(n_neighbors, ch_type, n_ch, n_bad):
     """Test LOF detection."""
     pytest.importorskip("sklearn")
     raw = read_raw_fif(raw_fname).load_data()
     assert raw.info["bads"] == []
-    bads = find_bad_channels_lof(raw, n_neighbors, picks=ch_type)
+    bads, scores = find_bad_channels_lof(
+        raw, n_neighbors, picks=ch_type, return_scores=True
+    )
+    bads_2 = find_bad_channels_lof(raw, n_neighbors, picks=ch_type)
+    assert len(scores) == n_ch
     assert len(bads) == n_bad
+    assert bads == bads_2
     with pytest.raises(ValueError, match="channel type"):
         find_bad_channels_lof(raw)
