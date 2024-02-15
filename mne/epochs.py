4e675cc2b2c2c7b66eb2777f80031e7d3cef94f3
CarinaFo <carinaforster0611@gmail.com>
2024-02-15 13:05:20 +1000

    added set bad epochs to NaN method for epochs class

--- mne/epochs.py
+++ mne/epochs.py
@@ -693,6 +693,41 @@ class BaseEpochs(
         self._check_consistency()
         self.set_annotations(annotations, on_missing="ignore")
 
+
+    def set_bad_epochs_to_NaN(
+        self,
+        bad_epochs_indices: list = None
+    ):
+        """
+        define bad epochs based on indices list and set to NaN.
+
+        Parameters
+        ----------
+        self : instance of Epochs
+        bad_epochs_indices : list of arrays
+            List of arrays with indices of bad epochs per channel.
+
+        Returns
+        -------
+        self : instance of Epochs
+            The modified instance with NaNs replacing outlier epochs.
+        """
+        if len(bad_epochs_indices) != self.get_data().shape[1]:
+                raise RuntimeError(
+                    "The length of the list of bad epochs indices "
+                    "must match the number of channels."
+                )
+        if self.preload:
+            # extract the data from the epochs object: shape (n_epochs, n_channels, n_times)
+            data = self.get_data()
+            # loop over channels of specific type
+            for ch in range(data.shape[1]):
+                # use those indices to set the epochs per channel to NaN for all timepoints
+                data[bad_epochs_indices[ch], ch, :] = np.nan
+            # put back into epochs structure
+            self.data = data
+        return self
+
     def _check_consistency(self):
         """Check invariants of epochs object."""
         if hasattr(self, "events"):
