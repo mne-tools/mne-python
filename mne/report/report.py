5e23fe00bf0f7ec9cf75708269bbf2bd95a2dad6
Sophie Herbst <ksherbst@gmail.com>
2024-02-16 17:18:57 +0100

    ENH: Add image_kwargs to report.add_epochs (#12443)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Richard Höchenberger <richard.hoechenberger@gmail.com>
    Co-authored-by: Eric Larson <larson.eric.d@gmail.com>

--- mne/report/report.py
+++ mne/report/report.py
@@ -1092,6 +1092,7 @@ class Report:
         *,
         psd=True,
         projs=None,
+        image_kwargs=None,
         topomap_kwargs=None,
         drop_log_ignore=("IGNORED",),
         tags=("epochs",),
@@ -1120,6 +1121,11 @@ class Report:
             If ``True``, add PSD plots based on all ``epochs``. If ``False``,
             do not add PSD plots.
         %(projs_report)s
+        image_kwargs : dict | None
+            Keyword arguments to pass to the "epochs image"-generating
+            function (:meth:`mne.Epochs.plot_image`).
+
+            .. versionadded:: 1.7
         %(topomap_kwargs)s
         drop_log_ignore : array-like of str
             The drop reasons to ignore when creating the drop log bar plot.
@@ -1130,7 +1136,7 @@ class Report:
 
         Notes
         -----
-        .. versionadded:: 0.24.0
+        .. versionadded:: 0.24
         """
         tags = _check_tags(tags)
         add_projs = self.projs if projs is None else projs
@@ -1138,6 +1144,7 @@ class Report:
             epochs=epochs,
             psd=psd,
             add_projs=add_projs,
+            image_kwargs=image_kwargs,
             topomap_kwargs=topomap_kwargs,
             drop_log_ignore=drop_log_ignore,
             section=title,
@@ -3900,6 +3907,7 @@ class Report:
         epochs,
         psd,
         add_projs,
+        image_kwargs,
         topomap_kwargs,
         drop_log_ignore,
         image_format,
@@ -3934,9 +3942,16 @@ class Report:
         ch_types = _get_data_ch_types(epochs)
         epochs.load_data()
 
+        if image_kwargs is None:
+            image_kwargs = dict()
+
         for ch_type in ch_types:
             with use_log_level(_verbose_safe_false(level="error")):
-                figs = epochs.copy().pick(ch_type, verbose=False).plot_image(show=False)
+                figs = (
+                    epochs.copy()
+                    .pick(ch_type, verbose=False)
+                    .plot_image(show=False, **image_kwargs)
+                )
 
             assert len(figs) == 1
             fig = figs[0]
