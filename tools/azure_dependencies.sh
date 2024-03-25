169372da67dc243b817f024820b349495a5aa109
Eric Larson <larson.eric.d@gmail.com>
2024-03-25 15:16:03 -0400

    MAINT: Reenable NumPy 2.0 (#12511)

--- tools/azure_dependencies.sh
+++ tools/azure_dependencies.sh
@@ -9,9 +9,9 @@ elif [ "${TEST_MODE}" == "pip-pre" ]; then
 	# python -m pip install $STD_ARGS --only-binary ":all:" --extra-index-url "https://www.riverbankcomputing.com/pypi/simple" "PyQt6!=6.6.1,!=6.6.2" PyQt6-sip PyQt6-Qt6 "PyQt6-Qt6!=6.6.1,!=6.6.2"
 	python -m pip install $STD_ARGS --only-binary ":all:" "PyQt6!=6.6.1,!=6.6.2" PyQt6-sip PyQt6-Qt6 "PyQt6-Qt6!=6.6.1,!=6.6.2"
 	echo "Numpy etc."
-	python -m pip install $STD_ARGS --only-binary ":all:" --default-timeout=60 --extra-index-url "https://pypi.anaconda.org/scientific-python-nightly-wheels/simple" "numpy<2.0.0.dev0" "scipy>=1.14.0.dev0" "scikit-learn>=1.5.dev0" matplotlib pillow statsmodels pyarrow h5py
-	# echo "dipy"
-	# python -m pip install $STD_ARGS --only-binary ":all:" --extra-index-url "https://pypi.anaconda.org/scipy-wheels-nightly/simple" dipy
+	# No pyarrow yet https://github.com/apache/arrow/issues/40216
+	# No h5py (and thus dipy) yet until they improve/refactor thier wheel building infrastructure for Windows
+	python -m pip install $STD_ARGS --only-binary ":all:" --default-timeout=60 --extra-index-url "https://pypi.anaconda.org/scientific-python-nightly-wheels/simple" "numpy>=2.1.0.dev0" "scipy>=1.14.0.dev0" "scikit-learn>=1.5.dev0" matplotlib pillow statsmodels
 	echo "OpenMEEG"
 	pip install $STD_ARGS --only-binary ":all:" --extra-index-url "https://test.pypi.org/simple" "openmeeg>=2.6.0.dev4"
 	echo "vtk"
@@ -29,6 +29,8 @@ elif [ "${TEST_MODE}" == "pip-pre" ]; then
 	python -m pip install --progress-bar off git+https://github.com/joblib/joblib@master
 	echo "EDFlib-Python"
 	python -m pip install $STD_ARGS git+https://github.com/the-siesta-group/edfio
+	# echo "pysnirf2"  # Needs h5py
+	# python -m pip install $STD_ARGS git+https://github.com/BUNPC/pysnirf2
 	./tools/check_qt_import.sh PyQt6
 	python -m pip install $STD_ARGS -e .[test]
 else
