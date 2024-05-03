b819e73e0f57fa16c70d12ceca806e5edd1c812f
Richard Höchenberger <richard.hoechenberger@gmail.com>
2024-05-03 20:05:24 +0200

    Handle doc build [circle full]

--- mne/html_templates/repr/static/repr.js
+++ mne/html_templates/repr/static/repr.js
@@ -2,9 +2,9 @@ const toggleVisibility = (className, button) => {
   const elements = document.querySelectorAll(`.${className}`)
 
   elements.forEach(element => {
-    if (element.classList.contains('repr-element-hidden')) {
+    if (element.classList.contains('repr-element-collapsed')) {
       // Force a reflow to ensure the display change takes effect before removing the class
-      element.classList.remove('repr-element-hidden')
+      element.classList.remove('repr-element-collapsed')
       element.offsetHeight // This forces the browser to recalculate layout
       element.classList.remove('repr-element-faded')
     } else {
@@ -12,7 +12,7 @@ const toggleVisibility = (className, button) => {
       element.classList.add('repr-element-faded')
       element.addEventListener('transitionend', handler = (e) => {
         if (e.propertyName === 'opacity' && getComputedStyle(element).opacity === '0.2') {
-          element.classList.add('repr-element-hidden')
+          element.classList.add('repr-element-collapsed')
           element.removeEventListener('transitionend', handler)
         }
       });
@@ -22,4 +22,5 @@ const toggleVisibility = (className, button) => {
   // Take care of the button content and tooltip
   button.innerText = button.innerText === "➖" ? "➕" : "➖"
   button.title = button.title === 'Hide section' ? 'Show section' : 'Hide section'
+  button.classList.toggle("collapsed")
 }
