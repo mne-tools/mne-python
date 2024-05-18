6df9b2dc035826c9f7e999459f31d55775dfd60b
Richard Höchenberger <richard.hoechenberger@gmail.com>
2024-05-18 16:52:28 +0200

    Make entire row clickable

--- mne/html_templates/repr/static/repr.js
+++ mne/html_templates/repr/static/repr.js
@@ -1,7 +1,12 @@
-const toggleVisibility = (className, button) => {
+const toggleVisibility = (className) => {
+
   const elements = document.querySelectorAll(`.${className}`)
 
   elements.forEach(element => {
+    if (element.classList.contains('repr-section-header')) {
+      // Don't collapse the section header row.
+       return
+    }
     if (element.classList.contains('repr-element-collapsed')) {
       // Force a reflow to ensure the display change takes effect before removing the class
       element.classList.remove('repr-element-collapsed')
@@ -19,7 +24,12 @@ const toggleVisibility = (className, button) => {
     }
   });
 
-  // Take care of the button content and tooltip
-  button.title = button.title === 'Hide section' ? 'Show section' : 'Hide section'
+  // Take care of button (adjust caret)
+  const button = document.querySelectorAll(`.repr-section-header.${className} > th.repr-section-toggle-col > button`)[0]
   button.classList.toggle('collapsed')
+
+  // Take care of the tooltip of the section header row
+  const sectionHeaderRow = document.querySelectorAll(`tr.repr-section-header.${className}`)[0]
+  sectionHeaderRow.classList.toggle('collapsed')
+  sectionHeaderRow.title = sectionHeaderRow.title === 'Hide section' ? 'Show section' : 'Hide section'
 }
