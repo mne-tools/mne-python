abe41fc79bd0b95c8cc77d5f3343793f61f2eb07
Eric Larson <larson.eric.d@gmail.com>
2024-05-19 12:55:04 -0400

    FIX: Use fixture to set and unset

--- mne/decoding/tests/test_search_light.py
+++ mne/decoding/tests/test_search_light.py
@@ -186,16 +186,28 @@ def test_search_light():
         assert isinstance(pipe.estimators_[0], BaggingClassifier)
 
 
-def test_generalization_light():
-    """Test GeneralizingEstimator."""
+@pytest.fixture()
+def metadata_routing():
+    """Temporarily enable metadata routing for new sklearn."""
     import sklearn
+
+    if NEW_MULTICLASS_SAMPLE_WEIGHT:
+        sklearn.set_config(enable_metadata_routing=True)
+
+    yield
+
+    if NEW_MULTICLASS_SAMPLE_WEIGHT:
+        sklearn.set_config(enable_metadata_routing=False)
+
+
+def test_generalization_light(metadata_routing):
+    """Test GeneralizingEstimator."""
     from sklearn.linear_model import LogisticRegression
     from sklearn.metrics import roc_auc_score
     from sklearn.multiclass import OneVsRestClassifier
     from sklearn.pipeline import make_pipeline
 
     if NEW_MULTICLASS_SAMPLE_WEIGHT:
-        sklearn.set_config(enable_metadata_routing=True)
         clf = LogisticRegression(random_state=0)
         clf.set_fit_request(sample_weight=True)
         logreg = OneVsRestClassifier(clf)
@@ -348,12 +360,9 @@ def test_cross_val_predict():
 def test_sklearn_compliance():
     """Test LinearModel compliance with sklearn."""
     pytest.importorskip("sklearn")
-    import sklearn
     from sklearn.linear_model import LogisticRegression
     from sklearn.utils.estimator_checks import check_estimator
 
-    if NEW_MULTICLASS_SAMPLE_WEIGHT:
-        sklearn.set_config(enable_metadata_routing=False)
     est = SlidingEstimator(LogisticRegression(), allow_2d=True)
 
     ignores = (
