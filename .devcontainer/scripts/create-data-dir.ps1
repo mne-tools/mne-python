436fa779ba91f85a43041264d136a13dc8ab7547
Richard Höchenberger <richard.hoechenberger@gmail.com>
2024-05-27 10:07:56 +0000

    Mount host's MNE data dir

--- .devcontainer/scripts/create-data-dir.ps1
+++ .devcontainer/scripts/create-data-dir.ps1
@@ -6,20 +6,20 @@ $newDirectoryPath = "$userProfilePath\mne_data"
 
 # Check if the directory already exists
 if (Test-Path -Path $newDirectoryPath) {
-    Write-Output "Directory already exists: $newDirectoryPath"
+    Write-Output "Found existing MNE data directory: $newDirectoryPath"
 } else {
     # Create the directory since it does not exist
     try {
         New-Item -ItemType Directory -Path $newDirectoryPath -Force
         # Verify if the directory was created successfully
         if (Test-Path -Path $newDirectoryPath) {
-            Write-Output "Directory created: $newDirectoryPath"
+            Write-Output "MNE data directory created: $newDirectoryPath"
         } else {
             Write-Output "Failed to create directory: $newDirectoryPath"
             exit 1
         }
     } catch {
-        Write-Output "Error creating directory: $_"
+        Write-Output "Error creating MNE data directory: $_"
         exit 1
     }
 }
