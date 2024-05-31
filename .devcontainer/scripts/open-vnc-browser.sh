045dbeb5a6a7f5e29ccd8c37190514f441916308
Richard Höchenberger <richard.hoechenberger@gmail.com>
2024-05-31 21:59:49 +0000

    Better xmessage

--- .devcontainer/scripts/open-vnc-browser.sh
+++ .devcontainer/scripts/open-vnc-browser.sh
@@ -4,4 +4,4 @@ set -eu
 
 echo -e "🌏 Opening VNC desktop in web browser…\n"
 xdg-open 'http://localhost:6080?autoconnect=true'
-echo -e "Welcome to the MNE-Python Dev Container!\nCreate a plot in VS Code and it will show up here." | xmessage -file -
+echo -e "Welcome to the MNE-Python Dev Container!\nCreate a plot in VS Code and it will show up here." | xmessage -center -timeout 60 -title "Welcome to MNE-Python!" -file -
