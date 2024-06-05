2f647ac8dca5d5584da3382809f116d6bca25c54
Eric Larson <larson.eric.d@gmail.com>
2024-06-05 16:24:10 -0700

    FIX: 3?

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -3,7 +3,8 @@
 python -m pip install --upgrade "pip!=20.3.0" build
 # TODO: Use PST nightly once it exists and revert SG change
 python -m pip install --upgrade --progress-bar off \
-    --only-binary "numpy,scipy,matplotlib,pandas,statsmodels" \
+    --extra-index-url="https://pypi.anaconda.org/scientific-python-nightly-wheels/simple" \
+    --only-binary "numpy,scipy,matplotlib,pandas,statsmodels,pydata-sphinx-theme" \
+    "pydata-sphinx-theme>=0.15.4.dev0" \
     "git+https://github.com/jschueller/sphinx-gallery.git@paral" \
-    "git+https://github.com/pydata/pydata-sphinx-theme.git" \
     -ve .[full,test,doc]
