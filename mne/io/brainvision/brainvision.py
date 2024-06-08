aca320ffac92da88f942d9aaa627c758d7447f3e
Clemens Brunner <clemens.brunner@gmail.com>
2024-06-08 09:48:53 +0200

    Add option to ignore BrainVision marker types

--- mne/io/brainvision/brainvision.py
+++ mne/io/brainvision/brainvision.py
@@ -50,6 +50,11 @@ class RawBrainVision(BaseRaw):
     scale : float
         The scaling factor for EEG data. Unless specified otherwise by
         header file, units are in microvolts. Default scale factor is 1.
+    ignore_marker_types : bool
+        If ``True``, ignore marker types and only use marker descriptions. Default is
+        ``False``.
+
+        .. versionadded:: 1.8
     %(preload)s
     %(verbose)s
 
@@ -57,6 +62,15 @@ class RawBrainVision(BaseRaw):
     ----------
     impedances : dict
         A dictionary of all electrodes and their impedances.
+
+    Notes
+    -----
+    BrainVision markers consist of a type and a description (in addition to other fields
+    like onset and duration). In contrast, annotations in MNE only have a description.
+    Therefore, a BrainVision marker of type "Stimulus" and description "S  1" will be
+    converted to an annotation "Stimulus/S  1" by default. If you want to ignore the
+    type and instead only use the description, set ``ignore_marker_types=True``, which
+    will convert the same marker to an annotation "S  1".
 
     See Also
     --------
@@ -72,6 +86,7 @@ class RawBrainVision(BaseRaw):
         eog=("HEOGL", "HEOGR", "VEOGb"),
         misc="auto",
         scale=1.0,
+        ignore_marker_types=False,
         preload=False,
         verbose=None,
     ):  # noqa: D107
@@ -129,7 +144,9 @@ class RawBrainVision(BaseRaw):
         self.impedances = _parse_impedance(split_settings, self.info["meas_date"])
 
         # Get annotations from marker file
-        annots = read_annotations(mrk_fname, info["sfreq"])
+        annots = read_annotations(
+            mrk_fname, info["sfreq"], ignore_marker_types=ignore_marker_types
+        )
         self.set_annotations(annots)
 
         # Drop the fake ahdr channel if needed
@@ -207,13 +224,15 @@ def _read_segments_c(raw, data, idx, fi, start, stop, cals, mult):
     _mult_cal_one(data, block, idx, cals, mult)
 
 
-def _read_mrk(fname):
+def _read_mrk(fname, ignore_marker_types=False):
     """Read annotations from a vmrk/amrk file.
 
     Parameters
     ----------
     fname : str
         vmrk/amrk file to be read.
+    ignore_marker_types : bool
+        If True, ignore marker types and only use marker descriptions. Default is False.
 
     Returns
     -------
@@ -293,36 +312,41 @@ def _read_mrk(fname):
         this_duration = int(this_duration) if this_duration.isdigit() else 0
         duration.append(this_duration)
         onset.append(int(this_onset) - 1)  # BV is 1-indexed, not 0-indexed
-        description.append(mtype + "/" + mdesc)
+        if not ignore_marker_types:
+            description.append(mtype + "/" + mdesc)
+        else:
+            description.append(mdesc)
 
     return np.array(onset), np.array(duration), np.array(description), date_str
 
 
-def _read_annotations_brainvision(fname, sfreq="auto"):
+def _read_annotations_brainvision(fname, sfreq="auto", ignore_marker_types=False):
     """Create Annotations from BrainVision vmrk/amrk.
 
-    This function reads a .vmrk or .amrk file and makes an
-    :class:`mne.Annotations` object.
+    This function reads a .vmrk or .amrk file and creates an :class:`mne.Annotations`
+    object.
 
     Parameters
     ----------
     fname : str | object
         The path to the .vmrk/.amrk file.
     sfreq : float | 'auto'
-        The sampling frequency in the file. It's necessary
-        as Annotations are expressed in seconds and vmrk/amrk
-        files are in samples. If set to 'auto' then
-        the sfreq is taken from the .vhdr/.ahdr file that
-        has the same name (without file extension). So
-        data.vmrk/amrk looks for sfreq in data.vhdr or,
-        if it does not exist, in data.ahdr.
+        The sampling frequency in the file. This is necessary because Annotations are
+        expressed in seconds and vmrk/amrk files are in samples. If set to 'auto' then
+        the sfreq is taken from the .vhdr/.ahdr file with the same name (without file
+        extension). So data.vmrk/amrk looks for sfreq in data.vhdr or, if it does not
+        exist, in data.ahdr.
+    ignore_marker_types : bool
+        If True, ignore marker types and only use marker descriptions. Default is False.
 
     Returns
     -------
     annotations : instance of Annotations
         The annotations present in the file.
     """
-    onset, duration, description, date_str = _read_mrk(fname)
+    onset, duration, description, date_str = _read_mrk(
+        fname, ignore_marker_types=ignore_marker_types
+    )
     orig_time = _str_to_meas_date(date_str)
 
     if sfreq == "auto":
@@ -919,6 +943,7 @@ def read_raw_brainvision(
     eog=("HEOGL", "HEOGR", "VEOGb"),
     misc="auto",
     scale=1.0,
+    ignore_marker_types=False,
     preload=False,
     verbose=None,
 ) -> RawBrainVision:
@@ -940,6 +965,9 @@ def read_raw_brainvision(
     scale : float
         The scaling factor for EEG data. Unless specified otherwise by
         header file, units are in microvolts. Default scale factor is 1.
+    ignore_marker_types : bool
+        If ``True``, ignore marker types and only use marker descriptions. Default is
+        ``False``.
     %(preload)s
     %(verbose)s
 
@@ -948,6 +976,15 @@ def read_raw_brainvision(
     raw : instance of RawBrainVision
         A Raw object containing BrainVision data.
         See :class:`mne.io.Raw` for documentation of attributes and methods.
+
+    Notes
+    -----
+    BrainVision markers consist of a type and a description (in addition to other fields
+    like onset and duration). In contrast, annotations in MNE only have a description.
+    Therefore, a BrainVision marker of type "Stimulus" and description "S  1" will be
+    converted to an annotation "Stimulus/S  1" by default. If you want to ignore the
+    type and instead only use the description, set ``ignore_marker_types=True``, which
+    will convert the same marker to an annotation "S  1".
 
     See Also
     --------
@@ -958,6 +995,7 @@ def read_raw_brainvision(
         eog=eog,
         misc=misc,
         scale=scale,
+        ignore_marker_types=ignore_marker_types,
         preload=preload,
         verbose=verbose,
     )
