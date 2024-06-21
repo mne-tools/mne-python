e89e74b65f7654b63cdd663da0b5ed9042edf27e
Eric Larson <larson.eric.d@gmail.com>
2024-06-21 10:16:52 -0400

    FIX: Location

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -2,4 +2,4 @@
 
 python -m pip install --upgrade "pip!=20.3.0" build
 # https://github.com/dipy/dipy/issues/3265 for numpy, dipy
-python -m pip install --upgrade --progress-bar off --only-binary "numpy<2,dipy!=1.9.0,scipy,matplotlib,pandas,statsmodels" git+https://github.com/sphinx-gallery/sphinx-gallery.git -ve .[full,test,doc]
+python -m pip install --upgrade --progress-bar off --only-binary "numpy,dipy,scipy,matplotlib,pandas,statsmodels" git+https://github.com/sphinx-gallery/sphinx-gallery.git -ve .[full,test,doc] "numpy<2" "dipy!=1.9.0"
