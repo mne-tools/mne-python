5d1cbae78a354321aef1f7f2fa5ecf6881f1533c
CarinaFo <carinaforster0611@gmail.com>
2024-07-06 10:36:55 +0200

    first draft without cluster plotting class implemented

--- mne/stats/cluster_level.py
+++ mne/stats/cluster_level.py
@@ -4,12 +4,17 @@
 # License: BSD-3-Clause
 # Copyright the MNE-Python contributors.
 
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 from scipy import ndimage, sparse
 from scipy.sparse.csgraph import connected_components
 from scipy.stats import f as fstat
 from scipy.stats import t as tstat
 
+from .. import EvokedArray
+from ..channels import find_ch_adjacency
 from ..fixes import has_numba, jit
 from ..parallel import parallel_func
 from ..source_estimate import MixedSourceEstimate, SourceEstimate, VolSourceEstimate
@@ -18,6 +23,7 @@ from ..utils import (
     ProgressBar,
     _check_option,
     _pl,
+    _soft_import,
     _validate_type,
     check_random_state,
     logger,
@@ -25,6 +31,7 @@ from ..utils import (
     verbose,
     warn,
 )
+from ..viz import plot_compare_evokeds
 from .parametric import f_oneway, ttest_1samp_no_p
 
 
@@ -1723,3 +1730,289 @@ def summarize_clusters_stc(
     data_summary[:, 0] = np.sum(data_summary, axis=1)
 
     return klass(data_summary, vertices, tmin, tstep, subject)
+
+
+def cluster_test(
+    df: pd.DataFrame,
+    formula: str = None,  # Wilkinson notation formula for design matrix
+    n_permutations: int = 10000,
+    seed: None | int | np.random.RandomState = None,
+    tail: int = 0,  # 0 for two-tailed, 1 for greater, -1 for less
+    n_jobs: int = 1,  # how many cores to use
+    adjacency: tuple = None,
+    max_step: int = 1,  # maximum distance between samples (time points)
+    exclude: list = None,  # exclude no time points or channels
+    step_down_p: int = 0,  # step down in jumps test
+    t_power: int = 1,  # weigh each location by its stats score
+    out_type: str = "indices",
+    check_disjoint: bool = False,
+    buffer_size: int = None,  # block size for chunking the data
+):
+    """
+    Run the cluster test using the new API.
+
+    # currently supports paired t-test
+
+    Parameters
+    ----------
+    dataframe : pd.DataFrame
+        Dataframe with evoked data, conditions and subject IDs.
+    formula : str, optional
+        Wilkinson notation formula for design matrix. Default is None.
+    n_permutations : int, optional
+        Number of permutations. Default is 10000.
+    seed : None | int | np.random.RandomState, optional
+        Seed for the random number generator. Default is None.
+    tail : int, optional
+        0 for two-tailed, 1 for greater, -1 for less. Default is 0.
+    n_jobs : int, optional
+        How many cores to use. Default is 1.
+    adjacency : None, optional
+        Adjacency matrix. Default is None.
+    max_step : int, optional
+        Maximum distance between samples (time points). Default is 1.
+    exclude : np.Array, optional
+        Exclude no time points or channels. Default is None.
+    step_down_p : int, optional
+        Step down in jumps test. Default is 0.
+    t_power : int, optional
+        Weigh each location by its stats score. Default is 1.
+    out_type : str, optional
+        Output type. Default is "indices".
+    check_disjoint : bool, optional
+        Check if clusters are disjoint. Default is False.
+    buffer_size : int, optional
+        Block size for chunking the data. Default is None.
+    seed : int, optional
+        Seed for the random number generator. Default is None.
+
+    Returns
+    -------
+    T_obs : array
+        The observed test statistic.
+    clusters : list
+        List of clusters.
+    cluster_p_values : array
+        Array of cluster p-values.
+    H0 : array
+        The permuted test statistics.
+    """
+    # for now this assumes a dataframe with a column for evoked data or epochs
+    # add a data column to the dataframe (numpy array)
+    df["data"] = [evoked.data for evoked in df.evoked]
+
+    # extract number of channels and timepoints
+    # (eventually should also allow for frequency)
+    n_channels, n_timepoints = df["data"][0].shape
+
+    # convert wide format to long format for formulaic
+    df_long = unpack_time_and_channels(df)
+
+    # Pivot the DataFrame
+    pivot_df = df_long.pivot_table(
+        index=["subject_index", "channel", "timepoint"],
+        columns="condition",
+        values="value",
+    ).reset_index()
+
+    # if not 2 unique conditions raise error
+    if len(pd.unique(df.condition)) != 2:
+        raise ValueError("Condition list needs to contain 2 unique values")
+
+    # Get the unique conditions
+    conditions = np.unique(df.condition)
+
+    # Compute the difference (assuming there are only 2 conditions)
+    pivot_df["evoked"] = pivot_df[conditions[0]] - pivot_df[conditions[1]]
+
+    # Optional: Clean up the DataFrame
+    pivot_df = pivot_df[["subject_index", "channel", "timepoint", "evoked"]]
+
+    # check if formula is present
+    if formula is not None:
+        formulaic = _soft_import(
+            "formulaic", purpose="set up Design Matrix"
+        )  # soft import (not a dependency for MNE)
+
+        # for the paired t-test y is the difference between conditions
+        # X is the design matrix with a column with 1s and 0s for each participant
+        # Create the design matrix using formulaic
+        y, X = formulaic.model_matrix(formula, pivot_df)
+    else:
+        raise ValueError(
+            "Formula is required and needs to be a string in Wilkinson notation."
+        )
+
+    # now prep design matrix outcome variable for input into MNE cluster function
+    # we initially had first channels, then timepoints,
+    # now we need first timepoints, then channels
+    y_for_cluster = y.values.reshape(-1, n_channels, n_timepoints).transpose(0, 2, 1)
+
+    adjacency, _ = find_ch_adjacency(df["evoked"][0].info, ch_type="eeg")
+
+    # define stat function and threshold
+    stat_fun, threshold = _check_fun(
+        X=y_for_cluster, stat_fun=None, threshold=None, tail=0, kind="within"
+    )
+
+    # Run the cluster-based permutation test
+    T_obs, clusters, cluster_p_values, H0 = _permutation_cluster_test(
+        [y_for_cluster],
+        n_permutations=10000,
+        threshold=threshold,
+        stat_fun=stat_fun,
+        tail=tail,
+        n_jobs=n_jobs,
+        adjacency=adjacency,
+        max_step=max_step,  # maximum distance between samples (time points)
+        exclude=exclude,  # exclude no time points or channels
+        step_down_p=step_down_p,  # step down in jumps test
+        t_power=t_power,  # weigh each location by its stats score
+        out_type=out_type,
+        check_disjoint=check_disjoint,
+        buffer_size=buffer_size,  # block size for chunking the data
+        seed=seed,
+    )
+
+    print(min(cluster_p_values))
+
+    return T_obs, clusters, cluster_p_values, H0
+
+
+def unpack_time_and_channels(df):
+    """
+    Extract the time and channel data from the DataFrame.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame in wide format.
+    """
+    # Extracting all necessary data using list comprehensions for better performance
+    long_format_data = [
+        {
+            "condition": row["condition"],
+            "subject_index": row["subject_index"],
+            "channel": channel,
+            "timepoint": timepoint,
+            "value": row["data"][channel, timepoint],
+        }
+        for idx, row in df.iterrows()
+        for channel in range(row["data"].shape[0])
+        for timepoint in range(row["data"].shape[1])
+    ]
+
+    # Creating the long format DataFrame
+    df_long = pd.DataFrame(long_format_data)
+
+    return df_long
+
+
+def plot_cluster(
+    contrast, target_only, non_target_only, T_obs, clusters, cluster_p_values
+):
+    """
+    Plot the cluster with the lowest p-value.
+
+    Parameters
+    ----------
+    contrast : list
+        List of contrast evoked objects.
+    target_only : list
+        List of target evoked objects.
+    non_target_only : list
+        List of non-target evoked objects.
+    T_obs : array
+        The observed test statistic.
+    clusters : list
+        List of clusters.
+    cluster_p_values : array
+        Array of cluster p-values.
+
+    Returns
+    -------
+    None
+
+    """
+    # configure variables for visualization
+    colors = {"target": "crimson", "non-target": "steelblue"}
+
+    # organize data for plotting
+    evokeds = {"target": target_only, "non-target": non_target_only}
+
+    lowest_p_cluster = np.argmin(cluster_p_values)
+
+    # plot the cluster with the lowest p-value
+    time_inds, space_inds = np.squeeze(clusters[lowest_p_cluster])
+    ch_inds = np.unique(space_inds)
+    time_inds = np.unique(time_inds)
+
+    # get topography for F stat
+    t_map = T_obs[time_inds, ...].mean(axis=0)
+
+    # get signals at the sensors contributing to the cluster
+    sig_times = contrast[0].times[time_inds]
+
+    # create spatial mask
+    mask = np.zeros((t_map.shape[0], 1), dtype=bool)
+    mask[ch_inds, :] = True
+
+    # initialize figure
+    fig, ax_topo = plt.subplots(1, 1, figsize=(10, 3), layout="constrained")
+
+    # plot average test statistic and mark significant sensors
+    t_evoked = EvokedArray(t_map[:, np.newaxis], contrast[0].info, tmin=0)
+    t_evoked.plot_topomap(
+        times=0,
+        mask=mask,
+        axes=ax_topo,
+        cmap="Reds",
+        vlim=(np.min, np.max),
+        show=False,
+        colorbar=False,
+        mask_params=dict(markersize=10),
+    )
+    image = ax_topo.images[0]
+
+    # remove the title that would otherwise say "0.000 s"
+    ax_topo.set_title("")
+
+    # soft import?
+    # make_axes_locatable = _soft_import(
+    #    "mpl_toolkits.axes_grid1.make_axes_locatable",
+    #    purpose="plot cluster results"
+    # )  # soft import (not a dependency for MNE)
+
+    # create additional axes (for ERF and colorbar)
+    divider = make_axes_locatable(ax_topo)
+
+    # add axes for colorbar
+    ax_colorbar = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(image, cax=ax_colorbar)
+    ax_topo.set_xlabel(
+        "Averaged t-map ({:0.3f} - {:0.3f} s)".format(*sig_times[[0, -1]])
+    )
+
+    # add new axis for time courses and plot time courses
+    ax_signals = divider.append_axes("right", size="300%", pad=1.2)
+    title = f"Cluster #1, {len(ch_inds)} sensor"
+    if len(ch_inds) > 1:
+        title += "s (mean)"
+        plot_compare_evokeds(
+            evokeds,
+            title=title,
+            picks=ch_inds,
+            axes=ax_signals,
+            colors=colors,
+            show=False,
+            split_legend=True,
+            truncate_yaxis="auto",
+        )
+
+    # plot temporal cluster extent
+    ymin, ymax = ax_signals.get_ylim()
+    ax_signals.fill_betweenx(
+        (ymin, ymax), sig_times[0], sig_times[-1], color="green", alpha=0.3
+    )
+
+    plt.show()
