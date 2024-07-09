bfe868aed8698ffa4024b6ea998edc27b9579d25
Eric Larson <larson.eric.d@gmail.com>
2024-07-09 15:43:03 -0400

    MAINT: Fix doc build

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -2,4 +2,8 @@
 
 python -m pip install --upgrade "pip!=20.3.0" build
 # https://github.com/dipy/dipy/issues/3265 for numpy, dipy
-python -m pip install --upgrade --progress-bar off --only-binary "numpy,dipy,scipy,matplotlib,pandas,statsmodels" git+https://github.com/sphinx-gallery/sphinx-gallery.git -ve .[full,test,doc] "numpy<2" "dipy!=1.9.0"
+python -m pip install --upgrade --progress-bar off \
+    --only-binary "numpy,dipy,scipy,matplotlib,pandas,statsmodels" \
+    -ve .[full,test,doc] "numpy<2" "dipy!=1.9.0" \
+    "git+https://github.com/larsoner/pyvista.git@refcycle" \
+    git+https://github.com/sphinx-gallery/sphinx-gallery.git
