d1208edfc2e69e0a3c368bef22f096f1f0bfa77f
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2024-07-28 07:41:23 +0200

    missed one

--- mne/tests/test_chpi.py
+++ mne/tests/test_chpi.py
@@ -97,7 +97,7 @@ def test_chpi_adjust():
     msg = [
         "HPIFIT: 5 coils digitized in order 5 1 4 3 2",
         "HPIFIT: 3 coils accepted: 1 2 4",
-        "Hpi coil moments (3 5):",
+        "Hpi coil moments (3, 5):",
         "2.08542e-15 -1.52486e-15 -1.53484e-15",
         "2.14516e-15 2.09608e-15 7.30303e-16",
         "-3.2318e-16 -4.25666e-16 2.69997e-15",
