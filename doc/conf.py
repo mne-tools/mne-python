35a2800061f1693a2f0c3c89d56919edaa1e35e2
Mohammad Mowlavi <79265093+MohammadMow@users.noreply.github.com>
2024-08-02 20:18:42 +0330

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -121,9 +121,9 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_includes", "changes/devel"]
 
 # NB: changes here should also be made to the linkcheck target in the Makefile
+exclude_patterns = ["_includes", "changes/devel"]
 
 # The suffix of source filenames.
 source_suffix = ".rst"
