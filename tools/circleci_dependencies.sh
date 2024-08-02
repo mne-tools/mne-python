51f59e81f0dbf1fa2ba2561c674d9e52c93d18df
Eric Larson <larson.eric.d@gmail.com>
2024-08-02 08:00:35 -0400

    FIX: Try new dipy [circle full]

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -ef
 
 python -m pip install --upgrade "pip!=20.3.0" build
-# https://github.com/dipy/dipy/issues/3265 for numpy, dipy
 python -m pip install --upgrade --progress-bar off \
     --only-binary "numpy,dipy,scipy,matplotlib,pandas,statsmodels" \
-    -ve .[full,test,doc] "numpy==1.26.4" "dipy!=1.9.0" \
+    -ve .[full,test,doc] "numpy>=2" "dipy>1.9.0" \
+    --extra-index-url "https://pypi.anaconda.org/scientific-python-nightly/simple" \
     "git+https://github.com/larsoner/pyvista.git@refcycle" \
     git+https://github.com/sphinx-gallery/sphinx-gallery.git \
     \
