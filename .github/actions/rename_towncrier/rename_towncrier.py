f10c3870416d8801527f14d866499216eebf6819
Eric Larson <larson.eric.d@gmail.com>
2024-08-09 12:39:33 -0400

    DOC: Fix headers

--- .github/actions/rename_towncrier/rename_towncrier.py
+++ .github/actions/rename_towncrier/rename_towncrier.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# Authors: The MNE-Python contributors.
+# License: BSD-3-Clause
+# Copyright the MNE-Python contributors.
 
 # Adapted from action-towncrier-changelog
 import json
