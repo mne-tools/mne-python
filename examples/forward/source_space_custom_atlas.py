4307d95571593b36ed410113162a0969881cbfcc
Shrecki <tericarax@gmail.com>
2024-10-15 11:24:50 +0200

    Updates description and fix styling of source space custom atlas

--- examples/forward/source_space_custom_atlas.py
+++ examples/forward/source_space_custom_atlas.py
@@ -6,12 +6,16 @@ Source reconstruction with a custom atlas
 =========================================
 
 This example shows how to use a custom atlas when performing source reconstruction.
-We showcase on the sample dataset how to apply the Yeo atlas during source reconstruction.
+We showcase on the sample dataset how to apply the Yeo atlas during source
+ reconstruction.
 You should replace the atlas with your own atlas and your own subject.
 
-Any atlas can be used instead of Yeo, provided each region contains a single label (ie: no probabilistic atlas).
+Any atlas can be used instead of Yeo, provided each region contains a single
+ label (ie: no probabilistic atlas).
 
-.. warning:: This tutorial uses FSL and FreeSurfer to perform MRI coregistrations. If you use a different software, replace the coregistration function appropriately.
+.. warning:: This tutorial uses FSL and FreeSurfer to perform MRI
+ coregistrations. If you use a different software, replace the
+ coregistration function appropriately.
 """
 
 # Authors: Fabrice Guibert <fabrice.guibert.96@gmail.com>
@@ -28,8 +32,11 @@ import nilearn.datasets
 
 import mne
 import mne.datasets
+from mne._freesurfer import read_freesurfer_lut
+from mne.minimum_norm import apply_inverse, make_inverse_operator
 
-# The atlas is in a template space. We download here as an example Yeo 2011's atlas, which is in the MNI152 1mm template space.
+# The atlas is in a template space. We download here as an example Yeo
+# 2011's atlas, which is in the MNI152 1mm template space.
 # Replace this part with your atlas and the template space you used.
 
 nilearn.datasets.fetch_atlas_yeo_2011()  # Download Yeo 2011
@@ -40,7 +47,8 @@ atlas_path = Path(yeo_path, "Yeo2011_7Networks_MNI152_FreeSurferConformed1mm.nii
 atlas_template_T1_path = Path(yeo_path, "FSL_MNI152_FreeSurferConformed_1mm.nii.gz")
 
 # The participant's T1 data. Here, we consider the sample dataset
-# The brain should be skull stripped. After freesurfer preprocessing, you can either use brain.mgz or antsdn.brain.mgz
+# The brain should be skull stripped. After freesurfer preprocessing,
+# you can either use brain.mgz or antsdn.brain.mgz
 data_path = mne.datasets.sample.data_path()
 subjects_mri_dir = Path(data_path, "subjects")
 subject_mri_path = Path(subjects_mri_dir, "sample")
@@ -53,8 +61,10 @@ assert T1_participant_path.is_file()
 
 # %%
 # The first step is to put the atlas in subject space.
-# We show this step with FSL and freesurfer with linear coregistration. If your atlas is already in participant space,
-# you can skip this step. Coregistration is done in two steps: compute the atlas template to subject T1 transform and apply this transform
+# We show this step with FSL and freesurfer with linear coregistration.
+# If your atlas is already in participant space,
+# you can skip this step. Coregistration is done in two steps:
+# compute the atlas template to subject T1 transform and apply this transform
 # to the atlas file with nearest neighbour interpolation.
 
 # FSL does not know how to read .mgz, so we need to convert the T1 to nifti format
@@ -62,26 +72,43 @@ assert T1_participant_path.is_file()
 T1_participant_nifti = Path(str(T1_participant_path).replace("mgz", "nii.gz"))
 subprocess.run(["mri_convert", T1_participant_path, T1_participant_nifti])
 
-# Compute template to subject anatomical transform
+# Compute template to subject anatomical transform using flirt
 template_to_anat_transform_path = Path(mri_path, "template_to_anat.mat")
-subprocess.run(["flirt", 
-                "-in", atlas_template_T1_path, 
-                "-ref", T1_participant_nifti,
-                "-out", Path(mri_path, "T1_atlas_coreg"),
-                "-omat", template_to_anat_transform_path])
+subprocess.run(
+    [
+        "flirt",
+        "-in",
+        atlas_template_T1_path,
+        "-ref",
+        T1_participant_nifti,
+        "-out",
+        Path(mri_path, "T1_atlas_coreg"),
+        "-omat",
+        template_to_anat_transform_path,
+    ]
+)
 
 # Apply the transform to the atlas
 atlas_participant = Path(mri_path, "yeo_atlas.nii.gz")
 
-subprocess.run(["flirt",
-                "-in", atlas_path,
-                "-ref", T1_participant_nifti,
-                "-out", atlas_participant,
-                "-applyxfm -init", template_to_anat_transform_path,
-                "-interp nearestneighbour"])
+subprocess.run(
+    [
+        "flirt",
+        "-in",
+        atlas_path,
+        "-ref",
+        T1_participant_nifti,
+        "-out",
+        atlas_participant,
+        "-applyxfm -init",
+        template_to_anat_transform_path,
+        "-interp nearestneighbour",
+    ]
+)
 
 # Convert resulting atlas from nifti to mgz
-# The filename must finish with aseg, to indicate to MNE that it is a proper atlas segmentation.
+# The filename must finish with aseg, to indicate to MNE that it is
+#  a proper atlas segmentation.
 atlas_converted = Path(str(atlas_participant).replace(".nii.gz", "aseg.mgz"))
 subprocess.run(["mri_convert", atlas_participant, atlas_converted])
 
@@ -96,8 +123,6 @@ assert atlas_converted.is_file()
 # In FreeSurfer and atlases, these typically take the form of lookup tables.
 # You can also build the dictionary by hand.
 
-from mne._freesurfer import read_freesurfer_lut
-
 atlas_labels = read_freesurfer_lut(Path(yeo_path, "Yeo2011_7Networks_ColorLUT.txt"))[0]
 print(atlas_labels)
 
@@ -105,9 +130,8 @@ print(atlas_labels)
 del atlas_labels["NONE"]
 
 # %%
-# For the purpose of source reconstruction, let's create a volumetric source estimate and source reconstruction with e.g eLORETA.
-from mne.minimum_norm import apply_inverse, make_inverse_operator
-
+# For the purpose of source reconstruction, let's create a volumetric
+# source estimate and source reconstruction with e.g eLORETA.
 vol_src = mne.setup_volume_source_space(
     "sample",
     subjects_dir=subjects_mri_dir,
