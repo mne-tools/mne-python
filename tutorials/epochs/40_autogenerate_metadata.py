3687006bf968100e772b451dbba9bb2d6f8d8966
Clemens Brunner <clemens.brunner@gmail.com>
2024-11-05 14:27:08 +0100

    Smaller suptitle

--- tutorials/epochs/40_autogenerate_metadata.py
+++ tutorials/epochs/40_autogenerate_metadata.py
@@ -485,7 +485,7 @@ mne.viz.plot_compare_evokeds(
 
 # topoplot of average field from time 0.0-0.1 s
 fig = resp_erp_incorrect.plot_topomap(times=0.05, average=0.1, size=3)
-fig.suptitle("Mean topography after incorrect responses", fontsize=16)
+fig.suptitle("Mean topography after incorrect responses", fontsize=14)
 
 # %%
 # We can see a strong negative deflection immediately after incorrect
