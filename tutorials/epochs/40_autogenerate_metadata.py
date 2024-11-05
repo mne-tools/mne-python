f07d0669fe4d620ccee9fbc144e9a6fd74ef2c54
Richard Höchenberger <richard.hoechenberger@gmail.com>
2024-11-05 14:03:36 +0100

    Fix averaging

--- tutorials/epochs/40_autogenerate_metadata.py
+++ tutorials/epochs/40_autogenerate_metadata.py
@@ -482,7 +482,7 @@ mne.viz.plot_compare_evokeds(
 )
 
 # topoplot of average field from time 0.0-0.1 s
-fig = resp_erp_incorrect.plot_topomap(times=0.05, average=0.05, size=3)
+fig = resp_erp_incorrect.plot_topomap(times=0.05, average=0.1, size=3)
 fig.suptitle("Avg. topography 0–100 ms after incorrect responses", fontsize=16)
 
 # %%
