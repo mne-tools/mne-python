mne-tools/mne-python

commit 9fd1869f64aefa6386cc1384821f7c524ef771c0

Fix bug with subject_info when loading from and exporting to EDF file (#11952)
Co-authored-by: Paul ROUJANSKY <paul.roujansky@bioserenity.com>

mne/io/edf/edf.py
@@ -642,6 +642,49 @@ def _get_info(
     info["chs"] = chs
     info["ch_names"] = ch_names
 
+    # Subject information
+    info["subject_info"] = {}
+
+    # String subject identifier
+    if edf_info["subject_info"].get("id") is not None:
+        info["subject_info"]["his_id"] = edf_info["subject_info"]["id"]
+    # Subject sex (0=unknown, 1=male, 2=female)
+    if edf_info["subject_info"].get("sex") is not None:
+        if edf_info["subject_info"]["sex"] == "M":
+            info["subject_info"]["sex"] = 1
+        elif edf_info["subject_info"]["sex"] == "F":
+            info["subject_info"]["sex"] = 2
+        else:
+            info["subject_info"]["sex"] = 0
+    # Subject names (first, middle, last).
+    if edf_info["subject_info"].get("name") is not None:
+        sub_names = edf_info["subject_info"]["name"].split("_")
+        if len(sub_names) < 2 or len(sub_names) > 3:
+            info["subject_info"]["last_name"] = edf_info["subject_info"]["name"]
+        elif len(sub_names) == 2:
+            info["subject_info"]["first_name"] = sub_names[0]
+            info["subject_info"]["last_name"] = sub_names[1]
+        else:
+            info["subject_info"]["first_name"] = sub_names[0]
+            info["subject_info"]["middle_name"] = sub_names[1]
+            info["subject_info"]["last_name"] = sub_names[2]
+    # Birthday in (year, month, day) format.
+    if isinstance(edf_info["subject_info"].get("birthday"), datetime):
+        info["subject_info"]["birthday"] = (
+            edf_info["subject_info"]["birthday"].year,
+            edf_info["subject_info"]["birthday"].month,
+            edf_info["subject_info"]["birthday"].day,
+        )
+    # Handedness (1=right, 2=left, 3=ambidextrous).
+    if edf_info["subject_info"].get("hand") is not None:
+        info["subject_info"]["hand"] = int(edf_info["subject_info"]["hand"])
+    # Height in meters.
+    if edf_info["subject_info"].get("height") is not None:
+        info["subject_info"]["height"] = float(edf_info["subject_info"]["height"])
+    # Weight in kilograms.
+    if edf_info["subject_info"].get("weight") is not None:
+        info["subject_info"]["weight"] = float(edf_info["subject_info"]["weight"])
+
     # Filter settings
     highpass = edf_info["highpass"]
     lowpass = edf_info["lowpass"]
@@ -764,7 +807,7 @@ def _read_edf_header(fname, exclude, infer_types, include=None):
         id_info = id_info.split(" ")
         if len(id_info):
             patient["id"] = id_info[0]
-            if len(id_info) == 4:
+            if len(id_info) >= 4:
                 try:
                     birthdate = datetime.strptime(id_info[2], "%d-%b-%Y")
                 except ValueError:
@@ -772,6 +815,16 @@ def _read_edf_header(fname, exclude, infer_types, include=None):
                 patient["sex"] = id_info[1]
                 patient["birthday"] = birthdate
                 patient["name"] = id_info[3]
+                if len(id_info) > 4:
+                    for info in id_info[4:]:
+                        if "=" in info:
+                            key, value = info.split("=")
+                            if key in ["weight", "height"]:
+                                patient[key] = float(value)
+                            elif key in ["hand"]:
+                                patient[key] = int(value)
+                            else:
+                                warn(f"Invalid patient information {key}")
 
         # Recording ID
         meas_id = {}

mne/export/_edf.py
@@ -189,20 +189,26 @@ def _export_raw(fname, raw, physical_range, add_ch_type):
         # set patient info
         subj_info = raw.info.get("subject_info")
         if subj_info is not None:
-            birthday = subj_info.get("birthday")
-
             # get the full name of subject if available
-            first_name = subj_info.get("first_name")
-            last_name = subj_info.get("last_name")
-            first_name = first_name or ""
-            last_name = last_name or ""
-            joiner = ""
-            if len(first_name) and len(last_name):
-                joiner = " "
-            name = joiner.join([first_name, last_name])
+            first_name = subj_info.get("first_name", "")
+            middle_name = subj_info.get("middle_name", "")
+            last_name = subj_info.get("last_name", "")
+            name = " ".join(filter(None, [first_name, middle_name, last_name]))
 
+            birthday = subj_info.get("birthday")
             hand = subj_info.get("hand")
+            weight = subj_info.get("weight")
+            height = subj_info.get("height")
             sex = subj_info.get("sex")
+
+            additional_patient_info = []
+            for key, value in [("height", height), ("weight", weight), ("hand", hand)]:
+                if value:
+                    additional_patient_info.append(f"{key}={value}")
+            if len(additional_patient_info) == 0:
+                additional_patient_info = None
+            else:
+                additional_patient_info = " ".join(additional_patient_info)
 
             if birthday is not None:
                 if hdl.setPatientBirthDate(birthday[0], birthday[1], birthday[2]) != 0:
@@ -211,9 +217,10 @@ def _export_raw(fname, raw, physical_range, add_ch_type):
                         f"returned an error"
                     )
             for key, val in [
+                ("PatientCode", subj_info.get("his_id", "")),
                 ("PatientName", name),
                 ("PatientGender", sex),
-                ("AdditionalPatientInfo", f"hand={hand}"),
+                ("AdditionalPatientInfo", additional_patient_info),
             ]:
                 # EDFwriter compares integer encodings of sex and will
                 # raise a TypeError if value is None as returned by

mne/export/tests/test_export.py
@@ -140,11 +140,19 @@ def test_double_export_edf(tmp_path):
         "bio",
     ]
     info = create_info(len(ch_types), sfreq=1000, ch_types=ch_types)
+    info = info.set_meas_date("2023-09-04 14:53:09.000")
     data = rng.random(size=(len(ch_types), 1000)) * 1e-5
 
     # include subject info and measurement date
     info["subject_info"] = dict(
-        first_name="mne", last_name="python", birthday=(1992, 1, 20), sex=1, hand=3
+        his_id="12345",
+        first_name="mne",
+        last_name="python",
+        birthday=(1992, 1, 20),
+        sex=1,
+        weight=78.3,
+        height=1.75,
+        hand=3,
     )
     raw = RawArray(data, info)
 
@@ -168,6 +176,10 @@ def test_double_export_edf(tmp_path):
         raw.get_data(), raw_read.get_data()[:, :orig_raw_len], decimal=4
     )
     assert_allclose(raw.times, raw_read.times[:orig_raw_len], rtol=0, atol=1e-5)
+
+    # check info
+    for key in set(raw.info) - {"chs"}:
+        assert raw.info[key] == raw_read.info[key]
 
     # check channel types except for 'bio', which loses its type
     orig_ch_types = raw.get_channel_types()

mne/io/edf/tests/test_edf.py
@@ -7,6 +7,7 @@
 #
 # License: BSD-3-Clause
 
+import datetime
 from contextlib import nullcontext
 from functools import partial
 from pathlib import Path
@@ -117,19 +118,53 @@ def test_edf_temperature(monkeypatch):
     assert raw.get_channel_types()[0] == "temperature"
 
 
+@testing.requires_testing_data
 def test_subject_info(tmp_path):
     """Test exposure of original channel units."""
-    raw = read_raw_edf(edf_path)
-    assert raw.info["subject_info"] is None  # XXX this is arguably a bug
+    raw = read_raw_edf(edf_stim_resamp_path, preload=True)
+
+    # check subject_info from `info`
+    assert raw.info["subject_info"] is not None
+    want = {
+        "his_id": "X",
+        "sex": 1,
+        "birthday": (1967, 10, 9),
+        "last_name": "X",
+    }
+    for key, val in want.items():
+        assert raw.info["subject_info"][key] == val, key
+
+    # check "subject_info" from `_raw_extras`
     edf_info = raw._raw_extras[0]
     assert edf_info["subject_info"] is not None
-    want = {"id": "X", "sex": "X", "birthday": "X", "name": "X"}
+    want = {
+        "id": "X",
+        "sex": "M",
+        "birthday": datetime.datetime(1967, 10, 9, 0, 0),
+        "name": "X",
+    }
     for key, val in want.items():
         assert edf_info["subject_info"][key] == val, key
+
+    # add information
+    raw.info["subject_info"]["hand"] = 0
+
+    # save raw to FIF and load it back
     fname = tmp_path / "test_raw.fif"
     raw.save(fname)
     raw = read_raw_fif(fname)
-    assert raw.info["subject_info"] is None  # XXX should eventually round-trip
+
+    # check subject_info from `info`
+    assert raw.info["subject_info"] is not None
+    want = {
+        "his_id": "X",
+        "sex": 1,
+        "birthday": (1967, 10, 9),
+        "last_name": "X",
+        "hand": 0,
+    }
+    for key, val in want.items():
+        assert raw.info["subject_info"][key] == val
 
 
 def test_bdf_data():

mne/io/edf/tests/test_gdf.py
@@ -100,8 +100,14 @@ def test_gdf2_birthday(tmp_path):
         assert np.fromfile(fid, np.uint64, 1)[0] == d
     raw = read_raw_gdf(new_fname, eog=None, misc=None, preload=True)
     assert raw._raw_extras[0]["subject_info"]["age"] == 44
-    # XXX this is a bug, it should be populated...
-    assert raw.info["subject_info"] is None
+    assert raw.info["subject_info"] is not None
+
+    birthdate = datetime(1, 1, 1, tzinfo=timezone.utc) + offset_44_yr
+    assert raw.info["subject_info"]["birthday"] == (
+        birthdate.year,
+        birthdate.month,
+        birthdate.day,
+    )
 
 
 @testing.requires_testing_data

commit 15c35047eddee41aebe3f89035125e0da818350e

FIX: gracefully handle bad patient info in edf reader
Bug introduced in #11952

Code assumed that hand info would be something like "hand='0'" or "hand='1'" but in my case it was "hand='None'" which caused an error because you cannot do int('None').

This just wraps things in a try-except block

mne/io/edf/edf.py
@@ -824,10 +824,19 @@ def _read_edf_header(
                     for info in id_info[4:]:
                         if "=" in info:
                             key, value = info.split("=")
+                            err = f"patient {key} info cannot be {value}, skipping."
                             if key in ["weight", "height"]:
-                                patient[key] = float(value)
+                                try:
+                                    patient[key] = float(value)
+                                except ValueError:
+                                    logger.debug(err)
+                                    continue
                             elif key in ["hand"]:
-                                patient[key] = int(value)
+                                try:
+                                    patient[key] = int(value)
+                                except ValueError:
+                                    logger.debug(err)
+                                    continue
                             else:
                                 warn(f"Invalid patient information {key}")
 