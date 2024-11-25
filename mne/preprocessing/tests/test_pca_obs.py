4bd3a519e36412898b9beea725c70b025d9c2138
Steinn Magnusson <s.magnusson@senec.com>
2024-11-25 09:19:00 +0100

    style: run pre-commit hooks on test file

--- mne/preprocessing/tests/test_pca_obs.py
+++ mne/preprocessing/tests/test_pca_obs.py
@@ -5,14 +5,13 @@
 # Copyright the MNE-Python contributors.
 
 import copy
-from pathlib import Path    
+from pathlib import Path
 
 import numpy as np
+import pytest
 from scipy.signal import firls
 
-import pytest
 from mne.io import read_raw_fif
-
 from mne.preprocessing import apply_pca_obs
 from mne.preprocessing.ecg import find_ecg_events
 
@@ -27,7 +26,7 @@ def short_raw_data():
 
 
 @pytest.mark.parametrize(
-    # TODO: Are there any parameters we can cycle through to 
+    # TODO: Are there any parameters we can cycle through to
     # test multiple? Different fs, windows, highpass freqs, etc.?
     # TODO: how do we determine qrs and filter_coords? What are these?
     ("fs", "highpass_freq", "qrs", "filter_coords"),
@@ -38,17 +37,17 @@ def short_raw_data():
 )
 def test_heart_artifact_removal(short_raw, fs, highpass_freq, qrs, filter_coords):
     """Test PCA-OBS analysis and heart artifact removal of ECG datasets."""
-
-    # get the sampling frequency of the test data and generate the filter coords as in our example
+    # get the sampling frequency of the test data and
+    # generate the filter coords as in our example
     fs = short_raw.info["sfreq"]
     a = [0, 0, 1, 1]
     f = [0, 0.4 / (fs / 2), 0.9 / (fs / 2), 1]  # 0.9 Hz highpass filter
-    ord = round(3 * fs / 0.5)
-    filter_coords = firls(ord + 1, f, a)
+    ord_ = round(3 * fs / 0.5)
+    filter_coords = firls(ord_ + 1, f, a)
 
     # extract the QRS
     ecg_events, _, _ = find_ecg_events(short_raw, ch_name=None)
-    ecg_event_samples = np.asarray([[ecg_event[0] for ecg_event in ecg_events]])  
+    ecg_event_samples = np.asarray([[ecg_event[0] for ecg_event in ecg_events]])
 
     # copy the original raw and remove the heart artifact in-place
     raw_orig = copy.deepcopy(short_raw)
@@ -59,11 +58,10 @@ def test_heart_artifact_removal(short_raw, fs, highpass_freq, qrs, filter_coords
         filter_coords=filter_coords,
     )
     # raw.get_data() ? to get shapes to compare
-    
+
     assert raw_orig != short_raw
 
     # # Do something with fs and highpass as processing of the data?
-    # ...
 
     # # call pca_obs algorithm
     # result = pca_obs(raw, qrs=qrs, filter_coords=filter_coords)
@@ -71,8 +69,9 @@ def test_heart_artifact_removal(short_raw, fs, highpass_freq, qrs, filter_coords
     # # assert results
     # assert result is not None
     # assert result.shape == (100, 100)
-    # assert result.shape == raw.shape  # is this a condition we can test? 
+    # assert result.shape == raw.shape  # is this a condition we can test?
     # assert result[0, 0] == 1.0
+
 
 if __name__ == "__main__":
     pytest.main(["mne/preprocessing/tests/test_pca_obs.py"])
