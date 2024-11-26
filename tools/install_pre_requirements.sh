da88dffbd4e68f2afe8b426e0160995bf3ed7f8c
Eric Larson <larson.eric.d@gmail.com>
2024-11-26 13:12:34 -0500

    FIX: Needed

--- tools/install_pre_requirements.sh
+++ tools/install_pre_requirements.sh
@@ -14,7 +14,8 @@ QT_BINDING="PyQt6"
 echo "PyQt6 and scientific-python-nightly-wheels dependencies"
 python -m pip install $STD_ARGS pip setuptools packaging \
 	threadpoolctl cycler fonttools kiwisolver pyparsing pillow python-dateutil \
-	patsy pytz tzdata nibabel tqdm trx-python joblib numexpr "$QT_BINDING"
+	patsy pytz tzdata nibabel tqdm trx-python joblib numexpr "$QT_BINDING" \
+	py-cpuinfo
 echo "NumPy/SciPy/pandas etc."
 python -m pip uninstall -yq numpy
 python -m pip install $STD_ARGS --only-binary ":all:" --default-timeout=60 \
