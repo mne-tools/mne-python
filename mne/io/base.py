9510d45940a4eed21adefd6df64d56d2c699b157
Clemens Brunner <clemens.brunner@gmail.com>
2024-12-10 11:26:27 +0100

    Add raw.rescale

--- mne/io/base.py
+++ mne/io/base.py
@@ -1501,6 +1501,56 @@ class BaseRaw(
             return self, events
 
     @verbose
+    def rescale(self, scale, *, verbose=None):
+        """Rescale channels.
+
+        Parameters
+        ----------
+        scale : int | float | dict
+            The scaling factor to apply. If a float, the same scaling factor is applied
+            to all channels (this works only if all channels are of the same type). If a
+            dict, the keys must be valid channel types and the values the scaling
+            factors to apply to the corresponding channels.
+        %(verbose)s
+
+        Returns
+        -------
+        raw : Raw
+            The raw object with rescaled data.
+
+        Examples
+        --------
+        To rescale all channels from µV to V, you can do::
+
+            >>> raw.rescale(1e-6)  # doctest: +SKIP
+
+        To rescale only EEG channels from µV to V, you can do::
+
+            >>> raw.rescale({"eeg": 1e-6})  # doctest: +SKIP
+        """
+        _validate_type(scale, (int, float, dict), "scale")
+        _check_preload(self, "raw.rescale")
+
+        if isinstance(scale, int | float):
+            if len(self.get_channel_types(unique=True)) == 1:
+                self.apply_function(lambda x: x * scale, channel_wise=False)
+            else:
+                raise ValueError(
+                    "If scale is a scalar, all channels must be of the same type. "
+                    "Consider passing a dict instead."
+                )
+        else:
+            for ch_type, ch_scale in scale.items():
+                if ch_type not in self.get_channel_types():
+                    raise ValueError(f"Invalid channel type: {ch_type}")
+                else:
+                    self.apply_function(
+                        lambda x: x * ch_scale, picks=ch_type, channel_wise=False
+                    )
+
+        return self
+
+    @verbose
     def crop(self, tmin=0.0, tmax=None, include_tmax=True, *, verbose=None):
         """Crop raw data file.
 
