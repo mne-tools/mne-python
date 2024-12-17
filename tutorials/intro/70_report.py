dcd26258c5fd83fd3974d73ba2b1e2773c33bc3d
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2024-12-17 20:16:31 +0100

    Add support for different encoding in ANT neuro CNT format reader (#13035)
    Co-authored-by: Eric Larson <larson.eric.d@gmail.com>

--- tutorials/intro/70_report.py
+++ tutorials/intro/70_report.py
@@ -7,11 +7,10 @@ Getting started with mne.Report
 
 :class:`mne.Report` is a way to create interactive HTML summaries of your data.
 These reports can show many different visualizations for one or multiple participants.
-A common use case is creating diagnostic summaries to check data
-quality at different stages in the processing pipeline. The report can show
-things like plots of data before and after each preprocessing step, epoch
-rejection statistics, MRI slices with overlaid BEM shells, all the way up to
-plots of estimated cortical activity.
+A common use case is creating diagnostic summaries to check data quality at different
+stages in the processing pipeline. The report can show things like plots of data before
+and after each preprocessing step, epoch rejection statistics, MRI slices with overlaid
+BEM shells, all the way up to plots of estimated cortical activity.
 
 Compared to a Jupyter notebook, :class:`mne.Report` is easier to deploy, as the
 HTML pages it generates are self-contained and do not require a running Python
