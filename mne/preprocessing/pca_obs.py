d19049b88b35b4c774a0aa429c5ce456885cad2c
Steinn Magnusson <s.magnusson@senec.com>
2024-12-18 14:40:36 +0100

    style: move fit_ecg_template function to bottom of file to improve readability

--- mne/preprocessing/pca_obs.py
+++ mne/preprocessing/pca_obs.py
@@ -16,101 +16,6 @@ from mne.io.fiff.raw import Raw
 from mne.utils import logger
 
 
-def fit_ecg_template(
-    data: np.ndarray,
-    pca_template: np.ndarray,
-    a_peak_idx: int,
-    peak_range: int,
-    pre_range: int,
-    post_range: int,
-    mid_p: float,
-    fitted_art: np.ndarray,
-    post_idx_previous_peak: int | None,
-    n_samples_fit: int,
-) -> tuple[np.ndarray, int]:
-    """
-    Fits the heartbeat artefact found in the data.
-
-    Returns the fitted artefact and the index of the next peak.
-
-    Parameters
-    ----------
-        data (ndarray): Data from the raw signal (n_channels, n_times)
-        pca_template (ndarray): Mean heartbeat and first N (default 4)
-            principal components of the heartbeat matrix
-        a_peak_idx (int): Sample index of current R-peak
-        peak_range (int): Half the median RR-interval
-        pre_range (int): Number of samples to fit before the R-peak
-        post_range (int): Number of samples to fit after the R-peak
-        mid_p (float): Sample index marking middle of the median RR interval
-            in the signal. Used to extract relevant part of PCA_template.
-        fitted_art (ndarray): The computed heartbeat artefact computed to
-            remove from the data
-        post_idx_previous_peak (optional int): Sample index of previous R-peak
-        n_samples_fit (int): Sample fit for interpolation in fitted artifact
-            windows. Helps reduce sharp edges at end of fitted heartbeat events
-
-    Returns
-    -------
-        tuple[np.ndarray, int]: the fitted artifact and the next peak index
-    """
-    # post_idx_nextpeak is passed in in PCA_OBS, used here as post_idx_previous_peak
-    # Then nextpeak is returned at the end and the process repeats
-    # select window of template
-    template = pca_template[mid_p - peak_range - 1 : mid_p + peak_range + 1, :]
-
-    # select window of data and detrend it
-    slice_ = data[a_peak_idx - peak_range : a_peak_idx + peak_range + 1]
-
-    detrended_data = detrend(slice_, type="constant")
-
-    # maps data on template and then maps it again back to the sensor space
-    least_square = np.linalg.lstsq(template, detrended_data, rcond=None)
-    pad_fit = np.dot(template, least_square[0])
-
-    # fit artifact
-    fitted_art[a_peak_idx - pre_range - 1 : a_peak_idx + post_range] = pad_fit[
-        mid_p - pre_range - 1 : mid_p + post_range
-    ].T
-
-    # if last peak, return
-    if post_idx_previous_peak is None:
-        return fitted_art, a_peak_idx + post_range
-
-    # interpolate time between peaks
-    intpol_window = np.ceil([post_idx_previous_peak, a_peak_idx - pre_range]).astype(
-        int
-    )  # interpolation window
-
-    if intpol_window[0] < intpol_window[1]:
-        # Piecewise Cubic Hermite Interpolating Polynomial(PCHIP) + replace EEG data
-
-        # You have x_fit which is two slices on either side of the interpolation window
-        #   endpoints
-        # You have y_fit which is the y vals corresponding to x values above
-        # You have x_interpol which is the time points between the two slices in x_fit
-        #   that you want to interpolate
-        # You have y_interpol which is values from pchip at the time points specified in
-        #   x_interpol
-        # points to be interpolated in pt - the gap between the endpoints of the window
-        x_interpol = np.arange(intpol_window[0], intpol_window[1] + 1, 1)
-        # Entire range of x values in this step (taking some
-        # number of samples before and after the window)
-        x_fit = np.concatenate(
-            [
-                np.arange(intpol_window[0] - n_samples_fit, intpol_window[0] + 1, 1),
-                np.arange(intpol_window[1], intpol_window[1] + n_samples_fit + 1, 1),
-            ]
-        )
-        y_fit = fitted_art[x_fit]
-        y_interpol = pchip(x_fit, y_fit)(x_interpol)  # perform interpolation
-
-        # make fitted artefact in the desired range equal to the completed fit above
-        fitted_art[post_idx_previous_peak : a_peak_idx - pre_range + 1] = y_interpol
-
-    return fitted_art, a_peak_idx + post_range
-
-
 def apply_pca_obs(
     raw: Raw,
     picks: list[str],
@@ -313,3 +218,98 @@ def _pca_obs(
     # Actually subtract the artefact, return needs to be the same shape as input data
     data -= fitted_art
     return data
+
+
+def fit_ecg_template(
+    data: np.ndarray,
+    pca_template: np.ndarray,
+    a_peak_idx: int,
+    peak_range: int,
+    pre_range: int,
+    post_range: int,
+    mid_p: float,
+    fitted_art: np.ndarray,
+    post_idx_previous_peak: int | None,
+    n_samples_fit: int,
+) -> tuple[np.ndarray, int]:
+    """
+    Fits the heartbeat artefact found in the data.
+
+    Returns the fitted artefact and the index of the next peak.
+
+    Parameters
+    ----------
+        data (ndarray): Data from the raw signal (n_channels, n_times)
+        pca_template (ndarray): Mean heartbeat and first N (default 4)
+            principal components of the heartbeat matrix
+        a_peak_idx (int): Sample index of current R-peak
+        peak_range (int): Half the median RR-interval
+        pre_range (int): Number of samples to fit before the R-peak
+        post_range (int): Number of samples to fit after the R-peak
+        mid_p (float): Sample index marking middle of the median RR interval
+            in the signal. Used to extract relevant part of PCA_template.
+        fitted_art (ndarray): The computed heartbeat artefact computed to
+            remove from the data
+        post_idx_previous_peak (optional int): Sample index of previous R-peak
+        n_samples_fit (int): Sample fit for interpolation in fitted artifact
+            windows. Helps reduce sharp edges at end of fitted heartbeat events
+
+    Returns
+    -------
+        tuple[np.ndarray, int]: the fitted artifact and the next peak index
+    """
+    # post_idx_nextpeak is passed in in PCA_OBS, used here as post_idx_previous_peak
+    # Then nextpeak is returned at the end and the process repeats
+    # select window of template
+    template = pca_template[mid_p - peak_range - 1 : mid_p + peak_range + 1, :]
+
+    # select window of data and detrend it
+    slice_ = data[a_peak_idx - peak_range : a_peak_idx + peak_range + 1]
+
+    detrended_data = detrend(slice_, type="constant")
+
+    # maps data on template and then maps it again back to the sensor space
+    least_square = np.linalg.lstsq(template, detrended_data, rcond=None)
+    pad_fit = np.dot(template, least_square[0])
+
+    # fit artifact
+    fitted_art[a_peak_idx - pre_range - 1 : a_peak_idx + post_range] = pad_fit[
+        mid_p - pre_range - 1 : mid_p + post_range
+    ].T
+
+    # if last peak, return
+    if post_idx_previous_peak is None:
+        return fitted_art, a_peak_idx + post_range
+
+    # interpolate time between peaks
+    intpol_window = np.ceil([post_idx_previous_peak, a_peak_idx - pre_range]).astype(
+        int
+    )  # interpolation window
+
+    if intpol_window[0] < intpol_window[1]:
+        # Piecewise Cubic Hermite Interpolating Polynomial(PCHIP) + replace EEG data
+
+        # You have x_fit which is two slices on either side of the interpolation window
+        #   endpoints
+        # You have y_fit which is the y vals corresponding to x values above
+        # You have x_interpol which is the time points between the two slices in x_fit
+        #   that you want to interpolate
+        # You have y_interpol which is values from pchip at the time points specified in
+        #   x_interpol
+        # points to be interpolated in pt - the gap between the endpoints of the window
+        x_interpol = np.arange(intpol_window[0], intpol_window[1] + 1, 1)
+        # Entire range of x values in this step (taking some
+        # number of samples before and after the window)
+        x_fit = np.concatenate(
+            [
+                np.arange(intpol_window[0] - n_samples_fit, intpol_window[0] + 1, 1),
+                np.arange(intpol_window[1], intpol_window[1] + n_samples_fit + 1, 1),
+            ]
+        )
+        y_fit = fitted_art[x_fit]
+        y_interpol = pchip(x_fit, y_fit)(x_interpol)  # perform interpolation
+
+        # make fitted artefact in the desired range equal to the completed fit above
+        fitted_art[post_idx_previous_peak : a_peak_idx - pre_range + 1] = y_interpol
+
+    return fitted_art, a_peak_idx + post_range
