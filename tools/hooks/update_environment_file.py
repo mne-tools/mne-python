9c2ab9195170dc7da6268311a7f848ed4b31eea6
Eric Larson <larson.eric.d@gmail.com>
2025-01-07 15:07:55 -0500

    FIX: Better?

--- tools/hooks/update_environment_file.py
+++ tools/hooks/update_environment_file.py
@@ -60,7 +60,7 @@ for dep in deps:
     # PySide6==6.7.0 only exists on PyPI, not conda-forge, so excluding it in
     # `environment.yaml` breaks the solver
     if package_name == "PySide6":
-        version_spec = version_spec.replace("!=6.7.0,", "!=6.8.1,")
+        version_spec = "<6.8.0"
     elif package_name == "scipy":
         version_spec = f"{version_spec},<1.15.0"
     # rstrip output line in case `version_spec` == ""
