402af8f152121463589bf9de530e33257d3d3815
Qian Chu <97355086+qian-chu@users.noreply.github.com>
2025-01-08 19:55:32 +0100

    Add doc for time reference requirement

--- mne/utils/docs.py
+++ mne/utils/docs.py
@@ -1509,6 +1509,15 @@ Supported formats:
     - EDF (``.edf``, uses `edfio <https://github.com/the-siesta-group/edfio>`_)
 """  # noqa: E501
 
+docdict["export_warning_annotations_raw"] = """\
+.. warning::
+    When exporting ``Raw`` with annotations, ``raw.info["meas_date"]`` must be the same as
+    ``raw.annotations.orig_time``. This guarantees that the annotations are in the same reference
+    frame as the samples.
+    When `Raw.first_time` is not zero (e.g., after cropping), the onsets are automatically corrected
+    so that onsets are always relative to the first sample.
+"""
+
 docdict["export_warning"] = """\
 .. warning::
     Since we are exporting to external formats, there's no guarantee that all
