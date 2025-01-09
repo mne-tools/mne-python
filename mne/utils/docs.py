fd7fcbf4d8ad18091f13cfde02b6a60b75bc7287
qian-chu <qianchu99@gmail.com>
2025-01-09 10:27:09 +0100

    reorder to respect alphabetical order

--- mne/utils/docs.py
+++ mne/utils/docs.py
@@ -1509,15 +1509,6 @@ Supported formats:
     - EDF (``.edf``, uses `edfio <https://github.com/the-siesta-group/edfio>`_)
 """  # noqa: E501
 
-docdict["export_warning_annotations_raw"] = """\
-.. warning::
-    When exporting ``Raw`` with annotations, ``raw.info["meas_date"]`` must be the same
-    as ``raw.annotations.orig_time``. This guarantees that the annotations are in the
-    same reference frame as the samples.
-    When `Raw.first_time` is not zero (e.g., after cropping), the onsets are
-    automatically corrected so that onsets are always relative to the first sample.
-"""
-
 docdict["export_warning"] = """\
 .. warning::
     Since we are exporting to external formats, there's no guarantee that all
@@ -1531,6 +1522,15 @@ use :meth:`mne.{0}.save` instead.
 Export does not apply projector(s). Unapplied projector(s) will be lost.
 Consider applying projector(s) before exporting with
 :meth:`mne.{0}.apply_proj`."""
+
+docdict["export_warning_annotations_raw"] = """\
+.. warning::
+    When exporting ``Raw`` with annotations, ``raw.info["meas_date"]`` must be the same
+    as ``raw.annotations.orig_time``. This guarantees that the annotations are in the
+    same reference frame as the samples.
+    When `Raw.first_time` is not zero (e.g., after cropping), the onsets are
+    automatically corrected so that onsets are always relative to the first sample.
+"""
 
 docdict["export_warning_note_epochs"] = _export_warning_note_base.format("Epochs")
 
