d615914270aa2089a478274d07a71e70116cf58a
Qian Chu <97355086+qian-chu@users.noreply.github.com>
2025-01-09 11:34:45 +0100

    move warning to `export_raw` docstr

--- mne/utils/docs.py
+++ mne/utils/docs.py
@@ -1523,15 +1523,6 @@ Export does not apply projector(s). Unapplied projector(s) will be lost.
 Consider applying projector(s) before exporting with
 :meth:`mne.{0}.apply_proj`."""
 
-docdict["export_warning_annotations_raw"] = """\
-.. warning::
-    When exporting ``Raw`` with annotations, ``raw.info["meas_date"]`` must be the same
-    as ``raw.annotations.orig_time``. This guarantees that the annotations are in the
-    same reference frame as the samples.
-    When `Raw.first_time` is not zero (e.g., after cropping), the onsets are
-    automatically corrected so that onsets are always relative to the first sample.
-"""
-
 docdict["export_warning_note_epochs"] = _export_warning_note_base.format("Epochs")
 
 docdict["export_warning_note_evoked"] = _export_warning_note_base.format("Evoked")
