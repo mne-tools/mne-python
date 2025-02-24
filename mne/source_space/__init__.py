325213a0b43538cbf31df9e547a0684254d3db19
Clemens Brunner <clemens.brunner@gmail.com>
2025-02-24 15:22:05 +0100

    Add missing imports

--- mne/source_space/__init__.py
+++ mne/source_space/__init__.py
@@ -3,3 +3,18 @@
 # Copyright the MNE-Python contributors.
 
 """Forward modeling code."""
+
+from ._source_space import (
+    SourceSpaces,
+    add_source_space_distances,
+    compute_distance_to_sensors,
+    find_source_space_hemi,
+    get_decimated_surfaces,
+    get_volume_labels_from_src,
+    label_src_vertno_sel,
+    morph_source_spaces,
+    read_source_spaces,
+    setup_source_space,
+    setup_volume_source_space,
+    write_source_spaces,
+)
