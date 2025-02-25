94c60bfbb1c06d475a9d22bbd59dce19546736c2
Eric Larson <larson.eric.d@gmail.com>
2025-02-25 11:00:05 -0500

    BUG: Fix bug with coord frame reading

--- mne/channels/tests/test_montage.py
+++ mne/channels/tests/test_montage.py
@@ -151,22 +151,30 @@ def test_dig_montage_trans(tmp_path):
     assert str(position1) == str(position2)  # exactly equal
 
 
-def test_fiducials():
+@pytest.mark.parametrize("fname", (fif_fname, ctf_fif_fname))
+def test_fiducials(tmp_path, fname):
     """Test handling of fiducials."""
     # Eventually the code used here should be unified with montage.py, but for
     # now it uses code in odd places
-    for fname in (fif_fname, ctf_fif_fname):
-        fids, coord_frame = read_fiducials(fname)
-        points = _fiducial_coords(fids, coord_frame)
-        assert points.shape == (3, 3)
-        # Fids
-        assert_allclose(points[:, 2], 0.0, atol=1e-6)
-        assert_allclose(points[::2, 1], 0.0, atol=1e-6)
-        assert points[2, 0] > 0  # RPA
-        assert points[0, 0] < 0  # LPA
-        # Nasion
-        assert_allclose(points[1, 0], 0.0, atol=1e-6)
-        assert points[1, 1] > 0
+    fids, coord_frame = read_fiducials(fname)
+    assert coord_frame == FIFF.FIFFV_COORD_HEAD
+    points = _fiducial_coords(fids, coord_frame)
+    assert points.shape == (3, 3)
+    # Fids
+    assert_allclose(points[:, 2], 0.0, atol=1e-6)
+    assert_allclose(points[::2, 1], 0.0, atol=1e-6)
+    assert points[2, 0] > 0  # RPA
+    assert points[0, 0] < 0  # LPA
+    # Nasion
+    assert_allclose(points[1, 0], 0.0, atol=1e-6)
+    assert points[1, 1] > 0
+    fname_out = tmp_path / "test-dig.fif"
+    mne.channels.make_dig_montage(
+        lpa=points[0], nasion=points[1], rpa=points[2], coord_frame="mri_voxel"
+    ).save(fname_out, overwrite=True)
+    fids_2, coord_frame_2 = read_fiducials(fname_out)
+    assert coord_frame != coord_frame_2
+    assert coord_frame_2 is not None
 
 
 def test_documented():
