5e2f83cd5a7969c461d6dfaed64b7106d1960645
Clemens Brunner <clemens.brunner@gmail.com>
2025-02-26 16:29:15 +0100

    Revert

--- mne/datasets/__init__.py
+++ mne/datasets/__init__.py
@@ -7,6 +7,32 @@ See :ref:`datasets` for more information.
 # License: BSD-3-Clause
 # Copyright the MNE-Python contributors.
 
+from . import fieldtrip_cmc
+from . import brainstorm
+from . import visual_92_categories
+from . import kiloword
+from . import eegbci
+from . import hf_sef
+from . import misc
+from . import mtrf
+from . import sample
+from . import somato
+from . import multimodal
+from . import fnirs_motor
+from . import opm
+from . import spm_face
+from . import testing
+from . import _fake
+from . import phantom_4dbti
+from . import sleep_physionet
+from . import limo
+from . import refmeg_noise
+from . import ssvep
+from . import erp_core
+from . import epilepsy_ecog
+from . import eyelink
+from . import ucl_opm_auditory
+
 from ._fetch import fetch_dataset
 from .utils import (
     _download_all_example_data,
