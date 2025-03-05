4648a4ff93ef5e176d52accbbf476afb946b4359
Scott Huberty <seh33@uw.edu>
2025-03-05 15:44:50 -0800

    FIX: hint

--- mne/io/egi/tests/test_egi.py
+++ mne/io/egi/tests/test_egi.py
@@ -5,6 +5,7 @@
 
 import os
 import shutil
+import tempfile
 from copy import deepcopy
 from datetime import datetime, timezone
 from pathlib import Path
@@ -586,3 +587,16 @@ def test_set_standard_montage_mff(fname, standard_montage):
     # Check that the reference remained
     for pick in picks:
         assert_allclose(raw.info["chs"][pick]["loc"][3:6], ref_loc)
+
+
+def test_egi_mff_bad_xml():
+    """Test that corrupt XML files are gracefully handled."""
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_dir = Path(temp_dir)
+        mff_fname = shutil.copytree(egi_mff_fname, temp_dir / "test_egi_bad_xml.mff")
+        bad_xml = mff_fname / "bad.xml"
+        bad_xml.write_text("<bad>")
+        with pytest.warns(RuntimeWarning, match="Could not parse the XML"):
+            raw = read_raw_egi(mff_fname)
+        # little check that the bad XML doesn't affect the parsing of other xml files
+        assert "DIN1" in raw.annotations.description
