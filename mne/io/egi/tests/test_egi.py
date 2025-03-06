ab068fb8402c2189352ea16f4226c68a2cf864c7
Scott Huberty <seh33@uw.edu>
2025-03-06 08:03:59 -0800

    FIX: syntax, testing data, import or skip
    - use importorskip
    - require testing data
    - use context manager to open and write to file
    - use tmp_path fixture

--- mne/io/egi/tests/test_egi.py
+++ mne/io/egi/tests/test_egi.py
@@ -5,7 +5,6 @@
 
 import os
 import shutil
-import tempfile
 from copy import deepcopy
 from datetime import datetime, timezone
 from pathlib import Path
@@ -589,13 +588,15 @@ def test_set_standard_montage_mff(fname, standard_montage):
         assert_allclose(raw.info["chs"][pick]["loc"][3:6], ref_loc)
 
 
+@requires_testing_data
 def test_egi_mff_bad_xml(tmp_path):
     """Test that corrupt XML files are gracefully handled."""
-        temp_dir = Path(temp_dir)
-        mff_fname = shutil.copytree(egi_mff_fname, temp_dir / "test_egi_bad_xml.mff")
-        bad_xml = mff_fname / "bad.xml"
-        bad_xml.write_text("<bad>")
-        with pytest.warns(RuntimeWarning, match="Could not parse the XML"):
-            raw = read_raw_egi(mff_fname)
-        # little check that the bad XML doesn't affect the parsing of other xml files
-        assert "DIN1" in raw.annotations.description
+    pytest.importorskip("defusedxml")
+    mff_fname = shutil.copytree(egi_mff_fname, tmp_path / "test_egi_bad_xml.mff")
+    bad_xml = mff_fname / "bad.xml"
+    with bad_xml.open("w") as fid:
+        fid.write("<foo>")
+    with pytest.warns(RuntimeWarning, match="Could not parse the XML"):
+        raw = read_raw_egi(mff_fname)
+    # little check that the bad XML doesn't affect the parsing of other xml files
+    assert "DIN1" in raw.annotations.description
