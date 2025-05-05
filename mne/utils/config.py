48a19e24958f561a1b37f35935f545d99a7628bb
bruAristimunha <b.aristimunha@gmail.com>
2025-05-05 20:33:27 +0200

    [EHN] improve the parallel load and set

--- mne/utils/config.py
+++ mne/utils/config.py
@@ -7,6 +7,7 @@
 import atexit
 import json
 import multiprocessing
+import contextlib
 import os
 import os.path as op
 import platform
@@ -217,17 +218,45 @@ _known_config_wildcards = (
     "MNE_LSL",  # mne-lsl
 )
 
+@contextlib.contextmanager
+def _open_lock(path, *args, **kwargs):
+    """
+    Context manager that opens a file with an optional file lock.
+    If the `filelock` package is available, a lock is acquired on a lock file
+    based on the given path (by appending '.lock'). 
+    Otherwise, a null context is used. The file is then opened in the 
+    specified mode.
+    """
+    filelock = _soft_import("filelock", purpose="parallel integration", strict=False)
+
+    if filelock is not None:
+        lock_path = f"{path}.lock"
+        try:
+            lock_context = FileLock(lock_path, timeout=2)
+        except TimeoutError:
+            lock_context = contextlib.nullcontext()
+    else:
+        lock_context = contextlib.nullcontext()
+
+    with lock_context:
+        if "hdf5" in str(path):
+            with h5py.File(path, *args, **kwargs) as fid:
+                yield fid
+        else:
+            with open(path, *args, **kwargs) as fid:
+                yield fid
+
 
 def _load_config(config_path, raise_error=False):
     """Safely load a config file."""
-    with open(config_path) as fid:
+    with _open_lock(config_path) as fid:
         try:
             config = json.load(fid)
         except ValueError:
             # No JSON object could be decoded --> corrupt file?
             msg = (
                 f"The MNE-Python config file ({config_path}) is not a valid JSON "
-                "file and might be corrupted"
+                "file and might be corrupted. Consider deleting it and generating again."
             )
             if raise_error:
                 raise RuntimeError(msg)
@@ -398,7 +427,7 @@ def set_config(key, value, home_dir=None, set_env=True):
     directory = op.dirname(config_path)
     if not op.isdir(directory):
         os.mkdir(directory)
-    with open(config_path, "w") as fid:
+    with _load_config(config_path, "w") as fid:
         json.dump(config, fid, sort_keys=True, indent=0)
 
 
