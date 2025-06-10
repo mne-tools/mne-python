26da4e85a949a8c1bf756b4fd9163ee6cecc6ed3
LaurentLM <laurent.lementec@gmail.com>
2025-06-10 23:22:45 +0100

    Added cast to date for pd.Timestamp inputs

--- mne/_fiff/meas_info.py
+++ mne/_fiff/meas_info.py
@@ -1010,6 +1010,12 @@ def _check_types(x, *, info, name, types, cast=None):
         x = cast(x)
     return x
 
+def _check_bday(birthday_input, *, info):
+    date = _check_types(birthday_input, info=info, name='subject_info["birthday"]', types=(datetime.date, None))
+    # test if we have a pd.Timestamp
+    if hasattr(date, "date"):
+        date = date.date()
+    return date
 
 class SubjectInfo(ValidatedDict):
     _attributes = {
@@ -1022,9 +1028,7 @@ class SubjectInfo(ValidatedDict):
         "middle_name": partial(
             _check_types, name='subject_info["middle_name"]', types=str
         ),
-        "birthday": partial(
-            _check_types, name='subject_info["birthday"]', types=(datetime.date, None)
-        ),
+        "birthday": partial(_check_bday),
         "sex": partial(_check_types, name='subject_info["sex"]', types=int),
         "hand": partial(_check_types, name='subject_info["hand"]', types=int),
         "weight": partial(
