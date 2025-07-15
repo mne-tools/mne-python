e42e1370e7e058bb57a67ac1cc90cff4fca7e702
Eric Larson <larson.eric.d@gmail.com>
2025-07-15 14:37:50 -0400

    MAINT: Try again [skip azp] [skip circle]

--- tools/github_actions_dependencies.sh
+++ tools/github_actions_dependencies.sh
@@ -21,8 +21,8 @@ if [ ! -z "$CONDA_ENV" ]; then
 		STD_ARGS="--progress-bar off"
 	fi
 elif [[ "${MNE_CI_KIND}" == "pip" ]]; then
-	# This one is free-threaded so can't have numba as of 2025/07/15
-	INSTALL_DEPS="nibabel scikit-learn numpydoc PySide6!=6.9.1 mne-qt-browser pandas h5io mffpy defusedxml numba pyvistaqt edfio mffpy"
+	# This one is free-threaded so can't have numba, h5py (or h5io), cryptography (thus twine) as of 2025/07/15
+	INSTALL_DEPS="nibabel scikit-learn numpydoc PySide6!=6.9.1 mne-qt-browser pandas mffpy defusedxml numba pyvistaqt edfio mffpy"
 else
 	test "${MNE_CI_KIND}" == "pip-pre"
 	STD_ARGS="$STD_ARGS --pre"
