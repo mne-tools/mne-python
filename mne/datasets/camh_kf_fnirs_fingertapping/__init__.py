b92062a46a72f34771f63992fffba7e6b1be9d6b
John Griffiths <JohnGriffiths@users.noreply.github.com>
2025-08-03 20:24:38 -0400

    Create __init__.py

---
+++ mne/datasets/camh_kf_fnirs_fingertapping/__init__.py
@@ -0,0 +1,7 @@
+# Authors: The MNE-Python contributors.
+# License: BSD-3-Clause
+# Copyright the MNE-Python contributors.
+
+"""CAMH KF fNIRS Fingertapping Dataset."""
+
+from .camh_kf_fnirs_fingertapping import data_path, get_version
