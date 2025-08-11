20c76a8c3daa2891a1fba8ba0b919a5d39226918
Eric Larson <larson.eric.d@gmail.com>
2025-08-11 13:30:24 -0400

    MAINT: expanduser [ci skip]

--- mne/io/curry/curry.py
+++ mne/io/curry/curry.py
@@ -32,7 +32,7 @@ CURRY_SUFFIX_LABELS = [".cdt.dpa", ".cdt.dpo", ".rs3"]
 
 
 def _check_curry_filename(fname):
-    fname_in = Path(fname)
+    fname_in = Path(fname).expanduser()
     fname_out = None
     # try suffixes
     if fname_in.suffix in CURRY_SUFFIX_DATA:
@@ -49,7 +49,9 @@ def _check_curry_filename(fname):
                 break
     # final check
     if not fname_out or not fname_out.exists():
-        raise FileNotFoundError("no curry data file found (.dat or .cdt)")
+        raise FileNotFoundError(
+            f"no curry data file found (.dat or .cdt), checked {fname_out or fname_in}"
+        )
     return fname_out
 
 
