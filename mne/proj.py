edf827cd74fd680010202d21d4c8a06c6a459e24
NatalieNegussie97 <natalienegussie1997@gmail.com>
2025-08-16 00:01:36 +0300

    fixing sensitivity_map docstring

--- mne/proj.py
+++ mne/proj.py
@@ -382,37 +382,42 @@ def sensitivity_map(
 ):
     """
     Compute sensitivity map.
+
     Such maps are used to know how much sources are visible by a type
     of sensor, and how much projections shadow some sources.
 
     Parameters
     ----------
-    fwd: Forward
+    fwd : Forward
         The forward operator.
-    projs: list
+    projs : list
         List of projection vectors.
-    ch_type: {'grad', 'mag', 'eeg'}
+    ch_type : {'grad', 'mag', 'eeg'}
         The type of sensors to use.
-    mode: {'free', 'fixed', 'ratio', 'radiality', 'angle', 'remaining', 'dampening'}, default='free'
+    mode : {'free', 'fixed', 'ratio', 'radiality', 'angle',
+            'remaining', 'dampening'}, default='free'
         Which sensitivity quantity to compute:
-
-        - 'free': Maximum obtainable signal over all source orientations.
-        - 'fixed': Signal from sources constrained to be normal to the cortical surface.
-        - 'ratio': Ratio of 'fixed' sensitivity to the 'free' (maximum-over-orientations) sensitivity.
-        - 'radiality': Ratio of signal from sources normal to the cortex to the maximum signal among all orientations.
-        - 'angle': Subspace angle with the noise subspace.
-        - 'remaining': Fraction of signal remaining after the projection (e.g., after SSS/SSP).
-        - 'dampening': Fraction of signal dampening due to the projection.
-    exclude: list of str | str
+        - 'free' : Maximum obtainable signal over all source orientations.
+        - 'fixed' : Signal from sources constrained to be normal to the
+          cortical surface.
+        - 'ratio' : Ratio of 'fixed' sensitivity to the 'free'
+          (maximum-over-orientations) sensitivity.
+        - 'radiality' : Ratio of signal from sources normal to the cortex
+          to the maximum signal among all orientations.
+        - 'angle' : Subspace angle with the noise subspace.
+        - 'remaining' : Fraction of signal remaining after the projection
+          (e.g., after SSS/SSP).
+        - 'dampening' : Fraction of signal dampening due to the projection.
+    exclude : list of str | str
         List of channels to exclude. If empty do not exclude any (default).
-        If 'bads', exclude channels in 'fwd['info']['bads']'.
+        If 'bads', exclude channels in ``fwd['info']['bads']``.
     %(verbose)s
 
     Returns
     -------
-    stc: SourceEstimate | VolSourceEstimate
-        The sensitivity map as a SourceEstimate or VolSourceEstimate instance
-        for visualization.
+    stc : SourceEstimate | VolSourceEstimate
+        The sensitivity map as a SourceEstimate or VolSourceEstimate
+        instance for visualization.
 
     Notes
     -----
