1f40fe4b56b726983d7f5276d2f4145a4209dd28
Genuster <genusterok@gmail.com>
2025-08-23 21:09:22 +0300

    add importorskip

--- mne/decoding/tests/test_xdawn.py
+++ mne/decoding/tests/test_xdawn.py
@@ -3,6 +3,8 @@
 # Copyright the MNE-Python contributors.
 
 import pytest
+
+pytest.importorskip("sklearn")
 from sklearn.utils.estimator_checks import parametrize_with_checks
 
 from mne.decoding import XdawnTransformer
@@ -12,5 +14,4 @@ from mne.decoding import XdawnTransformer
 @parametrize_with_checks([XdawnTransformer(reg="oas")])  # oas handles few sample cases
 def test_sklearn_compliance(estimator, check):
     """Test compliance with sklearn."""
-    pytest.importorskip("sklearn", minversion="1.4")  # TODO VERSION remove on 1.4+
     check(estimator)
