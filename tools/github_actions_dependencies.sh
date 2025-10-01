c9f94ff2b2032a95b38107e2b1fffa85811253d7
Eric Larson <larson.eric.d@gmail.com>
2025-10-01 12:06:22 -0400

    FIX: Path

--- tools/github_actions_dependencies.sh
+++ tools/github_actions_dependencies.sh
@@ -18,7 +18,7 @@ if [ ! -z "$CONDA_ENV" ]; then
 		INSTALL_ARGS=""
 	fi
 	# TODO: Until a PyVista release supports VTK 9.5+
-	INSTALL_ARGS="$INSTALL_ARGS https://github.com/pyvista/pyvista/archive/main.zip"
+	INSTALL_ARGS="$INSTALL_ARGS https://github.com/pyvista/pyvista/archive/refs/heads/main.zip"
 	# If on minimal or old, just install testing deps
 	if [[ "${CONDA_ENV}" == "environment_"* ]]; then
 		INSTALL_KIND="test"
