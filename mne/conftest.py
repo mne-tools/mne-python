fa63275e0c638f69403f42699f73791d6c8fdd39
Eric Larson <larson.eric.d@gmail.com>
2025-10-07 10:27:49 -0400

    FIX: Maybe

--- mne/conftest.py
+++ mne/conftest.py
@@ -180,7 +180,7 @@ def pytest_configure(config: pytest.Config):
     # pyvista <-> NumPy 2.0
     ignore:__array_wrap__ must accept context and return_scalar arguments.*:DeprecationWarning
     # pyvista <-> VTK dev
-    ignore:Call to deprecated method GetInputAsDataSet.*:DeprecationWarning
+    ignore:Call to deprecated method Get.*:DeprecationWarning
     # nibabel <-> NumPy 2.0
     ignore:__array__ implementation doesn't accept a copy.*:DeprecationWarning
     # quantities via neo
