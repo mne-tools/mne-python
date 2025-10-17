9f980716b3c7eb37be0829e1d423b6c8226b8e42
bruAristimunha <b.aristimunha@gmail.com>
2025-10-17 17:23:08 +0200

    updating the read

--- mne/io/eeglab/_eeglab.py
+++ mne/io/eeglab/_eeglab.py
@@ -12,7 +12,7 @@ except ImportError:  # scipy < 1.8
     from scipy.io.matlab.mio5_params import MatlabOpaque
 from scipy.io import loadmat, whosmat
 
-from ...utils import _import_pymatreader_funcs
+from ...utils import _import_pymatreader_funcs, warn
 
 
 def _todict_from_np_struct(data):  # taken from pymatreader.utils
@@ -144,12 +144,12 @@ def _readmat(fname, uint16_codec=None, *, preload=False):
         if is_possible_not_loaded:
             return mat_data
         else:
-            # warn(
-            #     "The 'data' variable in the .set file appears to be numeric. "
-            #     "In preload=False mode, the data is not loaded into memory. "
-            #     "Instead, the filename is provided in mat_data['data']. "
-            #     "To load the actual data, set preload=True."
-            # )
+            warn(
+                "The 'data' variable in the .set file appears to be numeric. "
+                "In preload=False mode, the data is not loaded into memory. "
+                "Instead, the filename is provided in mat_data['data']. "
+                "To load the actual data, set preload=True."
+            )
             preload = True
 
     # here is intended to be if and not else if
