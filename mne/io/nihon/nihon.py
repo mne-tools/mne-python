147ff5995f91a0a2b1dcce15b16980c2a03143a6
myd7349 <myd7349@gmail.com>
2025-10-24 01:40:08 +0800

    ENH: Add encoding parameter to Nihon Kohden reader (#13458)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Eric Larson <larson.eric.d@gmail.com>
    Co-authored-by: Daniel McCloy <dan@mccloy.info>

--- mne/io/nihon/nihon.py
+++ mne/io/nihon/nihon.py
@@ -23,7 +23,9 @@ def _ensure_path(fname):
 
 
 @fill_doc
-def read_raw_nihon(fname, preload=False, verbose=None) -> "RawNihon":
+def read_raw_nihon(
+    fname, preload=False, *, encoding="utf-8", verbose=None
+) -> "RawNihon":
     """Reader for an Nihon Kohden EEG file.
 
     Parameters
@@ -32,6 +34,9 @@ def read_raw_nihon(fname, preload=False, verbose=None) -> "RawNihon":
         Path to the Nihon Kohden data file (``.EEG``).
     preload : bool
         If True, all data are loaded at initialization.
+    %(encoding_nihon)s
+
+        .. versionadded:: 1.11
     %(verbose)s
 
     Returns
@@ -44,7 +49,7 @@ def read_raw_nihon(fname, preload=False, verbose=None) -> "RawNihon":
     --------
     mne.io.Raw : Documentation of attributes and methods of RawNihon.
     """
-    return RawNihon(fname, preload, verbose)
+    return RawNihon(fname, preload, encoding=encoding, verbose=verbose)
 
 
 _valid_headers = [
@@ -315,7 +320,7 @@ def _parse_sub_event_log(sub_event_log):
     return t_sub_desc, t_sub_onset
 
 
-def _read_nihon_annotations(fname):
+def _read_nihon_annotations(fname, encoding="utf-8"):
     fname = _ensure_path(fname)
     log_fname = fname.with_suffix(".LOG")
     if not log_fname.exists():
@@ -346,15 +351,10 @@ def _read_nihon_annotations(fname):
                     t_onset += t_sub_onset
 
                 t_desc = t_desc.rstrip(b"\x00")
-                for enc in _encodings:
-                    try:
-                        t_desc = t_desc.decode(enc)
-                    except UnicodeDecodeError:
-                        pass
-                    else:
-                        break
-                else:
-                    warn(f"Could not decode log as one of {_encodings}")
+                try:
+                    t_desc = t_desc.decode(encoding)
+                except UnicodeDecodeError:
+                    warn(f"Could not decode log as {encoding}")
                     continue
 
                 all_onsets.append(t_onset)
@@ -414,6 +414,9 @@ class RawNihon(BaseRaw):
         Path to the Nihon Kohden data ``.eeg`` file.
     preload : bool
         If True, all data are loaded at initialization.
+    %(encoding_nihon)s
+
+        .. versionadded:: 1.11
     %(verbose)s
 
     See Also
@@ -422,7 +425,7 @@ class RawNihon(BaseRaw):
     """
 
     @verbose
-    def __init__(self, fname, preload=False, verbose=None):
+    def __init__(self, fname, preload=False, *, encoding="utf-8", verbose=None):
         fname = _check_fname(fname, "read", True, "fname")
         data_name = fname.name
         logger.info(f"Loading {data_name}")
@@ -468,7 +471,7 @@ class RawNihon(BaseRaw):
         )
 
         # Get annotations from LOG file
-        annots = _read_nihon_annotations(fname)
+        annots = _read_nihon_annotations(fname, encoding)
 
         # Annotate acquisition skips
         controlblock = header["controlblocks"][0]
