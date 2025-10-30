67c106b37e36ffb50460f38e4ebd9f1b08ee21fc
Thomas S. Binns <t.s.binns@outlook.com>
2025-10-30 20:09:48 +0000

    Update directive formatting checker

--- doc/sphinxext/directive_formatting.py
+++ doc/sphinxext/directive_formatting.py
@@ -9,6 +9,7 @@ from mne_doc_utils import sphinx_logger
 
 def setup(app):
     app.connect("source-read", check_directive_formatting)
+    app.connect("autodoc-process-docstring", check_directive_formatting)
 
 
 def setup_module():
@@ -16,45 +17,55 @@ def setup_module():
     pass
 
 
-def check_directive_formatting(app=None, docname=None, content=None):
-    """Check that Sphinx directives are properly formatted."""
-    directives = [
-        "attention",
-        "caution",
-        "danger",
-        "error",
-        "hint",
-        "important",
-        "note",
-        "tip",
-        "warning",
-        "admonition",
-        "seealso",
-        "versionadded",
-        "version-added",
-        "versionchanged",
-        "version-changed",
-        "deprecated",
-        "version-deprecated",
-        "versionremoved",
-        "version-removed",
-    ]
+def check_directive_formatting(*args):
+    """Check that directives are not missing a space.
 
-    missing_space = re.search(r"\.\.[a-zA-Z]+::", content[0])
-    if missing_space is not None:
-        sphinx_logger.warning(
-            f"File '{docname}' is missing a space after '..' in the directive "
-            f"'{missing_space.group()}'"
-        )
+    For args, see Sphinx events 'source-read' and 'autodoc-process-docstring'.
+    """
+    if len(args) == 3:  # from source-read
+        source_type = "File"
+        name = args[1]
+        source = args[2][0].split("\n")
+    else:  # from autodoc-process-docstring
+        source_type = "Docstring"
+        name = args[2]
+        source = args[5]
 
-    content_stripped = content[0].replace(" ", "")
-    for match in re.finditer(rf"\.\.({('|').join(directives)})::", content_stripped):
-        prematch = content_stripped[match.start() - 2 : match.start()]
-        if (
-            prematch != "\n\n"  # no preceding blank line
-            and not re.search(r"(-|=|\^)\n", prematch)  # and not after a section header
-        ):
+    for idx, line in enumerate(source):
+        # check for missing space after '..'
+        missing = re.search(r"\.\.[a-zA-Z]+::", line)
+        if missing is not None:
             sphinx_logger.warning(
-                f"File '{docname}' is missing a blank line before the directive "
-                f"'{missing_space.group()}'"
+                f"{source_type} '{name}' is missing a space after '..' in the "
+                f"directive '{missing.group()}'"
             )
+
+        # check for missing preceding blank line
+        if idx == 0:
+            continue
+        dir_pattern = r"\.\. [a-zA-Z]+::"
+        head_pattern = r"^[-|=|\^]+$"
+        directive = re.search(dir_pattern, line)
+        if directive is not None:
+            line_prev = source[idx - 1].strip()
+            if (
+                line_prev != ""  # not an empty line
+                and not re.search(head_pattern, line_prev)  # not after a header
+                and not re.search(dir_pattern, line_prev)  # not after directive
+            ):
+                # check if previous line is part of another directive
+                bad = True
+                for line_prev in reversed(source[: idx - 1]):
+                    line_prev = line_prev.strip()
+                    if line_prev == "" or re.search(head_pattern, line_prev):
+                        # if a blank line or header, is not part of another directive
+                        break
+                    if re.search(dir_pattern, line_prev):
+                        bad = False  # is part of another directive
+                        break
+                    # or keep going until we reach the first line (so must be bad)
+                if bad:
+                    sphinx_logger.warning(
+                        f"{source_type} '{name}' is missing a blank line before the "
+                        f"directive '{directive.group()}'"
+                    )
