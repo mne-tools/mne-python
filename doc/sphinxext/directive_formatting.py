7f59317b65d748911dd6298bed4ca2fb23fcc968
Thomas S. Binns <t.s.binns@outlook.com>
2025-11-01 16:36:46 +0000

    Update directive formatting checker

--- doc/sphinxext/directive_formatting.py
+++ doc/sphinxext/directive_formatting.py
@@ -22,25 +22,42 @@ def check_directive_formatting(*args):
 
     For args, see Sphinx events 'source-read' and 'autodoc-process-docstring'.
     """
+    # Extract relevant info from args
     if len(args) == 3:  # from source-read
         source_type = "File"
         name = args[1]
-        source = args[2][0].split("\n")
-    else:  # from autodoc-process-docstring
+        source = args[2][0]
+        source_concat = source  # content already a single string
+    elif len(args) == 6:  # from autodoc-process-docstring
         source_type = "Docstring"
         name = args[2]
         source = args[5]
+        source_concat = "\n".join(source)  # combine lines into single string
+    else:
+        raise RuntimeError("Unexpected number of arguments from Sphinx event")
 
+    # Check if any directives are present
+    if re.search(r"\.\.\s*[a-zA-Z]+::", source_concat) is None:
+        return
+
+    # Separate content into lines (docstrings already are)
+    if source_type == "File":
+        source = source.split("\n")
+
+    # Check for bad formatting
     for idx, line in enumerate(source):
-        # check for missing space after '..'
+        # Check for missing space after '..'
         missing = re.search(r"\.\.[a-zA-Z]+::", line)
         if missing is not None:
             sphinx_logger.warning(
                 f"{source_type} '{name}' is missing a space after '..' in the "
                 f"directive '{missing.group()}'"
             )
+        # Extra spaces after '..' don't affect formatting
 
-        # check for missing preceding blank line
+        # Check for missing preceding blank line
+        # (exceptions are for directives at the start of files, after a header, or after
+        # another directive/another directive's content)
         if idx == 0:
             continue
         dir_pattern = r"\.\. [a-zA-Z]+::"
@@ -48,20 +65,20 @@ def check_directive_formatting(*args):
         directive = re.search(dir_pattern, line)
         if directive is not None:
             line_prev = source[idx - 1].strip()
-            if (
-                line_prev != ""  # not an empty line
-                and not re.search(head_pattern, line_prev)  # not after a header
-                and not re.search(dir_pattern, line_prev)  # not after directive
+            if (  # If previous line is...
+                line_prev != ""  # not empty
+                and not re.search(head_pattern, line_prev)  # not a header
+                and not re.search(dir_pattern, line_prev)  # not a directive
             ):
-                # check if previous line is part of another directive
+                # Check if previous line is part of another directive
                 bad = True
                 for line_prev in reversed(source[: idx - 1]):
                     line_prev = line_prev.strip()
                     if line_prev == "" or re.search(head_pattern, line_prev):
-                        # if a blank line or header, is not part of another directive
-                        break
+                        # is a blank line or header, so not part of another directive
+                        break  # must be bad formatting
                     if re.search(dir_pattern, line_prev):
-                        bad = False  # is part of another directive
+                        bad = False  # is part of another directive, is good formatting
                         break
                     # or keep going until we reach the first line (so must be bad)
                 if bad:
