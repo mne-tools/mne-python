7e0af0cca0da2aeeb1304bb6970c6c0197148127
Daniel McCloy <dan@mccloy.info>
2025-12-19 13:57:04 -0600

    restore from lockfile when setting up old CI

--- tools/github_actions_dependencies.sh
+++ tools/github_actions_dependencies.sh
@@ -27,8 +27,13 @@ if [ ! -z "$CONDA_ENV" ]; then
 		GROUP="test_extra"
 		EXTRAS="[hdf5]"
 	fi
-# TODO add a conditional branch here, that uses uv/pixi to recreate the "old" env
-#      from its lockfile, activate the env, and exit the bash script
+elif [[ ${MNE_CI_KIND} == "old" ]]; then
+	GROUP="test"
+	EXTRAS=""
+	STD_ARGS="--progress-bar off"
+	uv venv --python 3.10
+	uv pip sync ${SCRIPT_DIR}/pylock.ci-old.toml
+	source .venv/bin/activate
 elif [[ "${MNE_CI_KIND}" == "pip" ]]; then
 	GROUP="test_extra"
 	EXTRAS="[full-pyside6]"
