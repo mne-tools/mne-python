d224e67d4ecec9d9a0545ae14b45ec7501f06444
CarinaFo <carinaforster0611@gmail.com>
2026-01-10 14:30:22 +1000

    included None

--- mne/cov.py
+++ mne/cov.py
@@ -2364,7 +2364,7 @@ def whiten_evoked(
     )
 
     # create matrix based on nave_per_channel if attribute exists
-    if hasattr(evoked, "nave_per_channel"):
+    if hasattr(evoked, "nave_per_channel") and evoked.nave_per_channel is not None:
         noise_scaling_matrix = np.diag(np.sqrt(evoked.nave_per_channel[picks]))
         evoked.data[picks] = noise_scaling_matrix @ np.dot(W, evoked.data[picks])
     else:
