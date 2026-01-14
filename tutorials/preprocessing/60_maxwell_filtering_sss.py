03f6947341d850dcd83c59176693f7cf476343b1
skjerns <14980558+skjerns@users.noreply.github.com>
2026-01-14 14:55:18 +0100

    fix formatting error

--- tutorials/preprocessing/60_maxwell_filtering_sss.py
+++ tutorials/preprocessing/60_maxwell_filtering_sss.py
@@ -320,11 +320,11 @@ raw_sss.pick(["meg"]).plot(duration=2, butterfly=True)
 # - Keep the number of added components small (the ~8 in the original paper);
 #   adding too many can worsen numerical conditioning and increase noise.
 # - Prefer empty-room data from the same session/environment.
-# - eSSS can be combined with tSSS (“teSSS”, i.e. setting `st_duration=X`).
+# - eSSS can be combined with tSSS (“teSSS”, i.e. setting ``st_duration=X``).
 # - you can get projectsion from an empty room recording via
-#   proj = mne.compute_proj_raw(noise_raw.pick('meg'), n_grad=3, n_mag=3,
-#                               meg="combined")
-# You can enabme eSSS by setting ``extended_proj=proj`` in
+#   ``proj = mne.compute_proj_raw(noise_raw.pick('meg'), n_grad=3, n_mag=3,
+#                               meg="combined")``
+# You can enable eSSS by setting ``extended_proj=proj`` in
 # :func:`~mne.preprocessing.maxwell_filter`.
 #
 #
