8b8edb0a70e2fabb4d6c80a5d72caf63349a4a5e
Eric Larson <larson.eric.d@gmail.com>
2026-02-09 11:54:59 -0500

    BUG: Fix bug with plot_white (#13595)

--- tools/azure_dependencies.sh
+++ tools/azure_dependencies.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 STD_ARGS="--progress-bar off --upgrade "
 python -m pip install $STD_ARGS pip setuptools wheel
 if [ "${TEST_MODE}" == "pip" ]; then
-	python -m pip install $STD_ARGS --only-binary="numba,llvmlite,numpy,scipy,vtk,dipy,openmeeg" -e .[full] --group=test git+https://github.com/python-quantities/python-quantities
+	python -m pip install $STD_ARGS --only-binary="numba,llvmlite,numpy,scipy,vtk,dipy,openmeeg" -e .[full] --group=test
 elif [ "${TEST_MODE}" == "pip-pre" ]; then
 	${SCRIPT_DIR}/install_pre_requirements.sh
 	python -m pip install $STD_ARGS --pre -e . --group=test_extra
