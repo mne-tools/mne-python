7ecd9db18ff95d845582c3aafd20d23d64896c01
Thomas S. Binns <t.s.binns@outlook.com>
2026-02-09 18:56:44 +0000

    Try fix venv activation

--- tools/github_actions_download.sh
+++ tools/github_actions_download.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -ef
 
-if [[ ${MNE_CI_KIND} == "old" ]]; then
-    source .venv/bin/activate
-fi
-
 if [ "${DEPS}" != "minimal" ]; then
 	python -c 'import mne; mne.datasets.testing.data_path(verbose=True)';
 	python -c "import mne; mne.datasets.misc.data_path(verbose=True)";
--- tools/github_actions_infos.sh
+++ tools/github_actions_infos.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -ef
 
-if [[ ${MNE_CI_KIND} == "old" ]]; then
-    source .venv/bin/activate
-fi
-
 which mne
 mne sys_info -pd
 python -c "import numpy; numpy.show_config()"
