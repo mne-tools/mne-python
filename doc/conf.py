880f5f17135c5d9ab0d5615d9b60609141e21902
Daniel McCloy <dan@mccloy.info>
2026-02-18 09:25:14 -0600

    remove unnecessary noqa annotations

--- doc/conf.py
+++ doc/conf.py
@@ -987,7 +987,7 @@ html_context = {
             klass="only-dark",
         ),
         dict(
-            name="Commissariat à l´énergie atomique et aux énergies alternatives",  # noqa E501
+            name="Commissariat à l´énergie atomique et aux énergies alternatives",
             img="CEA.png",
             url="http://www.cea.fr/",
             size=md,
@@ -1092,7 +1092,7 @@ html_context = {
             klass="only-dark",
         ),
         dict(
-            name="Institut national de recherche en informatique et en automatique",  # noqa E501
+            name="Institut national de recherche en informatique et en automatique",
             img="inria.png",
             url="https://www.inria.fr/",
             size=xl,
@@ -1212,7 +1212,7 @@ html_context = {
         ),
         dict(
             title="Machine Learning",
-            text="Advanced decoding models including time general\u00adiza\u00adtion.",  # noqa E501
+            text="Advanced decoding models including time general\u00adiza\u00adtion.",
             url="auto_tutorials/machine-learning/50_decoding.html",
             img="sphx_glr_50_decoding_006.png",
             alt="Decoding",
@@ -1226,14 +1226,14 @@ html_context = {
         ),
         dict(
             title="Statistics",
-            text="Parametric and non-parametric, permutation tests and clustering.",  # noqa E501
+            text="Parametric and non-parametric, permutation tests and clustering.",
             url="auto_tutorials/stats-source-space/index.html",
             img="sphx_glr_20_cluster_1samp_spatiotemporal_001.png",
             alt="Clusters",
         ),
         dict(
             title="Connectivity",
-            text="All-to-all spectral and effective connec\u00adtivity measures.",  # noqa E501
+            text="All-to-all spectral and effective connec\u00adtivity measures.",
             url="https://mne.tools/mne-connectivity/stable/auto_examples/mne_inverse_label_connectivity.html",  # noqa E501
             img="https://mne.tools/mne-connectivity/stable/_images/sphx_glr_mne_inverse_label_connectivity_001.png",  # noqa E501
             alt="Connectivity",
