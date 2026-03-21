ba195c8622035dae0698e5ec396a0dc242e3b5fe
Thomas S. Binns <t.s.binns@outlook.com>
2026-03-21 13:36:51 +0000

    Update error msg

--- mne/viz/misc.py
+++ mne/viz/misc.py
@@ -80,7 +80,7 @@ def _get_ch_type_metadata(info, ch_names):
             ch_types.append(key)
     if len(indices) == 0:
         raise RuntimeError(
-            f"No plottable channel types found. "
+            "No plottable channel types found. "
             f"Allowed types are: {_DATA_CH_TYPES_SPLIT}"
         )
     return indices, titles, units, scalings, ch_types
