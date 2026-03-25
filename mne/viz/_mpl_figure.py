527108f283526f534646c4bed94e3d4a766b36e9
Clemens Brunner <clemens.brunner@gmail.com>
2026-03-25 14:51:32 +0100

    Fix top and bottom clipping

--- mne/viz/_mpl_figure.py
+++ mne/viz/_mpl_figure.py
@@ -367,7 +367,7 @@ class MNEBrowseFigure(BrowserBase, MNEFigure):
         # XXX simpler with constrained_layout? (when it's no longer "beta")
         l_margin = 1.0
         r_margin = 0.1
-        b_margin = 0.45
+        b_margin = 0.65
         t_margin = 0.25
         scroll_width = 0.25
         hscroll_dist = 0.25
@@ -1424,10 +1424,10 @@ class MNEBrowseFigure(BrowserBase, MNEFigure):
                     text = ax.annotate(
                         descr,
                         xy,
-                        xytext=(0, 9),
+                        xytext=(0, -3),
                         textcoords="offset points",
                         ha="center",
-                        va="baseline",
+                        va="top",
                         color=segment_color,
                     )
                     self.mne.annotation_texts.append(text)
