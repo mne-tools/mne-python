373b070c9cf492e1576fb9613c650712a8c3a1eb
Athish M <athishdresu@gmail.com>
2026-04-03 22:28:56 +0530

    DOC: Add crossrefs to info docstring

--- mne/_fiff/meas_info.py
+++ mne/_fiff/meas_info.py
@@ -1286,6 +1286,18 @@ class Info(ValidatedDict, SetChannelsMixin, MontageMixin, ContainsMixin):
         modified by various MNE-Python functions or methods (which have
         safeguards to ensure all fields remain in sync).
 
+        Some common methods that safely modify the ``info`` object include:
+        * :meth:`mne.io.Raw.add_proj`, :meth:`mne.Epochs.add_proj`,
+          :meth:`mne.Evoked.add_proj`
+        * :meth:`mne.io.Raw.del_proj`, :meth:`mne.Epochs.del_proj`,
+          :meth:`mne.Evoked.del_proj`
+        * :meth:`mne.io.Raw.set_channel_types`,
+          :meth:`mne.Epochs.set_channel_types`,
+          :meth:`mne.Evoked.set_channel_types`
+        * :meth:`mne.io.Raw.set_meas_date`,
+          :meth:`mne.Epochs.set_meas_date`,
+          :meth:`mne.Evoked.set_meas_date`
+
     Parameters
     ----------
     *args : list
