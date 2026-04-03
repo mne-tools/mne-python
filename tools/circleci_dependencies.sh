a27138c13549fbc7efef70b239de85181a8641c6
Eric Larson <larson.eric.d@gmail.com>
2026-04-03 11:25:13 -0400

    FIX: Groups

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -7,7 +7,7 @@ python -m pip install --upgrade "pip>=25.1" build
 python -m pip install --upgrade --progress-bar off $ONLY_BINARY \
     -ve .[full] \
     --group=test \
-    --group=doc \
+    --group=doc-full \
     -r doc/sphinxext/related_software.txt \
     "git+https://github.com/mne-tools/mne-bids.git" \
     "git+https://github.com/mne-tools/mne-qt-browser.git" \
