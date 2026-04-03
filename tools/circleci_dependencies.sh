e7ce877b07a7b789c64b355d168e292183049877
Eric Larson <larson.eric.d@gmail.com>
2026-04-03 12:42:43 -0400

    FIX: Maybe

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -4,12 +4,12 @@ ONLY_BINARY="--only-binary \"numpy,dipy,scipy,matplotlib,pandas,statsmodels,netC
 
 set -x
 python -m pip install --upgrade "pip>=25.1" build
-# rpy2 3.6.7 causes problems with our installed R version, so pin to 3.6.6
+# rpy2 3.6.7 (or its deps) cause problems with our installed R version, so pin them
 python -m pip install --upgrade --progress-bar off $ONLY_BINARY \
     -ve .[full] \
     --group=test \
     --group=doc-full \
-    "rpy2==3.6.6" \
+    "rpy2==3.6.6" "rpy2-rinterface==3.6.5" "rpy2-robjects==3.6.4" \
     -r doc/sphinxext/related_software.txt \
     "git+https://github.com/mne-tools/mne-bids.git" \
     "git+https://github.com/mne-tools/mne-qt-browser.git" \
