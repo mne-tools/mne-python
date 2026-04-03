d68db1c44a27aba3addc5c259f1345401729f982
Aniket Singh Yadav <singhyadavaniket43@gmail.com>
2026-04-03 09:02:57 +0000

    update location

--- doc/sphinxext/credit_tools.py
+++ doc/sphinxext/credit_tools.py
@@ -335,7 +335,7 @@ def generate_credit_rst(app=None, *, verbose=False):
     globs["bin/*"] = "mne.commands"
     globs["mne/morph_map.py"] = "mne.surface"
     globs["mne/baseline.py"] = "mne.epochs"
-    globs["mne/_edf/open.py"] = "mne.io.edf"
+    globs["mne/io/edf/_open.py"] = "mne.io"
     for key in """
         parallel.py rank.py misc.py data/*.* defaults.py fixes.py icons/*.* icons.*
     """.strip().split():
