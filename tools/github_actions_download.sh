9f747ae71545a7411e001abe9c797a7e71f17528
Scott Huberty <seh33@uw.edu>
2026-04-07 22:25:54 -0700

    triage

--- tools/github_actions_download.sh
+++ tools/github_actions_download.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -ef
+run_python() {
+  if [[ "${MNE_CI_KIND}" == "pixi" ]]; then
+    pixi run python "$@"
+  else
+    python "$@"
+  fi
+}
 
+# TODO: I think that DEPS is cruft. Its not set anywhere??
 if [ "${DEPS}" != "minimal" ]; then
-	python -c 'import mne; mne.datasets.testing.data_path(verbose=True)';
-	python -c "import mne; mne.datasets.misc.data_path(verbose=True)";
+	run_python -c 'import mne; mne.datasets.testing.data_path(verbose=True)';
+	run_python -c "import mne; mne.datasets.misc.data_path(verbose=True)";
 fi
