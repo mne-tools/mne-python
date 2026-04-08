649e2b2eac248c46f919fc6e4d3977316d0d3699
Scott Huberty <seh33@uw.edu>
2026-04-08 09:37:30 -0700

    fix: cruft

--- tools/github_actions_download.sh
+++ tools/github_actions_download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ef
 # TODO: I think that DEPS is cruft. Its not set anywhere??
 if [ "${DEPS}" != "minimal" ]; then
-	run_python -c 'import mne; mne.datasets.testing.data_path(verbose=True)';
-	run_python -c "import mne; mne.datasets.misc.data_path(verbose=True)";
+	python -c 'import mne; mne.datasets.testing.data_path(verbose=True)';
+	python -c "import mne; mne.datasets.misc.data_path(verbose=True)";
 fi
