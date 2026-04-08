336495ead38bb0129912111ebfc0cb21644cff05
Scott Huberty <seh33@uw.edu>
2026-04-08 09:31:27 -0700

    reveal PATH

--- tools/github_actions_download.sh
+++ tools/github_actions_download.sh
@@ -1,12 +1,4 @@
 #!/bin/bash -ef
-run_python() {
-  if [[ "${MNE_CI_KIND}" == "pixi" ]]; then
-    pixi run python "$@"
-  else
-    python "$@"
-  fi
-}
-
 # TODO: I think that DEPS is cruft. Its not set anywhere??
 if [ "${DEPS}" != "minimal" ]; then
 	run_python -c 'import mne; mne.datasets.testing.data_path(verbose=True)';
