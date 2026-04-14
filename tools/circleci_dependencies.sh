3e885300535d8894d0e09047976ec66b08ee11c4
Eric Larson <larson.eric.d@gmail.com>
2026-04-14 18:41:47 -0400

    FIX: Ref [skip azp] [skip actions]

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -13,7 +13,7 @@ python -m pip install --upgrade --progress-bar off $ONLY_BINARY \
     -r doc/sphinxext/related_software.txt \
     "git+https://github.com/mne-tools/mne-bids.git" \
     "git+https://github.com/mne-tools/mne-qt-browser.git" \
-    "git+https://github.com/larsoner/pyvista.git@weakref" \
+    "git+https://github.com/pyvista/pyvista.git" \
     "git+https://github.com/sphinx-gallery/sphinx-gallery.git"
 python -m pip install --upgrade --progress-bar off --no-deps $ONLY_BINARY \
     -r doc/sphinxext/related_software_nodeps.txt
