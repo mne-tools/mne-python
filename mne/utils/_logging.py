b70850f92fabb9efc1831ef3fbbc3eb950e0dd26
Eric Larson <larson.eric.d@gmail.com>
2026-04-15 11:15:07 -0400

    FIX: Why not noop

--- mne/utils/_logging.py
+++ mne/utils/_logging.py
@@ -161,16 +161,19 @@ class use_log_level:
         self._level = verbose
         self._add_frames = add_frames
         self._old_frames = _filter.add_frames
+        self._set = self._level is not None or self._add_frames is not None
 
     def __enter__(self):  # noqa: D105
-        level = logger.level if self._level is None else self._level
-        self._old_level = set_log_level(
-            level, return_old_level=True, add_frames=self._add_frames
-        )
+        if self._set:
+            level = logger.level if self._level is None else self._level
+            self._old_level = set_log_level(
+                level, return_old_level=True, add_frames=self._add_frames
+            )
 
     def __exit__(self, *args):  # noqa: D105
-        add_frames = self._old_frames if self._add_frames is not None else None
-        set_log_level(self._old_level, add_frames=add_frames)
+        if self._set:
+            add_frames = self._old_frames if self._add_frames is not None else None
+            set_log_level(self._old_level, add_frames=add_frames)
 
 
 _LOGGING_TYPES = dict(
