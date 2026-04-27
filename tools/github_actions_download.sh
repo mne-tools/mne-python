8741cedd4c099de2d4db4d2b0674e214e65e637c
Eric Larson <larson.eric.d@gmail.com>
2026-04-27 10:55:14 -0400

    MAINT: Make test dir read-only

--- tools/github_actions_download.sh
+++ tools/github_actions_download.sh
@@ -3,4 +3,7 @@
 if [ "${MNE_CI_KIND}" != "minimal" ]; then
 	python -c 'import mne; mne.datasets.testing.data_path(verbose=True)';
 	python -c "import mne; mne.datasets.misc.data_path(verbose=True)";
+	# Make read-only to make sure we don't modify its contents
+	PATH=$(python -c "import mne; print(mne.datasets.testing.data_path(verbose=False))")
+	chmod -R a-w "$PATH"
 fi
