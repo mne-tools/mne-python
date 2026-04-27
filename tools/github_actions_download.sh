2f35fb9ef2e69d266e09a2245e95e8b1caa425cb
Eric Larson <larson.eric.d@gmail.com>
2026-04-27 11:15:14 -0400

    FIX: Unify [skip circle]

--- tools/github_actions_download.sh
+++ tools/github_actions_download.sh
@@ -4,6 +4,7 @@ if [ "${MNE_CI_KIND}" != "minimal" ]; then
 	python -c 'import mne; mne.datasets.testing.data_path(verbose=True)';
 	python -c "import mne; mne.datasets.misc.data_path(verbose=True)";
 	# Make read-only to make sure we don't modify its contents
-	PATH=$(python -c "import mne; print(mne.datasets.testing.data_path(verbose=False))")
-	chmod -R a-w "$PATH"
+	TESTING_PATH=$(python -c "import mne; print(mne.datasets.testing.data_path(verbose=False))")
+	echo "Testing data path: $TESTING_PATH"
+	chmod -R a-w "$TESTING_PATH"
 fi
