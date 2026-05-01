f124275608b3c429cd5e533d75b3b1bdd9364265
Eric Larson <larson.eric.d@gmail.com>
2026-05-01 17:34:51 -0400

    FIX: Kind

--- tools/github_actions_verify_python.sh
+++ tools/github_actions_verify_python.sh
@@ -11,7 +11,7 @@ if [[ "${MNE_CI_KIND}" == "conda" ]] || [[ "${MNE_CI_KIND}" == "mamba" ]]; then
 	WANT="micromamba/envs/mne"
 elif [[ "${MNE_CI_KIND}" == "old" ]]; then
 	WANT="mne-python/mne-python/.venv/bin"
-elif [[ "${MNE_CI_KIND}" == "pip" ]] || [[ "${MNE_CI_KIND}" == "pip-pre" ]]; then
+elif [[ "${MNE_CI_KIND}" == "pip" ]] || [[ "${MNE_CI_KIND}" == "pip-pre" ]] || [[ "${MNE_CI_KIND}" == "minimal" ]]; then
 	WANT="hostedtoolcache/Python"
 else
 	echo "✕ ERROR: Unrecognized MNE_CI_KIND=${MNE_CI_KIND}"
