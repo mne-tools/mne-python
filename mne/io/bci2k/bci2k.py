6c05e3bf8e6aa28cb74f7abb689ce8a333023de3
Hansuja <hansujaigdtuwcseai@gmail.com>
2026-05-03 17:54:04 +0530

    fix offset error

--- mne/io/bci2k/bci2k.py
+++ mne/io/bci2k/bci2k.py
@@ -184,7 +184,7 @@ def _read_bci2k_data(fname, info_dict):
             raise ValueError(
                 "Expected SourceChOffset and SourceChGain lengths to match SourceCh."
             )
-        offsets_arr = np.array([_parse_value_with_unit(val)[0] for val in offsets])
+        offsets_arr = np.array([float(val) for val in offsets])
         gain_parsed = [_parse_value_with_unit(val, unit_scale=_VOLT_SCALE) for val in gains]
         gains_arr = np.array([val for val, _ in gain_parsed])
         gain_scales = np.array([scale for _, scale in gain_parsed])
