01c233a5d20b3eefc1d8e41fd08ac7368e85fba4
Bhargav Kowshik <bhargav.kowshik@gmail.com>
2026-05-12 17:06:48 +0530

    DOC: address review on apply_function Epochs docstring (#13891) Apply @tsbinns review suggestion (n_channels -> len(picks); explain channel_wise=True shape).

--- mne/utils/docs.py
+++ mne/utils/docs.py
@@ -1920,7 +1920,9 @@ docdict["fun_applyfun"] = applyfun_fun_base.format(
     " if ``channel_wise=True`` and ``(len(picks), n_times)`` otherwise"
 )
 docdict["fun_applyfun_epochs"] = applyfun_fun_base.format(
-    " if ``channel_wise=True`` and ``(n_epochs, n_channels, n_times)`` otherwise"
+    " if ``channel_wise=True`` (because it will apply to 1-D"
+    " slices along the times axis) and"
+    " ``(n_epochs, len(picks), n_times)`` otherwise"
 )
 docdict["fun_applyfun_evoked"] = applyfun_fun_base.format(
     " because it will apply channel-wise"
