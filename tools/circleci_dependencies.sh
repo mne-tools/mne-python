9e8ff4c2c8870762b5e3e9cf944ec1857e740f0d
Eric Larson <larson.eric.d@gmail.com>
2026-05-18 13:29:01 -0400

    FIX: More [circle full]

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -3,17 +3,19 @@
 ONLY_BINARY="--only-binary=:all:"
 
 set -x
-python -m pip install --upgrade "pip>=25.1" build
+python -m pip install --upgrade $ONLY_BINARY "pip>=25.1" build
 # rpy2 3.6.7 (or its deps) cause problems with our installed R version, so pin them
-python -m pip install --upgrade --progress-bar off $ONLY_BINARY \
+# and the interface doesn't have wheels so allow it to build
+python -m pip install --upgrade \
+    "rpy2==3.6.6" "rpy2-rinterface==3.6.5" "rpy2-robjects==3.6.4" \
+python -m pip install --upgrade $ONLY_BINARY \
     -ve .[full-pyside6] \
     --group=test \
     --group=doc-full \
-    "rpy2==3.6.6" "rpy2-rinterface==3.6.5" "rpy2-robjects==3.6.4" \
     -r doc/sphinxext/related_software.txt \
     "git+https://github.com/mne-tools/mne-bids.git" \
     "git+https://github.com/mne-tools/mne-qt-browser.git" \
     "git+https://github.com/pyvista/pyvista.git" \
     "git+https://github.com/sphinx-gallery/sphinx-gallery.git"
-python -m pip install --upgrade --progress-bar off --no-deps $ONLY_BINARY \
+python -m pip install --upgrade --no-deps $ONLY_BINARY \
     -r doc/sphinxext/related_software_nodeps.txt
