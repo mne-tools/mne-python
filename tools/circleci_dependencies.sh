b17e507f7bf8ab9e52eb793e16353f3e10882bdf
Eric Larson <larson.eric.d@gmail.com>
2026-05-18 13:47:41 -0400

    FIX: Circle [circle full]

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -1,21 +1,20 @@
 #!/bin/bash -ef
 
-ONLY_BINARY="--only-binary=:all:"
+ONLY_BINARY="--only-binary=:all: --no-binary=mne-ari,rpy2-interface"
 
 set -x
-python -m pip install --upgrade $ONLY_BINARY "pip>=25.1" build
+python -m pip install --upgrade "pip>=25.1" build
 # rpy2 3.6.7 (or its deps) cause problems with our installed R version, so pin them
-# and the interface doesn't have wheels so allow it to build
-python -m pip install --upgrade \
-    "rpy2==3.6.6" "rpy2-rinterface==3.6.5" "rpy2-robjects==3.6.4"
-python -m pip install --upgrade $ONLY_BINARY \
+python -m pip install --upgrade --progress-bar off $ONLY_BINARY \
     -ve .[full-pyside6] \
     --group=test \
     --group=doc-full \
+    "rpy2==3.6.6" "rpy2-rinterface==3.6.5" "rpy2-robjects==3.6.4" \
     -r doc/sphinxext/related_software.txt \
-    "git+https://github.com/mne-tools/mne-bids.git" \
-    "git+https://github.com/mne-tools/mne-qt-browser.git" \
-    "git+https://github.com/pyvista/pyvista.git" \
-    "git+https://github.com/sphinx-gallery/sphinx-gallery.git"
-python -m pip install --upgrade --no-deps $ONLY_BINARY \
+    "mne-bids @ https://github.com/mne-tools/mne-bids/archive/refs/heads/main.zip" \
+    "mne-qt-browser @ https://github.com/mne-tools/mne-qt-browser/archive/refs/heads/main.zip" \
+    "pyvista @ https://github.com/pyvista/pyvista/archive/refs/heads/main.zip" \
+    "pyvistaqt @ https://github.com/pyvista/pyvistaqt/archive/refs/heads/main.zip" \
+    "sphinx-gallery @ https://github.com/sphinx-gallery/sphinx-gallery/archive/refs/heads/master.zip"
+python -m pip install --upgrade --progress-bar off --no-deps $ONLY_BINARY \
     -r doc/sphinxext/related_software_nodeps.txt
