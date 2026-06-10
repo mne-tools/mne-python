8a352cfadf0c16abb6f52f23df58840551910c45
Daniel McCloy <dan@mccloy.info>
2026-06-10 11:36:43 -0500

    fix pypi/cf name mismatch; comments

--- tools/write-pixi-toml.py
+++ tools/write-pixi-toml.py
@@ -15,7 +15,7 @@ def pip_to_pixi(dependencies_list):
             name, version = dep.split(" ", maxsplit=1)
         else:
             name, version = dep, "*"
-        out[name] = version
+        out[name] = "".join(version.split(" "))  # pixi hates internal spaces
     return out
 
 
@@ -25,6 +25,9 @@ repo_root = Path(__file__).resolve().parent.parent
 pyproject_fpath = repo_root / "pyproject.toml"
 pyproject = tomlkit.loads(pyproject_fpath.read_bytes())
 outfile = repo_root / "tools" / "ci" / "macos-intel" / "pixi.toml"
+
+# handle naming differences between PyPI and conda-forge
+remapping = dict(neo="python-neo")
 
 # in keys: build-system, dependency-groups, project, tool
 # out keys: workspace, dependencies, pypi-dependencies (may need tasks)
@@ -42,13 +45,20 @@ dependencies.extend(pyproject["dependency-groups"]["test_extra"])
 dependencies.extend(pyproject["project"]["optional-dependencies"]["hdf5"])
 dependencies.extend(pyproject["project"]["optional-dependencies"]["full-no-qt"])
 dependencies.extend(pyproject["project"]["optional-dependencies"]["full-pyside6"])
+# exclude recursive MNE deps
 dependencies = list(
     filter(lambda x: isinstance(x, str) and "mne[" not in x, dependencies)
 )
+# strip out platform specifiers; for now this affects only pyobj-framework-cocoa, and
+# since this pixi.toml is destined for macos-intel job, we want it in there so we're
+# skipping writing the logic to parse the platform spec.
 for ix, dep in enumerate(dependencies):
     if (split_ix := dep.find(";")) >= 0:
         dependencies[ix] = dep[:split_ix].strip()
+# handle mismatch between names on PyPI and conda-forge
+dependencies = [remapping.get(dep, dep) for dep in dependencies]
 
+# exclude pypi-only deps
 dependencies = sorted(set(dependencies) - set(pypi_dependencies))
 
 out = {
