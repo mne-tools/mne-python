23588ab31d443862f63c4c7bb050928752fc3f06
Your Name <your@email.com>
2026-07-03 06:11:57 +0300

    DOC: fix alphabetical ordering of picks_all_get_data in docdict

--- mne/utils/docs.py
+++ mne/utils/docs.py
@@ -3547,6 +3547,12 @@ docdict["picks_all_data"] = _reflow_param_docstring(
 docdict["picks_all_data_noref"] = _reflow_param_docstring(
     f"{picks_base} all data channels {noref}"
 )
+docdict["picks_all_get_data"] = _reflow_param_docstring(
+    f"{picks_base} all channels, and bad channels are included by default "
+    f"when picks is None. If picks are given as channel type(s) (e.g., "
+    f"``'eeg'``), bad channels of those types are excluded by default. "
+    f"{reminder}"
+)
 docdict["picks_all_notypes"] = _reflow_param_docstring(
     f"{picks_base_notypes} all channels. {reminder}"
 )
@@ -3556,12 +3562,6 @@ docdict["picks_good_data"] = _reflow_param_docstring(
 )
 docdict["picks_good_data_noref"] = _reflow_param_docstring(
     f"{picks_base} good data channels {noref}"
-)
-docdict["picks_all_get_data"] = _reflow_param_docstring(
-    f"{picks_base} all channels, and bad channels are included by default "
-    f"when picks is None. If picks are given as channel type(s) (e.g., "
-    f"``'eeg'``), bad channels of those types are excluded by default. "
-    f"{reminder}"
 )
 docdict["picks_header"] = _picks_header
 docdict["picks_ica"] = """
