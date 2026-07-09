c7a07da185aea14cd0239810657ccffa7d707517
Eric Larson <larson.eric.d@gmail.com>
2026-07-09 13:08:26 -0400

    FIX: Code mode

--- mne/utils/docs.py
+++ mne/utils/docs.py
@@ -1723,7 +1723,7 @@ fig_facecolor : str | tuple
 
 docdict["figure_class"] = """
 figure_class : class
-    The backend specific `MNEBrowseFigure` class to use. This is typically used
+    The backend specific ``MNEBrowseFigure`` class to use. This is typically used
     to pass a subclass in order to customize the plot. This parameter requires
     cooperation from the backend, and is currently only supported by the
     ``matplotlib`` backend.
