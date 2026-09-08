d904193f33a48b3e74c8b5de3e1dcfd543f09501
Daniel McCloy <dan@mccloy.info>
2026-09-08 16:26:02 -0500

    add Sequence to numpydoc xref aliases

--- doc/conf.py
+++ doc/conf.py
@@ -220,6 +220,7 @@ numpydoc_xref_aliases = {
     "path-like": ":term:`path-like`",
     "array-like": ":term:`array_like <numpy:array_like>`",
     "Path": ":class:`python:pathlib.Path`",
+    "Sequence": ":class:`python:collections.abc.Sequence`",
     "bool": ":ref:`bool <python:typebool>`",
     # Matplotlib
     "colormap": ":ref:`colormap <matplotlib:colormaps>`",
