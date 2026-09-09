5df8514fbf21dc9d999de4dc384587ffd1efb201
Eric Larson <larson.eric.d@gmail.com>
2026-09-09 15:59:49 -0400

    PyVista try again [circle deploy] [skip azp] [skip actions]

--- tools/circleci_dependencies.sh
+++ tools/circleci_dependencies.sh
@@ -17,7 +17,7 @@ uv pip install --upgrade --overrides tools/circleci_uv_overrides.txt \
     --group=doc-full \
     "mne-bids @ https://github.com/mne-tools/mne-bids/archive/refs/heads/main.zip" \
     "mne-qt-browser @ https://github.com/mne-tools/mne-qt-browser/archive/refs/heads/main.zip" \
-    "pyvista @ https://github.com/pyvista/pyvista/archive/f7df2e1e5b14ea79f6cca1f50dd10cb8008a54a6.zip" \
+    "pyvista @ https://github.com/pyvista/pyvista/archive/b2d3a65bffc881a85673b911b02d90f1047bc7cf.zip" \
     "pyvistaqt @ https://github.com/pyvista/pyvistaqt/archive/refs/heads/main.zip" \
     "sphinx-gallery @ https://github.com/sphinx-gallery/sphinx-gallery/archive/refs/heads/master.zip" \
     -r doc/sphinxext/related_software.txt
