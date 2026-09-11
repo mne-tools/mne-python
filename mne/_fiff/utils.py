03f60510b19d564e1ede2001c4236b82897a2023
Eric Larson <larson.eric.d@gmail.com>
2026-09-11 12:10:40 -0400

    Hotfix

--- mne/_fiff/utils.py
+++ mne/_fiff/utils.py
@@ -50,7 +50,7 @@ def _check_orig_units(orig_units):
         # character in shift-jis encoding, it gets read in as utf-8, where `\xca` is an
         # (uppercase) Ê. Elsewhere in the codebase we call a `.lower()` on these strings
         # so if the keys here aren't also `.lower()` then the key will fail to be
-        matched.
+        # matched.
         remap_dict["\x83\xcaV".lower()] = "µV"  # for shift-jis mu, use micro
         if unit.lower() in remap_dict:
             orig_units_remapped[ch_name] = remap_dict[unit.lower()]
