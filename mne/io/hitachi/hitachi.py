a54b65dbd081b4f8e54d1b8e75385dc8be6b624f
Daniel McCloy <dan@mccloy.info>
2026-09-15 09:00:57 -0500

    default float sfreq for hitachi

--- mne/io/hitachi/hitachi.py
+++ mne/io/hitachi/hitachi.py
@@ -4,7 +4,6 @@
 
 import datetime as dt
 import re
-from typing import cast
 
 import numpy as np
 
@@ -147,7 +146,8 @@ def _get_hitachi_info(fname, S_offset, D_offset, ignore_names):
     subject_info = dict()
     ch_wavelengths = dict()
     fnirs_wavelengths = [None, None]
-    meas_date = age = ch_names = sfreq = None
+    meas_date = age = ch_names = None
+    sfreq = -1.0
     with open(fname, "rb") as fid:
         lines = fid.read()
     lines = lines.decode("latin-1").rstrip("\r\n")
@@ -311,8 +311,9 @@ def _get_hitachi_info(fname, S_offset, D_offset, ignore_names):
     if subject_info:
         info_extra["subject_info"] = subject_info
 
+    assert sfreq > 0.0, "failed to determine sampling frequency from file header"
     # Create mne structure
-    info = create_info(ch_names, cast(float, sfreq), ch_types=ch_types)
+    info = create_info(ch_names, sfreq, ch_types=ch_types)
     with info._unlock():
         info.update(info_extra)
         info["meas_date"] = meas_date
