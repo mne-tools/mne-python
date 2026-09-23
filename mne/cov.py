2a4d7aeeb294dde7e859e5461d42cfbc4ce6eba2
Clemens Brunner <clemens.brunner@gmail.com>
2026-09-23 15:05:06 +0200

    Fix all occurrences

--- mne/cov.py
+++ mne/cov.py
@@ -504,8 +504,7 @@ class Covariance(dict):
             matplotlib tick locator (may sometimes be inaccurate, use array for
             accuracy). If array-like, the array values are used as the contour levels.
             The values should be in µV for EEG, fT for magnetometers and fT/m for
-            gradiometers. If ``colorbar=True``, the colorbar will have ticks
-            corresponding to the contour levels. Default is ``6``.
+            gradiometers. Default is ``6``.
         outlines : 'head' | dict | None
             The outlines to be drawn. If 'head', the default head scheme will be
             drawn. If dict, each key refers to a tuple of x and y positions, the values
